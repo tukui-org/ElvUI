9f8ae3368ec2232a9e1a75462806db76508df88d
Alexandru Fomin <yo3fxy@gmail.com>
2010-02-18 04:03:31 +0800

    Tukui/mods/error.lua: fix EOL errors

--- Interface/AddOns/Tukui/mods/error.lua
+++ Interface/AddOns/Tukui/mods/error.lua
@@ -1,1 +1,21 @@
-if not TukuiErrorHide == true then return endlocal tError = CreateFrame('Frame', 'tError')local blacklist = {	[ERR_NO_ATTACK_TARGET] = true,	[OUT_OF_ENERGY] = true,	[ERR_ABILITY_COOLDOWN] = true,	[SPELL_FAILED_NO_COMBO_POINTS] = true,	[SPELL_FAILED_SPELL_IN_PROGRESS] = true,	[ERR_SPELL_COOLDOWN] = true,} UIErrorsFrame:UnregisterEvent"UI_ERROR_MESSAGE"local UI_ERROR_MESSAGE = function(self, event, error)	if(not blacklist[error]) then		UIErrorsFrame:AddMessage(error, 1, .1, .1)	endend tError:RegisterEvent('UI_ERROR_MESSAGE', UI_ERROR_MESSAGE)
+if not TukuiErrorHide == true then return end
+
+local tError = CreateFrame('Frame', 'tError')
+
+local blacklist = {
+	[ERR_NO_ATTACK_TARGET] = true,
+	[OUT_OF_ENERGY] = true,
+	[ERR_ABILITY_COOLDOWN] = true,
+	[SPELL_FAILED_NO_COMBO_POINTS] = true,
+	[SPELL_FAILED_SPELL_IN_PROGRESS] = true,
+	[ERR_SPELL_COOLDOWN] = true,
+}
+ 
+UIErrorsFrame:UnregisterEvent"UI_ERROR_MESSAGE"
+local UI_ERROR_MESSAGE = function(self, event, error)
+	if(not blacklist[error]) then
+		UIErrorsFrame:AddMessage(error, 1, .1, .1)
+	end
+end
+ 
+tError:RegisterEvent('UI_ERROR_MESSAGE', UI_ERROR_MESSAGE)
