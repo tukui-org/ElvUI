58f78652acc7c9d5568f099306a44c5697b6b088
Tukz <flip2k3@globetrotter.net>
2010-02-19 19:13:23 -0500

    UTF8 and Localization fixes for loot frame. (mainly for counter on button)

--- Interface/AddOns/Tukui/mods/lootroll.lua
+++ Interface/AddOns/Tukui/mods/lootroll.lua
@@ -270,30 +270,30 @@ end
 
 local locale = GetLocale()
 local rollpairs = locale == "deDE" and {
-	["(.*) passt automatisch bei (.+), weil er den Gegenstand nicht benutzen kann.$"]  = "pass",
-	["(.*) w\195\188rfelt nicht f\195\188r: (.+|r)$"] = "pass",
-	["(.*) hat f\195\188r (.+) 'Gier' ausgew\195\164hlt"] = "greed",
-	["(.*) hat f\195\188r (.+) 'Bedarf' ausgew\195\164hlt"] = "need",
-	["(.*) has selected Disenchant for: (.+)"]  = "disenchant",
+	["(.*) passt automatisch bei (.+), weil [ersi]+ den Gegenstand nicht benutzen kann.$"]  = "pass",
+	["(.*) würfelt nicht für: (.+|r)$"] = "pass",
+	["(.*) hat für (.+) 'Gier' ausgewählt"] = "greed",
+	["(.*) hat für (.+) 'Bedarf' ausgewählt"] = "need",
+	["(.*) hat für '(.+)' Entzauberung gewählt."]  = "disenchant",
 } or locale == "frFR" and {
-	["(.*) a pass\195\169 pour\194\160: (.+) parce qu'((il)|(elle)) ne peut pas ramasser cette objet.$"]  = "pass",
-	["(.*) a pass\195\169 pour\194\160: (.+)"]  = "pass",
-	["(.*) a choisi Cupidit\195\169 pour\194\160: (.+)"] = "greed",
-	["(.*) a choisi Besoin pour\194\160: (.+)"]  = "need",
-	["(.*) has selected Disenchant for: (.+)"]  = "disenchant",
+	["(.*) a passé pour : (.+) parce qu'((il)|(elle)) ne peut pas ramasser cette objet.$"]  = "pass",
+	["(.*) a passé pour : (.+)"]  = "pass",
+	["(.*) a choisi Cupidité pour : (.+)"] = "greed",
+	["(.*) a choisi Besoin pour : (.+)"]  = "need",
+	["(.*) a choisi Désenchantement pour : (.+)"]  = "disenchant",
 } or locale == "zhTW" and {
-	["^(.*) \232\135\170\229\139\149\232\168\173\229\174\154\231\130\186\230\148\190\230\163\132: (.+) \229\155\160\231\130\186\228\187\150/\229\165\185\228\184\141\232\131\189\230\139\191\229\143\150\232\169\178\231\137\169\229\147\129.$"] = "pass",
-	["^(.*) \230\148\190\230\163\132: (.+|r)$"]  = "pass",
-	["(.*) \229\183\177\233\129\184\230\147\135\232\178\170\229\169\170: (.+)"] = "greed",
-	["(.*) \229\183\177\233\129\184\230\147\135\233\156\128\230\177\130: (.+)"]  = "need",
-	["(.*) has selected Disenchant for: (.+)"]  = "disenchant",
+	["(.*)自動放棄:(.+)，因為"]  = "pass",
+	["(.*)放棄了:(.+)"] = "pass",
+	["(.*)選擇了貪婪優先:(.+)"] = "greed",
+	["(.*)選擇了需求優先:(.+)"] = "need",
+	["(.*)選擇分解:(.+)"] = "disenchant",
 } or locale == "ruRU" and {
-	["^(.*) автоматически передает предмет (.+) поскольку не может его забрать.$"] = "pass",
-	["^(.*) пропускает розыгрыш предмета (.+|r) поскольку не может его забрать.$"] = "pass",
-	["(.*) отказывается от предмета (.+)%p"]  = "pass",
-	["Разыгрывается%p (.*)%p (.+): \"Не откажусь\""] = "greed",
-	["Разыгрывается%p (.*)%p (.+): \"Мне это нужно\""] = "need",
-	["Разыгрывается%p (.*)%p (.+): \"Распылить\""] = "disenchant",
+	["(.*) автоматически передает предмет (.+), поскольку не может его забрать"] = "pass",
+	["(.*) пропускает розыгрыш предмета \"(.+)\", поскольку не может его забрать"] = "pass",
+	["(.*) отказывается от предмета (.+)%."]  = "pass",
+	["Разыгрывается: (.+)%. (.*): \"Не откажусь\""] = "greed",
+	["Разыгрывается: (.+)%. (.*): \"Мне это нужно\""] = "need",
+	["Разыгрывается: (.+)%. (.*): \"Распылить\""] = "disenchant",
 } or {
 	["^(.*) automatically passed on: (.+) because s?he cannot loot that item.$"] = "pass",
 	["^(.*) passed on: (.+|r)$"]  = "pass",
@@ -341,4 +341,3 @@ anchor:SetScript("OnEvent", function(frame, event, addon)
 
 	anchor:SetPoint("CENTER", UIParent, "CENTER", 0, -200)
 end)
-
