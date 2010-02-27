39ba1805c0e32705c0c5f42c46cb102e721b11bc
Tukz <flip2k3@globetrotter.net>
2010-02-26 19:02:07 -0500

    add print feature to general chat on how much gold you got from selling junk.

--- Interface/AddOns/Tukui/mods/selljunk.lua
+++ Interface/AddOns/Tukui/mods/selljunk.lua
@@ -12,19 +12,34 @@ AddOn:SetScript("OnEvent", OnEvent)
 --	Auto SELL JUNK
 ------------------------------------------------------------------------
 local function MERCHANT_SHOW(...)
-	for bagIndex = 0, 4 do
-		if GetContainerNumSlots(bagIndex) > 0 then
-			for slotIndex = 1, GetContainerNumSlots(bagIndex) do
-				if select(2,GetContainerItemInfo(bagIndex, slotIndex)) then
-					local quality = select(3, string.find(GetContainerItemLink(bagIndex, slotIndex), "(|c%x+)"))
-					
-					if quality == ITEM_QUALITY_COLORS[0].hex then
-						UseContainerItem(bagIndex, slotIndex)
-					end
-				end
-			end
-		end
-	end
+   local cost = 0
+   for bagIndex=0,4 do
+      for s=1,GetContainerNumSlots(bagIndex) do
+         local l = GetContainerItemLink(bagIndex, s)
+         if l then
+            local p = select(11, GetItemInfo(l))*select(2, GetContainerItemInfo(bagIndex, s))
+            if select(3, GetItemInfo(l))==0 then
+               UseContainerItem(bagIndex, s)
+               PickupMerchantItem()
+               cost = cost+p
+            end
+         end
+      end
+   end
+   if cost>0 then
+      local gold = floor(math.abs(cost) / 10000)
+      local silver = mod(floor(math.abs(cost) / 100), 100)
+      local copper = mod(floor(math.abs(cost)), 100)
+      
+      if gold ~= 0 then
+         cost = format("%s|cffffd700g|r %s|cffc7c7cfs|r %s|cffeda55fc|r", gold, silver, copper)
+      elseif silver ~= 0 then
+         cost = format("%s|cffc7c7cfs|r %s|cffeda55fc|r", silver, copper)
+      else
+         cost = format("%s|cffeda55fc|r", copper)
+      end
+      DEFAULT_CHAT_FRAME:AddMessage("+"..cost)
+   end
 end
 
 AddOn:RegisterEvent("MERCHANT_SHOW")
