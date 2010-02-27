4aef3af3b64ebddd604a0663763ec9fcec893fb4
Tukz <flip2k3@globetrotter.net>
2010-02-26 19:45:50 -0500

    hide error not working as intended, rewrite of error.lua and now only hide "spamming" things. Show all others error we need to know.

--- Interface/AddOns/Tukui/mods/error.lua
+++ Interface/AddOns/Tukui/mods/error.lua
@@ -2,6 +2,90 @@ if not TukuiErrorHide == true then return end
 
 local tError = CreateFrame('Frame', 'tError')
 
+local function argcheck(value, num, ...)
+	assert(type(num) == 'number', "Bad argument #2 to 'argcheck' (number expected, got "..type(num)..")")
+
+	for i=1,select("#", ...) do
+		if type(value) == select(i, ...) then return end
+	end
+
+	local types = strjoin(", ", ...)
+	local name = string.match(debugstack(2,2,0), ": in function [`<](.-)['>]")
+	error(("Bad argument #%d to '%s' (%s expected, got %s"):format(num, name, types, type(value)), 3)
+end
+
+local print = function(...) print("|cff33ff99tError:|r ", ...) end
+local error = function(...) print("|cffff0000Error:|r "..string.format(...)) end
+
+local event_metatable = {
+	__call = function(funcs, self, ...)
+		for _, func in pairs(funcs) do
+			func(self, ...)
+		end
+	end,
+}
+
+local RegisterEvent = tError.RegisterEvent
+function tError:RegisterEvent(event, func)
+	argcheck(event, 2, 'string')
+
+	if(type(func) == 'string' and type(self[func]) == 'function') then
+		func = self[func]
+	end
+
+	local curev = self[event]
+	if(curev and func) then
+		if(type(curev) == 'function') then
+			self[event] = setmetatable({curev, func}, event_metatable)
+		else
+			for _, infunc in next, curev do
+				if(infunc == func) then return end
+			end
+
+			table.insert(curev, func)
+		end
+	elseif(self:IsEventRegistered(event)) then
+		return
+	else
+		if(func) then
+			self[event] = func
+		elseif(not self[event]) then
+			error("Handler for event [%s] does not exist.", event)
+		end
+
+		RegisterEvent(self, event)
+	end
+end
+
+local UnregisterEvent = tError.UnregisterEvent
+
+function tError:UnregisterEvent(event, func)
+	argcheck(event, 2, 'string')
+
+	local curev = self[event]
+	if(type(curev) == 'table' and func) then
+		for k, infunc in pairs(curev) do
+			if(infunc == func) then
+				curev[k] = nil
+
+				if(#curev == 0) then
+					table.remove(curev, k)
+					UnregisterEvent(self, event)
+				end
+			end
+		end
+	else
+		self[event] = nil
+		UnregisterEvent(self, event)
+	end
+end
+
+tError:SetScript('OnEvent', function(self, event, ...)
+	self[event](self, event, ...)
+end)
+
+tError.dummy = function() end
+
 local blacklist = {
 	[ERR_NO_ATTACK_TARGET] = true,
 	[OUT_OF_ENERGY] = true,
@@ -10,12 +94,13 @@ local blacklist = {
 	[SPELL_FAILED_SPELL_IN_PROGRESS] = true,
 	[ERR_SPELL_COOLDOWN] = true,
 }
- 
+
 UIErrorsFrame:UnregisterEvent"UI_ERROR_MESSAGE"
+
 local UI_ERROR_MESSAGE = function(self, event, error)
 	if(not blacklist[error]) then
 		UIErrorsFrame:AddMessage(error, 1, .1, .1)
 	end
 end
- 
+
 tError:RegisterEvent('UI_ERROR_MESSAGE', UI_ERROR_MESSAGE)
