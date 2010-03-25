736dfa47f9490a87c81a8d888d8a4aa3347b5aa9
unknown <Tukz@.(none)>
2010-03-25 11:30:41 -0400

    fix the datatext haste always displaying 0

--- Interface/AddOns/Tukui/scripts/datatext.lua
+++ Interface/AddOns/Tukui/scripts/datatext.lua
@@ -915,19 +915,19 @@ if not db.haste == nil or db.haste > 0 then
 
 	local int = 1
 
-	local spellhaste = GetCombatRating(20)
-	local rangedhaste = GetCombatRating(19)
-	local attackhaste = GetCombatRating(18)
-
-	if attackhaste > spellhaste and select(2, UnitClass("Player")) ~= "HUNTER" then
-		haste = attackhaste
-	elseif select(2, UnitClass("Player")) == "HUNTER" then
-		haste = rangedhaste
-	else
-		haste = spellhaste
-	end
-
 	local function Update(self, t)
+		spellhaste = GetCombatRating(20)
+		rangedhaste = GetCombatRating(19)
+		attackhaste = GetCombatRating(18)
+		
+		if attackhaste > spellhaste and select(2, UnitClass("Player")) ~= "HUNTER" then
+			haste = attackhaste
+		elseif select(2, UnitClass("Player")) == "HUNTER" then
+			haste = rangedhaste
+		else
+			haste = spellhaste
+		end
+		
 		int = int - t
 		if int < 0 then
 			Text:SetText(haste.." "..tukuilocal.datatext_playerhaste)
