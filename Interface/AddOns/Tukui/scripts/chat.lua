75764b921389e1363e95b08cfe35b6fe5b409f56
unknown <Tukz@.(none)>
2010-06-23 13:21:40 -0400

    Enhance/rewrite a Blizzard feature, chatframe mousewheel.

--- Interface/AddOns/Tukui/scripts/chat.lua
+++ Interface/AddOns/Tukui/scripts/chat.lua
@@ -470,3 +470,29 @@ function TukuiDB.ChatCopyButtons()
 	end
 end
 TukuiDB.ChatCopyButtons()
+
+
+------------------------------------------------------------------------
+--	Enhance/rewrite a Blizzard feature, chatframe mousewheel.
+------------------------------------------------------------------------
+
+local ScrollLines = 3 -- set the jump when a scroll is done !
+function FloatingChatFrame_OnMouseScroll(self, delta)
+	if delta < 0 then
+		if IsShiftKeyDown() then
+			self:ScrollToBottom()
+		else
+			for i = 1, ScrollLines do
+				self:ScrollDown()
+			end
+		end
+	elseif delta > 0 then
+		if IsShiftKeyDown() then
+			self:ScrollToTop()
+		else
+			for i = 1, ScrollLines do
+				self:ScrollUp()
+			end
+		end
+	end
+end
