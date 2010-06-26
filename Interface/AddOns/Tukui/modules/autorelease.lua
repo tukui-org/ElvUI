5cdf296cef36490f626382ea91028fda69bf5ae1
unknown <Tukz@.(none)>
2010-06-26 19:37:47 -0400

    disable autorelease in battleground for shaman, yeah reincarnation!

--- Interface/AddOns/Tukui/modules/autorelease.lua
+++ Interface/AddOns/Tukui/modules/autorelease.lua
@@ -8,11 +8,13 @@ WINTERGRASP = tukuilocal.mount_wintergrasp
 local autoreleasepvp = CreateFrame("frame")
 autoreleasepvp:RegisterEvent("PLAYER_DEAD")
 autoreleasepvp:SetScript("OnEvent", function(self, event)
-	if (tostring(GetZoneText()) == WINTERGRASP) then
-		RepopMe()
-	end
+	if TukuiDB.myclass ~= "SHAMAN" then
+		if (tostring(GetZoneText()) == WINTERGRASP) then
+			RepopMe()
+		end
 
-	if MiniMapBattlefieldFrame.status == "active" then
-		RepopMe()
+		if MiniMapBattlefieldFrame.status == "active" then
+			RepopMe()
+		end
 	end
 end)
