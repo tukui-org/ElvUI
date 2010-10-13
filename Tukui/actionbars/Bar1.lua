6864cbe8a43d915a261c7510dffa5f530100f3e4
unknown <Tukz@.(none)>
2010-10-13 02:29:05 -0400

    locks and rogues, sorry, i forgot to add you stance bar for shadowdance and demon form. :P

--- Tukui/actionbars/Bar1.lua
+++ Tukui/actionbars/Bar1.lua
@@ -25,8 +25,9 @@ local Page = {
 	["DRUID"] = "[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] %s; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;",
 	["WARRIOR"] = "[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;",
 	["PRIEST"] = "[bonusbar:1] 7;",
-	["ROGUE"] = "[bonusbar:1] 7;",
-	["DEFAULT"] = "[bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6; [bonusbar:5] 11;"
+	["ROGUE"] = "[bonusbar:1] 7; [form:3] 6",
+	["WARLOCK"] = "[form:2] 6;",
+	["DEFAULT"] = "[bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6; [bonusbar:5] 11;",
 }
 
 local function GetBar()
