eb572a8fe6c26a01b266dedd4db7b7edcb66abd7
unknown <Tukz@.(none)>
2010-10-15 18:44:20 -0400

    this is needed for totem bar.

--- Tukui/actionbars/BarTotem.lua
+++ Tukui/actionbars/BarTotem.lua
@@ -1,3 +1,5 @@
+if TukuiCF["actionbar"].enable ~= true then return end
+
 -- we just use default totem bar for shaman
 -- we parent it to our shapeshift bar.
 -- This is approx the same script as it was in WOTLK Tukui version.
