7f2536f6ae21b6efc103c25b267a10c0a4571017
Tukz <tukz@tukui.org>
2010-10-17 13:11:44 -0400

    "Tukz, you need to fix your git client on your computer", yeah I know, i was just too lazy to do it after reinstalling my OS.

--- Tukui/modules/temp.lua
+++ Tukui/modules/temp.lua
@@ -1,4 +1,9 @@
 -- temporary stuff, this file is used only for coding test purpose.
 
+-- local frame= CreateFrame("frame", nil, UIParent, "SecureHandlerStateTemplate")
+-- local run = "print(stateid, newstate)"
+-- frame:SetAttribute("_onstate-page", run)
+-- RegisterStateDriver(frame, "page", "[bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6; [bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10; [bonusbar:5] 11; 1")
+
 
 
