298ec479b70b68ba08564224d5383cbdfad5da0a
Tukz <tukz@tukui.org>
2010-10-18 18:13:36 -0400

    Not needed anymore, already implemented on Blizzard default map.

--- Tukui/scripts/map.lua
+++ Tukui/scripts/map.lua
@@ -24,8 +24,6 @@ movebutton:SetBackdrop( {
 
 local addon = CreateFrame('Frame')
 addon:RegisterEvent('PLAYER_LOGIN')
-addon:RegisterEvent("PARTY_MEMBERS_CHANGED")
-addon:RegisterEvent("RAID_ROSTER_UPDATE")
 addon:RegisterEvent("PLAYER_REGEN_ENABLED")
 addon:RegisterEvent("PLAYER_REGEN_DISABLED")
 
@@ -140,14 +138,6 @@ movebutton:EnableMouse(true)
 movebutton:SetScript("OnMouseDown", OnMouseDown)
 movebutton:SetScript("OnMouseUp", OnMouseUp)
 
--- the classcolor function
-local function UpdateIconColor(self)
-	if not self.unit then return end -- it seem sometime self.unit is not found causing lua error. idn why but anyway.
-	local color = RAID_CLASS_COLORS[select(2, UnitClass(self.unit))]
-	if not color then return end -- sometime color return nil
-	self.icon:SetVertexColor(color.r, color.g, color.b)
-end
-
 local OnEvent = function(self, event)
 	-- fixing a stupid bug error by blizzard on default ui :x
 	if event == "PLAYER_REGEN_DISABLED" then
@@ -156,22 +146,6 @@ local OnEvent = function(self, event)
 	elseif event == "PLAYER_REGEN_ENABLED" then
 		WorldMapFrameSizeDownButton:Enable()
 		WorldMapFrameSizeUpButton:Enable()
-	elseif event == "RAID_ROSTER_UPDATE" or event == "PARTY_MEMBERS_CHANGED" then
-		for r=1, 40 do
-			if not _G["WorldMapRaid"..r] then return end
-			if UnitInParty(_G["WorldMapRaid"..r].unit) then
-				_G["WorldMapRaid"..r].icon:SetTexture("Interface\\AddOns\\Tukui\\media\\textures\\Party")
-			else
-				_G["WorldMapRaid"..r].icon:SetTexture("Interface\\AddOns\\Tukui\\media\\textures\\Raid")
-			end
-			_G["WorldMapRaid"..r]:SetScript("OnUpdate", UpdateIconColor)
-		end
-
-		for p=1, 4 do
-			if not _G["WorldMapParty"..p] then return end
-			_G["WorldMapParty"..p].icon:SetTexture("Interface\\AddOns\\Tukui\\media\\textures\\Party")
-			_G["WorldMapParty"..p]:SetScript("OnUpdate", UpdateIconColor)
-		end
 	end
 end
 addon:SetScript("OnEvent", OnEvent)
