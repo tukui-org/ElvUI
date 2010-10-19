54df97e7fffefe2f8efb628df8f7635e2f6dadae
Tukz <tukz@tukui.org>
2010-10-18 23:21:43 -0400

    disable /mss command if in combat because of a taint issue.

--- Tukui/actionbars/BarShift.lua
+++ Tukui/actionbars/BarShift.lua
@@ -19,6 +19,9 @@ TukuiShift:SetMovable(true)
 TukuiShift:SetUserPlaced(true)
 local ssmove = false
 local function showmovebutton()
+	-- don't allow moving while in combat
+	if InCombatLockdown() then print(ERR_NOT_IN_COMBAT) return end
+	
 	if ssmove == false then
 		ssmove = true
 		ssmover:SetAlpha(1)
