2d2dd9e70909967f5daa94dbfe18d9ea12f9d3d7
Joschasa <Andre@.(none)>
2010-10-19 17:14:40 +0200

    Ability to add watched currency to panel (cherry picked from commit 93a0fb626977f20365e596726e01ead85900cbc4)

--- Tukui/config/config.lua
+++ Tukui/config/config.lua
@@ -113,6 +113,7 @@ TukuiCF["datatext"] = {
 	["crit"] = 0,                          -- show your crit rating on panels.
 	["avd"] = 0,                           -- show your current avoidance against the level of the mob your targeting
 	["armor"] = 0,                         -- show your armor value against the level mob you are currently targeting
+	["currency"] = 0,                      -- show your tracked currency on panels
 
 	["battleground"] = true,               -- enable 3 stats in battleground only that replace stat1,stat2,stat3.
 	["time24"] = true,                     -- set time to 24h format.
---
+++ Tukui/datatext/currency.lua
@@ -0,0 +1,42 @@
+--------------------------------------------------------------------
+ -- CURRENCY
+--------------------------------------------------------------------
+
+if TukuiCF["datatext"].currency and TukuiCF["datatext"].currency > 0 then
+	local Stat = CreateFrame("Frame")
+	Stat:EnableMouse(true)
+	Stat:SetFrameStrata("BACKGROUND")
+	Stat:SetFrameLevel(3)
+
+	local Text  = TukuiInfoLeft:CreateFontString(nil, "OVERLAY")
+	Text:SetFont(TukuiCF.media.font, TukuiCF["datatext"].fontsize)
+	TukuiDB.PP(TukuiCF["datatext"].currency, Text)
+	
+	local function update()
+		local _text = "---"
+		for i = 1, GetNumWatchedTokens() do
+			local name, count, _, _, _ = GetBackpackCurrencyInfo(i)
+			if name and count then
+				if(i ~= 1) then _text = _text .. " " else _text = "" end
+				words = { strsplit(" ", name) }
+				for _, word in ipairs(words) do
+					_text = _text .. string.sub(word,1,1)
+				end
+				_text = _text .. ": " .. count
+			end
+		end
+		
+		Text:SetText(_text)
+	end
+	
+	local function OnEvent(self, event, ...)
+		update()
+		self:SetAllPoints(Text)
+		Stat:UnregisterEvent("PLAYER_LOGIN")	
+	end
+
+	Stat:RegisterEvent("PLAYER_LOGIN")	
+	hooksecurefunc("BackpackTokenFrame_Update", update)
+	Stat:SetScript("OnEvent", OnEvent)
+	Stat:SetScript("OnMouseDown", function() ToggleCharacter("TokenFrame") end)
+end
