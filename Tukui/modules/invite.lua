d8a618d402bc5b8fbe367bb914fe85e0d0dc7065
Elv22 <flipside_22@live.com>
2010-11-04 13:58:59 -0400

    Allow Auto-Invite if your a Raid Leader or Raid Assistant

--- Tukui/modules/invite.lua
+++ Tukui/modules/invite.lua
@@ -58,7 +58,7 @@ local ainvkeyword = "invite"
 local autoinvite = CreateFrame("frame")
 autoinvite:RegisterEvent("CHAT_MSG_WHISPER")
 autoinvite:SetScript("OnEvent", function(self,event,arg1,arg2)
-	if ((not UnitExists("party1") or IsPartyLeader("player")) and arg1:lower():match(ainvkeyword)) and ainvenabled == true then
+	if ((not UnitExists("party1") or IsPartyLeader("player") or IsRaidOfficer("player") or IsRaidLeader("player")) and arg1:lower():match(ainvkeyword)) and ainvenabled == true then
 		InviteUnit(arg2)
 	end
 end)
