c540c22d25163b2965f0515475747ccb08ab6b08
Elv22 <flipside_22@live.com>
2010-11-08 17:10:56 -0500

    Tank armor stat will now be whatever 3+ levels you are instead of always 81, 82, 83

--- Tukui/datatext/stat2.lua
+++ Tukui/datatext/stat2.lua
@@ -22,32 +22,42 @@ if TukuiCF["datatext"].stat2 and TukuiCF["datatext"].stat2 > 0 then
 				--Setup Armor Tooltip
 				self:SetAllPoints(Text)
 				self:SetScript("OnEnter", function()
-					if TukuiDB.Role == "Tank" then
-						GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, TukuiDB.Scale(6));
-						GameTooltip:ClearAllPoints()
-						GameTooltip:SetPoint("BOTTOM", self, "TOP", 0, TukuiDB.mult)
-						GameTooltip:ClearLines()
-						GameTooltip:AddLine(tukuilocal.datatext_mitigation)
-						GameTooltip:AddLine(' ')
-						local lv = 83
-						for i = 1, 4 do
-							local format = string.format
-							local mitigation = (effectiveArmor/(effectiveArmor+(467.5*lv-22167.5)));
-							if mitigation > .75 then
-								mitigation = .75
-							end
-							GameTooltip:AddDoubleLine(lv,format("%.2f", mitigation*100) .. "%",1,1,1)
-							lv = lv - 1
+				if TukuiDB.Role == "Tank" then
+					GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, TukuiDB.Scale(6));
+					GameTooltip:ClearAllPoints()
+					GameTooltip:SetPoint("BOTTOM", self, "TOP", 0, TukuiDB.mult)
+					GameTooltip:ClearLines()
+					GameTooltip:AddLine(tukuilocal.datatext_mitigation)
+					GameTooltip:AddLine(' ')
+					local lv = TukuiDB.level +3
+					for i = 1, 4 do
+						local format = string.format
+						local mitigation
+						if lv < 60 then
+							mitigation = (effectiveArmor/(effectiveArmor+400+(85 * lv)));
+						else
+							mitigation = (effectiveArmor/(effectiveArmor+(467.5*lv-22167.5)));
 						end
-						if UnitLevel("target") > 0 and UnitLevel("target") < UnitLevel("player") then
-							mitigation = (effectiveArmor/(effectiveArmor+(467.5*(UnitLevel("target"))-22167.5)));
-							if mitigation > .75 then
-								mitigation = .75
-							end
-							GameTooltip:AddDoubleLine(UnitLevel("target"),format("%.2f", mitigation*100) .. "%",1,1,1)
+						if mitigation > .75 then
+							mitigation = .75
 						end
-						GameTooltip:Show()
+						GameTooltip:AddDoubleLine(lv,format("%.2f", mitigation*100) .. "%",1,1,1)
+						lv = lv - 1
 					end
+					lv = UnitLevel("target")
+					if lv > 0 and (lv > TukuiDB.level + 3 or lv < TukuiDB.level) then
+						if lv < 60 then
+							mitigation = (effectiveArmor/(effectiveArmor+400+(85 * lv)));
+						else
+							mitigation = (effectiveArmor/(effectiveArmor+(467.5*lv-22167.5)));
+						end
+						if mitigation > .75 then
+							mitigation = .75
+						end
+						GameTooltip:AddDoubleLine(lv,format("%.2f", mitigation*100) .. "%",1,1,1)
+					end
+					GameTooltip:Show()
+				end
 				end)
 				self:SetScript("OnLeave", function() GameTooltip:Hide() end)				
 			elseif TukuiDB.Role == "Caster" then
