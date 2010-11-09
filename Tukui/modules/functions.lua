fce89a5335cfd0ba3448e31b6eb1edfde1c3b2d2
Elv22 <flipside_22@live.com>
2010-11-09 17:24:51 -0500

    Just adding a miniscript to determine if your resolution is bigger than 1440 (width) or not

--- Tukui/modules/functions.lua
+++ Tukui/modules/functions.lua
@@ -6,6 +6,12 @@ function TukuiDB.UIScale()
 		if TukuiCF["general"].autoscale ~= true then return end
 		TukuiCF["general"].uiscale = min(2, max(.64, 768/string.match(({GetScreenResolutions()})[GetCurrentResolution()], "%d+x(%d+)")))
 	end
+	
+	if tonumber(string.match(GetCVar("gxResolution"), "(%d+)x%d+")) <= 1440 then
+		TukuiDB.lowversion = true
+	else
+		TukuiDB.lowversion = false
+	end
 end
 TukuiDB.UIScale()
 
