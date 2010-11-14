b6b593426b8c9b0a574db2e4441064d72118ba02
Elv22 <flipside_22@live.com>
2010-11-13 20:01:55 -0500

    Move the character frame to the right of the bank bags frame if it is open

--- Tukui/scripts/bags.lua
+++ Tukui/scripts/bags.lua
@@ -59,14 +59,29 @@ local function Stuffing_OnShow()
 	Stuffing:SearchReset()
 end
 
+local function MoveChar()
+	if StuffingFrameBank and StuffingFrameBank:IsShown() then
+		CharacterFrame:ClearAllPoints()
+		CharacterFrame:SetPoint("TOPLEFT", StuffingFrameBank, "TOPRIGHT", TukuiDB.Scale(15), 0)
+	end
+end
+
+local function StuffingBank_OnShow()
+	Stuffing.frame:SetScript("OnUpdate", MoveChar)
+end
 
 local function StuffingBank_OnHide()
+	Stuffing.frame:SetScript("OnUpdate", nil)
 	CloseBankFrame()
+	if CharacterFrame:IsShown() then
+		ToggleCharacter("PaperDollFrame")
+		ToggleCharacter("PaperDollFrame")
+	end
+	
 	if Stuffing.frame:IsShown() then
 		Stuffing.frame:Hide()
 	end
 end
-
 
 local function Stuffing_OnHide()
 	if Stuffing.bankFrame and Stuffing.bankFrame:IsShown() then
@@ -74,16 +89,13 @@ local function Stuffing_OnHide()
 	end
 end
 
-
 local function Stuffing_Open()
 	Stuffing.frame:Show()
 end
 
-
 local function Stuffing_Close()
 	Stuffing.frame:Hide()
 end
-
 
 local function Stuffing_Toggle()
 	if Stuffing.frame:IsShown() then
@@ -423,8 +435,9 @@ function Stuffing:InitBank()
 	if self.bankFrame then
 		return
 	end
-
+	MoveChar()
 	local f = self:CreateBagFrame("Bank")
+
 	f:SetScript("OnHide", StuffingBank_OnHide)
 	self.bankFrame = f
 end
@@ -957,7 +970,10 @@ function Stuffing:BANKFRAME_OPENED()
 	if not self.bankFrame then
 		self:InitBank()
 	end
-
+	
+	StuffingBank_OnShow()
+	self.bankFrame:SetScript("OnShow", StuffingBank_OnShow)
+	
 	self:Layout(true)
 	for _, x in ipairs(bags_BANK) do
 		self:BagSlotUpdate(x)
@@ -1390,4 +1406,3 @@ function Stuffing.Menu(self, level)
 	info.tooltipTitle = CLOSE
 	UIDropDownMenu_AddButton(info, level)
 end
-
