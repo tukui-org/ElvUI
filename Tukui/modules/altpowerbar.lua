6b5482ecea3dcced1d9546a6ee6d4657e46b5ac3
Elv22 <flipside_22@live.com>
2010-11-21 10:38:08 -0500

    Forgot to add new file

---
+++ Tukui/modules/altpowerbar.lua
@@ -0,0 +1,7 @@
+-- reposition alt power bar (A new bar in Cataclysm) to top/center of the screen
+-- this bar is seen for the first time in Twilight Highlands with tentacles quest serie.
+
+PlayerPowerBarAlt:ClearAllPoints() 
+PlayerPowerBarAlt:SetPoint("TOP", 0, -32) 
+PlayerPowerBarAlt.ClearAllPoints = TukuiDB.dummy 
+PlayerPowerBarAlt.SetPoint = TukuiDB.dummy
