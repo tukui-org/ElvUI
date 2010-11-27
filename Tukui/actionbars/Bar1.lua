8db7abad819d0baddf7f9fa9dbe00e5df0763888
Elv22 <flipside_22@live.com>
2010-11-27 18:57:29 -0500

    Fix actionbar paging for vehicle/possession finally

--- Tukui/actionbars/Bar1.lua
+++ Tukui/actionbars/Bar1.lua
@@ -25,7 +25,7 @@ local Page = {
 	["WARRIOR"] = "[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;",
 	["PRIEST"] = "[bonusbar:1] 7;",
 	["ROGUE"] = "[bonusbar:1] 7; [form:3] 7;",
-	["DEFAULT"] = "[bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6; [bonusbar:5] 11;",
+	["DEFAULT"] = "[bonusbar:5] 11; [bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6;",
 }
 
 local function GetBar()
@@ -74,15 +74,8 @@ bar:SetScript("OnEvent", function(self, event, ...)
 			end	
 		]])
 		
-		self:SetAttribute("_onstate-bonus", [[
-			for i, button in ipairs(buttons) do
-				button:SetAttribute("actionpage", tonumber(newstate))
-			end
-		]])
-		RegisterStateDriver(self, "bonus", "[bonusbar:5] 11;")	
-		
 		RegisterStateDriver(self, "page", GetBar())
-		RegisterStateDriver(self, "vehicleupdate", "[vehicleui]s2;s1")
+		RegisterStateDriver(self, "vehicleupdate", "[vehicleui] s2;s1")
 	elseif event == "PLAYER_ENTERING_WORLD" then
 		MainMenuBar_UpdateKeyRing()
 		local button
