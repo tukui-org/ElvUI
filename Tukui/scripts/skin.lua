ef17850259bab322b95b86c1ca5e8e2d8edc7ccd
Elv22 <flipside_22@live.com>
2010-11-27 19:24:39 -0500

    Fix mac menu stuff not being skinned

--- Tukui/scripts/skin.lua
+++ Tukui/scripts/skin.lua
@@ -209,39 +209,49 @@ TukuiSkin:SetScript("OnEvent", function(self, event, addon)
 		MacOptionsFrameHeader:SetTexture("")
 		MacOptionsFrameHeader:ClearAllPoints()
 		MacOptionsFrameHeader:SetPoint("TOP", MacOptionsFrame, 0, 0)
- 
+
 		--Skin internal frames
 		TukuiDB.SetTemplate(MacOptionsFrameMovieRecording)
 		TukuiDB.SetTemplate(MacOptionsITunesRemote)
- 
+
 		--Skin buttons
 		SkinButton(_G["MacOptionsFrameCancel"])
 		SkinButton(_G["MacOptionsFrameOkay"])
 		SkinButton(_G["MacOptionsButtonKeybindings"])
 		SkinButton(_G["MacOptionsFrameDefaults"])
 		SkinButton(_G["MacOptionsButtonCompress"])
- 
+
 		--Reposition and resize buttons
 		tPoint, tRTo, tRP, tX, tY =  _G["MacOptionsButtonCompress"]:GetPoint()
 		_G["MacOptionsButtonCompress"]:SetWidth(136)
+		_G["MacOptionsButtonCompress"]:ClearAllPoints()
 		_G["MacOptionsButtonCompress"]:SetPoint(tPoint, tRTo, tRP, tX+4, tY)
- 
+
 		_G["MacOptionsFrameCancel"]:SetWidth(96)
 		_G["MacOptionsFrameCancel"]:SetHeight(22)
 		tPoint, tRTo, tRP, tX, tY =  _G["MacOptionsFrameCancel"]:GetPoint()
+		_G["MacOptionsFrameCancel"]:ClearAllPoints()
 		_G["MacOptionsFrameCancel"]:SetPoint(tPoint, tRTo, tRP, tX-2, tY)
- 
+
 		_G["MacOptionsFrameOkay"]:ClearAllPoints()
 		_G["MacOptionsFrameOkay"]:SetWidth(96)
 		_G["MacOptionsFrameOkay"]:SetHeight(22)
 		_G["MacOptionsFrameOkay"]:SetPoint("LEFT",_G["MacOptionsFrameCancel"],-99,0)
- 
+
 		_G["MacOptionsButtonKeybindings"]:ClearAllPoints()
 		_G["MacOptionsButtonKeybindings"]:SetWidth(96)
 		_G["MacOptionsButtonKeybindings"]:SetHeight(22)
 		_G["MacOptionsButtonKeybindings"]:SetPoint("LEFT",_G["MacOptionsFrameOkay"],-99,0)
- 
+
 		_G["MacOptionsFrameDefaults"]:SetWidth(96)
 		_G["MacOptionsFrameDefaults"]:SetHeight(22)
+
+		-- why these buttons is using game menu template? oO
+		_G["MacOptionsButtonCompressLeft"]:SetAlpha(0)
+		_G["MacOptionsButtonCompressMiddle"]:SetAlpha(0)
+		_G["MacOptionsButtonCompressRight"]:SetAlpha(0)
+		_G["MacOptionsButtonKeybindingsLeft"]:SetAlpha(0)
+		_G["MacOptionsButtonKeybindingsMiddle"]:SetAlpha(0)
+		_G["MacOptionsButtonKeybindingsRight"]:SetAlpha(0)
 	end
 end)
