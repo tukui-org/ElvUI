c5f0c43f39814b5968ea85e05234f8b51f24cbb0
Elv22 <flipside_22@live.com>
2010-11-27 23:56:19 -0500

    Best script ever, and no one gets to use it but me

--- Tukui/modules/spam.lua
+++ Tukui/modules/spam.lua
@@ -74,6 +74,19 @@ end
 ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", SPELL_FILTER)
 
 
+----------------------------------------------------------------------------------
+-- Only enabling this for me because i get asked too much
+----------------------------------------------------------------------------------
+if TukuiDB.myname == "Elv" then
+	local function NOOB_FILTER(self, event, arg1, arg2)
+		if strfind(arg1,"that mount") or strfind(arg1,"that mount?") or strfind(arg1, "mount from") or strfind(arg1, "mount from") or strfind(arg1, "your mount") or strfind(arg1, "the mount") --[[ <-- third world lingo ]] then
+			SendChatMessage("I got this mount from a world drop inside Desolace, you need to open up some crates near the shore, it only works for the first one you open per day.", "WHISPER", nil, arg2)
+		end
+	end
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", NOOB_FILTER)	
+end
+
+
 
 
 
