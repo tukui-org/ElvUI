1c0331370c5d80b2aa64484943358dd93febc1eb
Elv22 <flipside_22@live.com>
2010-12-01 14:44:48 -0500

    Fix taint with right chat animation

--- Tukui/datatext/currency.lua
+++ Tukui/datatext/currency.lua
@@ -9,7 +9,7 @@ if TukuiCF["datatext"].currency and TukuiCF["datatext"].currency > 0 then
 	Stat:SetFrameLevel(3)
 
 	local Text  = TukuiInfoLeft:CreateFontString(nil, "OVERLAY")
-	Text:SetFont(TukuiCF.media.font, TukuiCF["datatext"].fontsize, "THINOUTLINE")
+		Text:SetFont(TukuiCF.media.font, TukuiCF["datatext"].fontsize, "THINOUTLINE")
 	Text:SetShadowOffset(TukuiDB.mult, -TukuiDB.mult)
 	TukuiDB.PP(TukuiCF["datatext"].currency, Text)
 	
