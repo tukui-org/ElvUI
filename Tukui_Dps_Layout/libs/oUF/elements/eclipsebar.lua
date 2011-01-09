eb4ba9291ab9a4555274c1130f0760f76de6cb25
Elv22 <flipside_22@live.com>
2011-01-09 17:02:02 -0500

    Hide eclipsebar when in vehicle

--- Tukui_Dps_Layout/libs/oUF/elements/eclipsebar.lua
+++ Tukui_Dps_Layout/libs/oUF/elements/eclipsebar.lua
@@ -37,7 +37,9 @@ local UPDATE_VISIBILITY = function(self, event)
 	-- check form/mastery
 	local showBar
 	local form = GetShapeshiftFormID()
-	if(not form) then
+	if UnitInVehicle("player") then
+		showBar = false
+	elseif(not form) then
 		local ptt = GetPrimaryTalentTree()
 		if(ptt and ptt == 1) then -- player has balance spec
 			showBar = true
