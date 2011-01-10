34e716d9526eb186ec7bbaf5898282337b36358f
Elv22 <flipside_22@live.com>
2011-01-09 19:02:46 -0500

    StackSplitFrame over bag frame again..

--- Tukui/modules/bags/bags.lua
+++ Tukui/modules/bags/bags.lua
@@ -11,6 +11,8 @@ local tukuilocal = tukuilocal
 
 if not TukuiCF["others"].enablebag == true then return end
 
+StackSplitFrame:SetFrameStrata("TOOLTIP")
+
 local bags_BACKPACK = {0, 1, 2, 3, 4}
 local bags_BANK = {-1, 5, 6, 7, 8, 9, 10, 11}
 local BAGSFONT = TukuiCF["media"].font
