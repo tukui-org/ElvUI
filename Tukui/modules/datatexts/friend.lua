4ea09dc2dee32ad3068196613ec9e0c91ee9d450
Elv22 <flipside_22@live.com>
2011-01-13 16:08:43 -0500

    did last commit wrong

--- Tukui/modules/datatexts/friend.lua
+++ Tukui/modules/datatexts/friend.lua
@@ -174,9 +174,10 @@ if TukuiCF["datatext"].friends and TukuiCF["datatext"].friends > 0 then
 							if GetLocale() ~= "enUS" then -- feminine class localization (unsure if it's really needed)
 								for k,v in pairs(LOCALIZED_CLASS_NAMES_FEMALE) do if class == v then class = k end end
 							end
+							
+							local classc, levelc = (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[class], GetQuestDifficultyColor(level)
 							if classc == nil then classc = GetQuestDifficultyColor(level) end
 							
-							classc, levelc = (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[class], GetQuestDifficultyColor(level)
 							if UnitInParty(toonName) or UnitInRaid(toonName) then grouped = "|cffaaaaaa*|r" else grouped = "" end
 							GameTooltip:AddDoubleLine(format("%s |cff%02x%02x%02x(%d|r |cff%02x%02x%02x%s|r%s) |cff%02x%02x%02x%s|r",client,levelc.r*255,levelc.g*255,levelc.b*255,level,classc.r*255,classc.g*255,classc.b*255,toonName,grouped, 255, 0, 0, status),givenName.." "..surname,238,238,238,238,238,238)
 							if IsShiftKeyDown() then
