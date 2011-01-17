dde00eebbfbb85dd235381767b340701a3921ccc
Elv22 <flipside_22@live.com>
2011-01-17 12:06:06 -0500

    Convert ElvDB to TukuiDB on initialize, same for config options and locales.

--- ElvUI/init.lua
+++ ElvUI/init.lua
@@ -5,3 +5,10 @@
 ElvCF = {} -- Config Table
 ElvDB = {} -- Functions/Variables Table
 ElvL = {} -- Locales
+
+
+--Convert database to be used with Tukui
+TukuiCF = ElvCF
+TukuiDB = ElvDB
+Tukui = TukuiDB
+tukuilocal = ElvL
