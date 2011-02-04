ec340764922878235f4c9335829c4bfe469784f4
Elv22 <flipside_22@live.com>
2011-02-04 16:21:35 -0500

    Eyefinity support update.

--- ElvUI/core/pixelperfect.lua
+++ ElvUI/core/pixelperfect.lua
@@ -15,21 +15,22 @@ function E.UIScale()
 		
 		-- because some user enable bezel compensation, we need to find the real width of a single monitor.
 		-- I don't know how it really work, but i'm assuming they add pixel to width to compensate the bezel. :P
-		
+
 		-- HQ resolution
 		if width >= 9840 then width = 3280 end                   	                -- WQSXGA
-		if width >= 5760 and width < 9840 then width = 1920 end 	                -- WUXGA & HDTV
+		if width >= 7680 and width < 9840 then width = 2560 end                     -- WQXGA
+		if width >= 5760 and width < 7680 then width = 1920 end 	                -- WUXGA & HDTV
 		if width >= 5040 and width < 5760 then width = 1680 end 	                -- WSXGA+
-		
+
 		-- adding height condition here to be sure it work with bezel compensation because WSXGA+ and UXGA/HD+ got approx same width
 		if width >= 4800 and width < 5760 and height == 900 then width = 1600 end   -- UXGA & HD+
-		
+
 		-- low resolution screen
 		if width >= 4320 and width < 4800 then width = 1440 end 	                -- WSXGA
 		if width >= 4080 and width < 4320 then width = 1360 end 	                -- WXGA
 		if width >= 3840 and width < 4080 then width = 1224 end 	                -- SXGA & SXGA (UVGA) & WXGA & HDTV
 		
-		-- yep, now set Tukui to lower reso if screen #1 width < 1600
+		-- yep, now set Elvui to lower reso if screen #1 width < 1440
 		if width < 1440 and not C.general.overridelowtohigh then
 			E.lowversion = true
 		end
