37603c11b49cce3f70350105106dface6f9b65d7
Elv22 <flipside_22@live.com>
2011-02-05 15:37:48 -0500

    Removing loose files, dont code drunk.

--- ElvUI/layout/durability.lua
+++
@@ -1,7 +0,0 @@
-hooksecurefunc(DurabilityFrame,"SetPoint",function(self,_,parent)
-    if (parent == "MinimapCluster") or (parent == _G["MinimapCluster"]) then
-        DurabilityFrame:ClearAllPoints()
-		DurabilityFrame:SetPoint("RIGHT", Minimap, "RIGHT", 0, 0)
-		DurabilityFrame:SetScale(0.6)
-    end
-end)
--- ElvUI/layout/gm.lua
+++
@@ -1,9 +0,0 @@
-------------------------------------------------------------------------
---	GM ticket position
-------------------------------------------------------------------------
-local E, C, L = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
-
-TicketStatusFrame:ClearAllPoints()
-TicketStatusFrame:SetPoint("TOPLEFT", 250, -5)
-
-E.CreateMover(TicketStatusFrame, "GMMover", "GM Ticket Frame")
