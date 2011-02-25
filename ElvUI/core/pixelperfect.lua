4e4e8a3866c8b50a14983bec7b42bb14609f509b
Elv22 <flipside_22@live.com>
2011-02-25 11:33:16 -0500

    Attempting to fix some resolution issues

--- ElvUI/core/pixelperfect.lua
+++ ElvUI/core/pixelperfect.lua
@@ -1,14 +1,27 @@
 local E, C, L = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
 
+E.lowversions = {
+	["1280x720"] = true,
+	["1280x768"] = true,
+	["1280x800"] = true,
+	["1360x768"] = true,
+	["1366x768"] = true,
+	["1440x900"] = true,
+}
 
 function E.UIScale()
-	
-	if (E.getscreenwidth >= 3840) or (UIParent:GetWidth() + 1 > E.getscreenwidth and E.getscreenwidth ~= 1366) then
+	if C["general"].autoscale == true then
+		C["general"].uiscale = min(1, max(.64, 768/E.getscreenheight))
+	end
+
+	E.lowversion = false
+
+	if E.lowversions[E.getscreenresolution] and C["general"].resolutionoverride ~= "High" then
+			E.lowversion = true
+	elseif E.getscreenwidth >= 3840 or (UIParent:GetWidth() + 1 > E.getscreenwidth and not E.lowversions[E.getscreenresolution]) then
 		local width = E.getscreenwidth
 		local height = E.getscreenheight
-		
-		E.lowversion = false
-		
+	
 		-- because some user enable bezel compensation, we need to find the real width of a single monitor.
 		-- I don't know how it really work, but i'm assuming they add pixel to width to compensate the bezel. :P
 
@@ -27,20 +40,16 @@ function E.UIScale()
 		if width >= 3840 and width < 4080 then width = 1224 end 	                -- SXGA & SXGA (UVGA) & WXGA & HDTV
 		
 		-- yep, now set Elvui to lower reso if screen #1 width < 1600
-		if (width < 1600 and C.general.resolutionoverride ~= "High") or C.general.resolutionoverride == "Low" then
+		if (width < 1600 and C["general"].resolutionoverride ~= "High") then
 			E.lowversion = true
 		end
 		
 		-- register a constant, we will need it later for launch.lua
 		E.eyefinity = width
-	elseif (E.getscreenwidth < 1600 and C.general.resolutionoverride ~= "High") or C.general.resolutionoverride == "Low" then
-		E.lowversion = true
-	else
-		E.lowversion = false
 	end
 	
-	if C["general"].autoscale == true then
-		C["general"].uiscale = min(2, max(.64, 768/string.match(({GetScreenResolutions()})[GetCurrentResolution()], "%d+x(%d+)")))
+	if C["general"].resolutionoverride == "Low" then
+		E.lowversion = true
 	end
 end
 E.UIScale()
