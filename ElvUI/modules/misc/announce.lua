25356aa1102668378fc4a1cf80727bd62ab29dbe
Elv22 <flipside_22@live.com>
2011-03-22 18:39:09 -0400

    Fix spam when you interrupt in a bg and you aren't actually in a group.

--- ElvUI/modules/misc/announce.lua
+++ ElvUI/modules/misc/announce.lua
@@ -7,9 +7,9 @@ announce:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 announce:SetScript("OnEvent", function(self, _, _, event, _, sourceName, _, _, destName, _, _, _, _, spellID, spellName)
 	if not (event == "SPELL_INTERRUPT" and sourceName == E.myname) then return end
 	
-	if GetNumPartyMembers() > 0 then
+	if GetRealNumPartyMembers() > 0 then
 		SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", "PARTY", nil, nil)
-	elseif GetNumRaidMembers() > 0 then
+	elseif GetRealNumRaidMembers() > 0 then
 		SendChatMessage(INTERRUPTED.." "..destName.."'s \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", "RAID", nil, nil)
 	end
 end)
