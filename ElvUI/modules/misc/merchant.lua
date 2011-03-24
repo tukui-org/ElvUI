fda22545ff07e3d1bfd7f59f81224139de701cd9
Elv22 <flipside_22@live.com>
2011-03-24 09:30:13 -0400

    Fix buying max stack when holding down alt from a vender not working.

--- ElvUI/modules/misc/merchant.lua
+++ ElvUI/modules/misc/merchant.lua
@@ -46,9 +46,8 @@ local savedMerchantItemButton_OnModifiedClick = MerchantItemButton_OnModifiedCli
 function MerchantItemButton_OnModifiedClick(self, ...)
 	if ( IsAltKeyDown() ) then
 		local maxStack = select(8, GetItemInfo(GetMerchantItemLink(self:GetID())))
-		local name, texture, price, quantity, numAvailable, isUsable, extendedCost = GetMerchantItemInfo(self:GetID())
 		if ( maxStack and maxStack > 1 ) then
-			BuyMerchantItem(self:GetID(), floor(maxStack / quantity))
+			BuyMerchantItem(self:GetID(), GetMerchantItemMaxStack(self:GetID()))
 		end
 	end
 	savedMerchantItemButton_OnModifiedClick(self, ...)
