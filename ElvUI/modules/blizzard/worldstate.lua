6f816089f376552b8abf465577dd952ea100c1ad
Elv22 <flipside_22@live.com>
2011-03-30 01:50:49 -0400

    Move worldstate frame if toppanel option is set

---
+++ ElvUI/modules/blizzard/worldstate.lua
@@ -0,0 +1,5 @@
+local E, C, L, DB = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
+
+if C["general"].upperpanel == true then
+	WorldStateAlwaysUpFrame:SetPoint("TOP", UIParent, "TOP", -5, -70)
+end
