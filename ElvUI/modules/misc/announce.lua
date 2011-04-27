32688ff16528f3125c0247bcae561865b31d66b8
Elv22 <flipside_22@live.com>
2011-04-27 00:40:44 -0400

    Fix interrupt announce not working because of 4.1

--- ElvUI/modules/misc/announce.lua
+++ ElvUI/modules/misc/announce.lua
@@ -4,8 +4,9 @@ if not (C["others"].announceinterrupt == "SAY" or C["others"].announceinterrupt 
 
 local announce = CreateFrame("Frame")
 announce:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-announce:SetScript("OnEvent", function(self, _, _, event, _, sourceName, _, _, destName, _, _, _, _, spellID, spellName)
+announce:SetScript("OnEvent", function(self, _, _, event, _, _, sourceName, _, _, destName, _, _, _, _, spellID, spellName)
 	if not (event == "SPELL_INTERRUPT" and sourceName == E.myname) then return end
+	
 	
 	if C["others"].announceinterrupt == "PARTY" then
 		if GetRealNumPartyMembers() > 0 then
