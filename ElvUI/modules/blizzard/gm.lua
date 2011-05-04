9fbef5ec633ac4e2bfd5276b1819c91b884cacd1
Elv22 <flipside_22@live.com>
2011-05-04 17:51:09 -0400

    Blizzard's new ticket button will now spawn on the minimap instead of the microbar

--- ElvUI/modules/blizzard/gm.lua
+++ ElvUI/modules/blizzard/gm.lua
@@ -7,3 +7,7 @@ TicketStatusFrame:ClearAllPoints()
 TicketStatusFrame:SetPoint("TOPLEFT", 250, -5)
 
 E.CreateMover(TicketStatusFrame, "GMMover", "GM Ticket Frame")
+
+HelpOpenTicketButton:SetParent(Minimap)
+HelpOpenTicketButton:ClearAllPoints()
+HelpOpenTicketButton:SetPoint("TOPLEFT", Minimap, "TOPLEFT")
