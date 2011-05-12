862d933abc5652bbdd7bcd619cbb9baa5d3a07b9
Elv22 <flipside_22@live.com>
2011-05-12 17:31:03 -0400

    Skin KeyBindUI

--- ElvUI/modules/blizzard/skin.lua
+++ ElvUI/modules/blizzard/skin.lua
@@ -201,6 +201,41 @@ ElvuiSkin:RegisterEvent("ADDON_LOADED")
 ElvuiSkin:SetScript("OnEvent", function(self, event, addon)
 	if IsAddOnLoaded("Skinner") or IsAddOnLoaded("Aurora") then return end
 	
+	--Keybinds
+	if addon == "Blizzard_BindingUI" then
+		local buttons = {
+			"KeyBindingFrameDefaultButton",
+			"KeyBindingFrameUnbindButton",
+			"KeyBindingFrameOkayButton",
+			"KeyBindingFrameCancelButton",
+		}
+		
+		for _, v in pairs(buttons) do
+			_G[v]:StripTextures()
+			_G[v]:SetTemplate("Default", true)
+		end
+		
+		SkinCheckBox(KeyBindingFrameCharacterButton)
+		KeyBindingFrameHeaderText:ClearAllPoints()
+		KeyBindingFrameHeaderText:Point("TOP", KeyBindingFrame, "TOP", 0, -4)
+		KeyBindingFrame:StripTextures()
+		KeyBindingFrame:SetTemplate("Transparent")
+		
+		for i = 1, KEY_BINDINGS_DISPLAYED  do
+			local button1 = _G["KeyBindingFrameBinding"..i.."Key1Button"]
+			local button2 = _G["KeyBindingFrameBinding"..i.."Key2Button"]
+			button1:StripTextures(true)
+			button1:StyleButton(false)
+			button1:SetTemplate("Default", true)
+			button2:StripTextures(true)
+			button2:StyleButton(false)
+			button2:SetTemplate("Default", true)
+		end
+		
+		KeyBindingFrameUnbindButton:Point("RIGHT", KeyBindingFrameOkayButton, "LEFT", -3, 0)
+		KeyBindingFrameOkayButton:Point("RIGHT", KeyBindingFrameCancelButton, "LEFT", -3, 0)
+	end	
+	
 	--GBANK
 	if addon == "Blizzard_GuildBankUI" then
 		GuildBankFrame:StripTextures()
