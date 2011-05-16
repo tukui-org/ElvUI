6d102c9df37c927d38b6cd957a0f3ec066dba170
Elv22 <flipside_22@live.com>
2011-05-16 17:08:27 -0400

    Fix vehicle backdrop appearing behind the lower style panel

--- ElvUI/layout/layout.lua
+++ ElvUI/layout/layout.lua
@@ -171,6 +171,7 @@ if C["actionbar"].enable == true then
 	vbarbg:SetWidth((E.buttonsize * 12) + (E.buttonspacing * 13))
 	vbarbg:SetHeight(E.buttonsize + (E.buttonspacing * 2))
 	vbarbg:CreateShadow("Default")
+	vbarbg:SetFrameLevel(barbg:GetFrameLevel())
 end
 
 -- CHAT BACKGROUND LEFT (MOVES)
