7cf4f8cdfb40d83dfc797a2828927ef14745af11
Elv22 <flipside_22@live.com>
2011-06-19 15:29:15 -0400

    Adjust experience/rep bar width when raidbuffreminder is disabled

--- ElvUI/modules/unitframes/layouts/elvdps.lua
+++ ElvUI/modules/unitframes/layouts/elvdps.lua
@@ -408,7 +408,11 @@ local function Shared(self, unit)
 			local experience = CreateFrame("StatusBar", nil, self)
 			experience:SetStatusBarTexture(NORMTEX)
 			experience:SetStatusBarColor(0, 0.4, 1, .8)
-			experience:Size((E.minimapsize - 4) + 1 + (((E.minimapsize - 9) / 6)) + 4, 10)
+			if C["others"].raidbuffreminder == true then
+				experience:Size((E.minimapsize - 4) + 1 + (((E.minimapsize - 9) / 6)) + 4, 10)
+			else
+				experience:Size((E.minimapsize - 4), 10)
+			end
 			experience:Point("TOPLEFT", ElvuiMinimapStatsLeft, "BOTTOMLEFT", 2, -3)
 			experience:SetFrameLevel(Minimap:GetFrameLevel() + 1)
 			experience:SetFrameStrata(Minimap:GetFrameStrata())
@@ -449,7 +453,11 @@ local function Shared(self, unit)
 			local reputation = CreateFrame("StatusBar", nil, self)
 			reputation:SetStatusBarTexture(NORMTEX)
 			reputation:SetStatusBarColor(0, 1, 0.2, 1)
-			reputation:Size((E.minimapsize - 4) + 1 + (((E.minimapsize - 9) / 6)) + 4, 10)
+			if C["others"].raidbuffreminder == true then
+				reputation:Size((E.minimapsize - 4) + 1 + (((E.minimapsize - 9) / 6)) + 4, 10)
+			else
+				reputation:Size((E.minimapsize - 4), 10)
+			end
 			reputation:Point("TOPLEFT", ElvuiMinimapStatsLeft, "BOTTOMLEFT", 2, -3)
 			reputation:SetFrameLevel(Minimap:GetFrameLevel() + 1)
 			reputation:SetFrameStrata(Minimap:GetFrameStrata())
