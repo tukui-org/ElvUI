b502ff964c79e47167e9d0e68ed12ce7a6a45082
Elv22 <flipside_22@live.com>
2011-06-22 15:25:41 -0400

    Add a tooltip to the mastery datatext

--- ElvUI/modules/datatexts/mastery.lua
+++ ElvUI/modules/datatexts/mastery.lua
@@ -7,6 +7,7 @@ if not C["datatext"].mastery or C["datatext"].mastery == 0 then return end
 
 local Stat = CreateFrame("Frame")
 Stat:SetFrameStrata("MEDIUM")
+Stat:EnableMouse(true)
 Stat:SetFrameLevel(3)
 
 local Text  = ElvuiInfoLeft:CreateFontString(nil, "LOW")
@@ -17,9 +18,8 @@ Stat:SetParent(Text:GetParent())
 
 local _G = getfenv(0)
 local format = string.format
-local displayNumberString = string.join("", "%s", E.ValColor, "%d|r")
 local displayFloatString = string.join("", "%s", E.ValColor, "%.2f%%|r")
-local displayModifierString = string.join("", "%s", E.ValColor, "%d (+%.2f%%)|r")
+local displayModifierString = string.join("", "%s", E.ValColor, "%.2f%%|r")
 
 local function UpdateCaster(self)
 	local spellMastery = GetCombatRatingBonus(CR_HASTE_SPELL)
@@ -73,10 +73,54 @@ local function Update(self, t)
 		else
 			masteryTag = STAT_MASTERY..": "
 		end
-		Text:SetFormattedText(displayModifierString, masteryTag, GetCombatRating(CR_MASTERY), GetCombatRatingBonus(CR_MASTERY))
+		Text:SetFormattedText(displayModifierString, masteryTag, GetMastery())
 	end
+	
+	self:SetAllPoints(Text)
 	int = 2
 end
+
+Stat:SetScript('OnEnter', function(self)
+	local anchor, panel, xoff, yoff = E.DataTextTooltipAnchor(Text)
+	GameTooltip:SetOwner(panel, anchor, xoff, yoff)
+	GameTooltip:ClearLines()
+
+	local mastery = GetMastery();
+	local masteryBonus = GetCombatRatingBonus(CR_MASTERY);
+	mastery = format("%.2f", mastery);
+	
+	local title = HIGHLIGHT_FONT_COLOR_CODE..format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_MASTERY).." "..mastery..FONT_COLOR_CODE_CLOSE;
+	if (masteryBonus > 0) then
+		title = title..HIGHLIGHT_FONT_COLOR_CODE.." ("..format("%.2f", mastery-masteryBonus)..FONT_COLOR_CODE_CLOSE..GREEN_FONT_COLOR_CODE.."+"..format("%.2f", masteryBonus)..FONT_COLOR_CODE_CLOSE..HIGHLIGHT_FONT_COLOR_CODE..")";
+	end
+	GameTooltip:SetText(title);
+	
+	local masteryKnown = IsSpellKnown(CLASS_MASTERY_SPELLS[E.myclass]);
+	local primaryTalentTree = GetPrimaryTalentTree();
+	if (masteryKnown and primaryTalentTree) then
+		local masterySpell, masterySpell2 = GetTalentTreeMasterySpells(primaryTalentTree);
+		if (masterySpell) then
+			GameTooltip:AddSpellByID(masterySpell);
+		end
+		if (masterySpell2) then
+			GameTooltip:AddLine(" ");
+			GameTooltip:AddSpellByID(masterySpell2);
+		end
+		GameTooltip:AddLine(" ");
+		GameTooltip:AddLine(format(STAT_MASTERY_TOOLTIP, GetCombatRating(CR_MASTERY), masteryBonus), NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, true);
+	else
+		GameTooltip:AddLine(format(STAT_MASTERY_TOOLTIP, GetCombatRating(CR_MASTERY), masteryBonus), NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, true);
+		GameTooltip:AddLine(" ");
+		if (masteryKnown) then
+			GameTooltip:AddLine(STAT_MASTERY_TOOLTIP_NO_TALENT_SPEC, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b, true);
+		else
+			GameTooltip:AddLine(STAT_MASTERY_TOOLTIP_NOT_KNOWN, GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b, true);
+		end
+	end
+	
+	GameTooltip:Show()
+end)
+Stat:SetScript('OnLeave', function() GameTooltip:Hide() end)
 
 Stat:SetScript("OnUpdate", Update)
 Update(Stat, 6)
