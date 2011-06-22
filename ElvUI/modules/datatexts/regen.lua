a0edb9439a282303d5ca795a4f43437b561c1569
Elv22 <flipside_22@live.com>
2011-06-22 15:27:04 -0400

    Regen datatext now displays current mana regen per 5 second instead of per 1 second, in addition regen now only displays the current mana regen, not both incombat and out of combat. Add a tooltip to mana regen to display both infos.

--- ElvUI/modules/datatexts/regen.lua
+++ ElvUI/modules/datatexts/regen.lua
@@ -8,6 +8,7 @@ if not C["datatext"].manaregen or C["datatext"].manaregen == 0 then return end
 local Stat = CreateFrame("Frame")
 Stat:SetFrameStrata("MEDIUM")
 Stat:SetFrameLevel(3)
+Stat:EnableMouse(true)
 
 local Text  = ElvuiInfoLeft:CreateFontString(nil, "LOW")
 Text:SetFont(C["media"].font, C["datatext"].fontsize, "THINOUTLINE")
@@ -17,7 +18,7 @@ Stat:SetParent(Text:GetParent())
 
 local _G = getfenv(0)
 local format = string.format
-local displayManaRegen = string.join("", "%s", E.ValColor, "%.2f (%.2f)|r")
+local displayManaRegen = string.join("", "%s", E.ValColor, "%.2f|r")
 
 -- initial delay for update (let the ui load)
 local int = 5
@@ -26,10 +27,30 @@ local function Update(self, t)
 	if int > 0 then return end
 
 	local baseMR, castingMR = GetManaRegen()
-
-	Text:SetFormattedText(displayManaRegen, MANA_REGEN..": ", baseMR, castingMR)
+	
+	if InCombatLockdown() then
+		Text:SetFormattedText(displayManaRegen, MANA_REGEN..": ", castingMR*5)
+	else
+		Text:SetFormattedText(displayManaRegen, MANA_REGEN..": ", baseMR*5)
+	end
+	
+	self:SetAllPoints(Text)
 	int = 2
 end
 
 Stat:SetScript("OnUpdate", Update)
+Stat:SetScript('OnEnter', function(self)
+	local baseMR, castingMR = GetManaRegen()
+	local anchor, panel, xoff, yoff = E.DataTextTooltipAnchor(Text)
+	GameTooltip:SetOwner(panel, anchor, xoff, yoff)
+	GameTooltip:ClearLines()
+	GameTooltip:AddLine(MANA_REGEN)
+	GameTooltip:AddLine' '
+	GameTooltip:AddDoubleLine(NO.." "..COMBAT..":", format("%.2f", baseMR*5), 1, 1, 1)
+	GameTooltip:AddDoubleLine(COMBAT..":", format("%.2f", castingMR*5), 1, 1, 1)
+	
+	GameTooltip:Show()
+end)
+
+Stat:SetScript('OnLeave', function() GameTooltip:Hide() end)
 Update(Stat, 6)
