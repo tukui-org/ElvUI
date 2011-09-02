e5c919a555350514b5d29c42be78234897b23a80
Elv <flipside_22@live.com>
2011-09-02 13:57:09 -0400

    Fix vehicle seat readjusting to default position everytime you move it.

--- ElvUI/modules/blizzard/vehicle.lua
+++ ElvUI/modules/blizzard/vehicle.lua
@@ -9,15 +9,17 @@ function E.PostVehicleMove(frame)
 	VehicleSeatIndicator:SetPoint("CENTER", frame, "CENTER", 0, 0)
 end
 
-local once = false
 hooksecurefunc(VehicleSeatIndicator,"SetPoint",function(_,_,parent) -- vehicle seat indicator
     if (parent == "MinimapCluster") or (parent == _G["MinimapCluster"]) then
 		VehicleSeatIndicator:ClearAllPoints()
-		VehicleSeatIndicator:Point("TOPLEFT", E.UIParent, "TOPLEFT", 36, -40)
-		VehicleSeatIndicator:SetScale(0.8)
-		if once == false then
+		
+		if VehicleSeatMover then
+			VehicleSeatIndicator:Point("TOPRIGHT", VehicleSeatMover, "TOPRIGHT", 0, 0)
+		else
+			VehicleSeatIndicator:Point("TOPLEFT", E.UIParent, "TOPLEFT", 36, -40)
 			E.CreateMover(VehicleSeatIndicator, "VehicleSeatMover", "Vehicle Seat Frame")	
-			once = true
 		end
+		
+		VehicleSeatIndicator:SetScale(0.8)
     end
 end)
