8689e4db61eaee1746c575bd6977c0fa74bd56f7
Blazeflack <blazeflack@hotmail.com>
2011-12-13 12:25:40 +0100

    Add the Minimalist texture

--- ElvUI/media/sharedmedia.lua
+++ ElvUI/media/sharedmedia.lua
@@ -4,6 +4,7 @@ if LSM == nil then return end
 
 LSM:Register("statusbar","ElvUI Gloss", [[Interface\AddOns\ElvUI\media\textures\normTex.tga]])
 LSM:Register("statusbar","ElvUI Norm", [[Interface\AddOns\ElvUI\media\textures\normTex2.tga]])
+LSM:Register("statusbar","Minimalist", [[Interface\AddOns\ElvUI\media\textures\Minimalist.tga]])
 LSM:Register("background","ElvUI Blank", [[Interface\BUTTONS\WHITE8X8]])
 LSM:Register("border", "ElvUI GlowBorder", [[Interface\AddOns\ElvUI\media\textures\glowTex.tga]])
 LSM:Register("font","ElvUI Font", [[Interface\AddOns\ElvUI\media\fonts\PT_Sans_Narrow.ttf]])
