2b8e3112ee6619a671a0754fc08a08f3a708f00e
Elv <flipside_22@live.com>
2012-02-22 03:31:42 -0500

    Fix default position of arena/boss frames to work with both a high and low resolution

--- modules/unitframes/groups/boss.lua
+++ modules/unitframes/groups/boss.lua
@@ -390,7 +390,7 @@ function UF:Update_BossFrames(frame, db)
 	if not frame.mover then
 		frame:ClearAllPoints()	
 		if INDEX == 1 then
-			frame:Point('BOTTOMRIGHT', E.UIParent, 'BOTTOM', 700, 325) --Set to default position
+			frame:Point('BOTTOMRIGHT', E.UIParent, 'RIGHT', -105, -165) --Set to default position
 		else
 			if db.growthDirection == 'UP' then
 				frame:Point('BOTTOMRIGHT', _G['ElvUF_Boss'..INDEX-1], 'TOPRIGHT', 0, 12 + db.castbar.height)
