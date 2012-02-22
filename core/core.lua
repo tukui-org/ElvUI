11dd2564fc4fb45f8852611e77b7902128e860c0
Elv <flipside_22@live.com>
2012-02-22 04:45:16 -0500

    Attempt number 2 to fix addon message

--- core/core.lua
+++ core/core.lua
@@ -337,7 +337,8 @@ end
 function E:SendRecieve(event, prefix, message, channel, sender)
 	if event == "CHAT_MSG_ADDON" then
 		if sender == E.myname then return end
-		if prefix == "ElvUIVC" and sender ~= 'Elv' then
+
+		if prefix == "ElvUIVC" and sender ~= 'Elv' and sender ~= 'Elv-Illidan' then
 			if tonumber(message) > tonumber(E.version) then
 				E:Print(L["Your version of ElvUI is out of date. You can download the latest version from www.curse.com"])
 				self:UnregisterEvent("CHAT_MSG_ADDON")
