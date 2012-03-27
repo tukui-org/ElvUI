d7cbb4a08c9b0ce8ad4c78629804b0dccfde4183
Elv <flipside_22@live.com>
2012-03-27 00:29:09 -0400

    Fix battle.net messages when copying from the copy frame to the chat edit box.

--- modules/chat/chat.lua
+++ modules/chat/chat.lua
@@ -107,6 +107,19 @@ function CH:StyleChat(frame)
 			ChatEdit_ParseText(self, 0)		  
 		  end
 	   end
+	      
+		local found = false
+		while text:find("|K") do
+			found = true
+			text = text:gsub("|Kf(%d+)|", "")
+			text = text:gsub("|k", "")
+			text = text:gsub("%[|", "%[")
+			text = text:gsub("|%]", "%]")
+		end
+		
+		if found then
+			self:SetText(text)
+		end
 	end)
 	
 	hooksecurefunc("ChatEdit_UpdateHeader", function()
