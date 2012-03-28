ba7bb41dff53fdb296960d88da7859ac6d0c03ff
Blazeflack <blazeflack@hotmail.com>
2012-03-29 00:26:06 +0200

    Option to automatically vendor grey items has been added to ElvUI. Remove the now redundant code.

--- modules/misc/merchant.lua
+++ modules/misc/merchant.lua
@@ -2,30 +2,6 @@ local E, L, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, ProfileDB,
 local M = E:GetModule('Misc');
 
 function M:LoadMerchant()
-	local f = CreateFrame("Frame")
-	f:SetScript("OnEvent", function()
-		local c = 0
-		for b=0,4 do
-			for s=1,GetContainerNumSlots(b) do
-				local l = GetContainerItemLink(b, s)
-				if l then
-					local p = select(11, GetItemInfo(l))*select(2, GetContainerItemInfo(b, s))
-					if select(3, GetItemInfo(l))==0 and p>0 then
-						UseContainerItem(b, s)
-						PickupMerchantItem()
-						c = c+p
-					end
-				end
-			end
-		end
-
-		if c>0 then
-			local g, s, c = math.floor(c/10000) or 0, math.floor((c%10000)/100) or 0, c%100
-			E:Print(L['Vendored gray items for:'].." |cffffffff"..g..L.goldabbrev.." |cffffffff"..s..L.silverabbrev.." |cffffffff"..c..L.copperabbrev..".")
-		end
-	end)
-	f:RegisterEvent("MERCHANT_SHOW")
-	
 	-- buy max number value with alt
 	local savedMerchantItemButton_OnModifiedClick = MerchantItemButton_OnModifiedClick
 	function MerchantItemButton_OnModifiedClick(self, ...)
