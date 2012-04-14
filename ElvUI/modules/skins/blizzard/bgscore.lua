47980020ccb46f28a027a5ec45ae482002eddf8e
Elv <flipside_22@live.com>
2012-04-14 00:32:09 -0400

    Fix BGScoreBoardFrame skin, for some reason Blizzard allows way too many bars to be displayed.

--- ElvUI/modules/skins/blizzard/bgscore.lua
+++ ElvUI/modules/skins/blizzard/bgscore.lua
@@ -3,10 +3,16 @@ local S = E:GetModule('Skins')
 
 local function LoadSkin()
 	if E.global.skins.blizzard.enable ~= true or E.global.skins.blizzard.bgscore ~= true then return end
+	for i=19, MAX_WORLDSTATE_SCORE_BUTTONS do
+		_G['WorldStateScoreButton'..i]:StripTextures()
+	end
+	MAX_WORLDSTATE_SCORE_BUTTONS = 18; WorldStateScoreFrame_Resize()
+	
 	WorldStateScoreScrollFrame:StripTextures()
 	WorldStateScoreFrame:StripTextures()
 	WorldStateScoreFrame:SetTemplate("Transparent")
 	S:HandleCloseButton(WorldStateScoreFrameCloseButton)
+	S:HandleScrollBar(WorldStateScoreScrollFrameScrollBar)
 	WorldStateScoreFrameInset:Kill()
 	S:HandleButton(WorldStateScoreFrameLeaveButton)
 	
