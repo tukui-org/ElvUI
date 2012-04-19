1c04cc1e0dadb9ab135743e6229c479a89f962ee
Elv <flipside_22@live.com>
2012-04-19 18:55:42 -0400

    Fix raid frame taint error.

--- ElvUI/modules/unitframes/unitframes.lua
+++ ElvUI/modules/unitframes/unitframes.lua
@@ -431,6 +431,7 @@ function UF:DisableBlizzard(event)
 	CompactRaidFrameManager:HookScript('OnShow', HideRaid)
 	CompactRaidFrameContainer:UnregisterAllEvents()
 	HideRaid()
+	hooksecurefunc("CompactUnitFrame_RegisterEvents", CompactUnitFrame_UnregisterEvents)
 end
 
 function UF:ForceShow(frame)
@@ -506,7 +507,7 @@ local HandleFrame = function(baseName)
 end
 
 function ElvUF:DisableBlizzard(unit)
-	if(not unit) then return end
+	if(not unit) or InCombatLockdown() then return end
 
 	if(unit == 'player') then
 		HandleFrame(PlayerFrame)
