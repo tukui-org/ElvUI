9c19ab5bacec8beeb772681f0201b421801901e2
Elv <flipside_22@live.com>
2012-06-07 00:41:12 -0400

    Same as last commit, except don't do it backwards this time.

--- ElvUI/modules/unitframes/groups/raid.lua
+++ ElvUI/modules/unitframes/groups/raid.lua
@@ -55,11 +55,11 @@ for i=10, 40, 15 do
 			if inInstance and instanceType == "raid" and maxPlayers < math.min(self.db.maxColumns * self.db.unitsPerColumn, MAX_RAID_MEMBERS) then
 				self:SetAttribute("showRaid", false)
 				self:SetAttribute("showParty", false)	
-				RegisterAttributeDriver(self, 'state-visibility', 'show')	
+				self:ChangeVisibility(self, 'custom '..self.db.visibility)
 			elseif self.db.showParty then
 				self:SetAttribute("showParty", self.db.showParty)
 				self:SetAttribute("showRaid", self.db.showRaid)
-				self:ChangeVisibility(self, 'custom '..self.db.visibility)
+				RegisterAttributeDriver(self, 'state-visibility', 'show')	
 			end
 		else
 			self:RegisterEvent("PLAYER_REGEN_ENABLED")
