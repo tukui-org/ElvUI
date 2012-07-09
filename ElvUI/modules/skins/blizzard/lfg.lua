f7c6a6100fe49a8e2fc00c0a700dca262e4685e6
Elv <flipside_22@live.com>
2012-07-09 03:37:42 -0400

    Skin LFG frame

---
+++ ElvUI/modules/skins/blizzard/lfg.lua
@@ -0,0 +1,237 @@
+local E, L, V, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local S = E:GetModule('Skins')
+
+local function LoadSkin()
+	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.lfg ~= true then return end
+	PVEFrame:StripTextures()
+	PVEFrame:StripTextures()
+	PVEFrameLeftInset:StripTextures()
+	RaidFinderQueueFrame:StripTextures(true)
+	PVEFrameBg:Hide()
+	PVEFrameTitleBg:Hide()
+	PVEFramePortrait:Hide()
+	PVEFramePortraitFrame:Hide()
+	PVEFrameTopRightCorner:Hide()
+	PVEFrameTopBorder:Hide()
+	PVEFrameLeftInsetBg:Hide()
+	PVEFrame.shadows:Hide()
+	
+	GroupFinderFrameGroupButton1.icon:SetTexture("Interface\\Icons\\INV_Helmet_08")
+	GroupFinderFrameGroupButton2.icon:SetTexture("Interface\\Icons\\inv_helmet_06")
+	GroupFinderFrameGroupButton3.icon:SetTexture("Interface\\Icons\\Icon_Scenarios")
+	
+	local roleButtons = {
+		LFDQueueFrameRoleButtonHealer,
+		LFDQueueFrameRoleButtonDPS,
+		LFDQueueFrameRoleButtonLeader,
+		LFDQueueFrameRoleButtonTank,
+		RaidFinderQueueFrameRoleButtonHealer,
+		RaidFinderQueueFrameRoleButtonDPS,
+		RaidFinderQueueFrameRoleButtonLeader,
+		RaidFinderQueueFrameRoleButtonTank
+	}
+	
+	for _, roleButton in pairs(roleButtons) do
+		S:HandleCheckBox(roleButton.checkButton)
+		roleButton:GetChildren():SetFrameLevel(roleButton:GetChildren():GetFrameLevel() + 1)
+	end
+	
+	for i = 1, 3 do
+		local bu = GroupFinderFrame["groupButton"..i]
+
+		bu.ring:Hide()
+		bu.bg:SetTexture("")
+		bu.bg:SetAllPoints()
+
+		bu:SetTemplate()
+		bu:StyleButton()
+		
+		bu.icon:SetTexCoord(.08, .92, .08, .92)
+		bu.icon:SetPoint("LEFT", bu, "LEFT")
+		bu.icon:SetDrawLayer("OVERLAY")
+		bu.icon:Size(40)
+		bu.icon:ClearAllPoints()
+		bu.icon:SetPoint("LEFT", 10, 0)
+		bu.border = CreateFrame("Frame", nil, bu)
+		bu.border:SetTemplate('Default')
+		bu.border:Point("TOPLEFT", bu.icon, -2, 2)
+		bu.border:Point("BOTTOMRIGHT", bu.icon, 2, -2)
+		bu.icon:SetParent(bu.border)
+	end
+
+	PVEFrame:CreateBackdrop("Transparent")
+	PVEFrame:CreateShadow('Default')
+	for i=1, 2 do
+		S:HandleTab(_G['PVEFrameTab'..i])
+	end
+	PVEFrameTab1:SetPoint('BOTTOMLEFT', PVEFrame, 'BOTTOMLEFT', 19, -32)
+	
+	S:HandleCloseButton(PVEFrameCloseButton)
+
+	-- raid finder
+	S:HandleButton(LFDQueueFrameFindGroupButton, true)
+	
+	LFDParentFrame:StripTextures()
+	LFDParentFrameInset:StripTextures()
+	
+	local function ReskinRewards()
+		LFDQueueFrame:StripTextures()
+
+		for i = 1, LFD_MAX_REWARDS do
+			local button = _G["LFDQueueFrameRandomScrollFrameChildFrameItem"..i]
+			local icon = _G["LFDQueueFrameRandomScrollFrameChildFrameItem"..i.."IconTexture"]
+
+			if button then
+				if not button.reskinned then
+					local cta = _G["LFDQueueFrameRandomScrollFrameChildFrameItem"..i.."ShortageBorder"]
+					local count = _G["LFDQueueFrameRandomScrollFrameChildFrameItem"..i.."Count"]
+					local na = _G["LFDQueueFrameRandomScrollFrameChildFrameItem"..i.."NameFrame"]
+
+					icon:SetTexCoord(.08, .92, .08, .92)
+					icon:SetDrawLayer("OVERLAY")
+					count:SetDrawLayer("OVERLAY")
+					na:SetTexture()
+					na:SetSize(118, 39)
+					cta:SetAlpha(0)
+
+					button.border = CreateFrame("Frame", nil, button)
+					button.border:SetTemplate()
+					button.border:Point("TOPLEFT", icon, -2, 2)
+					button.border:Point("BOTTOMRIGHT", icon, 2, -2)
+					icon:SetParent(button.border)
+
+					button.reskinned = true
+				end
+			end
+		end
+	end
+
+	hooksecurefunc("LFDQueueFrameRandom_UpdateFrame", ReskinRewards)
+
+	for i = 1, NUM_LFD_CHOICE_BUTTONS do
+		S:HandleCheckBox(_G["LFDQueueFrameSpecificListButton"..i].enableButton)
+	end
+
+	S:HandleCheckBox(ScenarioQueueFrameSpecificButton1.enableButton)
+
+	for i = 1, NUM_LFR_CHOICE_BUTTONS do
+		local bu = _G["LFRQueueFrameSpecificListButton"..i].enableButton
+		S:HandleCheckBox(bu)
+		bu.SetNormalTexture = E.noop
+		bu.SetPushedTexture = E.noop
+	end
+
+	S:HandleDropDownBox(LFDQueueFrameTypeDropDown)
+
+	-- Raid Finder
+	RaidFinderFrame:StripTextures()
+	RaidFinderFrameBottomInset:StripTextures()
+	RaidFinderFrameRoleInset:StripTextures()
+	RaidFinderFrameBottomInsetBg:Hide()
+	RaidFinderFrameBtnCornerRight:Hide()
+	RaidFinderFrameButtonBottomBorder:Hide()
+	S:HandleDropDownBox(RaidFinderQueueFrameSelectionDropDown)
+	RaidFinderFrameFindRaidButton:StripTextures()
+	S:HandleButton(RaidFinderFrameFindRaidButton)
+	RaidFinderQueueFrame:StripTextures()
+
+	for i = 1, LFD_MAX_REWARDS do
+		local button = _G["RaidFinderQueueFrameScrollFrameChildFrameItem"..i]
+		local icon = _G["RaidFinderQueueFrameScrollFrameChildFrameItem"..i.."IconTexture"]
+
+		if button then
+			if not button.reskinned then
+				local cta = _G["RaidFinderQueueFrameScrollFrameChildFrameItem"..i.."ShortageBorder"]
+				local count = _G["RaidFinderQueueFrameScrollFrameChildFrameItem"..i.."Count"]
+				local na = _G["RaidFinderQueueFrameScrollFrameChildFrameItem"..i.."NameFrame"]
+				button:StripTextures()
+				
+				icon:SetTexCoord(.08, .92, .08, .92)
+				icon:SetDrawLayer("OVERLAY")
+				count:SetDrawLayer("OVERLAY")
+				na:SetTexture()
+				na:SetSize(118, 39)
+				cta:SetAlpha(0)
+
+				button.border = CreateFrame("Frame", nil, button)
+				button.border:SetTemplate()
+				button.border:Point("TOPLEFT", icon, -2, 2)
+				button.border:Point("BOTTOMRIGHT", icon, 2, -2)
+				icon:SetParent(button.border)
+
+				button.reskinned = true
+			end
+		end
+	end
+
+	-- Scenario finder
+	ScenarioFinderFrameInset:DisableDrawLayer("BORDER")
+	ScenarioFinderFrame.TopTileStreaks:Hide()
+	ScenarioFinderFrameBtnCornerRight:Hide()
+	ScenarioFinderFrameButtonBottomBorder:Hide()
+	ScenarioQueueFrame.Bg:Hide()
+	ScenarioFinderFrameInset:GetRegions():Hide()
+
+	ScenarioQueueFrameFindGroupButton:StripTextures()
+	S:HandleButton(ScenarioQueueFrameFindGroupButton)
+	S:HandleDropDownBox(ScenarioQueueFrameTypeDropDown)
+
+	-- Raid frame (social frame)
+	RaidFrameRaidBrowserButton:SetTemplate("Default")
+	S:HandleCheckBox(RaidFrameAllAssistCheckButton)
+
+	-- Looking for raid
+	LFRBrowseFrameRoleInset:DisableDrawLayer("BORDER")
+	RaidBrowserFrameBg:Hide()
+	LFRQueueFrameSpecificListScrollFrameScrollBackgroundTopLeft:Hide()
+	LFRQueueFrameSpecificListScrollFrameScrollBackgroundBottomRight:Hide()
+	LFRBrowseFrameRoleInsetBg:Hide()
+
+	for i = 1, 14 do
+		if i ~= 6 and i ~= 8 then
+			select(i, RaidBrowserFrame:GetRegions()):Hide()
+		end
+	end
+
+	RaidBrowserFrame:CreateBackdrop()
+	S:HandleCloseButton(RaidBrowserFrameCloseButton)
+	
+	S:HandleScrollBar(LFRQueueFrameCommentScrollFrameScrollBar)
+	S:HandleScrollBar(LFDQueueFrameSpecificListScrollFrameScrollBar)
+end
+
+S:RegisterSkin("ElvUI", LoadSkin)
+
+local function LoadSecondarySkin()
+	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.lfg ~= true then return end
+	ChallengesFrameInset:StripTextures()
+	ChallengesFrameInsetBg:Hide()
+	ChallengesFrameDetails.bg:Hide()
+	select(2, ChallengesFrameDetails:GetRegions()):Hide()
+	select(9, ChallengesFrameDetails:GetRegions()):Hide()
+	select(10, ChallengesFrameDetails:GetRegions()):Hide()
+	select(11, ChallengesFrameDetails:GetRegions()):Hide()
+	ChallengesFrameDungeonButton1:SetPoint("TOPLEFT", ChallengesFrame, "TOPLEFT", 8, -83)
+
+	for i = 1, 9 do
+		local bu = ChallengesFrame["button"..i]
+		S:HandleButton(bu)
+		bu:StyleButton()
+		bu:SetHighlightTexture("")
+		bu.selectedTex:SetAlpha(.2)
+		bu.selectedTex:SetPoint("TOPLEFT", 1, -1)
+		bu.selectedTex:SetPoint("BOTTOMRIGHT", -1, 1)
+		bu.NoMedal:Kill()
+	end
+
+	for i = 1, 3 do
+		local rewardsRow = ChallengesFrame["RewardRow"..i]
+		for j = 1, 2 do
+			local bu = rewardsRow["Reward"..j]
+			bu:CreateBackdrop()
+			bu.Icon:SetTexCoord(.08, .92, .08, .92)
+		end
+	end
+end
+
+S:RegisterSkin("Blizzard_ChallengesUI", LoadSecondarySkin)
