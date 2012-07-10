a030bb9fa4db2a3fff893ad5a0c5f1b7568d73bf
Elv <flipside_22@live.com>
2012-07-10 03:59:27 -0400

    Reattach achievement alert frames to the elvui mover

--- ElvUI/modules/blizzard/achievements.lua
+++ ElvUI/modules/blizzard/achievements.lua
@@ -1,8 +1,6 @@
 local E, L, DF = unpack(select(2, ...))
 local B = E:GetModule('Blizzard');
 
-if not TempEnable then return end
-
 local AchievementHolder = CreateFrame("Frame", "AchievementHolder", E.UIParent)
 AchievementHolder:SetWidth(180)
 AchievementHolder:SetHeight(20)
@@ -47,8 +45,8 @@ end
 
 --/run GuildChallengeAlertFrame_ShowAlert(3, 2, 5)
 function B:AchievementMovers()
-	hooksecurefunc("AchievementAlertFrame_FixAnchors", B.AchievementMove)
-	hooksecurefunc("DungeonCompletionAlertFrame_FixAnchors", function()
+	hooksecurefunc("AlertFrame_SetAchievementAnchors", B.AchievementMove)
+	hooksecurefunc("AlertFrame_SetDungeonCompletionAnchors", function()
 		for i=MAX_ACHIEVEMENT_ALERTS, 1, -1 do
 			local aFrame = _G["AchievementAlertFrame"..i]
 			if ( aFrame and aFrame:IsShown() ) then
@@ -71,7 +69,7 @@ function B:AchievementMovers()
 		end
 	end)	
 	
-	hooksecurefunc('GuildChallengeAlertFrame_FixAnchors', function()
+	hooksecurefunc('AlertFrame_SetGuildChallengeAnchors', function()
 		local aFrame
 		for i=MAX_ACHIEVEMENT_ALERTS, 1, -1 do
 			if _G["AchievementAlertFrame"..i] and _G["AchievementAlertFrame"..i]:IsShown() then
@@ -94,6 +92,45 @@ function B:AchievementMovers()
 			GuildChallengeAlertFrame:SetPoint("BOTTOM", aFrame, "TOP", 0, 10)
 		end
 	end)
+	
+	hooksecurefunc("AlertFrame_SetChallengeModeAnchors", function()
+		for i = MAX_ACHIEVEMENT_ALERTS, 1, -1 do
+			local aFrame = _G["AchievementAlertFrame"..i]
+			local dFrame = _G["DungeonCompletionAlertFrame1"]
+			local cFrame = _G["GuildChallengeAlertFrame"]
+			local cmFrame = _G["ChallengeModeAlertFrame1"]
+
+			cmFrame:ClearAllPoints()
+			if cFrame and cFrame:IsShown() then
+				if pos == "TOP" then
+					cmFrame:SetPoint("TOP", dFrame, "BOTTOM", 0, 3)
+				else
+					cmFrame:SetPoint("BOTTOM", dFrame, "TOP", 0, -3)
+				end
+				return
+			elseif dFrame and dFrame:IsShown() then
+				if pos == "TOP" then
+					cmFrame:SetPoint("TOP", dFrame, "BOTTOM", 0, 3)
+				else
+					cmFrame:SetPoint("BOTTOM", dFrame, "TOP", 0, -3)
+				end
+				return
+			elseif aFrame and aFrame:IsShown() then
+				if pos == "TOP" then
+					cmFrame:SetPoint("TOP", aFrame, "BOTTOM", 0, 3)
+				else
+					cmFrame:SetPoint("BOTTOM", aFrame, "TOP", 0, -3)
+				end
+				return
+			else
+				if pos == "TOP" then
+					cmFrame:SetPoint("TOP", AchievementHolder, "TOP")
+				else
+					cmFrame:SetPoint("BOTTOM", AchievementHolder, "BOTTOM")
+				end
+			end
+		end
+	end)	
 	
 	self:RegisterEvent("ACHIEVEMENT_EARNED", 'AchievementMove')
 	E:CreateMover(AchievementHolder, "AchievementMover", "Achievement Frames", nil, nil, PostAchievementMove)
