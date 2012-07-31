5a08b0e1e4863b141ccb5a75f740e70f743fc4a2
Elv <flipside_22@live.com>
2012-07-31 00:57:34 -0400

    Add corner icons in, also add spell ticks. Let me know what needs to be adjusted.

--- ElvUI/config/filters/unitframe.lua
+++ ElvUI/config/filters/unitframe.lua
@@ -66,38 +66,62 @@ end
 
 G.unitframe.buffwatch = {
 	PRIEST = {
-
+		{6788, "TOPRIGHT", {1, 0, 0}, true},	 -- Weakened Soul
+		{33076, "BOTTOMRIGHT", {0.2, 0.7, 0.2}},	 -- Prayer of Mending
+		{139, "BOTTOMLEFT", {0.4, 0.7, 0.2}}, -- Renew
+		{17, "TOPLEFT", {0.81, 0.85, 0.1}, true},	 -- Power Word: Shield
 	},
 	DRUID = {
-
+		{774, "TOPRIGHT", {0.8, 0.4, 0.8}},	 -- Rejuvenation
+		{8936, "BOTTOMLEFT", {0.2, 0.8, 0.2}},	 -- Regrowth
+		{94447, "TOPLEFT", {0.4, 0.8, 0.2}},	 -- Lifebloom
+		{48438, "BOTTOMRIGHT", {0.8, 0.4, 0}},	 -- Wild Growth
 	},
 	PALADIN = {
-
-	},
-	ROGUE = {
-	
-	},
-	DEATHKNIGHT = {
-	
-	},
-	MAGE = {
-
-	},
-	WARRIOR = {
-
+		{53563, "TOPRIGHT", {0.7, 0.3, 0.7}},	 -- Beacon of Light
+		{1022, "BOTTOMRIGHT", {0.2, 0.2, 1}, true},	-- Hand of Protection
+		{1044, "BOTTOMRIGHT", {0.89, 0.45, 0}, true},	-- Hand of Freedom
+		{1038, "BOTTOMRIGHT", {0.93, 0.75, 0}, true},	-- Hand of Salvation
+		{6940, "BOTTOMRIGHT", {0.89, 0.1, 0.1}, true},	-- Hand of Sacrifice
 	},
 	SHAMAN = {
-
-	},	
+		{61295, "TOPRIGHT", {0.7, 0.3, 0.7}},	 -- Riptide
+		{974, "BOTTOMLEFT", {0.2, 0.7, 0.2}, true},	 -- Earth Shield
+		{51945, "BOTTOMRIGHT", {0.7, 0.4, 0}},	 -- Earthliving
+	},
+	MONK = {
+		{119611, "TOPLEFT", {0.8, 0.4, 0.8}},	 --Renewing Mist
+		{116849, "TOPRIGHT", {0.2, 0.8, 0.2}},	 -- Life Cocoon
+		{124682, "BOTTOMLEFT", {0.4, 0.8, 0.2}}, -- Enveloping Mist
+		{124081, "BOTTOMRIGHT", {0.7, 0.4, 0}}, -- Zen Sphere
+	},
 }
 
 --List of spells to display ticks
 G.unitframe.ChannelTicks = {
-
+	--Warlock
+	[SpellName(1120)] = 5, --"Drain Soul"
+	[SpellName(689)] = 3, -- "Drain Life"
+	[SpellName(5740)] = 4, -- "Rain of Fire"
+	[SpellName(755)] = 3, -- Health Funnel
+	--Druid
+	[SpellName(44203)] = 4, -- "Tranquility"
+	[SpellName(16914)] = 10, -- "Hurricane"
+	--Priest
+	[SpellName(15407)] = 3, -- "Mind Flay"
+	[SpellName(48045)] = 5, -- "Mind Sear"
+	[SpellName(47540)] = 2, -- "Penance"
+	[SpellName(64901)] = 4, -- Hymn of Hope
+	[SpellName(64843)] = 4, -- Divine Hymn
+	--Mage
+	[SpellName(5143)] = 5, -- "Arcane Missiles"
+	[SpellName(10)] = 5, -- "Blizzard"
+	[SpellName(12051)] = 4, -- "Evocation"
 }
 
 --Spells Effected By Haste
 G.unitframe.HastedChannelTicks = {
-
+	[SpellName(64901)] = true, -- Hymn of Hope
+	[SpellName(64843)] = true, -- Divine Hymn
+	[SpellName(1120)] = true, -- Drain Soul
 }
-
