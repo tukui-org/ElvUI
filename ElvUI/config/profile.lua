b8bba6d948e4a5a42d98a2915f9641233c226d4c
Elv <flipside_22@live.com>
2012-07-31 23:13:38 -0400

    Fix rogue actionbar

--- ElvUI/config/profile.lua
+++ ElvUI/config/profile.lua
@@ -1184,7 +1184,7 @@ P["actionbar"] = {
 			["DRUID"] = "[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] 8; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;",
 			["WARRIOR"] = "[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;",
 			["PRIEST"] = "[bonusbar:1] 7;",
-			["ROGUE"] = "[bonusbar:1] 7; [form:3] 7;",
+			["ROGUE"] = "[stance:1] 7; [stance:3] 7;", -- set to "[stance:1] 7; [stance:3] 10;" if you want a shadow dance bar
 			["MONK"] = "[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;"
 		},
 		['visibility'] = "[petbattle] hide; show",
