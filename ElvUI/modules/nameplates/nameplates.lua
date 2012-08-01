f75eef7145b53b5907aacea9e615bcc042aefa7f
Elv <flipside_22@live.com>
2012-07-31 22:30:19 -0400

    for some reason shadows on nameplates had a higher frame level than the nameplate itself

--- ElvUI/modules/nameplates/nameplates.lua
+++ ElvUI/modules/nameplates/nameplates.lua
@@ -285,6 +285,7 @@ function NP:SkinPlate(frame)
 		frame.hp.shadow:Point("TOPRIGHT", frame.hp, 5, 5)
 		frame.hp.shadow:Point("BOTTOMRIGHT", frame.hp, 5, -5)	
 		frame.hp.shadow:SetBackdropBorderColor(1, 1, 1, 0.75)
+		frame.hp.shadow:SetFrameLevel(0)
 		frame.hp.shadow:SetAlpha(0)
 		self:CreateVirtualFrame(frame.hp)
 		
