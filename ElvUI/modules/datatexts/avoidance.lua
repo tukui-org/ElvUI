1a27fbe6452079a8c0ca412e3d04f962414ef07c
Elv <flipside_22@live.com>
2012-08-03 22:53:21 -0400

    Fix miscalculation in determining total avoidance required to become unhittable

--- ElvUI/modules/datatexts/avoidance.lua
+++ ElvUI/modules/datatexts/avoidance.lua
@@ -5,9 +5,18 @@ local DT = E:GetModule('DataTexts')
 local displayString, lastPanel
 local format = string.format
 local targetlv, playerlv
-local basemisschance, leveldifference, dodge, parry, block, avoidance, unhittable, avoided, blocked
+local basemisschance, leveldifference, dodge, parry, block, avoidance, unhittable, avoided, blocked, numAvoidances, unhittableMax
 local chanceString = "%.2f%%"
 local modifierString = string.join("", "%d (+", chanceString, ")")
+
+function IsWearingShield()
+	local slotID = GetInventorySlotInfo("SecondaryHandSlot")
+	local itemID = GetInventoryItemID('player', slotID)
+	
+	if itemID then
+		return select(7, GetItemInfo(itemID)) == 'Shields'
+	end
+end
 
 local function OnEvent(self, event, unit)
 	if event == "UNIT_AURA" and unit ~= 'player' then return end
@@ -40,24 +49,32 @@ local function OnEvent(self, event, unit)
 		block = (GetBlockChance()+abs(leveldifference*.2))
 	end
 	
+	unhittableMax = 100
+	numAvoidances = 4
 	if dodge <= 0 then dodge = 0 end
 	if parry <= 0 then parry = 0 end
 	if block <= 0 then block = 0 end
 	
-	if E.myclass == "DRUID" then
+	if E.myclass == "DRUID" and GetBonusBarOffset() == 3 then
 		parry = 0
-		block = 0
-	elseif E.myclass == "DEATHKNIGHT" then
-		block = 0
+		numAvoidances = numAvoidances - 1
 	end
+	
+	if not IsWearingShield() then
+		block = 0
+		numAvoidances = numAvoidances - 1
+	end
+	
+	unhittableMax = unhittableMax + ((0.2 * leveldifference) * numAvoidances)
+	
 	avoided = (dodge+parry+basemisschance) --First roll on hit table determining if the hit missed
 	blocked = (100 - avoided)*block/100 --If the hit landed then the second roll determines if the his was blocked
 	avoidance = (avoided+blocked)
-	unhittable = avoidance - 102.4
+	unhittable = avoidance - unhittableMax
 	
 	self.text:SetFormattedText(displayString, L['AVD: '], avoidance)
 
-	
+	--print(unhittableMax) -- should report 102.4 for a level differance of +3 for shield classes, 101.2 for druids, 101.8 for monks and dks
 	lastPanel = self
 end
 
@@ -109,5 +126,5 @@ E['valueColorUpdateFuncs'][ValueColorUpdate] = true
 	onEnterFunc - function to fire OnEnter
 	onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip.
 ]]
-DT:RegisterDatatext('Avoidance', {"UNIT_TARGET", "UNIT_STATS", "UNIT_AURA", "FORGE_MASTER_ITEM_CHANGED", "ACTIVE_TALENT_GROUP_CHANGED", "PLAYER_TALENT_UPDATE"}, OnEvent, nil, nil, OnEnter)
+DT:RegisterDatatext('Avoidance', {"UNIT_TARGET", "UNIT_STATS", "UNIT_AURA", "FORGE_MASTER_ITEM_CHANGED", "ACTIVE_TALENT_GROUP_CHANGED", "PLAYER_TALENT_UPDATE", 'PLAYER_EQUIPMENT_CHANGED'}, OnEvent, nil, nil, OnEnter)
 
