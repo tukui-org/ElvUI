5bb4f4344dffe578fe79d47cbfaa8ad68fa60539
Elv <Adam@ADAM-PC>
2012-08-13 03:07:02 -0400

    Add a font for russian pixel.

--- ElvUI/media/sharedmedia.lua
+++ ElvUI/media/sharedmedia.lua
@@ -1,11 +1,8 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
-if LSM == nil then return end
-
 LSM:Register("background","ElvUI Blank", [[Interface\BUTTONS\WHITE8X8]])
 LSM:Register("border", "ElvUI GlowBorder", [[Interface\AddOns\ElvUI\media\textures\glowTex.tga]])
 LSM:Register("font", "ElvUI Alt-Font", [[Interface\AddOns\ElvUI\media\fonts\Continuum_Medium.ttf]])
-LSM:Register("font", "ElvUI Pixel", [[Interface\AddOns\ElvUI\media\fonts\Homespun.ttf]])
 LSM:Register("font","ElvUI Alt-Combat", [[Interface\AddOns\ElvUI\media\fonts\DieDieDie.ttf]])
 LSM:Register("font","ElvUI Combat", [[Interface\AddOns\ElvUI\media\fonts\Action_Man.ttf]])
 LSM:Register("font","ElvUI Font", [[Interface\AddOns\ElvUI\media\fonts\PT_Sans_Narrow.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
@@ -21,3 +18,9 @@ LSM:Register("sound","Whisper Alert", [[Interface\AddOns\ElvUI\media\sounds\whis
 LSM:Register("statusbar","ElvUI Gloss", [[Interface\AddOns\ElvUI\media\textures\normTex.tga]])
 LSM:Register("statusbar","ElvUI Norm", [[Interface\AddOns\ElvUI\media\textures\normTex2.tga]])
 LSM:Register("statusbar","Minimalist", [[Interface\AddOns\ElvUI\media\textures\Minimalist.tga]])
+
+if GetLocale() == 'ruRU' then
+	LSM:Register("font", "ElvUI Pixel", [[Interface\AddOns\ElvUI\media\fonts\Visitor_Rus.ttf]],LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
+else
+	LSM:Register("font", "ElvUI Pixel", [[Interface\AddOns\ElvUI\media\fonts\Homespun.ttf]])
+end
