fc9aa4d07d6266a6d53ef11f854079285e4f7a5a
Elv <Adam@ADAM-PC>
2012-08-15 11:28:18 -0400

    Fix buff indicator icons not working.

--- ElvUI/config/filters/unitframe.lua
+++ ElvUI/config/filters/unitframe.lua
@@ -75,50 +75,50 @@ end
 
 G.unitframe.buffwatch = {
 	PRIEST = {
-		{6788, "TOPRIGHT", {1, 0, 0}, true},	 -- Weakened Soul
-		{33076, "BOTTOMRIGHT", {0.2, 0.7, 0.2}},	 -- Prayer of Mending
-		{139, "BOTTOMLEFT", {0.4, 0.7, 0.2}}, -- Renew
-		{17, "TOPLEFT", {0.81, 0.85, 0.1}, true},	 -- Power Word: Shield
-		{10060 , "RIGHT", {227/255, 23/255, 13/255}}, -- Power Infusion
-		{47788, "LEFT", {221/255, 117/255, 0}, true}, -- Guardian Spirit
-		{33206, "LEFT", {227/255, 23/255, 13/255}, true}, -- Pain Suppression		
+		ClassBuff(6788, "TOPRIGHT", {1, 0, 0}, true),	 -- Weakened Soul
+		ClassBuff(33076, "BOTTOMRIGHT", {0.2, 0.7, 0.2}),	 -- Prayer of Mending
+		ClassBuff(139, "BOTTOMLEFT", {0.4, 0.7, 0.2}), -- Renew
+		ClassBuff(17, "TOPLEFT", {0.81, 0.85, 0.1}, true),	 -- Power Word: Shield
+		ClassBuff(10060 , "RIGHT", {227/255, 23/255, 13/255}), -- Power Infusion
+		ClassBuff(47788, "LEFT", {221/255, 117/255, 0}, true), -- Guardian Spirit
+		ClassBuff(33206, "LEFT", {227/255, 23/255, 13/255}, true), -- Pain Suppression		
 	},
 	DRUID = {
-		{774, "TOPRIGHT", {0.8, 0.4, 0.8}},	 -- Rejuvenation
-		{8936, "BOTTOMLEFT", {0.2, 0.8, 0.2}},	 -- Regrowth
-		{94447, "TOPLEFT", {0.4, 0.8, 0.2}},	 -- Lifebloom
-		{48438, "BOTTOMRIGHT", {0.8, 0.4, 0}},	 -- Wild Growth
+		ClassBuff(774, "TOPRIGHT", {0.8, 0.4, 0.8}),	 -- Rejuvenation
+		ClassBuff(8936, "BOTTOMLEFT", {0.2, 0.8, 0.2}),	 -- Regrowth
+		ClassBuff(94447, "TOPLEFT", {0.4, 0.8, 0.2}),	 -- Lifebloom
+		ClassBuff(48438, "BOTTOMRIGHT", {0.8, 0.4, 0}),	 -- Wild Growth
 	},
 	PALADIN = {
-		{53563, "TOPRIGHT", {0.7, 0.3, 0.7}},	 -- Beacon of Light
-		{1022, "BOTTOMRIGHT", {0.2, 0.2, 1}, true},	-- Hand of Protection
-		{1044, "BOTTOMRIGHT", {0.89, 0.45, 0}, true},	-- Hand of Freedom
-		{1038, "BOTTOMRIGHT", {0.93, 0.75, 0}, true},	-- Hand of Salvation
-		{6940, "BOTTOMRIGHT", {0.89, 0.1, 0.1}, true},	-- Hand of Sacrifice
+		ClassBuff(53563, "TOPRIGHT", {0.7, 0.3, 0.7}),	 -- Beacon of Light
+		ClassBuff(1022, "BOTTOMRIGHT", {0.2, 0.2, 1}, true),	-- Hand of Protection
+		ClassBuff(1044, "BOTTOMRIGHT", {0.89, 0.45, 0}, true),	-- Hand of Freedom
+		ClassBuff(1038, "BOTTOMRIGHT", {0.93, 0.75, 0}, true),	-- Hand of Salvation
+		ClassBuff(6940, "BOTTOMRIGHT", {0.89, 0.1, 0.1}, true),	-- Hand of Sacrifice
 	},
 	SHAMAN = {
-		{61295, "TOPRIGHT", {0.7, 0.3, 0.7}},	 -- Riptide
-		{974, "BOTTOMLEFT", {0.2, 0.7, 0.2}, true},	 -- Earth Shield
-		{51945, "BOTTOMRIGHT", {0.7, 0.4, 0}},	 -- Earthliving
+		ClassBuff(61295, "TOPRIGHT", {0.7, 0.3, 0.7}),	 -- Riptide
+		ClassBuff(974, "BOTTOMLEFT", {0.2, 0.7, 0.2}, true),	 -- Earth Shield
+		ClassBuff(51945, "BOTTOMRIGHT", {0.7, 0.4, 0}),	 -- Earthliving
 	},
 	MONK = {
-		{119611, "TOPLEFT", {0.8, 0.4, 0.8}},	 --Renewing Mist
-		{116849, "TOPRIGHT", {0.2, 0.8, 0.2}},	 -- Life Cocoon
-		{124682, "BOTTOMLEFT", {0.4, 0.8, 0.2}}, -- Enveloping Mist
-		{124081, "BOTTOMRIGHT", {0.7, 0.4, 0}}, -- Zen Sphere
+		ClassBuff(119611, "TOPLEFT", {0.8, 0.4, 0.8}),	 --Renewing Mist
+		ClassBuff(116849, "TOPRIGHT", {0.2, 0.8, 0.2}),	 -- Life Cocoon
+		ClassBuff(124682, "BOTTOMLEFT", {0.4, 0.8, 0.2}), -- Enveloping Mist
+		ClassBuff(124081, "BOTTOMRIGHT", {0.7, 0.4, 0}), -- Zen Sphere
 	},
 	ROGUE = {
-		{57934, "TOPRIGHT", {227/255, 23/255, 13/255}}, -- Tricks of the Trade
+		ClassBuff(57934, "TOPRIGHT", {227/255, 23/255, 13/255}), -- Tricks of the Trade
 	},
 	MAGE = {
-		{111264, "TOPLEFT", {0.2, 0.2, 1}}, -- Ice Ward
+		ClassBuff(111264, "TOPLEFT", {0.2, 0.2, 1}), -- Ice Ward
 	},
 	WARRIOR = {
-		{114030, "TOPLEFT", {0.2, 0.2, 1}}, -- Vigilance
-		{3411, "TOPRIGHT", {227/255, 23/255, 13/255}}, -- Intervene	
+		ClassBuff(114030, "TOPLEFT", {0.2, 0.2, 1}), -- Vigilance
+		ClassBuff(3411, "TOPRIGHT", {227/255, 23/255, 13/255}), -- Intervene	
 	},
 	DEATHKNIGHT = {
-		{49016, "TOPRIGHT", {227/255, 23/255, 13/255}}, -- Unholy Frenzy	
+		ClassBuff(49016, "TOPRIGHT", {227/255, 23/255, 13/255}), -- Unholy Frenzy	
 	},
 }
 
