6e3d9f1f1551af7a64bcf0073da37234f1e2f754
Elv <Adam@ADAM-PC>
2012-08-19 08:10:01 -0400

    reverse a few fonts

--- ElvUI/core/fonts.lua
+++ ElvUI/core/fonts.lua
@@ -9,27 +9,26 @@ local function SetFont(obj, font, size, style, r, g, b, sr, sg, sb, sox, soy)
 	elseif r then obj:SetAlpha(r) end
 end
 
-function E:UpdateBlizzardFonts()	
+function E:UpdateBlizzardFonts()
 	local NORMAL     = self["media"].normFont
 	local COMBAT     = LSM:Fetch('font', self.private.general.dmgfont)
-	local NUMBER     = LSM:Fetch('font', 'ElvUI Pixel')
+	local NUMBER     = self["media"].normFont
 	local CHAT		 = LSM:Fetch('font', self.db.chat.font)
 	local MONOCHROME = ''
 	local _, editBoxFontSize, _, _, _, _, _, _, _, _ = GetChatWindowInfo(1)
-	
+
 	UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT = 12
 	CHAT_FONT_HEIGHTS = {12, 13, 14, 15, 16, 17, 18, 19, 20}
-	
+
 	UNIT_NAME_FONT     = CHAT
 	NAMEPLATE_FONT     = CHAT
 	DAMAGE_TEXT_FONT   = COMBAT
 	STANDARD_TEXT_FONT = NORMAL
-	
-	
+
 	if self.db.general.font:lower():find('pixel') then
 		MONOCHROME = 'MONOCHROME'
 	end
-	
+
 	if self.eyefinity then
 		-- damage are huge on eyefinity, so we disable it
 		InterfaceOptionsCombatTextPanelTargetDamage:Hide()
@@ -40,19 +39,19 @@ function E:UpdateBlizzardFonts()
 		SetCVar("PetMeleeDamage",0)
 		SetCVar("CombatDamage",0)
 		SetCVar("CombatHealing",0)
-		
+
 		-- set an invisible font for xp, honor kill, etc
 		local INVISIBLE = [=[Interface\Addons\ElvUI\media\fonts\Invisible.ttf]=]
 		COMBAT = INVISIBLE
-	end	
-	
+	end
+
 	-- Base fonts
-	SetFont(NumberFontNormal,					NUMBER, 10, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0, 1, -1)
+	SetFont(NumberFontNormal,					LSM:Fetch('font', 'ElvUI Pixel'), 10, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0, 1, -1)
 	SetFont(GameTooltipHeader,                  NORMAL, self.db.general.fontSize)
-	SetFont(NumberFont_OutlineThick_Mono_Small, NUMBER, 10, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0, 1, -1)
-	SetFont(NumberFont_Outline_Huge,            NUMBER, 28, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0, 1, -1)
-	SetFont(NumberFont_Outline_Large,           NUMBER, 14, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0, 1, -1)
-	SetFont(NumberFont_Outline_Med,             NUMBER, 10, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0, 1, -1)
+	SetFont(NumberFont_OutlineThick_Mono_Small, NUMBER, self.db.general.fontSize, "OUTLINE")
+	SetFont(NumberFont_Outline_Huge,            NUMBER, 28, MONOCHROME.."THICKOUTLINE", 28)
+	SetFont(NumberFont_Outline_Large,           NUMBER, 15, MONOCHROME.."OUTLINE")
+	SetFont(NumberFont_Outline_Med,             NUMBER, self.db.general.fontSize*1.1, "OUTLINE")
 	SetFont(NumberFont_Shadow_Med,              NORMAL, self.db.general.fontSize) --chat editbox uses this
 	SetFont(NumberFont_Shadow_Small,            NORMAL, self.db.general.fontSize)
 	SetFont(QuestFont,                          NORMAL, self.db.general.fontSize)
@@ -63,7 +62,7 @@ function E:UpdateBlizzardFonts()
 	SetFont(SystemFont_Med1,                    NORMAL, self.db.general.fontSize)
 	SetFont(SystemFont_Med3,                    NORMAL, self.db.general.fontSize*1.1)
 	SetFont(SystemFont_OutlineThick_Huge2,      NORMAL, 20, MONOCHROME.."THICKOUTLINE")
-	SetFont(SystemFont_Outline_Small,           NUMBER, 10, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0, 1, -1)
+	SetFont(SystemFont_Outline_Small,           NUMBER, self.db.general.fontSize, "OUTLINE")
 	SetFont(SystemFont_Shadow_Large,            NORMAL, 15)
 	SetFont(SystemFont_Shadow_Med1,             NORMAL, self.db.general.fontSize)
 	SetFont(SystemFont_Shadow_Med3,             NORMAL, self.db.general.fontSize*1.1)
