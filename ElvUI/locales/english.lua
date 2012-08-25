6abfa66c3943246d88ab6bbba09f722934d20641
Elv <Adam@ADAM-PC>
2012-08-25 13:46:42 -0400

    Update All locales.

--- ElvUI/locales/english.lua
+++ ElvUI/locales/english.lua
@@ -1,7 +1,6 @@
 -- English localization file for enUS and enGB.
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
 local L = AceLocale:NewLocale("ElvUI", "enUS", true);
-
 if not L then return; end
 
 --ActionBars
