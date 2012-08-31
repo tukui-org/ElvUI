9ad88d801b5ba39243e8c1e5ca49c0c28eeff172
Elv <Adam@ADAM-PC>
2012-08-31 18:47:21 -0400

    quick typo fix

--- ElvUI/locales/english.lua
+++ ElvUI/locales/english.lua
@@ -574,7 +574,7 @@ L["A setting you have changed will change an option for this character only. Thi
 L["Are you sure you want to delete all your gray items?"] = true;
 L["Are you sure you want to disband the group?"] = true;
 L["Are you sure you want to reset every mover back to it's default position?"] = true;
-L["Because of the mass confusion caused by the new aura system I've implemented a new step to the installation process. This is optional. If you like how your auras are setup go to the last setup and click finished to not be prompted again."] = true;
+L["Because of the mass confusion caused by the new aura system I've implemented a new step to the installation process. This is optional. If you like how your auras are setup go to the last step and click finished to not be prompted again."] = true;
 L["Can't buy anymore slots!"] = true;
 L["Hover your mouse over any actionbutton or spellbook button to bind it. Press the escape key or right click to clear the current actionbutton's keybinding."] = true;
 L["One or more of the changes you have made require a ReloadUI."] = true;
