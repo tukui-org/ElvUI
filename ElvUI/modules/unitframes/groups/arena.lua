9752b8f020d7793712578d615282727b0531e4d1
Elv <Adam@ADAM-PC>
2012-09-21 04:26:15 -0400

    Fix lua error

--- ElvUI/modules/unitframes/groups/arena.lua
+++ ElvUI/modules/unitframes/groups/arena.lua
@@ -364,7 +364,7 @@ function UF:Update_ArenaFrames(frame, db)
 			frame:Tag(frame[objectName], objectDB.text_format or '')
 			frame[objectName]:SetJustifyH(objectDB.justifyH or 'CENTER')
 			frame[objectName]:ClearAllPoints()
-			frame[objectName]:SetPoint(objectDB.justifyH, frame, 'CENTER', objectDB.xOffset, objectDB.yOffset)
+			frame[objectName]:SetPoint(objectDB.justifyH or 'CENTER', frame, 'CENTER', objectDB.xOffset, objectDB.yOffset)
 		end
 	end
 	
