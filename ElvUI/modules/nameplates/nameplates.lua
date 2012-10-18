dc1ccbb866496ab70af80f90507b078868054bbf
Elv <Adam@ADAM-PC>
2012-10-17 23:45:43 -0400

    non pixelmode nameplate border got brokeneded

--- ElvUI/modules/nameplates/nameplates.lua
+++ ElvUI/modules/nameplates/nameplates.lua
@@ -323,7 +323,7 @@ function NP:SkinPlate(frame)
 	if not frame.hp then
 		frame.oldhp = oldhp
 		frame.oldhp:HookScript('OnValueChanged', NP.ScanHealth)
-		local SHADOW_SPACING = ((noscalemult * 5) - UIParent:GetScale() / 3)
+		local SHADOW_SPACING = E.PixelMode and (((noscalemult * 5) - UIParent:GetScale() / 3)) or 5
 		frame.hp = CreateFrame("Statusbar", nil, frame)
 		frame.hp:SetFrameLevel(oldhp:GetFrameLevel() + 1)
 		frame.hp:SetFrameStrata(oldhp:GetFrameStrata())
