14a1eccb9f21627785ee650ee86254b38e6b116a
Elv <Adam@ADAM-PC>
2012-10-25 02:41:45 -0400

    lua error on sort

--- ElvUI/modules/bags/sort.lua
+++ ElvUI/modules/bags/sort.lua
@@ -347,7 +347,7 @@ end
 function B:CanItemGoInBag(bag, slot, targetBag)
 	local item = bagIDs[B:Encode_BagSlot(bag, slot)]
 	local itemFamily = GetItemFamily(item)
-	if itemFamily > 0 then
+	if itemFamily and itemFamily > 0 then
 		local equipSlot = select(9, GetItemInfo(item))
 		if equipSlot == "INVTYPE_BAG" then
 			itemFamily = 1
