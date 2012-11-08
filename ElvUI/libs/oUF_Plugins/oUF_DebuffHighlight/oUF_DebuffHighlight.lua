29a9bf0e09d7a6cf0ab0e5af0cf49545f1140a90
Elv <Adam@ADAM-PC>
2012-11-08 18:25:48 -0500

    Apparent debuff highighting fix

--- ElvUI/libs/oUF_Plugins/oUF_DebuffHighlight/oUF_DebuffHighlight.lua
+++ ElvUI/libs/oUF_Plugins/oUF_DebuffHighlight/oUF_DebuffHighlight.lua
@@ -71,7 +71,9 @@ local function CheckSpec(self, event, levels)
 end
  
 local function Update(object, event, unit)
+	if unit ~= object.unit then return; end
 	local debuffType, texture  = GetDebuffType(unit, object.DebuffHighlightFilter)
+
 	if debuffType then
 		local color = DebuffTypeColor[debuffType]
 		if object.DebuffHighlightBackdrop then
