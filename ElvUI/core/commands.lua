fbc56cec518974b7c82e9bcb18b3f9546f792816
Elv <Adam@ADAM-PC>
2012-12-07 00:43:48 -0800

    Add a command to help me cleanup my shitty guild.

--- ElvUI/core/commands.lua
+++ ElvUI/core/commands.lua
@@ -129,6 +129,43 @@ function E:DelayScriptCall(msg)
 	end
 end
 
+function E:MassGuildKick(msg)
+	local minLevel, minDays, minRankIndex = string.split(',', msg)
+	minRankIndex = tonumber(minRankIndex);
+	minLevel = tonumber(minLevel);
+	minDays = tonumber(minDays);
+	
+	if not minLevel or not minDays then
+		E:Print("Usage: /cleanguild <minLevel>, <minDays>, [<minRankIndex>]");
+		return;
+	end
+	
+	if minDays > 31 then
+		E:Print("Maximum days value must be below 32.");
+		return;
+	end
+	
+	if not minRankIndex then minRankIndex = GuildControlGetNumRanks() - 1 end
+	
+	for i = 1, GetNumGuildMembers() do
+		local name, _, rankIndex, level, class, _, note, officerNote, connected, _, class = GetGuildRosterInfo(i)
+		local minLevelx = minLevel
+		
+		if class == "DEATHKNIGHT" then
+			minLevelx = minLevelx + 55
+		end
+		
+		if not connected then
+			local years, months, days, hours = GetGuildRosterLastOnline(i)
+			if days ~= nil and ((years > 0 or months > 0 or days >= minDays) and rankIndex >= minRankIndex) and note ~= nil and officerNote ~= nil and (level <= minLevelx) then
+				GuildUninvite(name)
+			end
+		end
+	end
+
+	SendChatMessage("Guild Cleanup Results: Removed all guild members below rank "..GuildControlGetRankName(minRankIndex)..", that have a minimal level of "..minLevel..", and have not been online for at least: "..minDays.." days.", "GUILD")
+end
+
 function E:LoadCommands()
 	self:RegisterChatCommand("in", "DelayScriptCall")
 	self:RegisterChatCommand("ec", "ToggleConfig")
@@ -148,6 +185,7 @@ function E:LoadCommands()
 	self:RegisterChatCommand('elvsays', 'ElvSays')
 	self:RegisterChatCommand('elvsayschannel', 'ElvSaysChannel')
 	self:RegisterChatCommand('elvsaystarget', 'ElvSaysTarget')
+	self:RegisterChatCommand('cleanguild', 'MassGuildKick')
 	if E.ActionBars then
 		self:RegisterChatCommand('kb', E.ActionBars.ActivateBindMode)
 	end
