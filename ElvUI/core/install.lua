a690db96363cf7d9adb7b2a7fb07df5f1ae08fde
Blazeflack <blazeflack@hotmail.com>
2012-12-27 03:14:32 +0100

    Modify install, setup auras for healer layout

--- ElvUI/core/install.lua
+++ ElvUI/core/install.lua
@@ -342,16 +342,20 @@ function E:SetupLayout(layout, noDataReset)
 		end
 		
 		if not noDataReset then
-			--Player, Target and Focus settings are set in SetupAuras
+			E.db.unitframe.units.target.buffs.numrows = 1;
+			E.db.unitframe.units.target.buffs.anchorPoint = 'TOPRIGHT';
+			E.db.unitframe.units.target.debuffs.numrows = 1;
+			E.db.unitframe.units.target.debuffs.anchorPoint = 'TOPRIGHT';			
 			E.db.unitframe.units.target.castbar.yOffset = 0;
+
 			E.db.unitframe.units.targettarget.height = 35;
+			E.db.unitframe.units.targettarget.debuffs.anchorPoint = 'TOPRIGHT';
+
+			E.db.unitframe.units.focus.buffs.anchorPoint = 'TOPLEFT';
+			E.db.unitframe.units.focus.debuffs.anchorPoint = 'TOPLEFT';
 			E.db.unitframe.units.focus.castbar.yOffset = 0;
-			
-			E.db.unitframe.units.focustarget.buffs['growth-y'] = 'UP';
-			E.db.unitframe.units.focustarget.buffs.initialAnchor = 'BOTTOMLEFT';
+
 			E.db.unitframe.units.focustarget.buffs.anchorPoint = 'TOPLEFT';
-			E.db.unitframe.units.focustarget.debuffs['growth-y'] = 'UP';
-			E.db.unitframe.units.focustarget.debuffs.initialAnchor = 'BOTTOMLEFT';
 			E.db.unitframe.units.focustarget.debuffs.anchorPoint = 'TOPLEFT';
 			
 			E.db.unitframe.units.boss.growthDirection = 'UP';
@@ -464,20 +468,6 @@ end
 
 
 local function SetupAuras(style)
-	E:CopyTable(E.db.unitframe.units.player.buffs, P.unitframe.units.player.buffs)
-	E:CopyTable(E.db.unitframe.units.player.debuffs, P.unitframe.units.player.debuffs)
-	E:CopyTable(E.db.unitframe.units.player.aurabar, P.unitframe.units.player.aurabar)
-	
-	E:CopyTable(E.db.unitframe.units.target.buffs, P.unitframe.units.target.buffs)
-	E:CopyTable(E.db.unitframe.units.target.debuffs, P.unitframe.units.target.debuffs)	
-	E:CopyTable(E.db.unitframe.units.target.aurabar, P.unitframe.units.target.aurabar)
-	E.db.unitframe.units.target.smartAuraDisplay = P.unitframe.units.target.smartAuraDisplay
-	
-	E:CopyTable(E.db.unitframe.units.focus.buffs, P.unitframe.units.focus.buffs)
-	E:CopyTable(E.db.unitframe.units.focus.debuffs, P.unitframe.units.focus.debuffs)	
-	E:CopyTable(E.db.unitframe.units.focus.aurabar, P.unitframe.units.focus.aurabar)
-	E.db.unitframe.units.focus.smartAuraDisplay = P.unitframe.units.focus.smartAuraDisplay		
-	
 	if not style then
 		--PLAYER
 		E.db.unitframe.units.player.buffs.enable = true;
