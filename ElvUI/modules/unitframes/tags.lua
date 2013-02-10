0755151c3005be2e15a53805b2ea17ddfcc91fcc
Elv <Adam@ADAM-PC>
2013-02-10 07:12:08 -0500

    rename to [swiftmendtargets]

--- ElvUI/modules/unitframes/tags.lua
+++ ElvUI/modules/unitframes/tags.lua
@@ -474,7 +474,7 @@ local function GetDistance(x1, y1, x2, y2)
 	return ceil(sqrt((x2 - x1)^2 + (y2 - y1)^2) / 0.04249)
 end
 
-ElvUF.Tags.Methods['numswiftmendtargets'] = function(unit)
+ElvUF.Tags.Methods['swiftmendtargets'] = function(unit)
 	local px, py = GetPlayerMapPosition(unit)
 	local unitsInRange = 0
 	for groupUnit, _ in pairs(GroupUnits) do
