80d4bc740f6613a8d58eff500d9652d94dc48eb6
Elv <Adam@ADAM-PC>
2013-03-06 13:54:19 -0500

    Fix reported bug with aura filtering.

--- ElvUI/modules/unitframes/elements/aurabars.lua
+++ ElvUI/modules/unitframes/elements/aurabars.lua
@@ -103,7 +103,7 @@ function UF:AuraBarFilter(unit, name, rank, icon, count, debuffType, duration, e
 	end
 	
 	if UF:CheckFilter(db.onlyDispellable, isFriend) then
-		if (self.type == 'buffs' and not isStealable) or (self.type == 'debuffs' and dtype and  not E:IsDispellableByMe(dtype)) then
+		if (self.type == 'buffs' and not isStealable) or (self.type == 'debuffs' and dtype and  not E:IsDispellableByMe(dtype)) or dtype == nil then
 			returnValue = false;
 		end
 		anotherFilterExists = true
