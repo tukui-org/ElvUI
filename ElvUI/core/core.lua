af3a469e502a94f945cfa936fe06c5551bff36e6
Elv <Adam@ADAM-PC>
2013-03-07 03:10:10 -0500

    Fix a retarded lua error.

--- ElvUI/core/core.lua
+++ ElvUI/core/core.lua
@@ -471,7 +471,7 @@ function E:UpdateAll(ignoreInstall)
 	self.data.RegisterCallback(self, "OnProfileChanged", "UpdateAll")
 	self.data.RegisterCallback(self, "OnProfileCopied", "UpdateAll")
 	self.data.RegisterCallback(self, "OnProfileReset", "OnProfileReset")
-	
+	LibStub('LibDualSpec-1.0'):EnhanceDatabase(self.data, "ElvUI")
 	self.db = self.data.profile;
 	self.global = self.data.global;
 	
