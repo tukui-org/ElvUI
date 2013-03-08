186ac3327694cd40bbc712934ee7d417b795294b
Elv <Adam@ADAM-PC>
2013-03-08 15:02:40 -0500

    God help us all; health values reached 1 billion.

--- ElvUI/core/math.lua
+++ ElvUI/core/math.lua
@@ -10,10 +10,12 @@ local floor = math.floor
 
 --Return short value of a number
 function E:ShortValue(v)
-	if v >= 1e6 then
-		return ("%.1fm"):format(v / 1e6):gsub("%.?0+([km])$", "%1")
+	if v >= 1e9 then
+		return ("%.1fb"):format(v / 1e9):gsub("%.?0+([kmb])$", "%1")
+	elseif v >= 1e6 then
+		return ("%.1fm"):format(v / 1e6):gsub("%.?0+([kmb])$", "%1")
 	elseif v >= 1e3 or v <= -1e3 then
-		return ("%.1fk"):format(v / 1e3):gsub("%.?0+([km])$", "%1")
+		return ("%.1fk"):format(v / 1e3):gsub("%.?0+([kmb])$", "%1")
 	else
 		return v
 	end
