488531b4f83ef7ceff9bf2ca4d0788b3b7a5ead8
Elv <Adam@ADAM-PC>
2013-05-30 17:49:45 -0400

    Remove loot specialization datatext, rewrite specswitch datatext to be "Talent / Loot Specialization", renaming the datatext, this may reset the specswitch datatext setting if you were using it.

--- ElvUI/modules/datatexts/specswitch.lua
+++ ElvUI/modules/datatexts/specswitch.lua
@@ -8,17 +8,50 @@ local displayString = '';
 local talent = {}
 local activeString = join("", "|cff00FF00" , ACTIVE_PETS, "|r")
 local inactiveString = join("", "|cffFF0000", FACTION_INACTIVE, "|r")
+local menuFrame = CreateFrame("Frame", "LootSpecializationDatatextClickMenu", E.UIParent, "UIDropDownMenuTemplate")
+local menuList = {
+	{ text = SELECT_LOOT_SPECIALIZATION, isTitle = true, notCheckable = true },
+	{ notCheckable = true, func = function() SetLootSpecialization(0) end },
+	{ notCheckable = true },
+	{ notCheckable = true },
+	{ notCheckable = true },
+	{ notCheckable = true }
+}
+
 
 local function OnEvent(self, event)
 	lastPanel = self
-	if not GetSpecializationInfo(1) then
-		return
-	end	
+
+	local specIndex = GetSpecialization();
+	if not specIndex then return end	
 	
 	active = GetActiveSpecGroup()
+
+	local talent, loot = '', ''
 	if GetSpecialization(false, false, active) then
-		self.text:SetText(select(2, GetSpecializationInfo(GetSpecialization(false, false, active))))
+		talent = format('|T%s:14:14:0:0:64:64:4:60:4:60|t', select(4, GetSpecializationInfo(GetSpecialization(false, false, active))))
 	end
+
+	local specialization = GetLootSpecialization()
+	if specialization == 0 then
+		local specIndex = GetSpecialization();
+		
+		if specIndex then
+			local specID, _, _, texture = GetSpecializationInfo(specIndex);
+			loot = format('|T%s:14:14:0:0:64:64:4:60:4:60|t**', texture)
+		else
+			loot = 'N/A'
+		end
+	else
+		local specID, _, _, texture = GetSpecializationInfoByID(specialization);
+		if specID then
+			loot = format('|T%s:14:14:0:0:64:64:4:60:4:60|t', texture)
+		else
+			loot = 'N/A'
+		end
+	end
+
+	self.text:SetText(format('%s: %s %s: %s', L['Spec'], talent, LOOT, loot))
 end
 
 local function OnEnter(self)
@@ -29,12 +62,52 @@ local function OnEnter(self)
 			DT.tooltip:AddLine(join(" ", format(displayString, select(2, GetSpecializationInfo(GetSpecialization(false, false, i)))), (i == active and activeString or inactiveString)),1,1,1)
 		end
 	end
+
+	DT.tooltip:AddLine(' ')
+	local specialization = GetLootSpecialization()
+	if specialization == 0 then
+		local specIndex = GetSpecialization();
+		
+		if specIndex then
+			local specID, name = GetSpecializationInfo(specIndex);
+			DT.tooltip:AddLine(format('|cffFFFFFF%s:|r %s', SELECT_LOOT_SPECIALIZATION, format(LOOT_SPECIALIZATION_DEFAULT, name)))
+		end
+	else
+		local specID, name = GetSpecializationInfoByID(specialization);
+		if specID then
+			DT.tooltip:AddLine(format('|cffFFFFFF%s:|r %s', SELECT_LOOT_SPECIALIZATION, name))
+		end
+	end
+
+	DT.tooltip:AddLine(' ')
+	DT.tooltip:AddLine(L['|cffFFFFFFLeft Click:|r Change Talent Specialization'])
+	DT.tooltip:AddLine(L['|cffFFFFFFRight Click:|r Change Loot Specialization'])	
 	
 	DT.tooltip:Show()
 end
 
-local function OnClick(self)
-	SetActiveSpecGroup(active == 1 and 2 or 1)
+local function OnClick(self, button)
+	local specIndex = GetSpecialization();
+	if not specIndex then return end
+
+	if button == "LeftButton" then
+		SetActiveSpecGroup(active == 1 and 2 or 1)
+	else
+		local specID, specName = GetSpecializationInfo(specIndex);
+		menuList[2].text = format(LOOT_SPECIALIZATION_DEFAULT, specName);
+
+		for index = 1, 4 do
+			local id, name = GetSpecializationInfo(index);
+			if ( id ) then
+				menuList[index + 2].text = name
+				menuList[index + 2].func = function() SetLootSpecialization(id) end
+			else
+				menuList[index + 2] = nil
+			end
+		end
+
+		EasyMenu(menuList, menuFrame, "cursor", -15, -7, "MENU", 2)
+	end
 end
 
 local function ValueColorUpdate(hex, r, g, b)
@@ -56,4 +129,4 @@ E['valueColorUpdateFuncs'][ValueColorUpdate] = true
 	click - function to fire when clicking the datatext
 	onEnterFunc - function to fire OnEnter
 ]]
-DT:RegisterDatatext('Spec Switch',{"PLAYER_ENTERING_WORLD", "CHARACTER_POINTS_CHANGED", "PLAYER_TALENT_UPDATE", "ACTIVE_TALENT_GROUP_CHANGED"}, OnEvent, nil, OnClick, OnEnter)
+DT:RegisterDatatext('Talent/Loot Specialization',{"PLAYER_ENTERING_WORLD", "CHARACTER_POINTS_CHANGED", "PLAYER_TALENT_UPDATE", "ACTIVE_TALENT_GROUP_CHANGED", 'PLAYER_LOOT_SPEC_UPDATED'}, OnEvent, nil, OnClick, OnEnter)
