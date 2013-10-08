9e100a029c0084f1344dfc104a9c9f7e52260dc2
Elv <elv@tukui.org>
2013-10-08 15:39:50 -0400

    Coloring bug.

--- ElvUI/modules/unitframes/elements/aurabars.lua
+++ ElvUI/modules/unitframes/elements/aurabars.lua
@@ -187,16 +187,19 @@ function UF:ColorizeAuraBars(event, unit)
 		end
 
 		if UF.db.colors.transparentAurabars and not frame.statusBar.isTransparent then
-			UF:ToggleTransparentStatusBar(true, frame.statusBar, frame.statusBar.bg, nil, true)
+			UF:ToggleTransparentStatusBar(true, frame.statusBar, frame.statusBar.bg, nil, true)	
+		elseif(frame.statusBar.isTransparent and not UF.db.colors.transparentAurabars) then
+			UF:ToggleTransparentStatusBar(false, frame.statusBar, frame.statusBar.bg, nil, true)
+		end	
+
+		if(UF.db.colors.transparentAurabars) then
 			local _, _, _, alpha = frame:GetBackdropColor()
 			if colors then
 				frame:SetBackdropColor(colors.r * 0.58, colors.g * 0.58, colors.b * 0.58, alpha)
 			else
 				local r, g, b = frame.statusBar:GetStatusBarColor()
 				frame:SetBackdropColor(r * 0.58, g * 0.58, b * 0.58, alpha)
-			end			
-		elseif(frame.statusBar.isTransparent and not UF.db.colors.transparentAurabars) then
-			UF:ToggleTransparentStatusBar(false, frame.statusBar, frame.statusBar.bg, nil, true)
-		end	
+			end		
+		end
 	end
 end
