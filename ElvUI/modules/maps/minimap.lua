7d296817553eecbb3d2de1ab615aa1bfd762b723
Elv <elv@tukui.org>
2013-10-10 13:40:53 -0400

    Register Astrolabe with ElvUIFarmMode minimap. Should allow Gatherer addon to work with the /farmmode map.

--- ElvUI/modules/maps/minimap.lua
+++ ElvUI/modules/maps/minimap.lua
@@ -2,6 +2,9 @@ local E, L, V, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, Private
 local M = E:NewModule('Minimap', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0');
 E.Minimap = M
 
+local Astrolabe = DongleStub("Astrolabe-1.0")
+local AstrolabeMapMonitor = DongleStub("AstrolabeMapMonitor")
+
 local gsub = string.gsub
 local upper = string.upper
 
@@ -347,6 +350,7 @@ function M:Initialize()
 	fm:SetMovable(true)
 	fm:SetScript("OnDragStart", function(self) self:StartMoving() end)
 	fm:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
+	AstrolabeMapMonitor:MonitorWorldMap(fm)
 	fm:Hide()
 	E.FrameLocks['FarmModeMap'] = true;
 	
@@ -368,6 +372,7 @@ function M:Initialize()
 		if IsAddOnLoaded('GatherMate2') then
 			LibStub('AceAddon-3.0'):GetAddon('GatherMate2'):GetModule('Display'):ReparentMinimapPins(FarmModeMap)
 		end		
+		Astrolabe:SetTargetMinimap(FarmModeMap)
 	end)
 	
 	FarmModeMap:SetScript('OnHide', function() 
@@ -386,6 +391,7 @@ function M:Initialize()
 		if IsAddOnLoaded('GatherMate2') then
 			LibStub('AceAddon-3.0'):GetAddon('GatherMate2'):GetModule('Display'):ReparentMinimapPins(Minimap)
 		end	
+		Astrolabe:SetTargetMinimap(Minimap)
 	end)
 
 	
