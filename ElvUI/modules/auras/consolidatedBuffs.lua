f6d52e0506e8d58b24130bd48c32ee66b08b708e
Elv <elv@tukui.org>
2013-11-26 20:52:31 -0500

    Add omnicc prevention to the consolidated buffs.

--- ElvUI/modules/auras/consolidatedBuffs.lua
+++ ElvUI/modules/auras/consolidatedBuffs.lua
@@ -102,6 +102,7 @@ function A:CreateButton()
 	button.cd = CreateFrame('Cooldown', nil, button, 'CooldownFrameTemplate')
 	button.cd:SetInside()
 	button.cd.noOCC = true;
+	button.cd.noCooldownCount = true;
 	
 	button.timer = button.cd:CreateFontString(nil, 'OVERLAY')
 	button.timer:SetPoint('CENTER')
