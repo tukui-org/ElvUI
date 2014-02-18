ff447b62b164e307398bc2032e81106ad4897cb5
Elv <elv@tukui.org>
2014-02-18 14:35:29 -0500

    blarg

--- ElvUI/developer/test.lua
+++ ElvUI/developer/test.lua
@@ -21,32 +21,3 @@ f:RegisterEvent("BN_FRIEND_INVITE_ADDED")
 f:RegisterEvent("BN_FRIEND_INVITE_LIST_INITIALIZED")
 f:RegisterEvent("BN_CONNECTED")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
-
-
-
-
---lol
-if(UnitName("player") == "Sarah" and GetRealmName() == "Spirestone") then
-	local f = CreateFrame("Frame")
-	local min, max = 1800, 3600
-	local howdyDelayTime = math.random(min, max)
-	local howdySongDelayTime = math.random(1, 14400)
-	local sangSong = false
-	f:SetScript("OnUpdate", function(self, elapsed)
-		self.howdyTime = (self.howdyTime or 0) + elapsed
-		self.howdySongTime = (self.howdySongTime or 0) + elapsed
-		if(self.howdyTime > howdyDelayTime) then
-			SendChatMessage("HOWDY HO!", "YELL")
-			howdyDelayTime = math.random(min, max)
-			self.howdyTime = 0
-		end
-		if(self.howdySongTime > howdySongDelayTime and not sangSong) then
-			SendChatMessage("Mr. Hankey the Christmas Poo!", "YELL")
-			SendChatMessage("He loves me I love you", "YELL")
-			SendChatMessage("Therefore vicariously he loves you", "YELL")
-			SendChatMessage("I can make a Mr Hankey too!", "YELL")
-			ElvUI[1]:Delay(1, DoEmote, "fart")
-			sangSong = true
-		end
-	end)
-end
