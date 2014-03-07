a629fba35022c84553e658328af5a2a16fcc7f52
Blazeflack <blazeflack@hotmail.com>
2014-03-07 19:31:04 +0100

    Reflect recent changes in install.lua

--- ElvUI/core/install.lua
+++ ElvUI/core/install.lua
@@ -235,9 +235,9 @@ function E:SetupTheme(theme, noDisplayMsg)
 		E.db.unitframe.colors.castClassColor = true
 	else
 		E.db.general.bordercolor = E:GetColor(.2, .2, .2)
-		E.db.general.backdropcolor = E:GetColor(.16, .16, .16)
-		E.db.general.backdropfadecolor = E:GetColor(.10, .10, .10, .8)
-		E.db.unitframe.colors.auraBarBuff = E:GetColor(.2, .2, .2)
+		E.db.general.backdropcolor = E:GetColor(40/255, 40/255, 40/255)
+		E.db.general.backdropfadecolor = E:GetColor(26/255, 26/255, 26/255, .8)
+		E.db.unitframe.colors.auraBarBuff = E:GetColor(40/255, 40/255, 40/255)
 		E.db.unitframe.colors.healthclass = false
 		E.db.unitframe.colors.health = E:GetColor(.267, .267, .267)
 		E.db.unitframe.colors.castColor = E:GetColor(.66, .66, .66)
