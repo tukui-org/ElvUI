063d88082d3d984a96390f53359a937ee553a395
Elv <elv@tukui.org>
2014-09-16 00:03:54 -0400

    Add flash loop animation.

--- ElvUI/core/animation.lua
+++ ElvUI/core/animation.lua
@@ -4,6 +4,7 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 
 local random = math.random
+
 function E:SetUpAnimGroup(object, type, ...)
 	if not type then type = 'Flash' end
 	
@@ -16,6 +17,21 @@ function E:SetUpAnimGroup(object, type, ...)
 		object.anim.fadeout = object.anim:CreateAnimation("ALPHA", "FadeOut")
 		object.anim.fadeout:SetChange(-1)
 		object.anim.fadeout:SetOrder(1)
+	elseif type == 'FlashLoop' then
+		object.anim = object:CreateAnimationGroup("Flash")
+		object.anim.fadein = object.anim:CreateAnimation("ALPHA", "FadeIn")
+		object.anim.fadein:SetChange(1)
+		object.anim.fadein:SetOrder(2)
+
+		object.anim.fadeout = object.anim:CreateAnimation("ALPHA", "FadeOut")
+		object.anim.fadeout:SetChange(-1)
+		object.anim.fadeout:SetOrder(1)	
+
+		object.anim:SetScript("OnFinished", function(self, requested)
+			if(not requested) then
+				object.anim:Play()
+			end
+		end)	
 	elseif type == 'Shake' then
 		object.shake = object:CreateAnimationGroup("Shake")
 		object.shake:SetLooping("REPEAT")
@@ -117,9 +133,9 @@ function E:StopShakeHorizontal(object)
 	end
 end
 
-function E:Flash(object, duration)
+function E:Flash(object, duration, loop)
 	if not object.anim then
-		E:SetUpAnimGroup(object, 'Flash')
+		E:SetUpAnimGroup(object, loop and "FlashLoop" or 'Flash')
 	end
 
 	if not object.anim.playing then
