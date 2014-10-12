458fd19081a94dc424098b25fe71ccbdb9b472a2
Elv <elv@tukui.org>
2014-10-12 09:36:21 -0400

    Add developer command /framelist. Will list all the frames under the mouse and output to the copy chat frame. Args: true or false to display all hidden frames under mouse as well. Defaults to false.

--- ElvUI/developer/frame.lua
+++ ElvUI/developer/frame.lua
@@ -49,6 +49,41 @@ SlashCmdList["FRAME"] = function(arg)
 	end
 end
 
+
+SLASH_FRAMELIST1 = "/framelist"
+SlashCmdList["FRAMELIST"] = function(msg)
+	if(not FrameStackTooltip) then
+		UIParentLoadAddOn("Blizzard_DebugTools");
+	end
+
+	local isPreviouslyShown = FrameStackTooltip:IsShown()
+	if(not isPreviouslyShown) then
+		if(msg == tostring(true)) then
+			FrameStackTooltip_Toggle(true);
+		else
+			FrameStackTooltip_Toggle();
+		end
+	end
+
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	for i = 2, FrameStackTooltip:NumLines() do
+		local text = _G["FrameStackTooltipTextLeft"..i]:GetText();
+		if(text and text ~= "") then
+			print(text)
+		end
+	end
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+	if(CopyChatFrame:IsShown()) then
+		CopyChatFrame:Hide()
+	end
+
+	ElvUI[1]:GetModule("Chat"):CopyChat(ChatFrame1)
+	if(not isPreviouslyShown) then
+		FrameStackTooltip_Toggle();
+	end
+end
+
 function TextureList(frame)
 	frame = _G[frame] or FRAME
 	--[[for key, obj in pairs(frame) do
