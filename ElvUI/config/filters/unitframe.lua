eb596ebef09403f71f683e9075dd727d955870b4
Elv <elv@tukui.org>
2014-10-15 12:46:57 -0400

    Update eternal flame spell id for holy paladins.

--- ElvUI/config/filters/unitframe.lua
+++ ElvUI/config/filters/unitframe.lua
@@ -679,7 +679,7 @@ G.unitframe.buffwatch = {
 		ClassBuff(6940, "BOTTOMRIGHT", {0.89, 0.1, 0.1}, true),	-- Hand of Sacrifice
 		ClassBuff(114039, "BOTTOMRIGHT", {164/255, 105/255, 184/255}), -- Hand of Purity
 		ClassBuff(20925, 'TOPLEFT', {0.93, 0.75, 0}), -- Sacred Shield
-		ClassBuff(114163, 'BOTTOMLEFT', {0.87, 0.7, 0.03}), -- Eternal Flame
+		ClassBuff(156322, 'BOTTOMLEFT', {0.87, 0.7, 0.03}), -- Eternal Flame
 	},
 	SHAMAN = {
 		ClassBuff(61295, "TOPRIGHT", {0.7, 0.3, 0.7}),	 -- Riptide
