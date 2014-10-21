f45ac95bb031f2c131ae346d1522511be7f9d57b
Blazeflack <blazeflack@hotmail.com>
2014-10-22 01:47:18 +0200

    This way is better for nil protection

--- ElvUI/modules/actionbars/stanceBar.lua
+++ ElvUI/modules/actionbars/stanceBar.lua
@@ -34,7 +34,11 @@ function AB:StyleShapeShift(event)
 		if i <= numForms then
 			texture, name, isActive, isCastable = GetShapeshiftFormInfo(i);
 
-			if (texture and type(texture) == "string" and (lower(texture) == "interface\\icons\\spell_nature_wispsplode" or lower(texture) == "interface\\icons\\ability_rogue_envelopingshadows")) and self.db.stanceBar.style == 'darkenInactive' then
+			if not texture then
+				texture = "Interface\\Icons\\Spell_Nature_WispSplode"
+			end
+
+			if (type(texture) == "string" and (lower(texture) == "interface\\icons\\spell_nature_wispsplode" or lower(texture) == "interface\\icons\\ability_rogue_envelopingshadows") and self.db.stanceBar.style == 'darkenInactive' then
 				_, _, texture = GetSpellInfo(name)
 			end
 			
