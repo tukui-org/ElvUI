7c8e8d12cf1177795465a906aceefa9f49b31a41
Elv <elv@tukui.org>
2014-10-22 15:09:02 -0400

    AFK mode tweaks.

--- ElvUI/modules/misc/afk.lua
+++ ElvUI/modules/misc/afk.lua
@@ -23,11 +23,13 @@ function AFK:SetAFK(status)
 			self.AFKMode.bottom.guild:SetText(L["No Guild"])
 		end
 
-		--self.AFKMode.bottom.model.curAnimation = "wave"
-		--self.AFKMode.bottom.model.startTime = GetTime()
-		--self.AFKMode.bottom.model.delay = 2.3
+		self.AFKMode.bottom.model.curAnimation = "wave"
+		self.AFKMode.bottom.model.startTime = GetTime()
+		self.AFKMode.bottom.model.duration = 2.3
 		self.AFKMode.bottom.model:SetUnit("player")
-		--self.AFKMode.bottom.model:SetAnimation(67)
+		self.AFKMode.bottom.model.isIdle = nil
+		self.AFKMode.bottom.model:SetAnimation(67)
+		self.AFKMode.bottom.model.idleDuration = 40
 		self.startTime = GetTime()
 		self.timer = self:ScheduleRepeatingTimer('UpdateTimer', 1)
 
@@ -71,29 +73,12 @@ end
 
 function AFK:LoopAnimations()
 	if(ElvUIAFKPlayerModel.curAnimation == "wave") then
-		ElvUIAFKPlayerModel:SetAnimation(69)
-		ElvUIAFKPlayerModel.curAnimation = "dance"
-		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 25
-		ElvUIAFKPlayerModel.isIdle = false
-	elseif(ElvUIAFKPlayerModel.curAnimation == "dance") then
-		ElvUIAFKPlayerModel:SetAnimation(74)
-		ElvUIAFKPlayerModel.curAnimation = "roar"
-		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 3
-		ElvUIAFKPlayerModel.isIdle = false		
-	elseif(ElvUIAFKPlayerModel.curAnimation == "roar") then
-		ElvUIAFKPlayerModel:SetAnimation(70)
-		ElvUIAFKPlayerModel.curAnimation = "laugh"
-		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 0 --3.2
-		ElvUIAFKPlayerModel.isIdle = true
-	--[[elseif(ElvUIAFKPlayerModel.curAnimation == "laugh") then
-		ElvUIAFKPlayerModel:SetAnimation(100)
-		ElvUIAFKPlayerModel.curAnimation = "sleep"
-		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 0
-		ElvUIAFKPlayerModel.isIdle = true]]			
+			ElvUIAFKPlayerModel:SetAnimation(69)
+			ElvUIAFKPlayerModel.curAnimation = "dance"
+			ElvUIAFKPlayerModel.startTime = GetTime()
+			ElvUIAFKPlayerModel.duration = 300
+			ElvUIAFKPlayerModel.isIdle = false
+			ElvUIAFKPlayerModel.idleDuration = 120
 	end
 end
 
@@ -107,7 +92,7 @@ function AFK:Initialize()
 	self.AFKMode:EnableKeyboard(true)
 	self.AFKMode:SetScript("OnKeyDown", OnKeyDown)
 
-	self.AFKMode.top = CreateFrame("Frame", nil, self.AFKMode)
+	--[[self.AFKMode.top = CreateFrame("Frame", nil, self.AFKMode)
 	self.AFKMode.top:SetTemplate("Transparent")
 	self.AFKMode.top:SetPoint("TOP", self.AFKMode, "TOP", 0, 2)
 	self.AFKMode.top:SetWidth(GetScreenWidth())
@@ -129,7 +114,7 @@ function AFK:Initialize()
 	self.AFKMode.top.factionLeft = self.AFKMode.top:CreateTexture(nil, 'OVERLAY')
 	self.AFKMode.top.factionLeft:SetPoint("BOTTOMLEFT", self.AFKMode.top, "BOTTOMLEFT", 10, -16)
 	self.AFKMode.top.factionLeft:SetTexture("Interface\\Timer\\"..factionGroup.."-Logo")
-	self.AFKMode.top.factionLeft:SetSize(140, 140)
+	self.AFKMode.top.factionLeft:SetSize(140, 140)]]
 
 	self.AFKMode.bottom = CreateFrame("Frame", nil, self.AFKMode)
 	self.AFKMode.bottom:SetTemplate("Transparent")
@@ -137,20 +122,25 @@ function AFK:Initialize()
 	self.AFKMode.bottom:SetWidth(GetScreenWidth())
 	self.AFKMode.bottom:SetHeight(GetScreenHeight() * (1 / 10))
 
+	self.AFKMode.bottom.logo = self.AFKMode.bottom:CreateTexture(nil, 'OVERLAY')
+	self.AFKMode.bottom.logo:SetSize(320, 150)
+	self.AFKMode.bottom.logo:SetPoint("CENTER", self.AFKMode.bottom, "CENTER", 0, 50)
+	self.AFKMode.bottom.logo:SetTexture("Interface\\AddOns\\ElvUI\\media\\textures\\logo.tga")
+
 	self.AFKMode.bottom.name = self.AFKMode.bottom:CreateFontString(nil, 'OVERLAY')
-	self.AFKMode.bottom.name:SetFont(E.LSM:Fetch("font", "ElvUI Alt-Combat"), 24, "OUTLINE")
+	self.AFKMode.bottom.name:SetFont(E.LSM:Fetch("font", "ElvUI Pixel"), 20, "MONOCHROMEOUTLINE")
 	self.AFKMode.bottom.name:SetText(E.myname.."-"..E.myrealm)
 	self.AFKMode.bottom.name:SetPoint("TOPLEFT", self.AFKMode.bottom, "TOPLEFT", 20, -12)
 	self.AFKMode.bottom.name:SetTextColor(classColor.r, classColor.g, classColor.b)
 
 	self.AFKMode.bottom.guild = self.AFKMode.bottom:CreateFontString(nil, 'OVERLAY')
-	self.AFKMode.bottom.guild:SetFont(E.LSM:Fetch("font", "ElvUI Alt-Combat"), 24, "OUTLINE")
+	self.AFKMode.bottom.guild:SetFont(E.LSM:Fetch("font", "ElvUI Pixel"), 20, "MONOCHROMEOUTLINE")
 	self.AFKMode.bottom.guild:SetText(L["No Guild"])
 	self.AFKMode.bottom.guild:SetPoint("TOPLEFT", self.AFKMode.bottom.name, "BOTTOMLEFT", 0, -6)
 	self.AFKMode.bottom.guild:SetTextColor(0.7, 0.7, 0.7)
 
 	self.AFKMode.bottom.time = self.AFKMode.bottom:CreateFontString(nil, 'OVERLAY')
-	self.AFKMode.bottom.time:SetFont(E.LSM:Fetch("font", "ElvUI Alt-Combat"), 24, "OUTLINE")
+	self.AFKMode.bottom.time:SetFont(E.LSM:Fetch("font", "ElvUI Pixel"), 20, "MONOCHROMEOUTLINE")
 	self.AFKMode.bottom.time:SetText("00:00")
 	self.AFKMode.bottom.time:SetPoint("TOPLEFT", self.AFKMode.bottom.guild, "BOTTOMLEFT", 0, -6)
 	self.AFKMode.bottom.time:SetTextColor(0.7, 0.7, 0.7)
@@ -159,14 +149,14 @@ function AFK:Initialize()
 	self.AFKMode.bottom.model:SetPoint("BOTTOMRIGHT", self.AFKMode.bottom, "BOTTOMRIGHT", 90, -80)
 	self.AFKMode.bottom.model:SetSize(700, 700)
 	self.AFKMode.bottom.model:SetFacing(6)
-	--[[self.AFKMode.bottom.model:SetScript("OnUpdateModel", function(self) 
+	self.AFKMode.bottom.model:SetScript("OnUpdateModel", function(self) 
 		local timePassed = GetTime() - self.startTime
-		if(timePassed > self.delay) and self.isIdle ~= true then
+		if(timePassed > self.duration) and self.isIdle ~= true then
 			self:SetAnimation(0)
 			self.isIdle = true
-			AFK.animTimer = AFK:ScheduleTimer("LoopAnimations", 5)
+			AFK.animTimer = AFK:ScheduleTimer("LoopAnimations", self.idleDuration)
 		end
-	end)]]
+	end)
 
 	self:Toggle()
 	self.isActive = false	
