679715bb1b195a9cff39c1629433cb5e05aad201
Elv <elv@tukui.org>
2014-10-26 00:41:36 -0400

    Only show the out of date popup when you are more than 10 revisions out of date. Fix the popup becoming auto focus'd when it is shown.

--- ElvUI/core/core.lua
+++ ElvUI/core/core.lua
@@ -466,10 +466,14 @@ local function SendRecieve(self, event, prefix, message, channel, sender)
 	if event == "CHAT_MSG_ADDON" then
 		if(sender == myName) then return end
 
-		if prefix == "ELVUI_VERSIONCHK" and myName ~= "Elv-ShatteredHand" and not E.recievedOutOfDateMessage then
+		if prefix == "ELVUI_VERSIONCHK" and not E.recievedOutOfDateMessage then
 			if(tonumber(message) ~= nil and tonumber(message) > tonumber(E.version)) then
 				E:Print(L["ElvUI is out of date. You can download the newest version from www.tukui.org. Get premium membership and have ElvUI automatically updated with the Tukui Client!"])
-				E:StaticPopup_Show("ELVUI_UPDATE_AVAILABLE")
+				
+				if((tonumber(message) - tonumber(E.version)) > 0.1) then
+					E:StaticPopup_Show("ELVUI_UPDATE_AVAILABLE")
+				end
+
 				E.recievedOutOfDateMessage = true
 			end
 		end
