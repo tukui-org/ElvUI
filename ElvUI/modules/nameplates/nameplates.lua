3d420b70e834d67f6e26a8e0644e677c7d10e33b
Blazeflack <blazeflack@hotmail.com>
2014-11-01 13:56:47 +0100

    Fix syntax errors, take 2.

--- ElvUI/modules/nameplates/nameplates.lua
+++ ElvUI/modules/nameplates/nameplates.lua
@@ -1431,7 +1431,7 @@ function NP:SetAuraInstance(guid, spellID, expiration, stacks, caster, duration,
 				filter = true;
 				if debugAuras[name] and debugAuras[name] == true then
 					print("The aura: ", name, " was previously allowed and is now allowed because of a whitelist filter")
-				elseif debugAuras[name] and debugAuras[name] == false
+				elseif debugAuras[name] and debugAuras[name] == false then
 					print("The aura: ", name, " was previously disallowed and is now allowed because of a whitelist filter")
 				else
 					print("The aura: ", name, " was allowed because of a whitelist filter")
@@ -1442,7 +1442,7 @@ function NP:SetAuraInstance(guid, spellID, expiration, stacks, caster, duration,
 				filter = false;
 				if debugAuras[name] and debugAuras[name] == true then
 					print("The aura: ", name, " was previously allowed but now filtered out because of a strict whitelist filter")
-				elseif debugAuras[name] and debugAuras[name] == false
+				elseif debugAuras[name] and debugAuras[name] == false then
 					print("The aura: ", name, " was previously disallowed and still filtered out because of a strict whitelist filter")
 				else
 					print("The aura: ", name, " was filtered out because of a strict whitelist filter")
@@ -1456,7 +1456,7 @@ function NP:SetAuraInstance(guid, spellID, expiration, stacks, caster, duration,
 		filter = false;
 		if debugAuras[name] and debugAuras[name] == true then
 			print("The aura: ", name, " was previously allowed but now filtered out because of a global blacklist filter")
-		elseif debugAuras[name] and debugAuras[name] == false
+		elseif debugAuras[name] and debugAuras[name] == false then
 			print("The aura: ", name, " was previously disallowed and still filtered out because of a global blacklist filter")
 		else
 			print("The aura: ", name, " was filtered out because of a global blacklist filter")
