18946cfa3a9968732e32e64e0cc2c075f4b3404b
Elv <elv@tukui.org>
2014-11-03 13:36:51 -0500

    These aren't all needed

--- ElvUI/modules/misc/afk.lua
+++ ElvUI/modules/misc/afk.lua
@@ -168,15 +168,10 @@ local function Chat_OnEvent(self, event, arg1, arg2, arg3, arg4, arg5, arg6, arg
 	local accessID = ChatHistory_GetAccessID(chatGroup, chatTarget);
 	local typeID = ChatHistory_GetAccessID(type, chatTarget, arg12 == "" and arg13 or arg12);	
 	if CH.db.shortChannels then
-		body = body:gsub("|Hchannel:(.-)|h%[(.-)%]|h", CH.ShortChannel)
-		body = body:gsub('CHANNEL:', '')
 		body = body:gsub("^(.-|h) "..L['whispers'], "%1")
-		body = body:gsub("^(.-|h) "..L['says'], "%1")
-		body = body:gsub("^(.-|h) "..L['yells'], "%1")
 		body = body:gsub("<"..AFKString..">", "[|cffFF0000"..L['AFK'].."|r] ")
 		body = body:gsub("<"..DND..">", "[|cffE7E716"..L['DND'].."|r] ")
 		body = body:gsub("%[BN_CONVERSATION:", '%['.."")			
-		body = body:gsub("^%["..RAID_WARNING.."%]", '['..L['RW']..']')	
 	end
 
 	self:AddMessage(CH:ConcatenateTimeStamp(body), info.r, info.g, info.b, info.id, false, accessID, typeID);
