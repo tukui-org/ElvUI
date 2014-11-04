35e9fa7b152b1395027a048cbb38bad8420221eb
Elv <elv@tukui.org>
2014-11-04 17:34:31 -0500

    Add short channel back to AFKMode

--- ElvUI/modules/misc/afk.lua
+++ ElvUI/modules/misc/afk.lua
@@ -168,6 +168,7 @@ local function Chat_OnEvent(self, event, arg1, arg2, arg3, arg4, arg5, arg6, arg
 	local accessID = ChatHistory_GetAccessID(chatGroup, chatTarget);
 	local typeID = ChatHistory_GetAccessID(type, chatTarget, arg12 == "" and arg13 or arg12);	
 	if CH.db.shortChannels then
+		body = body:gsub("|Hchannel:(.-)|h%[(.-)%]|h", CH.ShortChannel)
 		body = body:gsub("^(.-|h) "..L['whispers'], "%1")
 		body = body:gsub("<"..AFKString..">", "[|cffFF0000"..L['AFK'].."|r] ")
 		body = body:gsub("<"..DND..">", "[|cffE7E716"..L['DND'].."|r] ")
