2f95e7242180bcc2434ff72ee53f26a58c997af3
Blazeflack <blazeflack@hotmail.com>
2014-11-24 13:04:12 +0100

    Add datatexts for the following new stats: Multistrike, Versatility, Bonus Armor, Speed, Leech

---
+++ ElvUI/modules/datatexts/multistrike.lua
@@ -0,0 +1,50 @@
+local E, L, V, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local DT = E:GetModule('DataTexts')
+
+local multistrike
+local displayModifierString = ''
+local lastPanel;
+local join = string.join
+local format = string.format
+
+local function OnEnter(self)
+	DT:SetupTooltip(self)
+	
+	local text, tooltip;
+	text = HIGHLIGHT_FONT_COLOR_CODE..format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_MULTISTRIKE).." "..format("%.2F%%", GetMultistrike())..FONT_COLOR_CODE_CLOSE
+	tooltip = format(CR_MULTISTRIKE_TOOLTIP, GetMultistrike(), GetMultistrikeEffect(), BreakUpLargeNumbers(GetCombatRating(CR_MULTISTRIKE)), GetCombatRatingBonus(CR_MULTISTRIKE))
+	
+	DT.tooltip:AddDoubleLine(text, nil, 1, 1, 1);
+	DT.tooltip:AddLine(tooltip, nil, nil, nil, true);
+	DT.tooltip:Show()
+end
+
+local function OnEvent(self, event, unit)
+	multistrike = GetMultistrike()
+	self.text:SetFormattedText(displayModifierString, STAT_MULTISTRIKE, multistrike)
+	lastPanel = self
+end
+
+local function ValueColorUpdate(hex, r, g, b)
+	displayModifierString = join("", "%s: ", hex, "%.2f%%|r")
+	
+	if lastPanel ~= nil then
+		OnEvent(lastPanel)
+	end
+end
+E['valueColorUpdateFuncs'][ValueColorUpdate] = true
+
+--[[
+	DT:RegisterDatatext(name, events, eventFunc, updateFunc, clickFunc, onEnterFunc, onLeaveFunc)
+	
+	name - name of the datatext (required)
+	events - must be a table with string values of event names to register 
+	eventFunc - function that gets fired when an event gets triggered
+	updateFunc - onUpdate script target function
+	click - function to fire when clicking the datatext
+	onEnterFunc - function to fire OnEnter
+	onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip.
+]]
+
+
+DT:RegisterDatatext('Multistrike', {"UNIT_STATS", "UNIT_AURA", "FORGE_MASTER_ITEM_CHANGED", "ACTIVE_TALENT_GROUP_CHANGED", "PLAYER_TALENT_UPDATE", "PLAYER_DAMAGE_DONE_MODS"}, OnEvent, nil, nil, OnEnter)
