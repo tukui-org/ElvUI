ee1f99b0d58d581cef976180486ba4a4be7c2069
Elv <elv@tukui.org>
2015-01-03 00:05:38 -0500

    Fix bug with garrison datatext tooltip not updating correctly.

--- ElvUI/modules/datatexts/garrison.lua
+++ ElvUI/modules/datatexts/garrison.lua
@@ -5,25 +5,28 @@ local GARRISON_CURRENCY = 824
 local format = string.format
 local GARRISON_ICON = format("\124T%s:%d:%d:0:0:64:64:4:60:4:60\124t", select(3, GetCurrencyInfo(GARRISON_CURRENCY)), 16, 16)
 
-local function OnEvent(self, event, ...)
-	local _, numResources = GetCurrencyInfo(GARRISON_CURRENCY)
-	self.text:SetFormattedText("%s %s", GARRISON_ICON, numResources)
-end
-
-
-local function OnEnter(self)
+local function OnEnter(self, _, noUpdate)
 	DT:SetupTooltip(self)
 
+	if(not noUpdate) then
+		DT.tooltip:Hide()
+		C_Garrison.RequestLandingPageShipmentInfo();
+		return
+	end
 	local buildings = C_Garrison.GetBuildings();
 	local numBuildings = #buildings
+	local hasBuilding = false
 	if(numBuildings > 0) then
-		DT.tooltip:AddLine(L["Building(s) Report:"])
-		
 		for i = 1, #buildings do
 			local buildingID = buildings[i].buildingID;
 			if ( buildingID ) then
 				local name, _, _, shipmentsReady, shipmentsTotal = C_Garrison.GetLandingPageShipmentInfo(buildingID);
 				if ( name and shipmentsReady and shipmentsTotal ) then
+					if(hasBuilding == false) then
+						DT.tooltip:AddLine(L["Building(s) Report:"])
+						hasBuilding = true
+					end
+
 					DT.tooltip:AddDoubleLine(name, format(GARRISON_LANDING_SHIPMENT_COUNT, shipmentsReady, shipmentsTotal), 1, 1, 1)
 				end
 			end
@@ -52,9 +55,25 @@ local function OnEnter(self)
 		end
 	end
 
+	if(hasBuilding == true or numMissions > 0) then
+		DT.tooltip:Show()
+	else
+		DT.tooltip:Hide()
+	end
+end
 
-	DT.tooltip:Show()
+local function OnEvent(self, event, ...)
+	if(event == "GARRISON_LANDINGPAGE_SHIPMENTS") then
+		if(GetMouseFocus() == self) then
+			OnEnter(self, nil, true)
+		end
+
+		return
+	end
+
+	local _, numResources = GetCurrencyInfo(GARRISON_CURRENCY)
+	self.text:SetFormattedText("%s %s", GARRISON_ICON, numResources)
 end
 
 
-DT:RegisterDatatext('Garrison', {"PLAYER_ENTERING_WORLD", "CURRENCY_DISPLAY_UPDATE"}, OnEvent, nil, GarrisonLandingPage_Toggle, OnEnter)
+DT:RegisterDatatext('Garrison', {"PLAYER_ENTERING_WORLD", "CURRENCY_DISPLAY_UPDATE", "GARRISON_LANDINGPAGE_SHIPMENTS"}, OnEvent, nil, GarrisonLandingPage_Toggle, OnEnter)
