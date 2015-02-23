ffc910fb71546c4d941873b7df772a0e9d50d778
Blazeflack <blazeflack@hotmail.com>
2015-02-23 23:37:44 +0100

    Attempt to fix issue with profiles getting messed up when you have dualspec enabled and change spec with the profiles tab open.

--- ElvUI/libs/LibDualSpec-1.0/LibDualSpec-1.0.lua
+++ ElvUI/libs/LibDualSpec-1.0/LibDualSpec-1.0.lua
@@ -64,6 +64,7 @@ local mixin = lib.mixin
 -- "Externals"
 local AceDB3 = LibStub('AceDB-3.0', true)
 local AceDBOptions3 = LibStub('AceDBOptions-3.0', true)
+local AceConfigRegistry3 = LibStub('AceConfigRegistry-3.0', true)
 
 -- ----------------------------------------------------------------------------
 -- MoP compatibility
@@ -191,6 +192,10 @@ function mixin:CheckDualSpecState()
 		if newProfile ~= currentProfile then
 			db.char.profile = currentProfile
 			self:SetProfile(newProfile)
+			
+			--Bugfix: 	Changing spec while having the profiles tab open causes dualspec to mess up the profile selections.
+			--			This will force the selection to update properly, so GetCurrentProfile doesn't return incorrect values
+			AceConfigRegistry3:NotifyChange(registry[self].name)
 		end
 	end
 end
