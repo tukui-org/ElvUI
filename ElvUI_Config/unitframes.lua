84fdcb19c890a5779e2ed26bb06d558b6fb47f75
Blazeflack <blazeflack@hotmail.com>
2015-03-21 13:06:40 +0100

    Fix a bug that allowed you to create customtexts with names that match existing elements for that frame.
    This fixes #619

--- ElvUI_Config/unitframes.lua
+++ ElvUI_Config/unitframes.lua
@@ -945,8 +945,9 @@ local function GetOptionsTable_CustomText(updateFunc, groupName, numUnits, order
 			if not E.db.unitframe.units[groupName].customTexts then
 				E.db.unitframe.units[groupName].customTexts = {};
 			end
-
-			if E.db.unitframe.units[groupName].customTexts[textName] or (_G[groupName] and _G[groupName][textName] or _G[groupName.."Group1UnitButton1"] and _G[groupName.."Group1UnitButton1"][textName]) then
+			
+			local frameName = "ElvUF_"..E:StringTitle(groupName)
+			if E.db.unitframe.units[groupName].customTexts[textName] or (_G[frameName] and _G[frameName][textName] or _G[frameName.."Group1UnitButton1"] and _G[frameName.."Group1UnitButton1"][textName]) then
 				E:Print(L["The name you have selected is already in use by another element."])
 				return;
 			end
