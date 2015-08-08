3dec47e60a5d2142d389ec323159763abffcdf7b
Blazeflack <blazeflack@hotmail.com>
2015-08-08 14:11:07 +0200

    Hide Aura Bar spark if "Uniform Threshold" is more than 0 and timeleft is more than "Uniform Threshold".
    This closes #135

--- ElvUI/libs/oUF_Plugins/oUF_AuraBars/oUF_AuraBars.lua
+++ ElvUI/libs/oUF_Plugins/oUF_AuraBars/oUF_AuraBars.lua
@@ -167,7 +167,11 @@ local function UpdateBars(auraBars)
 			bar:SetValue(timeleft)
 			bar.spelltime:SetText(FormatTime(timeleft))
 			if auraBars.spark == true then
-				bar.spark:Show()
+				if auraBars.scaleTime and auraBars.scaleTime > 0 and timeleft < auraBars.scaleTime then
+					bar.spark:Show()
+				else
+					bar.spark:Hide()
+				end
 			end
 		end
 	end
