0e0907e44c2ff2d9a867cc626c3a7148b04f48ce
Blazeflack <blazeflack@hotmail.com>
2015-10-16 21:33:52 +0200

    Added portraits to party frames

--- ElvUI/modules/unitframes/groups/party.lua
+++ ElvUI/modules/unitframes/groups/party.lua
@@ -24,6 +24,9 @@ function UF:Construct_PartyFrames(unitGroup)
 
 		self.Power = UF:Construct_PowerBar(self, true, true, 'LEFT')
 		self.Power.frequentUpdates = false;
+		
+		self.Portrait3D = UF:Construct_Portrait(self, 'model')
+		self.Portrait2D = UF:Construct_Portrait(self, 'texture')
 
 		self.Name = UF:Construct_NameText(self)
 		self.Buffs = UF:Construct_Buffs(self)
@@ -97,6 +100,12 @@ end
 
 function UF:Update_PartyFrames(frame, db)
 	frame.db = db
+	if frame.Portrait then
+		frame.Portrait:Hide()
+		frame.Portrait:ClearAllPoints()
+		frame.Portrait.backdrop:Hide()
+	end
+	frame.Portrait = db.portrait.style == '2D' and frame.Portrait2D or frame.Portrait3D
 	local SPACING = E.Spacing;
 	local BORDER = E.Border;
 	local UNIT_WIDTH = db.width
@@ -109,6 +118,9 @@ function UF:Update_PartyFrames(frame, db)
 	local POWERBAR_OFFSET = db.power.offset
 	local POWERBAR_HEIGHT = db.power.height
 	local POWERBAR_WIDTH = db.width - (BORDER*2)
+	local USE_PORTRAIT = db.portrait.enable
+	local USE_PORTRAIT_OVERLAY = db.portrait.overlay and USE_PORTRAIT
+	local PORTRAIT_WIDTH = db.portrait.width
 
 	frame.db = db
 	frame.colors = ElvUF.colors
@@ -121,6 +133,10 @@ function UF:Update_PartyFrames(frame, db)
 
 		if USE_MINI_POWERBAR then
 			POWERBAR_WIDTH = POWERBAR_WIDTH / 2
+		end
+		
+		if USE_PORTRAIT_OVERLAY or not USE_PORTRAIT then
+			PORTRAIT_WIDTH = 0
 		end
 	end
 
@@ -253,6 +269,17 @@ function UF:Update_PartyFrames(frame, db)
 			else
 				health:Point("BOTTOMLEFT", frame, "BOTTOMLEFT", BORDER, (USE_POWERBAR and ((BORDER + SPACING)*2) or BORDER) + POWERBAR_HEIGHT)
 			end
+			
+			health.bg:ClearAllPoints()
+			if not USE_PORTRAIT_OVERLAY then
+				health:Point("TOPLEFT", PORTRAIT_WIDTH+BORDER+POWERBAR_OFFSET, -BORDER)
+				health.bg:SetParent(health)
+				health.bg:SetAllPoints()
+			else
+				health.bg:Point('BOTTOMLEFT', health:GetStatusBarTexture(), 'BOTTOMRIGHT')
+				health.bg:Point('TOPRIGHT', health)
+				health.bg:SetParent(frame.Portrait.overlay)
+			end
 
 			health:SetOrientation(db.health.orientation)
 		end
@@ -289,14 +316,14 @@ function UF:Update_PartyFrames(frame, db)
 				--Position
 				power:ClearAllPoints()
 				if USE_POWERBAR_OFFSET then
-					power:Point("TOPLEFT", frame.Health, "TOPLEFT", -POWERBAR_OFFSET, -POWERBAR_OFFSET)
+					power:Point("TOPLEFT", frame.Health, "TOPLEFT", -(POWERBAR_OFFSET + PORTRAIT_WIDTH), -POWERBAR_OFFSET)
 					power:Point("BOTTOMRIGHT", frame.Health, "BOTTOMRIGHT", -POWERBAR_OFFSET, -POWERBAR_OFFSET)
 					power:SetFrameStrata("LOW")
 					power:SetFrameLevel(2)
 				elseif USE_MINI_POWERBAR then
 					power:Width(POWERBAR_WIDTH - BORDER*2)
 					power:Height(POWERBAR_HEIGHT)
-					power:Point("LEFT", frame, "BOTTOMLEFT", (BORDER*2 + 4), BORDER + (POWERBAR_HEIGHT/2))
+					power:Point("LEFT", frame, "BOTTOMLEFT", (BORDER*2 + 4) + PORTRAIT_WIDTH, BORDER + (POWERBAR_HEIGHT/2))
 					power:SetFrameStrata("MEDIUM")
 					power:SetFrameLevel(frame:GetFrameLevel() + 3)
 				elseif USE_INSET_POWERBAR then
@@ -312,6 +339,55 @@ function UF:Update_PartyFrames(frame, db)
 			else
 				frame:DisableElement('Power')
 				power:Hide()
+			end
+		end
+		
+		--Portrait
+		do
+			local portrait = frame.Portrait
+
+			--Set Points
+			if USE_PORTRAIT then
+				if not frame:IsElementEnabled('Portrait') then
+					frame:EnableElement('Portrait')
+				end
+
+				portrait:ClearAllPoints()
+
+				if USE_PORTRAIT_OVERLAY then
+					if db.portrait.style == '3D' then
+						portrait:SetFrameLevel(frame.Health:GetFrameLevel() + 1)
+					end
+					portrait:SetAllPoints(frame.Health)
+					portrait:SetAlpha(0.3)
+					portrait:Show()
+					portrait.backdrop:Hide()
+				else
+					portrait:SetAlpha(1)
+					portrait:Show()
+					portrait.backdrop:Show()
+					if db.portrait.style == '3D' then
+						portrait:SetFrameLevel(frame:GetFrameLevel() + 5)
+					end
+					
+					portrait.backdrop:ClearAllPoints()
+					portrait.backdrop:SetPoint("TOPLEFT", frame, "TOPLEFT", POWERBAR_OFFSET, 0)
+
+					if USE_MINI_POWERBAR or USE_POWERBAR_OFFSET or not USE_POWERBAR or USE_INSET_POWERBAR then
+						portrait.backdrop:Point("BOTTOMRIGHT", frame.Health.backdrop, "BOTTOMLEFT", E.PixelMode and 1 or -SPACING, 0)
+					else
+						portrait.backdrop:Point("BOTTOMRIGHT", frame.Power.backdrop, "BOTTOMLEFT", E.PixelMode and 1 or -SPACING, 0)
+					end
+
+					portrait:Point('BOTTOMLEFT', portrait.backdrop, 'BOTTOMLEFT', BORDER, BORDER)
+					portrait:Point('TOPRIGHT', portrait.backdrop, 'TOPRIGHT', -BORDER, -BORDER)
+				end
+			else
+				if frame:IsElementEnabled('Portrait') then
+					frame:DisableElement('Portrait')
+					portrait:Hide()
+					portrait.backdrop:Hide()
+				end
 			end
 		end
 
@@ -339,8 +415,8 @@ function UF:Update_PartyFrames(frame, db)
 					end
 
 					if USE_PORTRAIT and not USE_PORTRAIT_OVERLAY then
-						threat.glow:Point("TOPRIGHT", frame.Portrait.backdrop, "TOPRIGHT", SHADOW_SPACING, -SHADOW_SPACING)
-						threat.glow:Point("BOTTOMRIGHT", frame.Portrait.backdrop, "BOTTOMRIGHT", SHADOW_SPACING, -SHADOW_SPACING)
+						threat.glow:Point("TOPLEFT", frame.Portrait.backdrop, "TOPLEFT", -SHADOW_SPACING, SHADOW_SPACING)
+						threat.glow:Point("BOTTOMLEFT", frame.Portrait.backdrop, "BOTTOMLEFT", -SHADOW_SPACING, SHADOW_SPACING)
 					end
 				elseif db.threatStyle == "ICONTOPLEFT" or db.threatStyle == "ICONTOPRIGHT" or db.threatStyle == "ICONBOTTOMLEFT" or db.threatStyle == "ICONBOTTOMRIGHT" or db.threatStyle == "ICONTOP" or db.threatStyle == "ICONBOTTOM" or db.threatStyle == "ICONLEFT" or db.threatStyle == "ICONRIGHT" then
 					threat:SetFrameStrata('HIGH')
@@ -549,6 +625,16 @@ function UF:Update_PartyFrames(frame, db)
 				if not frame:IsElementEnabled('HealPrediction') then
 					frame:EnableElement('HealPrediction')
 				end
+				
+				if not USE_PORTRAIT_OVERLAY then
+					healPrediction.myBar:SetParent(frame)
+					healPrediction.otherBar:SetParent(frame)
+					healPrediction.absorbBar:SetParent(frame)
+				else
+					healPrediction.myBar:SetParent(frame.Portrait.overlay)
+					healPrediction.otherBar:SetParent(frame.Portrait.overlay)
+					healPrediction.absorbBar:SetParent(frame.Portrait.overlay)
+				end
 
 				healPrediction.myBar:SetOrientation(db.health.orientation)
 				healPrediction.otherBar:SetOrientation(db.health.orientation)
@@ -654,7 +740,7 @@ function UF:Update_PartyFrames(frame, db)
 		end
 	end
 
-	UF:ToggleTransparentStatusBar(UF.db.colors.transparentHealth, frame.Health, frame.Health.bg, true)
+	UF:ToggleTransparentStatusBar(UF.db.colors.transparentHealth, frame.Health, frame.Health.bg, (USE_PORTRAIT and USE_PORTRAIT_OVERLAY) ~= true)
 	if frame.Power then
 		UF:ToggleTransparentStatusBar(UF.db.colors.transparentPower, frame.Power, frame.Power.bg)
 	end
