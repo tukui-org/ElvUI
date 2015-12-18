16a52e07d19bbc31db4cc705794518edf4ebbd9e
Blazeflack <blazeflack@hotmail.com>
2015-12-18 19:13:44 +0100

    Moved Encode/Decode functions from WeakAuras to math.lua

--- ElvUI/core/math.lua
+++ ElvUI/core/math.lua
@@ -2,9 +2,10 @@ local E, L, V, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, Private
 
 --Cache global variables
 local select, tonumber, assert, type, unpack = select, tonumber, assert, type, unpack
-local tinsert, tremove = tinsert, tremove
+local tinsert, tremove, tconcat = tinsert, tremove, table.concat
 local atan2, modf, ceil, floor, abs, sqrt, pi, mod = math.atan2, math.modf, math.ceil, math.floor, math.abs, math.sqrt, math.pi, mod
-local format, sub, upper = string.format, string.sub, string.upper
+local bit_band, bit_lshift, bit_rshift = bit.band, bit.lshift, bit.rshift
+local format, sub, upper, string_char, string_byte = string.format, string.sub, string.upper, string.char, string.byte
 --WoW API / Variables
 local CreateFrame = CreateFrame
 local UnitPosition = UnitPosition
@@ -387,4 +388,82 @@ function E:FormatMoney(amount, style, textonly)
 
 	-- Shouldn't be here; punt
 	return self:FormatMoney(amount, "SMART")
+end
+
+--This code is taken from WeakAuras2, credit goes to Mirrored and the WeakAuras Team
+--This code is based on the Encode7Bit algorithm from LibCompress
+--Credit goes to Galmok of European Stormrage (Horde), galmok@gmail.com
+local bytetoB64 = {
+    [0]="a","b","c","d","e","f","g","h",
+    "i","j","k","l","m","n","o","p",
+    "q","r","s","t","u","v","w","x",
+    "y","z","A","B","C","D","E","F",
+    "G","H","I","J","K","L","M","N",
+    "O","P","Q","R","S","T","U","V",
+    "W","X","Y","Z","0","1","2","3",
+    "4","5","6","7","8","9","(",")"
+}
+
+local B64tobyte = {
+      a =  0,  b =  1,  c =  2,  d =  3,  e =  4,  f =  5,  g =  6,  h =  7,
+      i =  8,  j =  9,  k = 10,  l = 11,  m = 12,  n = 13,  o = 14,  p = 15,
+      q = 16,  r = 17,  s = 18,  t = 19,  u = 20,  v = 21,  w = 22,  x = 23,
+      y = 24,  z = 25,  A = 26,  B = 27,  C = 28,  D = 29,  E = 30,  F = 31,
+      G = 32,  H = 33,  I = 34,  J = 35,  K = 36,  L = 37,  M = 38,  N = 39,
+      O = 40,  P = 41,  Q = 42,  R = 43,  S = 44,  T = 45,  U = 46,  V = 47,
+      W = 48,  X = 49,  Y = 50,  Z = 51,["0"]=52,["1"]=53,["2"]=54,["3"]=55,
+    ["4"]=56,["5"]=57,["6"]=58,["7"]=59,["8"]=60,["9"]=61,["("]=62,[")"]=63
+}
+
+local encodeB64Table = {};
+function E:EncodeB64(str)
+    local B64 = encodeB64Table;
+    local remainder = 0;
+    local remainder_length = 0;
+    local encoded_size = 0;
+    local l=#str
+    local code
+    for i=1,l do
+        code = string_byte(str, i);
+        remainder = remainder + bit_lshift(code, remainder_length);
+        remainder_length = remainder_length + 8;
+        while(remainder_length) >= 6 do
+            encoded_size = encoded_size + 1;
+            B64[encoded_size] = bytetoB64[bit_band(remainder, 63)];
+            remainder = bit_rshift(remainder, 6);
+            remainder_length = remainder_length - 6;
+        end
+    end
+    if remainder_length > 0 then
+        encoded_size = encoded_size + 1;
+        B64[encoded_size] = bytetoB64[remainder];
+    end
+    return tconcat(B64, "", 1, encoded_size)
+end
+
+local decodeB64Table = {}
+function E:DecodeB64(str)
+    local bit8 = decodeB64Table;
+    local decoded_size = 0;
+    local ch;
+    local i = 1;
+    local bitfield_len = 0;
+    local bitfield = 0;
+    local l = #str;
+    while true do
+        if bitfield_len >= 8 then
+            decoded_size = decoded_size + 1;
+            bit8[decoded_size] = string_char(bit_band(bitfield, 255));
+            bitfield = bit_rshift(bitfield, 8);
+            bitfield_len = bitfield_len - 8;
+        end
+        ch = B64tobyte[str:sub(i, i)];
+        bitfield = bitfield + bit_lshift(ch or 0, bitfield_len);
+        bitfield_len = bitfield_len + 6;
+        if i > l then
+            break;
+        end
+        i = i + 1;
+    end
+    return tconcat(bit8, "", 1, decoded_size)
 end
