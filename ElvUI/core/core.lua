434bc90c00006ffac04a6ee2ad3cfe4a4aabe04e
Blazeflack <blazeflack@hotmail.com>
2016-01-20 20:44:19 +0100

    Fixed previous commit to check if .a exists before comparing to it

--- ElvUI/core/core.lua
+++ ElvUI/core/core.lua
@@ -200,7 +200,7 @@ function E:GetColorTable(data)
 	if data.r > 1 or data.r < 0 then data.r = 1 end
 	if data.g > 1 or data.g < 0 then data.g = 1 end
 	if data.b > 1 or data.b < 0 then data.b = 1 end
-	if data.a > 1 or data.a < 0 then data.a = 1 end
+	if data.a and (data.a > 1 or data.a < 0) then data.a = 1 end
 
 	if data.a then
 		return {data.r, data.g, data.b, data.a}
