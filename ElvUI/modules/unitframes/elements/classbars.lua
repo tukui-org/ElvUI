0760694a1064f24bbf0a870fd84779718d41457a
Blazeflack <blazeflack@hotmail.com>
2016-01-30 22:38:25 +0100

    Last desparate attempt to fix alignment issue for higher resolutions

--- ElvUI/modules/unitframes/elements/classbars.lua
+++ ElvUI/modules/unitframes/elements/classbars.lua
@@ -122,7 +122,7 @@ function UF:UpdateHoly(event, unit, powerType)
 				if USE_MINI_CLASSBAR then
 					self.HolyPower[i]:Point("LEFT", self.HolyPower[i-1], "RIGHT", maxHolyPower == 5 and 7 or 13, 0)
 				else
-					self.HolyPower[i]:Point("LEFT", self.HolyPower[i-1], "RIGHT", E.Border-E.Spacing, 0)
+					self.HolyPower[i]:SetPoint("LEFT", self.HolyPower[i-1], "RIGHT", E.Border-E.Spacing, 0)
 				end
 			end
 
