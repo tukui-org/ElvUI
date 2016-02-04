73d9fa9d14900391d300e61c496d47744413943d
Blazeflack <blazeflack@hotmail.com>
2016-02-04 21:45:33 +0100

    Fixed a copy/paste error which caused non-PP health to grow slightly when changing to "RIGHT" orientation

--- ElvUI/modules/unitframes/elements/health.lua
+++ ElvUI/modules/unitframes/elements/health.lua
@@ -97,7 +97,7 @@ function UF:Configure_HealthBar(frame)
 			health:Point("BOTTOMLEFT", frame, "BOTTOMLEFT", frame.PORTRAIT_WIDTH + frame.BORDER +frame.SPACING, (frame.USE_POWERBAR and ((frame.BORDER + frame.SPACING)*2) or (frame.BORDER +frame.SPACING)) + frame.POWERBAR_HEIGHT)
 		end
 	elseif frame.ORIENTATION == "RIGHT" then
-		health:Point("TOPLEFT", frame, "TOPLEFT", frame.BORDER +frame.SPACING + frame.STAGGER_WIDTH + frame.POWERBAR_OFFSET, -frame.BORDER +frame.SPACING - frame.CLASSBAR_YOFFSET)
+		health:Point("TOPLEFT", frame, "TOPLEFT", frame.BORDER +frame.SPACING + frame.STAGGER_WIDTH + frame.POWERBAR_OFFSET, -frame.BORDER -frame.SPACING - frame.CLASSBAR_YOFFSET)
 
 		if frame.POWERBAR_DETACHED or frame.USE_INSET_POWERBAR then
 			health:Point("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -frame.PORTRAIT_WIDTH - frame.BORDER -frame.SPACING, frame.BORDER +frame.SPACING)
