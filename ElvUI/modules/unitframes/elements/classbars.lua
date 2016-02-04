4844e926509e72cf9b6ee56fde75f0f2d04d9318
Blazeflack <blazeflack@hotmail.com>
2016-02-04 20:34:29 +0100

    Remove a few prints I left behind

--- ElvUI/modules/unitframes/elements/classbars.lua
+++ ElvUI/modules/unitframes/elements/classbars.lua
@@ -198,7 +198,6 @@ function UF:Configure_ClassBar(frame)
 end
 
 local function ToggleResourceBar(bars)
-	print("ToggleResourceBar")
 	local frame = bars.origParent or bars:GetParent()
 	local db = frame.db
 	if not db then return end
@@ -208,7 +207,6 @@ local function ToggleResourceBar(bars)
 	frame.CLASSBAR_YOFFSET = (not frame.USE_CLASSBAR or not frame.CLASSBAR_SHOWN or frame.CLASSBAR_DETACHED) and 0 or (frame.USE_MINI_CLASSBAR and ((frame.SPACING+(frame.CLASSBAR_HEIGHT/2))) or (frame.CLASSBAR_HEIGHT + frame.SPACING))
 
 	if not frame.CLASSBAR_DETACHED then --Only update when necessary
-		print("ToggleResourceBar not detached")
 		UF:Configure_HealthBar(frame)
 		UF:Configure_Portrait(frame, true) --running :Hide on portrait makes the frame all funky
 		UF:Configure_Threat(frame)
