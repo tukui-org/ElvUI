2e7afd1c5584769a8844c5143b17a9021bce7fee
Blazeflack <blazeflack@hotmail.com>
2016-02-14 18:46:37 +0100

    Increased x and y offset limits on unitframe auras per user request

--- ElvUI_Config/unitframes.lua
+++ ElvUI_Config/unitframes.lua
@@ -446,13 +446,13 @@ local function GetOptionsTable_Auras(friendlyUnitOnly, auraType, isGroupFrame, u
 				order = 4,
 				type = 'range',
 				name = L["xOffset"],
-				min = -300, max = 300, step = 1,
+				min = -1000, max = 1000, step = 1,
 			},
 			yOffset = {
 				order = 5,
 				type = 'range',
 				name = L["yOffset"],
-				min = -300, max = 300, step = 1,
+				min = -1000, max = 1000, step = 1,
 			},
 			anchorPoint = {
 				type = 'select',
