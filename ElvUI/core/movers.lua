a5bfa547ebbe9bb726e45041d1db616d86624057
Elv <elv@tukui.org>
2016-02-20 23:41:32 -0500

    Add code to enable/disable movers. Need to apply it throughout the UI so movers aren't shown for disabled elements.

--- ElvUI/core/movers.lua
+++ ElvUI/core/movers.lua
@@ -16,6 +16,7 @@ local ERR_NOT_IN_COMBAT = ERR_NOT_IN_COMBAT
 -- GLOBALS: ElvUIParent, ElvUIMoverNudgeWindow
 
 E.CreatedMovers = {}
+E.DisabledMovers = {}
 
 local function SizeChanged(frame)
 	if InCombatLockdown() then return; end
@@ -345,6 +346,8 @@ function E:CreateMover(parent, name, text, overlay, snapoffset, postdrag, moverT
 end
 
 function E:ToggleMovers(show, moverType)
+	self.configMode = show
+
 	for name, _ in pairs(E.CreatedMovers) do
 		if not show then
 			_G[name]:Hide()
@@ -356,6 +359,42 @@ function E:ToggleMovers(show, moverType)
 			end
 		end
 	end
+end
+
+function E:DisableMover(name)
+	if(self.DisabledMovers[name]) then return end
+	if(not self.CreatedMovers[name]) then
+		error("mover doesn't exist")
+	end	
+		
+	self.DisabledMovers[name] = {}
+	for x, y in pairs(self.CreatedMovers[name]) do
+		self.DisabledMovers[name][x] = y
+	end
+	
+	if self.configMode then
+		_G[name]:Hide()
+	end
+	
+	self.CreatedMovers[name] = nil
+end
+
+function E:EnableMover(name)
+	if(self.CreatedMovers[name]) then return end
+	if(not self.DisabledMovers[name]) then
+		error("mover doesn't exist")
+	end	
+	
+	self.CreatedMovers[name] = {}
+	for x, y in pairs(self.DisabledMovers[name]) do
+		self.CreatedMovers[name][x] = y
+	end
+	
+	if self.configMode then
+		_G[name]:Show()
+	end	
+	
+	self.DisabledMovers[name] = nil
 end
 
 function E:ResetMovers(arg)
