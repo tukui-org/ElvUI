8cedfccfeccdb9a2b6ceafd7514fc00a068b1e52
Blazeflack <blazeflack@hotmail.com>
2016-03-04 18:35:32 +0100

    Fixed TargetGlow to work the same way as ThreatGlow when using Power Offset.
    Ticket #956

--- ElvUI/modules/unitframes/elements/targetglow.lua
+++ ElvUI/modules/unitframes/elements/targetglow.lua
@@ -19,6 +19,13 @@ function UF:Construct_TargetGlow(frame)
 	frame.shadow = nil
 	--x:SetFrameStrata('BACKGROUND')
 	x:Hide();
+	
+	--Secondary TargetGlow, for power frame when using power offset
+	frame:CreateShadow('Default')
+	local shadow = frame.shadow
+	shadow:SetFrameStrata('BACKGROUND')
+	x.powerGlow = shadow
+	frame.shadow = nil
 
 	return x
 end
@@ -29,6 +36,9 @@ function UF:UpdateTargetGlow(event)
 
 	if UnitIsUnit(unit, 'target') then
 		self.TargetGlow:Show()
+		if self.USE_POWERBAR_OFFSET then
+			self.TargetGlow.powerGlow:Show()
+		end
 		local reaction = UnitReaction(unit, 'player')
 
 		if UnitIsPlayer(unit) then
@@ -36,40 +46,52 @@ function UF:UpdateTargetGlow(event)
 			if class then
 				local color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
 				self.TargetGlow:SetBackdropBorderColor(color.r, color.g, color.b)
+				self.TargetGlow.powerGlow:SetBackdropBorderColor(color.r, color.g, color.b)
 			else
 				self.TargetGlow:SetBackdropBorderColor(1, 1, 1)
+				self.TargetGlow.powerGlow:SetBackdropBorderColor(1, 1, 1)
 			end
 		elseif reaction then
 			local color = FACTION_BAR_COLORS[reaction]
 			self.TargetGlow:SetBackdropBorderColor(color.r, color.g, color.b)
+			self.TargetGlow.powerGlow:SetBackdropBorderColor(color.r, color.g, color.b)
 		else
 			self.TargetGlow:SetBackdropBorderColor(1, 1, 1)
+			self.TargetGlow.powerGlow:SetBackdropBorderColor(1, 1, 1)
 		end
 	else
 		self.TargetGlow:Hide()
+		self.TargetGlow.powerGlow:Hide()
 	end
 end
 
 function UF:Configure_TargetGlow(frame)
-		local SHADOW_SPACING = frame.SHADOW_SPACING
-		local tGlow = frame.TargetGlow
-		tGlow:ClearAllPoints()
+	local SHADOW_SPACING = frame.SHADOW_SPACING
+	local targetHealthGlow = frame.TargetGlow
+	local targetPowerGlow = frame.TargetGlow.powerGlow
+	targetHealthGlow:ClearAllPoints()
 
-		tGlow:Point("TOPLEFT", -SHADOW_SPACING, SHADOW_SPACING)
-		tGlow:Point("TOPRIGHT", SHADOW_SPACING, SHADOW_SPACING)
+	if frame.USE_POWERBAR_OFFSET then
+		if frame.ORIENTATION == "RIGHT" then
+			targetHealthGlow:Point("TOPLEFT", frame.Health.backdrop, "TOPLEFT", -frame.SHADOW_SPACING - frame.SPACING - frame.STAGGER_WIDTH, frame.SHADOW_SPACING + frame.SPACING + (frame.USE_CLASSBAR and (frame.USE_MINI_CLASSBAR and 0 or frame.CLASSBAR_HEIGHT)))
+			targetHealthGlow:Point("BOTTOMRIGHT", frame.Health.backdrop, "BOTTOMRIGHT", frame.SHADOW_SPACING + frame.SPACING, -frame.SHADOW_SPACING - frame.SPACING)
+		else
+			targetHealthGlow:Point("TOPLEFT", frame.Health.backdrop, "TOPLEFT", -frame.SHADOW_SPACING - frame.SPACING, frame.SHADOW_SPACING + frame.SPACING + (frame.USE_CLASSBAR and (frame.USE_MINI_CLASSBAR and 0 or frame.CLASSBAR_HEIGHT) or 0))
+			targetHealthGlow:Point("BOTTOMRIGHT", frame.Health.backdrop, "BOTTOMRIGHT", frame.SHADOW_SPACING + frame.SPACING + frame.STAGGER_WIDTH, -frame.SHADOW_SPACING - frame.SPACING)
+		end
+
+		targetPowerGlow:ClearAllPoints()
+		targetPowerGlow:Point("TOPLEFT", frame.Power.backdrop, "TOPLEFT", -frame.SHADOW_SPACING - frame.SPACING, frame.SHADOW_SPACING + frame.SPACING)
+		targetPowerGlow:Point("BOTTOMRIGHT", frame.Power.backdrop, "BOTTOMRIGHT", frame.SHADOW_SPACING + frame.SPACING, -frame.SHADOW_SPACING - frame.SPACING)
+	else
+		targetHealthGlow:Point("TOPLEFT", -frame.SHADOW_SPACING, frame.SHADOW_SPACING-(frame.USE_MINI_CLASSBAR and frame.CLASSBAR_YOFFSET or 0))
 
 		if frame.USE_MINI_POWERBAR then
-			tGlow:Point("BOTTOMLEFT", -SHADOW_SPACING, -SHADOW_SPACING + (frame.POWERBAR_HEIGHT/2))
-			tGlow:Point("BOTTOMRIGHT", SHADOW_SPACING, -SHADOW_SPACING + (frame.POWERBAR_HEIGHT/2))
+			targetHealthGlow:Point("BOTTOMLEFT", -frame.SHADOW_SPACING, -frame.SHADOW_SPACING + (frame.POWERBAR_HEIGHT/2))
+			targetHealthGlow:Point("BOTTOMRIGHT", frame.SHADOW_SPACING, -frame.SHADOW_SPACING + (frame.POWERBAR_HEIGHT/2))
 		else
-			tGlow:Point("BOTTOMLEFT", -SHADOW_SPACING, -SHADOW_SPACING)
-			tGlow:Point("BOTTOMRIGHT", SHADOW_SPACING, -SHADOW_SPACING)
+			targetHealthGlow:Point("BOTTOMLEFT", -frame.SHADOW_SPACING, -frame.SHADOW_SPACING)
+			targetHealthGlow:Point("BOTTOMRIGHT", frame.SHADOW_SPACING, -frame.SHADOW_SPACING)
 		end
-
-		if USE_POWERBAR_OFFSET then
-			tGlow:Point("TOPLEFT", -SHADOW_SPACING+frame.POWERBAR_OFFSET, SHADOW_SPACING)
-			tGlow:Point("TOPRIGHT", SHADOW_SPACING, SHADOW_SPACING)
-			tGlow:Point("BOTTOMLEFT", -SHADOW_SPACING+frame.POWERBAR_OFFSET, -SHADOW_SPACING+frame.POWERBAR_OFFSET)
-			tGlow:Point("BOTTOMRIGHT", SHADOW_SPACING, -SHADOW_SPACING+frame.POWERBAR_OFFSET)
-		end
+	end
 end
