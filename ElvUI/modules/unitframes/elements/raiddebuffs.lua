46535e22a94fb3dc8ae1e5d1dfb3280daee5e21f
Blazeflack <blazeflack@hotmail.com>
2016-03-09 17:37:59 +0100

    Fixed thin borders on RaidDebuffs

--- ElvUI/modules/unitframes/elements/raiddebuffs.lua
+++ ElvUI/modules/unitframes/elements/raiddebuffs.lua
@@ -9,19 +9,12 @@ local CreateFrame = CreateFrame
 
 function UF:Construct_RaidDebuffs(frame)
 	local rdebuff = CreateFrame('Frame', nil, frame.RaisedElementParent)
-	rdebuff:SetTemplate("Default")
+	rdebuff:SetTemplate("Default", nil, nil, (UF.thinBorders and not E.global.tukuiMode))
 
-	if E.PixelMode then
-		rdebuff.border = rdebuff:CreateTexture(nil, "BACKGROUND");
-		rdebuff.border:Point("TOPLEFT", -E.mult, E.mult);
-		rdebuff.border:Point("BOTTOMRIGHT", E.mult, -E.mult);
-		rdebuff.border:SetTexture(E["media"].blankTex);
-		rdebuff.border:SetVertexColor(0, 0, 0);
-	end
-
+	local offset = (UF.thinBorders and not E.global.tukuiMode) and E.mult or E.Border
 	rdebuff.icon = rdebuff:CreateTexture(nil, 'OVERLAY')
 	rdebuff.icon:SetTexCoord(unpack(E.TexCoords))
-	rdebuff.icon:SetInside()
+	rdebuff.icon:SetInside(rdebuff, offset, offset)
 
 	rdebuff.count = rdebuff:CreateFontString(nil, 'OVERLAY')
 	rdebuff.count:FontTemplate(nil, 10, 'OUTLINE')
