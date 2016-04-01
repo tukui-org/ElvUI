4687525d716e02152a43c4b59a85b8690195da5c
Blazeflack <blazeflack@hotmail.com>
2016-04-01 14:34:45 +0200

    Fixed position of infopanel when stagger is shown
    This fixes #1048

--- ElvUI/modules/unitframes/elements/infopanel.lua
+++ ElvUI/modules/unitframes/elements/infopanel.lua
@@ -24,16 +24,16 @@ function UF:Configure_InfoPanel(frame)
 		if frame.ORIENTATION == "RIGHT" and not (frame.unitframeType == "arena") then
 			frame.InfoPanel:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -frame.BORDER - frame.SPACING - (E.global.tukuiMode and frame.BORDER*2 or 0), frame.BORDER + frame.SPACING + (E.global.tukuiMode and frame.BORDER*2 or 0))
 			if(frame.USE_POWERBAR and not frame.USE_INSET_POWERBAR and not frame.POWERBAR_DETACHED) then
-				frame.InfoPanel:SetPoint("TOPLEFT", frame.Power.backdrop, "BOTTOMLEFT", frame.BORDER + (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
+				frame.InfoPanel:SetPoint("TOPLEFT", frame.Power.backdrop, "BOTTOMLEFT", frame.BORDER - frame.STAGGER_WIDTH + (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
 			else
-				frame.InfoPanel:SetPoint("TOPLEFT", frame.Health.backdrop, "BOTTOMLEFT", frame.BORDER + (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
+				frame.InfoPanel:SetPoint("TOPLEFT", frame.Health.backdrop, "BOTTOMLEFT", frame.BORDER - frame.STAGGER_WIDTH + (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
 			end
 		else
 			frame.InfoPanel:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", frame.BORDER + frame.SPACING + (E.global.tukuiMode and frame.BORDER*2 or 0), frame.BORDER + frame.SPACING + (E.global.tukuiMode and frame.BORDER*2 or 0))
 			if(frame.USE_POWERBAR and not frame.USE_INSET_POWERBAR and not frame.POWERBAR_DETACHED) then
-				frame.InfoPanel:SetPoint("TOPRIGHT", frame.Power.backdrop, "BOTTOMRIGHT", -frame.BORDER - (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
+				frame.InfoPanel:SetPoint("TOPRIGHT", frame.Power.backdrop, "BOTTOMRIGHT", -frame.BORDER + frame.STAGGER_WIDTH - (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
 			else
-				frame.InfoPanel:SetPoint("TOPRIGHT", frame.Health.backdrop, "BOTTOMRIGHT", -frame.BORDER - (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
+				frame.InfoPanel:SetPoint("TOPRIGHT", frame.Health.backdrop, "BOTTOMRIGHT", -frame.BORDER + frame.STAGGER_WIDTH - (E.global.tukuiMode and frame.BORDER*2 or 0), -(frame.SPACING*3) - (E.global.tukuiMode and frame.BORDER*2 or 0))
 			end
 		end
 
