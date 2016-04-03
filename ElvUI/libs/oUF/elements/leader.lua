51c8cffd61a601138b7e536585dbfaa155bcfd8c
Blazeflack <blazeflack@hotmail.com>
2016-04-03 17:40:42 +0200

    Updated oUF element leader

--- ElvUI/libs/oUF/elements/leader.lua
+++ ElvUI/libs/oUF/elements/leader.lua
@@ -1,3 +1,33 @@
+--[[ Element: Leader Icon
+
+ Toggles visibility based on the units leader status.
+
+ Widget
+
+ Leader - Any UI widget.
+
+ Notes
+
+ The default leader icon will be applied if the UI widget is a texture and
+ doesn't have a texture or color defined.
+
+ Examples
+
+   -- Position and size
+   local Leader = self:CreateTexture(nil, "OVERLAY")
+   Leader:SetSize(16, 16)
+   Leader:SetPoint("BOTTOM", self, "TOP")
+   
+   -- Register it with oUF
+   self.Leader = Leadera
+
+ Hooks
+
+ Override(self) - Used to completely override the internal update function.
+                  Removing the table key entry will make the element fall-back
+                  to its internal function again.
+]]
+
 local parent, ns = ...
 local oUF = ns.oUF
 
@@ -48,6 +78,7 @@ end
 local Disable = function(self)
 	local leader = self.Leader
 	if(leader) then
+		leader:Hide()
 		self:UnregisterEvent("PARTY_LEADER_CHANGED", Path)
 		self:UnregisterEvent("GROUP_ROSTER_UPDATE", Path)
 	end
