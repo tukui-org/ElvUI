d91a1aff5c0e09ef12416d2e1a390a26f62e03b2
Blazeflack <blazeflack@hotmail.com>
2016-04-27 14:28:05 +0200

    Changed how position of druid mana text is determined, and fixed issue which caused text to appear below health frame and thus be invisible.

--- ElvUI/modules/unitframes/elements/classbars.lua
+++ ElvUI/modules/unitframes/elements/classbars.lua
@@ -5,6 +5,7 @@ local UF = E:GetModule('UnitFrames');
 --Lua functions
 local select, unpack = select, unpack
 local ceil, floor = math.ceil, math.floor
+local find = string.find
 --WoW API / Variables
 local CreateFrame = CreateFrame
 local UnitPower = UnitPower
@@ -640,20 +641,35 @@ function UF:EclipseDirection()
 end
 
 function UF:DruidPostUpdateAltPower(unit, min, max)
-	local powerText = self:GetParent().Power.value
+	local parent = self:GetParent()
+	local powerText = parent.Power.value
+	local powerTextParent = powerText:GetParent()
+	local db = parent.db
+
+	local powerTextPosition = db.power.position
 
 	if min ~= max then
 		local color = ElvUF['colors'].power['MANA']
 		color = E:RGBToHex(color[1], color[2], color[3])
 
+		self.Text:SetParent(powerTextParent)
+
 		self.Text:ClearAllPoints()
 		if powerText:GetText() then
-			if select(4, powerText:GetPoint()) < 0 then
+			if find(powerTextPosition, "RIGHT") then
 				self.Text:Point("RIGHT", powerText, "LEFT", 3, 0)
 				self.Text:SetFormattedText(color.."%d%%|r |cffD7BEA5- |r", floor(min / max * 100))
-			else
+			elseif find(powerTextPosition, "LEFT") then
 				self.Text:Point("LEFT", powerText, "RIGHT", -3, 0)
 				self.Text:SetFormattedText("|cffD7BEA5-|r"..color.." %d%%|r", floor(min / max * 100))
+			else
+				if select(4, powerText:GetPoint()) <= 0 then
+					self.Text:Point("LEFT", powerText, "RIGHT", -3, 0)
+					self.Text:SetFormattedText("|cffD7BEA5-|r"..color.." %d%%|r", floor(min / max * 100))
+				else
+					self.Text:Point("RIGHT", powerText, "LEFT", 3, 0)
+					self.Text:SetFormattedText(color.."%d%%|r |cffD7BEA5- |r", floor(min / max * 100))
+				end
 			end
 		else
 			self.Text:Point(powerText:GetPoint())
@@ -670,7 +686,7 @@ function UF:EclipsePostUpdateVisibility()
 	local isShown = self:IsShown()
 	if druidEclipseIsShown ~= isShown then
 		druidEclipseIsShown = isShown
-		
+
 		--Only toggle if the eclipse bar was not replaced with druid mana
 		if not druidManaIsShown then
 			ToggleResourceBar(self)
