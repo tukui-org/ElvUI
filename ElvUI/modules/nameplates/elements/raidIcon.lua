656b88f78bc5c04048fc444ecde3327a3b9f0297
Elv <elv@tukui.org>
2016-05-21 09:30:58 -0400

    Display raid icon above name when healthbar isn't shown.

--- ElvUI/modules/nameplates/elements/raidIcon.lua
+++ ElvUI/modules/nameplates/elements/raidIcon.lua
@@ -4,6 +4,13 @@ local mod = E:GetModule('NamePlates')
 function mod:UpdateElement_RaidIcon(frame)
 	local icon = frame.RaidIcon;
 	local index = GetRaidTargetIndex(frame.unit);
+	icon:ClearAllPoints()
+	if(frame.HealthBar:IsShown()) then
+		icon:SetPoint("RIGHT", frame.HealthBar, "LEFT", -6, 0)
+	else
+		icon:SetPoint("BOTTOM", frame.Name, "TOP", 0, 3)
+	end
+	
 	if ( index ) then
 		SetRaidTargetIconTexture(icon, index);
 		icon:Show();
