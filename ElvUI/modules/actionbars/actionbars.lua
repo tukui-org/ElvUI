tukui-org/ElvUI

commit 690a8fc5e59d1f38ec3c47fba6edd19ba4e13845

Fixed issue which caused available action pages to update to incorrect values when exiting vehicles.
This fixes #1094

ElvUI/modules/actionbars/actionbars.lua
@@ -11,6 +11,11 @@ local format, gsub, split, strfind = string.format, string.gsub, string.split, s
 --WoW API / Variables
 local hooksecurefunc = hooksecurefunc
 local CreateFrame = CreateFrame
+local UnitHealth = UnitHealth
+local UnitHealthMax = UnitHealthMax
+local UnitCastingInfo = UnitCastingInfo
+local UnitChannelInfo = UnitChannelInfo
+local UnitAffectingCombat = UnitAffectingCombat
 local UnitExists = UnitExists
 local UnitOnTaxi = UnitOnTaxi
 local VehicleExit = VehicleExit
@@ -38,6 +43,10 @@ local GetCVarBool, SetCVar = GetCVarBool, SetCVar
 local C_PetBattlesIsInBattle = C_PetBattles.IsInBattle
 local NUM_ACTIONBAR_BUTTONS = NUM_ACTIONBAR_BUTTONS
 local LE_ACTIONBAR_STATE_MAIN = LE_ACTIONBAR_STATE_MAIN
+local BOTTOMLEFT_ACTIONBAR_PAGE = BOTTOMLEFT_ACTIONBAR_PAGE
+local BOTTOMRIGHT_ACTIONBAR_PAGE = BOTTOMRIGHT_ACTIONBAR_PAGE
+local RIGHT_ACTIONBAR_PAGE = RIGHT_ACTIONBAR_PAGE
+local LEFT_ACTIONBAR_PAGE = LEFT_ACTIONBAR_PAGE
 
 --Global variables that we don't cache, list them here for mikk's FindGlobals script
 -- GLOBALS: LeaveVehicleButton, Minimap, SpellFlyout, SpellFlyoutHorizontalBackground
@@ -53,7 +62,37 @@ local LE_ACTIONBAR_STATE_MAIN = LE_ACTIONBAR_STATE_MAIN
 -- GLOBALS: InterfaceOptionsActionBarsPanelLockActionBars
 -- GLOBALS: InterfaceOptionsActionBarsPanelPickupActionKeyDropDown
 -- GLOBALS: InterfaceOptionsStatusTextPanelXP
--- GLOBALS: PlayerTalentFrame, SpellFlyoutBackgroundEnd
+-- GLOBALS: PlayerTalentFrame, SpellFlyoutBackgroundEnd, UIParent
+-- GLOBALS: VIEWABLE_ACTION_BAR_PAGES, SHOW_MULTI_ACTIONBAR_1, SHOW_MULTI_ACTIONBAR_2
+-- GLOBALS: SHOW_MULTI_ACTIONBAR_3, SHOW_MULTI_ACTIONBAR_4
+
+--This is a modified version of the Blizzard equivalent, which leaves out the check for "IsNormalActionBarState()"
+--Without this the available action pages update to incorrect values when exiting a vehicle
+function AB:MultiActionBar_Update()
+	if ( SHOW_MULTI_ACTIONBAR_1 ) then
+		MultiBarBottomLeft.isShowing = 1;
+		VIEWABLE_ACTION_BAR_PAGES[BOTTOMLEFT_ACTIONBAR_PAGE] = nil;
+	else
+		MultiBarBottomLeft.isShowing = nil;
+		VIEWABLE_ACTION_BAR_PAGES[BOTTOMLEFT_ACTIONBAR_PAGE] = 1;
+	end
+	if ( SHOW_MULTI_ACTIONBAR_2 ) then
+		VIEWABLE_ACTION_BAR_PAGES[BOTTOMRIGHT_ACTIONBAR_PAGE] = nil;
+	else
+		VIEWABLE_ACTION_BAR_PAGES[BOTTOMRIGHT_ACTIONBAR_PAGE] = 1;
+	end
+	if ( SHOW_MULTI_ACTIONBAR_3 ) then
+		VIEWABLE_ACTION_BAR_PAGES[RIGHT_ACTIONBAR_PAGE] = nil;
+	else
+		VIEWABLE_ACTION_BAR_PAGES[RIGHT_ACTIONBAR_PAGE] = 1;
+	end
+	if ( SHOW_MULTI_ACTIONBAR_3 and SHOW_MULTI_ACTIONBAR_4 ) then
+		VIEWABLE_ACTION_BAR_PAGES[LEFT_ACTIONBAR_PAGE] = nil;
+	else
+		VIEWABLE_ACTION_BAR_PAGES[LEFT_ACTIONBAR_PAGE] = 1;
+	end
+end
+
 
 local Sticky = LibStub("LibSimpleSticky-1.0");
 local _LOCK
@@ -790,6 +829,7 @@ function AB:DisableBlizzard()
 	InterfaceOptionsStatusTextPanelXP:SetAlpha(0)
 	InterfaceOptionsStatusTextPanelXP:SetScale(0.00001)
 	self:SecureHook('BlizzardOptionsPanel_OnEvent')
+	self:SecureHook("MultiActionBar_Update")
 	--InterfaceOptionsFrameCategoriesButton6:SetScale(0.00001)
 	if PlayerTalentFrame then
 		PlayerTalentFrame:UnregisterEvent("ACTIVE_TALENT_GROUP_CHANGED")

commit 176378da18afe16007b1431af000afd3a27a2f6c

Fixed a taint caused by commit 690a8fc5 which may have prevented people from changing action page while in combat.

ElvUI/modules/actionbars/actionbars.lua
@@ -66,34 +66,15 @@ local LEFT_ACTIONBAR_PAGE = LEFT_ACTIONBAR_PAGE
 -- GLOBALS: VIEWABLE_ACTION_BAR_PAGES, SHOW_MULTI_ACTIONBAR_1, SHOW_MULTI_ACTIONBAR_2
 -- GLOBALS: SHOW_MULTI_ACTIONBAR_3, SHOW_MULTI_ACTIONBAR_4
 
-
---This is a modified version of the Blizzard equivalent, which leaves out the check for "IsNormalActionBarState()"
---Without this the available action pages update to incorrect values when exiting a vehicle
-function AB:MultiActionBar_Update()
-	if ( SHOW_MULTI_ACTIONBAR_1 ) then
-		MultiBarBottomLeft.isShowing = 1;
-		VIEWABLE_ACTION_BAR_PAGES[BOTTOMLEFT_ACTIONBAR_PAGE] = nil;
-	else
-		MultiBarBottomLeft.isShowing = nil;
-		VIEWABLE_ACTION_BAR_PAGES[BOTTOMLEFT_ACTIONBAR_PAGE] = 1;
-	end
-	if ( SHOW_MULTI_ACTIONBAR_2 ) then
-		VIEWABLE_ACTION_BAR_PAGES[BOTTOMRIGHT_ACTIONBAR_PAGE] = nil;
-	else
-		VIEWABLE_ACTION_BAR_PAGES[BOTTOMRIGHT_ACTIONBAR_PAGE] = 1;
-	end
-	if ( SHOW_MULTI_ACTIONBAR_3 ) then
-		VIEWABLE_ACTION_BAR_PAGES[RIGHT_ACTIONBAR_PAGE] = nil;
-	else
-		VIEWABLE_ACTION_BAR_PAGES[RIGHT_ACTIONBAR_PAGE] = 1;
-	end
-	if ( SHOW_MULTI_ACTIONBAR_3 and SHOW_MULTI_ACTIONBAR_4 ) then
-		VIEWABLE_ACTION_BAR_PAGES[LEFT_ACTIONBAR_PAGE] = nil;
-	else
-		VIEWABLE_ACTION_BAR_PAGES[LEFT_ACTIONBAR_PAGE] = 1;
+--When WoW calls "MultiActionBar_Update", it checks if MainMenuBar:IsShown() returns true
+--This is not the case for us, after exiting a vehicle the MainMenuBar is not set to shown,
+--due to us disabling some Blizzard functionality, so we have to force the update ourselves.
+function AB:MultiActionBar_Update(preventLoop)
+	if not preventLoop then
+		BeginActionBarTransition(MainMenuBar, 1); --Make MainMenuBar:IsShown() return true
+		MultiActionBar_Update(true) --Update again and supply "true" argument to prevent loop
 	end
 end
-
 
 local Sticky = LibStub("LibSimpleSticky-1.0");
 local _LOCK