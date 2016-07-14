90b89ab9e53aa26fd73fafc8b55923904d88fd4b
Merathilis <neo0608@gmail.com>
2016-07-14 17:48:02 +0200

    Cleanup Tradeskill Skin. See: http://git.tukui.org/Elv/elvui-beta/issues/5#note_10079
    Ticket Reference: Elv/elvui-beta#5

--- ElvUI/modules/skins/blizzard/tradeskill.lua
+++ ElvUI/modules/skins/blizzard/tradeskill.lua
@@ -8,13 +8,10 @@ local unpack = unpack
 
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.tradeskill ~= true then return end
+	
+	-- MainFrame
 	TradeSkillFramePortrait:Kill()
 	TradeSkillFrame:StripTextures(true)
-	TradeSkillFrame.DetailsFrame:StripTextures()
-	TradeSkillFrame.DetailsInset:StripTextures()
-	TradeSkillFrame.RecipeInset:StripTextures()
-	TradeSkillFrame.DetailsFrame.Background:Hide()
-
 	TradeSkillFrame:SetTemplate("Transparent")
 	TradeSkillFrame:Height(TradeSkillFrame:GetHeight() + 12)
 	TradeSkillFrame.RankFrame:StripTextures()
@@ -24,22 +21,6 @@ local function LoadSkin()
 	TradeSkillFrame.FilterButton:StripTextures(true)
 	TradeSkillFrame.FilterButton:CreateBackdrop('Default', true)
 	TradeSkillFrame.FilterButton.backdrop:SetAllPoints()
-
-	-- Needs Review
-	-- S:HandleTab(TradeSkillFrame.RecipeList.LearnedTab)
-	TradeSkillFrame.RecipeList.LearnedTab:StripTextures()
-	-- S:HandleTab(TradeSkillFrame.RecipeList.UnlearnedTab)
-	TradeSkillFrame.RecipeList.UnlearnedTab:StripTextures()
-
-	S:HandleButton(TradeSkillFrame.DetailsFrame.CreateAllButton, true)
-	S:HandleButton(TradeSkillFrame.DetailsFrame.CreateButton, true)
-	S:HandleButton(TradeSkillFrame.DetailsFrame.ExitButton, true)
-	S:HandleEditBox(TradeSkillFrame.SearchBox)
-
-	-- Needs Review
-	-- S:HandleScrollBar(TradeSkillFrame.RecipeList.scrollBar)
-	-- S:HandleScrollBar(TradeSkillFrame.DetailsFrame.ScrollBar)
-
 	TradeSkillFrame.LinkToButton:GetNormalTexture():SetTexCoord(0.25, 0.7, 0.37, 0.75)
 	TradeSkillFrame.LinkToButton:GetPushedTexture():SetTexCoord(0.25, 0.7, 0.45, 0.8)
 	TradeSkillFrame.LinkToButton:GetHighlightTexture():Kill()
@@ -47,14 +28,32 @@ local function LoadSkin()
 	TradeSkillFrame.LinkToButton:Size(17, 14)
 	TradeSkillFrame.LinkToButton:SetPoint("BOTTOMRIGHT", TradeSkillFrame.FilterButton, "TOPRIGHT", -2, 4)
 
-	-- Needs review
-	-- S:HandleNextPrevButton(TradeSkillFrame.DetailsFrame.CreateMultipleInputBox.DecrementButton)
-	-- S:HandleNextPrevButton(TradeSkillFrame.DetailsFrame.CreateMultipleInputBox.IncrementButton)
-	-- TradeSkillFrame.DetailsFrame.CreateMultipleInputBox.IncrementButton:Point("LEFT", TradeSkillFrame.DetailsFrame.CreateMultipleInputBox, "RIGHT", 4, 0)
+	S:HandleEditBox(TradeSkillFrame.SearchBox)
+	S:HandleCloseButton(TradeSkillFrameCloseButton)
+
+	-- RecipeList
+	TradeSkillFrame.RecipeInset:StripTextures()
+	TradeSkillFrame.RecipeInset:StripTextures()
+	TradeSkillFrame.RecipeList.LearnedTab:StripTextures()
+	TradeSkillFrame.RecipeList.UnlearnedTab:StripTextures()
+
+	-- DetailsFrame
+	TradeSkillFrame.DetailsFrame:StripTextures()
+	TradeSkillFrame.DetailsInset:StripTextures()
+	TradeSkillFrame.DetailsFrame.Background:Hide()
 	S:HandleEditBox(TradeSkillFrame.DetailsFrame.CreateMultipleInputBox)
 	TradeSkillFrame.DetailsFrame.CreateMultipleInputBox:DisableDrawLayer("BACKGROUND")
 
-	S:HandleCloseButton(TradeSkillFrameCloseButton)
+	S:HandleButton(TradeSkillFrame.DetailsFrame.CreateAllButton, true)
+	S:HandleButton(TradeSkillFrame.DetailsFrame.CreateButton, true)
+	S:HandleButton(TradeSkillFrame.DetailsFrame.ExitButton, true)
+
+	-- Button / ScrollBar will be fixed in 7.1 see: http://git.tukui.org/Elv/elvui-beta/issues/5#note_10079
+	--[[S:HandleScrollBar(TradeSkillFrame.RecipeList.scrollBar)
+	S:HandleScrollBar(TradeSkillFrame.DetailsFrame.ScrollBar)
+	S:HandleNextPrevButton(TradeSkillFrame.DetailsFrame.CreateMultipleInputBox.DecrementButton)
+	S:HandleNextPrevButton(TradeSkillFrame.DetailsFrame.CreateMultipleInputBox.IncrementButton)
+	TradeSkillFrame.DetailsFrame.CreateMultipleInputBox.IncrementButton:Point("LEFT", TradeSkillFrame.DetailsFrame.CreateMultipleInputBox, "RIGHT", 4, 0)]]
 
 	-- Needs Review
 	--[[local once = false
