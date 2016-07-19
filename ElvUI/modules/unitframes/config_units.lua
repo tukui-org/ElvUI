3f15e01fb8fdc3e4e9654d24f2cdbdd70739c993
Blazeflack <blazeflack@hotmail.com>
2016-07-19 23:14:51 +0200

    Temporarily removed the Modules folder to fix casing fuckup in git

--- ElvUI/modules/unitframes/config_units.lua
+++
@@ -1,6 +0,0 @@
-local E, L, V, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
-local UF = E:GetModule('UnitFrames');
-local LSM = LibStub("LibSharedMedia-3.0");
-local _, ns = ...
-local ElvUF = ns.oUF
-assert(ElvUF, "ElvUI was unable to locate oUF.")
