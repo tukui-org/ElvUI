4630c87b946abe231d01e877095f6b6faeed82ba
Blazeflack <blazeflack@hotmail.com>
2016-07-24 17:22:37 +0200

    Fixed issue which caused classbar to stay visible for paladins even when disabled

--- ElvUI/Modules/unitframes/elements/classbars.lua
+++ ElvUI/Modules/unitframes/elements/classbars.lua
@@ -335,8 +335,7 @@ function UF:UpdateClassBar(cur, max, hasMaxChanged, powerType, event)
 	local db = frame.db
 	if not db then return; end
 
-	local isShown = self:IsShown()
-	if cur == 0 and db.classbar.autoHide or max == nil then
+	if not frame.USE_CLASSBAR or (cur == 0 and db.classbar.autoHide) or max == nil then
 		self:Hide()
 	else
 		self:Show()
