fbe31c38fd8e21b0354c43a54ac0eea29aa55f42
Blazeflack <blazeflack@hotmail.com>
2016-07-26 09:55:06 +0200

    Fixed positioning issues with TicketStatusFrame and the open ticket buttons
    This fixes #1219, #1220

--- ElvUI/Modules/blizzard/gm.lua
+++ ElvUI/Modules/blizzard/gm.lua
@@ -2,15 +2,32 @@ local E, L, DF = unpack(select(2, ...))
 local B = E:GetModule('Blizzard');
 
 --No point caching anything here, but list them here for mikk's FindGlobals script
--- GLOBALS: TicketStatusFrame, HelpOpenTicketButton, Minimap
+-- GLOBALS: TicketStatusFrame, HelpOpenTicketButton, Minimap, GMMover, UIParent, hooksecurefunc
 
 function B:PositionGMFrames()
 	TicketStatusFrame:ClearAllPoints()
 	TicketStatusFrame:Point("TOPLEFT", E.UIParent, 'TOPLEFT', 250, -5)
-
 	E:CreateMover(TicketStatusFrame, "GMMover", L["GM Ticket Frame"])
 
+	--Blizzard repositions this frame now in UIParent_UpdateTopFramePositions
+	hooksecurefunc(TicketStatusFrame, "SetPoint", function(self, _, anchor)
+		if anchor == UIParent then
+			TicketStatusFrame:ClearAllPoints()
+			TicketStatusFrame:Point("TOPLEFT", GMMover, 0, 0)
+		end
+	end)
+
 	HelpOpenTicketButton:SetParent(Minimap)
-	HelpOpenTicketButton:ClearAllPoints()
-	HelpOpenTicketButton:Point("TOPRIGHT", Minimap, "TOPRIGHT")
+	HelpOpenWebTicketButton:SetParent(Minimap)
+
+	local function PositionTicketButtons()
+		--We can use same position, as they don't show up at the same time
+		HelpOpenTicketButton:ClearAllPoints()
+		HelpOpenTicketButton:Point("TOPRIGHT", Minimap, "TOPRIGHT")
+		HelpOpenWebTicketButton:ClearAllPoints()
+		HelpOpenWebTicketButton:Point("TOPRIGHT", Minimap, "TOPRIGHT")
+	end
+	hooksecurefunc("HelpOpenTicketButton_Move", PositionTicketButtons)
+
+	PositionTicketButtons()
 end
