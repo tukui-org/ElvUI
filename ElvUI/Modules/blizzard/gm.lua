c0e830d64e1a2ef1877a6f7e3491443b8594b67d
Blazeflack <blazeflack@hotmail.com>
2016-07-26 09:57:50 +0200

    nothing

--- ElvUI/Modules/blizzard/gm.lua
+++ ElvUI/Modules/blizzard/gm.lua
@@ -2,7 +2,7 @@ local E, L, DF = unpack(select(2, ...))
 local B = E:GetModule('Blizzard');
 
 --No point caching anything here, but list them here for mikk's FindGlobals script
--- GLOBALS: TicketStatusFrame, HelpOpenTicketButton, Minimap, GMMover, UIParent, hooksecurefunc
+-- GLOBALS: TicketStatusFrame, HelpOpenTicketButton, HelpOpenWebTicketButton, Minimap, GMMover, UIParent, hooksecurefunc
 
 function B:PositionGMFrames()
 	TicketStatusFrame:ClearAllPoints()
