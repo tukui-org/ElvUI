03f26999dc30d19e4ea3d517f0d5b39b889fba46
Simpy <kodi03466@hotmail.com>
2016-07-29 20:00:22 -0400

    Message: Interface\AddOns\ElvUI\Modules\unitframes\tags.lua:308: attempt to compare number with nil Line308: if altR > 1 or altG > 1 or altB > 1 then

--- ElvUI/Modules/unitframes/tags.lua
+++ ElvUI/Modules/unitframes/tags.lua
@@ -303,7 +303,7 @@ ElvUF.Tags.Methods['powercolor'] = function(unit)
 	local color = ElvUF['colors'].power[pToken]
 	if color then
 		return Hex(color[1], color[2], color[3])
-	else
+	elseif altR then
 		--UnitPowerType is not consistent in how it returns rgb color values
 		if altR > 1 or altG > 1 or altB > 1 then
 			altR, altG, altB = altR/255, altG/255, altB/255
