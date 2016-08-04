cdb02cf7d8e93e8ca6187c29d7c793fd0b8047bc
Blazeflack <blazeflack@hotmail.com>
2016-08-04 15:09:53 +0200

    Updated oUF element "Auras" with changes from oUF pull request 290
    https://github.com/haste/oUF/pull/290

--- ElvUI/Libraries/oUF/elements/aura.lua
+++ ElvUI/Libraries/oUF/elements/aura.lua
@@ -129,14 +129,14 @@ local createAuraIcon = function(icons, index)
 	return button
 end
 
-local customFilter = function(icons, unit, icon, name, rank, texture, count, dtype, duration, timeLeft, caster)
+local customFilter = function(icons, unit, icon, name, rank, texture, count, dtype, duration, expirationTime, caster)
 	if((icons.onlyShowPlayer and icon.isPlayer) or (not icons.onlyShowPlayer and name)) then
 		return true
 	end
 end
 
 local updateIcon = function(unit, icons, index, offset, filter, isDebuff, visible)
-	local name, rank, texture, count, dtype, duration, timeLeft, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff = UnitAura(unit, index, filter)
+	local name, rank, texture, count, dtype, duration, expirationTime, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff, casterIsPlayer, nameplateShowAll = UnitAura(unit, index, filter)
 	
 	if icons.forceShow then
 		spellID = 47540
@@ -201,7 +201,7 @@ local updateIcon = function(unit, icons, index, offset, filter, isDebuff, visibl
 		]]
 		local show = true
 		if not icons.forceShow then
-			show = (icons.CustomFilter or customFilter) (icons, unit, icon, name, rank, texture, count, dtype, duration, timeLeft, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff)
+			show = (icons.CustomFilter or customFilter) (icons, unit, icon, name, rank, texture, count, dtype, duration, expirationTime, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff, casterIsPlayer, nameplateShowAll)
 		end
 		if(show) then
 			-- We might want to consider delaying the creation of an actual cooldown
@@ -210,7 +210,7 @@ local updateIcon = function(unit, icons, index, offset, filter, isDebuff, visibl
 			local cd = icon.cd
 			if(cd and not icons.disableCooldown) then
 				if(duration and duration > 0) then
-					cd:SetCooldown(timeLeft - duration, duration)
+					cd:SetCooldown(expirationTime - duration, duration)
 					cd:Show()
 				else
 					cd:Hide()
