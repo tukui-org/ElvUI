5b5e5ff09df6a6a95ff12fe2739e9793861b168c
Blazeflack <blazeflack@hotmail.com>
2016-08-09 12:20:56 +0200

    Made some tweaks to the new Currencies datatext.
    Reference: !288

--- ElvUI/Modules/datatexts/currencies.lua
+++ ElvUI/Modules/datatexts/currencies.lua
@@ -3,24 +3,36 @@ local DT = E:GetModule('DataTexts')
 
 --Cache global variables
 --Lua functions
+local select = select
 local format = string.format
 local join = string.join
 --WoW API / Variables
 local GetCurrencyInfo = GetCurrencyInfo
 local GetMoney = GetMoney
+local EXPANSION_NAME6 = EXPANSION_NAME6
+local OTHER = OTHER
 
 -- Currencies we care about
 -- Legion
-local ANCIENT_MANA = 1155
-local CURIOUS_COIN = 1275
-local ORDER_RESOURCES = 1220
-local SIGHTLESS_EYE = 1149
-local SEAL_OF_BROKEN_FATE = 1273
-local NETHERSHARD = 1226
-local SHADOWY_COIN = 1154
+local ANCIENT_MANA_ID = 1155
+local ANCIENT_MANA_NAME = GetCurrencyInfo(ANCIENT_MANA_ID)
+local CURIOUS_COIN_ID = 1275
+local CURIOUS_COIN_NAME = GetCurrencyInfo(CURIOUS_COIN_ID)
+local ORDER_RESOURCES_ID = 1220
+local ORDER_RESOURCES_NAME = GetCurrencyInfo(ORDER_RESOURCES_ID)
+local SIGHTLESS_EYE_ID = 1149
+local SIGHTLESS_EYE_NAME = GetCurrencyInfo(SIGHTLESS_EYE_ID)
+local SEAL_OF_BROKEN_FATE_ID = 1273
+local SEAL_OF_BROKEN_NAME = GetCurrencyInfo(SEAL_OF_BROKEN_FATE_ID)
+local NETHERSHARD_ID = 1226
+local NETHERSHARD_NAME = GetCurrencyInfo(NETHERSHARD_ID)
+local SHADOWY_COIN_ID = 1154
+local SHADOWY_COIN_NAME = GetCurrencyInfo(SHADOWY_COIN_ID)
 -- Other 
-local APEXIS_CRYSTAL = 823
-local DARKMOON_PRIZE_TICKET = 515
+local APEXIS_CRYSTAL_ID = 823
+local APEXIS_CRYSTAL_NAME = GetCurrencyInfo(APEXIS_CRYSTAL_ID)
+local DARKMOON_PRIZE_TICKET_ID = 515
+local DARKMOON_PRIZE_TICKET_NAME = GetCurrencyInfo(DARKMOON_PRIZE_TICKET_ID)
 
 local gold
 
@@ -35,19 +47,19 @@ local function OnEnter(self)
 	DT.tooltip:AddDoubleLine(L["Gold:"], E:FormatMoney(gold, E.db.datatexts.goldFormat or "BLIZZARD", not E.db.datatexts.goldCoins), 1, 1, 1)
 	DT.tooltip:AddLine(' ')
 	
-	DT.tooltip:AddLine(L["Legion:"])
-	DT.tooltip:AddDoubleLine(L["Ancient Mana"], select(2, GetCurrencyInfo(ANCIENT_MANA)), 1, 1, 1)
-	DT.tooltip:AddDoubleLine(L["Curious Coin"], select(2, GetCurrencyInfo(CURIOUS_COIN)), 1, 1, 1)
-	DT.tooltip:AddDoubleLine(L["Order Resources"], select(2, GetCurrencyInfo(ORDER_RESOURCES)), 1, 1, 1)
-	DT.tooltip:AddDoubleLine(L["Sightless Eye"], select(2, GetCurrencyInfo(SIGHTLESS_EYE)), 1, 1, 1)
-	DT.tooltip:AddDoubleLine(L["Seal of Broken Fate"], select(2, GetCurrencyInfo(SEAL_OF_BROKEN_FATE)), 1, 1, 1)
-	DT.tooltip:AddDoubleLine(L["Nethershard"], select(2, GetCurrencyInfo(NETHERSHARD)), 1, 1, 1)
-	DT.tooltip:AddDoubleLine(L["Shadowy Coin"], select(2, GetCurrencyInfo(SHADOWY_COIN)), 1, 1, 1)
+	DT.tooltip:AddLine(EXPANSION_NAME6) --"Legion"
+	DT.tooltip:AddDoubleLine(ANCIENT_MANA_NAME, select(2, GetCurrencyInfo(ANCIENT_MANA_ID)), 1, 1, 1)
+	DT.tooltip:AddDoubleLine(CURIOUS_COIN_NAME, select(2, GetCurrencyInfo(CURIOUS_COIN_ID)), 1, 1, 1)
+	DT.tooltip:AddDoubleLine(ORDER_RESOURCES_NAME, select(2, GetCurrencyInfo(ORDER_RESOURCES_ID)), 1, 1, 1)
+	DT.tooltip:AddDoubleLine(SIGHTLESS_EYE_NAME, select(2, GetCurrencyInfo(SIGHTLESS_EYE_ID)), 1, 1, 1)
+	DT.tooltip:AddDoubleLine(SEAL_OF_BROKEN_NAME, select(2, GetCurrencyInfo(SEAL_OF_BROKEN_FATE_ID)), 1, 1, 1)
+	DT.tooltip:AddDoubleLine(NETHERSHARD_NAME, select(2, GetCurrencyInfo(NETHERSHARD_ID)), 1, 1, 1)
+	DT.tooltip:AddDoubleLine(SHADOWY_COIN_NAME, select(2, GetCurrencyInfo(SHADOWY_COIN_ID)), 1, 1, 1)
 	DT.tooltip:AddLine(' ')
 	
-	DT.tooltip:AddLine(L["Other:"])
-	DT.tooltip:AddDoubleLine(L["Apexis Crystals"], select(2, GetCurrencyInfo(APEXIS_CRYSTAL)), 1, 1, 1)
-	DT.tooltip:AddDoubleLine(L["Darkmoon Prize Ticket"], select(2, GetCurrencyInfo(DARKMOON_PRIZE_TICKET)), 1, 1, 1)
+	DT.tooltip:AddLine(OTHER)
+	DT.tooltip:AddDoubleLine(APEXIS_CRYSTAL_NAME, select(2, GetCurrencyInfo(APEXIS_CRYSTAL_ID)), 1, 1, 1)
+	DT.tooltip:AddDoubleLine(DARKMOON_PRIZE_TICKET_NAME, select(2, GetCurrencyInfo(DARKMOON_PRIZE_TICKET_ID)), 1, 1, 1)
 
 	DT.tooltip:Show()
 end
