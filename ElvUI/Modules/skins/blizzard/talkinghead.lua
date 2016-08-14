c0f22ef1cc3662bd16fe952201ddf148e4b1fe56
Blazeflack <blazeflack@hotmail.com>
2016-08-14 21:42:39 +0200

    Reverted "Improved the TalkingHead frame skin a bit"
    The frame looks too big and clonky with a backdrop. Blizzard did a good job with the default background texture too. Personally I think it looks better this way.
    
    This reverts commit 712f7d07

--- ElvUI/Modules/skins/blizzard/talkinghead.lua
+++ ElvUI/Modules/skins/blizzard/talkinghead.lua
@@ -6,19 +6,10 @@ local function LoadSkin()
 	
 	TalkingHeadFrame:StripTextures()
 	TalkingHeadFrame.MainFrame:StripTextures()
-	TalkingHeadFrame.BackgroundFrame:StripTextures()
-	TalkingHeadFrame.BackgroundFrame:CreateBackdrop('Transparent')
-	TalkingHeadFrame.BackgroundFrame.backdrop:SetAllPoints()
 	TalkingHeadFrame.PortraitFrame:StripTextures()
-
-	TalkingHeadFrame.MainFrame.Model:CreateBackdrop('Transparent')
-	TalkingHeadFrame.MainFrame.Model:CreateShadow('Default')
 	TalkingHeadFrame.MainFrame.Model.PortraitBg:Hide()
 	
-	local button = TalkingHeadFrame.MainFrame.CloseButton
-	S:HandleCloseButton(button)
-	button:ClearAllPoints()
-	button:Point('TOPRIGHT', TalkingHeadFrame.BackgroundFrame, 'TOPRIGHT', -2, -2)
+	S:HandleCloseButton(TalkingHeadFrame.MainFrame.CloseButton)
 end
 
 S:RegisterSkin('Blizzard_TalkingHeadUI', LoadSkin)
