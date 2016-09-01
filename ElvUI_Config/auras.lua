c5a6ab3b8930bf62b5cccd60d53a960fccb58f3e
Blazeflack <blazeflack@hotmail.com>
2016-09-01 10:27:07 +0200

    Added headers to Buffs and Debuffs tab groups

--- ElvUI_Config/auras.lua
+++ ElvUI_Config/auras.lua
@@ -1,89 +1,98 @@
 local E, L, V, P, G, _ = unpack(ElvUI); --Import: Engine, Locales, ProfileDB, GlobalDB
 local A = E:GetModule('Auras')
 
-local auraOptions = {
-	size = {
-		type = 'range',
-		name = L["Size"],
-		desc = L["Set the size of the individual auras."],
-		min = 16, max = 60, step = 2,
-		order = 1,
-	},
-	growthDirection = {
-		type = 'select',
-		order = 2,
-		name = L["Growth Direction"],
-		desc = L["The direction the auras will grow and then the direction they will grow after they reach the wrap after limit."],
-		values = {
-			DOWN_RIGHT = format(L["%s and then %s"], L["Down"], L["Right"]),
-			DOWN_LEFT = format(L["%s and then %s"], L["Down"], L["Left"]),
-			UP_RIGHT = format(L["%s and then %s"], L["Up"], L["Right"]),
-			UP_LEFT = format(L["%s and then %s"], L["Up"], L["Left"]),
-			RIGHT_DOWN = format(L["%s and then %s"], L["Right"], L["Down"]),
-			RIGHT_UP = format(L["%s and then %s"], L["Right"], L["Up"]),
-			LEFT_DOWN = format(L["%s and then %s"], L["Left"], L["Down"]),
-			LEFT_UP = format(L["%s and then %s"], L["Left"], L["Up"]),
+local function GetAuraOptions(headerName)
+	local auraOptions = {
+		header = {
+			order = 0,
+			type = "header",
+			name = headerName,
 		},
-	},
-	wrapAfter = {
-		type = 'range',
-		order = 3,
-		name = L["Wrap After"],
-		desc = L["Begin a new row or column after this many auras."],
-		min = 1, max = 32, step = 1,
-	},
-	maxWraps = {
-		name = L["Max Wraps"],
-		order = 4,
-		desc = L["Limit the number of rows or columns."],
-		type = 'range',
-		min = 1, max = 32, step = 1,
-	},
-	horizontalSpacing = {
-		order = 5,
-		type = 'range',
-		name = L["Horizontal Spacing"],
-		min = 0, max = 50, step = 1,
-	},
-	verticalSpacing = {
-		order = 6,
-		type = 'range',
-		name = L["Vertical Spacing"],
-		min = 0, max = 50, step = 1,
-	},
-	sortMethod = {
-		order = 7,
-		name = L["Sort Method"],
-		desc = L["Defines how the group is sorted."],
-		type = 'select',
-		values = {
-			['INDEX'] = L["Index"],
-			['TIME'] = L["Time"],
-			['NAME'] = L["Name"],
+		size = {
+			type = 'range',
+			name = L["Size"],
+			desc = L["Set the size of the individual auras."],
+			min = 16, max = 60, step = 2,
+			order = 1,
 		},
-	},
-	sortDir = {
-		order = 8,
-		name = L["Sort Direction"],
-		desc = L["Defines the sort order of the selected sort method."],
-		type = 'select',
-		values = {
-			['+'] = L["Ascending"],
-			['-'] = L["Descending"],
+		growthDirection = {
+			type = 'select',
+			order = 2,
+			name = L["Growth Direction"],
+			desc = L["The direction the auras will grow and then the direction they will grow after they reach the wrap after limit."],
+			values = {
+				DOWN_RIGHT = format(L["%s and then %s"], L["Down"], L["Right"]),
+				DOWN_LEFT = format(L["%s and then %s"], L["Down"], L["Left"]),
+				UP_RIGHT = format(L["%s and then %s"], L["Up"], L["Right"]),
+				UP_LEFT = format(L["%s and then %s"], L["Up"], L["Left"]),
+				RIGHT_DOWN = format(L["%s and then %s"], L["Right"], L["Down"]),
+				RIGHT_UP = format(L["%s and then %s"], L["Right"], L["Up"]),
+				LEFT_DOWN = format(L["%s and then %s"], L["Left"], L["Down"]),
+				LEFT_UP = format(L["%s and then %s"], L["Left"], L["Up"]),
+			},
 		},
-	},
-	seperateOwn = {
-		order = 9,
-		name = L["Seperate"],
-		desc = L["Indicate whether buffs you cast yourself should be separated before or after."],
-		type = 'select',
-		values = {
-			[-1] = L["Other's First"],
-			[0] = L["No Sorting"],
-			[1] = L["Your Auras First"],
+		wrapAfter = {
+			type = 'range',
+			order = 3,
+			name = L["Wrap After"],
+			desc = L["Begin a new row or column after this many auras."],
+			min = 1, max = 32, step = 1,
 		},
-	},
-}
+		maxWraps = {
+			name = L["Max Wraps"],
+			order = 4,
+			desc = L["Limit the number of rows or columns."],
+			type = 'range',
+			min = 1, max = 32, step = 1,
+		},
+		horizontalSpacing = {
+			order = 5,
+			type = 'range',
+			name = L["Horizontal Spacing"],
+			min = 0, max = 50, step = 1,
+		},
+		verticalSpacing = {
+			order = 6,
+			type = 'range',
+			name = L["Vertical Spacing"],
+			min = 0, max = 50, step = 1,
+		},
+		sortMethod = {
+			order = 7,
+			name = L["Sort Method"],
+			desc = L["Defines how the group is sorted."],
+			type = 'select',
+			values = {
+				['INDEX'] = L["Index"],
+				['TIME'] = L["Time"],
+				['NAME'] = L["Name"],
+			},
+		},
+		sortDir = {
+			order = 8,
+			name = L["Sort Direction"],
+			desc = L["Defines the sort order of the selected sort method."],
+			type = 'select',
+			values = {
+				['+'] = L["Ascending"],
+				['-'] = L["Descending"],
+			},
+		},
+		seperateOwn = {
+			order = 9,
+			name = L["Seperate"],
+			desc = L["Indicate whether buffs you cast yourself should be separated before or after."],
+			type = 'select',
+			values = {
+				[-1] = L["Other's First"],
+				[0] = L["No Sorting"],
+				[1] = L["Your Auras First"],
+			},
+		},
+	}
+	
+	return auraOptions
+end
 
 E.Options.args.auras = {
 	type = 'group',
@@ -122,6 +131,11 @@ E.Options.args.auras = {
 			type = 'group',
 			name = L["General"],
 			args = {
+				header = {
+					order = 0,
+					type = "header",
+					name = L["General"],
+				},
 				fadeThreshold = {
 					type = 'range',
 					name = L["Fade Threshold"],
@@ -209,7 +223,7 @@ E.Options.args.auras = {
 			name = L["Buffs"],
 			get = function(info) return E.db.auras.buffs[ info[#info] ] end,
 			set = function(info, value) E.db.auras.buffs[ info[#info] ] = value; A:UpdateHeader(ElvUIPlayerBuffs) end,
-			args = auraOptions,
+			args = GetAuraOptions(L["Buffs"]),
 		},
 		debuffs = {
 			order = 20,
@@ -217,7 +231,7 @@ E.Options.args.auras = {
 			name = L["Debuffs"],
 			get = function(info) return E.db.auras.debuffs[ info[#info] ] end,
 			set = function(info, value) E.db.auras.debuffs[ info[#info] ] = value; A:UpdateHeader(ElvUIPlayerDebuffs) end,
-			args = auraOptions,
+			args = GetAuraOptions(L["Debuffs"]),
 		},
 	},
 }
