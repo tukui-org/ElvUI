87bf02c82316d3d712bbb0553d860f8e897bec36
Blazeflack <blazeflack@hotmail.com>
2016-09-03 16:18:36 +0200

    Removed leftover print statement from previous commit

--- ElvUI_Config/bags.lua
+++ ElvUI_Config/bags.lua
@@ -345,7 +345,7 @@ E.Options.args.bags = {
 					type = "multiselect",
 					name = L["Ignored Items and Search Syntax"],
 					values = function() return E.db.bags.ignoredItems end,
-					get = function(info, value)	print(E.db.bags.ignoredItems and E.db.bags.ignoredItems[value]) return E.db.bags.ignoredItems[value] end,
+					get = function(info, value)	return E.db.bags.ignoredItems[value] end,
 					set = function(info, value)
 						E.db.bags.ignoredItems[value] = nil
 						GameTooltip:Hide()--Make sure tooltip is properly hidden
