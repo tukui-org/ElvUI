2bf00b6f36d9c4835de08a6b4d5edde9089a6dd9
Blazeflack <blazeflack@hotmail.com>
2016-09-19 12:22:24 +0200

    Changed unitframe castbars to use MEDIUM frame strata to fix world map overlap.

--- ElvUI/Modules/unitframes/elements/castbar.lua
+++ ElvUI/Modules/unitframes/elements/castbar.lua
@@ -36,7 +36,7 @@ local ticks = {}
 
 function UF:Construct_Castbar(frame, direction, moverName)
 	local castbar = CreateFrame("StatusBar", nil, frame)
-	castbar:SetFrameStrata("HIGH")
+	castbar:SetFrameStrata("MEDIUM")
 	self['statusbars'][castbar] = true
 	castbar.CustomDelayText = self.CustomCastDelayText
 	castbar.CustomTimeText = self.CustomTimeText
@@ -196,7 +196,7 @@ function UF:Configure_Castbar(frame)
 		local anchorPoint = db.castbar.iconPosition
 		castbar.Icon.bg:ClearAllPoints()
 		castbar.Icon.bg:Point(INVERT_ANCHORPOINT[anchorPoint], attachPoint, anchorPoint, db.castbar.iconXOffset, db.castbar.iconYOffset)
-		castbar.Icon.bg:SetFrameStrata("HIGH")
+		castbar.Icon.bg:SetFrameStrata("MEDIUM")
 	elseif(db.castbar.icon) then
 		castbar.Icon.bg:ClearAllPoints()
 		if frame.ORIENTATION == "RIGHT" then
