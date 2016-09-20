tukui-org/ElvUI

commit 4723bc73036cd6534c6c78fe411618d7a99cea2f

Fixed issue which prevented reagent bank count font from updating immediately.

ElvUI/Modules/bags/bags.lua
@@ -324,6 +324,14 @@ function B:UpdateCountDisplay()
 			bagFrame:UpdateAllSlots()
 		end
 	end
+
+	--Reagent Bank
+	for i = 1, 98 do
+		local slot = self.BankFrame.reagentFrame.slots[i]
+		slot.Count:FontTemplate(E.LSM:Fetch("font", E.db.bags.countFont), E.db.bags.countFontSize, E.db.bags.countFontOutline)
+		slot.Count:SetTextColor(color.r, color.g, color.b)
+		self:UpdateReagentSlot(i)
+	end
 end
 
 function B:UpdateBagTypes(isBank)

commit cf3435f3b36c4dc8812fce64cd8449e7148d6f83

Fixed error in bags when changing profile, caused by commit 4723bc73

ElvUI/Modules/bags/bags.lua
@@ -326,11 +326,13 @@ function B:UpdateCountDisplay()
 	end
 
 	--Reagent Bank
-	for i = 1, 98 do
-		local slot = self.BankFrame.reagentFrame.slots[i]
-		slot.Count:FontTemplate(E.LSM:Fetch("font", E.db.bags.countFont), E.db.bags.countFontSize, E.db.bags.countFontOutline)
-		slot.Count:SetTextColor(color.r, color.g, color.b)
-		self:UpdateReagentSlot(i)
+	if self.BankFrame and self.BankFrame.reagentFrame then
+		for i = 1, 98 do
+			local slot = self.BankFrame.reagentFrame.slots[i]
+			slot.Count:FontTemplate(E.LSM:Fetch("font", E.db.bags.countFont), E.db.bags.countFontSize, E.db.bags.countFontOutline)
+			slot.Count:SetTextColor(color.r, color.g, color.b)
+			self:UpdateReagentSlot(i)
+		end
 	end
 end
 