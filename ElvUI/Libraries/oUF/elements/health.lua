97eb0c1fe1f7e1da195ffbd6de898e5fd81ce93b
Blazeflack <blazeflack@hotmail.com>
2016-11-04 18:15:10 +0100

    Updated oUF element Health with latest changes from oUF
    https://github.com/oUF-wow/oUF/commit/b894e1c181749102d404692b9f267f652307a0ae

--- ElvUI/Libraries/oUF/elements/health.lua
+++ ElvUI/Libraries/oUF/elements/health.lua
@@ -90,7 +90,7 @@ local updateFrequentUpdates
 oUF.colors.health = {49/255, 207/255, 37/255}
 
 local Update = function(self, event, unit)
-	if(self.unit ~= unit) or not unit then return end
+	if(not unit or self.unit ~= unit) then return end
 	local health = self.Health
 
 	if(health.PreUpdate) then health:PreUpdate(unit) end
