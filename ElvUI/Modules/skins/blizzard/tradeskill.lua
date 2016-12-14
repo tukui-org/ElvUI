ff31a41d5c5c857c1e152503890a1a2dc1d15664
Merathilis <neo0608@gmail.com>
2016-12-14 18:11:17 +0100

    Skin the scrollBar for GuildCrafters

--- ElvUI/Modules/skins/blizzard/tradeskill.lua
+++ ElvUI/Modules/skins/blizzard/tradeskill.lua
@@ -96,7 +96,7 @@ local function LoadSkin()
 	TradeSkillFrame.DetailsFrame.GuildFrame:SetTemplate("Transparent")
 	TradeSkillFrame.DetailsFrame.GuildFrame.Container:StripTextures()
 	TradeSkillFrame.DetailsFrame.GuildFrame.Container:SetTemplate("Transparent")
-	-- S:HandleScrollBar(TradeSkillFrame.DetailsFrame.GuildFrame.Container.ScrollFrame.scrollBar) --This cannot be skinned due to issues on Blizzards end.
+	S:HandleScrollBar(TradeSkillFrameScrollFrame.scrollBar)
 
 	--BUGFIX: TradeSkillFrame.RecipeList.scrollBar
 	--Hide current scrollbar
