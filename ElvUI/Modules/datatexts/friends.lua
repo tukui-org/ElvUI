2247699b9cf4badce0a85a9bd1e679a62e48809f
Blazeflack <blazeflack@hotmail.com>
2016-12-19 20:50:21 +0100

    Prevent an error in the friend datatext when friend list is bugged

--- ElvUI/Modules/datatexts/friends.lua
+++ ElvUI/Modules/datatexts/friends.lua
@@ -323,6 +323,11 @@ local function OnEnter(self)
 								levelc = RAID_CLASS_COLORS["PRIEST"]
 								classc = RAID_CLASS_COLORS["PRIEST"]
 							end
+							
+							--Sometimes the friend list is fubar with level 0 unknown friends
+							if not classc then
+								classc = RAID_CLASS_COLORS["PRIEST"]
+							end
 
 							if UnitInParty(info[4]) or UnitInRaid(info[4]) then grouped = 1 else grouped = 2 end
 							DT.tooltip:AddDoubleLine(format(levelNameString,levelc.r*255,levelc.g*255,levelc.b*255,info[15],classc.r*255,classc.g*255,classc.b*255,info[3],groupedTable[grouped], 255, 0, 0, statusTable[status]),info[2],238,238,238,238,238,238)
