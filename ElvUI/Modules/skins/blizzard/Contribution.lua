9797f261ba0231645c630496cb8a0f924cf510d8
Blazeflack <blazeflack@hotmail.com>
2017-03-30 16:43:34 +0200

    Skinned the reward buttons on the Contribution frame

--- ElvUI/Modules/skins/blizzard/Contribution.lua
+++ ElvUI/Modules/skins/blizzard/Contribution.lua
@@ -10,24 +10,39 @@ local ipairs = ipairs
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.Contribution ~= true then return end
 
-	ContributionCollectionFrame:CreateBackdrop("Transparent")
 	S:HandleCloseButton(ContributionCollectionFrame.CloseButton)
+	ContributionCollectionFrame.CloseButton.CloseButtonBackground:SetAlpha(0)
 	ContributionBuffTooltip:StripTextures()
 	ContributionBuffTooltip:SetTemplate("Transparent")
 	ContributionTooltip:StripTextures()
 	ContributionTooltip:CreateBackdrop("Transparent")
 
 	hooksecurefunc(ContributionMixin, "SetupContributeButton", function(self)
-		-- Skin the ContributeButtons
-		S:HandleButton(self.ContributeButton)
+		if (not self.isSkinned) then
+			-- Skin the ContributeButtons
+			S:HandleButton(self.ContributeButton)
+			self.isSkinned = true
+		end
 
 		-- Skin the StatusBar
 		local statusBar = self.Status
-		if statusBar and not statusBar.skinned then
+		if statusBar and not statusBar.isSkinned then
 			statusBar:StripTextures()
 			E:RegisterStatusBar(statusBar)
 			statusBar:CreateBackdrop('Default')
-			statusBar.skinned = true
+			statusBar.isSkinned = true
+		end
+	end)
+	
+	hooksecurefunc(ContributionMixin, "AddReward", function(self, _, rewardID)
+		local reward = self:FindOrAcquireReward(rewardID);
+		if (reward and not reward.isSkinned) then
+			reward:CreateBackdrop("Default", true)
+			reward:StyleButton()
+			reward.Border:SetAlpha(0)
+			reward.Icon:SetTexCoord(unpack(E.TexCoords))
+			reward.backdrop:SetOutside(reward.Icon)
+			reward.isSkinned = true
 		end
 	end)
 end
