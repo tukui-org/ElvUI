92763a8dad27d4783e38e0a4d52a30106ac1f100
Simpy <kodi03466@hotmail.com>
2017-04-18 14:58:44 -0400

    prevent error (rare case this is nil if opposite faction says something but it phrase into nothing? ..?)
    " ", -- [1]
    "Name-Realm", -- [2]
    "Common", -- [3]

--- ElvUI/Modules/misc/chatBubbles.lua
+++ ElvUI/Modules/misc/chatBubbles.lua
@@ -31,30 +31,32 @@ function M:UpdateBubbleBorder()
 
 	local classColorTable, lowerCaseWord, isFirstWord, rebuiltString, tempWord
 	local text = self.text:GetText()
-	for word in text:gmatch("[^%s]+") do
-		lowerCaseWord = word:lower()
-		lowerCaseWord = lowerCaseWord:gsub("%p", "")
+	if text then
+		for word in text:gmatch("[^%s]+") do
+			lowerCaseWord = word:lower()
+			lowerCaseWord = lowerCaseWord:gsub("%p", "")
 
-		if E.private.general.classColorMentionsSpeech then
-			if(CH.ClassNames[lowerCaseWord]) then
-				classColorTable = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[CH.ClassNames[lowerCaseWord]] or RAID_CLASS_COLORS[CH.ClassNames[lowerCaseWord]];
-				tempWord = word:gsub("%p", "")
-				word = word:gsub(tempWord, format("\124cff%.2x%.2x%.2x", classColorTable.r*255, classColorTable.g*255, classColorTable.b*255)..tempWord.."\124r")
-			elseif(CH.ClassNames[word]) then
-				classColorTable = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[CH.ClassNames[word]] or RAID_CLASS_COLORS[CH.ClassNames[word]];
-				word = word:gsub(word:gsub("%-","%%-"), format("\124cff%.2x%.2x%.2x", classColorTable.r*255, classColorTable.g*255, classColorTable.b*255)..word.."\124r")
+			if E.private.general.classColorMentionsSpeech then
+				if(CH.ClassNames[lowerCaseWord]) then
+					classColorTable = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[CH.ClassNames[lowerCaseWord]] or RAID_CLASS_COLORS[CH.ClassNames[lowerCaseWord]];
+					tempWord = word:gsub("%p", "")
+					word = word:gsub(tempWord, format("\124cff%.2x%.2x%.2x", classColorTable.r*255, classColorTable.g*255, classColorTable.b*255)..tempWord.."\124r")
+				elseif(CH.ClassNames[word]) then
+					classColorTable = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[CH.ClassNames[word]] or RAID_CLASS_COLORS[CH.ClassNames[word]];
+					word = word:gsub(word:gsub("%-","%%-"), format("\124cff%.2x%.2x%.2x", classColorTable.r*255, classColorTable.g*255, classColorTable.b*255)..word.."\124r")
+				end
+			end
+
+			if not isFirstWord then
+				rebuiltString = word
+				isFirstWord = true
+			else
+				rebuiltString = format("%s %s", rebuiltString, word)
 			end
 		end
 
-		if not isFirstWord then
-			rebuiltString = word
-			isFirstWord = true
-		else
-			rebuiltString = format("%s %s", rebuiltString, word)
-		end
+		self.text:SetText(rebuiltString)
 	end
-
-	self.text:SetText(rebuiltString)
 end
 
 function M:SkinBubble(frame)
