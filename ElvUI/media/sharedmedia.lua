746cd55aace0b437e3666560740dfcc4b466d07a
Darth Predator <darthpred@gmail.com>
2017-04-24 13:03:19 +0300

    This font actually supports cyrillics

--- ElvUI/media/sharedmedia.lua
+++ ElvUI/media/sharedmedia.lua
@@ -5,7 +5,7 @@ LSM:Register("border", "ElvUI GlowBorder", [[Interface\AddOns\ElvUI\media\textur
 LSM:Register("font", "Continuum Medium", [[Interface\AddOns\ElvUI\media\fonts\Continuum_Medium.ttf]])
 LSM:Register("font","Die Die Die!", [[Interface\AddOns\ElvUI\media\fonts\DieDieDie.ttf]])
 LSM:Register("font","Action Man", [[Interface\AddOns\ElvUI\media\fonts\Action_Man.ttf]])
-LSM:Register("font", "Expressway", [[Interface\AddOns\ElvUI\media\fonts\Expressway.ttf]])
+LSM:Register("font", "Expressway", [[Interface\AddOns\ElvUI\media\fonts\Expressway.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
 LSM:Register("font","PT Sans Narrow", [[Interface\AddOns\ElvUI\media\fonts\PT_Sans_Narrow.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
 LSM:Register("font", "Homespun", [[Interface\AddOns\ElvUI\media\fonts\Homespun.ttf]],LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
 LSM:Register("sound", "Awww Crap", [[Interface\AddOns\ElvUI\media\sounds\awwcrap.ogg]])
