91ae59c5a5385e3948d11056d4f70be9266ea8b0
Bunny67 <zolov6767@gmail.com>
2017-04-29 08:17:56 -0400

    Update character.lua

--- ElvUI/Modules/skins/blizzard/character.lua
+++ ElvUI/Modules/skins/blizzard/character.lua
@@ -331,12 +331,11 @@ local function LoadSkin()
 		for i=1, #PAPERDOLL_SIDEBARS do
 			local tab = _G["PaperDollSidebarTab"..i]
 			if tab and not tab.backdrop then
+				tab.Icon:SetAllPoints()
 				tab.Highlight:SetColorTexture(1, 1, 1, 0.3)
-				tab.Highlight:Point("TOPLEFT", 3, -4)
-				tab.Highlight:Point("BOTTOMRIGHT", -1, 0)
+				tab.Highlight:SetAllPoints()
 				tab.Hider:SetColorTexture(0.4,0.4,0.4,0.4)
-				tab.Hider:Point("TOPLEFT", 3, -4)
-				tab.Hider:Point("BOTTOMRIGHT", -1, 0)
+				tab.Hider:SetAllPoints()
 				tab.TabBg:Kill()
 
 				if i == 1 then
@@ -351,8 +350,6 @@ local function LoadSkin()
 					end
 				end
 				tab:CreateBackdrop("Default")
-				tab.backdrop:Point("TOPLEFT", 1, -2)
-				tab.backdrop:Point("BOTTOMRIGHT", 1, -2)
 			end
 		end
 	end
