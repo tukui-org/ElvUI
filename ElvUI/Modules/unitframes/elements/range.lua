136fd5b1604a2daf73c8bafb910cd3f1dc913e3a
Blazeflack <blazeflack@hotmail.com>
2017-06-18 15:06:16 +0200

    First steps towards making spells used for range check configurable

--- ElvUI/Modules/unitframes/elements/range.lua
+++ ElvUI/Modules/unitframes/elements/range.lua
@@ -1,10 +1,11 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local UF = E:GetModule('UnitFrames');
 local SpellRange = LibStub("SpellRange-1.0")
+local _,class = UnitClass("player")
 
 --Cache global variables
 --Lua functions
-local ipairs = ipairs
+local pairs, ipairs = pairs, ipairs
 local tinsert, tremove, twipe = table.insert, table.remove, table.wipe
 --WoW API / Variables
 local CheckInteractDistance = CheckInteractDistance
@@ -39,76 +40,21 @@ function UF:Configure_Range(frame)
 	end
 end
 
-local function AddSpell(table, spellID)
-	table[#table + 1] = spellID
+UF.SpellRangeTable = {}
+
+local function AddSpell(tbl, spellID)
+	UF.SpellRangeTable[class] = UF.SpellRangeTable[class] or {}
+	UF.SpellRangeTable[class][tbl] = UF.SpellRangeTable[class][tbl] or {}
+	UF.SpellRangeTable[class][tbl][#UF.SpellRangeTable[class][tbl] + 1] = spellID
 end
 
-local _,class = UnitClass("player")
-local friendlySpells, resSpells, longEnemySpells, enemySpells, petSpells = {}, {}, {}, {}, {}
-
-do
-	if class == "PRIEST" then
-		AddSpell(enemySpells, 585) -- Smite (40 yards)
-		AddSpell(enemySpells, 589) -- Shadow Word: Pain (40 yards)
-		AddSpell(friendlySpells, 2061) -- Flash Heal (40 yards)
-		AddSpell(friendlySpells, 17) -- Power Word: Shield (40 yards)
-		AddSpell(resSpells, 2006) -- Resurrection (40 yards)
-	elseif class == "DRUID" then
-		AddSpell(enemySpells, 8921) -- Moonfire (40 yards, all specs, lvl 3)
-		AddSpell(friendlySpells, 8936) -- Regrowth (40 yards, all specs, lvl 5)
-		AddSpell(resSpells, 50769) -- Revive (40 yards, all specs, lvl 14)
-	elseif class == "PALADIN" then
-		AddSpell(enemySpells, 20271) -- Judgement (30 yards)
-		AddSpell(longEnemySpells, 20473) -- Holy Shock (40 yards)
-		AddSpell(friendlySpells, 19750) -- Flash of Light (40 yards)
-		AddSpell(resSpells, 7328) -- Redemption (40 yards)
-	elseif class == "SHAMAN" then
-		AddSpell(enemySpells, 188196) -- Lightning Bolt (Elemental) (40 yards)
-		AddSpell(enemySpells, 187837) -- Lightning Bolt (Enhancement) (40 yards)
-		AddSpell(enemySpells, 403) -- Lightning Bolt (Resto) (40 yards)
-		AddSpell(friendlySpells, 8004) -- Healing Surge (Resto/Elemental) (40 yards)
-		AddSpell(friendlySpells, 188070) -- Healing Surge (Enhancement) (40 yards)
-		AddSpell(resSpells, 2008) -- Ancestral Spirit (40 yards) 
-	elseif class == "WARLOCK" then
-		AddSpell(enemySpells, 5782) -- Fear (30 yards)
-		AddSpell(longEnemySpells, 234153) -- Drain Life (40 yards)
-		AddSpell(longEnemySpells, 198590) --Drain Soul (40 yards)
-		AddSpell(longEnemySpells, 232670) --Shadow Bolt (40 yards, lvl 1 spell)
-		AddSpell(longEnemySpells, 686) --Shadow Bolt (Demonology) (40 yards, lvl 1 spell)
-		AddSpell(petSpells, 755) -- Health Funnel (45 yards)
-		AddSpell(friendlySpells, 20707) -- Soulstone (40 yards)
-	elseif class == "MAGE" then
-		AddSpell(enemySpells, 118) -- Polymorph (30 yards)
-		AddSpell(longEnemySpells, 116) -- Frostbolt (Frost) (40 yards)
-		AddSpell(longEnemySpells, 44425) -- Arcane Barrage (Arcane) (40 yards)
-		AddSpell(longEnemySpells, 133) -- Fireball (Fire) (40 yards)
-		AddSpell(friendlySpells, 130) -- Slow Fall (40 yards)
-	elseif class == "HUNTER" then
-		AddSpell(petSpells, 982) -- Mend Pet (45 yards)
-		AddSpell(enemySpells, 75) -- Auto Shot (40 yards)
-	elseif class == "DEATHKNIGHT" then
-		AddSpell(enemySpells, 49576) -- Death Grip
-		AddSpell(longEnemySpells, 47541) -- Death Coil (Unholy) (40 yards)
-		AddSpell(resSpells, 61999) -- Raise Ally (40 yards)
-	elseif class == "ROGUE" then
-		AddSpell(enemySpells, 185565) -- Poisoned Knife (Assassination) (30 yards)
-		AddSpell(enemySpells, 185763) -- Pistol Shot (Outlaw) (20 yards)
-		AddSpell(enemySpells, 114014) -- Shuriken Toss (Sublety) (30 yards)
-		AddSpell(enemySpells, 1725) -- Distract (30 yards)
-		AddSpell(friendlySpells, 57934) -- Tricks of the Trade (100 yards)
-	elseif class == "WARRIOR" then
-		AddSpell(enemySpells, 5246) -- Intimidating Shout (Arms/Fury) (8 yards)
-		AddSpell(enemySpells, 100) -- Charge (Arms/Fury) (8-25 yards)
-		AddSpell(longEnemySpells, 355) -- Taunt (30 yards)
-	elseif class == "MONK" then
-		AddSpell(enemySpells, 115546) -- Provoke (30 yards)
-		AddSpell(longEnemySpells, 117952) -- Crackling Jade Lightning (40 yards)
-		AddSpell(friendlySpells, 116694) -- Effuse (40 yards)
-		AddSpell(resSpells, 115178) -- Resuscitate (40 yards)
-	elseif class == "DEMONHUNTER" then
-		AddSpell(enemySpells, 183752) -- Consume Magic (20 yards)
-		AddSpell(longEnemySpells, 185123) -- Throw Glaive (Havoc) (30 yards)
-		AddSpell(longEnemySpells, 204021) -- Fiery Brand (Vengeance) (30 yards)
+function UF:UpdateRangeCheckSpells()
+	for tbl, spells in pairs(E.global.unitframe.spellRangeCheck[class]) do
+		for spellID in pairs(spells) do
+			if spellID then --We allow value to be false to disable this spell from being used
+				AddSpell(tbl, spellID)
+			end
+		end
 	end
 end
 
@@ -135,8 +81,8 @@ local function friendlyIsInRange(unit)
 		return true
 	end
 
-	if UnitIsDeadOrGhost(unit) and #resSpells > 0 then
-		for _, spellID in ipairs(resSpells) do
+	if UnitIsDeadOrGhost(unit) and #UF.SpellRangeTable[class].resSpells > 0 then
+		for _, spellID in ipairs(UF.SpellRangeTable[class].resSpells) do
 			if SpellRange.IsSpellInRange(spellID, unit) == 1 then
 				return true
 			end
@@ -145,11 +91,11 @@ local function friendlyIsInRange(unit)
 		return false
 	end
 
-	if #friendlySpells == 0 and (UnitInRaid(unit) or UnitInParty(unit)) then
+	if #UF.SpellRangeTable[class].friendlySpells == 0 and (UnitInRaid(unit) or UnitInParty(unit)) then
 		unit = getUnit(unit)
 		return unit and UnitInRange(unit)
 	else
-		for _, spellID in ipairs(friendlySpells) do
+		for _, spellID in ipairs(UF.SpellRangeTable[class].friendlySpells) do
 			if SpellRange.IsSpellInRange(spellID, unit) == 1 then
 				return true
 			end
@@ -164,12 +110,12 @@ local function petIsInRange(unit)
 		return true
 	end
 	
-	for _, spellID in ipairs(friendlySpells) do
+	for _, spellID in ipairs(UF.SpellRangeTable[class].friendlySpells) do
 		if SpellRange.IsSpellInRange(spellID, unit) == 1 then
 			return true
 		end
 	end
-	for _, spellID in ipairs(petSpells) do
+	for _, spellID in ipairs(UF.SpellRangeTable[class].petSpells) do
 		if SpellRange.IsSpellInRange(spellID, unit) == 1 then
 			return true
 		end
@@ -183,7 +129,7 @@ local function enemyIsInRange(unit)
 		return true
 	end
 	
-	for _, spellID in ipairs(enemySpells) do
+	for _, spellID in ipairs(UF.SpellRangeTable[class].enemySpells) do
 		if SpellRange.IsSpellInRange(spellID, unit) == 1 then
 			return true
 		end
@@ -193,7 +139,7 @@ local function enemyIsInRange(unit)
 end
 
 local function enemyIsInLongRange(unit)
-	for _, spellID in ipairs(longEnemySpells) do
+	for _, spellID in ipairs(UF.SpellRangeTable[class].longEnemySpells) do
 		if SpellRange.IsSpellInRange(spellID, unit) == 1 then
 			return true
 		end
--- ElvUI/Settings/Global.lua
+++ ElvUI/Settings/Global.lua
@@ -22,11 +22,6 @@ G['classtimer'] = {}
 
 G["nameplate"] = {}
 
-G['unitframe'] = {
-	['aurafilters'] = {},
-	['buffwatch'] = {},
-}
-
 G["chat"] = {
 	["classColorMentionExcludedNames"] = {},
 }
@@ -37,4 +32,180 @@ G["bags"] = {
 
 G["datatexts"] = {
 	["customCurrencies"] = {},
+}
+
+G['unitframe'] = {
+	['aurafilters'] = {},
+	['buffwatch'] = {},
+	["spellRangeCheck"] = {
+		["PRIEST"] = {
+			["enemySpells"] = {
+				[585] = true, -- Smite (40 yards)
+				[589] = true, -- Shadow Word: Pain (40 yards)
+			},
+			["longEnemySpells"] = {},
+			["friendlySpells"] = {
+				[2061] = true, -- Flash Heal (40 yards)
+				[17] = true, -- Power Word: Shield (40 yards)
+			},
+			["resSpells"] = {
+				[2006] = true, -- Resurrection (40 yards)
+			},
+			["petSpells"] = {},
+		},
+		["DRUID"] = {
+			["enemySpells"] = {
+				[8921] = true, -- Moonfire (40 yards, all specs, lvl 3)
+			},
+			["longEnemySpells"] = {},
+			["friendlySpells"] = {
+				[8936] = true, -- Regrowth (40 yards, all specs, lvl 5)
+			},
+			["resSpells"] = {
+				[50769] = true, -- Revive (40 yards, all specs, lvl 14)
+			},
+			["petSpells"] = {},
+		},
+		["PALADIN"] = {
+			["enemySpells"] = {
+				[20271] = true, -- Judgement (30 yards)
+			},
+			["longEnemySpells"] = {
+				[20473] = true, -- Holy Shock (40 yards)
+			},
+			["friendlySpells"] = {
+				[19750] = true, -- Flash of Light (40 yards)
+			},
+			["resSpells"] = {
+				[7328] = true, -- Redemption (40 yards)
+			},
+			["petSpells"] = {},
+		},
+		["SHAMAN"] = {
+			["enemySpells"] = {
+				[188196] = true, -- Lightning Bolt (Elemental) (40 yards)
+				[187837] = true, -- Lightning Bolt (Enhancement) (40 yards)
+				[403] = true, -- Lightning Bolt (Resto) (40 yards)
+			},
+			["longEnemySpells"] = {},
+			["friendlySpells"] = {
+				[8004] = true, -- Healing Surge (Resto/Elemental) (40 yards)
+				[188070] = true, -- Healing Surge (Enhancement) (40 yards)
+			},
+			["resSpells"] = {
+				[2008] = true, -- Ancestral Spirit (40 yards)
+			},
+			["petSpells"] = {},
+		},
+		["WARLOCK"] = {
+			["enemySpells"] = {
+				[5782] = true, -- Fear (30 yards)
+			},
+			["longEnemySpells"] = {
+				[234153] = true, -- Drain Life (40 yards)
+				[198590] = true, --Drain Soul (40 yards)
+				[232670] = true, --Shadow Bolt (40 yards, lvl 1 spell)
+				[686] = true, --Shadow Bolt (Demonology) (40 yards, lvl 1 spell)
+			},
+			["friendlySpells"] = {
+				[20707] = true, -- Soulstone (40 yards)
+			},
+			["resSpells"] = {},
+			["petSpells"] = {
+				[755] = true, -- Health Funnel (45 yards)
+			},
+		},
+		["MAGE"] = {
+			["enemySpells"] = {
+				[118] = true, -- Polymorph (30 yards)
+			},
+			["longEnemySpells"] = {
+				[116] = true, -- Frostbolt (Frost) (40 yards)
+				[44425] = true, -- Arcane Barrage (Arcane) (40 yards)
+				[133] = true, -- Fireball (Fire) (40 yards)
+			},
+			["friendlySpells"] = {
+				[130] = true, -- Slow Fall (40 yards)
+			},
+			["resSpells"] = {},
+			["petSpells"] = {},
+		},
+		["HUNTER"] = {
+			["enemySpells"] = {
+				[75] = true, -- Auto Shot (40 yards)
+			},
+			["longEnemySpells"] = {},
+			["friendlySpells"] = {},
+			["resSpells"] = {},
+			["petSpells"] = {
+				[982] = true, -- Mend Pet (45 yards)
+			},
+		},
+		["DEATHKNIGHT"] = {
+			["enemySpells"] = {
+				[49576] = true, -- Death Grip
+			},
+			["longEnemySpells"] = {
+				[47541] = true, -- Death Coil (Unholy) (40 yards)
+			},
+			["friendlySpells"] = {},
+			["resSpells"] = {
+				[61999] = true, -- Raise Ally (40 yards)
+			},
+			["petSpells"] = {},
+		},
+		["ROGUE"] = {
+			["enemySpells"] = {
+				[185565] = true, -- Poisoned Knife (Assassination) (30 yards)
+				[185763] = true, -- Pistol Shot (Outlaw) (20 yards)
+				[114014] = true, -- Shuriken Toss (Sublety) (30 yards)
+				[1725] = true, -- Distract (30 yards)
+			},
+			["longEnemySpells"] = {},
+			["friendlySpells"] = {
+				[57934] = true, -- Tricks of the Trade (100 yards)
+			},
+			["resSpells"] = {},
+			["petSpells"] = {},
+		},
+		["WARRIOR"] = {
+			["enemySpells"] = {
+				[5246] = true, -- Intimidating Shout (Arms/Fury) (8 yards)
+				[100] = true, -- Charge (Arms/Fury) (8-25 yards)
+			},
+			["longEnemySpells"] = {
+				[355] = true, -- Taunt (30 yards)
+			},
+			["friendlySpells"] = {},
+			["resSpells"] = {},
+			["petSpells"] = {},
+		},
+		["MONK"] = {
+			["enemySpells"] = {
+				[115546] = true, -- Provoke (30 yards)
+			},
+			["longEnemySpells"] = {
+				[117952] = true, -- Crackling Jade Lightning (40 yards)
+			},
+			["friendlySpells"] = {
+				[116694] = true, -- Effuse (40 yards)
+			},
+			["resSpells"] = {
+				[115178] = true, -- Resuscitate (40 yards)
+			},
+			["petSpells"] = {},
+		},
+		["DEMONHUNTER"] = {
+			["enemySpells"] = {
+				[183752] = true, -- Consume Magic (20 yards)
+			},
+			["longEnemySpells"] = {
+				[185123] = true, -- Throw Glaive (Havoc) (30 yards)
+				[204021] = true, -- Fiery Brand (Vengeance) (30 yards)
+			},
+			["friendlySpells"] = {},
+			["resSpells"] = {},
+			["petSpells"] = {},
+		},
+	},
 }
