c96e4f0756d0ae03af97b3193dd230c87666cfcb
Simpy <kodi03466@hotmail.com>
2017-08-30 14:39:50 -0400

    arrange this differently

--- ElvUI/Modules/nameplates/nameplates.lua
+++ ElvUI/Modules/nameplates/nameplates.lua
@@ -715,8 +715,8 @@ function mod:UpdateElement_Filters(frame)
 	twipe(filterList)
 
 	for filterName, filter in pairs(E.global.nameplate.filters) do
-		if E.db.nameplates and E.db.nameplates.filters and E.db.nameplates.filters[filterName] then
-			if filter.triggers and E.db.nameplates.filters[filterName].triggers and E.db.nameplates.filters[filterName].triggers.enable then
+		if filter.triggers and E.db.nameplates and E.db.nameplates.filters then
+			if E.db.nameplates.filters[filterName] and E.db.nameplates.filters[filterName].triggers and E.db.nameplates.filters[filterName].triggers.enable then
 				tinsert(filterList, {filterName, filter.triggers.priority or 1})
 			end
 		end
