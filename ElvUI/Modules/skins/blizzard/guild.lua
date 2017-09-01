b997ed1bf71e54f79d596719b72d118be83bd3e2
Blazeflack <blazeflack@hotmail.com>
2017-09-01 20:58:03 +0200

    Fixed taint which prevented kicking someone from guild
    Reference: #64

--- ElvUI/Modules/skins/blizzard/guild.lua
+++ ElvUI/Modules/skins/blizzard/guild.lua
@@ -123,9 +123,18 @@ local function LoadSkin()
 	GuildMemberOfficerNoteBackground:SetTemplate("Transparent")
 	GuildMemberRankDropdown:SetFrameLevel(GuildMemberRankDropdown:GetFrameLevel() + 5)
 	S:HandleDropDownBox(GuildMemberRankDropdown, 175)
+
 	--Increase height of GuildMemberDetailFrame by changing global variables
-	GUILD_DETAIL_NORM_HEIGHT = 225; --Default 175
-	GUILD_DETAIL_OFFICER_HEIGHT = 278; --Default 228
+	local GUILD_DETAIL_OFFICER_HEIGHT = GUILD_DETAIL_OFFICER_HEIGHT
+	local GUILD_DETAIL_NORM_HEIGHT = GUILD_DETAIL_NORM_HEIGHT
+	hooksecurefunc(GuildMemberDetailFrame, "SetHeight", function(self, _, breakLoop)
+		if breakLoop then return; end
+		if CanViewOfficerNote() then
+			GuildMemberDetailFrame:SetHeight(GUILD_DETAIL_OFFICER_HEIGHT + 50 + GuildMemberDetailName:GetHeight() + GuildMemberDetailRankLabel:GetHeight(), true)
+		else
+			GuildMemberDetailFrame:SetHeight(GUILD_DETAIL_NORM_HEIGHT + 50 + GuildMemberDetailName:GetHeight() + GuildMemberDetailRankLabel:GetHeight(), true)
+		end
+	end)
 
 	--News
 	GuildNewsFrame:StripTextures()
