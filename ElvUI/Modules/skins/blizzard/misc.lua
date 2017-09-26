57524147c9436f469a4aaaf1711727f3e732ef26
Simpy <kodi03466@hotmail.com>
2017-09-26 12:51:49 -0400

    finish TableAttributeDisplay (i think)

--- ElvUI/Modules/skins/blizzard/misc.lua
+++ ElvUI/Modules/skins/blizzard/misc.lua
@@ -1100,25 +1100,83 @@ local function LoadSkin()
 	end
 	hooksecurefunc("NavBar_AddButton", SkinNavBarButtons)
 
-	--New Table Attribute Display (/fstack -> then Alt)
-	TableAttributeDisplay:StripTextures()
-	TableAttributeDisplay:SetTemplate("Transparent")
-	S:HandleScrollBar(TableAttributeDisplayScrollBar)
-	S:HandleCloseButton(TableAttributeDisplay.CloseButton)
-	-- S:HandleButton(TableAttributeDisplay.OpenParentButton) --they need some love
-	-- S:HandleButton(TableAttributeDisplay.DuplicateButton) --they need some love
-	TableAttributeDisplay.DuplicateButton:ClearAllPoints()
-	TableAttributeDisplay.DuplicateButton:SetPoint("LEFT", TableAttributeDisplay.NavigateForwardButton, "RIGHT")
-	S:HandleCheckBox(TableAttributeDisplay.VisibilityButton)
-	S:HandleCheckBox(TableAttributeDisplay.HighlightButton)
-	S:HandleCheckBox(TableAttributeDisplay.DynamicUpdateButton)
-	TableAttributeDisplay.NavigateBackwardButton:ClearAllPoints()
-	TableAttributeDisplay.NavigateBackwardButton:SetPoint("LEFT", TableAttributeDisplay.OpenParentButton, "RIGHT")
-	TableAttributeDisplay.NavigateForwardButton:ClearAllPoints()
-	TableAttributeDisplay.NavigateForwardButton:SetPoint("LEFT", TableAttributeDisplay.NavigateBackwardButton, "RIGHT", 2, 0)
-	S:HandleNextPrevButton(TableAttributeDisplay.NavigateBackwardButton, nil, true)
-	S:HandleNextPrevButton(TableAttributeDisplay.NavigateForwardButton)
-	S:HandleEditBox(TableAttributeDisplay.FilterBox)
+	--New Table Attribute Display (/fstack -> then Ctrl for NA? [or Alt for EU?])
+	local function SkinTableAttributeDisplay(frame)
+		frame:StripTextures()
+		frame:SetTemplate("Transparent")
+		frame.ScrollFrameArt:StripTextures()
+		frame.ScrollFrameArt:SetTemplate("Transparent")
+		S:HandleCloseButton(frame.CloseButton)
+		frame.OpenParentButton:ClearAllPoints()
+		frame.OpenParentButton:SetPoint("TOPLEFT", frame, "TOPLEFT", 2, -2)
+		S:HandleNextPrevButton(frame.OpenParentButton, true)
+		frame.OpenParentButton:Size(17)
+		frame.DuplicateButton:ClearAllPoints()
+		frame.DuplicateButton:SetPoint("LEFT", frame.NavigateForwardButton, "RIGHT")
+		S:HandleCheckBox(frame.VisibilityButton)
+		S:HandleCheckBox(frame.HighlightButton)
+		S:HandleCheckBox(frame.DynamicUpdateButton)
+		frame.NavigateBackwardButton:ClearAllPoints()
+		frame.NavigateBackwardButton:SetPoint("LEFT", frame.OpenParentButton, "RIGHT", 2, 0)
+		frame.NavigateForwardButton:ClearAllPoints()
+		frame.NavigateForwardButton:SetPoint("LEFT", frame.NavigateBackwardButton, "RIGHT", 2, 0)
+		frame.DuplicateButton:ClearAllPoints()
+		frame.DuplicateButton:SetPoint("LEFT", frame.NavigateForwardButton, "RIGHT", 2, 0)
+		S:HandleNextPrevButton(frame.DuplicateButton, true, true)
+		frame.DuplicateButton:Size(17)
+		S:HandleNextPrevButton(frame.NavigateBackwardButton, nil, true)
+		S:HandleNextPrevButton(frame.NavigateForwardButton)
+		S:HandleEditBox(frame.FilterBox)
+
+		-- reason: UIParentScrollBar .. ???
+		if frame.LinesScrollFrame and frame.LinesScrollFrame.ScrollBar then
+			local s = frame.LinesScrollFrame.ScrollBar
+			s.ScrollUpButton:StripTextures()
+			if not s.ScrollUpButton.icon then
+				S:HandleNextPrevButton(s.ScrollUpButton)
+				SquareButton_SetIcon(s.ScrollUpButton, 'UP')
+				s.ScrollUpButton:Size(s.ScrollUpButton:GetWidth() + 7, s.ScrollUpButton:GetHeight() + 7)
+			end
+
+			s.ScrollDownButton:StripTextures()
+			if not s.ScrollDownButton.icon then
+				S:HandleNextPrevButton(s.ScrollDownButton)
+				SquareButton_SetIcon(s.ScrollDownButton, 'DOWN')
+				s.ScrollDownButton:Size(s.ScrollDownButton:GetWidth() + 7, s.ScrollDownButton:GetHeight() + 7)
+			end
+
+			if not s.trackbg then
+				s.trackbg = CreateFrame("Frame", "$parentTrackBG", frame.LinesScrollFrame)
+				s.trackbg:Point("TOPLEFT", s.ScrollUpButton, "BOTTOMLEFT", 0, -1)
+				s.trackbg:Point("TOPRIGHT", s.ScrollUpButton, "BOTTOMRIGHT", 0, -1)
+				s.trackbg:Point("BOTTOMLEFT", s.ScrollDownButton, "TOPLEFT", 0, 1)
+				s.trackbg:SetTemplate("Transparent")
+			end
+
+			local t = frame.LinesScrollFrame.ScrollBar:GetThumbTexture()
+			if t then
+				t:SetTexture(nil)
+				if not s.thumbbg then
+					s.thumbbg = CreateFrame("Frame", "$parentThumbBG", frame.LinesScrollFrame)
+					s.thumbbg:Point("TOPLEFT", t, "TOPLEFT", 2, -3)
+					s.thumbbg:Point("BOTTOMRIGHT", t, "BOTTOMRIGHT", -2, 3)
+					s.thumbbg:SetTemplate("Default", true, true)
+					s.thumbbg.backdropTexture:SetVertexColor(0.6, 0.6, 0.6)
+					if s.trackbg then
+						s.thumbbg:SetFrameLevel(s.trackbg:GetFrameLevel()+1)
+					end
+				end
+			end
+		end
+	end
+
+	SkinTableAttributeDisplay(TableAttributeDisplay)
+	hooksecurefunc(TableInspectorMixin, "OnLoad", function(self)
+		if self and self.ScrollFrameArt and not self.skinned then
+			SkinTableAttributeDisplay(self)
+			self.skinned = true
+		end
+	end)
 end
 
 S:AddCallback("SkinMisc", LoadSkin)
