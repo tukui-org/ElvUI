f5e83a0d30c093b4d6a259ecfa740151e0b3548b
Blazeflack <blazeflack@hotmail.com>
2017-09-26 22:34:36 +0200

    Removed LibArtifactData-1.0 as ElvUI license is not compliant

--- ElvUI/Modules/DataBars/Artifact.lua
+++ ElvUI/Modules/DataBars/Artifact.lua
@@ -172,9 +172,9 @@ end
 
 function mod:GetAPForItem(itemID)
 	if (LAP:DoesItemGrantArtifactPower(itemID)) then
-		return LAP:GetArtifactPowerGrantedByItem(itemID)
+		return LAP:GetArtifactPowerGrantedByItem(itemID, 1 --[[Temporary multiplier of 1 until we add our own method of getting it]])
 	end
-	
+
 	return 0
 end
 
