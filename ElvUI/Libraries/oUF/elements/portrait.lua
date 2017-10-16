359317dcb13b1abc277f39e63bce2d32f8cac1aa
Merathilis <neo0608@gmail.com>
2017-10-16 13:39:57 +0200

    Update oUF with the latest commit: https://github.com/oUF-wow/oUF/commit/ae7a12c61890a4ca04982fd0fe6ce768be120ce1
    Fix updates for *target units

--- ElvUI/Libraries/oUF/elements/portrait.lua
+++ ElvUI/Libraries/oUF/elements/portrait.lua
@@ -36,7 +36,7 @@ local _, ns = ...
 local oUF = ns.oUF
 
 local function Update(self, event, unit)
-	if(self.unit ~= unit) then return end
+	if(not unit or not UnitIsUnit(self.unit, unit)) then return end
 
 	local element = self.Portrait
 
@@ -48,28 +48,29 @@ local function Update(self, event, unit)
 	--]]
 	if(element.PreUpdate) then element:PreUpdate(unit) end
 
-	local modelUpdated = false
-	if(element:IsObjectType('PlayerModel')) then
-		local guid = UnitGUID(unit)
-		if(not UnitExists(unit) or not UnitIsConnected(unit) or not UnitIsVisible(unit)) then
-			element:SetCamDistanceScale(0.25)
-			element:SetPortraitZoom(0)
-			element:SetPosition(0, 0, 0.5)
-			element:ClearModel()
-			element:SetModel([[Interface\Buttons\TalkToMeQuestionMark.m2]])
-			element.guid = nil
-			modelUpdated = true
-		elseif(element.guid ~= guid or event == 'UNIT_MODEL_CHANGED') then
-			element:SetCamDistanceScale(1)
-			element:SetPortraitZoom(1)
-			element:SetPosition(0, 0, 0)
-			element:ClearModel()
-			element:SetUnit(unit)
-			element.guid = guid
-			modelUpdated = true
+	local guid = UnitGUID(unit)
+	local isAvailable = UnitIsConnected(unit) and UnitIsVisible(unit)
+	if(event ~= 'OnUpdate' or element.guid ~= guid or element.state ~= isAvailable) then
+		if(element:IsObjectType('PlayerModel')) then
+			if(not isAvailable) then
+				element:SetCamDistanceScale(0.25)
+				element:SetPortraitZoom(0)
+				element:SetPosition(0, 0, 0.25)
+				element:ClearModel()
+				element:SetModel([[Interface\Buttons\TalkToMeQuestionMark.m2]])
+			else
+				element:SetCamDistanceScale(1)
+				element:SetPortraitZoom(1)
+				element:SetPosition(0, 0, 0)
+				element:ClearModel()
+				element:SetUnit(unit)
+			end
+		else
+			SetPortraitTexture(element, unit)
 		end
-	else
-		SetPortraitTexture(element, unit)
+
+		element.guid = guid
+		element.state = isAvailable
 	end
 
 	--[[ Callback: Portrait:PostUpdate(unit)
@@ -104,7 +105,6 @@ local function Enable(self, unit)
 		element.__owner = self
 		element.ForceUpdate = ForceUpdate
 
-		self:RegisterEvent('UNIT_PORTRAIT_UPDATE', Path)
 		self:RegisterEvent('UNIT_MODEL_CHANGED', Path)
 		self:RegisterEvent('UNIT_CONNECTION', Path)
 
@@ -129,7 +129,6 @@ local function Disable(self)
 	if(element) then
 		element:Hide()
 
-		self:UnregisterEvent('UNIT_PORTRAIT_UPDATE', Path)
 		self:UnregisterEvent('UNIT_MODEL_CHANGED', Path)
 		self:UnregisterEvent('PARTY_MEMBER_ENABLE', Path)
 		self:UnregisterEvent('UNIT_CONNECTION', Path)
