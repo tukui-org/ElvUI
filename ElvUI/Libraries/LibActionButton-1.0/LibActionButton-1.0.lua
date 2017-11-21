09df9175ad7b1b628346a5076c0909a7ef1b6549
Simpy <kodi03466@hotmail.com>
2017-11-20 19:41:55 -0500

    Updated LibActionButton to r71 (Manually import commit from LibActionButton [1ef73f04])
    Protect against errors when trying to access a forbidden GameTooltip.
    The GameTooltip can become forbidden when showing buffs on friendly nameplates in instances, or similar occurances.

--- ElvUI/Libraries/LibActionButton-1.0/LibActionButton-1.0.lua
+++ ElvUI/Libraries/LibActionButton-1.0/LibActionButton-1.0.lua
@@ -1,18 +1,18 @@
 --[[
-Copyright (c) 2010-2016, Hendrik "nevcairiel" Leppkes <h.leppkes@gmail.com>
+Copyright (c) 2010-2017, Hendrik "nevcairiel" Leppkes <h.leppkes@gmail.com>
 
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without 
+Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice, 
+    * Redistributions of source code must retain the above copyright notice,
       this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, 
-      this list of conditions and the following disclaimer in the documentation 
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
       and/or other materials provided with the distribution.
-    * Neither the name of the developer nor the names of its contributors 
-      may be used to endorse or promote products derived from this software without 
+    * Neither the name of the developer nor the names of its contributors
+      may be used to endorse or promote products derived from this software without
       specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -28,6 +28,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ]]
+
 local MAJOR_VERSION = "LibActionButton-1.0-ElvUI"
 local MINOR_VERSION = 10
 
@@ -122,6 +123,13 @@ local ShowOverlayGlow, HideOverlayGlow
 local EndChargeCooldown
 
 local InitializeEventHandler, OnEvent, ForAllButtons, OnUpdate
+
+local function GameTooltip_GetOwnerForbidden()
+	if GameTooltip:IsForbidden() then
+		return nil
+	end
+	return GameTooltip:GetOwner()
+end
 
 local DefaultConfig = {
 	outOfRangeColoring = "button",
@@ -576,6 +584,7 @@ function Generic:OnEnter()
 end
 
 function Generic:OnLeave()
+	if GameTooltip:IsForbidden() then return end
 	GameTooltip:Hide()
 	self:SetScript('OnUpdate', nil)
 end
@@ -746,8 +755,8 @@ end
 
 function OnEvent(frame, event, arg1, ...)
 	if (event == "UNIT_INVENTORY_CHANGED" and arg1 == "player") or event == "LEARNED_SPELL_IN_TAB" then
-		local tooltipOwner = GameTooltip:GetOwner()
-		if ButtonRegistry[tooltipOwner] then
+		local tooltipOwner = GameTooltip_GetOwnerForbidden()
+		if tooltipOwner and ButtonRegistry[tooltipOwner] then
 			tooltipOwner:SetTooltip()
 		end
 	elseif event == "ACTIONBAR_SLOT_CHANGED" then
@@ -788,21 +797,21 @@ function OnEvent(frame, event, arg1, ...)
 	elseif event == "ACTIONBAR_UPDATE_COOLDOWN" then
 		for button in next, ActionButtons do
 			UpdateCooldown(button)
-			if GameTooltip:GetOwner() == button then
+			if GameTooltip_GetOwnerForbidden() == button then
 				UpdateTooltip(button)
 			end
 		end
 	elseif event == "SPELL_UPDATE_COOLDOWN" then
 		for button in next, NonActionButtons do
 			UpdateCooldown(button)
-			if GameTooltip:GetOwner() == button then
+			if GameTooltip_GetOwnerForbidden() == button then
 				UpdateTooltip(button)
 			end
 		end
 	elseif event == "LOSS_OF_CONTROL_ADDED" then
 		for button in next, ActiveButtons do
 			UpdateCooldown(button)
-			if GameTooltip:GetOwner() == button then
+			if GameTooltip_GetOwnerForbidden() == button then
 				UpdateTooltip(button)
 			end
 		end
@@ -1171,7 +1180,7 @@ function Update(self)
 
 	UpdateNewAction(self)
 
-	if GameTooltip:GetOwner() == self then
+	if GameTooltip_GetOwnerForbidden() == self then
 		UpdateTooltip(self)
 	end
 
@@ -1347,6 +1356,7 @@ function UpdateFlash(self)
 end
 
 function UpdateTooltip(self)
+	if GameTooltip:IsForbidden() then return end
 	if (GetCVar("UberTooltips") == "1") then
 		GameTooltip_SetDefaultAnchor(GameTooltip, self);
 	else
