c858629e9e87be2b5be4abbedb78da02cefff458
Blazeflack <blazeflack@hotmail.com>
2017-11-25 22:23:07 +0100

    Changed ace3 skin to no longer add border on SimpleGroup widgets
    This closes #208
    /unlabel "In Progress"
    /unlabel "Accepted"

--- ElvUI/Modules/skins/addons/ace3.lua
+++ ElvUI/Modules/skins/addons/ace3.lua
@@ -265,7 +265,7 @@ function S:SkinAce3()
 		if TYPE == "ScrollFrame" then
 			local frame = widget.scrollbar
 			SkinScrollBar(frame)
-		elseif TYPE == "InlineGroup" or TYPE == "TreeGroup" or TYPE == "TabGroup" or TYPE == "SimpleGroup" or TYPE == "Frame" or TYPE == "DropdownGroup" or TYPE == "Window" then
+		elseif TYPE == "InlineGroup" or TYPE == "TreeGroup" or TYPE == "TabGroup" or TYPE == "Frame" or TYPE == "DropdownGroup" or TYPE == "Window" then
 			local frame = widget.content:GetParent()
 			if TYPE == "Frame" then
 				frame:StripTextures()
@@ -340,6 +340,10 @@ function S:SkinAce3()
 			if widget.scrollbar then
 				SkinScrollBar(widget.scrollbar)
 			end
+		elseif TYPE == "SimpleGroup" then
+			local frame = widget.content:GetParent()
+			frame:SetTemplate("Transparent", nil, true) --ignore border updates
+			frame:SetBackdropBorderColor(0,0,0,0) --Make border completely transparent
 		end
 
 		return oldRegisterAsContainer(self, widget)
