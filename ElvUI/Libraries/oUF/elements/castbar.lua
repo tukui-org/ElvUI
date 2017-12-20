8b39bcde40ec20b678d8e9d5d24307d0af2cc803
Merathilis <neo0608@gmail.com>
2017-12-20 22:35:48 +0100

    Update oUF with the latest changes:
    castbar: Account for orientation and reverse fill for Spark repositioning.

--- ElvUI/Libraries/oUF/elements/castbar.lua
+++ ElvUI/Libraries/oUF/elements/castbar.lua
@@ -521,7 +521,15 @@ local function onUpdate(self, elapsed)
 		self:SetValue(duration)
 
 		if(self.Spark) then
-			self.Spark:SetPoint('CENTER', self, 'LEFT', (duration / self.max) * self:GetWidth(), 0)
+			local horiz = self.horizontal
+			local size = self[horiz and 'GetWidth' or 'GetHeight'](self)
+
+			local offset = (duration / self.max) * size
+			if(self:GetReverseFill()) then
+				offset = size - offset
+			end
+
+			self.Spark:SetPoint('CENTER', self, horiz and 'LEFT' or 'BOTTOM', horiz and offset or 0, horiz and 0 or offset)
 		end
 	elseif(self.channeling) then
 		local duration = self.duration - elapsed
@@ -553,7 +561,15 @@ local function onUpdate(self, elapsed)
 		self.duration = duration
 		self:SetValue(duration)
 		if(self.Spark) then
-			self.Spark:SetPoint('CENTER', self, 'LEFT', (duration / self.max) * self:GetWidth(), 0)
+			local horiz = self.horizontal
+			local size = self[horiz and 'GetWidth' or 'GetHeight'](self)
+
+			local offset = (duration / self.max) * size
+			if(self:GetReverseFill()) then
+				offset = size - offset
+			end
+
+			self.Spark:SetPoint('CENTER', self, horiz and 'LEFT' or 'BOTTOM', horiz and offset or 0, horiz and 0 or offset)
 		end
 	elseif(self.holdTime > 0) then
 		self.holdTime = self.holdTime - elapsed
@@ -596,6 +612,7 @@ local function Enable(self, unit)
 			self:RegisterEvent("UNIT_SPELLCAST_FAILED_QUIET", UNIT_SPELLCAST_FAILED_QUIET)
 		end
 
+		element.horizontal = element:GetOrientation() == 'HORIZONTAL'
 		element.holdTime = 0
 		element:SetScript('OnUpdate', element.OnUpdate or onUpdate)
 
