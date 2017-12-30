dbad21553861b17d6af21e923dffb18acb452437
Elv <elv@tukui.org>
2017-12-30 10:15:18 -0500

    remove this chat logo

--- ElvUI/Modules/chat/chat.lua
+++ ElvUI/Modules/chat/chat.lua
@@ -240,7 +240,6 @@ local rolePaths = {
 local specialChatIcons
 do --this can save some main file locals
 	local IconPath = "|TInterface\\AddOns\\ElvUI\\media\\textures\\chatLogos\\"
-	local oldBlue = IconPath.."elvui.blp:13:22|t"
 	local ElvBlue = IconPath.."elvui_blue.tga:13:25|t"
 	--local ElvPink = IconPath.."elvui_pink.tga:13:25|t"
 	--local ElvRed = IconPath.."elvui_red.tga:13:25|t"
@@ -250,13 +249,13 @@ do --this can save some main file locals
 	local MrHankey = IconPath.."mr_hankey.tga:16:18|t"
 	specialChatIcons = {
 		["Area52"] = {
-			["Illidelv"] = oldBlue,
+			["Illidelv"] = ElvBlue,
 		},
 		["Kil'jaeden"] = {
-			["Elvz"] = oldBlue,
+			["Elvz"] = ElvBlue,
 		},
 		["Spirestone"] = {
-			["Elv"] = oldBlue,
+			["Elv"] = ElvBlue,
 			["Tirain"] = MrHankey,
 			["Sinth"] = MrHankey,
 		},
