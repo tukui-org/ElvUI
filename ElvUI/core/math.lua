19f639364a067fcbef5baf31f7825c598fc69da6
Simpy <kodi03466@hotmail.com>
2018-01-01 11:09:44 -0500

    cache next in math

--- ElvUI/core/math.lua
+++ ElvUI/core/math.lua
@@ -2,7 +2,7 @@ local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, Private
 
 --Cache global variables
 local select, tonumber, assert, type, unpack, pairs = select, tonumber, assert, type, unpack, pairs
-local tinsert, tremove = tinsert, tremove
+local tinsert, tremove, next = tinsert, tremove, next
 local atan2, modf, ceil, floor, abs, sqrt, mod = math.atan2, math.modf, math.ceil, math.floor, math.abs, math.sqrt, mod
 local format, sub, upper, split, utf8sub = string.format, string.sub, string.upper, string.split, string.utf8sub
 --WoW API / Variables
