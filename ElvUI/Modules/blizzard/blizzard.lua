067d4b510e0b9539404ca5a085d1922f544a618b
Simpy <kodi03466@hotmail.com>
2018-01-21 15:09:59 -0500

    check for TalentMicroButtonAlert correctly

--- ElvUI/Modules/blizzard/blizzard.lua
+++ ElvUI/Modules/blizzard/blizzard.lua
@@ -34,21 +34,23 @@ function B:Initialize()
 	end)
 
 	-- MicroButton Talent Alert
-	if(E.global.general.showMissingTalentAlert and TalentMicroButtonAlert) then
-		TalentMicroButtonAlert:ClearAllPoints()
-		TalentMicroButtonAlert:SetPoint("CENTER", E.UIParent, "TOP", 0, -75)
-		TalentMicroButtonAlert:StripTextures()
-		TalentMicroButtonAlert.Arrow:Hide()
-		TalentMicroButtonAlert.Text:FontTemplate()
-		TalentMicroButtonAlert:CreateBackdrop("Transparent")
-		S:HandleCloseButton(TalentMicroButtonAlert.CloseButton)
+	if TalentMicroButtonAlert then -- why do we need to check this?
+		if E.global.general.showMissingTalentAlert then
+			TalentMicroButtonAlert:ClearAllPoints()
+			TalentMicroButtonAlert:SetPoint("CENTER", E.UIParent, "TOP", 0, -75)
+			TalentMicroButtonAlert:StripTextures()
+			TalentMicroButtonAlert.Arrow:Hide()
+			TalentMicroButtonAlert.Text:FontTemplate()
+			TalentMicroButtonAlert:CreateBackdrop("Transparent")
+			S:HandleCloseButton(TalentMicroButtonAlert.CloseButton)
 
-		TalentMicroButtonAlert.tex = TalentMicroButtonAlert:CreateTexture(nil, "OVERLAY")
-		TalentMicroButtonAlert.tex:Point("RIGHT", -10, 0)
-		TalentMicroButtonAlert.tex:SetTexture("Interface\\DialogFrame\\UI-Dialog-Icon-AlertNew")
-		TalentMicroButtonAlert.tex:SetSize(32, 32)
-	else
-		TalentMicroButtonAlert:Kill() -- Kill it, because then the blizz default will show
+			TalentMicroButtonAlert.tex = TalentMicroButtonAlert:CreateTexture(nil, "OVERLAY")
+			TalentMicroButtonAlert.tex:Point("RIGHT", -10, 0)
+			TalentMicroButtonAlert.tex:SetTexture("Interface\\DialogFrame\\UI-Dialog-Icon-AlertNew")
+			TalentMicroButtonAlert.tex:SetSize(32, 32)
+		else
+			TalentMicroButtonAlert:Kill() -- Kill it, because then the blizz default will show
+		end
 	end
 end
 
