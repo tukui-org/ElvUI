ad0c89c95f7256bbe262a6d7c888afb0aacec23e
Merathilis <neo0608@gmail.com>
2018-02-27 11:50:50 +0100

    I assume, this is correct :thinking:

--- ElvUI/Modules/misc/chatBubbles.lua
+++ ElvUI/Modules/misc/chatBubbles.lua
@@ -243,7 +243,7 @@ local function ChatBubble_OnUpdate(self, elapsed)
 	if (M.BubbleFrame.lastupdate < .1) then return end
 	M.BubbleFrame.lastupdate = 0
 	local chatBubble = FindChatBubble(self.msg)
-	if chatBubble or lastupdate > 0.3 then
+	if chatBubble or M.BubbleFrame.lastupdate > 0.3 then
 		self:Hide()
 		if chatBubble then
 			if not chatBubble.isSkinnedElvUI then
