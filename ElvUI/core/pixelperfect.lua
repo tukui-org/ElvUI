34fc4b8b748e031e64a762fc200b49266f49ca44
Simpy <kodi03466@hotmail.com>
2018-03-13 18:36:26 -0400

    fix #337
    also feed my sanity

--- ElvUI/core/pixelperfect.lua
+++ ElvUI/core/pixelperfect.lua
@@ -13,9 +13,8 @@ local InCombatLockdown = InCombatLockdown
 -- GLOBALS: UIParent, WorldMapFrame
 
 --Determine if Eyefinity is being used, setup the pixel perfect script.
-local scale
-local uiParentWidth, uiParentHeight, uiParentScale
 
+local scale, uiParentWidth, uiParentHeight, uiParentScale
 --This handles resizing/repositioning after leaving combat
 local frame = CreateFrame("Frame")
 frame:SetScript("OnEvent", function(self, event)
@@ -43,8 +42,10 @@ end)
 function E:UIScale(event)
 	local width = self.screenwidth;
 	local height = self.screenheight;
+	local inCombatLockdown = InCombatLockdown()
+	local isMacClient = IsMacClient()
 
-	if IsMacClient() and self.global.screenheight and self.global.screenwidth and (height ~= self.global.screenheight or width ~= self.global.screenwidth) then
+	if isMacClient and self.global.screenheight and self.global.screenwidth and (height ~= self.global.screenheight or width ~= self.global.screenwidth) then
 		self.screenheight = self.global.screenheight
 		self.screenwidth = self.global.screenwidth
 
@@ -53,15 +54,16 @@ function E:UIScale(event)
 		height = self.screenheight
 	end
 
-	if(GetCVar('uiScale')) then
-		self.global.uiScale = GetCVar('uiScale')
+	local uiScaleCVar = GetCVar('uiScale')
+	if uiScaleCVar then
+		self.global.uiScale = uiScaleCVar
 	end
 
 	local minScale = self.global.general.minUiScale or 0.64
 	if self.global.general.autoScale then
 		scale = max(minScale, min(1.15, 768/height));
 	else
-		scale = max(minScale, min(1.15, self.global.uiScale or (768/height) or UIParent:GetScale()));
+		scale = max(minScale, min(1.15, self.global.uiScale or (height > 0 and (768/height)) or UIParent:GetScale()));
 	end
 
 	if width < 1600 then
@@ -71,18 +73,18 @@ function E:UIScale(event)
 		-- I don't know how it really work, but i'm assuming they add pixel to width to compensate the bezel. :P
 
 		-- HQ resolution
-		if width >= 9840 then width = 3280; end                   	                -- WQSXGA
-		if width >= 7680 and width < 9840 then width = 2560; end                     -- WQXGA
-		if width >= 5760 and width < 7680 then width = 1920; end 	                -- WUXGA & HDTV
-		if width >= 5040 and width < 5760 then width = 1680; end 	                -- WSXGA+
+		if width >= 9840 then width = 3280; end							-- WQSXGA
+		if width >= 7680 and width < 9840 then width = 2560; end		-- WQXGA
+		if width >= 5760 and width < 7680 then width = 1920; end		-- WUXGA & HDTV
+		if width >= 5040 and width < 5760 then width = 1680; end		-- WSXGA+
 
 		-- adding height condition here to be sure it work with bezel compensation because WSXGA+ and UXGA/HD+ got approx same width
-		if width >= 4800 and width < 5760 and height == 900 then width = 1600; end   -- UXGA & HD+
+		if width >= 4800 and width < 5760 and height == 900 then width = 1600; end	-- UXGA & HD+
 
 		-- low resolution screen
-		if width >= 4320 and width < 4800 then width = 1440; end 	                -- WSXGA
-		if width >= 4080 and width < 4320 then width = 1360; end 	                -- WXGA
-		if width >= 3840 and width < 4080 then width = 1224; end 	                -- SXGA & SXGA (UVGA) & WXGA & HDTV
+		if width >= 4320 and width < 4800 then width = 1440; end		-- WSXGA
+		if width >= 4080 and width < 4320 then width = 1360; end		-- WXGA
+		if width >= 3840 and width < 4080 then width = 1224; end		-- SXGA & SXGA (UVGA) & WXGA & HDTV
 
 		-- yep, now set ElvUI to lower resolution if screen #1 width < 1600
 		if width < 1600 then
@@ -99,26 +101,25 @@ function E:UIScale(event)
 
 	if self.global.general.autoScale then
 		--Set UIScale, NOTE: SetCVar for UIScale can cause taints so only do this when we need to..
-		if E.Round and E:Round(UIParent:GetScale(), 5) ~= E:Round(scale, 5) and (event == 'PLAYER_LOGIN') then
+		if E.Round and (E:Round(UIParent:GetScale(), 5) ~= E:Round(scale, 5)) and (event == 'PLAYER_LOGIN') then
 			SetCVar("useUiScale", 1);
 			SetCVar("uiScale", scale);
 		end
 
 		--SetCVar for UI scale only accepts value as low as 0.64, so scale UIParent if needed
-		if (scale < 0.64) then
+		if scale < 0.64 then
 			UIParent:SetScale(scale)
 		end
 	end
 
 	if (event == 'PLAYER_LOGIN' or event == 'UI_SCALE_CHANGED') then
-		if IsMacClient() then
+		if isMacClient then
 			self.global.screenheight = floor(GetScreenHeight()*100+.5)/100
 			self.global.screenwidth = floor(GetScreenWidth()*100+.5)/100
 		end
 
 		--Resize self.UIParent if Eyefinity is on.
 		if self.eyefinity then
-
 			-- if autoscale is off, find a new width value of self.UIParent for screen #1.
 			if not self.global.general.autoScale or height > 1200 then
 				local h = UIParent:GetHeight();
@@ -129,7 +130,7 @@ function E:UIScale(event)
 				height = h;
 			end
 
-			if InCombatLockdown() then --Delay changing size if we are in combat, to prevent error when people have minimized the game
+			if inCombatLockdown then --Delay changing size if we are in combat, to prevent error when people have minimized the game
 				uiParentWidth = width
 				uiParentHeight = height
 				uiParentScale = scale
@@ -144,7 +145,7 @@ function E:UIScale(event)
 			]]
 			--self.UIParent:SetSize(UIParent:GetWidth() - 250, UIParent:GetHeight() - 250);
 
-			if InCombatLockdown() then
+			if inCombatLockdown then
 				uiParentWidth = width
 				uiParentHeight = height
 				uiParentScale = scale
@@ -154,7 +155,7 @@ function E:UIScale(event)
 			end
 		end
 
-		if InCombatLockdown() then
+		if inCombatLockdown then
 			frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 		else
 			self.UIParent:ClearAllPoints();
