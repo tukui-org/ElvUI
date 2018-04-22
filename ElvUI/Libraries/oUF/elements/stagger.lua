6dc954561d2d91f7e3ced8a12b5eca63f123df3e
Merathilis <neo0608@gmail.com>
2018-04-22 11:25:26 +0200

    Update oUF: https://github.com/oUF-wow/oUF/commit/1cdcab52539272da6c5a2853540757eb820face0 --stagger: fix wrong event name and re-register stock UI events on disable
    https://github.com/oUF-wow/oUF/commit/f95fd8ab833d840915c972442c5ec2dd1a8a85a3 --stagger: take one more stock UI event into account

--- ElvUI/Libraries/oUF/elements/stagger.lua
+++ ElvUI/Libraries/oUF/elements/stagger.lua
@@ -192,7 +192,8 @@ local function Enable(self)
 		MonkStaggerBar:UnregisterEvent('PLAYER_ENTERING_WORLD')
 		MonkStaggerBar:UnregisterEvent('PLAYER_SPECIALIZATION_CHANGED')
 		MonkStaggerBar:UnregisterEvent('UNIT_DISPLAYPOWER')
-		MonkStaggerBar:UnregisterEvent('UPDATE_VEHICLE_ACTION_BAR')
+		MonkStaggerBar:UnregisterEvent('UNIT_EXITED_VEHICLE')
+		MonkStaggerBar:UnregisterEvent('UPDATE_VEHICLE_ACTIONBAR')
 
 		element:Hide()
 
@@ -209,10 +210,11 @@ local function Disable(self)
 		self:UnregisterEvent('UNIT_DISPLAYPOWER', VisibilityPath)
 		self:UnregisterEvent('PLAYER_TALENT_UPDATE', VisibilityPath)
 
-		MonkStaggerBar:UnregisterEvent('PLAYER_ENTERING_WORLD')
-		MonkStaggerBar:UnregisterEvent('PLAYER_SPECIALIZATION_CHANGED')
-		MonkStaggerBar:UnregisterEvent('UNIT_DISPLAYPOWER')
-		MonkStaggerBar:UnregisterEvent('UPDATE_VEHICLE_ACTION_BAR')
+		MonkStaggerBar:RegisterEvent('PLAYER_ENTERING_WORLD')
+		MonkStaggerBar:RegisterEvent('PLAYER_SPECIALIZATION_CHANGED')
+		MonkStaggerBar:RegisterEvent('UNIT_DISPLAYPOWER')
+		MonkStaggerBar:RegisterEvent('UNIT_EXITED_VEHICLE')
+		MonkStaggerBar:RegisterEvent('UPDATE_VEHICLE_ACTIONBAR')
 	end
 end
 
