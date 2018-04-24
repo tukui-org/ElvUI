d7a8e82e92d875cd852d52e7be7201169c1b123a
Merathilis <neo0608@gmail.com>
2018-04-24 11:24:54 +0200

    Update oUF: https://github.com/oUF-wow/oUF/commit/80327fa743e98392023e24cad8d0ecd492a940f3 -- auras: Add aura count nil check

--- ElvUI/Libraries/oUF/elements/auras.lua
+++ ElvUI/Libraries/oUF/elements/auras.lua
@@ -145,6 +145,9 @@ local function updateIcon(element, unit, index, offset, filter, isDebuff, visibl
 		nameplateShowSelf, spellID, canApply, isBossDebuff, casterIsPlayer, nameplateShowAll,
 		timeMod, effect1, effect2, effect3 = UnitAura(unit, index, filter)
 
+	-- count may be nil sometimes
+	count = count or 0
+
 	-- ElvUI block
 	if element.forceShow then
 		spellID = 47540
