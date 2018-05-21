5569234545b4239c7bc47a7dc561a5e7e581fc35
Simpy <kodi03466@hotmail.com>
2018-05-20 21:57:07 -0400

    additional checks

--- ElvUI/Modules/unitframes/elements/frameglow.lua
+++ ElvUI/Modules/unitframes/elements/frameglow.lua
@@ -58,7 +58,7 @@ function UF:FrameGlow_ClassGlowPosition(frame, powerName, glow, offset, fromScri
 end
 
 function UF:FrameGlow_HookPowerBar(frame, power, powerName, glow, offset)
-	if frame and power and not glow[powerName..'Hooked'] then
+	if (frame and power and powerName and glow and offset) and not glow[powerName..'Hooked'] then
 		glow[powerName..'Hooked'] = true
 		local func = function() UF:FrameGlow_ClassGlowPosition(frame, powerName, glow, offset, true) end
 		power:HookScript('OnShow', func)
