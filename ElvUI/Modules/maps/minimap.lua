7d49e2d1dcf738ef262b8e23bb1b81d267163543
Simpy <kodi03466@hotmail.com>
2018-06-07 18:16:35 -0400

    clean cache in minimap.lua

--- ElvUI/Modules/maps/minimap.lua
+++ ElvUI/Modules/maps/minimap.lua
@@ -8,44 +8,29 @@ local _G = _G
 local tinsert = table.insert
 local strsub = strsub
 --WoW API / Variables
-local ClearAllTracking = ClearAllTracking
+local C_Timer_After = C_Timer.After
 local CloseAllWindows = CloseAllWindows
 local CloseMenus = CloseMenus
 local CreateFrame = CreateFrame
-local C_Timer_After = C_Timer.After
 local GarrisonLandingPageMinimapButton_OnClick = GarrisonLandingPageMinimapButton_OnClick
 local GetMinimapZoneText = GetMinimapZoneText
-local GetNumTrackingTypes = GetNumTrackingTypes
-local GetTrackingInfo = GetTrackingInfo
 local GetZonePVPInfo = GetZonePVPInfo
 local GuildInstanceDifficulty = GuildInstanceDifficulty
 local InCombatLockdown = InCombatLockdown
 local IsAddOnLoaded = IsAddOnLoaded
 local IsShiftKeyDown = IsShiftKeyDown
-local ToggleDropDownMenu = ToggleDropDownMenu
-local UIDropDownMenu_AddButton = UIDropDownMenu_AddButton
-local UIDropDownMenu_CreateInfo = UIDropDownMenu_CreateInfo
 local MainMenuMicroButton_SetNormal = MainMenuMicroButton_SetNormal
-local MiniMapTrackingDropDownButton_IsActive = MiniMapTrackingDropDownButton_IsActive
-local MiniMapTrackingDropDown_IsNoTrackingActive = MiniMapTrackingDropDown_IsNoTrackingActive
-local MiniMapTracking_SetTracking = MiniMapTracking_SetTracking
-local Minimap_OnClick = Minimap_OnClick
 local PlaySound = PlaySound
 local ShowUIPanel, HideUIPanel = ShowUIPanel, HideUIPanel
 local ToggleAchievementFrame = ToggleAchievementFrame
 local ToggleCharacter = ToggleCharacter
 local ToggleCollectionsJournal = ToggleCollectionsJournal
+local ToggleDropDownMenu = ToggleDropDownMenu
 local ToggleFrame = ToggleFrame
 local ToggleFriendsFrame = ToggleFriendsFrame
 local ToggleGuildFrame = ToggleGuildFrame
 local ToggleHelpFrame = ToggleHelpFrame
 local ToggleLFDParentFrame = ToggleLFDParentFrame
-local UnitClass = UnitClass
-local HUNTER_TRACKING = HUNTER_TRACKING
-local HUNTER_TRACKING_TEXT = HUNTER_TRACKING_TEXT
-local MINIMAP_TRACKING_NONE = MINIMAP_TRACKING_NONE
-local TOWNSFOLK = TOWNSFOLK
-local TOWNSFOLK_TRACKING_TEXT = TOWNSFOLK_TRACKING_TEXT
 
 --Global variables that we don't cache, list them here for mikk's FindGlobals script
 -- GLOBALS: GetMinimapShape, SpellBookFrame, PlayerTalentFrame, TalentFrame_LoadUI
@@ -57,7 +42,7 @@ local TOWNSFOLK_TRACKING_TEXT = TOWNSFOLK_TRACKING_TEXT
 -- GLOBALS: AudioOptionsFrameCancel, InterfaceOptionsFrame, InterfaceOptionsFrameCancel
 -- GLOBALS: LibStub, ElvUIPlayerBuffs, MMHolder, StoreMicroButton, TimeManagerClockButton
 -- GLOBALS: FeedbackUIButton, MiniMapTrackingDropDown, LeftMiniPanel, RightMiniPanel
--- GLOBALS: MinimapMover, AurasHolder, AurasMover
+-- GLOBALS: MinimapMover, AurasHolder, AurasMover, Minimap_OnClick
 -- GLOBALS: GarrisonLandingPageMinimapButton, GarrisonLandingPageTutorialBox, MiniMapMailFrame
 -- GLOBALS: QueueStatusMinimapButton, QueueStatusFrame, MiniMapInstanceDifficulty
 -- GLOBALS: MiniMapChallengeMode, MinimapBorder, MinimapBorderTop, MinimapZoomIn, MinimapZoomOut
