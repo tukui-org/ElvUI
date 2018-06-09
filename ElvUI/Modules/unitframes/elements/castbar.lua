5cd35bf9f3f0bbc3bb769f9c4601fa79e7fc5b5d
Blazeflack <blazeflack@hotmail.com>
2018-06-09 13:43:47 +0200

    Removed mage T17 code from unitframe castbar channel ticks
    This fixes #426

--- ElvUI/Modules/unitframes/elements/castbar.lua
+++ ElvUI/Modules/unitframes/elements/castbar.lua
@@ -314,9 +314,6 @@ function UF:SetCastTicks(frame, numTicks, extraTickRatio)
 	end
 end
 
-local MageSpellName = GetSpellInfo(5143) --Arcane Missiles
-local MageBuffName = GetSpellInfo(166872) --4p T17 bonus proc for arcane
-
 function UF:PostCastStart(unit, name)
 	local db = self:GetParent().db
 	if not db or not db.castbar then return; end
@@ -392,13 +389,6 @@ function UF:PostCastStart(unit, name)
 
 			UF:SetCastTicks(self, baseTicks, extraTickRatio)
 		elseif baseTicks then
-		-- T17 mage buff
-		--[[
-			local hasBuff = UnitBuff("player", MageBuffName)
-			if name == MageSpellName and hasBuff then
-				baseTicks = baseTicks + 5
-			end
-		]]
 			UF:SetCastTicks(self, baseTicks)
 		else
 			UF:HideTicks()
@@ -489,9 +479,6 @@ function UF:PostChannelUpdate(unit, name)
 			UF:SetCastTicks(self, baseTicks, self.extraTickRatio)
 		elseif baseTicks then
 			local hasBuff = UnitBuff("player", MageBuffName)
-			if name == MageSpellName and hasBuff then
-				baseTicks = baseTicks + 5
-			end
 			if self.chainChannel then
 				baseTicks = baseTicks + 1
 			end
