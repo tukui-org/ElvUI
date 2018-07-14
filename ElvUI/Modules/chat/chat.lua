899aa41bcde2de9ba27f12e6e5a6fd21eaa723c3
Simpy <kodi03466@hotmail.com>
2018-07-14 16:37:23 -0400

    fix leaking `filter` in chat.lua from chatfilter commit

--- ElvUI/Modules/chat/chat.lua
+++ ElvUI/Modules/chat/chat.lua
@@ -1241,7 +1241,7 @@ function CH:ChatFrame_MessageEventHandler(self, event, arg1, arg2, arg3, arg4, a
 
 		local chatFilters = ChatFrame_GetMessageEventFilters(event)
 		if chatFilters then
-			local newarg1, newarg2, newarg3, newarg4, newarg5, newarg6, newarg7, newarg8, newarg9, newarg10, newarg11, newarg12, newarg13, newarg14;
+			local filter, newarg1, newarg2, newarg3, newarg4, newarg5, newarg6, newarg7, newarg8, newarg9, newarg10, newarg11, newarg12, newarg13, newarg14;
 			for _, filterFunc in next, chatFilters do
 				filter, newarg1, newarg2, newarg3, newarg4, newarg5, newarg6, newarg7, newarg8, newarg9, newarg10, newarg11, newarg12, newarg13, newarg14 = filterFunc(self, event, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14);
 				if ( filter ) then
