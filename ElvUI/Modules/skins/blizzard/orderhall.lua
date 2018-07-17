c93aff8decc99e5e10609a6c897108746c111f4a
Simpy <kodi03466@hotmail.com>
2018-07-17 19:57:18 -0400

    add the artifact appearance skin and orderhall talents skin from my UI

--- ElvUI/Modules/skins/blizzard/orderhall.lua
+++ ElvUI/Modules/skins/blizzard/orderhall.lua
@@ -5,11 +5,15 @@ local S = E:GetModule('Skins')
 --Lua functions
 local _G = _G
 local pairs = pairs
+local select = select
+local unpack = unpack
 --WoW API / Variables
 local hooksecurefunc = hooksecurefunc
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS
+local CreateFrame = CreateFrame
 --Global variables that we don't cache, list them here for mikk's FindGlobals script
--- GLOBALS: CUSTOM_CLASS_COLORS, OrderHallCommandBar, OrderHallMissionFrame
+-- GLOBALS: CUSTOM_CLASS_COLORS, OrderHallCommandBar, OrderHallMissionFrame, ClassHallTalentInset
+-- GLOBALS: OrderHallTalentFrame, OrderHallTalentFramePortrait, OrderHallTalentFramePortraitFrame
 
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.orderhall ~= true then return end
@@ -81,28 +85,6 @@ local function LoadSkin()
 	S:HandleCloseButton(ZoneSupportMissionPage.CloseButton)
 	S:HandleButton(ZoneSupportMissionPage.StartMissionButton)
 	S:HandleButton(MissionComplete.NextMissionButton)
-
-	--[[FJX ME
-	-- TalentFrame
-	local TalentFrame = _G["OrderHallTalentFrame"]
-	if TalentFrame then
-		TalentFrame:StripTextures()
-		TalentFrame.LeftInset:StripTextures()
-		if TalentFrame.CornerLogo then TalentFrame.CornerLogo:Hide()
-		end
-		if TalentFrame.StyleFrame then
-			TalentFrame.StyleFrame:Hide()
-		end
-		TalentFrame:SetTemplate("Transparent")
-		TalentFrame.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
-		S:HandleCloseButton(TalentFrame.CloseButton)
-
-		-- Chromie Frame
-		_G["OrderHallTalentFramePortraitFrame"]:Hide()
-		_G["OrderHallTalentFramePortrait"]:Hide()
-		S:HandleButton(_G["OrderHallTalentFrame"].BackButton)
-	end
-	]]
 end
 
 S:AddCallbackForAddon('Blizzard_GarrisonUI', "OrderHall", LoadSkin)
@@ -121,6 +103,117 @@ local function LoadSkinCommandBar()
 	OrderHallCommandBar.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
 	OrderHallCommandBar.AreaName:SetVertexColor(classColor.r, classColor.g, classColor.b)
 	OrderHallCommandBar.WorldMapButton:Hide()
+
+	local function colorBorder(child, backdrop, atlas)
+		if child.AlphaIconOverlay:IsShown() then --isBeingResearched or (talentAvailability and not selected)
+			local alpha = child.AlphaIconOverlay:GetAlpha()
+			if alpha <= 0.5 then --talentAvailability
+				backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5) --[border = grey, shadow x2]
+				child.darkOverlay:SetColorTexture(0, 0, 0, 0.50)
+				child.darkOverlay:Show()
+			elseif alpha <= 0.7 then --isBeingResearched
+				backdrop:SetBackdropBorderColor(0,1,1) --[border = teal, shadow x1]
+				child.darkOverlay:SetColorTexture(0, 0, 0, 0.25)
+				child.darkOverlay:Show()
+			end
+		elseif atlas == "orderhalltalents-spellborder-green" then
+			backdrop:SetBackdropBorderColor(0,1,0) --[border = green, no shadow]
+			child.darkOverlay:Hide()
+		elseif atlas == "orderhalltalents-spellborder-yellow" then
+			backdrop:SetBackdropBorderColor(1,1,0) --[border = yellow, no shadow]
+			child.darkOverlay:Hide()
+		elseif atlas == "orderhalltalents-spellborder" then
+			backdrop:SetBackdropBorderColor(unpack(E.media.bordercolor))
+			child.darkOverlay:SetColorTexture(0, 0, 0, 0.75) --[border will be default, shadow x3]
+			child.darkOverlay:Show()
+		end
+	end
+
+	OrderHallTalentFrame:HookScript("OnShow", function(self)
+		if self.skinned then return end
+
+		self:StripTextures()
+		self.LeftInset:StripTextures()
+		if self.CornerLogo then
+			self.CornerLogo:Hide()
+		end
+		if self.StyleFrame then
+			self.StyleFrame:Hide()
+		end
+		self:SetTemplate("Transparent")
+		self.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
+		S:HandleCloseButton(self.CloseButton)
+
+		OrderHallTalentFramePortraitFrame:Hide()
+		OrderHallTalentFramePortrait:Hide()
+		S:HandleButton(self.BackButton)
+		-- -- -- -- --
+		for i = 1, 7 do
+			local bg = CreateFrame("Frame", "OrderHallTalentFrame"..i.."PanelBackground", self)
+			if i == 1 then
+				bg:Point("TOPLEFT", self, "TOPLEFT", E.PixelMode and 6 or 9, -80)
+			else
+				bg:Point("TOPLEFT", "OrderHallTalentFrame"..(i-1).."PanelBackground", "BOTTOMLEFT", 0, -6)
+			end
+			bg:SetTemplate("Transparent")
+			bg:SetBackdropColor(0, 0, 0, 0.5)
+			bg:SetSize(E.PixelMode and 322 or 316, 52)
+		end
+		-- -- -- -- --
+		local Portrait = OrderHallTalentFramePortrait
+		local PortraitFrame = OrderHallTalentFramePortraitFrame
+
+		Portrait:Hide()
+		PortraitFrame:Hide()
+
+		local TalentInset = ClassHallTalentInset
+		local TalentClassBG = OrderHallTalentFrame.Background
+		TalentInset:CreateBackdrop("Transparent")
+		TalentInset.backdrop:SetFrameLevel(TalentInset.backdrop:GetFrameLevel()+1)
+		TalentInset.backdrop:Point('TOPLEFT', TalentClassBG, 'TOPLEFT', E.Border-1, -E.Border+1)
+		TalentInset.backdrop:Point('BOTTOMRIGHT', TalentClassBG, 'BOTTOMRIGHT', -E.Border+1, E.Border-1)
+		TalentClassBG:SetAtlas("orderhalltalents-background-"..E.myclass)
+		TalentClassBG:SetDrawLayer("ARTWORK")
+		TalentClassBG:SetAlpha(0.8)
+
+		for i=1, self:GetNumChildren() do
+			local child = select(i, self:GetChildren())
+			if child and child.Icon and not child.backdrop then
+				child:StyleButton()
+				child:CreateBackdrop()
+				child.Border:SetAlpha(0)
+				child.Highlight:SetAlpha(0)
+				child.AlphaIconOverlay:SetTexture(nil)
+				child.Icon:SetTexCoord(unpack(E.TexCoords))
+				child.Icon:SetInside(child.backdrop)
+				child.hover:SetInside(child.backdrop)
+				child.pushed:SetInside(child.backdrop)
+				child.backdrop:SetFrameLevel(child.backdrop:GetFrameLevel()+1)
+
+				child.darkOverlay = child:CreateTexture()
+				child.darkOverlay:SetAllPoints(child.Icon)
+				child.darkOverlay:SetDrawLayer('OVERLAY')
+				child.darkOverlay:Hide()
+
+				colorBorder(child, child.backdrop, child.Border:GetAtlas())
+
+				child.TalentDoneAnim:HookScript("OnFinished", function()
+					child.Border:SetAlpha(0) -- clear the yellow glow border again, after it finishes the animation
+				end)
+			end
+		end
+		self.choiceTexturePool:ReleaseAll()
+		hooksecurefunc(self, "RefreshAllData", function(frame)
+			frame.choiceTexturePool:ReleaseAll()
+			for i=1, frame:GetNumChildren() do
+				local child = select(i, frame:GetChildren())
+				if child and child.Icon and child.backdrop then
+					colorBorder(child, child.backdrop, child.Border:GetAtlas())
+				end
+			end
+		end)
+		self.skinned = true
+	end)
 end
 
 S:AddCallbackForAddon('Blizzard_OrderHallUI', "OrderHallCommandBar", LoadSkinCommandBar)
