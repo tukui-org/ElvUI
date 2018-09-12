daec94f263a16fb70792e150d26f9e5eeb0bb008
Simpy <kodi03466@hotmail.com>
2018-09-12 15:31:33 -0400

    only show the prep spec icon when the pvp spec icon is enabled

--- ElvUI/Modules/unitframes/groups/arena.lua
+++ ElvUI/Modules/unitframes/groups/arena.lua
@@ -34,17 +34,19 @@ function UF:PostUpdateArenaPreparation(_, specID)
 	local pvpIconEnabled = self.__owner.db.pvpSpecIcon and self.__owner:IsElementEnabled('PVPSpecIcon')
 	if class and spec then
 		self.__owner.ArenaPrepSpec:SetText(spec.."  -  "..LOCALIZED_CLASS_NAMES_MALE[class])
-		self.__owner.ArenaPrepIcon:SetTexture(texture or [[INTERFACE\ICONS\INV_MISC_QUESTIONMARK]])
-		self.__owner.ArenaPrepIcon.bg:Show()
-		self.__owner.ArenaPrepIcon:Show()
+
 		if pvpIconEnabled then
+			self.__owner.ArenaPrepIcon:SetTexture(texture or [[INTERFACE\ICONS\INV_MISC_QUESTIONMARK]])
+			self.__owner.ArenaPrepIcon.bg:Show()
+			self.__owner.ArenaPrepIcon:Show()
 			self.__owner.PVPSpecIcon:Hide()
 		end
 	else
 		self.__owner.ArenaPrepSpec:SetText('')
-		self.__owner.ArenaPrepIcon.bg:Hide()
-		self.__owner.ArenaPrepIcon:Hide()
+
 		if pvpIconEnabled then
+			self.__owner.ArenaPrepIcon.bg:Hide()
+			self.__owner.ArenaPrepIcon:Hide()
 			self.__owner.PVPSpecIcon:Show()
 		end
 	end
