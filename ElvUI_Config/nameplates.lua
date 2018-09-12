95315f216b0aca03ca0c6babc1e06d662fc2ec30
Merathilis <neo0608@gmail.com>
2018-09-12 16:04:24 +0200

    Different attempt for the NamePlate aura size.

--- ElvUI_Config/nameplates.lua
+++ ElvUI_Config/nameplates.lua
@@ -2075,9 +2075,18 @@ local function GetUnitSettings(unit, name)
 						get = function(info) return E.db.nameplates.units[unit].buffs[ info[#info] ] end,
 						set = function(info, value) E.db.nameplates.units[unit].buffs[ info[#info] ] = value; NP:ConfigureAll() end,
 					},
+					sizeOverride = {
+						order = 4,
+						name = L["Size Override"],
+						desc = L["If not set to 0 then override the size of the aura icon to this."],
+						type = "range",
+						min = 0, max = 60, step = 1,
+						get = function(info) return E.db.nameplates.units[unit].buffs[ info[#info] ] end,
+						set = function(info, value) E.db.nameplates.units[unit].buffs[ info[#info] ] = value; NP:ConfigureAll() end,
+					},
 					filtersGroup = {
 						name = FILTERS,
-						order = 4,
+						order = 5,
 						type = "group",
 						guiInline = true,
 						args = {
@@ -2248,9 +2257,18 @@ local function GetUnitSettings(unit, name)
 						get = function(info) return E.db.nameplates.units[unit].debuffs[ info[#info] ] end,
 						set = function(info, value) E.db.nameplates.units[unit].debuffs[ info[#info] ] = value; NP:ConfigureAll() end,
 					},
+					sizeOverride = {
+						order = 4,
+						name = L["Size Override"],
+						desc = L["If not set to 0 then override the size of the aura icon to this."],
+						type = "range",
+						min = 0, max = 60, step = 1,
+						get = function(info) return E.db.nameplates.units[unit].debuffs[ info[#info] ] end,
+						set = function(info, value) E.db.nameplates.units[unit].debuffs[ info[#info] ] = value; NP:ConfigureAll() end,
+					},
 					filtersGroup = {
 						name = FILTERS,
-						order = 4,
+						order = 5,
 						type = "group",
 						guiInline = true,
 						args = {
