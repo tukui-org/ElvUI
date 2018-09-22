f6732d7c2e98e211de5a258aadb78732238c4f33
Benik <benik@otenet.gr>
2018-09-22 14:27:19 +0300

    Fixed Haste datatext. This should deal with #794

--- ElvUI/Modules/datatexts/haste.lua
+++ ElvUI/Modules/datatexts/haste.lua
@@ -7,9 +7,11 @@ local join = string.join
 local format = string.format
 --WoW API / Variables
 local _G = _G
+local GetHaste = GetHaste
 local GetCombatRating = GetCombatRating
 local GetCombatRatingBonus = GetCombatRatingBonus
 local BreakUpLargeNumbers = BreakUpLargeNumbers
+local GetPVPGearStatRules = GetPVPGearStatRules
 local STAT_HASTE = STAT_HASTE
 local CR_HASTE_MELEE = CR_HASTE_MELEE
 local HIGHLIGHT_FONT_COLOR_CODE = HIGHLIGHT_FONT_COLOR_CODE
@@ -17,14 +19,15 @@ local FONT_COLOR_CODE_CLOSE = FONT_COLOR_CODE_CLOSE
 local PAPERDOLLFRAME_TOOLTIP_FORMAT = PAPERDOLLFRAME_TOOLTIP_FORMAT
 local STAT_HASTE_TOOLTIP = STAT_HASTE_TOOLTIP
 local STAT_HASTE_BASE_TOOLTIP = STAT_HASTE_BASE_TOOLTIP
+local RED_FONT_COLOR_CODE = RED_FONT_COLOR_CODE
 
 local displayNumberString = ''
 local lastPanel;
 
 local function OnEvent(self)
-	local rating = CR_HASTE_MELEE;
+	local haste = GetHaste()
+	self.text:SetFormattedText(displayNumberString, STAT_HASTE, haste)
 
-	self.text:SetFormattedText(displayNumberString, STAT_HASTE, GetCombatRatingBonus(rating))
 	lastPanel = self
 end
 
@@ -33,14 +36,21 @@ local OnEnter = function(self)
 
 	local rating = CR_HASTE_MELEE;
 	local classTooltip = _G["STAT_HASTE_"..E.myclass.."_TOOLTIP"]
-	local hasteRating = GetCombatRatingBonus(rating)
+	local haste = GetHaste()
+	
+	local hasteFormatString;
+	if (haste < 0 and not GetPVPGearStatRules()) then
+		hasteFormatString = RED_FONT_COLOR_CODE.."%s"..FONT_COLOR_CODE_CLOSE;
+	else
+		hasteFormatString = "%s";
+	end
 
 	if (not classTooltip) then
 		classTooltip = STAT_HASTE_TOOLTIP
 	end
 
-	DT.tooltip:AddLine(HIGHLIGHT_FONT_COLOR_CODE..format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_HASTE).." ".. format("%s", format("%.2F%%", hasteRating))..FONT_COLOR_CODE_CLOSE)
-	DT.tooltip:AddLine(classTooltip..format(STAT_HASTE_BASE_TOOLTIP, BreakUpLargeNumbers(GetCombatRating(rating)), hasteRating))
+	DT.tooltip:AddLine(HIGHLIGHT_FONT_COLOR_CODE..format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_HASTE).." ".. format(hasteFormatString, format("%.2F%%", haste))..FONT_COLOR_CODE_CLOSE)
+	DT.tooltip:AddLine(classTooltip..format(STAT_HASTE_BASE_TOOLTIP, BreakUpLargeNumbers(GetCombatRating(rating)), GetCombatRatingBonus(rating)))
 
 	DT.tooltip:Show()
 end
