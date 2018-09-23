7c9904aea7be2a1bf237b2a2ff637e0aaac608bb
Darth Predator <darthpred@gmail.com>
2018-09-23 17:02:52 +0300

    Fixed flawed logic + fixed default config group + added an ability to copy not only from but to selected profile

--- ElvUI/core/copymodule.lua
+++ ElvUI/core/copymodule.lua
@@ -1,12 +1,15 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local CP = E:NewModule('CopyProfile', "AceEvent-3.0","AceTimer-3.0","AceComm-3.0","AceSerializer-3.0")
 
+local pairs, next = pairs, next
+local format = format
+
 --Default template for a config group for a single module.
 --Contains header, general group toggle (shown only if the setting actually exists) and imports button.
 --Usage as seen in ElvUI_Config\modulecopy.lua
 function CP:CreateModuleConfigGroup(Name, section)
 	local config = {
-		order = 1,
+		order = 10,
 		type = 'group',
 		name = Name,
 		args = {
@@ -19,24 +22,48 @@ function CP:CreateModuleConfigGroup(Name, section)
 				order = 1,
 				type = "toggle",
 				name = L["General"],
-				hidden = function(info) return E.global.profileCopy.auras[ info[#info] ] == nil end,
-				get = function(info) return E.global.profileCopy.auras[ info[#info] ] end,
-				set = function(info, value) E.global.profileCopy.auras[ info[#info] ] = value; end
+				hidden = function(info) return E.global.profileCopy[section][ info[#info] ] == nil end,
+				get = function(info) return E.global.profileCopy[section][ info[#info] ] end,
+				set = function(info, value) E.global.profileCopy[section][ info[#info] ] = value; end
 			},
 			PreButtonSpacer = {
 				order = 200,
 				type = "description",
 				name = "",
 			},
-			copy = {
+			import = {
 				order = 201,
 				type = "execute",
 				name = L["Import Now"],
 				func = function() CP:ImportFromProfile(section) end,
 			},
+			export = {
+				order = 202,
+				type = "execute",
+				name = L["Export Now"],
+				func = function() CP:ExportToProfile(section) end,
+			},
 		},
 	}
 	return config
+end
+
+function CP:CopyTable(CopyFrom, CopyTo, module)
+	for key, value in pairs(CopyTo) do
+		print(key)
+		if type(value) ~= "table" then
+			if module == true or (type(module) == "table" and module.general == nil or (not CopyTo.general and module.general)) then --Some dark magic of a logic to figure out stuff
+				print("Debug, copy module option "..key)
+			end
+		else
+			if module == true then
+				print("I should copy over the whole section.")
+			elseif module[key] then
+				print("Debug, copy module table "..key)
+				CP:CopyTable(CopyFrom[key], CopyTo[key], module[key])
+			end
+		end
+	end
 end
 
 --[[
@@ -65,26 +92,38 @@ function CP:ImportFromProfile(section)
 	if section == "selected" then error('Section name could not be "selected". This name is reserved for internal setting') end
 	print(section)
 	local module = E.global.profileCopy[section]
-	if not module then error(format('Provided section name "%s" does not have a preset for profile copy.', section)) end
+	if not module then error(format('Provided section name "%s" does not have a template for profile copy.', section)) end
 	--Starting digging through the settings
-	print(#module)
-	if type(module) == "table" and #module > 0 then
-		for key, value in pairs(E.db[section]) do
-			if type(value) ~= "table" then
-				if E.global.profileCopy[section].general == nil or (not E.db[section].general and E.global.profileCopy[section].general) then
-					print("Debug, copy "..section.." option "..key)
-				end
-			else
-				if E.global.profileCopy[section][key] then
-					print("Debug, copy "..section.." table "..key)
-				end
-			end
-		end
+	local CopyFrom = ElvDB["profiles"][E.global.profileCopy.selected][section]
+	local CopyTo = E.db[section]
+	if type(module) == "table" and next(module) then
+		CP:CopyTable(CopyFrom, CopyTo, module, true)
 	elseif type(module) == "boolean" then
 		print("I should copy over the whole section.")
 	else
 		error(format('Provided section name "%s" does not have a valid copy template.', section))
 	end
+	-- E:UpdateAll(true)
+end
+
+function CP:ExportToProfile(section)
+	--Some checks for the occasion someone passes wrong stuff
+	if not section then error("No profile section provided. Usage CP:ImportFromProfile(section)") end
+	if section == "selected" then error('Section name could not be "selected". This name is reserved for internal setting') end
+	print(section)
+	local module = E.global.profileCopy[section]
+	if not module then error(format('Provided section name "%s" does not have a template for profile copy.', section)) end
+	--Starting digging through the settings
+	local CopyFrom = E.db[section]
+	local CopyTo = ElvDB["profiles"][E.global.profileCopy.selected][section]
+	if type(module) == "table" and next(module) then
+		CP:CopyTable(CopyFrom, CopyTo, module, true)
+	elseif type(module) == "boolean" then
+		print("I should copy over the whole section.")
+	else
+		error(format('Provided section name "%s" does not have a valid copy template.', section))
+	end
+	-- E:UpdateAll(true)
 end
 
 --Maybe actually not needed at all
