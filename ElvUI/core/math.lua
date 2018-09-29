2be33a89c9d47069b04dfc592f57bf1eeef24688
Azilroka <darktemplar9900@yahoo.com>
2018-09-28 20:29:50 -0500

    E:RGBToHex fallback to White not Black

--- ElvUI/core/math.lua
+++ ElvUI/core/math.lua
@@ -110,9 +110,9 @@ end
 
 --RGB to Hex
 function E:RGBToHex(r, g, b)
-	r = r <= 1 and r >= 0 and r or 0
-	g = g <= 1 and g >= 0 and g or 0
-	b = b <= 1 and b >= 0 and b or 0
+	r = r <= 1 and r >= 0 and r or 1
+	g = g <= 1 and g >= 0 and g or 1
+	b = b <= 1 and b >= 0 and b or 1
 	return format("|cff%02x%02x%02x", r*255, g*255, b*255)
 end
 
