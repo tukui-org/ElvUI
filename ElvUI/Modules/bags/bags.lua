c21c3266912f36872fa37ec7244026529e78350f
Merathilis <neo0608@gmail.com>
2018-10-09 19:14:19 +0200

    Adjust the SellFrame statusbar a bit. :thinking:

--- ElvUI/Modules/bags/bags.lua
+++ ElvUI/Modules/bags/bags.lua
@@ -2106,7 +2106,13 @@ function B:CreateSellFrame()
 	B.SellFrame.statusbar:Size(180, 16)
 	B.SellFrame.statusbar:Point("BOTTOM", B.SellFrame, "BOTTOM", 0, 4)
 	B.SellFrame.statusbar:SetStatusBarTexture(E["media"].normTex)
+	B.SellFrame.statusbar:SetStatusBarColor(1, 0, 0)
 	B.SellFrame.statusbar:CreateBackdrop("Transparent")
+
+	B.SellFrame.statusbar.anim = CreateAnimationGroup(B.SellFrame.statusbar)
+	B.SellFrame.statusbar.anim.progress = B.SellFrame.statusbar.anim:CreateAnimation("Progress")
+	B.SellFrame.statusbar.anim.progress:SetSmoothing("Out")
+	B.SellFrame.statusbar.anim.progress:SetDuration(.3)
 
 	B.SellFrame.statusbar.ValueText = B.SellFrame.statusbar:CreateFontString(nil, "OVERLAY")
 	B.SellFrame.statusbar.ValueText:FontTemplate(nil, 12, "OUTLINE")
