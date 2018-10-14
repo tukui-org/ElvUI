3a21226c40d4feabe10f1cb6acc1c36b9f7dba13
Azilroka <darktemplar9900@yahoo.com>
2018-10-13 20:15:57 -0500

    Revert "UF: Health bg use the same texture as Health."
    This reverts commit 212664529c64a36e91984d12a3781f030c7188e8.

--- ElvUI/Modules/unitframes/elements/health.lua
+++ ElvUI/Modules/unitframes/elements/health.lua
@@ -145,8 +145,6 @@ function UF:Configure_HealthBar(frame)
 		health.bg:SetParent(frame.Portrait.overlay)
 	end
 
-	health.bg:SetTexture(E.LSM:Fetch('statusbar', E.private.general.normTex))
-
 	if db.health then
 		if db.health.reverseFill then
 			health:SetReverseFill(true)
