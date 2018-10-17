c13f747bbf37287ff71a33ea54e0664b3a2db0d1
Simpy <kodi03466@hotmail.com>
2018-10-17 14:28:23 -0400

    Revert "checking gettime"
    This reverts commit 70a8f0bdaa7de4bfda9dd19a98c0eec1fd30ff57.

--- ElvUI/core/core.lua
+++ ElvUI/core/core.lua
@@ -927,7 +927,7 @@ function E:SendMessage()
 	end
 
 	if simpyMeraTesting then
-		print('!! Sent Message !!', (GetTime()))
+		print('!! Sent Message !!')
 	end
 
 	SendMessageTimer = nil
@@ -945,7 +945,7 @@ local function SendRecieve(frame, event, prefix, message, derp, sender, ...)
 		if event ~= "LOADING_SCREEN_DISABLED" then
 			return
 		else
-			print('!! LOADING_SCREEN_DISABLED !!', (GetTime()))
+			print('!! LOADING_SCREEN_DISABLED !!')
 		end
 	end
 
@@ -1006,7 +1006,7 @@ function E:DelayedElvUIGVC()
 		if not inMaxChannels then
 			JoinPermanentChannel('ElvUIGVC', nil, nil, true)
 			if simpyMeraTesting then
-				print('!! Joined Channel !!', (GetTime()))
+				print('!! Joined Channel !!')
 			end
 		end
 	end
