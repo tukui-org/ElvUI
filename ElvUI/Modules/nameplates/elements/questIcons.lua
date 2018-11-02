e7e4290060321817d4816420375bd418391e3dff
Merathilis <neo0608@gmail.com>
2018-11-02 09:13:44 +0100

    This didnt work either. >.>

--- ElvUI/Modules/nameplates/elements/questIcons.lua
+++ ElvUI/Modules/nameplates/elements/questIcons.lua
@@ -125,13 +125,6 @@ function mod:UpdateElement_QuestIcon(frame)
 	local questIcon = frame.QuestIcon
 	local QuestList = self:GetQuests(frame.unit)
 
-	local scenarioName, currentStage, numStages, flags, _, _, _, xp, money, scenarioType, _, textureKitID = C_Scenario.GetInfo()
-	local inChallengeMode = (scenarioType == LE_SCENARIO_TYPE_CHALLENGE_MODE)
-
-	if inChallengeMode then
-		return
-	end
-
 	for i=1, #questIcon do
 		questIcon[i]:Hide()
 	end
