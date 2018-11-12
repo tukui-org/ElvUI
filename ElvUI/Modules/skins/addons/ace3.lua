6868c2483ca51c15060cd942ada8e6ee8758cd23
Merathilis <neo0608@gmail.com>
2018-11-12 08:37:39 +0100

    @Simpy lol :>

--- ElvUI/Modules/skins/addons/ace3.lua
+++ ElvUI/Modules/skins/addons/ace3.lua
@@ -47,16 +47,16 @@ function S:SkinAce3()
 
 			S:HandleButton(widget.button)
 			S:HandleScrollBar(widget.scrollBar)
-			widget.scrollBar:Point("RIGHT", frame, "RIGHT", 0 -4)
-			widget.scrollBG:Point("TOPRIGHT", widget.scrollBar, "TOPLEFT", -2, 19)
-			widget.scrollBG:Point("BOTTOMLEFT", widget.button, "TOPLEFT")
-			widget.scrollFrame:Point("BOTTOMRIGHT", widget.scrollBG, "BOTTOMRIGHT", -4, 8)
-		elseif TYPE == "CheckBox" then
+			widget.scrollBar:Point('RIGHT', frame, 'RIGHT', 0 -4)
+			widget.scrollBG:Point('TOPRIGHT', widget.scrollBar, 'TOPLEFT', -2, 19)
+			widget.scrollBG:Point('BOTTOMLEFT', widget.button, 'TOPLEFT')
+			widget.scrollFrame:Point('BOTTOMRIGHT', widget.scrollBG, 'BOTTOMRIGHT', -4, 8)
+		elseif TYPE == 'CheckBox' then
 			local check = widget.check
 			local checkbg = widget.checkbg
 			local highlight = widget.highlight
 
-			checkbg:CreateBackdrop("Default")
+			checkbg:CreateBackdrop('Default')
 			checkbg.backdrop:SetInside(widget.checkbg, 5, 5)
 			checkbg.backdrop:SetFrameLevel(widget.checkbg.backdrop:GetFrameLevel() + 1)
 
@@ -71,7 +71,7 @@ function S:SkinAce3()
 			checkbg.SetTexture = E.noop
 			highlight.SetTexture = E.noop
 			check.SetTexture = E.noop
-		elseif TYPE == "Dropdown" then
+		elseif TYPE == 'Dropdown' then
 			local frame = widget.dropdown
 			local button = widget.button
 			local button_cover = widget.button_cover
@@ -81,7 +81,7 @@ function S:SkinAce3()
 			S:HandleNextPrevButton(button, true)
 
 			if not frame.backdrop then
-				frame:CreateBackdrop("Default")
+				frame:CreateBackdrop('Default')
 			end
 
 			frame.backdrop:SetPoint('TOPLEFT', 15, -2)
@@ -95,14 +95,14 @@ function S:SkinAce3()
 			button:SetPoint('RIGHT', frame.backdrop, 'RIGHT', -2, 0)
 
 			text:ClearAllPoints()
-			text:SetJustifyH("RIGHT")
+			text:SetJustifyH('RIGHT')
 			text:SetPoint('RIGHT', button, 'LEFT', -3, 0)
 
 			button:SetParent(frame.backdrop)
 			text:SetParent(frame.backdrop)
 			button:HookScript('OnClick', SkinDropdownPullout)
 			button_cover:HookScript('OnClick', SkinDropdownPullout)
-		elseif TYPE == "LSM30_Font" or TYPE == "LSM30_Sound" or TYPE == "LSM30_Border" or TYPE == "LSM30_Background" or TYPE == "LSM30_Statusbar" then
+		elseif TYPE == 'LSM30_Font' or TYPE == 'LSM30_Sound' or TYPE == 'LSM30_Border' or TYPE == 'LSM30_Background' or TYPE == 'LSM30_Statusbar' then
 			local frame = widget.frame
 			local button = frame.dropButton
 			local text = frame.text
@@ -111,7 +111,7 @@ function S:SkinAce3()
 			S:HandleNextPrevButton(button, true)
 
 			if not frame.backdrop then
-				frame:CreateBackdrop("Default")
+				frame:CreateBackdrop('Default')
 			end
 
 			frame.label:ClearAllPoints()
@@ -125,7 +125,7 @@ function S:SkinAce3()
 			button:SetPoint('RIGHT', frame.backdrop, 'RIGHT', -2, 0)
 
 			frame.backdrop:SetPoint('TOPLEFT', 0, -21)
-			frame.backdrop:SetPoint("BOTTOMRIGHT", -4, -1)
+			frame.backdrop:SetPoint('BOTTOMRIGHT', -4, -1)
 
 			if TYPE == 'LSM30_Sound' then
 				widget.soundbutton:SetParent(frame.backdrop)
@@ -142,15 +142,15 @@ function S:SkinAce3()
 			button:SetParent(frame.backdrop)
 			text:SetParent(frame.backdrop)
 			button:HookScript('OnClick', SkinDropdownPullout)
-		elseif TYPE == "EditBox" then
+		elseif TYPE == 'EditBox' then
 			local frame = widget.editbox
 			local button = widget.button
 			S:HandleEditBox(frame)
 			S:HandleButton(button)
 
-			button:SetPoint("RIGHT", frame.backdrop, 'RIGHT', -2, 0)
+			button:SetPoint('RIGHT', frame.backdrop, 'RIGHT', -2, 0)
 
-			hooksecurefunc(frame, "SetPoint", function(self, a, b, c, d, e)
+			hooksecurefunc(frame, 'SetPoint', function(self, a, b, c, d, e)
 				if d == 7 then
 					self:SetPoint(a, b, c, 0, e)
 				end
@@ -158,14 +158,14 @@ function S:SkinAce3()
 
 			frame.backdrop:SetPoint('TOPLEFT', 0, -2)
 			frame.backdrop:SetPoint('BOTTOMRIGHT', -1, 0)
-		elseif (TYPE == "Button" or TYPE == "Button-ElvUI") then
+		elseif (TYPE == 'Button' or TYPE == 'Button-ElvUI') then
 			local frame = widget.frame
 			S:HandleButton(frame, nil, true)
 			frame:StripTextures()
 			frame:CreateBackdrop('Default', true)
 			frame.backdrop:SetInside()
 			widget.text:SetParent(frame.backdrop)
-		elseif TYPE == "Slider" then
+		elseif TYPE == 'Slider' then
 			local frame = widget.slider
 			local editbox = widget.editbox
 			local lowtext = widget.lowtext
@@ -175,11 +175,11 @@ function S:SkinAce3()
 
 			editbox:SetTemplate('Default')
 			editbox:Height(15)
-			editbox:Point("TOP", frame, "BOTTOM", 0, -1)
+			editbox:Point('TOP', frame, 'BOTTOM', 0, -1)
 
-			lowtext:Point("TOPLEFT", frame, "BOTTOMLEFT", 2, -2)
-			hightext:Point("TOPRIGHT", frame, "BOTTOMRIGHT", -2, -2)
-		elseif TYPE == "Keybinding" then
+			lowtext:Point('TOPLEFT', frame, 'BOTTOMLEFT', 2, -2)
+			hightext:Point('TOPRIGHT', frame, 'BOTTOMRIGHT', -2, -2)
+		elseif TYPE == 'Keybinding' then
 			local button = widget.button
 			local msgframe = widget.msgframe
 
@@ -191,13 +191,13 @@ function S:SkinAce3()
 			msgframe:StripTextures()
 			msgframe:SetTemplate('Transparent')
 			msgframe.msg:ClearAllPoints()
-			msgframe.msg:SetPoint("CENTER")
-		elseif (TYPE == "ColorPicker" or TYPE == "ColorPicker-ElvUI") then
+			msgframe.msg:SetPoint('CENTER')
+		elseif (TYPE == 'ColorPicker' or TYPE == 'ColorPicker-ElvUI') then
 			local frame = widget.frame
 			local colorSwatch = widget.colorSwatch
 
 			if not frame.backdrop then
-				frame:CreateBackdrop("Default")
+				frame:CreateBackdrop('Default')
 			end
 
 			frame.backdrop:SetSize(16, 16)
@@ -214,7 +214,7 @@ function S:SkinAce3()
 
 			if frame.checkers then
 				frame.checkers:ClearAllPoints()
-				frame.checkers:SetDrawLayer("ARTWORK")
+				frame.checkers:SetDrawLayer('ARTWORK')
 				frame.checkers:SetParent(frame.backdrop)
 				frame.checkers:SetInside(frame.backdrop)
 			end
@@ -229,24 +229,24 @@ function S:SkinAce3()
 			return oldRegisterAsContainer(self, widget)
 		end
 		local TYPE = widget.type
-		if TYPE == "ScrollFrame" then
+		if TYPE == 'ScrollFrame' then
 			S:HandleScrollBar(widget.scrollbar)
-		elseif TYPE == "InlineGroup" or TYPE == "TreeGroup" or TYPE == "TabGroup" or TYPE == "Frame" or TYPE == "DropdownGroup" or TYPE == "Window" then
+		elseif TYPE == 'InlineGroup' or TYPE == 'TreeGroup' or TYPE == 'TabGroup' or TYPE == 'Frame' or TYPE == 'DropdownGroup' or TYPE == 'Window' then
 			local frame = widget.content:GetParent()
-			if TYPE == "Frame" then
+			if TYPE == 'Frame' then
 				frame:StripTextures()
 				if not E.GUIFrame then
 					E.GUIFrame = frame
 				end
 				for i=1, frame:GetNumChildren() do
 					local child = select(i, frame:GetChildren())
-					if child:GetObjectType() == "Button" and child:GetText() then
+					if child:GetObjectType() == 'Button' and child:GetText() then
 						S:HandleButton(child)
 					else
 						child:StripTextures()
 					end
 				end
-			elseif TYPE == "Window" then
+			elseif TYPE == 'Window' then
 				frame:StripTextures()
 				S:HandleCloseButton(frame.obj.closebutton)
 			end
@@ -254,7 +254,7 @@ function S:SkinAce3()
 
 			if widget.treeframe then
 				widget.treeframe:SetTemplate('Transparent')
-				frame:Point("TOPLEFT", widget.treeframe, "TOPRIGHT", 1, 0)
+				frame:Point('TOPLEFT', widget.treeframe, 'TOPRIGHT', 1, 0)
 
 				local oldRefreshTree = widget.RefreshTree
 				widget.RefreshTree = function(self, scrollToSelection)
@@ -283,16 +283,16 @@ function S:SkinAce3()
 				end
 			end
 
-			if TYPE == "TabGroup" then
+			if TYPE == 'TabGroup' then
 				local oldCreateTab = widget.CreateTab
 				widget.CreateTab = function(self, id)
 					local tab = oldCreateTab(self, id)
 					tab:StripTextures()
-					tab.backdrop = CreateFrame("Frame", nil, tab)
-					tab.backdrop:SetTemplate("Transparent")
+					tab.backdrop = CreateFrame('Frame', nil, tab)
+					tab.backdrop:SetTemplate('Transparent')
 					tab.backdrop:SetFrameLevel(tab:GetFrameLevel() - 1)
-					tab.backdrop:Point("TOPLEFT", 10, -3)
-					tab.backdrop:Point("BOTTOMRIGHT", -10, 0)
+					tab.backdrop:Point('TOPLEFT', 10, -3)
+					tab.backdrop:Point('BOTTOMRIGHT', -10, 0)
 					return tab
 				end
 			end
@@ -300,9 +300,9 @@ function S:SkinAce3()
 			if widget.scrollbar then
 				S:HandleScrollBar(widget.scrollbar)
 			end
-		elseif TYPE == "SimpleGroup" then
+		elseif TYPE == 'SimpleGroup' then
 			local frame = widget.content:GetParent()
-			frame:SetTemplate("Transparent", nil, true) --ignore border updates
+			frame:SetTemplate('Transparent', nil, true) --ignore border updates
 			frame:SetBackdropBorderColor(0,0,0,0) --Make border completely transparent
 		end
 
