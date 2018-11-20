35edcf1e25cfece9ce1a8563901a54d681928b61
Merathilis <neo0608@gmail.com>
2018-11-20 10:50:07 +0100

    Damn blizzard bug.

--- ElvUI/Modules/blizzard/blizzard.lua
+++ ElvUI/Modules/blizzard/blizzard.lua
@@ -30,11 +30,15 @@ function B:Initialize()
 
 	E:CreateMover(LossOfControlFrame, 'LossControlMover', L["Loss Control Icon"])
 
+	-- Quick Join Bug
 	CreateFrame("Frame"):SetScript("OnUpdate", function(self)
 		if LFRBrowseFrame.timeToClear then
 			LFRBrowseFrame.timeToClear = nil
 		end
 	end)
+
+	-- Fix Guild Set Rank Error introduced in Patch 27326
+	GuildControlUIRankSettingsFrameRosterLabel = CreateFrame("Frame", nil, E.HiddenFrame)
 
 	-- MicroButton Talent Alert
 	if TalentMicroButtonAlert then -- why do we need to check this?
