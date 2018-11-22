7588b469b2ae616b472b4cdd6df1301e78dc682e
Michael James <darktemplar9900@yahoo.com>
2018-11-22 04:16:01 -0600

    Here Darth

--- ElvUI/Modules/skins/addons/ace3.lua
+++ ElvUI/Modules/skins/addons/ace3.lua
@@ -60,7 +60,7 @@ function S:SkinAce3()
 			checkbg.backdrop:SetInside(widget.checkbg, 5, 5)
 			checkbg.backdrop:SetFrameLevel(widget.checkbg.backdrop:GetFrameLevel() + 1)
 
-			check:SetTexture(E.media.blankTex)
+			check:SetTexture([[Interface\AddOns\ElvUI\media\textures\normTex]])
 			check:SetVertexColor(unpack(E.media.rgbvaluecolor))
 
 			checkbg:SetTexture('')
