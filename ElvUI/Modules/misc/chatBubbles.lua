395d6d723d7f085e321143a52a0a84194051215d
Azilroka <darktemplar9900@yahoo.com>
2018-12-13 21:45:37 -0600

    ChatBubbles Pixel Perfection

--- ElvUI/Modules/misc/chatBubbles.lua
+++ ElvUI/Modules/misc/chatBubbles.lua
@@ -93,7 +93,7 @@ end
 
 function M:SkinBubble(frame)
 	if frame:IsForbidden() then return end
-	local mult = E.mult * UIParent:GetEffectiveScale()
+	local mult = E.mult * tonumber(string.sub(UIParent:GetEffectiveScale(), 0, 7))
 	for i = 1, frame:GetNumRegions() do
 		local region = select(i, frame:GetRegions())
 		if region:GetObjectType() == "Texture" then
