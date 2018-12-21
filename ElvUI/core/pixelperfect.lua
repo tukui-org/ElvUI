bdb82ed7f9906057379a3d46ef841c63243f2135
Simpy <kodi03466@hotmail.com>
2018-12-20 19:21:04 -0500

    5th

--- ElvUI/core/pixelperfect.lua
+++ ElvUI/core/pixelperfect.lua
@@ -15,7 +15,7 @@ local GetCVar, SetCVar = GetCVar, SetCVar
 function E:UIScale(event, loginFrame)
 	local UIParent = _G.UIParent
 	local width, height = E.screenwidth, E.screenheight
-	local magic = E:Round(768/height, 10)
+	local magic = E:Round(768/height, 5)
 
 	local uiScaleCVar = GetCVar('uiScale')
 	if uiScaleCVar then
