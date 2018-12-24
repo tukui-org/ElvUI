c3188027a5b60f2d22191356e88b082cee56f177
Merathilis <neo0608@gmail.com>
2018-12-24 12:03:50 +0100

    Slightly adjust the FrameLevel for rewards on the Garrison skin.

--- ElvUI/Modules/skins/blizzard/garrison.lua
+++ ElvUI/Modules/skins/blizzard/garrison.lua
@@ -23,6 +23,7 @@ local function LoadSkin()
 
 			reward:ClearAllPoints()
 			reward:SetPoint("TOPRIGHT", -E.mult + (index * -65), -E.mult)
+			reward:SetFrameLevel(5)
 
 			if reward.IconBorder then
 				reward.IconBorder:SetTexture(nil)
