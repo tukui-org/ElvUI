421a47074d58cc289990389faa880eeff77ba8f0
Merathilis <neo0608@gmail.com>
2018-12-28 14:02:57 +0100

    Added parchment support for AlliedRaces skin.

--- ElvUI/Modules/skins/blizzard/AlliedRaces.lua
+++ ElvUI/Modules/skins/blizzard/AlliedRaces.lua
@@ -11,36 +11,69 @@ local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.AlliedRaces ~= true then return end
 
 	local AlliedRacesFrame = _G.AlliedRacesFrame
-	S:HandlePortraitFrame(AlliedRacesFrame, true)
-	select(2, AlliedRacesFrame.ModelFrame:GetRegions()):Hide()
 
-	local scrollFrame = AlliedRacesFrame.RaceInfoFrame.ScrollFrame
-	scrollFrame.ScrollBar.Border:Hide()
-	scrollFrame.ScrollBar.ScrollUpBorder:Hide()
-	scrollFrame.ScrollBar.ScrollDownBorder:Hide()
-	S:HandleScrollSlider(scrollFrame.ScrollBar)
+	if E.private.skins.parchmentRemover.enable then
+		S:HandlePortraitFrame(AlliedRacesFrame, true)
+		select(2, AlliedRacesFrame.ModelFrame:GetRegions()):Hide()
 
-	scrollFrame.Child.ObjectivesFrame:StripTextures()
-	scrollFrame.Child.ObjectivesFrame:CreateBackdrop("Transparent")
+		local scrollFrame = AlliedRacesFrame.RaceInfoFrame.ScrollFrame
+		scrollFrame.ScrollBar.Border:Hide()
+		scrollFrame.ScrollBar.ScrollUpBorder:Hide()
+		scrollFrame.ScrollBar.ScrollDownBorder:Hide()
+		S:HandleScrollSlider(scrollFrame.ScrollBar)
 
-	AlliedRacesFrame.RaceInfoFrame.AlliedRacesRaceName:SetTextColor(1, .8, 0)
-	scrollFrame.Child.RaceDescriptionText:SetTextColor(1, 1, 1)
-	scrollFrame.Child.RacialTraitsLabel:SetTextColor(1, .8, 0)
+		scrollFrame.Child.ObjectivesFrame:StripTextures()
+		scrollFrame.Child.ObjectivesFrame:CreateBackdrop("Transparent")
 
-	AlliedRacesFrame:HookScript("OnShow", function(self)
-		local parent = scrollFrame.Child
-		for i = 1, parent:GetNumChildren() do
-			local bu = select(i, parent:GetChildren())
+		AlliedRacesFrame.RaceInfoFrame.AlliedRacesRaceName:SetTextColor(1, .8, 0)
+		scrollFrame.Child.RaceDescriptionText:SetTextColor(1, 1, 1)
+		scrollFrame.Child.RacialTraitsLabel:SetTextColor(1, .8, 0)
 
-			if bu.Icon and not bu.IsSkinned then
-				select(3, bu:GetRegions()):Hide()
-				S:HandleTexture(bu.Icon, bu)
-				bu.Text:SetTextColor(1, 1, 1)
+		AlliedRacesFrame:HookScript("OnShow", function(self)
+			local parent = scrollFrame.Child
+			for i = 1, parent:GetNumChildren() do
+				local bu = select(i, parent:GetChildren())
 
-				bu.IsSkinned = true
+				if bu.Icon and not bu.IsSkinned then
+					select(3, bu:GetRegions()):Hide()
+					S:HandleTexture(bu.Icon, bu)
+					bu.Text:SetTextColor(1, 1, 1)
+
+					bu.IsSkinned = true
+				end
 			end
-		end
-	end)
+		end)
+	else
+		AlliedRacesFrame.NineSlice:SetAlpha(0)
+		AlliedRacesFramePortrait:SetAlpha(0)
+		AlliedRacesFrameBg:SetAlpha(0)
+		AlliedRacesFrame.TitleBg:SetAlpha(0)
+		AlliedRacesFrame.ModelFrame:StripTextures()
+
+		AlliedRacesFrame:CreateBackdrop("Transparent")
+
+		local scrollFrame = AlliedRacesFrame.RaceInfoFrame.ScrollFrame
+		scrollFrame.ScrollBar.Border:Hide()
+		scrollFrame.ScrollBar.ScrollUpBorder:Hide()
+		scrollFrame.ScrollBar.ScrollDownBorder:Hide()
+		S:HandleScrollSlider(scrollFrame.ScrollBar)
+
+		S:HandleCloseButton(AlliedRacesFrameCloseButton)
+
+		AlliedRacesFrame:HookScript("OnShow", function(self)
+			local parent = scrollFrame.Child
+			for i = 1, parent:GetNumChildren() do
+				local bu = select(i, parent:GetChildren())
+
+				if bu.Icon and not bu.IsSkinned then
+					select(3, bu:GetRegions()):Hide()
+					S:HandleTexture(bu.Icon, bu)
+
+					bu.IsSkinned = true
+				end
+			end
+		end)
+	end
 end
 
 S:AddCallbackForAddon("Blizzard_AlliedRacesUI", "AlliedRaces", LoadSkin)
