34a5bbf7a6cf3244ff13a39bdf9f0f063f1f3c05
Azilroka <darktemplar9900@yahoo.com>
2019-01-03 22:25:40 -0600

    NP Color Config

--- ElvUI_Config/nameplates.lua
+++ ElvUI_Config/nameplates.lua
@@ -3095,22 +3095,6 @@ E.Options.args.nameplate = {
 									type = 'description',
 									name = ' ',
 								},
-								glowColor = {
-									name = L["Target Indicator Color"],
-									type = 'color',
-									order = 5,
-									hasAlpha = true,
-									get = function(info)
-										local t = E.db.nameplates.glowColor
-										local d = P.nameplates.glowColor
-										return t.r, t.g, t.b, t.a, d.r, d.g, d.b, d.a
-									end,
-									set = function(info, r, g, b, a)
-										local t = E.db.nameplates.glowColor
-										t.r, t.g, t.b, t.a = r, g, b, a
-										NP:ConfigureAll()
-									end,
-								},
 								targetGlow = {
 									order = 6,
 									type = "select",
@@ -3162,6 +3146,203 @@ E.Options.args.nameplate = {
 									type = "toggle",
 									name = L["Enemy"],
 									set = function(info, value) E.db.nameplates.clickThrough.enemy = value; NP:SetNamePlateEnemyClickThrough() end,
+								},
+							},
+						},
+					},
+				},
+				colorsGroup ={
+					type = 'group',
+					name = COLORS,
+					args = {
+						general = {
+							order = 1,
+							type = "group",
+							name = L["General"],
+							guiInline = true,
+							get = function(info)
+								local t = E.db.nameplates.colors[info[#info]]
+								local d = P.nameplates.colors[info[#info]]
+								return t.r, t.g, t.b, t.a, d.r, d.g, d.b, d.a
+							end,
+							set = function(info, r, g, b, a)
+								local t = E.db.nameplates.colors[info[#info]]
+								t.r, t.g, t.b, t.a = r, g, b, a
+								NP:ConfigureAll()
+							end,
+							args = {
+								glow = {
+									name = L["Target Indicator Color"],
+									type = 'color',
+									order = 5,
+									hasAlpha = true,
+								},
+							},
+						},
+						threat = {
+							order = 150,
+							type = "group",
+							name = L["Threat"],
+							guiInline = true,
+							get = function(info)
+								local t = E.db.nameplates.colors.threat[info[#info]]
+								local d = P.nameplates.colors.threat[info[#info]]
+								return t.r, t.g, t.b, t.a, d.r, d.g, d.b, d.a
+							end,
+							set = function(info, r, g, b, a)
+								local t = E.db.nameplates.colors[info[#info]]
+								t.r, t.g, t.b, t.a = r, g, b, a
+								NP:ConfigureAll()
+							end,
+							args = {
+								beingTankedByTankColor = {
+									name = L["Tanked Color"],
+									order = 5,
+									type = 'color',
+									hasAlpha = false,
+									disabled = function() return (not E.db.nameplates.threat.beingTankedByTank or not E.db.nameplates.threat.useThreatColor) end,
+								},
+								goodColor = {
+									type = "color",
+									order = 7,
+									name = L["Good Color"],
+									hasAlpha = false,
+									disabled = function() return not E.db.nameplates.threat.useThreatColor end,
+								},
+								badColor = {
+									name = L["Bad Color"],
+									order = 8,
+									type = 'color',
+									hasAlpha = false,
+									disabled = function() return not E.db.nameplates.threat.useThreatColor end,
+								},
+								goodTransition = {
+									type = "color",
+									order = 10,
+									name = L["Good Transition Color"],
+									hasAlpha = false,
+									disabled = function() return not E.db.nameplates.threat.useThreatColor end,
+								},
+								badTransition = {
+									name = L["Bad Transition Color"],
+									order = 11,
+									type = 'color',
+									hasAlpha = false,
+									disabled = function() return not E.db.nameplates.threat.useThreatColor end,
+								},
+							},
+						},
+						castGroup = {
+							order = 175,
+							type = "group",
+							name = L["Cast Bar"],
+							guiInline = true,
+							get = function(info)
+								local t = E.db.nameplates.colors[ info[#info] ]
+								local d = P.nameplates.colors[info[#info]]
+								return t.r, t.g, t.b, t.a, d.r, d.g, d.b
+							end,
+							set = function(info, r, g, b)
+								local t = E.db.nameplates.colors[ info[#info] ]
+								t.r, t.g, t.b = r, g, b
+								NP:ConfigureAll()
+							end,
+							args = {
+								cast = {
+									type = "color",
+									order = 1,
+									name = L["Cast Color"],
+									hasAlpha = false,
+								},
+								castNoInterrupt = {
+									name = L["Cast No Interrupt Color"],
+									order = 2,
+									type = 'color',
+									hasAlpha = false,
+								},
+							},
+						},
+						reactions = {
+							order = 200,
+							type = "group",
+							name = L["Reaction Colors"],
+							guiInline = true,
+							get = function(info)
+								local t = E.db.nameplates.colors.reactions[ info[#info] ]
+								local d = P.nameplates.colors.reactions[info[#info]]
+								return t.r, t.g, t.b, t.a, d.r, d.g, d.b
+							end,
+							set = function(info, r, g, b)
+								local t = E.db.nameplates.colors.reactions[ info[#info] ]
+								t.r, t.g, t.b = r, g, b
+								NP:ConfigureAll()
+							end,
+							args = {
+								bad = {
+									name = L["Enemy"],
+									order = 1,
+									type = 'color',
+									hasAlpha = false,
+								},
+								neutral = {
+									name = L["Neutral"],
+									order = 2,
+									type = 'color',
+									hasAlpha = false,
+								},
+								good = {
+									name = L["Friendly"],
+									order = 4,
+									type = 'color',
+									hasAlpha = false,
+								},
+								tapped = {
+									name = L["Tagged NPC"],
+									order = 5,
+									type = 'color',
+									hasAlpha = false,
+								},
+							},
+						},
+						healPrediction = {
+							order = 225,
+							name = L["Heal Prediction"],
+							type = 'group',
+							guiInline = true,
+							get = function(info)
+								local t = E.db.nameplates.colors.healPrediction[ info[#info] ]
+								local d = P.nameplates.colors.healPrediction[ info[#info] ]
+								return t.r, t.g, t.b, t.a, d.r, d.g, d.b, d.a
+							end,
+							set = function(info, r, g, b, a)
+								local t = E.db.nameplates.colors.healPrediction[ info[#info] ]
+								t.r, t.g, t.b, t.a = r, g, b, a
+								NP:ConfigureAll()
+							end,
+							args = {
+								personal = {
+									order = 1,
+									name = L["Personal"],
+									type = 'color',
+									hasAlpha = true,
+								},
+								others = {
+									order = 2,
+									name = L["Others"],
+									type = 'color',
+									hasAlpha = true,
+								},
+								absorbs = {
+									order = 4,
+									name = L["Absorbs"],
+									type = 'color',
+									hasAlpha = true,
+								},
+								healAbsorbs = {
+									order = 5,
+									name = L["Heal Absorbs"],
+									type = 'color',
+									hasAlpha = true,
 								},
 							},
 						},
@@ -3390,182 +3571,6 @@ E.Options.args.nameplate = {
 							get = function(info) return E.db.nameplates.threat[ info[#info] ] end,
 							set = function(info, value) E.db.nameplates.threat[ info[#info] ] = value; end,
 							disabled = function() return not E.db.nameplates.threat.useThreatColor end,
-						},
-						beingTankedByTankColor = {
-							name = L["Tanked Color"],
-							order = 5,
-							type = 'color',
-							hasAlpha = false,
-							disabled = function() return (not E.db.nameplates.threat.beingTankedByTank or not E.db.nameplates.threat.useThreatColor) end,
-						},
-						spacer1 = {
-							order = 6,
-							type = 'description',
-							name = '',
-						},
-						goodColor = {
-							type = "color",
-							order = 7,
-							name = L["Good Color"],
-							hasAlpha = false,
-							disabled = function() return not E.db.nameplates.threat.useThreatColor end,
-						},
-						badColor = {
-							name = L["Bad Color"],
-							order = 8,
-							type = 'color',
-							hasAlpha = false,
-							disabled = function() return not E.db.nameplates.threat.useThreatColor end,
-						},
-						spacer2 = {
-							order = 9,
-							type = 'description',
-							name = '',
-						},
-						goodTransition = {
-							type = "color",
-							order = 10,
-							name = L["Good Transition Color"],
-							hasAlpha = false,
-							disabled = function() return not E.db.nameplates.threat.useThreatColor end,
-						},
-						badTransition = {
-							name = L["Bad Transition Color"],
-							order = 11,
-							type = 'color',
-							hasAlpha = false,
-							disabled = function() return not E.db.nameplates.threat.useThreatColor end,
-						},
-					},
-				},
-				castGroup = {
-					order = 175,
-					type = "group",
-					name = L["Cast Bar"],
-					get = function(info)
-						local t = E.db.nameplates[ info[#info] ]
-						local d = P.nameplates[info[#info]]
-						return t.r, t.g, t.b, t.a, d.r, d.g, d.b
-					end,
-					set = function(info, r, g, b)
-						local t = E.db.nameplates[ info[#info] ]
-						t.r, t.g, t.b = r, g, b
-						NP:ForEachPlate("ConfigureElement_CastBar")
-					end,
-					args = {
-						castColor = {
-							type = "color",
-							order = 1,
-							name = L["Cast Color"],
-							hasAlpha = false,
-						},
-						castNoInterruptColor = {
-							name = L["Cast No Interrupt Color"],
-							order = 2,
-							type = 'color',
-							hasAlpha = false,
-						},
-					},
-				},
-				reactions = {
-					order = 200,
-					type = "group",
-					name = L["Reaction Colors"],
-					get = function(info)
-						local t = E.db.nameplates.reactions[ info[#info] ]
-						local d = P.nameplates.reactions[info[#info]]
-						return t.r, t.g, t.b, t.a, d.r, d.g, d.b
-					end,
-					set = function(info, r, g, b)
-						local t = E.db.nameplates.reactions[ info[#info] ]
-						t.r, t.g, t.b = r, g, b
-						NP:ForEachPlate("UpdateElement_HealthColor", true)
-						NP:ForEachPlate("UpdateElement_Name", true)
-					end,
-					args = {
-						--[[offline = {
-							type = "color",
-							order = 0,
-							name = L["Offline"],
-							hasAlpha = false,
-						},]]
-						bad = {
-							name = L["Enemy"],
-							order = 1,
-							type = 'color',
-							hasAlpha = false,
-						},
-						neutral = {
-							name = L["Neutral"],
-							order = 2,
-							type = 'color',
-							hasAlpha = false,
-						},
-						spacer1 = {
-							order = 3,
-							type = 'description',
-							name = '',
-						},
-						good = {
-							name = L["Friendly"],
-							order = 4,
-							type = 'color',
-							hasAlpha = false,
-						},
-						tapped = {
-							name = L["Tagged NPC"],
-							order = 5,
-							type = 'color',
-							hasAlpha = false,
-						},
-					},
-				},
-				healPrediction = {
-					order = 225,
-					name = L["Heal Prediction"],
-					type = 'group',
-					get = function(info)
-						local t = E.db.nameplates.healPrediction[ info[#info] ]
-						local d = P.nameplates.healPrediction[ info[#info] ]
-						return t.r, t.g, t.b, t.a, d.r, d.g, d.b, d.a
-					end,
-					set = function(info, r, g, b, a)
-						local t = E.db.nameplates.healPrediction[ info[#info] ]
-						t.r, t.g, t.b, t.a = r, g, b, a
-						NP:ForEachPlate("ConfigureElement_HealthBar", true)
-						if NP.PlayerFrame__ and NP.PlayerFrame__.unitFrame and NP.PlayerFrame__.unitFrame.UnitType then
-							NP:ConfigureElement_HealthBar(NP.PlayerFrame__.unitFrame, true)
-						end
-					end,
-					args = {
-						personal = {
-							order = 1,
-							name = L["Personal"],
-							type = 'color',
-							hasAlpha = true,
-						},
-						others = {
-							order = 2,
-							name = L["Others"],
-							type = 'color',
-							hasAlpha = true,
-						},
-						spacer1 = {
-							order = 3,
-							type = 'description',
-							name = '',
-						},
-						absorbs = {
-							order = 4,
-							name = L["Absorbs"],
-							type = 'color',
-							hasAlpha = true,
-						},
-						healAbsorbs = {
-							order = 5,
-							name = L["Heal Absorbs"],
-							type = 'color',
-							hasAlpha = true,
 						},
 					},
 				},
