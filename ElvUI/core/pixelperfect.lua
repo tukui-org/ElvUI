fd6b5471bf6505c9d1a9cb8b2151b1a1f0b40ccb
Simpy <kodi03466@hotmail.com>
2019-02-10 02:57:02 -0500

    we should be able to directly call this api now for scale multiplier

--- ElvUI/core/pixelperfect.lua
+++ ElvUI/core/pixelperfect.lua
@@ -5,27 +5,28 @@ local _G = _G
 --Lua functions
 local abs, floor = math.abs, math.floor
 --WoW API / Variables
+local PixelUtil_GetNearestPixelSize = PixelUtil.GetNearestPixelSize
 
 function E:SetResolutionVariables(width, height)
 	if E.global.general.eyefinity and width >= 3840 then
-		-- because some user enable bezel compensation, we need to find the real width of a single monitor.
-		-- I don't know how it really work, but i'm assuming they add pixel to width to compensate the bezel. :P
+		--because some user enable bezel compensation, we need to find the real width of a single monitor.
+		--I don't know how it really work, but i'm assuming they add pixel to width to compensate the bezel. :P
 
-		-- HQ resolution
-		if width >= 9840 then width = 3280; end							-- WQSXGA
-		if width >= 7680 and width < 9840 then width = 2560; end		-- WQXGA
-		if width >= 5760 and width < 7680 then width = 1920; end		-- WUXGA & HDTV
-		if width >= 5040 and width < 5760 then width = 1680; end		-- WSXGA+
+		--HQ resolution
+		if width >= 9840 then width = 3280; end							--WQSXGA
+		if width >= 7680 and width < 9840 then width = 2560; end		--WQXGA
+		if width >= 5760 and width < 7680 then width = 1920; end		--WUXGA & HDTV
+		if width >= 5040 and width < 5760 then width = 1680; end		--WSXGA+
 
-		-- adding height condition here to be sure it work with bezel compensation because WSXGA+ and UXGA/HD+ got approx same width
-		if width >= 4800 and width < 5760 and height == 900 then width = 1600; end	-- UXGA & HD+
+		--adding height condition here to be sure it work with bezel compensation because WSXGA+ and UXGA/HD+ got approx same width
+		if width >= 4800 and width < 5760 and height == 900 then width = 1600; end	--UXGA & HD+
 
-		-- low resolution screen
-		if width >= 4320 and width < 4800 then width = 1440; end		-- WSXGA
-		if width >= 4080 and width < 4320 then width = 1360; end		-- WXGA
-		if width >= 3840 and width < 4080 then width = 1224; end		-- SXGA & SXGA (UVGA) & WXGA & HDTV
+		--low resolution screen
+		if width >= 4320 and width < 4800 then width = 1440; end		--WSXGA
+		if width >= 4080 and width < 4320 then width = 1360; end		--WXGA
+		if width >= 3840 and width < 4080 then width = 1224; end		--SXGA & SXGA (UVGA) & WXGA & HDTV
 
-		-- register a constant, we will need it later for launch.lua
+		--register a constant, we will need it later for launch.lua
 		E.eyefinity = width
 	end
 end
@@ -37,25 +38,22 @@ function E:UIScale()
 	local scale = E.global.general.UIScale
 	UIParent:SetScale(scale)
 
-	local width, height = E.screenwidth, E.screenheight
-	local effectiveScale = UIParent:GetEffectiveScale()
-	local pixelScale = 768.0 / height
-
-	E.mult = (effectiveScale / scale) - ((effectiveScale - pixelScale) / scale)
+	E.mult = PixelUtil_GetNearestPixelSize(1, scale)
 	E.Spacing = (E.PixelMode and 0) or E.mult
 	E.Border = (E.PixelMode and E.mult) or E.mult*2
 
 	--Check if we are using `E.eyefinity`
+	local width, height = E.screenwidth, E.screenheight
 	E:SetResolutionVariables(width, height)
 
 	--Resize E.UIParent if Eyefinity is on.
 	local testingEyefinity = false
 	if testingEyefinity then
-		-- Eyefinity Test: Resize the E.UIParent to be smaller than it should be, all objects inside should relocate.
-		-- Dragging moveable frames outside the box and reloading the UI ensures that they are saving position correctly.
+		--Eyefinity Test: Resize the E.UIParent to be smaller than it should be, all objects inside should relocate.
+		--Dragging moveable frames outside the box and reloading the UI ensures that they are saving position correctly.
 		width, height = UIParent:GetWidth() - 250, UIParent:GetHeight() - 250
 	elseif E.eyefinity and height > 1200 then
-		-- find a new width value of E.UIParent for screen #1.
+		--find a new width value of E.UIParent for screen #1.
 		local uiHeight = UIParent:GetHeight()
 		width, height = E.eyefinity / (height / uiHeight), uiHeight
 	else
@@ -74,7 +72,7 @@ function E:UIScale()
 	E.diffGetBottom = E:Round(abs(UIParent:GetBottom() - E.UIParent:GetBottom()))
 end
 
--- pixel perfect script of custom ui scale.
+--pixel perfect script of custom ui scale.
 function E:Scale(x)
 	return E.mult * floor(x/E.mult+.5)
 end
