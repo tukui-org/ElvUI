3431e181d8ea027bc37c0e78f0a8b7c4c0189bd2
Elv <elv@tukui.org>
2019-02-17 11:39:42 -0500

    Revert "workaround blizz mount tooltip bug (duplicate lines)"
    This reverts commit 6e956cafbf29812033d2f5d2cde77911c8d4f74b.

--- ElvUI/Modules/tooltip/tooltip.lua
+++ ElvUI/Modules/tooltip/tooltip.lua
@@ -84,7 +84,6 @@ local TAPPED_COLOR = { r=.6, g=.6, b=.6 }
 local AFK_LABEL = " |cffFFFFFF[|r|cffFF0000"..L["AFK"].."|r|cffFFFFFF]|r"
 local DND_LABEL = " |cffFFFFFF[|r|cffFFFF00"..L["DND"].."|r|cffFFFFFF]|r"
 local keybindFrame
-local mountLines = {}
 
 local classification = {
 	worldboss = format("|cffAF5050 %s|r", _G.BOSS),
@@ -464,9 +463,9 @@ function TT:GameTooltip_OnTooltipSetUnit(tt)
 					for x in gmatch(sourceModified, '[^\10]+\10?') do
 						local left, right = strmatch(x, '(.-|r)%s?([^\10]+)\10?')
 						if left and right then
-							mountLines[left] = right
+							tt:AddDoubleLine(left, right, nil, nil, nil, 1, 1, 1)
 						else
-							mountLines[_G.FROM] = sourceText:gsub('|c%x%x%x%x%x%x%x%x','')
+							tt:AddDoubleLine(_G.FROM, sourceText:gsub('|c%x%x%x%x%x%x%x%x',''), nil, nil, nil, 1, 1, 1)
 						end
 					end
 				end
@@ -474,12 +473,6 @@ function TT:GameTooltip_OnTooltipSetUnit(tt)
 				break
 			end
 		end
-
-		--fixes blizz duplicate line bug
-		for leftLine, rightLine in pairs(mountLines) do
-			tt:AddDoubleLine(leftLine, rightLine, nil, nil, nil, 1, 1, 1)
-		end
-		wipe(mountLines)
 	end
 
 	if not isShiftKeyDown and not isControlKeyDown then
