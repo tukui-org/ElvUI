edb659aa8e938137187a1a9709426469f6bdfc21
Azilroka <darktemplar9900@yahoo.com>
2019-02-24 11:09:47 -0600

    Readd NP

---
+++ ElvUI/Modules/Nameplates/Elements/Plugins.lua
@@ -0,0 +1,220 @@
+local E, L, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+
+local NP = E:GetModule('NamePlates')
+
+function NP:Construct_QuestIcons(nameplate)
+	local QuestIcons = CreateFrame('Frame', nameplate:GetDebugName()..'QuestIcons', nameplate)
+	QuestIcons:Hide()
+	QuestIcons:SetSize(NP.db.questIconSize + 4, NP.db.questIconSize + 4)
+
+	for _, object in pairs({'Item', 'Loot', 'Skull', 'Chat'}) do
+		QuestIcons[object] = QuestIcons:CreateTexture(nil, 'BORDER', nil, 1)
+		QuestIcons[object]:SetPoint('CENTER')
+		QuestIcons[object]:SetSize(NP.db.questIconSize, NP.db.questIconSize)
+		QuestIcons[object]:Hide()
+	end
+
+	QuestIcons.Item:SetTexCoord(unpack(E.TexCoords))
+
+	QuestIcons.Skull:SetSize(NP.db.questIconSize + 4, NP.db.questIconSize + 4)
+
+	QuestIcons.Chat:SetSize(NP.db.questIconSize + 4, NP.db.questIconSize + 4)
+	QuestIcons.Chat:SetTexture([[Interface\WorldMap\ChatBubble_64.PNG]])
+	QuestIcons.Chat:SetTexCoord(0, 0.5, 0.5, 1)
+
+	QuestIcons.Text = QuestIcons:CreateFontString(nil, 'OVERLAY')
+	QuestIcons.Text:SetPoint('BOTTOMRIGHT', QuestIcons, 'BOTTOMRIGHT', 2, -0.8)
+	QuestIcons.Text:SetFont(E.Libs.LSM:Fetch('font', NP.db.font), NP.db.fontSize, NP.db.fontOutline)
+
+	return QuestIcons
+end
+
+function NP:Update_QuestIcons(nameplate)
+	if NP.db.questIcon and (nameplate.frameType == 'FRIENDLY_NPC' or nameplate.frameType == 'ENEMY_NPC') then
+		if not nameplate:IsElementEnabled('QuestIcons') then
+			nameplate:EnableElement('QuestIcons')
+		end
+		nameplate.QuestIcons:ClearAllPoints()
+		nameplate.QuestIcons:SetPoint('LEFT', nameplate, 'RIGHT', 4, 0)
+		nameplate.QuestIcons:SetSize(NP.db.questIconSize + 4, NP.db.questIconSize + 4)
+		nameplate.QuestIcons.Item:SetSize(NP.db.questIconSize, NP.db.questIconSize)
+		nameplate.QuestIcons.Loot:SetSize(NP.db.questIconSize, NP.db.questIconSize)
+		nameplate.QuestIcons.Skull:SetSize(NP.db.questIconSize + 4, NP.db.questIconSize + 4)
+		nameplate.QuestIcons.Chat:SetSize(NP.db.questIconSize + 4, NP.db.questIconSize + 4)
+	else
+		if nameplate:IsElementEnabled('QuestIcons') then
+			nameplate:DisableElement('QuestIcons')
+		end
+	end
+end
+
+function NP:Construct_ClassificationIndicator(nameplate)
+	local ClassificationIndicator = nameplate:CreateTexture(nil, 'OVERLAY')
+
+	return ClassificationIndicator
+end
+
+function NP:Update_ClassificationIndicator(nameplate)
+	local db = NP.db.units[nameplate.frameType]
+
+	if (nameplate.frameType == 'FRIENDLY_NPC' or nameplate.frameType == 'ENEMY_NPC') and db.eliteIcon.enable then
+		if not nameplate:IsElementEnabled('ClassificationIndicator') then
+			nameplate:EnableElement('ClassificationIndicator')
+		end
+
+		nameplate.ClassificationIndicator:ClearAllPoints()
+		nameplate.ClassificationIndicator:SetSize(db.eliteIcon.size, db.eliteIcon.size)
+
+		if db.health.enable then
+			nameplate.ClassificationIndicator:Point(db.eliteIcon.position, nameplate.Health, db.eliteIcon.position, db.eliteIcon.xOffset, db.eliteIcon.yOffset)
+		else
+			nameplate.ClassificationIndicator:Point('RIGHT', nameplate.Name, 'LEFT', 0, 0)
+		end
+	else
+		if nameplate:IsElementEnabled('ClassificationIndicator') then
+			nameplate:DisableElement('ClassificationIndicator')
+		end
+	end
+end
+
+function NP:Construct_TargetIndicator(nameplate)
+	local TargetIndicator = CreateFrame('Frame', nameplate:GetDebugName()..'TargetIndicator', nameplate)
+
+	TargetIndicator.Shadow = CreateFrame('Frame', nil, TargetIndicator)
+	TargetIndicator.Shadow:SetBackdrop({edgeFile = E.LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(5)})
+	TargetIndicator.Shadow:Hide()
+
+	for _, object in pairs({'Spark', 'TopIndicator', 'LeftIndicator', 'RightIndicator'}) do
+		TargetIndicator[object] = TargetIndicator:CreateTexture(nil, 'BACKGROUND', nil, -5)
+		TargetIndicator[object]:SetSnapToPixelGrid(false)
+		TargetIndicator[object]:SetTexelSnappingBias(0)
+		TargetIndicator[object]:Hide()
+	end
+
+	TargetIndicator.Spark:SetTexture([[Interface\AddOns\ElvUI\media\textures\spark]])
+	TargetIndicator.TopIndicator:SetTexture([[Interface\AddOns\ElvUI\media\textures\nameplateTargetIndicator]])
+	TargetIndicator.LeftIndicator:SetTexture([[Interface\AddOns\ElvUI\media\textures\nameplateTargetIndicatorLeft]])
+	TargetIndicator.RightIndicator:SetTexture([[Interface\AddOns\ElvUI\media\textures\nameplateTargetIndicatorRight]])
+
+	return TargetIndicator
+end
+
+function NP:Update_TargetIndicator(nameplate)
+	local db = NP.db.units[nameplate.frameType]
+
+	nameplate.TargetIndicator.style = NP['db'].targetGlow
+	nameplate.TargetIndicator.lowHealthThreshold = NP['db'].lowHealthThreshold
+
+	if NP['db'].targetGlow ~= 'none' then
+		local GlowStyle = NP['db'].targetGlow
+		local Color = NP['db'].colors.glowColor
+		if nameplate.TargetIndicator.TopIndicator and (GlowStyle == 'style3' or GlowStyle == 'style5' or GlowStyle == 'style6') then
+			local topArrowSpace = -3
+			if db.showName and (nameplate.Name:GetText() ~= nil and nameplate.Name:GetText() ~= '') then
+				topArrowSpace = NP['db'].fontSize + topArrowSpace
+			end
+			nameplate.TargetIndicator.TopIndicator:SetPoint('BOTTOM', nameplate.Health, 'TOP', 0, topArrowSpace)
+			nameplate.TargetIndicator.TopIndicator:SetVertexColor(Color.r, Color.g, Color.b)
+		end
+
+		if (nameplate.TargetIndicator.LeftIndicator and nameplate.TargetIndicator.RightIndicator) and (GlowStyle == 'style4' or GlowStyle == 'style7' or GlowStyle == 'style8') then
+			nameplate.TargetIndicator.LeftIndicator:SetPoint('LEFT', nameplate.Health, 'RIGHT', -3, 0)
+			nameplate.TargetIndicator.RightIndicator:SetPoint('RIGHT', nameplate.Health, 'LEFT', 3, 0)
+			nameplate.TargetIndicator.LeftIndicator:SetVertexColor(Color.r, Color.g, Color.b)
+			nameplate.TargetIndicator.RightIndicator:SetVertexColor(Color.r, Color.g, Color.b)
+		end
+
+		if nameplate.TargetIndicator.Shadow and (GlowStyle == 'style1' or GlowStyle == 'style5' or GlowStyle == 'style7') then
+			nameplate.TargetIndicator.Shadow:SetOutside(nameplate.Health, E:Scale(E.PixelMode and 6 or 8), E:Scale(E.PixelMode and 6 or 8))
+			nameplate.TargetIndicator.Shadow:SetBackdropBorderColor(Color.r, Color.g, Color.b)
+		end
+
+		if nameplate.TargetIndicator.Spark and (GlowStyle == 'style2' or GlowStyle == 'style6' or GlowStyle == 'style8') then
+			local scale = 1
+			if NP['db'].useTargetScale then
+				if NP['db'].targetScale >= 0.75 then
+					scale = NP['db'].targetScale
+				else
+					scale = 0.75
+				end
+			end
+
+			local size = (E.Border + 14) * scale;
+
+			nameplate.TargetIndicator.Spark:SetPoint('TOPLEFT', nameplate.Health, 'TOPLEFT', -(size * 2), size)
+			nameplate.TargetIndicator.Spark:SetPoint('BOTTOMRIGHT', nameplate.Health, 'BOTTOMRIGHT', size * 2, -size)
+			nameplate.TargetIndicator.Spark:SetVertexColor(Color.r, Color.g, Color.b)
+		end
+	end
+end
+
+function NP:Construct_Highlight(nameplate)
+	local Highlight = CreateFrame('Frame', nameplate:GetDebugName()..'Highlight', nameplate)
+
+	Highlight.texture = Highlight:CreateTexture(nil, 'BACKGROUND', nil, 1)
+
+	return Highlight
+end
+
+function NP:Update_Highlight(nameplate)
+	local db = NP.db.units[nameplate.frameType]
+
+	if db.health.enable then
+		nameplate.Highlight.texture:SetColorTexture(1, 1, 1, .3)
+		nameplate.Highlight.texture:SetAllPoints(nameplate.Health)
+		nameplate.Highlight.texture:SetAlpha(1)
+	else
+		nameplate.Highlight.texture:SetTexture([[Interface\AddOns\ElvUI\media\textures\spark]])
+		nameplate.Highlight.texture:SetAllPoints(nameplate)
+		nameplate.Highlight.texture:SetAlpha(.5)
+	end
+end
+
+function NP:Construct_HealerSpecs(nameplate)
+	local texture = nameplate:CreateTexture(nil, "OVERLAY")
+	texture:SetSize(40, 40)
+	texture:SetTexture([[Interface\AddOns\ElvUI\media\textures\healer]])
+	texture:Hide()
+
+	return texture
+end
+
+function NP:Update_HealerSpecs(nameplate)
+	local db = NP.db.units[nameplate.frameType]
+
+	if (nameplate.frameType == 'FRIENDLY_PLAYER' or nameplate.frameType == 'ENEMY_PLAYER') and db.markHealers then
+		if not nameplate:IsElementEnabled('HealerSpecs') then
+			nameplate:EnableElement('HealerSpecs')
+		end
+
+		nameplate.HealerSpecs:SetPoint("RIGHT", nameplate.Health, "LEFT", -6, 0)
+	else
+		if nameplate:IsElementEnabled('HealerSpecs') then
+			nameplate:DisableElement('HealerSpecs')
+		end
+	end
+end
+
+function NP:Construct_DetectionIndicator(nameplate)
+	local model = CreateFrame("PlayerModel", nil, nameplate)
+	model:SetSize(75, 75)
+	model:Hide()
+
+	return model
+end
+
+function NP:Update_DetectionIndicator(nameplate)
+	local db = NP.db.units[nameplate.frameType]
+
+	if (nameplate.frameType == 'ENEMY_NPC') and db.detection and db.detection.enable then
+		if not nameplate:IsElementEnabled('DetectionIndicator') then
+			nameplate:EnableElement('DetectionIndicator')
+		end
+
+		nameplate.DetectionIndicator:SetPoint("BOTTOM", nameplate, "TOP", 0, 0)
+	else
+		if nameplate:IsElementEnabled('DetectionIndicator') then
+			nameplate:DisableElement('DetectionIndicator')
+		end
+	end
+end
---
+++ ElvUI/Modules/Nameplates/Elements/Threat.lua
@@ -0,0 +1,104 @@
+local E, L, V, P, G = unpack(ElvUI)
+local NP = E:GetModule('NamePlates')
+
+local UnitExists = UnitExists
+local UnitIsUnit = UnitIsUnit
+local UnitGroupRolesAssigned = UnitGroupRolesAssigned
+local UnitPlayerControlled = UnitPlayerControlled
+local UnitIsTapDenied = UnitIsTapDenied
+local UnitIsPlayer = UnitIsPlayer
+local UnitReaction = UnitReaction
+
+function NP:Construct_ThreatIndicator(nameplate)
+	local ThreatIndicator = nameplate:CreateTexture(nil, 'OVERLAY')
+	ThreatIndicator:SetSize(16, 16)
+	ThreatIndicator:SetPoint('CENTER', nameplate, 'TOPRIGHT')
+
+	function ThreatIndicator:PreUpdate(unit)
+		NP:PreUpdateThreat(self, unit)
+	end
+
+	function ThreatIndicator:PostUpdate(unit, status)
+		NP:PostUpdateThreat(self, unit, status)
+	end
+
+	return ThreatIndicator
+end
+
+function NP:PreUpdateThreat(threat, unit)
+	local ROLE = NP.IsInGroup and UnitExists(unit..'target') and UnitGroupRolesAssigned(unit..'target') or 'NONE'
+	if ROLE == 'TANK' then
+		threat.feedbackUnit = unit..'target'
+		threat.offtank = not UnitIsUnit(unit..'target', 'player')
+		threat.isTank = true
+	else
+		threat.feedbackUnit = 'player'
+		threat.offtank = false
+		threat.isTank = NP.PlayerRole == 'TANK' and true or false
+	end
+end
+
+function NP:PostUpdateThreat(threat, unit, status)
+	if NP.db.threat and NP.db.threat.useThreatColor and NP.IsInGroup then
+		local r, g, b
+		if (not UnitPlayerControlled(unit) and UnitIsTapDenied(unit)) then
+			r, g, b = NP.db.colors.reactions.tapped.r, NP.db.colors.reactions.tapped.g, NP.db.colors.reactions.tapped.b
+		elseif status then
+			if (status == 3) then --Securely Tanking
+				if threat.isTank then
+					r, g, b = NP.db.colors['threat'].goodColor.r, NP.db.colors['threat'].goodColor.g, NP.db.colors['threat'].goodColor.b
+				else
+					r, g, b = NP.db.colors['threat'].badColor.r, NP.db.colors['threat'].badColor.g, NP.db.colors['threat'].badColor.b
+				end
+			elseif (status == 2) then --insecurely tanking
+				if threat.isTank then
+					r, g, b = NP.db.colors['threat'].badTransition.r, NP.db.colors['threat'].badTransition.g, NP.db.colors['threat'].badTransition.b
+				else
+					r, g, b = NP.db.colors['threat'].goodTransition.r, NP.db.colors['threat'].goodTransition.g, NP.db.colors['threat'].goodTransition.b
+				end
+			elseif (status == 1) then --not tanking but threat higher than tank
+				if threat.isTank then
+					r, g, b = NP.db.colors['threat'].goodTransition.r, NP.db.colors['threat'].goodTransition.g, NP.db.colors['threat'].goodTransition.b
+				else
+					r, g, b = NP.db.colors['threat'].badTransition.r, NP.db.colors['threat'].badTransition.g, NP.db.colors['threat'].badTransition.b
+				end
+			else -- not tanking at all
+				if threat.isTank then
+					--Check if it is being tanked by an offtank.
+					if threat.offtank then
+						r, g, b = NP.db.colors['threat'].beingTankedByTankColor.r, NP.db.colors['threat'].beingTankedByTankColor.g, NP.db.colors['threat'].beingTankedByTankColor.b
+					else
+						r, g, b = NP.db.colors['threat'].badColor.r, NP.db.colors['threat'].badColor.g, NP.db.colors['threat'].badColor.b
+					end
+				else
+					if threat.offtank then
+						r, g, b = NP.db.colors['threat'].beingTankedByTankColor.r, NP.db.colors['threat'].beingTankedByTankColor.g, NP.db.colors['threat'].beingTankedByTankColor.b
+					else
+						r, g, b = NP.db.colors['threat'].goodColor.r, NP.db.colors['threat'].goodColor.g, NP.db.colors['threat'].goodColor.b
+					end
+				end
+			end
+		elseif not UnitIsPlayer(unit) then
+			local reactionType = UnitReaction(unit, 'player')
+			if reactionType == 4 then
+				r, g, b = NP.db.colors.reactions.neutral.r, NP.db.colors.reactions.neutral.g, NP.db.colors.reactions.neutral.b
+			elseif reactionType > 4 then
+				r, g, b = NP.db.colors.reactions.good.r, NP.db.colors.reactions.good.g, NP.db.colors.reactions.good.b
+			else
+				r, g, b = NP.db.colors.reactions.bad.r, NP.db.colors.reactions.bad.g, NP.db.colors.reactions.bad.b
+			end
+		end
+		if r and g and b then
+			threat.__owner.Health:SetStatusBarColor(r, g, b)
+		end
+	end
+end
+
+function NP:Update_Threat(nameplate)
+	local db = NP.db.units[nameplate.frameType]
+
+	if NP.db.threat.useThreatColor then
+		nameplate.Health.colorReaction = false
+		nameplate.Health.colorClass = db.healthbar.useClassColor or false
+	end
+end
