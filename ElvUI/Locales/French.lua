2a0d48401cec4ec90a8e55af208bb5ab48cf9f18
Blazeflack <blazeflack@hotmail.com>
2019-03-02 23:16:48 +0100

    Added option to suppress the "UI scale changed" popup for the session.
    This closes #1076

--- ElvUI/Locales/French.lua
+++ ElvUI/Locales/French.lua
@@ -357,6 +357,7 @@ L["One or more of the changes you have made require a ReloadUI."] = "Une ou plus
 L["One or more of the changes you have made will effect all characters using this addon. You will have to reload the user interface to see the changes you have made."] = "Un ou plusieurs changements que vous avez effectués a une incidence sur tous les personnages qui utilisent cet addon. Vous devez recharger l'interface utilisateur pour voir le(s) changement(s) apporté(s)."
 L["Preview Changes"] = true
 L["Save"] = "Sauvegarder"
+L["Suppress In This Session"] = true
 L["The profile you tried to import already exists. Choose a new name or accept to overwrite the existing profile."] = "Le profil que vous essayez d'importer existe déjà. Choisissez un nouveau nom ou acceptez d'écraser le profil existant."
 L["The UI Scale has been changed, if you would like to preview the change press the preview button. It is recommended that you reload your User Interface for the best appearance."] = true
 L["This release of ElvUI contains changes to how we handle UI scale. See changelog for specifics. We need to set your UI scale again in order to use a new system. It appears your old UI scale was %s.\n\nYou can either apply this value, or use the 'Auto Scale' function to apply the UI scale that is considered the most optimal for your resolution.\n\nYou also have the option of choosing your own UI scale in the General section of the ElvUI config. In theory ElvUI should be able to look pixel perfect with any UI scale now but there may be a few issues with the ingame config."] = true
