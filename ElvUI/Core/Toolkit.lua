079bbd81d8303f90bd6210b4c692f8c8d5b10a7b
Azilroka <darktemplar9900@yahoo.com>
2019-03-02 16:44:36 -0600

    Revert "Fix this"
    This reverts commit 5fe8c9c751fc707d69ad80a59d50a39980e631e2.

--- ElvUI/Core/Toolkit.lua
+++ ElvUI/Core/Toolkit.lua
@@ -268,13 +268,10 @@ local function FontTemplate(fs, font, fontSize, fontStyle)
 
 	if fontStyle == 'SHADOW' then
 		fs:SetFont(font, fontSize)
-	else
-		fs:SetFont(font, fontSize, fontStyle)
-	end
-
-	if fontStyle == 'SHADOW' or fontStyle == 'NONE' then
 		fs:SetShadowColor(0, 0, 0, 1)
 		fs:SetShadowOffset(E.mult or 1, -(E.mult or 1))
+	else
+		fs:SetFont(font, fontSize, fontStyle)
 	end
 
 	E.texts[fs] = true
