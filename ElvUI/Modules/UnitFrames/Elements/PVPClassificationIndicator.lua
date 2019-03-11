dd199125130e38be1b243e203552f052d742df27
Azilroka <darktemplar9900@yahoo.com>
2019-03-10 21:43:08 -0500

    UF: PvPClassificationIndicator Part 1

---
+++ ElvUI/Modules/UnitFrames/Elements/PVPClassificationIndicator.lua
@@ -0,0 +1,24 @@
+local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local UF = E:GetModule('UnitFrames');
+
+--Lua functions
+
+--WoW API / Variables
+
+function UF:Construct_PvPClassificationIndicator(frame)
+    local PvPClassificationIndicator = frame.RaisedElementParent.TextureParent:CreateTexture(nil, 'OVERLAY')
+
+	return PvPClassificationIndicator
+end
+
+function UF:Configure_PvPClassificationIndicator(frame)
+	local PvPClassificationIndicator = frame.PvPClassificationIndicator
+	PvPClassificationIndicator:ClearAllPoints()
+	PvPClassificationIndicator:Point(frame.db.pvpClassificationIndicator.anchorPoint, frame.Health, frame.db.pvpClassificationIndicator.anchorPoint, frame.db.pvpClassificationIndicator.xOffset, frame.db.pvpClassificationIndicator.yOffset)
+
+	if frame.db.pvpClassificationIndicator.enable and not frame:IsElementEnabled('PvPClassificationIndicator') then
+		frame:EnableElement('PvPClassificationIndicator')
+	elseif not frame.db.pvpClassificationIndicator.enable and frame:IsElementEnabled('PvPClassificationIndicator') then
+		frame:DisableElement('PvPClassificationIndicator')
+	end
+end
