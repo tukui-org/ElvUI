70efa6fa7737c46d5220ca1570a739535b3da9d7
Blazeflack <blazeflack@hotmail.com>
2019-03-14 19:53:37 +0100

    Removed clamp on config window

--- ElvUI/init.lua
+++ ElvUI/init.lua
@@ -363,7 +363,6 @@ function AddOn:ToggleConfig(msg)
 				_G.ElvUIGUIFrame = self.GUIFrame
 
 				local maxWidth, maxHeight = self.UIParent:GetSize()
-				frame:SetClampedToScreen(true)
 				frame:SetMinResize(600, 500)
 				frame:SetMaxResize(maxWidth-50, maxHeight-50)
 
