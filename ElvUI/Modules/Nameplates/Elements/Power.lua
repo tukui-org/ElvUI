4f4c57547b8d3e4d2bac83a1851401903a352180
Azilroka <darktemplar9900@yahoo.com>
2019-03-16 11:53:49 -0500

    oUF Handles this

--- ElvUI/Modules/Nameplates/Elements/Power.lua
+++ ElvUI/Modules/Nameplates/Elements/Power.lua
@@ -13,19 +13,13 @@ function NP:Power_PreUpdate(unit)
 
 	if self.__owner.PowerColorChanged then return end
 
-	local db = NP.db.units[self.__owner.frameType]
-	if db and db.power.classColor and self.__owner.classFile then
-		local color = _G.CUSTOM_CLASS_COLORS and _G.CUSTOM_CLASS_COLORS[self.__owner.classFile] or RAID_CLASS_COLORS[self.__owner.classFile]
-		self:SetStatusBarColor(color.r, color.g, color.b)
+	local Color = NP.db.colors.power[pToken]
+	if Color then
+		self:SetStatusBarColor(Color.r, Color.g, Color.b)
 	else
-		local Color = NP.db.colors.power[pToken]
+		Color = _G.ElvUI.oUF.colors.power[pToken]
 		if Color then
-			self:SetStatusBarColor(Color.r, Color.g, Color.b)
-		else
-			Color = _G.ElvUI.oUF.colors.power[pToken]
-			if Color then
-				self:SetStatusBarColor(unpack(Color))
-			end
+			self:SetStatusBarColor(unpack(Color))
 		end
 	end
 end
