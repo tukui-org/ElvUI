9e3d227dfeda32dfbd6e78b015898a0f432ba8d8
Simpy <kodi03466@hotmail.com>
2019-03-19 20:04:38 -0400

    another safety thing

--- ElvUI/Modules/UnitFrames/Elements/ClassBars.lua
+++ ElvUI/Modules/UnitFrames/Elements/ClassBars.lua
@@ -269,6 +269,8 @@ end
 
 local function ToggleResourceBar(bars, overrideVisibility)
 	local frame = bars.origParent or bars:GetParent()
+	if bars == frame.AlternativePower then return end
+
 	local db = frame.db
 	if not db then return end
 
