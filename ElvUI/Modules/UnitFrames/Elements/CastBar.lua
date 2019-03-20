0744c54badf36e7a526f67b7f37e88f63b27bf6b
Merathilis <neo0608@gmail.com>
2019-03-20 13:14:06 +0100

    nothing

--- ElvUI/Modules/UnitFrames/Elements/CastBar.lua
+++ ElvUI/Modules/UnitFrames/Elements/CastBar.lua
@@ -177,7 +177,7 @@ function UF:Configure_Castbar(frame)
 			end
 		end
 
-		castbar.Spark:Height(db.infoPanel and db.infoPanel.height * 2) -- Grab the height from the castbar if inside infopanel again.
+		castbar.Spark:Height(db.infoPanel and db.infoPanel.height * 2) -- Grab the height from the infopanel.
 
 		if(castbar.Holder.mover) then
 			E:DisableMover(castbar.Holder.mover:GetName())
