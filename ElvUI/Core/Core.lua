063e79e1f416cb33ce38647dbfbfea9a7b6b4ea4
Simpy <kodi03466@hotmail.com>
2019-03-27 11:30:50 -0400

    we should actually nil check these old options so the old ones get removed.. regardless of the switch o.0

--- ElvUI/Core/Core.lua
+++ ElvUI/Core/Core.lua
@@ -1710,16 +1710,16 @@ function E:DBConversions()
 		E.db.chat.panelColorConverted = true
 	end
 
-	if E.db.chat.hideVoiceButtons then
-		E.db.chat.pinVoiceButtons = false
+	if E.db.chat.hideVoiceButtons ~= nil then
+		E.db.chat.pinVoiceButtons = not E.db.chat.hideVoiceButtons
 		E.db.chat.hideVoiceButtons = nil
 	end
 
 	--Vendor Greys option is now in bags table
-	if E.db.general.vendorGrays then
+	if E.db.general.vendorGrays ~= nil then
 		E.db.bags.vendorGrays.enable = E.db.general.vendorGrays
-		E.db.general.vendorGrays = nil
 		E.db.general.vendorGraysDetails = nil
+		E.db.general.vendorGrays = nil
 	end
 
 	--Heal Prediction is now a table instead of a bool
