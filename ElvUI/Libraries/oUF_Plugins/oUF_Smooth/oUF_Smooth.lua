bb750b129eb776766d802af950dd74cf941feff1
Simpy <kodi03466@hotmail.com>
2019-03-28 10:45:25 -0400

    add ls- bar smoothing

--- ElvUI/Libraries/oUF_Plugins/oUF_Smooth/oUF_Smooth.lua
+++ ElvUI/Libraries/oUF_Plugins/oUF_Smooth/oUF_Smooth.lua
@@ -2,63 +2,99 @@ local _, ns = ...
 local oUF = ns.oUF or oUF
 if not oUF then return end
 
-local smoothing = {}
-local function Smooth(self, value)
-	if value ~= self:GetValue() or value == 0 then
-		smoothing[self] = value
-	else
-		smoothing[self] = nil
+local abs, next, Lerp = abs, next, Lerp
+do -- Credit: ls- (lightspark)
+	local objects = {}
+	local TARGET_FPS = 60
+
+	local function clamp(v, min, max)
+		min = min or 0
+		max = max or 1
+
+		if v > max then
+			return max
+		elseif v < min then
+			return min
+		end
+
+		return v
+	end
+
+	local function isCloseEnough(new, target, range)
+		if range > 0 then
+			return abs((new - target) / range) <= 0.001
+		end
+
+		return true
+	end
+
+	local frame = CreateFrame("Frame", "LSBarSmoother")
+	frame:SetScript("OnUpdate", function(_, elapsed)
+		for object, target in next, objects do
+			local new = Lerp(object._value, target, clamp(0.33 * elapsed * TARGET_FPS))
+			if isCloseEnough(new, target, object._max - object._min) then
+				new = target
+				objects[object] = nil
+			end
+
+			object:SetValue_(new)
+			object._value = new
+		end
+	end)
+
+	local function bar_SetSmoothedValue(self, value)
+		self._value = self:GetValue()
+		objects[self] = clamp(value, self._min, self._max)
+	end
+
+	local function bar_SetSmoothedMinMaxValues(self, min, max)
+		self:SetMinMaxValues_(min, max)
+
+		if self._max and self._max ~= max then
+			local ratio = 1
+			if max ~= 0 and self._max and self._max ~= 0 then
+				ratio = max / (self._max or max)
+			end
+
+			local target = objects[self]
+			if target then
+				objects[self] = target * ratio
+			end
+
+			local cur = self._value
+			if cur then
+				self:SetValue_(cur * ratio)
+				self._value = cur * ratio
+			end
+		end
+
+		self._min = min
+		self._max = max
+	end
+
+	function E:SmoothBar(bar)
+		-- reset the bar
+		bar:SetMinMaxValues(0, 1)
+		bar:SetValue(0)
+
+		bar.SetValue_ = bar.SetValue
+		bar.SetMinMaxValues_ = bar.SetMinMaxValues
+		bar.SetValue = bar_SetSmoothedValue
+		bar.SetMinMaxValues = bar_SetSmoothedMinMaxValues
+	end
+
+	function E:DesmoothBar(bar)
+		if bar.SetValue_ then
+			bar.SetValue = bar.SetValue_
+			bar.SetValue_ = nil
+		end
+
+		if bar.SetMinMaxValues_ then
+			bar.SetMinMaxValues = bar.SetMinMaxValues_
+			bar.SetMinMaxValues_ = nil
+		end
 	end
 end
 
-local function SmoothBar(bar)
-	if not bar.SetValue_ then
-		bar.SetValue_ = bar.SetValue;
-		bar.SetValue = Smooth;
-	end
-end
-
-local function ResetBar(bar)
-	if bar.SetValue_ then
-		bar.SetValue = bar.SetValue_;
-		bar.SetValue_ = nil;
-	end
-end
-
-local function hook(frame)
-	if frame.Health then
-		SmoothBar(frame.Health)
-	end
-	if frame.Power then
-		SmoothBar(frame.Power)
-	end
-	if frame.AlternativePower then
-		SmoothBar(frame.AlternativePower)
-	end
-end
-
-for i, frame in ipairs(oUF.objects) do hook(frame) end
+for _, frame in ipairs(oUF.objects) do hook(frame) end
 oUF:RegisterInitCallback(hook)
-
-local f, min, max = CreateFrame('Frame'), math.min, math.max
-f:SetScript('OnUpdate', function()
-	local rate = GetFramerate()
-	local limit = 30/rate
-
-	for bar, value in pairs(smoothing) do
-		local cur = bar:GetValue()
-		local new = cur + min((value-cur)/3, max(value-cur, limit))
-		if new ~= new then
-			-- Mad hax to prevent QNAN.
-			new = value
-		end
-		bar:SetValue_(new)
-		if (cur == value or abs(new - value) < 2) and bar.Smooth then
-			bar:SetValue_(value)
-			smoothing[bar] = nil
-		elseif not bar.Smooth then
-			bar:SetValue_(value)
-			smoothing[bar] = nil
-		end
-	end
-end)
