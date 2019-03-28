1d310eb703d589ade3788402e99c94127f89dbaf
Simpy <kodi03466@hotmail.com>
2019-03-28 10:46:08 -0400

    Revert "Smoooothies bar by @ls-. For testing purpose only on NP Health for now."
    This reverts commit a5418431a6ecc2182f0f11e40cbb50533df3b0d1.

--- ElvUI/Modules/Nameplates/Elements/Health.lua
+++ ElvUI/Modules/Nameplates/Elements/Health.lua
@@ -78,7 +78,6 @@ function NP:Construct_Health(nameplate)
 	Health.bg:SetTexture(E.media.blankTex)
 	Health.bg.multiplier = 0.2]]
 
-	E:SmoothBar(Health)
 	NP.StatusBars[Health] = true
 
 	local statusBarTexture = Health:GetStatusBarTexture()
