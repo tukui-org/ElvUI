36dfb076947f2378359307ae40268ccb4008fed7
Simpy <kodi03466@hotmail.com>
2019-03-31 22:59:28 -0400

    Revert "fix the extra actionbar cd swipe being under the icon texture, just set the icon onto the 'border' level, this will work"
    This reverts commit 3badec9865bf5f6c09f54ff2bc7f1a4b6b3b18a3.

--- ElvUI/Modules/ActionBars/ExtraAB.lua
+++ ElvUI/Modules/ActionBars/ExtraAB.lua
@@ -79,8 +79,7 @@ function AB:SetupExtraButton()
 
 			self:StyleButton(button, true)
 			button:SetTemplate()
-			button.icon:SetDrawLayer('BORDER')
-
+			button.icon:SetDrawLayer('ARTWORK')
 			local tex = button:CreateTexture(nil, 'OVERLAY')
 			tex:SetColorTexture(0.9, 0.8, 0.1, 0.3)
 			tex:SetInside()
