7a2b523ffb86b01407059f8661e4def682ed8a4e
Simpy <kodi03466@hotmail.com>
2019-04-03 20:09:38 -0400

    add a `cd.disabled` to ignore OnSetCooldown and kill / ignore a cd..

--- ElvUI/Core/Cooldowns.lua
+++ ElvUI/Core/Cooldowns.lua
@@ -167,7 +167,7 @@ end
 
 E.RegisteredCooldowns = {}
 function E:OnSetCooldown(start, duration)
-	if (start and duration) and (duration > MIN_DURATION) then
+	if (not self.disabled) and (start and duration) and (duration > MIN_DURATION) then
 		local timer = self.timer or E:CreateCooldownTimer(self)
 		timer.start = start
 		timer.duration = duration
