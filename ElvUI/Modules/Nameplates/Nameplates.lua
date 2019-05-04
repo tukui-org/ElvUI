14e261f7881d8ce33d9e06ad0b3074ac070c976d
Merathilis <neo0608@gmail.com>
2019-05-04 19:53:24 +0200

    Revert "We also need to set the values if we dont want to clamp the Nameplates @Azilroka <3"
    This reverts commit 5183b86efa5d0950a31135ef7e53d0090024721b.

--- ElvUI/Modules/Nameplates/Nameplates.lua
+++ ElvUI/Modules/Nameplates/Nameplates.lua
@@ -114,9 +114,6 @@ function NP:SetCVars()
 	if NP.db.clampToScreen then
 		SetCVar('nameplateOtherTopInset', 0.08)
 		SetCVar('nameplateOtherBottomInset', 0.1)
-	else
-		SetCVar('nameplateOtherTopInset', -1)
-		SetCVar('nameplateOtherBottomInset', -1)
 	end
 end
 
