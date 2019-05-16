bb73ae51544c04491099f4f66d757ad440b0d9fd
Merathilis <neo0608@gmail.com>
2019-05-16 11:45:59 +0200

    Update AzeriteEssenceUI

--- ElvUI/Modules/Skins/Blizzard/AzeriteEssence.lua
+++ ElvUI/Modules/Skins/Blizzard/AzeriteEssence.lua
@@ -5,19 +5,31 @@ local S = E:GetModule('Skins')
 local _G = _G
 local pairs = pairs
 --WoW API / Variables
+local C_AzeriteEssence_CanOpenUI = C_AzeriteEssence.CanOpenUI
 
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.AzeriteEssence ~= true then return end
-	if not C_AzeriteEssence.CanOpenUI() then return end
-	--Probably Temp name
-	local AzeriteEssenceUI = _G.AzeriteEssenceUI
-	S:HandlePortraitFrame(AzeriteEssenceUI, true)
-	S:HandleScrollBar(AzeriteEssenceUI.ScrollFrame.ScrollBar)
+	if not C_AzeriteEssence_CanOpenUI() then return end
 
-	--[[
-	for _, Slot in pairs(AzeriteEssenceUI.SlotsFrame.Slots) do
+	local AzeriteEssenceUI = _G.AzeriteEssenceUI
+
+	-- Reposition the Level Badge
+	AzeriteEssenceUI.PowerLevelBadgeFrame:ClearAllPoints()
+	AzeriteEssenceUI.PowerLevelBadgeFrame:SetPoint("TOPLEFT", 0, 0)
+
+	S:HandlePortraitFrame(AzeriteEssenceUI, true)
+	S:HandleScrollBar(AzeriteEssenceUI.EssenceList.ScrollBar)
+
+	for _, button in pairs(AzeriteEssenceUI.EssenceList.buttons) do
+		button:DisableDrawLayer('ARTWORK')
+		S:HandleIcon(button.Icon)
+		button:StyleButton()
+		button:CreateBackdrop()
+		button.backdrop:SetAllPoints()
+		--button.PendingGlow:SetTexture() --needs some love
 	end
-	]]
+
+	-- ToDO: Skin the HeaderButton
 end
 
 S:AddCallbackForAddon("Blizzard_AzeriteEssenceUI", "AzeriteEssenceUI", LoadSkin)
