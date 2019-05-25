1d1342f5719b14a259eef1b8d53d93a540aadd26
Simpy <kodi03466@hotmail.com>
2019-05-25 05:49:56 -0400

    set the tex in construct since we can, dont have to wait for SB update

--- ElvUI/Modules/Nameplates/Elements/Plugins.lua
+++ ElvUI/Modules/Nameplates/Elements/Plugins.lua
@@ -249,21 +249,23 @@ end
 function NP:Construct_Cutaway(nameplate)
 	local Cutaway = CreateFrame('Frame', nameplate:GetDebugName()..'Cutaway', nameplate)
 
-    Cutaway.Health = CreateFrame('StatusBar', nameplate:GetDebugName()..'CutawayHealth', nameplate.Health)
-    if NP.db.cutaway.health.forceBlankTexture then
-        Cutaway.Health:SetStatusBarTexture(E.media.blankTex)
-    else
-        NP.StatusBars[Cutaway.Health] = true
-    end
+	Cutaway.Health = CreateFrame('StatusBar', nameplate:GetDebugName()..'CutawayHealth', nameplate.Health)
+	if NP.db.cutaway.health.forceBlankTexture then
+		Cutaway.Health:SetStatusBarTexture(E.media.blankTex)
+	else
+		Cutaway.Health:SetStatusBarTexture(E.Libs.LSM:Fetch('statusbar', NP.db.statusbar))
+		NP.StatusBars[Cutaway.Health] = true
+	end
 	Cutaway.Health:SetFrameLevel(4)
 	Cutaway.Health:SetAllPoints()
 
-    Cutaway.Power = CreateFrame('StatusBar', nameplate:GetDebugName()..'CutawayPower', nameplate.Power)
-    if NP.db.cutaway.power.forceBlankTexture then
-        Cutaway.Power:SetStatusBarTexture(E.media.blankTex)
-    else
-        NP.StatusBars[Cutaway.Power] = true
-    end
+	Cutaway.Power = CreateFrame('StatusBar', nameplate:GetDebugName()..'CutawayPower', nameplate.Power)
+	if NP.db.cutaway.power.forceBlankTexture then
+		Cutaway.Power:SetStatusBarTexture(E.media.blankTex)
+	else
+		Cutaway.Power:SetStatusBarTexture(E.Libs.LSM:Fetch('statusbar', NP.db.statusbar))
+		NP.StatusBars[Cutaway.Power] = true
+	end
 	Cutaway.Power:SetFrameLevel(4)
 	Cutaway.Power:SetAllPoints()
 
@@ -271,21 +273,21 @@ function NP:Construct_Cutaway(nameplate)
 end
 
 function NP:Update_Cutaway(nameplate)
-    local eitherEnabled = NP.db.cutaway.health.enabled or NP.db.cutaway.power.enabled;
-    if not eitherEnabled then
-        if nameplate:IsElementEnabled('Cutaway') then
-            nameplate:DisableElement('Cutaway')
-        end
-    else
-        if not nameplate:IsElementEnabled('Cutaway') then
-            nameplate:EnableElement('Cutaway')
-        end
-        nameplate.Cutaway.Health.enabled = NP.db.cutaway.health.enabled
-        nameplate.Cutaway.Health.lengthBeforeFade = NP.db.cutaway.health.lengthBeforeFade
-        nameplate.Cutaway.Health.fadeOutTime = NP.db.cutaway.health.fadeOutTime
+	local eitherEnabled = NP.db.cutaway.health.enabled or NP.db.cutaway.power.enabled;
+	if not eitherEnabled then
+		if nameplate:IsElementEnabled('Cutaway') then
+			nameplate:DisableElement('Cutaway')
+		end
+	else
+		if not nameplate:IsElementEnabled('Cutaway') then
+			nameplate:EnableElement('Cutaway')
+		end
+		nameplate.Cutaway.Health.enabled = NP.db.cutaway.health.enabled
+		nameplate.Cutaway.Health.lengthBeforeFade = NP.db.cutaway.health.lengthBeforeFade
+		nameplate.Cutaway.Health.fadeOutTime = NP.db.cutaway.health.fadeOutTime
 
-        nameplate.Cutaway.Power.enabled = NP.db.cutaway.power.enabled
-        nameplate.Cutaway.Power.lengthBeforeFade = NP.db.cutaway.power.lengthBeforeFade
-        nameplate.Cutaway.Power.fadeOutTime = NP.db.cutaway.power.fadeOutTime
-    end
+		nameplate.Cutaway.Power.enabled = NP.db.cutaway.power.enabled
+		nameplate.Cutaway.Power.lengthBeforeFade = NP.db.cutaway.power.lengthBeforeFade
+		nameplate.Cutaway.Power.fadeOutTime = NP.db.cutaway.power.fadeOutTime
+	end
 end
