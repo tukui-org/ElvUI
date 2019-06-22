86df7262605ced9f845630f9a112cd8a6ad31446
Merathilis <neo0608@gmail.com>
2019-06-22 10:24:15 +0200

    Rewrote the AzeriteEssence skin.

--- ElvUI/Modules/Skins/Blizzard/AzeriteEssence.lua
+++ ElvUI/Modules/Skins/Blizzard/AzeriteEssence.lua
@@ -3,8 +3,9 @@ local S = E:GetModule('Skins')
 
 --Lua functions
 local _G = _G
-local pairs = pairs
+local ipairs, pairs, unpack = ipairs, pairs, unpack
 --WoW API / Variables
+local hooksecurefunc = hooksecurefunc
 local C_AzeriteEssence_CanOpenUI = C_AzeriteEssence.CanOpenUI
 
 local function LoadSkin()
@@ -12,6 +13,10 @@ local function LoadSkin()
 	if not C_AzeriteEssence_CanOpenUI() then return end
 
 	local AzeriteEssenceUI = _G.AzeriteEssenceUI
+	local r, g, b = unpack(E.media.rgbvaluecolor)
+
+	S:HandlePortraitFrame(AzeriteEssenceUI, true)
+	S:HandleScrollBar(AzeriteEssenceUI.EssenceList.ScrollBar)
 
 	-- Reposition the Level Badge
 	AzeriteEssenceUI.PowerLevelBadgeFrame:ClearAllPoints()
@@ -22,25 +27,49 @@ local function LoadSkin()
 	AzeriteEssenceUI.OrbBackground:SetAllPoints(AzeriteEssenceUI.ItemModelScene)
 	AzeriteEssenceUI.OrbRing:SetSize(483, 480)
 
-	S:HandlePortraitFrame(AzeriteEssenceUI, true)
-	S:HandleScrollBar(AzeriteEssenceUI.EssenceList.ScrollBar)
+	local HeaderButton = AzeriteEssenceUI.EssenceList.HeaderButton
+	HeaderButton:DisableDrawLayer('BORDER')
+	HeaderButton:DisableDrawLayer('BACKGROUND')
 
-	-- Essence List on the right
-	for _, button in pairs(AzeriteEssenceUI.EssenceList.buttons) do
-		button:DisableDrawLayer('ARTWORK')
-		S:HandleIcon(button.Icon)
-		button:StyleButton()
-		button:CreateBackdrop()
-		button.backdrop:SetAllPoints()
-		--button.PendingGlow:SetTexture() --needs some love
+	HeaderButton:CreateBackdrop()
+	HeaderButton.backdrop:SetPoint('TOPLEFT', HeaderButton.ExpandedIcon, -4, 6)
+	HeaderButton.backdrop:SetPoint('BOTTOMRIGHT', HeaderButton.ExpandedIcon, 4, -6)
+	HeaderButton:SetScript('OnEnter', function()
+		HeaderButton.backdrop:SetBackdropColor(r, g, b, .25)
+	end)
+	HeaderButton:SetScript('OnLeave', function()
+		HeaderButton.backdrop:SetBackdropColor(0, 0, 0, 0)
+	end)
+
+	for _, milestoneFrame in pairs(AzeriteEssenceUI.Milestones) do
+		if milestoneFrame.LockedState then
+			milestoneFrame.LockedState.UnlockLevelText:SetTextColor(.6, .8, 1)
+		end
 	end
 
-	-- Header on the Essence List
-	AzeriteEssenceUI:HookScript('OnShow', function(self)
-		self.EssenceList.HeaderButton:StripTextures()
-		if not self.EssenceList.HeaderButton.backdrop then
-			self.EssenceList.HeaderButton:CreateBackdrop('Transparent')
-			self.EssenceList.HeaderButton.backdrop:SetAllPoints()
+	hooksecurefunc(AzeriteEssenceUI.EssenceList, 'Refresh', function(self)
+		for i, button in ipairs(self.buttons) do
+			if not button.isSkinned then
+				S:HandleIcon(button.Icon)
+				button:CreateBackdrop()
+				button.backdrop:SetAllPoints()
+
+				button.PendingGlow:SetTexture("")
+				local h1 = button:GetHighlightTexture()
+				h1:SetColorTexture(r, g, b, .25)
+				h1:SetOutside(button.backdrop)
+
+				button.IsSkinned = true
+			end
+			button.Background:SetTexture("")
+
+			if button:IsShown() then
+				if button.PendingGlow:IsShown() then
+					button.backdrop:SetBackdropBorderColor(1, .8, 0)
+				else
+					button.backdrop:SetBackdropBorderColor(0, 0, 0)
+				end
+			end
 		end
 	end)
 end
