fade914eda0bed7ebdaa343277d0fedde2804fee
Merathilis <neo0608@gmail.com>
2019-06-27 13:18:01 +0200

    Update PVPMatch Skin: Mera wants backdrop here!!

--- ElvUI/Modules/Skins/Blizzard/PVPMatch.lua
+++ ElvUI/Modules/Skins/Blizzard/PVPMatch.lua
@@ -15,9 +15,10 @@ local function LoadSkin()
 	local PVPMatchScoreboard = _G.PVPMatchScoreboard
 	PVPMatchScoreboard:StripTextures()
 	PVPMatchScoreboard:DisableDrawLayer("BORDER")
-	PVPMatchScoreboard:SetTemplate('Transparent')
+	PVPMatchScoreboard:CreateBackdrop('Transparent')
 
 	PVPMatchScoreboard.Content:StripTextures()
+	PVPMatchScoreboard.Content.TabContainer.InsetBorderTop:Hide()
 
 	S:HandleScrollBar(PVPMatchScoreboard.Content.ScrollFrame.ScrollBar)
 	S:HandleCloseButton(PVPMatchScoreboard.CloseButton)
@@ -37,7 +38,7 @@ local function LoadSkin()
 	local PVPMatchResults = _G.PVPMatchResults
 	PVPMatchResults:StripTextures()
 	PVPMatchResults:DisableDrawLayer("BORDER")
-	PVPMatchResults:SetTemplate('Transparent')
+	PVPMatchResults:CreateBackdrop('Transparent')
 
 	PVPMatchResults.content:StripTextures()
 	PVPMatchResults.content.tabContainer:StripTextures()
