e3fcfacac3a81bb1c42c1a1accc0a86bb6ce5932
Merathilis <neo0608@gmail.com>
2019-07-18 14:05:36 +0200

    @AcidWeb

--- ElvUI/Modules/Skins/Blizzard/Spellbook.lua
+++ ElvUI/Modules/Skins/Blizzard/Spellbook.lua
@@ -184,6 +184,14 @@ local function LoadSkin()
 
 	_G.SpellBookFrameTabButton1:ClearAllPoints()
 	_G.SpellBookFrameTabButton1:Point('TOPLEFT', SpellBookFrame, 'BOTTOMLEFT', 0, 2)
+
+	-- Some Texture Magic
+	hooksecurefunc("FormatProfession", function(frame, id)
+		local texture = select(2, GetProfessionInfo(id))
+		if frame.icon and texture then
+			frame.icon:SetTexture(texture)
+		end
+	end)
 end
 
 S:AddCallback("Spellbook", LoadSkin)
