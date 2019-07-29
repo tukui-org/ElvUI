542ab3ef6de03a24dea4e663169dcd482960cca9
NihilisticPandemonium <nihilisticpandemonium@gmail.com>
2019-07-29 04:40:53 -0700

    11.20

--- ElvUI/Settings/Global.lua
+++ ElvUI/Settings/Global.lua
@@ -3,7 +3,7 @@ local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateD
 --Global Settings
 G.general = {
 	UIScale = 0.64,
-	version = 11.19,
+	version = 11.20,
 	locale = "auto",
 	eyefinity = false,
 	ignoreScalePopup = false,
