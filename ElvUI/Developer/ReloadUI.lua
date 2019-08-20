53531e10d1788e3e495e7ffe1eb4664a16a30ce0
Simpy <kodi03466@hotmail.com>
2019-08-20 18:48:28 -0400

    more single quotes

--- ElvUI/Developer/ReloadUI.lua
+++ ElvUI/Developer/ReloadUI.lua
@@ -2,6 +2,6 @@
 local SlashCmdList = SlashCmdList
 -- GLOBALS: SLASH_RELOADUI1, SLASH_RELOADUI2
 
-SLASH_RELOADUI1 = "/rl"
-SLASH_RELOADUI2 = "/reloadui"
+SLASH_RELOADUI1 = '/rl'
+SLASH_RELOADUI2 = '/reloadui'
 SlashCmdList.RELOADUI = ReloadUI
