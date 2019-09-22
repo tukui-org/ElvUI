fe2224f3cccea23f048faf7995339383042c817b
Azilroka <darktemplar9900@yahoo.com>
2019-09-22 02:59:09 -0500

    Minimap Cleanup

--- ElvUI/init.lua
+++ ElvUI/init.lua
@@ -176,7 +176,6 @@ function AddOn:OnInitialize()
 	self:UpdateMedia()
 	self:RegisterEvent('PLAYER_REGEN_DISABLED')
 	self:Contruct_StaticPopups()
-	AddOn.Layout:Initialize()
 	self:InitializeInitialModules()
 
 	if GetAddOnEnableState(self.myname, 'Tukui') == 2 then
