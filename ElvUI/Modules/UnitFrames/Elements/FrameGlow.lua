b19608fa12d0b6694de2f669f7c617b27ecc3e66
Simpy <kodi03466@hotmail.com>
2019-10-11 14:39:12 -0400

    accidentally made a duplicate function so this has been wrong for a bit. thanks for Apollon for pointing this out to me xD
    fixes a duplicate rename from adff4004ffa109e11165c2196bfac280713991b5

--- ElvUI/Modules/UnitFrames/Elements/FrameGlow.lua
+++ ElvUI/Modules/UnitFrames/Elements/FrameGlow.lua
@@ -33,7 +33,7 @@ function UF:FrameGlow_ElementHook(frame, glow, which)
 		end
 
 		if which == 'mouseoverGlow' then
-			UF:FrameGlow_PositionGlow(frame)
+			UF:FrameGlow_PositionTexture(frame)
 			UF:FrameGlow_CheckMouseover(frame)
 		else
 			UF:FrameGlow_PositionGlow(frame, glow, glow.powerGlow)
@@ -326,7 +326,7 @@ function UF:FrameGlow_CheckMouseover(frame)
 	end
 end
 
-function UF:FrameGlow_PositionGlow(frame)
+function UF:FrameGlow_PositionTexture(frame)
 	if frame.FrameGlow and frame.FrameGlow.texture then
 		frame.FrameGlow.texture:ClearAllPoints()
 		frame.FrameGlow.texture:Point('TOPLEFT', frame.Health, 'TOPLEFT')
