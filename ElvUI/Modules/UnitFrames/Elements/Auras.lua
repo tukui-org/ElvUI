98d9cbf2999e889f94c40586fd8b4b79c81aeb88
Simpy <kodi03466@hotmail.com>
2019-10-15 04:24:18 -0400

    ok dont need this then

--- ElvUI/Modules/UnitFrames/Elements/Auras.lua
+++ ElvUI/Modules/UnitFrames/Elements/Auras.lua
@@ -1,6 +1,5 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local UF = E:GetModule('UnitFrames')
-local NP = E:GetModule('NamePlates')
 local LSM = E.Libs.LSM
 
 --Lua functions
