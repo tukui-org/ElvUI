76f823834ac8e340607883a61e2e053f9bfaf521
Simpy <kodi03466@hotmail.com>
2019-10-17 01:57:35 -0400

    only need to do this once, not repeatedly

--- ElvUI/Core/API.lua
+++ ElvUI/Core/API.lua
@@ -67,52 +67,55 @@ function E:IsFoolsDay()
 	return strfind(date(), '04/01/') and not E.global.aprilFools
 end
 
-function E:ScanTooltipTextures(clean, grabTextures)
-	local tt, essenceTextureID = E.ScanTooltip, 2975691
+do
+	local essenceTextureID = 2975691
+	function E:ScanTooltipTextures(clean, grabTextures)
+		local tt = E.ScanTooltip
 
-	if not tt.gems then
-		tt.gems = {}
-	else
-		wipe(tt.gems)
-	end
-
-	if not tt.essences then
-		tt.essences = {}
-	else
-		for _, essences in pairs(tt.essences) do
-			wipe(essences)
+		if not tt.gems then
+			tt.gems = {}
+		else
+			wipe(tt.gems)
 		end
-	end
 
-	for i = 1, 10 do
-		local tex = _G['ElvUI_ScanTooltipTexture'..i]
-		local texture = tex and tex:GetTexture()
-		if texture then
-			if grabTextures then
-				if texture == essenceTextureID then
-					local selected = (tt.gems[i-1] ~= essenceTextureID and tt.gems[i-1]) or nil
-					if not tt.essences[i] then
-						tt.essences[i] = {}
+		if not tt.essences then
+			tt.essences = {}
+		else
+			for _, essences in pairs(tt.essences) do
+				wipe(essences)
+			end
+		end
+
+		for i = 1, 10 do
+			local tex = _G['ElvUI_ScanTooltipTexture'..i]
+			local texture = tex and tex:GetTexture()
+			if texture then
+				if grabTextures then
+					if texture == essenceTextureID then
+						local selected = (tt.gems[i-1] ~= essenceTextureID and tt.gems[i-1]) or nil
+						if not tt.essences[i] then
+							tt.essences[i] = {}
+						end
+
+						tt.essences[i][1] = selected
+						tt.essences[i][2] = tex:GetAtlas()
+						tt.essences[i][3] = texture
+
+						if selected then
+							tt.gems[i-1] = nil
+						end
+					else
+						tt.gems[i] = texture
 					end
-
-					tt.essences[i][1] = selected
-					tt.essences[i][2] = tex:GetAtlas()
-					tt.essences[i][3] = texture
-
-					if selected then
-						tt.gems[i-1] = nil
-					end
-				else
-					tt.gems[i] = texture
+				end
+				if clean then
+					tex:SetTexture()
 				end
 			end
-			if clean then
-				tex:SetTexture()
-			end
 		end
-	end
 
-	return tt.gems, tt.essences
+		return tt.gems, tt.essences
+	end
 end
 
 function E:GetPlayerRole()
