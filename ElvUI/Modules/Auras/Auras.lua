5ed45205990fbc2c3618feffe6d97f703a1c7fd6
Azilroka <darktemplar9900@yahoo.com>
2019-10-20 23:26:01 -0500

    Auras - Adjust StatusBar Width / Height

--- ElvUI/Modules/Auras/Auras.lua
+++ ElvUI/Modules/Auras/Auras.lua
@@ -142,14 +142,14 @@ function A:CreateIcon(button)
 	E:RegisterStatusBar(button.statusBar)
 	button.statusBar:CreateBackdrop()
 
-	local pos, spacing = self.db.barPosition, self.db.barSpacing
+	local pos, spacing, iconSize = self.db.barPosition, self.db.barSpacing, db.size - (E.Border * 2)
 	local isOnTop = pos == 'TOP' and true or false
 	local isOnBottom = pos == 'BOTTOM' and true or false
 	local isOnLeft = pos == 'LEFT' and true or false
 	local isOnRight = pos == 'RIGHT' and true or false
 
-	button.statusBar:Width((isOnTop or isOnBottom) and db.size or (self.db.barWidth + (E.PixelMode and 0 or 2)))
-	button.statusBar:Height((isOnLeft or isOnRight) and db.size or (self.db.barHeight + (E.PixelMode and 0 or 2)))
+	button.statusBar:Width((isOnTop or isOnBottom) and iconSize or (self.db.barWidth + (E.PixelMode and 0 or 2)))
+	button.statusBar:Height((isOnLeft or isOnRight) and iconSize or (self.db.barHeight + (E.PixelMode and 0 or 2)))
 	button.statusBar:Point(E.InversePoints[pos], button, pos, (isOnTop or isOnBottom) and 0 or ((isOnLeft and -((E.PixelMode and 1 or 3) + spacing)) or ((E.PixelMode and 1 or 3) + spacing)), (isOnLeft or isOnRight) and 0 or ((isOnTop and ((E.PixelMode and 1 or 3) + spacing) or -((E.PixelMode and 1 or 3) + spacing))))
 	if isOnLeft or isOnRight then button.statusBar:SetOrientation('VERTICAL') end
 	if not self.db.barShow then button.statusBar:Hide() end
@@ -403,7 +403,7 @@ function A:UpdateHeader(header)
 
 	header:SetAttribute("template", ("ElvUIAuraTemplate%d"):format(db.size))
 
-	local pos, spacing = self.db.barPosition, self.db.barSpacing
+	local pos, spacing, iconSize = self.db.barPosition, self.db.barSpacing, db.size - (E.Border * 2)
 	local isOnTop = pos == 'TOP' and true or false
 	local isOnBottom = pos == 'BOTTOM' and true or false
 	local isOnLeft = pos == 'LEFT' and true or false
@@ -434,8 +434,8 @@ function A:UpdateHeader(header)
 			child:Hide()
 		end
 
-		child.statusBar:Width((isOnTop or isOnBottom) and db.size or (self.db.barWidth + (E.PixelMode and 0 or 2)))
-		child.statusBar:Height((isOnLeft or isOnRight) and db.size or (self.db.barHeight + (E.PixelMode and 0 or 2)))
+		child.statusBar:Width((isOnTop or isOnBottom) and iconSize or (self.db.barWidth + (E.PixelMode and 0 or 2)))
+		child.statusBar:Height((isOnLeft or isOnRight) and iconSize or (self.db.barHeight + (E.PixelMode and 0 or 2)))
 		child.statusBar:ClearAllPoints()
 		child.statusBar:Point(E.InversePoints[pos], child, pos, (isOnTop or isOnBottom) and 0 or ((isOnLeft and -((E.PixelMode and 1 or 3) + spacing)) or ((E.PixelMode and 1 or 3) + spacing)), (isOnLeft or isOnRight) and 0 or ((isOnTop and ((E.PixelMode and 1 or 3) + spacing) or -((E.PixelMode and 1 or 3) + spacing))))
 		if isOnLeft or isOnRight then
