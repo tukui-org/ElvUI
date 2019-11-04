f1eb48a21c911290cfedbfd35f47f9543b06a939
Azilroka <darktemplar9900@yahoo.com>
2019-11-03 21:57:52 -0600

    Hide the ScanTooltip

--- ElvUI/Modules/Bags/Bags.lua
+++ ElvUI/Modules/Bags/Bags.lua
@@ -569,6 +569,8 @@ function B:UpdateSlot(frame, bagID, slotID)
 				if (BoE or BoU) then break end
 			end
 
+			E.ScanTooltip:Hide()
+
 			if BoE or BoU then
 				slot.bindType:SetText(BoE and L["BoE"] or L["BoU"])
 				slot.bindType:SetVertexColor(r, g, b)
