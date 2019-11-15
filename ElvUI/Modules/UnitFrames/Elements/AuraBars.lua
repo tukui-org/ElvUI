9c225379ab7355105847d183d50e73757b9ea11a
Azilroka <darktemplar9900@yahoo.com>
2019-11-15 14:04:43 -0600

    Fix AuraBar Coloring

--- ElvUI/Modules/UnitFrames/Elements/AuraBars.lua
+++ ElvUI/Modules/UnitFrames/Elements/AuraBars.lua
@@ -197,6 +197,10 @@ function UF:PostUpdateBar_AuraBars(unit, statusBar, index, position, duration, e
 
 	local colors = E.global.unitframe.AuraBarColors[spellID] or E.global.unitframe.AuraBarColors[tostring(spellID)] or E.global.unitframe.AuraBarColors[spellName]
 
+	if E.db.unitframe.colors.auraBarTurtle and (E.global.unitframe.aurafilters.TurtleBuffs.spells[spellID] or E.global.unitframe.aurafilters.TurtleBuffs.spells[spellName]) and not colors and (spellName ~= GOTAK or (spellName == GOTAK and spellID == GOTAK_ID)) then
+		colors = E.db.unitframe.colors.auraBarTurtleColor
+	end
+
 	if not colors then
 		if UF.db.colors.auraBarByType and statusBar.filter == 'HARMFUL' then
 			if (not debuffType or (debuffType == '' or debuffType == 'none')) then
@@ -218,9 +222,6 @@ function UF:PostUpdateBar_AuraBars(unit, statusBar, index, position, duration, e
 	end
 
 	statusBar.custom_backdrop = UF.db.colors.customaurabarbackdrop and UF.db.colors.aurabar_backdrop
-	if E.db.unitframe.colors.auraBarTurtle and (E.global.unitframe.aurafilters.TurtleBuffs.spells[spellID] or E.global.unitframe.aurafilters.TurtleBuffs.spells[spellName]) and not colors and (spellName ~= GOTAK or (spellName == GOTAK and spellID == GOTAK_ID)) then
-		colors = E.db.unitframe.colors.auraBarTurtleColor
-	end
 
 	if statusBar.bg then
 		if (UF.db.colors.transparentAurabars and not statusBar.isTransparent) or (statusBar.isTransparent and (not UF.db.colors.transparentAurabars or statusBar.invertColors ~= UF.db.colors.invertAurabars)) then
