a17d6b3ab710c95a440e8d011cfe77a1ec87d906
Merathilis <neo0608@gmail.com>
2020-01-21 14:28:37 +0100

    Dont code if you are high! bad mera

--- ElvUI/Modules/UnitFrames/Groups/Party.lua
+++ ElvUI/Modules/UnitFrames/Groups/Party.lua
@@ -264,7 +264,7 @@ function UF:Update_PartyFrames(frame, db)
 
 		UF:Configure_ReadyCheckIcon(frame)
 
-		UF:Constgure_AltPowerBar(frame)
+		UF:Configure_AltPowerBar(frame)
 		UF:Configure_CustomTexts(frame)
 	end
 
