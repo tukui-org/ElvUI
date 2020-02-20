af077c0753db4c14b17c9caf70e6123bf9d5ad1e
Simpy <kodi03466@hotmail.com>
2020-02-20 00:36:35 -0500

    Revert "attempt to support utf8 in this gsub, testing it dono if it works"
    This reverts commit 4c4d9e4078e26e7d0518705440c97cc416fc9e65.

--- ElvUI/Core/Tags.lua
+++ ElvUI/Core/Tags.lua
@@ -249,9 +249,6 @@ do
 		return default
 	end
 
-	-- we need to match each char, so any utf8 chars can be matched, mostly ghetto but lets try x-x
-	local chars = '[%w';for l, u in pairs(_G.utf8_lc_uc) do chars = chars .. l .. u end;chars = chars .. ']'
-
 	-- the third arg here is added from the user as like [name:health{ff00ff:00ff00}] or [name:health{class:00ff00}]
 	ElvUF.Tags.Events['name:health'] = 'UNIT_NAME_UPDATE UNIT_FACTION UNIT_HEALTH_FREQUENT UNIT_MAXHEALTH'
 	ElvUF.Tags.Methods['name:health'] = function(unit, _, args)
@@ -269,7 +266,7 @@ do
 			fillColor, baseColor = '|cFFff3333', '|cFFffffff'
 		end
 
-		return gsub(name, chars, fillName)
+		return gsub(name, '[%w%p]', fillName)
 	end
 end
 
