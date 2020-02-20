06a5eba1dc1783c7e720b9a72bf73e270bd3d71a
Simpy <kodi03466@hotmail.com>
2020-02-20 00:07:36 -0500

    Revert "init the tags the first time?"
    This reverts commit 0d480c5662184d6ec2e879044d395ab88157e196.

--- ElvUI/Modules/UnitFrames/UnitFrames.lua
+++ ElvUI/Modules/UnitFrames/UnitFrames.lua
@@ -534,7 +534,6 @@ function UF:Update_AllFrames()
 	for unit in pairs(self.units) do
 		if self.db.units[unit].enable then
 			self[unit]:Update()
-			self[unit]:UpdateTags()
 			self[unit]:Enable()
 			E:EnableMover(self[unit].mover:GetName())
 		else
@@ -546,9 +545,8 @@ function UF:Update_AllFrames()
 
 	for unit, group in pairs(self.groupunits) do
 		if self.db.units[group].enable then
-			self[unit]:Update()
-			self[unit]:UpdateTags()
 			self[unit]:Enable()
+			self[unit]:Update()
 			E:EnableMover(self[unit].mover:GetName())
 		else
 			self[unit]:Disable()
