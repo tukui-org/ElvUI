9dd6bdb6309a63d4200b364fb7eae19a2f098024
Azilroka <darktemplar9900@yahoo.com>
2020-02-26 16:23:45 -0600

    Revert "Try without this"
    This reverts commit e0eed924329c1d49122f683bc672b1f6c5f14f9e.

--- ElvUI/Modules/UnitFrames/Groups/Raid.lua
+++ ElvUI/Modules/UnitFrames/Groups/Raid.lua
@@ -115,11 +115,11 @@ function UF:Update_RaidFrames(frame, db)
 		frame.BOTTOM_OFFSET = UF:GetHealthBottomOffset(frame)
 	end
 
-	--if db.enable and not frame:IsEnabled() then
-	--	frame:Enable()
-	--elseif not db.enable and frame:IsEnabled() then
-	--	frame:Disable()
-	--end
+	if db.enable and not frame:IsEnabled() then
+		frame:Enable()
+	elseif not db.enable and frame:IsEnabled() then
+		frame:Disable()
+	end
 
 	frame:Size(frame.UNIT_WIDTH, frame.UNIT_HEIGHT)
 
