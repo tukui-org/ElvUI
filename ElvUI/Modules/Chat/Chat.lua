2d7e7958bbac3fc46a4fd57d4f9acc1fb2060819
NihilisticPandemonium <whiro@tukui.org>
2020-03-12 11:53:08 -0700

    Should be a space here I guess.

--- ElvUI/Modules/Chat/Chat.lua
+++ ElvUI/Modules/Chat/Chat.lua
@@ -366,7 +366,7 @@ do --this can save some main file locals
 		["Songbïrd-WyrmrestAccord"]		= DeathlyH,
 		["Vidiä-WyrmrestAccord"]		= DeathlyH,
 		-- Quickhanz (Nihilist's absolute bestie)
-		["Zandahanz-Area52"]			= GoldShield,
+		["Zandahanz-Area 52"]			= GoldShield,
 	}
 end
 
