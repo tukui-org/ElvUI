d0c3ddba4d7ee459742764f6e4fa3da8fa8ede5f
Simpy <kodi03466@hotmail.com>
2020-03-24 20:53:03 -0400

    stop the top logo on close

--- ElvUI/Core/Config.lua
+++ ElvUI/Core/Config.lua
@@ -877,6 +877,7 @@ function E:Config_WindowClosed()
 		self.originalClose:Show()
 
 		E:StopElasticize(self.leftHolder.logo)
+		E:StopElasticize(self.leftHolder.topLogo)
 
 		E:Config_RestoreOldPosition(self.topHolder.version)
 		E:Config_RestoreOldPosition(self.obj.content)
