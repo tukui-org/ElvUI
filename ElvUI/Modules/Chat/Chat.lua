9cfd1399bbbaada6c6990e62611fea65c624240a
Simpy <kodi03466@hotmail.com>
2020-04-26 09:06:25 -0400

    let the "close editbox when repeating in combat" let you reopen the editbox to finish what you were saying but it will reclose again if you try to repeat something new, basically this would let you finish what you were saying if you enjoy saying "wheeeeeeee" or "loooooool" during combat etc but forget this setting

--- ElvUI/Modules/Chat/Chat.lua
+++ ElvUI/Modules/Chat/Chat.lua
@@ -518,11 +518,12 @@ function CH:StyleChat(frame)
 	frame:StripTextures(true)
 	_G[name..'ButtonFrame']:Kill()
 
+	local repeatedText
 	local function OnTextChanged(editBox)
 		local text = editBox:GetText()
 		local len = strlen(text)
 
-		if InCombatLockdown() then
+		if (not repeatedText or not strfind(text, repeatedText, 1, true)) and InCombatLockdown() then
 			local MIN_REPEAT_CHARACTERS = E.db.chat.numAllowedCombatRepeat
 			if len > MIN_REPEAT_CHARACTERS then
 				local repeatChar = true
@@ -534,6 +535,7 @@ function CH:StyleChat(frame)
 					end
 				end
 				if repeatChar then
+					repeatedText = text
 					editBox:Hide()
 					return
 				end
@@ -556,6 +558,10 @@ function CH:StyleChat(frame)
 		end
 
 		editbox.characterCount:SetText(len > 0 and (255 - len) or '')
+
+		if repeatedText then
+			repeatedText = nil
+		end
 	end
 
 	--Work around broken SetAltArrowKeyMode API. Code from Prat and modified by Simpy
