6c8232bb7ff3dff97f384372215456cdba28a892
Simpy <kodi03466@hotmail.com>
2020-04-29 15:03:25 -0400

    status report upgrade, will show "Plugins" if only elvui plugins are enabled, otherwise it will show "Yes" it's based on the elvui plugin lib to determine if a plugin is installed

--- ElvUI/Core/StatusReport.lua
+++ ElvUI/Core/StatusReport.lua
@@ -11,17 +11,20 @@ local GetRealZoneText = GetRealZoneText
 local GetSpecialization = GetSpecialization
 local GetSpecializationInfo = GetSpecializationInfo
 
-local function AreOtherAddOnsEnabled()
+function E:AreOtherAddOnsEnabled()
+	local EP, addons, plugins = E.Libs.EP.plugins
+
 	for i = 1, GetNumAddOns() do
 		local name = GetAddOnInfo(i)
-		if name ~= 'ElvUI' and name ~= 'ElvUI_OptionsUI' and E:IsAddOnEnabled(name) then --Loaded or load on demand
-			return 'Yes'
+		if name ~= 'ElvUI' and name ~= 'ElvUI_OptionsUI' and E:IsAddOnEnabled(name) then
+			if EP[name] then plugins = true else addons = true end
 		end
 	end
-	return 'No'
+
+	return addons, plugins
 end
 
-local function GetDisplayMode()
+function E:GetDisplayMode()
 	local window, maximize = GetCVar('gxWindow') == '1', GetCVar('gxMaximize') == '1'
 	return (window and maximize and 'Windowed (Fullscreen)') or (window and 'Windowed') or 'Fullscreen'
 end
@@ -84,7 +87,7 @@ local function GetSpecName()
 	return EnglishSpecName[GetSpecializationInfo(GetSpecialization())]
 end
 
-local function CreateContentLines(num, parent, anchorTo)
+function E:CreateStatusContent(num, parent, anchorTo)
 	local content = CreateFrame('Frame', nil, parent)
 	content:Size(260, (num * 20) + ((num-1)*5)) --20 height and 5 spacing
 	content:Point('TOP', anchorTo, 'BOTTOM',0 , -5)
@@ -119,54 +122,59 @@ local function CloseClicked()
 	end
 end
 
+function E:CreateStatusSection(width, height, parent, anchor1, anchorTo, anchor2, yOffset)
+	local parentWidth, parentHeight = parent:GetSize()
+
+	if width > parentWidth then parent:SetWidth(width + 25) end
+	if height then parent:SetHeight(parentHeight + height) end
+
+	local section = CreateFrame('Frame', nil, parent)
+	section:Size(width, height)
+	section:Point(anchor1, anchorTo, anchor2, 0, yOffset)
+
+	local header = CreateFrame('Frame', nil, section)
+	header:Size(300, 30)
+	header:Point('TOP', section)
+	section.Header = header
+
+	local text = section.Header:CreateFontString(nil, 'ARTWORK', 'SystemFont_Outline')
+	text:Point('TOP')
+	text:Point('BOTTOM')
+	text:SetJustifyH('CENTER')
+	text:SetJustifyV('MIDDLE')
+
+	local font, fontHeight, fontFlags = text:GetFont()
+	text:FontTemplate(font, fontHeight*1.3, fontFlags)
+	section.Header.Text = text
+
+	local leftDivider = section.Header:CreateTexture(nil, 'ARTWORK')
+	leftDivider:Height(8)
+	leftDivider:Point('LEFT', section.Header, 'LEFT', 5, 0)
+	leftDivider:Point('RIGHT', section.Header.Text, 'LEFT', -5, 0)
+	leftDivider:SetTexture('Interface\\Tooltips\\UI-Tooltip-Border')
+	leftDivider:SetTexCoord(0.81, 0.94, 0.5, 1)
+	section.Header.LeftDivider = leftDivider
+
+	local rightDivider = section.Header:CreateTexture(nil, 'ARTWORK')
+	rightDivider:Height(8)
+	rightDivider:Point('RIGHT', section.Header, 'RIGHT', -5, 0)
+	rightDivider:Point('LEFT', section.Header.Text, 'RIGHT', 5, 0)
+	rightDivider:SetTexture('Interface\\Tooltips\\UI-Tooltip-Border')
+	rightDivider:SetTexCoord(0.81, 0.94, 0.5, 1)
+	section.Header.RightDivider = rightDivider
+
+	return section
+end
+
 function E:CreateStatusFrame()
-	local function CreateSection(width, height, parent, anchor1, anchorTo, anchor2, yOffset)
-		local section = CreateFrame('Frame', nil, parent)
-		section:Size(width, height)
-		section:Point(anchor1, anchorTo, anchor2, 0, yOffset)
-
-		local header = CreateFrame('Frame', nil, section)
-		header:Size(300, 30)
-		header:Point('TOP', section)
-		section.Header = header
-
-		local text = section.Header:CreateFontString(nil, 'ARTWORK', 'SystemFont_Outline')
-		text:Point('TOP')
-		text:Point('BOTTOM')
-		text:SetJustifyH('CENTER')
-		text:SetJustifyV('MIDDLE')
-
-		local font, fontHeight, fontFlags = text:GetFont()
-		text:FontTemplate(font, fontHeight*1.3, fontFlags)
-		section.Header.Text = text
-
-		local leftDivider = section.Header:CreateTexture(nil, 'ARTWORK')
-		leftDivider:Height(8)
-		leftDivider:Point('LEFT', section.Header, 'LEFT', 5, 0)
-		leftDivider:Point('RIGHT', section.Header.Text, 'LEFT', -5, 0)
-		leftDivider:SetTexture('Interface\\Tooltips\\UI-Tooltip-Border')
-		leftDivider:SetTexCoord(0.81, 0.94, 0.5, 1)
-		section.Header.LeftDivider = leftDivider
-
-		local rightDivider = section.Header:CreateTexture(nil, 'ARTWORK')
-		rightDivider:Height(8)
-		rightDivider:Point('RIGHT', section.Header, 'RIGHT', -5, 0)
-		rightDivider:Point('LEFT', section.Header.Text, 'RIGHT', 5, 0)
-		rightDivider:SetTexture('Interface\\Tooltips\\UI-Tooltip-Border')
-		rightDivider:SetTexCoord(0.81, 0.94, 0.5, 1)
-		section.Header.RightDivider = rightDivider
-
-		return section
-	end
-
 	--Main frame
 	local StatusFrame = CreateFrame('Frame', 'ElvUIStatusReport', E.UIParent)
-	StatusFrame:Size(320, 555)
 	StatusFrame:Point('CENTER', E.UIParent, 'CENTER')
 	StatusFrame:SetFrameStrata('HIGH')
 	StatusFrame:CreateBackdrop('Transparent', nil, true)
 	StatusFrame.backdrop:SetBackdropColor(0, 0, 0, 0.6)
 	StatusFrame:SetMovable(true)
+	StatusFrame:SetSize(0, 35)
 	StatusFrame:Hide()
 
 	--Close button and script to retoggle the options.
@@ -192,41 +200,28 @@ function E:CreateStatusFrame()
 	titleLogoFrame.LogoBottom = LogoBottom
 
 	--Sections
-	StatusFrame.Section1 = CreateSection(300, 125, StatusFrame, 'TOP', StatusFrame, 'TOP', -30)
-	StatusFrame.Section2 = CreateSection(300, 150, StatusFrame, 'TOP', StatusFrame.Section1, 'BOTTOM', 0)
-	StatusFrame.Section3 = CreateSection(300, 185, StatusFrame, 'TOP', StatusFrame.Section2, 'BOTTOM', 0)
-	StatusFrame.Section4 = CreateSection(300, 60, StatusFrame, 'TOP', StatusFrame.Section3, 'BOTTOM', 0)
-
-	--Section headers
-	StatusFrame.Section1.Header.Text:SetText('|cff1784d1AddOn Info|r')
-	StatusFrame.Section2.Header.Text:SetText('|cff1784d1WoW Info|r')
-	StatusFrame.Section3.Header.Text:SetText('|cff1784d1Character Info|r')
-	StatusFrame.Section4.Header.Text:SetText('|cff1784d1Export To|r')
+	StatusFrame.Section1 = E:CreateStatusSection(300, 125, StatusFrame, 'TOP', StatusFrame, 'TOP', -30)
+	StatusFrame.Section2 = E:CreateStatusSection(300, 150, StatusFrame, 'TOP', StatusFrame.Section1, 'BOTTOM', 0)
+	StatusFrame.Section3 = E:CreateStatusSection(300, 185, StatusFrame, 'TOP', StatusFrame.Section2, 'BOTTOM', 0)
+	StatusFrame.Section4 = E:CreateStatusSection(300, 60, StatusFrame, 'TOP', StatusFrame.Section3, 'BOTTOM', 0)
 
 	--Section content
-	StatusFrame.Section1.Content = CreateContentLines(4, StatusFrame.Section1, StatusFrame.Section1.Header)
-	StatusFrame.Section2.Content = CreateContentLines(5, StatusFrame.Section2, StatusFrame.Section2.Header)
-	StatusFrame.Section3.Content = CreateContentLines(6, StatusFrame.Section3, StatusFrame.Section3.Header)
+	StatusFrame.Section1.Content = E:CreateStatusContent(4, StatusFrame.Section1, StatusFrame.Section1.Header)
+	StatusFrame.Section2.Content = E:CreateStatusContent(5, StatusFrame.Section2, StatusFrame.Section2.Header)
+	StatusFrame.Section3.Content = E:CreateStatusContent(6, StatusFrame.Section3, StatusFrame.Section3.Header)
 	StatusFrame.Section4.Content = CreateFrame('Frame', nil, StatusFrame.Section4)
 	StatusFrame.Section4.Content:Size(240, 25)
 	StatusFrame.Section4.Content:Point('TOP', StatusFrame.Section4.Header, 'BOTTOM', 0, 0)
 
 	--Content lines
-	StatusFrame.Section1.Content.Line1.Text:SetFormattedText('Version of ElvUI: |cff4beb2c%s|r', E.version)
-	StatusFrame.Section1.Content.Line2.Text:SetFormattedText('Other AddOns Enabled: |cff4beb2c%s|r', AreOtherAddOnsEnabled())
 	StatusFrame.Section1.Content.Line3.Text:SetFormattedText('Recommended Scale: |cff4beb2c%s|r', E:PixelBestSize())
 	StatusFrame.Section1.Content.Line4.Text:SetFormattedText('UI Scale Is: |cff4beb2c%s|r', E.global.general.UIScale)
 	StatusFrame.Section2.Content.Line1.Text:SetFormattedText('Version of WoW: |cff4beb2c%s (build %s)|r', E.wowpatch, E.wowbuild)
 	StatusFrame.Section2.Content.Line2.Text:SetFormattedText('Client Language: |cff4beb2c%s|r', GetLocale())
-	StatusFrame.Section2.Content.Line3.Text:SetFormattedText('Display Mode: |cff4beb2c%s|r', GetDisplayMode())
-	StatusFrame.Section2.Content.Line4.Text:SetFormattedText('Resolution: |cff4beb2c%s|r', E.resolution)
 	StatusFrame.Section2.Content.Line5.Text:SetFormattedText('Using Mac Client: |cff4beb2c%s|r', (E.isMacClient == true and 'Yes' or 'No'))
 	StatusFrame.Section3.Content.Line1.Text:SetFormattedText('Faction: |cff4beb2c%s|r', E.myfaction)
 	StatusFrame.Section3.Content.Line2.Text:SetFormattedText('Race: |cff4beb2c%s|r', E.myrace)
 	StatusFrame.Section3.Content.Line3.Text:SetFormattedText('Class: |cff4beb2c%s|r', EnglishClassName[E.myclass])
-	StatusFrame.Section3.Content.Line4.Text:SetFormattedText('Specialization: |cff4beb2c%s|r', GetSpecName())
-	StatusFrame.Section3.Content.Line5.Text:SetFormattedText('Level: |cff4beb2c%s|r', E.mylevel)
-	StatusFrame.Section3.Content.Line6.Text:SetFormattedText('Zone: |cff4beb2c%s|r', GetRealZoneText())
 
 	--Export buttons
 	StatusFrame.Section4.Content.Button1 = CreateFrame('Button', nil, StatusFrame.Section4.Content, 'UIPanelButtonTemplate')
@@ -245,11 +240,24 @@ function E:CreateStatusFrame()
 	return StatusFrame
 end
 
-local function UpdateDynamicValues()
+function E:UpdateStatusFrame()
 	local StatusFrame = E.StatusFrame
 
+	--Section headers
+	local valueColor = E.media.hexvaluecolor
+	StatusFrame.Section1.Header.Text:SetFormattedText('%sAddOn Info|r', valueColor)
+	StatusFrame.Section2.Header.Text:SetFormattedText('%sWoW Info|r', valueColor)
+	StatusFrame.Section3.Header.Text:SetFormattedText('%sCharacter Info|r', valueColor)
+	StatusFrame.Section4.Header.Text:SetFormattedText('%sExport To|r', valueColor)
+
+	local verWarning = E.recievedOutOfDateMessage and 'ff3333' or E.shownUpdatedWhileRunningPopup and 'ff9933'
+	StatusFrame.Section1.Content.Line1.Text:SetFormattedText('Version of ElvUI: |cff%s%s|r', verWarning or '33ff33', E.version)
+
+	local addons, plugins = E:AreOtherAddOnsEnabled()
+	StatusFrame.Section1.Content.Line2.Text:SetFormattedText('Other AddOns Enabled: |cff%s|r', (not addons and plugins and 'ff9933Plugins') or (addons and 'ff3333Yes') or '33ff33No')
+
 	local Section2 = StatusFrame.Section2
-	Section2.Content.Line3.Text:SetFormattedText('Display Mode: |cff4beb2c%s|r', GetDisplayMode())
+	Section2.Content.Line3.Text:SetFormattedText('Display Mode: |cff4beb2c%s|r', E:GetDisplayMode())
 	Section2.Content.Line4.Text:SetFormattedText('Resolution: |cff4beb2c%s|r', E.resolution)
 
 	local Section3 = StatusFrame.Section3
@@ -266,7 +274,7 @@ function E:ShowStatusReport()
 	end
 
 	if not E.StatusFrame:IsShown() then
-		UpdateDynamicValues()
+		E:UpdateStatusFrame()
 		E.StatusFrame:Raise() --Set framelevel above everything else
 		E.StatusFrame:Show()
 	else
