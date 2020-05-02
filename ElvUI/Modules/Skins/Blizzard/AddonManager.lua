78f13163a6fd015e78e210496610495fa3f14e1e
Simpy <kodi03466@hotmail.com>
2020-05-01 21:59:17 -0400

    attempt to fix `AddOn index must be in the range of 1 to 4`

--- ElvUI/Modules/Skins/Blizzard/AddonManager.lua
+++ ElvUI/Modules/Skins/Blizzard/AddonManager.lua
@@ -3,6 +3,7 @@ local S = E:GetModule('Skins')
 
 local _G = _G
 local gsub = gsub
+local GetNumAddOns = GetNumAddOns
 local GetAddOnInfo = GetAddOnInfo
 local GetAddOnEnableState = GetAddOnEnableState
 local UIDropDownMenu_GetSelectedValue = UIDropDownMenu_GetSelectedValue
@@ -37,61 +38,64 @@ function S:AddonList()
 
 	local font = E.Libs.LSM:Fetch('font', 'Expressway')
 	hooksecurefunc('AddonList_Update', function()
+		local numEntrys = GetNumAddOns()
 		for i = 1, maxShown do
 			local index = AddonList.offset + i
-			local entry = _G["AddonListEntry"..i]
-			local string = _G["AddonListEntry"..i.."Title"]
-			local name, title, _, loadable, reason = GetAddOnInfo(index)
+			if index < numEntrys then
+				local entry = _G["AddonListEntry"..i]
+				local string = _G["AddonListEntry"..i.."Title"]
+				local name, title, _, loadable, reason = GetAddOnInfo(index)
 
-			-- Get the character from the current list (nil is all characters)
-			local checkall
-			local character = UIDropDownMenu_GetSelectedValue(AddonCharacterDropDown)
-			if character == true then
-				character = nil
-			else
-				checkall = GetAddOnEnableState(nil, index)
-			end
+				-- Get the character from the current list (nil is all characters)
+				local checkall
+				local character = UIDropDownMenu_GetSelectedValue(AddonCharacterDropDown)
+				if character == true then
+					character = nil
+				else
+					checkall = GetAddOnEnableState(nil, index)
+				end
 
-			local checkstate = GetAddOnEnableState(character, index)
-			local checkbox = _G["AddonListEntry"..i.."Enabled"]
-			local checktex = checkbox:GetCheckedTexture()
-			local enabled = checkstate > 0
+				local checkstate = GetAddOnEnableState(character, index)
+				local checkbox = _G["AddonListEntry"..i.."Enabled"]
+				local checktex = checkbox:GetCheckedTexture()
+				local enabled = checkstate > 0
 
-			string:FontTemplate(font, 13, 'NONE')
-			entry.Status:FontTemplate(font, 11, 'NONE')
-			entry.Reload:FontTemplate(font, 11, 'NONE')
-			entry.Reload:SetTextColor(1.0, 0.3, 0.3)
-			entry.LoadAddonButton.Text:FontTemplate(font, 11, 'NONE')
+				string:FontTemplate(font, 13, 'NONE')
+				entry.Status:FontTemplate(font, 11, 'NONE')
+				entry.Reload:FontTemplate(font, 11, 'NONE')
+				entry.Reload:SetTextColor(1.0, 0.3, 0.3)
+				entry.LoadAddonButton.Text:FontTemplate(font, 11, 'NONE')
 
-			local enabledForSome = not character and checkstate == 1
-			local disabled = not enabled or enabledForSome
+				local enabledForSome = not character and checkstate == 1
+				local disabled = not enabled or enabledForSome
 
-			if disabled then
-				entry.Status:SetTextColor(0.4, 0.4, 0.4)
-			else
-				entry.Status:SetTextColor(0.7, 0.7, 0.7)
-			end
+				if disabled then
+					entry.Status:SetTextColor(0.4, 0.4, 0.4)
+				else
+					entry.Status:SetTextColor(0.7, 0.7, 0.7)
+				end
 
-			if disabled or reason == "DEP_DISABLED" then
-				string:SetText(gsub(title or name, '|c%x%x%x%x%x%x%x%x(.-)|?r?','%1'))
-			end
+				if disabled or reason == "DEP_DISABLED" then
+					string:SetText(gsub(title or name, '|c%x%x%x%x%x%x%x%x(.-)|?r?','%1'))
+				end
 
-			if enabledForSome then
-				string:SetTextColor(0.5, 0.5, 0.5)
-			elseif enabled and (loadable or reason == "DEP_DEMAND_LOADED" or reason == "DEMAND_LOADED") then
-				string:SetTextColor(0.9, 0.9, 0.9)
-			elseif enabled and reason ~= "DEP_DISABLED" then
-				string:SetTextColor(1.0, 0.2, 0.2)
-			else
-				string:SetTextColor(0.3, 0.3, 0.3)
-			end
+				if enabledForSome then
+					string:SetTextColor(0.5, 0.5, 0.5)
+				elseif enabled and (loadable or reason == "DEP_DEMAND_LOADED" or reason == "DEMAND_LOADED") then
+					string:SetTextColor(0.9, 0.9, 0.9)
+				elseif enabled and reason ~= "DEP_DISABLED" then
+					string:SetTextColor(1.0, 0.2, 0.2)
+				else
+					string:SetTextColor(0.3, 0.3, 0.3)
+				end
 
-			if not enabled and checkall == 1 then
-				checktex:SetVertexColor(0.3, 0.3, 0.3)
-				checktex:SetDesaturated(false)
-				checktex:Show()
-			elseif checkstate == 0 then
-				checktex:Hide()
+				if not enabled and checkall == 1 then
+					checktex:SetVertexColor(0.3, 0.3, 0.3)
+					checktex:SetDesaturated(false)
+					checktex:Show()
+				elseif checkstate == 0 then
+					checktex:Hide()
+				end
 			end
 		end
 	end)
