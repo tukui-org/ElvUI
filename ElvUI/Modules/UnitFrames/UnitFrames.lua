4bbfe47cadf64b8902a83d3857d9f14f50220b16
Simpy <kodi03466@hotmail.com>
2020-05-07 17:04:23 -0400

    dont show them with only 5 people

--- ElvUI/Modules/UnitFrames/UnitFrames.lua
+++ ElvUI/Modules/UnitFrames/UnitFrames.lua
@@ -865,8 +865,8 @@ function UF:HandleSmartVisibility()
 			maxPlayers = 40
 		end
 
-		sv.raid.visibility = 'show'
-		sv.raid40.visibility = 'show'
+		sv.raid.visibility = '[@raid6,noexists] hide;show'
+		sv.raid40.visibility = '[@raid6,noexists] hide;show'
 		sv.raid.enable = maxPlayers < 40
 		sv.raid40.enable = maxPlayers == 40
 
