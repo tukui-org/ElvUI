7df45ff01834d95985cd0c247e17ee140d18e0af
Azilroka <darktemplar9900@yahoo.com>
2020-05-08 11:14:45 -0500

    DT Stuff

--- ElvUI/Modules/DataTexts/DataTexts.lua
+++ ElvUI/Modules/DataTexts/DataTexts.lua
@@ -246,9 +246,9 @@ function DT:RegisterPanel(panel, numPoints, anchor, xOff, yOff, vertical)
 end
 
 function DT:Panel_DefaultGlobalSettings(panel)
-	G.datatexts.customPanels[panel] = E:CopyTable({}, G.datatexts.newPanelInfo)
-	G.datatexts.customPanels[panel].enable = nil
-	G.datatexts.customPanels[panel].name = nil
+	E.global.datatexts.customPanels[panel] = E:CopyTable({}, G.datatexts.newPanelInfo)
+	E.global.datatexts.customPanels[panel].enable = nil
+	E.global.datatexts.customPanels[panel].name = nil
 end
 
 function DT:AssignPanelToDataText(dt, data, event, ...)
