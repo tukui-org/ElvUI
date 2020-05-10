61f1f785fe5c6957a4f6172ef8578a03539d218f
Azilroka <darktemplar9900@yahoo.com>
2020-05-10 02:55:55 -0500

    DT: Agility

---
+++ ElvUI/Modules/DataTexts/Agility.lua
@@ -0,0 +1,25 @@
+local E, L, V, P, G = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local DT = E:GetModule('DataTexts')
+
+local displayNumberString = ''
+local lastPanel;
+local join = string.join
+local UnitStat = UnitStat
+
+local function OnEvent(self)
+	self.text:SetFormattedText(displayNumberString, L['Agility: '], UnitStat("player", 2))
+
+	lastPanel = self
+end
+
+local function ValueColorUpdate(hex, r, g, b)
+	displayNumberString = join("", "%s", hex, "%.f|r")
+
+	if lastPanel ~= nil then
+		OnEvent(lastPanel)
+	end
+end
+
+E['valueColorUpdateFuncs'][ValueColorUpdate] = true
+
+DT:RegisterDatatext('Agility', 'Primary', { "UNIT_STATS", "UNIT_AURA", "ACTIVE_TALENT_GROUP_CHANGED", "PLAYER_TALENT_UPDATE"}, OnEvent)
