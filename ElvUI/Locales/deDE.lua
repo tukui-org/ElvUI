9949aebbb3bf3d7757cee3078a6041bab2483f87
Merathilis <neo0608@gmail.com>
2020-05-11 07:05:26 +0000

    Update german locale

--- ElvUI/Locales/deDE.lua
+++ ElvUI/Locales/deDE.lua
@@ -386,12 +386,12 @@ Options:
 ]=]
 
 L["EHELP_COMMANDS"] = ([=[Hier ist eine liste aller wichtigen *ElvUI|r Kommandos:
- */ec|r or */elvui|r  -  Öffnet das *OptionsUI|r.
+ */ec|r or */elvui|r  -  Öffnet die *ElvUI Optionen|r.
  */moveui|r  -  Zeigt alle Ankerpunkte um Elemente zu verschieben.
  */kb|r  -  Aktiviert den Tastenbelegungsmodus.
  */resetui|r  -  Setzt alle Fenster auf ihre Standardposition zurück.
  */bgstats|r  -  Aktiviert Schlachtfeld Statistiken über den Informationsleisten.
- */hdt|r  -  Informationsleisten bearbeiten ohne das *OptionsUI|r öffnen zu müssen.
+ */hdt|r  -  Informationsleisten bearbeiten ohne die *Optionen|r öffnen zu müssen.
  */estatus|r  -  Wichtige Informationen für Supportfragen.
  */egrid|r ^64|r oder ^128|r oder ^256|r  -  Aktiviert ein Pixel Raster.
  */luaerror|r ^on|r oder ^off|r  -  Deaktiviert alle AddOns außer ElvUI.
