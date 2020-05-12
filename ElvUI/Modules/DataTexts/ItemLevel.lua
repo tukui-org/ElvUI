e47a8b90946aa72e0fddff3b6dc0449f9e348b0c
Simpy <kodi03466@hotmail.com>
2020-05-11 23:23:19 -0400

    ilvl colours ^~^

--- ElvUI/Modules/DataTexts/ItemLevel.lua
+++ ElvUI/Modules/DataTexts/ItemLevel.lua
@@ -1,11 +1,14 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local DT = E:GetModule('DataTexts')
 
-local displayString1 = ''
-local displayString2 = ''
-local lastPanel
+local ipairs = ipairs
 local strjoin = strjoin
 local format = format
+local pi = math.pi
+
+local lastPanel
+local displayString1 = ''
+local displayString2 = ''
 
 local ITEM_LEVEL_ABBR = ITEM_LEVEL_ABBR
 local LFG_LIST_ITEM_LEVEL_INSTR_PVP_SHORT = LFG_LIST_ITEM_LEVEL_INSTR_PVP_SHORT
@@ -13,40 +16,42 @@ local STAT_AVERAGE_ITEM_LEVEL = STAT_AVERAGE_ITEM_LEVEL
 local GMSURVEYRATING3 = GMSURVEYRATING3
 
 local GetAverageItemLevel= GetAverageItemLevel
-local GetInstanceInfo = GetInstanceInfo
-local C_PvP_IsWarModeActive = C_PvP.IsWarModeActive
 local GetInventoryItemLink = GetInventoryItemLink
+local GetInventoryItemTexture = GetInventoryItemTexture
 
-local slotID = { 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
+local iconString = '|T%s:13:15:0:0:50:50:4:46:4:46|t %s'
+local slotID = { 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 }
 
-local function OnEvent(self, event)
-	local avgItemLevel, avgItemLevelEquipped, avgItemLevelPvp = GetAverageItemLevel()
-
-	if avgItemLevel == avgItemLevelEquipped then
-		self.text:SetFormattedText(displayString2, ITEM_LEVEL_ABBR, avgItemLevelEquipped)
+local function colorize(num)
+	if num >= 0 then
+		return .1, 1, .1
 	else
-		self.text:SetFormattedText(displayString1, ITEM_LEVEL_ABBR, avgItemLevelEquipped, avgItemLevel)
+		return E:ColorGradient(-(pi/num), 1, .1, .1, 1, 1, .1, .1, 1, .1)
 	end
 end
 
-local function OnEnter(self)
-	local avgItemLevel, avgItemLevelEquipped, avgItemLevelPvp = GetAverageItemLevel()
+local function OnEvent(self)
+	local avg, avgEquipped = GetAverageItemLevel()
+	local same = avg == avgEquipped
+	self.text:SetFormattedText(same and displayString2 or displayString1, ITEM_LEVEL_ABBR, avgEquipped, same and avg or '0')
+end
 
+local function OnEnter(self)
 	DT:SetupTooltip(self)
 
-	DT.tooltip:AddDoubleLine(STAT_AVERAGE_ITEM_LEVEL, format('%0.2f', avgItemLevel), 1, 1, 1, 0, 1, 0)
-	DT.tooltip:AddDoubleLine(GMSURVEYRATING3, format('%0.2f', avgItemLevelEquipped), 1, 1, 1, 0, 1, 0)
-	DT.tooltip:AddDoubleLine(LFG_LIST_ITEM_LEVEL_INSTR_PVP_SHORT, format('%0.2f', avgItemLevelPvp), 1, 1, 1, 0, 1, 0)
+	local avg, avgEquipped, avgPvp = GetAverageItemLevel()
+	DT.tooltip:AddDoubleLine(STAT_AVERAGE_ITEM_LEVEL, format('%0.2f', avg), 1, 1, 1, .1, 1, .1)
+	DT.tooltip:AddDoubleLine(GMSURVEYRATING3, format('%0.2f', avgEquipped), 1, 1, 1, colorize(avgEquipped - avg))
+	DT.tooltip:AddDoubleLine(LFG_LIST_ITEM_LEVEL_INSTR_PVP_SHORT, format('%0.2f', avgPvp), 1, 1, 1, colorize(avgPvp - avg))
+	DT.tooltip:AddLine(' ')
 
-	DT.tooltip:AddLine(" ")
-
-	for _, k in pairs(slotID) do
-		local itemLink = GetInventoryItemLink("player", k)
-		local itemIcon = GetInventoryItemTexture("player", k)
+	for _, k in ipairs(slotID) do
 		local itemInfo = E:GetGearSlotInfo('player', k)
-		if itemInfo and itemInfo.iLvl then
-			local r, g, b = E:ColorGradient((itemInfo.iLvl - avgItemLevel) * 100, 1, 0, 0, 1, 1, 0, 0, 1, 0)
-			DT.tooltip:AddDoubleLine("|T"..itemIcon..":13:15:0:0:50:50:4:46:4:46|t "..itemLink, itemInfo.iLvl, 1, 1, 1, r, g, b)
+		local ilvl = itemInfo and itemInfo.iLvl
+		if ilvl then
+			local link = GetInventoryItemLink('player', k)
+			local icon = GetInventoryItemTexture('player', k)
+			DT.tooltip:AddDoubleLine(format(iconString, icon, link), ilvl, 1, 1, 1, colorize(ilvl - avg))
 		end
 	end
 
@@ -55,14 +60,14 @@ local function OnEnter(self)
 	lastPanel = self
 end
 
-local function ValueColorUpdate(hex, r, g, b)
-	displayString1 = strjoin("", "%s: ", hex, "%0.2f / %0.2f|r")
-	displayString2 = strjoin("", "%s: ", hex, "%0.2f|r")
+local function ValueColorUpdate(hex)
+	displayString1 = strjoin('', '%s: ', hex, '%0.2f / %0.2f|r')
+	displayString2 = strjoin('', '%s: ', hex, '%0.2f|r')
 
 	if lastPanel ~= nil then
 		OnEvent(lastPanel)
 	end
 end
-E["valueColorUpdateFuncs"][ValueColorUpdate] = true
+E.valueColorUpdateFuncs[ValueColorUpdate] = true
 
-DT:RegisterDatatext("Item Level", 'Stats', {"PLAYER_AVG_ITEM_LEVEL_UPDATE"}, OnEvent, nil, nil, OnEnter, nil, LFG_LIST_ITEM_LEVEL_INSTR_SHORT)
+DT:RegisterDatatext('Item Level', 'Stats', {'PLAYER_AVG_ITEM_LEVEL_UPDATE'}, OnEvent, nil, nil, OnEnter, nil, _G.LFG_LIST_ITEM_LEVEL_INSTR_SHORT)
