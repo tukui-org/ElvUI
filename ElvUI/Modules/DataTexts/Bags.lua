5fef7c6f816f5f4d1b0599db5e9c8f9e8ff9d883
Azilroka <darktemplar9900@yahoo.com>
2020-05-17 10:00:14 -0500

    DT: Bag Update

--- ElvUI/Modules/DataTexts/Bags.lua
+++ ElvUI/Modules/DataTexts/Bags.lua
@@ -35,19 +35,21 @@ local function OnEnter(self)
 	DT:SetupTooltip(self)
 
 	for i = 0, NUM_BAG_SLOTS do
-		local bagFreeSlots = GetContainerNumFreeSlots(i)
-		local bagName = GetBagName(i) or 'Backpack'
-		local bagSlots = GetContainerNumSlots(i)
-		local r, g, b, icon = 1, 1, 1, 'Interface/Buttons/Button-Backpack-Up'
-		local r2, g2, b2 = E:ColorGradient(bagFreeSlots/bagSlots, .1, 1, .1, 1, 1, .1, 1, .1, .1)
+		local bagName = GetBagName(i)
+		if bagName then
+			local bagFreeSlots = GetContainerNumFreeSlots(i)
+			local bagSlots = GetContainerNumSlots(i)
+			local r, g, b, icon = 1, 1, 1, 'Interface/Buttons/Button-Backpack-Up'
+			local r2, g2, b2 = E:ColorGradient(bagFreeSlots/bagSlots, .1, 1, .1, 1, 1, .1, 1, .1, .1)
 
-		if i > 0 then
-			local quality = GetInventoryItemQuality("player", 19 + i)
-			r, g, b = GetItemQualityColor(quality or 1)
-			icon = GetInventoryItemTexture("player", 19 + i)
+			if i > 0 then
+				local quality = GetInventoryItemQuality("player", 19 + i)
+				r, g, b = GetItemQualityColor(quality or 1)
+				icon = GetInventoryItemTexture("player", 19 + i)
+			end
+
+			DT.tooltip:AddDoubleLine(format(iconString, icon, bagName), format('%d / %d', bagFreeSlots, bagSlots), r, g, b, r2, g2, b2)
 		end
-
-		DT.tooltip:AddDoubleLine(format(iconString, icon, bagName), format('%d / %d', bagFreeSlots, bagSlots), r, g, b, r2, g2, b2)
 	end
 
 	for i = 1, MAX_WATCHED_TOKENS do
