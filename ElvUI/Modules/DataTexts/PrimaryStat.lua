6787a226074bf8b97e1f5fc8646986c7e5583d75
Azilroka <darktemplar9900@yahoo.com>
2020-05-19 15:52:36 -0500

    Ok BuG lets try this

--- ElvUI/Modules/DataTexts/PrimaryStat.lua
+++ ElvUI/Modules/DataTexts/PrimaryStat.lua
@@ -1,15 +1,15 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local DT = E:GetModule('DataTexts')
 
-local select = select
 local strjoin = strjoin
+local GetNumSpecializations = GetNumSpecializations
 local GetSpecializationInfo = GetSpecializationInfo
 local GetSpecialization = GetSpecialization
 local UnitStat = UnitStat
 local STAT_CATEGORY_ATTRIBUTES = STAT_CATEGORY_ATTRIBUTES
 local SPEC_FRAME_PRIMARY_STAT = SPEC_FRAME_PRIMARY_STAT
 
-local displayString, statID, lastPanel = ''
+local displayString, lastPanel = ''
 
 local SPEC_STAT_STRINGS = {
 	[_G.LE_UNIT_STAT_STRENGTH]	= _G.SPEC_FRAME_PRIMARY_STAT_STRENGTH,
@@ -17,14 +17,20 @@ local SPEC_STAT_STRINGS = {
 	[_G.LE_UNIT_STAT_INTELLECT]	= _G.SPEC_FRAME_PRIMARY_STAT_INTELLECT,
 }
 
+local SPECIALIZATION_CACHE = {}
+
 local function OnEvent(self, event)
-	if statID == nil or (event == 'ACTIVE_TALENT_GROUP_CHANGED' or event == 'PLAYER_ENTERING_WORLD') then
-		statID = select(6, GetSpecializationInfo(GetSpecialization()))
+	if not next(SPECIALIZATION_CACHE) then
+		for index = 1, GetNumSpecializations() do
+			local id, _, _, _, _, statID = GetSpecializationInfo(index)
+			if id then
+				SPECIALIZATION_CACHE[id] = { statID = statID }
+			end
+		end
 	end
 
-	local primStat = UnitStat("player", statID)
-
-	self.text:SetFormattedText(displayString, SPEC_STAT_STRINGS[statID]..': ', primStat)
+	local StatID = SPECIALIZATION_CACHE[GetSpecialization()].statID
+	self.text:SetFormattedText(displayString, SPEC_STAT_STRINGS[StatID]..': ', UnitStat("player", StatID))
 
 	lastPanel = self
 end
