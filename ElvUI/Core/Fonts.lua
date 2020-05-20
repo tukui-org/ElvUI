1586eaa18fe3ea2e470bc0c11292f38c3288b83e
Azilroka <darktemplar9900@yahoo.com>
2020-05-19 19:37:07 -0500

    Implement #181

--- ElvUI/Core/Fonts.lua
+++ ElvUI/Core/Fonts.lua
@@ -40,10 +40,18 @@ function E:UpdateBlizzardFonts()
 		COMBAT = E.Media.Fonts.Invisible
 	end
 
+	if E.private.general.replaceNameFont then
+		_G.UNIT_NAME_FONT		= NAMEFONT
+	end
+
+	if E.private.general.replaceCombatFont then
+		_G.DAMAGE_TEXT_FONT		= COMBAT
+		-- No matter what we do here with the Combat Font, the player Combat Font will always looks like shit, so just dont touch it.
+		--SetFont(_G.CombatTextFont,						COMBAT, 200, 'OUTLINE')						-- number here just increase the font quality.
+	end
+
 	if E.private.general.replaceBlizzFonts then
 		--_G.NAMEPLATE_FONT		= NAMEFONT
-		_G.UNIT_NAME_FONT		= NAMEFONT
-		_G.DAMAGE_TEXT_FONT		= COMBAT
 		_G.STANDARD_TEXT_FONT	= NORMAL
 
 		--SetFont(_G.NumberFontNormal,					LSM:Fetch('font', 'Homespun'), 10, 'MONOCHROMEOUTLINE', 1, 1, 1, 0, 0, 0)
@@ -153,8 +161,5 @@ function E:UpdateBlizzardFonts()
 		SetFont(_G.FriendsFont_11,						NORMAL, 11)
 		SetFont(_G.SpellFont_Small,						NORMAL, 10)
 		SetFont(_G.SubSpellFont,						NORMAL, 10) -- Spellbook Sub Names
-
-		-- No matter what we do here with the Combat Font, the player Combat Font will always looks like shit, so just dont touch it.
-		--SetFont(_G.CombatTextFont,						COMBAT, 200, 'OUTLINE')						-- number here just increase the font quality.
 	end
 end
