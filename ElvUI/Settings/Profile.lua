a958b280c47fa8d4ee858d6c7a7c377a65232dde
Simpy <kodi03466@hotmail.com>
2020-05-19 21:55:43 -0400

    this is dead

--- ElvUI/Settings/Profile.lua
+++ ElvUI/Settings/Profile.lua
@@ -553,7 +553,6 @@ P.nameplates = {
 	font = 'PT Sans Narrow',
 	fontOutline = 'OUTLINE',
 	fontSize = 11,
-	loadDistance = 40,
 	lowHealthThreshold = 0.4,
 	motionType =  'STACKED',
 	nameColoredGlow = false,
