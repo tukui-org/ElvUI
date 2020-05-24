f6d48f4b4e7bb70effe6f53a57d7d42ff345a017
Azilroka <darktemplar9900@yahoo.com>
2020-05-24 18:09:47 -0500

    cache

--- ElvUI/Modules/DataTexts/CustomCurrency.lua
+++ ElvUI/Modules/DataTexts/CustomCurrency.lua
@@ -2,7 +2,7 @@ local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, Private
 local DT = E:GetModule('DataTexts')
 
 local _G = _G
-local tinsert = tinsert
+local tinsert, tremove = tinsert, tremove
 local pairs, format = pairs, format
 local GetCurrencyInfo = GetCurrencyInfo
 local GetCurrencyListInfo = GetCurrencyListInfo
