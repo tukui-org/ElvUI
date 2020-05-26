83c984fa5ca800b03ce12310ab154c295a67c9ef
Simpy <kodi03466@hotmail.com>
2020-05-25 23:43:37 -0400

    fix `/played` in copy chat

--- ElvUI/Modules/Chat/Chat.lua
+++ ElvUI/Modules/Chat/Chat.lua
@@ -720,10 +720,14 @@ do
 		local emoji = (x~="" and x) and strmatch(x, 'elvmoji:%%(.+)')
 		return (emoji and E.Libs.Base64:Decode(emoji)) or y
 	end
+	local fourString = function(v, w, x, y)
+		return format('%s%s%s', v, w, (v and v == "1" and x) or y)
+	end
 	removeIconFromLine = function(text)
 		text = gsub(text, [[|TInterface\TargetingFrame\UI%-RaidTargetingIcon_(%d+):0|t]], raidIconFunc) --converts raid icons into {star} etc, if possible.
 		text = gsub(text, "(%s?)(|?)|[TA].-|[ta](%s?)", stripTextureFunc) --strip any other texture out but keep a single space from the side(s).
 		text = gsub(text, "(|?)|H(.-)|h(.-)|h", hyperLinkFunc) --strip hyperlink data only keeping the actual text.
+		text = gsub(text, "(%d-)(.-)|4(.-):(.-);", fourString) --stuff where it goes 'day' or 'days' like played; tech this is wrong but okayish
 		return text
 	end
 end
