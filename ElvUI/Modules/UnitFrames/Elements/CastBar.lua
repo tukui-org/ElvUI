5f7e1cb99776381104cf9b21cbdf2b59c977f59e
Repooc <tjcooper1982@gmail.com>
2020-06-09 18:35:29 -0400

    I'm stupid, only a few have movers and they are all in individualUnits

--- ElvUI/Modules/UnitFrames/Elements/CastBar.lua
+++ ElvUI/Modules/UnitFrames/Elements/CastBar.lua
@@ -83,8 +83,7 @@ function UF:Construct_Castbar(frame, moverName)
 	if moverName then
 		local name = frame:GetName()
 		local configName = name:gsub('^ElvUF_', ''):lower()
-		local configSection = (configName == "party" or configName == "arena" or configName == "boss") and "groupUnits" or "individualUnits"
-		E:CreateMover(castbar.Holder, name..'CastbarMover', moverName, nil, -6, nil, 'ALL,SOLO', nil, 'unitframe,'..configSection..','..configName..',castbar')
+		E:CreateMover(castbar.Holder, name..'CastbarMover', moverName, nil, -6, nil, 'ALL,SOLO', nil, 'unitframe,individualUnits,'..configName..',castbar')
 	end
 
 	local icon = button:CreateTexture(nil, "ARTWORK")
