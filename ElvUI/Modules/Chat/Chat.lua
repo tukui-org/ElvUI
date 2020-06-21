96942eaceb4454654277c80bf58df9f70578d55d
Simpy <kodi03466@hotmail.com>
2020-06-21 03:41:37 -0400

    :L

--- ElvUI/Modules/Chat/Chat.lua
+++ ElvUI/Modules/Chat/Chat.lua
@@ -2039,7 +2039,7 @@ end
 
 local protectLinks = {}
 function CH:CheckKeyword(message, author)
-	local letInCombat = (not CH.db.noAlertInCombat or not InCombatLockdown())
+	local letInCombat = not CH.db.noAlertInCombat or not InCombatLockdown()
 	local letSound = not CH.SoundTimer and (CH.db.keywordSound ~= 'None' and author ~= PLAYER_NAME) and letInCombat
 
 	for hyperLink in gmatch(message, "|%x+|H.-|h.-|h|r") do
