fc8edc4d40913bb9d2a768690c85376b967d8197
Azilroka <darktemplar9900@yahoo.com>
2020-06-28 19:48:12 -0500

    Skins: Quest Border Color Issue

--- ElvUI/Modules/Skins/Blizzard/Quest.lua
+++ ElvUI/Modules/Skins/Blizzard/Quest.lua
@@ -271,6 +271,7 @@ function S:BlizzardQuestFrames()
 			RewardButton.NameFrame:Hide()
 
 			hooksecurefunc(RewardButton.IconBorder, 'SetVertexColor', function(_, r, g, b) RewardButton.Icon.backdrop:SetBackdropBorderColor(r, g, b) end)
+			hooksecurefunc(RewardButton.IconBorder, 'Hide', function() RewardButton.Icon.backdrop:SetBackdropBorderColor(unpack(E.media.bordercolor)) end)
 		end
 	end)
 
