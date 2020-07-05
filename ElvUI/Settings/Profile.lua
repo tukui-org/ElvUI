2dd7921461619f3b64cb897d31cdee257276f262
Simpy <kodi03466@hotmail.com>
2020-07-05 16:08:14 -0400

    this looks bad on sham

--- ElvUI/Settings/Profile.lua
+++ ElvUI/Settings/Profile.lua
@@ -1854,7 +1854,7 @@ P.unitframe.units.player.health.position = 'LEFT'
 P.unitframe.units.player.health.text_format = '[healthcolor][health:current-percent]'
 P.unitframe.units.player.health.xOffset = 2
 P.unitframe.units.player.power.position = 'RIGHT'
-P.unitframe.units.player.power.text_format = '[classpowercolor][classpower:current< |cFFffffff- ][powercolor][power:current]'
+P.unitframe.units.player.power.text_format = '[classpowercolor][classpower:current]  [powercolor][power:current]'
 P.unitframe.units.player.power.xOffset = -2
 
 P.unitframe.units.target.aurabar.maxDuration = 120
