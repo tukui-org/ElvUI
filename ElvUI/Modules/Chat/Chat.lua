6e60b98ead222fefe6c606aa7b0254585371c044
Simpy <kodi03466@hotmail.com>
2020-07-08 23:52:22 -0400

    fix whisper sound

--- ElvUI/Modules/Chat/Chat.lua
+++ ElvUI/Modules/Chat/Chat.lua
@@ -1861,7 +1861,7 @@ function CH:ChatFrame_MessageEventHandler(frame, event, arg1, arg2, arg3, arg4, 
 			local accessID = ChatHistory_GetAccessID(chatGroup, chatTarget)
 			local typeID = ChatHistory_GetAccessID(infoType, chatTarget, arg12 or arg13)
 
-			local alertType = CH.db.channelAlerts[historyTypes[chatType]]
+			local alertType = CH.db.channelAlerts[historyTypes[event]]
 			if notChatHistory and not CH.SoundTimer and (arg2 ~= PLAYER_NAME and alertType and alertType ~= 'None') and (not CH.db.noAlertInCombat or not InCombatLockdown()) then
 				CH.SoundTimer = E:Delay(5, CH.ThrottleSound)
 				PlaySoundFile(LSM:Fetch("sound", alertType), "Master")
@@ -2242,8 +2242,6 @@ function CH:DisplayChatHistory()
 		return
 	end
 
-	CH.SoundTimer = true
-
 	for _, chat in ipairs(_G.CHAT_FRAMES) do
 		for _, d in ipairs(data) do
 			if type(d) == 'table' then
@@ -2261,8 +2259,6 @@ function CH:DisplayChatHistory()
 			end
 		end
 	end
-
-	CH.SoundTimer = nil
 end
 
 tremove(_G.ChatTypeGroup.GUILD, 2)
