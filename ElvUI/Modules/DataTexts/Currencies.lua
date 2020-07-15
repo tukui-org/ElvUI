417207b26369f5418b9ef7c8208fb5b37631fbae
Simpy <kodi03466@hotmail.com>
2020-07-15 15:00:14 -0400

    Revert "" -> '"
    This reverts commit de66ee01859ea270581a4f2947249dc618c8411e.

--- ElvUI/Modules/DataTexts/Currencies.lua
+++ ElvUI/Modules/DataTexts/Currencies.lua
@@ -10,11 +10,11 @@ local GetMoney = GetMoney
 
 local BONUS_ROLL_REWARD_MONEY = BONUS_ROLL_REWARD_MONEY
 
-local iconString = '|T%s:16:16:0:0:64:64:4:60:4:60|t'
+local iconString = "|T%s:16:16:0:0:64:64:4:60:4:60|t"
 DT.CurrencyList = { GOLD = BONUS_ROLL_REWARD_MONEY, BACKPACK = 'Backpack' }
 
 local function OnClick()
-	_G.ToggleCharacter('TokenFrame')
+	_G.ToggleCharacter("TokenFrame")
 end
 
 local function GetInfo(id)
@@ -31,7 +31,7 @@ end
 
 local goldText
 local function OnEvent(self)
-	goldText = E:FormatMoney(GetMoney(), E.global.datatexts.settings.Currencies.goldFormat or 'BLIZZARD', not E.global.datatexts.settings.Currencies.goldCoins)
+	goldText = E:FormatMoney(GetMoney(), E.global.datatexts.settings.Currencies.goldFormat or "BLIZZARD", not E.global.datatexts.settings.Currencies.goldCoins)
 
 	local displayed = E.db.datatexts.currencies.displayedCurrency
 	if displayed == 'BACKPACK' then
@@ -39,12 +39,12 @@ local function OnEvent(self)
 		for i = 1, 3 do
 			local _, num, icon = GetBackpackCurrencyInfo(i)
 			if num then
-				displayString = (i > 1 and displayString..' ' or displayString)..format('%s %s', format(iconString, icon), E:ShortValue(num))
+				displayString = (i > 1 and displayString..' ' or displayString)..format("%s %s", format(iconString, icon), E:ShortValue(num))
 			end
 		end
 
 		self.text:SetText(displayString == '' and goldText or displayString)
-	elseif displayed == 'GOLD' then
+	elseif displayed == "GOLD" then
 		self.text:SetText(goldText)
 	else
 		local id = tonumber(displayed)
@@ -54,12 +54,12 @@ local function OnEvent(self)
 		if not name then return end
 
 		local style = E.db.datatexts.currencies.displayStyle
-		if style == 'ICON' then
-			self.text:SetFormattedText('%s %s', icon, E:ShortValue(num))
-		elseif style == 'ICON_TEXT' then
-			self.text:SetFormattedText('%s %s %s', icon, name, E:ShortValue(num))
+		if style == "ICON" then
+			self.text:SetFormattedText("%s %s", icon, E:ShortValue(num))
+		elseif style == "ICON_TEXT" then
+			self.text:SetFormattedText("%s %s %s", icon, name, E:ShortValue(num))
 		else --ICON_TEXT_ABBR
-			self.text:SetFormattedText('%s %s %s', icon, E:AbbreviateString(name), E:ShortValue(num))
+			self.text:SetFormattedText("%s %s %s", icon, E:AbbreviateString(name), E:ShortValue(num))
 		end
 	end
 end
@@ -92,8 +92,8 @@ local function OnEnter(self)
 		DT.tooltip:AddLine(' ')
 	end
 
-	DT.tooltip:AddDoubleLine(L["Gold"]..':', goldText, nil, nil, nil, 1, 1, 1)
+	DT.tooltip:AddDoubleLine(L["Gold"]..":", goldText, nil, nil, nil, 1, 1, 1)
 	DT.tooltip:Show()
 end
 
-DT:RegisterDatatext('Currencies', nil, {'PLAYER_MONEY', 'SEND_MAIL_MONEY_CHANGED', 'SEND_MAIL_COD_CHANGED', 'PLAYER_TRADE_MONEY', 'TRADE_MONEY_CHANGED', 'CHAT_MSG_CURRENCY', 'CURRENCY_DISPLAY_UPDATE'}, OnEvent, nil, OnClick, OnEnter, nil, _G.CURRENCY)
+DT:RegisterDatatext('Currencies', nil, {"PLAYER_MONEY", "SEND_MAIL_MONEY_CHANGED", "SEND_MAIL_COD_CHANGED", "PLAYER_TRADE_MONEY", "TRADE_MONEY_CHANGED", "CHAT_MSG_CURRENCY", "CURRENCY_DISPLAY_UPDATE"}, OnEvent, nil, OnClick, OnEnter, nil, _G.CURRENCY)
