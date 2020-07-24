08d1971782a0092180c86a264aebe6be6cc99b2c
Simpy <kodi03466@hotmail.com>
2020-07-23 20:19:20 -0400

    shorter

--- ElvUI/Modules/Blizzard/Kill.lua
+++ ElvUI/Modules/Blizzard/Kill.lua
@@ -2,13 +2,14 @@ local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, Private
 local B = E:GetModule('Blizzard')
 
 local _G = _G
+local hooksecurefunc = hooksecurefunc
 function B:KillBlizzard()
 	if E.global.general.disableTutorialButtons then
 		_G.HelpPlate:Kill()
 		_G.HelpPlateTooltip:Kill()
 
-		hooksecurefunc(_G.HelpTip, "Show", function(self, parent, info, relativeRegion)
-			_G.HelpTip.Hide(self, parent, info.text)
+		hooksecurefunc(_G.HelpTip, "Show", function(tip, parent, info)
+			tip:Hide(parent, info.text)
 		end)
 	end
 end
