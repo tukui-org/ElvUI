3200d84ca120bc47393f8ef9f203703d1ba2cfc1
Merathilis <neo0608@gmail.com>
2020-08-01 08:39:31 +0200

    Updated WeeklyRewards skin

--- ElvUI/Modules/Skins/Blizzard/WeeklyRewards.lua
+++ ElvUI/Modules/Skins/Blizzard/WeeklyRewards.lua
@@ -2,18 +2,49 @@ local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, Private
 local S = E:GetModule('Skins')
 
 local _G = _G
+local pairs = pairs
+
+-- Credits Siweia | AuroraClassic
+
+local function SkinActivityFrame(frame, isObject)
+	if frame.Border then
+		if isObject then
+			frame.Border:SetAlpha(0)
+		else
+			frame.Border:SetTexCoord(.926, 1, 0, 1)
+			frame.Border:SetSize(25, 137)
+			frame.Border:SetPoint("LEFT", frame, "RIGHT", 3, 0)
+		end
+	end
+	frame:CreateBackdrop()
+end
 
 function S:Blizzard_WeeklyRewards()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.weeklyRewards) then return end
 
+	-- /run UIParent_OnEvent({}, "WEEKLY_REWARDS_SHOW")
 	local frame = _G.WeeklyRewardsFrame
+
 	if E.private.skins.parchmentRemoverEnable then
 		frame:StripTextures()
 	end
-
 	frame:CreateBackdrop('Transparent')
-
 	S:HandleCloseButton(frame.CloseButton)
+
+	local header = frame.HeaderFrame
+	if E.private.skins.parchmentRemoverEnable then
+		header:StripTextures()
+	end
+	header:CreateBackdrop('Transparent')
+	header:Point('TOP', 1, -42)
+
+	SkinActivityFrame(frame.RaidFrame)
+	SkinActivityFrame(frame.MythicFrame)
+	SkinActivityFrame(frame.PVPFrame)
+
+	for _, frame in pairs(frame.Activities) do
+		SkinActivityFrame(frame, true)
+	end
 end
 
 S:AddCallbackForAddon('Blizzard_WeeklyRewards')
