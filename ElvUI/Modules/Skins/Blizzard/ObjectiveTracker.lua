013cef655e281793382c155c215c7ecd73d67d2d
Paweł Jastrzębski <pawelj@iosphe.re>
2020-08-01 17:58:40 +0200

    Objective Tracker: Added missing module

--- ElvUI/Modules/Skins/Blizzard/ObjectiveTracker.lua
+++ ElvUI/Modules/Skins/Blizzard/ObjectiveTracker.lua
@@ -171,6 +171,7 @@ function S:ObjectiveTrackerFrame()
 	hooksecurefunc(_G.WORLD_QUEST_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)			--[Skin]: World Quest Progress Bar
 	hooksecurefunc(_G.DEFAULT_OBJECTIVE_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)	--[Skin]: Quest Progress Bar
 	hooksecurefunc(_G.SCENARIO_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)			--[Skin]: Scenario Progress Bar
+	hooksecurefunc(_G.CAMPAIGN_QUEST_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)		--[Skin]: Campaign Progress Bar
 	hooksecurefunc(_G.QUEST_TRACKER_MODULE,"AddTimerBar",SkinTimerBars)						--[Skin]: Quest Timer Bar
 	hooksecurefunc(_G.SCENARIO_TRACKER_MODULE,"AddTimerBar",SkinTimerBars)					--[Skin]: Scenario Timer Bar
 	hooksecurefunc(_G.ACHIEVEMENT_TRACKER_MODULE,"AddTimerBar",SkinTimerBars)				--[Skin]: Achievement Timer Bar
