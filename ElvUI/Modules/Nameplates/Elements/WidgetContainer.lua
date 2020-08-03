a33d67adc7aecfd488dc0d0826130bbb08d3fb83
Merathilis <neo0608@gmail.com>
2020-08-03 12:18:14 +0200

    NP: Widget

--- ElvUI/Modules/Nameplates/Elements/WidgetContainer.lua
+++ ElvUI/Modules/Nameplates/Elements/WidgetContainer.lua
@@ -1,31 +1,33 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local NP = E:GetModule('NamePlates')
 
+local ipairs = ipairs
+
 function NP:Construct_WidgetContainer(nameplate)
 	local WidgetContainer = CreateFrame('Frame', nil, nameplate, 'UIWidgetContainerTemplate')
 	WidgetContainer:Hide()
-	WidgetContainer:SetPoint('BOTTOM', nameplate, 'TOP')
+	WidgetContainer:SetPoint('BOTTOM', nameplate, 'TOP', 0, 5)
 	WidgetContainer:UnregisterForWidgetSet()
 
 	return WidgetContainer
 end
 
 function NP.Widget_DefaultLayout(widgetContainerFrame, sortedWidgets)
-	widgetContainerFrame.horizontalRowContainerPool:ReleaseAll();
+	widgetContainerFrame.horizontalRowContainerPool:ReleaseAll()
 
 	for index, widgetFrame in ipairs(sortedWidgets) do
-		widgetFrame:ClearAllPoints();
+		widgetFrame:ClearAllPoints()
 
 		-- Default this to top-bottom until there is a better way...
 		if index == 1 then
 			-- This is the first widget in the set, so just anchor it to the widget container
-			widgetFrame:SetPoint("TOP", widgetContainerFrame);
+			widgetFrame:SetPoint('TOP', widgetContainerFrame)
 		else
 			-- This is not the first widget in the set, so anchor it to the previous widget
-			local relative = sortedWidgets[index - 1];
-			widgetFrame:SetPoint("TOP", relative, "BOTTOM", 0, widgetContainerFrame.verticalAnchorYOffset or -10);
+			local relative = sortedWidgets[index - 1]
+			widgetFrame:SetPoint('TOP', relative, 'BOTTOM', 0, widgetContainerFrame.verticalAnchorYOffset or -10)
 		end
 
-		widgetFrame:SetParent(widgetContainerFrame);
+		widgetFrame:SetParent(widgetContainerFrame)
 	end
 end
