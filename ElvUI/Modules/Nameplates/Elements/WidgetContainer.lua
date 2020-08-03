b3dfda14ff35c04a96faa7123d86b5ddc30fe74c
Merathilis <neo0608@gmail.com>
2020-08-03 19:44:04 +0200

    @Azilroka... plx dont punch :D

--- ElvUI/Modules/Nameplates/Elements/WidgetContainer.lua
+++ ElvUI/Modules/Nameplates/Elements/WidgetContainer.lua
@@ -12,22 +12,96 @@ function NP:Construct_WidgetContainer(nameplate)
 	return WidgetContainer
 end
 
+-- Copy & Pasta Blizz Code
 function NP.Widget_DefaultLayout(widgetContainerFrame, sortedWidgets)
-	widgetContainerFrame.horizontalRowContainerPool:ReleaseAll()
+	local horizontalRowContainer = nil;
+
+	widgetContainerFrame.horizontalRowContainerPool:ReleaseAll();
 
 	for index, widgetFrame in ipairs(sortedWidgets) do
-		widgetFrame:ClearAllPoints()
+		widgetFrame:ClearAllPoints();
 
-		-- Default this to top-bottom until there is a better way...
-		if index == 1 then
-			-- This is the first widget in the set, so just anchor it to the widget container
-			widgetFrame:SetPoint('TOP', widgetContainerFrame)
+		local widgetSetUsesVertical = widgetContainerFrame.widgetSetLayoutDirection == Enum.UIWidgetSetLayoutDirection.Vertical;
+		local widgetUsesVertical = widgetFrame.layoutDirection == Enum.UIWidgetLayoutDirection.Vertical;
+
+		local useOverlapLayout = widgetFrame.layoutDirection == Enum.UIWidgetLayoutDirection.Overlap;
+		local useVerticalLayout = widgetUsesVertical or (widgetFrame.layoutDirection == Enum.UIWidgetLayoutDirection.Default and widgetSetUsesVertical);
+
+		if useOverlapLayout then
+			-- This widget uses overlap layout
+
+			if index == 1 then
+				-- But this is the first widget in the set, so just anchor it to the widget container
+				if widgetSetUsesVertical then
+					widgetFrame:SetPoint(widgetContainerFrame.verticalAnchorPoint, widgetContainerFrame);
+				else
+					widgetFrame:SetPoint(widgetContainerFrame.horizontalAnchorPoint, widgetContainerFrame);
+				end
+			else
+				-- This is not the first widget in the set, so anchor it so it overlaps the previous widget
+				local relative = sortedWidgets[index - 1];
+				if widgetSetUsesVertical then
+					-- Overlap it vertically
+					widgetFrame:SetPoint(widgetContainerFrame.verticalAnchorPoint, relative, widgetContainerFrame.verticalAnchorPoint, 0, 0);
+				else
+					-- Overlap it horizontally
+					widgetFrame:SetPoint(widgetContainerFrame.horizontalAnchorPoint, relative, widgetContainerFrame.horizontalAnchorPoint, 0, 0);
+				end
+			end
+
+			widgetFrame:SetParent(widgetContainerFrame);
+		elseif useVerticalLayout then
+			-- This widget uses vertical layout
+
+			if index == 1 then
+				-- This is the first widget in the set, so just anchor it to the widget container
+				widgetFrame:SetPoint(widgetContainerFrame.verticalAnchorPoint, widgetContainerFrame);
+			else
+				-- This is not the first widget in the set, so anchor it to the previous widget (or the horizontalRowContainer if that exists)
+				local relative = horizontalRowContainer or sortedWidgets[index - 1];
+				widgetFrame:SetPoint(widgetContainerFrame.verticalAnchorPoint, relative, widgetContainerFrame.verticalRelativePoint, 0, widgetContainerFrame.verticalAnchorYOffset);
+
+				if horizontalRowContainer then
+					-- This widget is vertical, so horizontalRowContainer is done. Call layout on it and clear horizontalRowContainer
+					horizontalRowContainer:Layout();
+					horizontalRowContainer = nil;
+				end
+			end
+
+			widgetFrame:SetParent(widgetContainerFrame);
 		else
-			-- This is not the first widget in the set, so anchor it to the previous widget
-			local relative = sortedWidgets[index - 1]
-			widgetFrame:SetPoint('TOP', relative, 'BOTTOM', 0, widgetContainerFrame.verticalAnchorYOffset or -10)
-		end
+			-- This widget uses horizontal layout
 
-		widgetFrame:SetParent(widgetContainerFrame)
+			local forceNewRow = widgetFrame.layoutDirection == Enum.UIWidgetLayoutDirection.HorizontalForceNewRow;
+			local needNewRowContainer = not horizontalRowContainer or forceNewRow;
+			if needNewRowContainer then
+				-- We either don't have a horizontalRowContainer or this widget has requested a new row be started
+				if horizontalRowContainer then
+					horizontalRowContainer:Layout();
+				end
+
+				local newHorizontalRowContainer = widgetContainerFrame.horizontalRowContainerPool:Acquire();
+				newHorizontalRowContainer:Show();
+
+				if index == 1 then
+					-- This is the first widget in the set, so just anchor it to the widget container
+					newHorizontalRowContainer:SetPoint(widgetContainerFrame.verticalAnchorPoint, widgetContainerFrame, widgetContainerFrame.verticalAnchorPoint);
+				else
+					-- This is not the first widget in the set, so anchor it to the previous widget (or the horizontalRowContainer if that exists)
+					local relative = horizontalRowContainer or sortedWidgets[index - 1];
+					newHorizontalRowContainer:SetPoint(widgetContainerFrame.verticalAnchorPoint, relative, widgetContainerFrame.verticalRelativePoint, 0, widgetContainerFrame.verticalAnchorYOffset);
+				end
+				widgetFrame:SetPoint("TOPLEFT", newHorizontalRowContainer);
+				widgetFrame:SetParent(newHorizontalRowContainer);
+
+				-- The old horizontalRowContainer is no longer needed for anchoring, so set it to newHorizontalRowContainer
+				horizontalRowContainer = newHorizontalRowContainer;
+			else
+				-- horizontalRowContainer already existed, so we just keep going in it, anchoring to the previous widget
+				local relative = sortedWidgets[index - 1];
+				widgetFrame:SetParent(horizontalRowContainer);
+				widgetFrame:SetPoint(widgetContainerFrame.horizontalAnchorPoint, relative, widgetContainerFrame.horizontalRelativePoint, widgetContainerFrame.horizontalAnchorXOffset, 0);
+			end
+		end
 	end
 end
