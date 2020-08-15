63008efab95f8ca798df6c2ff2eef5c0bbf9f442
Elv <elv@tukui.org>
2020-08-15 11:42:09 -0400

    dont show "1" on quest icons

--- ElvUI/Modules/Nameplates/Plugins/QuestIcons.lua
+++ ElvUI/Modules/Nameplates/Plugins/QuestIcons.lua
@@ -232,7 +232,7 @@ local function Update(self, event, unit)
 				icon:ClearAllPoints()
 				icon:SetPoint(newPosition, element, newPosition, (strmatch(setPosition, "LEFT") and -offset) or offset, 0)
 
-				if questType ~= "CHAT" and icon.Text then
+				if questType ~= "CHAT" and icon.Text and (not isPercent and objectiveCount > 1) then
 					icon.Text:SetText((isPercent and objectiveCount.."%") or objectiveCount)
 				end
 
