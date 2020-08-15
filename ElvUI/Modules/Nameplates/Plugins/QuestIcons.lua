45371e83e0472a91e8e81c00a1a34e4a2fd3b219
Simpy <kodi03466@hotmail.com>
2020-08-15 14:16:25 -0400

    quest icon % oops

--- ElvUI/Modules/Nameplates/Plugins/QuestIcons.lua
+++ ElvUI/Modules/Nameplates/Plugins/QuestIcons.lua
@@ -231,7 +231,7 @@ local function Update(self, event, unit)
 				icon:ClearAllPoints()
 				icon:SetPoint(newPosition, element, newPosition, (strmatch(setPosition, "LEFT") and -offset) or offset, 0)
 
-				if questType ~= "CHAT" and icon.Text and (not isPercent and objectiveCount > 1) then
+				if questType ~= "CHAT" and icon.Text and (isPercent or objectiveCount > 1) then
 					icon.Text:SetText((isPercent and objectiveCount.."%") or objectiveCount)
 				end
 
