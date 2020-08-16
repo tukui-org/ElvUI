067e7081342f80de951faa9db099588c2641413f
Paweł Jastrzębski <pawelj@iosphe.re>
2020-08-16 11:08:00 +0200

    Changed behavior of INCOMPATIBLE_ADDON popup
    Locale string need to be keep short as it overflow too easily.

--- ElvUI/Core/StaticPopups.lua
+++ ElvUI/Core/StaticPopups.lua
@@ -178,8 +178,8 @@ E.PopupDialogs.DISABLE_INCOMPATIBLE_ADDON = {
 
 E.PopupDialogs.INCOMPATIBLE_ADDON = {
 	text = L["INCOMPATIBLE_ADDON"],
-	OnAccept = function() DisableAddOn(E.PopupDialogs.INCOMPATIBLE_ADDON.addon); ReloadUI(); end,
-	OnCancel = function() E.private[strlower(E.PopupDialogs.INCOMPATIBLE_ADDON.module)].enable = false; ReloadUI(); end,
+	OnAccept = function() E.private[strlower(E.PopupDialogs.INCOMPATIBLE_ADDON.module)].enable = false; ReloadUI(); end,
+	OnCancel = function() DisableAddOn(E.PopupDialogs.INCOMPATIBLE_ADDON.addon); ReloadUI(); end,
 	button3 = L["Disable Warning"],
 	OnAlt = function ()
 		E:StaticPopup_Hide('INCOMPATIBLE_ADDON')
