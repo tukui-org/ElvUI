4dbd4c4447a2b3756d7cd802badf706366a9cc1b
Simpy <kodi03466@hotmail.com>
2020-08-16 15:59:00 -0400

    keep that api the same

--- ElvUI/Core/Toolkit.lua
+++ ElvUI/Core/Toolkit.lua
@@ -11,14 +11,14 @@ local hooksecurefunc = hooksecurefunc
 local backdropr, backdropg, backdropb, backdropa, borderr, borderg, borderb = 0, 0, 0, 1, 0, 0, 0
 
 -- 8.2 restricted frame check
-function E:PointsRestricted(frame)
+function E:SetPointsRestricted(frame)
 	if frame and not pcall(frame.GetPoint, frame) then
 		return true
 	end
 end
 
 function E:SafeGetPoint(frame)
-	if frame and frame.GetPoint and not E:PointsRestricted(frame) then
+	if frame and frame.GetPoint and not E:SetPointsRestricted(frame) then
 		return frame:GetPoint()
 	end
 end
@@ -207,7 +207,7 @@ local function SetOutside(obj, anchor, xOffset, yOffset, anchor2)
 	anchor = anchor or obj:GetParent()
 
 	assert(anchor)
-	if E:PointsRestricted(obj) or obj:GetPoint() then
+	if E:SetPointsRestricted(obj) or obj:GetPoint() then
 		obj:ClearAllPoints()
 	end
 
@@ -222,7 +222,7 @@ local function SetInside(obj, anchor, xOffset, yOffset, anchor2)
 	anchor = anchor or obj:GetParent()
 
 	assert(anchor)
-	if E:PointsRestricted(obj) or obj:GetPoint() then
+	if E:SetPointsRestricted(obj) or obj:GetPoint() then
 		obj:ClearAllPoints()
 	end
 
