5bfa4f4af666f7d0521cfa7f4b3faf08ecb6b393
Simpy <kodi03466@hotmail.com>
2020-08-22 08:27:21 -0400

    shorter

--- ElvUI/Modules/Nameplates/Elements/Plugins.lua
+++ ElvUI/Modules/Nameplates/Elements/Plugins.lua
@@ -217,8 +217,9 @@ end
 
 function NP:Update_Fader(nameplate)
 	local db = NP:PlateDB(nameplate)
+	local vis = db.visibility
 
-	if (not db.visibility) or db.visibility.showAlways then
+	if not vis or vis.showAlways then
 		if nameplate:IsElementEnabled('Fader') then
 			nameplate:DisableElement('Fader')
 
@@ -240,10 +241,10 @@ function NP:Update_Fader(nameplate)
 			nameplate.Fader:SetOption('Casting', true)
 		end
 
-		nameplate.Fader:SetOption('Combat', db.visibility.showInCombat)
-		nameplate.Fader:SetOption('PlayerTarget', db.visibility.showWithTarget)
-		nameplate.Fader:SetOption('DelayAlpha', (db.visibility.alphaDelay > 0 and db.visibility.alphaDelay) or nil)
-		nameplate.Fader:SetOption('Delay', (db.visibility.hideDelay > 0 and db.visibility.hideDelay) or nil)
+		nameplate.Fader:SetOption('Combat', vis.showInCombat)
+		nameplate.Fader:SetOption('PlayerTarget', vis.showWithTarget)
+		nameplate.Fader:SetOption('DelayAlpha', (vis.alphaDelay > 0 and vis.alphaDelay) or nil)
+		nameplate.Fader:SetOption('Delay', (vis.hideDelay > 0 and vis.hideDelay) or nil)
 
 		nameplate.Fader:ForceUpdate()
 	end
