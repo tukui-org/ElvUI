0353f7006dc2a861aefd32e502ce3b7bfac18e06
Simpy <kodi03466@hotmail.com>
2020-08-22 04:36:19 -0400

    locales use double quotes :")

--- ElvUI/Modules/DataTexts/Missions.lua
+++ ElvUI/Modules/DataTexts/Missions.lua
@@ -56,7 +56,7 @@ local LE_GARRISON_TYPE_8_0 = Enum.GarrisonType.Type_8_0
 local LE_GARRISON_TYPE_9_0 = Enum.GarrisonType.Type_9_0
 local RESEARCH_TIME_LABEL = RESEARCH_TIME_LABEL
 local DATE_COMPLETED = DATE_COMPLETED:gsub('(%%s)', '|cFF33FF33%1|r') -- 'Completed: |cFF33FF33%s|r'
-local BODYGUARD_LEVEL_XP_FORMAT = L['Rank'] .. ' %d (%d/%d)'
+local BODYGUARD_LEVEL_XP_FORMAT = L["Rank"] .. ' %d (%d/%d)'
 local EXPANSION_NAME5 = EXPANSION_NAME5 -- 'Warlords of Draenor'
 local EXPANSION_NAME6 = EXPANSION_NAME6 -- 'Legion'
 local EXPANSION_NAME7 = EXPANSION_NAME7 -- 'Battle for Azeroth'
@@ -71,15 +71,15 @@ local callingsData = {}
 local widgetIDs = {
 	Alliance = {
 		56156, -- A Tempered Blade
-		{L['Farseer Ori'], 1940},
-		{L['Hunter Akana'], 1613},
-		{L['Bladesman Inowari'], 1966}
+		{L["Farseer Ori"], 1940},
+		{L["Hunter Akana"], 1613},
+		{L["Bladesman Inowari"], 1966}
 	},
 	Horde = {
 		55500, -- Save a Friend
-		{L['Neri Sharpfin'], 1621},
-		{L['Poen Gillbrack'], 1622},
-		{L['Vim Brineheart'], 1920}
+		{L["Neri Sharpfin"], 1621},
+		{L["Poen Gillbrack"], 1622},
+		{L["Vim Brineheart"], 1920}
 	}
 }
 local covenantTreeIDs = {
@@ -205,7 +205,7 @@ local function OnEnter()
 	DT.tooltip:ClearLines()
 
 	DT.tooltip:AddLine(EXPANSION_NAME8, 1, .5, 0)
-	DT.tooltip:AddDoubleLine(L['Mission(s) Report:'], AddInfo(1813), nil, nil, nil, 1, 1, 1)
+	DT.tooltip:AddDoubleLine(L["Mission(s) Report:"], AddInfo(1813), nil, nil, nil, 1, 1, 1)
 	AddInProgressMissions(LE_FOLLOWER_TYPE_GARRISON_9_0)
 
 	if C_CovenantCallings_AreCallingsUnlocked() then
@@ -218,7 +218,7 @@ local function OnEnter()
 		end
 		if questNum > 0 then
 			DT.tooltip:AddLine(' ')
-			DT.tooltip:AddLine(format('%s %s', questNum, L['Calling Quest(s) available.']))
+			DT.tooltip:AddLine(format('%s %s', questNum, L["Calling Quest(s) available."]))
 		end
 	end
 
@@ -231,7 +231,7 @@ local function OnEnter()
 		-- Battle for Azeroth
 		DT.tooltip:AddLine(' ')
 		DT.tooltip:AddLine(EXPANSION_NAME7, 1, .5, 0)
-		DT.tooltip:AddDoubleLine(L['Mission(s) Report:'], AddInfo(1560), nil, nil, nil, 1, 1, 1)
+		DT.tooltip:AddDoubleLine(L["Mission(s) Report:"], AddInfo(1560), nil, nil, nil, 1, 1, 1)
 		AddInProgressMissions(LE_FOLLOWER_TYPE_GARRISON_8_0)
 
 		-- Island Expeditions
@@ -258,13 +258,13 @@ local function OnEnter()
 		local widgetGroup = widgetIDs[E.myfaction]
 		if E.MapInfo.mapID == NAZJATAR_MAP_ID and widgetGroup and C_QuestLog_IsQuestFlaggedCompleted(widgetGroup[1]) then
 			DT.tooltip:AddLine(' ')
-			DT.tooltip:AddLine(L['Nazjatar Follower XP'])
+			DT.tooltip:AddLine(L["Nazjatar Follower XP"])
 
 			for i = 2, 4 do
 				local npcName, widgetID = unpack(widgetGroup[i])
 				local cur, toNext, _, rank, maxRank = E:GetWidgetInfoBase(widgetID)
 				if npcName and rank then
-					DT.tooltip:AddDoubleLine(npcName, (maxRank and L['Max Rank']) or BODYGUARD_LEVEL_XP_FORMAT:format(rank, cur, toNext), 1, 1, 1)
+					DT.tooltip:AddDoubleLine(npcName, (maxRank and L["Max Rank"]) or BODYGUARD_LEVEL_XP_FORMAT:format(rank, cur, toNext), 1, 1, 1)
 				end
 			end
 		end
@@ -275,7 +275,7 @@ local function OnEnter()
 		-- Legion
 		DT.tooltip:AddLine(' ')
 		DT.tooltip:AddLine(EXPANSION_NAME6, 1, .5, 0)
-		DT.tooltip:AddDoubleLine(L['Mission(s) Report:'], AddInfo(1220), nil, nil, nil, 1, 1, 1)
+		DT.tooltip:AddDoubleLine(L["Mission(s) Report:"], AddInfo(1220), nil, nil, nil, 1, 1, 1)
 
 		AddInProgressMissions(LE_FOLLOWER_TYPE_GARRISON_7_0)
 		AddFollowerInfo(LE_GARRISON_TYPE_7_0)
@@ -303,11 +303,11 @@ local function OnEnter()
 		-- Warlords of Draenor
 		DT.tooltip:AddLine(' ')
 		DT.tooltip:AddLine(EXPANSION_NAME5, 1, .5, 0)
-		DT.tooltip:AddDoubleLine(L['Mission(s) Report:'], AddInfo(824), nil, nil, nil, 1, 1, 1)
+		DT.tooltip:AddDoubleLine(L["Mission(s) Report:"], AddInfo(824), nil, nil, nil, 1, 1, 1)
 		AddInProgressMissions(LE_FOLLOWER_TYPE_GARRISON_6_0)
 
 		DT.tooltip:AddLine(' ')
-		DT.tooltip:AddDoubleLine(L['Naval Mission(s) Report:'], AddInfo(1101), nil, nil, nil, 1, 1 , 1)
+		DT.tooltip:AddDoubleLine(L["Naval Mission(s) Report:"], AddInfo(1101), nil, nil, nil, 1, 1 , 1)
 		AddInProgressMissions(LE_FOLLOWER_TYPE_GARRISON_6_2)
 
 		--Buildings
@@ -320,7 +320,7 @@ local function OnEnter()
 				if name and shipmentsTotal then
 					if AddLine then
 						DT.tooltip:AddLine(' ')
-						DT.tooltip:AddLine(L['Building(s) Report:'])
+						DT.tooltip:AddLine(L["Building(s) Report:"])
 						AddLine = false
 					end
 
