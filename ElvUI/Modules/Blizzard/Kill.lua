b8febe3ddaf91e207dc6c7e79795df8c6b174d3c
Simpy <kodi03466@hotmail.com>
2020-09-03 17:56:04 -0400

    help tip can block frames, ig we can make a list?

--- ElvUI/Modules/Blizzard/Kill.lua
+++ ElvUI/Modules/Blizzard/Kill.lua
@@ -3,13 +3,23 @@ local B = E:GetModule('Blizzard')
 
 local _G = _G
 local hooksecurefunc = hooksecurefunc
+
+-- these have a block frame until you do the little tutorial
+local allowTipsFrom = {
+	GarrisonMissionTutorialFrame = true,
+	VoidStorageFrame = true
+}
+
 function B:KillBlizzard()
 	if E.global.general.disableTutorialButtons then
 		_G.HelpPlate:Kill()
 		_G.HelpPlateTooltip:Kill()
 
 		hooksecurefunc(_G.HelpTip, 'Show', function(tip, parent, info)
-			tip:Hide(parent, info.text)
+			local parentName = parent.GetName and parent:GetName()
+			if not allowTipsFrom[parentName] then
+				tip:Hide(parent, info.text)
+			end
 		end)
 	end
 end
