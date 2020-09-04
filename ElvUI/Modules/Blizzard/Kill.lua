2b7ec1f3beba5d22edf58800cd42714d708de063
Simpy <kodi03466@hotmail.com>
2020-09-04 01:12:57 -0400

    Revert "new method to deal with tutorials"
    This reverts commit 5b3bc15f853dd94d0a1e8778ec4b3f3d4b876222.

--- ElvUI/Modules/Blizzard/Kill.lua
+++ ElvUI/Modules/Blizzard/Kill.lua
@@ -2,9 +2,24 @@ local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, Private
 local B = E:GetModule('Blizzard')
 
 local _G = _G
+local hooksecurefunc = hooksecurefunc
+
+-- these have a block frame until you do the little tutorial
+local allowTipsFrom = {
+	GarrisonMissionTutorialFrame = true,
+	VoidStorageFrame = true
+}
+
 function B:KillBlizzard()
 	if E.global.general.disableTutorialButtons then
-		_G.TutorialFrame:UnregisterAllEvents()
-		_G.HelpTip:ForceHideAll()
+		_G.HelpPlate:Kill()
+		_G.HelpPlateTooltip:Kill()
+
+		hooksecurefunc(_G.HelpTip, 'Show', function(tip, parent, info)
+			local parentName = parent.GetName and parent:GetName()
+			if not allowTipsFrom[parentName] then
+				tip:Hide(parent, info.text)
+			end
+		end)
 	end
 end
