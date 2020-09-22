9091e59200c716dda5d8422fc501f17469f6c5ee
Simpy <kodi03466@hotmail.com>
2020-09-22 19:15:44 -0400

    safety first? :L

--- ElvUI/Modules/Blizzard/Kill.lua
+++ ElvUI/Modules/Blizzard/Kill.lua
@@ -3,8 +3,11 @@ local B = E:GetModule('Blizzard')
 
 local _G = _G
 local hooksecurefunc = hooksecurefunc
+local InCombatLockdown = InCombatLockdown
 
 local function AcknowledgeTips()
+	if InCombatLockdown() then return end -- just incase cause this code path will call SetCVar
+
 	for frame in _G.HelpTip.framePool:EnumerateActive() do
 		frame:Acknowledge()
 	end
