157e498f0c59f2f7f38a0953b499d5472c67246e
Azilroka <darktemplar9900@yahoo.com>
2020-10-10 20:34:19 -0500

    Remove this

--- ElvUI/Core/Core.lua
+++ ElvUI/Core/Core.lua
@@ -1382,7 +1382,7 @@ function E:DBConversions()
 	end
 
 	if E.global.unitframe.DebuffHighlightColors then
-		E.global.unitframe.AuraHighlightColors = E:CopyTable(E.global.unitframe.AuraHighlightColors, E.global.unitframe.DebuffHighlightColors)
+		E:CopyTable(E.global.unitframe.AuraHighlightColors, E.global.unitframe.DebuffHighlightColors)
 		E.global.unitframe.DebuffHighlightColors = nil
 	end
 end
