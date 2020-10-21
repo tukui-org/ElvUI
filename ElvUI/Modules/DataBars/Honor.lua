79c4e2f33bcfb94ef9d55b353745f22bc21e6c01
Merathilis <neo0608@gmail.com>
2020-10-21 12:17:41 +0200

    Fix Honor DataBar options. @Azilroka

--- ElvUI/Modules/DataBars/Honor.lua
+++ ElvUI/Modules/DataBars/Honor.lua
@@ -3,9 +3,12 @@ local DB = E:GetModule('DataBars')
 
 local _G = _G
 local format = format
+
+local InCombatLockdown = InCombatLockdown
 local UnitHonor = UnitHonor
 local UnitHonorLevel = UnitHonorLevel
 local UnitHonorMax = UnitHonorMax
+local UnitIsPVP = UnitIsPVP
 local TogglePVPUI = TogglePVPUI
 local HONOR = HONOR
 
@@ -13,7 +16,10 @@ local CurrentHonor, MaxHonor, CurrentLevel, PercentHonor, RemainingHonor
 
 function DB:HonorBar_Update(event, unit)
 	local bar = DB.StatusBars.Honor
-	if not DB.db.honor.enable or (event == 'PLAYER_FLAGS_CHANGED' and unit ~= 'player') then
+	if event == 'PLAYER_FLAGS_CHANGED' and unit ~= 'player' then return end
+
+	if not DB.db.honor.enable or (DB.db.honor.hideInCombat and (event == 'PLAYER_REGEN_DISABLED' or InCombatLockdown())) or
+		(DB.db.honor.hideOutsidePvP and not UnitIsPVP('player')) or (DB.db.honor.hideBelowMaxLevel and E.mylevel < _G.MAX_PLAYER_LEVEL) then
 		bar:Hide()
 		bar.holder:Hide()
 		return
