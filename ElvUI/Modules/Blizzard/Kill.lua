65a3516fd86459bf15ebffea7b80794d4a671e39
Simpy <kodi03466@hotmail.com>
2020-10-21 01:25:59 -0400

    attempt 2

--- ElvUI/Modules/Blizzard/Kill.lua
+++ ElvUI/Modules/Blizzard/Kill.lua
@@ -13,6 +13,12 @@ local function AcknowledgeTips()
 	end
 end
 
+local function ShutdownNPE(NPE)
+	if NPE and NPE:GetIsActive() then
+		NPE:RegisterComplete()
+	end
+end
+
 function B:DisableHelpTip()
 	if not E.global.general.disableTutorialButtons then return end
 
@@ -20,8 +26,17 @@ function B:DisableHelpTip()
 	hooksecurefunc(_G.HelpTip, 'Show', AcknowledgeTips)
 
 	local NPE = _G.NewPlayerExperience
-	if NPE and NPE:GetIsActive() then
-		NPE:RegisterComplete()
+	if NPE then
+		ShutdownNPE(NPE)
+	else
+		local frame = CreateFrame('Frame')
+		frame:RegisterEvent('ADDON_LOADED')
+		frame:SetScript('OnEvent', function(f, event, addon)
+			if addon == 'Blizzard_NewPlayerExperience' then
+				ShutdownNPE()
+				f:UnregisterEvent(event)
+			end
+		end)
 	end
 end
 
@@ -29,4 +44,3 @@ function B:KillBlizzard()
 	_G.Display_UIScaleSlider:Kill()
 	_G.Display_UseUIScale:Kill()
 end
-
