60ca3711784fe3e39b085ac353bfc1c1b183c1d4
Simpy <kodi03466@hotmail.com>
2020-10-21 16:08:30 -0400

    not 50, this way so in SL its ok

--- ElvUI/Modules/DataBars/Azerite.lua
+++ ElvUI/Modules/DataBars/Azerite.lua
@@ -8,7 +8,6 @@ local format = format
 local InCombatLockdown = InCombatLockdown
 local HasArtifactEquipped = HasArtifactEquipped
 local SocketInventoryItem = SocketInventoryItem
-local IsPlayerAtEffectiveMaxLevel = IsPlayerAtEffectiveMaxLevel
 local UIParentLoadAddOn = UIParentLoadAddOn
 local ToggleFrame = ToggleFrame
 local Item = Item
@@ -135,7 +134,7 @@ function DB:AzeriteBar()
 	DB:CreateBarBubbles(Azerite)
 
 	Azerite.ShouldHide = function()
-		return (DB.db.azerite.hideAtMaxLevel and C_AzeriteItem_IsAzeriteItemAtMaxLevel()) or not IsPlayerAtEffectiveMaxLevel()
+		return (DB.db.azerite.hideAtMaxLevel and C_AzeriteItem_IsAzeriteItemAtMaxLevel()) or (E.mylevel ~= 50)
 	end
 
 	E:CreateMover(Azerite.holder, 'AzeriteBarMover', L["Azerite Bar"], nil, nil, nil, nil, nil, 'databars,azerite')
