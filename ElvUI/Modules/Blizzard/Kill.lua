dee50c94d5a227bb200d863ce4b46a2fc23ff836
Simpy <kodi03466@hotmail.com>
2020-10-21 01:47:52 -0400

    shut off new player experience with elvui (might code support for it later but for right now some users are upset to have to 1-10 with the their UI)

--- ElvUI/Modules/Blizzard/Kill.lua
+++ ElvUI/Modules/Blizzard/Kill.lua
@@ -5,6 +5,11 @@ local _G = _G
 local hooksecurefunc = hooksecurefunc
 local InCombatLockdown = InCombatLockdown
 
+function B:KillBlizzard()
+	_G.Display_UIScaleSlider:Kill()
+	_G.Display_UseUIScale:Kill()
+end
+
 local function AcknowledgeTips()
 	if InCombatLockdown() then return end -- just incase cause this code path will call SetCVar
 
@@ -13,29 +18,25 @@ local function AcknowledgeTips()
 	end
 end
 
-function B:ShutdownNPE()
+function B:DisableHelpTip() -- auto complete helptips
+	if not E.global.general.disableTutorialButtons then return end
+
+	hooksecurefunc(_G.HelpTip, 'Show', AcknowledgeTips)
+	E:Delay(1, AcknowledgeTips)
+end
+
+-- NOTE: ActionBars heavily conflicts with NPE
+local function ShutdownNPE()
 	local NPE = _G.NewPlayerExperience
 	if NPE and NPE:GetIsActive() then
 		NPE:Shutdown()
 	end
 end
 
-function B:DisableHelpTip()
-	if not E.global.general.disableTutorialButtons then return end
-
-	-- auto complete helptips
-	AcknowledgeTips()
-	hooksecurefunc(_G.HelpTip, 'Show', AcknowledgeTips)
-
-	-- disable new player experience
+function B:DisableNPE() -- disable new player experience
 	if _G.NewPlayerExperience then
-		B:ShutdownNPE()
+		ShutdownNPE()
 	else
-		B:RegisterEvent('ADDON_LOADED', 'ShutdownNPE')
+		B:RegisterEvent('ADDON_LOADED', ShutdownNPE)
 	end
-end
-
-function B:KillBlizzard()
-	_G.Display_UIScaleSlider:Kill()
-	_G.Display_UseUIScale:Kill()
 end
