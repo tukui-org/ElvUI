5d4c8ea4f81ee843137fa5f1cd77327a8ff5a9ae
Simpy <kodi03466@hotmail.com>
2020-10-22 02:16:52 -0400

    mimic phase icon points

--- ElvUI/Modules/UnitFrames/Elements/PhaseIndicator.lua
+++ ElvUI/Modules/UnitFrames/Elements/PhaseIndicator.lua
@@ -41,6 +41,7 @@ function UF:Configure_PhaseIcon(frame)
 	local size = 32 * (frame.db.phaseIndicator.scale or 1)
 	PhaseIndicator:Size(size)
 	PhaseIndicator.Center:Size(size)
+	PhaseIndicator.Center:SetAllPoints()
 
 	if frame.db.phaseIndicator.enable and not frame:IsElementEnabled('PhaseIndicator') then
 		frame:EnableElement('PhaseIndicator')
