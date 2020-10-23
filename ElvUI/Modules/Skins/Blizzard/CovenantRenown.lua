189d00d354e8fa1238924a9da9a3671f10345889
Merathilis <neo0608@gmail.com>
2020-10-23 11:30:28 +0200

    Update CovenantRenown Skin

--- ElvUI/Modules/Skins/Blizzard/CovenantRenown.lua
+++ ElvUI/Modules/Skins/Blizzard/CovenantRenown.lua
@@ -2,12 +2,33 @@ local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, Private
 local S = E:GetModule('Skins')
 
 local _G = _G
+local next = next
+
+local hooksecurefunc = hooksecurefunc
 
 function S:Blizzard_CovenantRenown()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.covenantRenown) then return end
 
 	local frame = _G.CovenantRenownFrame
 	S:HandleCloseButton(frame.CloseButton)
+
+	hooksecurefunc(frame.TrackFrame, 'Init', function(self)
+		for _, element in next, self.Elements do
+			if not element.IsSkinned then
+				element.LevelBorder:SetAlpha(0)
+				element.IsSkinned = true
+			end
+		end
+	end)
+
+	if E.private.skins.parchmentRemoverEnable then
+		frame:CreateBackdrop('Transparent')
+
+		hooksecurefunc(frame, 'SetUpTextureKits', function(self)
+			self:StripTextures()
+			self.CloseButton.Border:Hide()
+		end)
+	end
 end
 
 S:AddCallbackForAddon('Blizzard_CovenantRenown')
