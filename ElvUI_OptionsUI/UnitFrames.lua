2233aa420c63194eabc6f42ca6d6429019abda88
Caedis <1590-Caedis@users.noreply.git.tukui.org>
2020-11-06 15:51:21 -0600

    Add options for castbar time and text font options

--- ElvUI_OptionsUI/UnitFrames.lua
+++ ElvUI_OptionsUI/UnitFrames.lua
@@ -1034,14 +1034,41 @@ local function GetOptionsTable_Castbar(hasTicks, updateFunc, groupName, numUnits
 						get = function(info) return E.db.unitframe.units[groupName].castbar[info[#info]] end,
 						set = function(info, value) E.db.unitframe.units[groupName].castbar[info[#info]] = value; updateFunc(UF, groupName, numUnits) end,
 						args = {
-							xOffsetText = {
+							textUseGeneralFontSettings = {
 								order = 1,
+								type = 'toggle',
+								name = L["Use General UnitFrames Font"],
+							},
+							textFont = {
+								order = 2,
+								type = 'select',
+								dialogControl = 'LSM30_Font',
+								name = L["Font"],
+								values = _G.AceGUIWidgetLSMlists.font,
+								disabled = function() return E.db.unitframe.units[groupName].castbar.textUseGeneralFontSettings end
+							},
+							textFontSize = {
+								order = 3,
+								type = 'range',
+								name = L["Font Size"],
+								min = 7, max = 212, step = 1,
+								disabled = function() return E.db.unitframe.units[groupName].castbar.textUseGeneralFontSettings end
+							},
+							textFontStyle = {
+								order = 4,
+								type = 'select',
+								name = L["Font Outline"],
+								values = C.Values.FontFlags,
+								disabled = function() return E.db.unitframe.units[groupName].castbar.textUseGeneralFontSettings end
+							},
+							xOffsetText = {
+								order = 4,
 								type = 'range',
 								name = L["X-Offset"],
 								min = -100, max = 100, step = 1,
 							},
 							yOffsetText = {
-								order = 2,
+								order = 5,
 								type = 'range',
 								name = L["Y-Offset"],
 								min = -50, max = 50, step = 1,
@@ -1056,14 +1083,41 @@ local function GetOptionsTable_Castbar(hasTicks, updateFunc, groupName, numUnits
 						get = function(info) return E.db.unitframe.units[groupName].castbar[info[#info]] end,
 						set = function(info, value) E.db.unitframe.units[groupName].castbar[info[#info]] = value; updateFunc(UF, groupName, numUnits) end,
 						args = {
-							xOffsetTime = {
+							timeUseGeneralFontSettings = {
 								order = 1,
+								type = 'toggle',
+								name = L["Use General UnitFrames Font"],
+							},
+							timeFont = {
+								order = 2,
+								type = 'select',
+								dialogControl = 'LSM30_Font',
+								name = L["Font"],
+								values = _G.AceGUIWidgetLSMlists.font,
+								disabled = function() return E.db.unitframe.units[groupName].castbar.timeUseGeneralFontSettings end
+							},
+							timeFontSize = {
+								order = 3,
+								type = 'range',
+								name = L["Font Size"],
+								min = 7, max = 212, step = 1,
+								disabled = function() return E.db.unitframe.units[groupName].castbar.timeUseGeneralFontSettings end
+							},
+							timeFontStyle = {
+								order = 4,
+								type = 'select',
+								name = L["Font Outline"],
+								values = C.Values.FontFlags,
+								disabled = function() return E.db.unitframe.units[groupName].castbar.timeUseGeneralFontSettings end
+							},
+							xOffsetText = {
+								order = 4,
 								type = 'range',
 								name = L["X-Offset"],
 								min = -100, max = 100, step = 1,
 							},
-							yOffsetTime = {
-								order = 2,
+							yOffsetText = {
+								order = 5,
 								type = 'range',
 								name = L["Y-Offset"],
 								min = -50, max = 50, step = 1,
