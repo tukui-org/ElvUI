4613c8096ef101f8d66c6f648ff64bba520b8c60
Simpy <kodi03466@hotmail.com>
2020-11-07 19:59:07 -0500

    prep 12.11

--- ElvUI/Modules/ActionBars/ActionBars.lua
+++ ElvUI/Modules/ActionBars/ActionBars.lua
@@ -2,7 +2,7 @@ local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, Private
 local AB = E:GetModule('ActionBars')
 
 local _G = _G
-local unpack = unpack
+local next, unpack = next, unpack
 local ipairs, pairs, select, strmatch = ipairs, pairs, select, strmatch
 local format, gsub, strsplit, strfind = format, gsub, strsplit, strfind
 
