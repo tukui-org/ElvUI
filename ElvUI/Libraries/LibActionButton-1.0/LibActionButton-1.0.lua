b922e1dfd9de531d2f56c46b5a0ff6e1f8b81024
Simpy <kodi03466@hotmail.com>
2020-11-10 16:39:56 -0500

    prep 12.12

--- ElvUI/Libraries/LibActionButton-1.0/LibActionButton-1.0.lua
+++ ElvUI/Libraries/LibActionButton-1.0/LibActionButton-1.0.lua
@@ -29,7 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ]]
 local MAJOR_VERSION = "LibActionButton-1.0-ElvUI"
-local MINOR_VERSION = 21 -- the real minor version is 79
+local MINOR_VERSION = 22 -- the real minor version is 79
 
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
 local lib, oldversion = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
