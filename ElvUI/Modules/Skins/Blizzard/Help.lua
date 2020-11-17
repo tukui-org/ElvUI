426e0d1ea20e95ca7c4f9ad76218f4563a212a7c
Simpy <kodi03466@hotmail.com>
2020-11-17 13:32:34 -0500

    super rare upgrade for help frame makes it at least 20% cooler

--- ElvUI/Modules/Skins/Blizzard/Help.lua
+++ ElvUI/Modules/Skins/Blizzard/Help.lua
@@ -6,12 +6,18 @@ local _G = _G
 function S:HelpFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.help) then return end
 
-	local helpFrame = _G.HelpFrame
-	helpFrame:StripTextures()
-	helpFrame:CreateBackdrop('Transparent')
-	S:HandleCloseButton(_G.HelpFrameCloseButton, helpFrame.backdrop)
+	local main = _G.HelpFrame
+	main:StripTextures()
+	main:CreateBackdrop('Transparent')
+	main.backdrop:SetOutside(main, 8, 8)
+	S:HandleCloseButton(_G.HelpFrameCloseButton, main.backdrop)
 
-	_G.HelpBrowser.BrowserInset:StripTextures()
+	local browser = _G.HelpBrowser
+	browser.BrowserInset:StripTextures()
+	browser:CreateBackdrop()
+	browser.backdrop:ClearAllPoints()
+	browser.backdrop:SetPoint('TOPLEFT', browser, 'TOPLEFT', -1, 1)
+	browser.backdrop:SetPoint('BOTTOMRIGHT', browser, 'BOTTOMRIGHT', 1, -2)
 end
 
 S:AddCallback('HelpFrame')
