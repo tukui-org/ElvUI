a2ce57267d68b02525562cc8d2d91f0e732e9b75
Caedis <1590-Caedis@users.noreply.git.tukui.org>
2020-11-29 11:56:47 -0600

    Fix the styling of the torghast buffs button when pressed

--- ElvUI/Modules/Blizzard/ObjectiveFrame.lua
+++ ElvUI/Modules/Blizzard/ObjectiveFrame.lua
@@ -53,7 +53,13 @@ function B:SetupTorghastBuffFrame()
 	local buffsPos = E.db.general.torghastBuffsPosition or 'AUTO'
 	if buffsPos == 'RIGHT' or (buffsPos == 'AUTO' and IsFramePositionedLeft(_G.ScenarioBlocksFrame)) then
 		container.List:Point('TOPLEFT', container, 'TOPRIGHT', 15, 1)
-		--container:GetNormalTexture()
+
+		container.List:SetScript('OnShow', function(self)
+			self.button:SetHighlightAtlas("jailerstower-animapowerbutton-highlight", true)
+			self.button:SetPushedAtlas("jailerstower-animapowerbutton-normalpressed", true)
+			self.button:SetButtonState("NORMAL")
+			self.button:SetButtonState("PUSHED", true)
+		end)
 	else
 		container.List:Point('TOPRIGHT', container, 'TOPLEFT', 15, 1)
 	end
