c87c956e914216c73a29bea60b5eeada3a6c39a0
Azilroka <darktemplar9900@yahoo.com>
2020-12-31 20:16:47 -0600

    Short Value Tags

--- ElvUI/Core/Math.lua
+++ ElvUI/Core/Math.lua
@@ -231,20 +231,20 @@ function E:GetXYOffset(position, forcedX, forcedY)
 	end
 end
 
-function E:GetFormattedText(style, min, max, dec)
+function E:GetFormattedText(style, min, max, dec, short)
 	if max == 0 then max = 1 end
 
 	if style == 'CURRENT' or ((style == 'CURRENT_MAX' or style == 'CURRENT_MAX_PERCENT' or style == 'CURRENT_PERCENT') and min == max) then
-		return format(E.GetFormattedTextStyles.CURRENT, E:ShortValue(min, dec))
+		return format(E.GetFormattedTextStyles.CURRENT, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min))
 	else
 		local useStyle = E.GetFormattedTextStyles[style]
 		if not useStyle then return end
 
 		if style == 'DEFICIT' then
 			local deficit = max - min
-			return (deficit > 0 and format(useStyle, E:ShortValue(deficit, dec))) or ''
+			return (deficit > 0 and format(useStyle, short and E:ShortValue(deficit, dec) or BreakUpLargeNumbers(deficit))) or ''
 		elseif style == 'CURRENT_MAX' then
-			return format(useStyle, E:ShortValue(min, dec), E:ShortValue(max, dec))
+			return format(useStyle, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min), short and E:ShortValue(max, dec) or BreakUpLargeNumbers(max))
 		elseif style == 'PERCENT' or style == 'CURRENT_PERCENT' or style == 'CURRENT_MAX_PERCENT' then
 			if dec then useStyle = gsub(useStyle, '%d', tonumber(dec) or 0) end
 			local perc = min / max * 100
@@ -252,9 +252,9 @@ function E:GetFormattedText(style, min, max, dec)
 			if style == 'PERCENT' then
 				return format(useStyle, perc)
 			elseif style == 'CURRENT_PERCENT' then
-				return format(useStyle, E:ShortValue(min, dec), perc)
+				return format(useStyle, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min), perc)
 			elseif style == 'CURRENT_MAX_PERCENT' then
-				return format(useStyle, E:ShortValue(min, dec), E:ShortValue(max, dec), perc)
+				return format(useStyle, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min), short and E:ShortValue(max, dec) or BreakUpLargeNumbers(max), perc)
 			end
 		end
 	end
