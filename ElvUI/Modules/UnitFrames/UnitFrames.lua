23bd2c4877caa9c690bef07a66ca111cc4c2adcd
Azilroka <darktemplar9900@yahoo.com>
2021-03-27 13:17:52 -0500

    Party doesn't need to be set to Raid. Thanks @Thaya

--- ElvUI/Modules/UnitFrames/UnitFrames.lua
+++ ElvUI/Modules/UnitFrames/UnitFrames.lua
@@ -904,7 +904,7 @@ function UF:CreateHeader(parent, groupFilter, overrideName, template, groupName,
 
 	local header = ElvUF:SpawnHeader(overrideName, headerTemplate, nil,
 		'oUF-initialConfigFunction', format('self:SetWidth(%d); self:SetHeight(%d);', db.width, db.height),
-		'groupFilter', groupFilter, 'showParty', true, 'showRaid', true, 'showSolo', true,
+		'groupFilter', groupFilter, 'showParty', true, 'showRaid', group ~= "party", 'showSolo', true,
 		template and 'template', template
 	)
 
