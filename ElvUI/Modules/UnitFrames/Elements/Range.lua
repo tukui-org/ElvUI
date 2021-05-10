6d86a093f61348b1334a0d4c82afbabc1cf6ba46
Simpy <kodi03466@hotmail.com>
2021-05-09 22:32:21 -0400

    E:GetGroupUnit to use in Range.lua

--- ElvUI/Modules/UnitFrames/Elements/Range.lua
+++ ElvUI/Modules/UnitFrames/Elements/Range.lua
@@ -3,36 +3,14 @@ local UF = E:GetModule('UnitFrames')
 local RangeCheck = E.Libs.RangeCheck
 
 local UnitCanAttack = UnitCanAttack
-local UnitInParty = UnitInParty
-local UnitInRaid = UnitInRaid
 local UnitInRange = UnitInRange
 local UnitIsConnected = UnitIsConnected
-local UnitPhaseReason = UnitPhaseReason
 local UnitIsPlayer = UnitIsPlayer
 local UnitIsUnit = UnitIsUnit
+local UnitPhaseReason = UnitPhaseReason
 
-local function getUnit(unit)
-	if not unit:find('party') or not unit:find('raid') then
-		for i=1, 4 do
-			if UnitIsUnit(unit, 'party'..i) then
-				return 'party'..i
-			end
-		end
-
-		for i=1, 40 do
-			if UnitIsUnit(unit, 'raid'..i) then
-				return 'raid'..i
-			end
-		end
-	else
-		return unit
-	end
-end
-
-local function friendlyIsInRange(unit)
-	if not UnitIsUnit(unit, 'player') and (UnitInParty(unit) or UnitInRaid(unit)) then
-		unit = getUnit(unit) -- swap the unit with `raid#` or `party#` when its NOT `player`, UnitIsUnit is true, and its not using `raid#` or `party#` already
-	end
+local function friendlyIsInRange(realUnit)
+	local unit = E:GetGroupUnit(realUnit) or realUnit
 
 	if UnitIsPlayer(unit) and UnitPhaseReason(unit) then
 		return false -- is not in same phase
