34c8df47873ed43cfddb1b6e708681e3da25a18a
Repooc <tjcooper1982@gmail.com>
2021-05-29 00:04:58 -0400

    swap this around incase db has an invalid entry and defaults to worldping

--- ElvUI/Modules/DataTexts/System.lua
+++ ElvUI/Modules/DataTexts/System.lua
@@ -247,7 +247,7 @@ local function OnUpdate(self, elapsed)
 
 		local framerate = floor(GetFramerate())
 		local _, _, homePing, worldPing = GetNetStats()
-		local latency = E.global.datatexts.settings.System.latency == 'WORLD' and worldPing or homePing
+		local latency = E.global.datatexts.settings.System.latency == 'HOME' and homePing or worldPing
 
 		local fps = framerate >= 30 and 1 or (framerate >= 20 and framerate < 30) and 2 or (framerate >= 10 and framerate < 20) and 3 or 4
 		local ping = latency < 150 and 1 or (latency >= 150 and latency < 300) and 2 or (latency >= 300 and latency < 500) and 3 or 4
