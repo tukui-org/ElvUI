2533915c73aa919367faf90eb5ef53ec8a99fb56
Simpy <kodi03466@hotmail.com>
2021-06-29 07:30:09 -0400

    these are unused and junk so just remove them for now

--- ElvUI/Modules/Skins/Blizzard/Soulbinds.lua
+++ ElvUI/Modules/Skins/Blizzard/Soulbinds.lua
@@ -2,37 +2,8 @@ local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateD
 local S = E:GetModule('Skins')
 
 local _G = _G
-local ipairs = ipairs
-local hooksecurefunc = hooksecurefunc
 
 -- Credits: siweia - Aurora Classic
-local function SkinConduitList(frame)
-	local header = frame.CategoryButton.Container
-	if not header.styled then
-		header:DisableDrawLayer('BACKGROUND')
-		header:CreateBackdrop('Transparent')
-		header.backdrop:Point('TOPLEFT', 2, 0)
-		header.backdrop:Point('BOTTOMRIGHT', 15, 0)
-		header.backdrop:SetBackdropColor(1, 0, 0, .35)
-
-		header.styled = true
-	end
-
-	for button in frame.pool:EnumerateActive() do
-		if not button.styled then
-			for _, element in ipairs(button.Hovers) do
-				element:SetColorTexture(1, 1, 1, .25)
-			end
-
-			button.PendingBackground:SetColorTexture(1, .8, 0, .25)
-			button.Spec.IconOverlay:Hide()
-			S:HandleIcon(button.Spec.Icon)
-
-			button.styled = true
-		end
-	end
-end
-
 function S:Blizzard_Soulbinds()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.soulbinds) then return end
 
@@ -48,18 +19,3 @@ function S:Blizzard_Soulbinds()
 end
 
 S:AddCallbackForAddon('Blizzard_Soulbinds')
-
--- ToDo: Find a way to skin me :thinking:
-function S:Blizzard_LandingSoulbinds()
-	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.soulbinds) then return end
-
-	hooksecurefunc(_G.LandingPageSoulbindButtonMixin, 'SetSoulbind', function(button, soulbindData)
-		if not button.IsSkinned then
-			button:StyleButton()
-
-			button.IsSkinned = true
-		end
-	end)
-end
-
---S:AddCallbackForAddon('Blizzard_LandingSoulbinds')
