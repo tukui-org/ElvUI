c0bfe576d96b0d2c6a55e6425088454824a43a99
Simpy <kodi03466@hotmail.com>
2021-06-29 07:08:52 -0400

    slot buttons skinned

--- ElvUI/Modules/Skins/Blizzard/Collectables.lua
+++ ElvUI/Modules/Skins/Blizzard/Collectables.lua
@@ -616,14 +616,14 @@ function S:Blizzard_Collections()
 	local WardrobeTransmogFrame = _G.WardrobeTransmogFrame
 	WardrobeTransmogFrame:StripTextures()
 
-	--[[for i = 1, #WardrobeTransmogFrame.ModelScene.SlotButtons do
-		local slotButton = WardrobeTransmogFrame.ModelScene.SlotButtons[i]
+	for i = 1, #WardrobeTransmogFrame.SlotButtons do
+		local slotButton = WardrobeTransmogFrame.SlotButtons[i]
 		slotButton:SetFrameLevel(slotButton:GetFrameLevel() + 2)
 		slotButton:StripTextures()
 		slotButton:CreateBackdrop(nil, nil, nil, nil, nil, nil, true)
 		slotButton.Border:Kill()
 		slotButton.Icon:SetTexCoord(unpack(E.TexCoords))
-	end]]
+	end
 
 	WardrobeTransmogFrame.SpecButton:ClearAllPoints()
 	WardrobeTransmogFrame.SpecButton:Point('RIGHT', WardrobeTransmogFrame.ApplyButton, 'LEFT', -2, 0)
