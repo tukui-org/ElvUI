91e4a07b3d4b151b8f0cb6852a02f0dd77952c0e
Repooc <tjcooper1982@gmail.com>
2021-07-01 04:46:56 -0400

    its fixed :P

--- ElvUI/Modules/Skins/Addons/Ace3.lua
+++ ElvUI/Modules/Skins/Addons/Ace3.lua
@@ -252,7 +252,6 @@ function S:Ace3_RegisterAsWidget(widget)
 	elseif TYPE == 'Button' or TYPE == 'Button-ElvUI' then
 		local frame = widget.frame
 		S:HandleButton(frame, true)
-		-- frame:SetInside()  --! Simpy, look at this for the issue with addons that use ace
 	elseif TYPE == 'Slider' or TYPE == 'Slider-ElvUI' then
 		local frame = widget.slider
 		local editbox = widget.editbox
