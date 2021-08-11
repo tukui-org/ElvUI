331171bf39bd5857d56816f10eca41a91022f5b1
Simpy <kodi03466@hotmail.com>
2021-08-10 22:00:56 -0400

    move this function into commands file

--- ElvUI/Core/Commands.lua
+++ ElvUI/Core/Commands.lua
@@ -3,7 +3,8 @@ local DT = E:GetModule('DataTexts')
 local AB = E:GetModule('ActionBars')
 
 local _G = _G
-local tonumber, type, pairs, select = tonumber, type, pairs, select
+local type, pairs, select = type, pairs, select
+local strmatch, tonumber, tostring = strmatch, tonumber, tostring
 local lower, format, wipe, next, print = strlower, format, wipe, next, print
 
 local debugprofilestop = debugprofilestop
@@ -14,6 +15,7 @@ local GetNumAddOns = GetNumAddOns
 local GetCVarBool = GetCVarBool
 local DisableAddOn = DisableAddOn
 local ResetCPUUsage = ResetCPUUsage
+local GetFunctionCPUUsage = GetFunctionCPUUsage
 local UpdateAddOnCPUUsage = UpdateAddOnCPUUsage
 local ReloadUI = ReloadUI
 local SetCVar = SetCVar
@@ -105,6 +107,85 @@ do
 			self:Print(format(message, per and per > 0 and format('%.3f', per) or 0, passed and passed > 0 and format('%.3f', passed) or 0))
 			toggle = false
 		end
+	end
+end
+
+do
+	local CPU_USAGE = {}
+	local function CompareCPUDiff(showall, minCalls)
+		local greatestUsage, greatestCalls, greatestName, newName, newFunc
+		local greatestDiff, lastModule, mod, usage, calls, diff = 0
+
+		for name, oldUsage in pairs(CPU_USAGE) do
+			newName, newFunc = strmatch(name, '^([^:]+):(.+)$')
+			if not newFunc then
+				E:Print('CPU_USAGE:', name, newFunc)
+			else
+				if newName ~= lastModule then
+					mod = E:GetModule(newName, true) or E
+					lastModule = newName
+				end
+				usage, calls = GetFunctionCPUUsage(mod[newFunc], true)
+				diff = usage - oldUsage
+				if showall and (calls > minCalls) then
+					E:Print('Name('..name..')  Calls('..calls..') MS('..(usage or 0)..') Diff('..(diff > 0 and format('%.3f', diff) or 0)..')')
+				end
+				if (diff > greatestDiff) and calls > minCalls then
+					greatestName, greatestUsage, greatestCalls, greatestDiff = name, usage, calls, diff
+				end
+			end
+		end
+
+		if greatestName then
+			E:Print(greatestName.. ' had the CPU usage of: '..(greatestUsage > 0 and format('%.3f', greatestUsage) or 0)..'ms. And has been called '.. greatestCalls..' times.')
+		else
+			E:Print('CPU Usage: No CPU Usage differences found.')
+		end
+
+		wipe(CPU_USAGE)
+	end
+
+	function E:GetTopCPUFunc(msg)
+		if not GetCVarBool('scriptProfile') then
+			E:Print('For `/cpuusage` to work, you need to enable script profiling via: `/console scriptProfile 1` then reload. Disable after testing by setting it back to 0.')
+			return
+		end
+
+		local module, showall, delay, minCalls = strmatch(msg, '^(%S+)%s*(%S*)%s*(%S*)%s*(.*)$')
+		local checkCore, mod = (not module or module == '') and 'E'
+
+		showall = (showall == 'true' and true) or false
+		delay = (delay == 'nil' and nil) or tonumber(delay) or 5
+		minCalls = (minCalls == 'nil' and nil) or tonumber(minCalls) or 15
+
+		wipe(CPU_USAGE)
+		if module == 'all' then
+			for moduName, modu in pairs(self.modules) do
+				for funcName, func in pairs(modu) do
+					if funcName ~= 'GetModule' and type(func) == 'function' then
+						CPU_USAGE[moduName..':'..funcName] = GetFunctionCPUUsage(func, true)
+					end
+				end
+			end
+		else
+			if not checkCore then
+				mod = self:GetModule(module, true)
+				if not mod then
+					self:Print(module..' not found, falling back to checking core.')
+					mod, checkCore = self, 'E'
+				end
+			else
+				mod = self
+			end
+			for name, func in pairs(mod) do
+				if (name ~= 'GetModule') and type(func) == 'function' then
+					CPU_USAGE[(checkCore or module)..':'..name] = GetFunctionCPUUsage(func, true)
+				end
+			end
+		end
+
+		self:Delay(delay, CompareCPUDiff, showall, minCalls)
+		self:Print('Calculating CPU Usage differences (module: '..(checkCore or module)..', showall: '..tostring(showall)..', minCalls: '..tostring(minCalls)..', delay: '..tostring(delay)..')')
 	end
 end
 
