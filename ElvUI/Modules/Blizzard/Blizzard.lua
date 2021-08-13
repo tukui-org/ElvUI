d24961e39ad7bf04073022cbf71ba05eeaf1101f
Simpy <kodi03466@hotmail.com>
2021-08-13 19:52:18 -0400

    quick join bug? this code is mega old. gonna delete it

--- ElvUI/Modules/Blizzard/Blizzard.lua
+++ ElvUI/Modules/Blizzard/Blizzard.lua
@@ -82,13 +82,6 @@ function B:Initialize()
 	_G.BNToastFrame.mover:Size(_G.BNToastFrame:GetSize())
 	TT:SecureHook(_G.BNToastFrame, 'SetPoint', 'RepositionBNET')
 
-	-- Quick Join Bug
-	CreateFrame('Frame'):SetScript('OnUpdate', function()
-		if _G.LFRBrowseFrame.timeToClear then
-			_G.LFRBrowseFrame.timeToClear = nil
-		end
-	end)
-
 	--Add (+X%) to quest rewards experience text
 	B:SecureHook('QuestInfo_Display', 'QuestXPPercent')
 end
