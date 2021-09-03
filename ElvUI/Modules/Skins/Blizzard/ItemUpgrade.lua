2cd382e2c542cf321274414613f01c1936cda770
Luckyone <Luckyone.Development@pm.me>
2021-09-03 15:31:32 +0200

    ItemUpgrade

--- ElvUI/Modules/Skins/Blizzard/ItemUpgrade.lua
+++ ElvUI/Modules/Skins/Blizzard/ItemUpgrade.lua
@@ -2,10 +2,6 @@ local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateD
 local S = E:GetModule('Skins')
 
 local _G = _G
-local unpack = unpack
-local hooksecurefunc = hooksecurefunc
-local GetItemUpgradeItemInfo = GetItemUpgradeItemInfo
-local BAG_ITEM_QUALITY_COLORS = BAG_ITEM_QUALITY_COLORS
 
 function S:Blizzard_ItemUpgradeUI()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.itemUpgrade) then return end
@@ -13,41 +9,20 @@ function S:Blizzard_ItemUpgradeUI()
 	local ItemUpgradeFrame = _G.ItemUpgradeFrame
 	S:HandlePortraitFrame(ItemUpgradeFrame)
 
-	local ItemButton = ItemUpgradeFrame.ItemButton
-	ItemButton:SetTemplate()
-	ItemButton.Frame:SetTexture('')
-	ItemButton:SetPushedTexture('')
-	S:HandleItemButton(ItemButton)
+	local UpgradeDropDown = ItemUpgradeFrame.ItemInfo.Dropdown
+	S:HandleDropDownBox(UpgradeDropDown, 130)
 
-	local Highlight = ItemButton:GetHighlightTexture()
-	Highlight:SetColorTexture(1, 1, 1, .25)
+	local UpgradeButton = ItemUpgradeFrame.UpgradeButton
+	S:HandleButton(UpgradeButton, true)
 
-	hooksecurefunc('ItemUpgradeFrame_Update', function()
-		local icon, _, quality = GetItemUpgradeItemInfo()
-		if icon then
-			ItemButton.IconTexture:SetTexCoord(unpack(E.TexCoords))
-			local color = BAG_ITEM_QUALITY_COLORS[quality or 1]
-			ItemButton.backdrop:SetBackdropBorderColor(color.r, color.g, color.b)
-		else
-			ItemButton.IconTexture:SetTexture('')
-			ItemButton.backdrop:SetBackdropBorderColor(0, 0, 0)
-		end
-	end)
+	local ButtonFrame = ItemUpgradeFrame.UpgradeItemButton.ButtonFrame
+	ButtonFrame:StripTextures()
 
-	local UpgradeDropDown = ItemUpgradeFrame.UpgradeLevelDropDown.DropDownMenu
-	S:HandleDropDownBox(UpgradeDropDown, 115)
+	local CurrenciesBorder = ItemUpgradeFramePlayerCurrenciesBorder
+	CurrenciesBorder:StripTextures()
 
-	local TextFrame = ItemUpgradeFrame.TextFrame
-	TextFrame:StripTextures()
-	TextFrame:CreateBackdrop('Transparent')
-	TextFrame.backdrop:Point('TOPLEFT', ItemButton.IconTexture, 'TOPRIGHT', 3, 1)
-	TextFrame.backdrop:Point('BOTTOMRIGHT', -6, 2)
-
-	_G.ItemUpgradeFrameMoneyFrame:StripTextures()
-	S:HandleIcon(_G.ItemUpgradeFrameMoneyFrame.Currency.icon)
-	S:HandleButton(_G.ItemUpgradeFrameUpgradeButton, true)
-	ItemUpgradeFrame.FinishedGlow:Kill()
-	ItemUpgradeFrame.ButtonFrame:DisableDrawLayer('BORDER')
+	local BGTex = ItemUpgradeFrame.UpgradeCostFrame.BGTex
+	BGTex:StripTextures()
 end
 
 S:AddCallbackForAddon('Blizzard_ItemUpgradeUI')
