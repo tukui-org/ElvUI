91b1dc90eb4d7d65114d1954a3181e80dcac5688
Luckyone <Luckyone.Development@pm.me>
2021-09-03 16:20:09 +0200

    GuildBank rework

--- ElvUI/Modules/Skins/Blizzard/GuildBank.lua
+++ ElvUI/Modules/Skins/Blizzard/GuildBank.lua
@@ -10,67 +10,62 @@ function S:Blizzard_GuildBankUI()
 	_G.GuildBankFrame:StripTextures()
 	_G.GuildBankFrame:SetTemplate('Transparent')
 	S:HandleCloseButton(_G.GuildBankFrame.CloseButton)
+	_G.GuildBankFrame.Emblem:Kill()
+	_G.GuildBankFrame.MoneyFrameBG:StripTextures()
 
-	_G.GuildBankEmblemFrame:StripTextures(true)
-	_G.GuildBankMoneyFrameBackground:Kill()
-	S:HandleScrollBar(_G.GuildBankPopupScrollFrameScrollBar)
-	S:HandleButton(_G.GuildBankFrameDepositButton, true)
-	S:HandleButton(_G.GuildBankFrameWithdrawButton, true)
+	S:HandleButton(_G.GuildBankFrame.DepositButton, true)
+	S:HandleButton(_G.GuildBankFrame.WithdrawButton, true)
 	S:HandleButton(_G.GuildBankInfoSaveButton, true)
-	S:HandleButton(_G.GuildBankFramePurchaseButton, true)
+	S:HandleButton(_G._G.GuildBankFrame.BuyInfo.PurchaseButton, true)
 
-	_G.GuildBankFrameWithdrawButton:Point('RIGHT', _G.GuildBankFrameDepositButton, 'LEFT', -2, 0)
+	_G.GuildBankFrame.WithdrawButton:Point('RIGHT', _G.GuildBankFrame.DepositButton, 'LEFT', -2, 0)
 	_G.GuildBankInfoScrollFrame:Point('TOPLEFT', _G.GuildBankInfo, 'TOPLEFT', -10, 12)
 	_G.GuildBankInfoScrollFrame:StripTextures()
 	_G.GuildBankInfoScrollFrame:Width(_G.GuildBankInfoScrollFrame:GetWidth() - 8)
 	_G.GuildBankTransactionsScrollFrame:StripTextures()
 
-	_G.GuildBankFrameBlackBG:CreateBackdrop('Transparent', nil, nil, nil, nil, nil, nil, nil, 1)
-	_G.GuildBankFrameBlackBG.backdrop:Point('TOPLEFT', _G.GuildBankFrameBlackBG, 'TOPLEFT', 4, 0)
-	_G.GuildBankFrameBlackBG.backdrop:Point('BOTTOMRIGHT', _G.GuildBankFrameBlackBG, 'BOTTOMRIGHT', -3, 3)
+	_G.GuildBankFrame.BlackBG:CreateBackdrop('Transparent', nil, nil, nil, nil, nil, nil, nil, 1)
+	_G.GuildBankFrame.BlackBG.backdrop:Point('TOPLEFT', _G.GuildBankFrame.BlackBG, 'TOPLEFT', 4, 0)
+	_G.GuildBankFrame.BlackBG.backdrop:Point('BOTTOMRIGHT', _G.GuildBankFrame.BlackBG, 'BOTTOMRIGHT', -3, 3)
 
 	S:HandleScrollBar(_G.GuildBankTransactionsScrollFrameScrollBar)
 	S:HandleScrollBar(_G.GuildBankInfoScrollFrameScrollBar)
 	_G.GuildBankTransactionsScrollFrameScrollBar:ClearAllPoints()
-	_G.GuildBankTransactionsScrollFrameScrollBar:Point('TOPRIGHT', _G.GuildBankFrameBlackBG.backdrop, 'TOPRIGHT', -4, -21)
-	_G.GuildBankTransactionsScrollFrameScrollBar:Point('BOTTOMRIGHT', _G.GuildBankFrameBlackBG.backdrop, 'BOTTOMRIGHT', -4, 21)
+	_G.GuildBankTransactionsScrollFrameScrollBar:Point('TOPRIGHT', _G.GuildBankFrame.BlackBG.backdrop, 'TOPRIGHT', -4, -21)
+	_G.GuildBankTransactionsScrollFrameScrollBar:Point('BOTTOMRIGHT', _G.GuildBankFrame.BlackBG.backdrop, 'BOTTOMRIGHT', -4, 21)
 	_G.GuildBankInfoScrollFrameScrollBar:ClearAllPoints()
-	_G.GuildBankInfoScrollFrameScrollBar:Point('TOPRIGHT', _G.GuildBankFrameBlackBG.backdrop, 'TOPRIGHT', -4, -21)
-	_G.GuildBankInfoScrollFrameScrollBar:Point('BOTTOMRIGHT', _G.GuildBankFrameBlackBG.backdrop, 'BOTTOMRIGHT', -4, 21)
+	_G.GuildBankInfoScrollFrameScrollBar:Point('TOPRIGHT', _G.GuildBankFrame.BlackBG.backdrop, 'TOPRIGHT', -4, -21)
+	_G.GuildBankInfoScrollFrameScrollBar:Point('BOTTOMRIGHT', _G.GuildBankFrame.BlackBG.backdrop, 'BOTTOMRIGHT', -4, 21)
 
-	for i = 1, _G.NUM_GUILDBANK_COLUMNS do
-		_G['GuildBankColumn'..i]:StripTextures()
+	for i=1, _G.MAX_GUILDBANK_TABS do
+		local tab = _G['GuildBankTab'..i]
+		tab:StripTextures()
 
-		for x = 1, _G.NUM_SLOTS_PER_GUILDBANK_GROUP do
-			local button = _G['GuildBankColumn'..i..'Button'..x]
-			local icon = _G['GuildBankColumn'..i..'Button'..x..'IconTexture']
-			local texture = _G['GuildBankColumn'..i..'Button'..x..'NormalTexture']
-			if texture then
-				texture:SetTexture()
-			end
-
-			button:StyleButton()
-			button:SetTemplate(nil, true)
-
-			icon:SetInside()
-			icon:SetTexCoord(unpack(E.TexCoords))
-			S:HandleIconBorder(button.IconBorder)
-		end
-	end
-
-	for i = 1, _G.MAX_GUILDBANK_TABS do
-		local button = _G['GuildBankTab'..i..'Button']
-		local texture = _G['GuildBankTab'..i..'ButtonIconTexture']
-		_G['GuildBankTab'..i]:StripTextures(true)
-
+		local button = tab.Button
+		local icon = button.IconTexture
+		local texture = icon:GetTexture()
 		button:StripTextures()
 		button:StyleButton(true)
 		button:SetTemplate(nil, true)
+		icon:SetTexture(texture)
+		icon:SetTexCoord(unpack(E.TexCoords))
+		icon:SetInside()
+	end
 
-		button.searchOverlay:SetColorTexture(0, 0, 0, 0.8)
+	for i = 1, 7 do
+		local column = _G.GuildBankFrame['Column'..i]
+		column:StripTextures()
 
-		texture:SetInside()
-		texture:SetTexCoord(unpack(E.TexCoords))
+		for x = 1, 14 do
+			local button = column['Button'..x]
+			button:StripTextures()
+			button:SetTemplate('Transparent')
+
+			button.icon:SetInside()
+			button.icon:SetTexCoord(unpack(E.TexCoords))
+
+			S:HandleIconBorder(button.IconBorder)
+		end
 	end
 
 	for i = 1, 4 do
@@ -87,7 +82,7 @@ function S:Blizzard_GuildBankUI()
 	_G.GuildBankPopupFrame:Show() --Toggle the frame in order to create the necessary button elements
 	_G.GuildBankPopupFrame:Hide()
 
-	S:HandleIconSelectionFrame(_G.GuildBankPopupFrame, _G.NUM_GUILDBANK_ICONS_SHOWN, 'GuildBankPopupButton', 'GuildBankPopup')
+	--S:HandleIconSelectionFrame(_G.GuildBankPopupFrame, _G.NUM_GUILDBANK_ICONS_SHOWN, 'GuildBankPopupButton', 'GuildBankPopup')
 end
 
 S:AddCallbackForAddon('Blizzard_GuildBankUI')
