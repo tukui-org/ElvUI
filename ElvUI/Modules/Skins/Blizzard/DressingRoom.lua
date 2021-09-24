ae217a47c5262876328b46a365f7440c46eb10c5
Merathilis <neo0608@gmail.com>
2021-09-24 16:24:50 +0200

    Update some skins due to 40290 additions/changes

--- ElvUI/Modules/Skins/Blizzard/DressingRoom.lua
+++ ElvUI/Modules/Skins/Blizzard/DressingRoom.lua
@@ -2,6 +2,18 @@ local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateD
 local S = E:GetModule('Skins')
 
 local _G = _G
+local unpack = unpack
+local hooksecurefunc = hooksecurefunc
+
+-- Please check someone, Patch 9.1.5 new OutFit thing
+local function ResetToggleTexture(button, texture)
+	button:GetNormalTexture():SetTexCoord(unpack(E.TexCoords))
+	button:GetNormalTexture():SetInside()
+	button:SetNormalTexture(texture)
+	button:GetPushedTexture():SetTexCoord(unpack(E.TexCoords))
+	button:GetPushedTexture():SetInside()
+	button:SetPushedTexture(texture)
+end
 
 function S:DressUpFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.dressingroom) then return end
@@ -11,6 +23,9 @@ function S:DressUpFrame()
 
 	S:HandleButton(_G.DressUpFrameResetButton)
 	S:HandleButton(_G.DressUpFrameCancelButton)
+	S:HandleButton(DressUpFrame.LinkButton)
+	S:HandleButton(DressUpFrame.ToggleOutfitDetailsButton)
+	ResetToggleTexture(DressUpFrame.ToggleOutfitDetailsButton, 1392954) -- find a better texture
 
 	local DressUpFrameOutfitDropDown = _G.DressUpFrameOutfitDropDown
 	S:HandleDropDownBox(DressUpFrameOutfitDropDown)
@@ -21,7 +36,25 @@ function S:DressUpFrame()
 	S:HandleMaxMinFrame(DressUpFrame.MaximizeMinimizeFrame)
 	_G.DressUpFrameResetButton:Point('RIGHT', _G.DressUpFrameCancelButton, 'LEFT', -2, 0)
 
-	-- Wardrobe edit frame
+	-- 9.1.5 Outfit DetailPanel | Dont use StripTextures on the DetailsPanel, plx
+	DressUpFrame.OutfitDetailsPanel:DisableDrawLayer('BACKGROUND')
+	DressUpFrame.OutfitDetailsPanel:DisableDrawLayer('OVERLAY') -- to keep Artwork on the frame
+	DressUpFrame.OutfitDetailsPanel:CreateBackdrop('Transparent')
+
+	-- ToDO: Reposition the frame
+	-- @Simpy please check <3
+	hooksecurefunc(DressUpFrame.OutfitDetailsPanel, 'Refresh', function(self)
+		if self.slotPool then
+			for slot in self.slotPool:EnumerateActive() do
+				if not slot.isSkinned then
+					S:HandleIcon(slot.Icon)
+					--S:HandleIconBorder(slot.IconBorder) -- i dont get it
+					slot.isSkinned = true
+				end
+			end
+		end
+	end)
+
 	local WardrobeOutfitFrame = _G.WardrobeOutfitFrame
 	WardrobeOutfitFrame:StripTextures(true)
 	WardrobeOutfitFrame:SetTemplate('Transparent')
