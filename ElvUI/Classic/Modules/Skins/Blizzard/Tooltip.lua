542e4e344c1b123d24e1a31ff98b04f5bfd0eb95
Simpy <kodi03466@hotmail.com>
2021-10-01 15:24:35 -0400

    Skins, Tooltips: fix up tooltips on tbc and classic

--- ElvUI/Classic/Modules/Skins/Blizzard/Tooltip.lua
+++ ElvUI/Classic/Modules/Skins/Blizzard/Tooltip.lua
@@ -3,10 +3,6 @@ local S = E:GetModule('Skins')
 local TT = E:GetModule('Tooltip')
 
 local _G = _G
-local pairs = pairs
-local ipairs = ipairs
-local GameTooltip = _G.GameTooltip
-local GameTooltipStatusBar = _G.GameTooltipStatusBar
 
 function S:StyleTooltips()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.tooltip) then return end
@@ -15,10 +11,12 @@ function S:StyleTooltips()
 		_G.ItemRefTooltip,
 		_G.ItemRefShoppingTooltip1,
 		_G.ItemRefShoppingTooltip2,
+		_G.AutoCompleteBox,
 		_G.FriendsTooltip,
 		_G.EmbeddedItemTooltip,
 		_G.ReputationParagonTooltip,
 		_G.GameTooltip,
+		_G.WorldMapTooltip,
 		_G.ShoppingTooltip1,
 		_G.ShoppingTooltip2,
 		_G.QuickKeybindTooltip,
@@ -26,7 +24,7 @@ function S:StyleTooltips()
 		_G.ElvUIConfigTooltip,
 		_G.ElvUISpellBookTooltip
 	}) do
-		TT:SetStyle(tt)
+		TT:SecureHookScript(tt, 'OnShow', 'SetStyle')
 	end
 end
 
@@ -36,46 +34,25 @@ function S:TooltipFrames()
 	S:StyleTooltips()
 	S:HandleCloseButton(_G.ItemRefCloseButton)
 
-	-- Skin Blizzard Tooltips
-	GameTooltipStatusBar:SetStatusBarTexture(E.media.normTex)
-	E:RegisterStatusBar(GameTooltipStatusBar)
-	GameTooltipStatusBar:CreateBackdrop('Transparent')
-	GameTooltipStatusBar:ClearAllPoints()
-	GameTooltipStatusBar:Point('TOPLEFT', GameTooltip, 'BOTTOMLEFT', E.Border, -(E.Spacing * 3))
-	GameTooltipStatusBar:Point('TOPRIGHT', GameTooltip, 'BOTTOMRIGHT', -E.Border, -(E.Spacing * 3))
+	-- EmbeddedItemTooltip (also Paragon Reputation)
+	local EmbeddedTT = _G.EmbeddedItemTooltip.ItemTooltip
+	S:HandleIcon(EmbeddedTT.Icon, true)
+	S:HandleIconBorder(EmbeddedTT.IconBorder, EmbeddedTT.Icon.backdrop)
 
-	local tooltips = {
-		_G.ItemRefTooltip,
-		_G.ItemRefShoppingTooltip1,
-		_G.ItemRefShoppingTooltip2,
-		_G.AutoCompleteBox,
-		_G.FriendsTooltip,
-		_G.ShoppingTooltip1,
-		_G.ShoppingTooltip2,
-		_G.EmbeddedItemTooltip,
-		_G.WorldMapTooltip,
-		_G.ElvUIConfigTooltip,
-		-- already have locals
-		GameTooltip,
-		_G.ElvUISpellBookTooltip
-	}
+	-- Skin GameTooltip Status Bar
+	_G.GameTooltipStatusBar:SetStatusBarTexture(E.media.normTex)
+	_G.GameTooltipStatusBar:CreateBackdrop('Transparent')
+	_G.GameTooltipStatusBar:ClearAllPoints()
+	_G.GameTooltipStatusBar:Point('TOPLEFT', _G.GameTooltip, 'BOTTOMLEFT', E.Border, -(E.Spacing * 3))
+	_G.GameTooltipStatusBar:Point('TOPRIGHT', _G.GameTooltip, 'BOTTOMRIGHT', -E.Border, -(E.Spacing * 3))
+	E:RegisterStatusBar(_G.GameTooltipStatusBar)
 
-	for _, tt in ipairs(tooltips) do
-		TT:SecureHookScript(tt, 'OnShow', 'SetStyle')
-	end
-
-	-- EmbeddedItemTooltip
-	local reward = _G.EmbeddedItemTooltip.ItemTooltip
-	local icon = reward.Icon
-	if reward and reward.backdrop then
-		reward.backdrop:Point('TOPLEFT', icon, 'TOPLEFT', -2, 2)
-		reward.backdrop:Point('BOTTOMRIGHT', icon, 'BOTTOMRIGHT', 2, -2)
-	end
-
+	-- Tooltip Styling
 	TT:SecureHook('GameTooltip_ShowStatusBar') -- Skin Status Bars
 	TT:SecureHook('GameTooltip_ShowProgressBar') -- Skin Progress Bars
+	TT:SecureHook('GameTooltip_ClearProgressBars')
 	TT:SecureHook('GameTooltip_AddQuestRewardsToTooltip') -- Color Progress Bars
-	TT:SecureHook('GameTooltip_UpdateStyle', 'SetStyle') -- GameTooltip_SetBackdropStyle
+	TT:SecureHook('SharedTooltip_SetBackdropStyle', 'SetStyle') -- This also deals with other tooltip borders like AzeriteEssence Tooltip
 end
 
 S:AddCallback('TooltipFrames')
