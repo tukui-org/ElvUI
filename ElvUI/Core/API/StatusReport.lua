a0b2b084d347e61196a84748a10e98fc0f4f93c5
Repooc <tjcooper1982@gmail.com>
2021-10-06 14:11:26 -0400

    simple/temp protect estatus from failing on classic era & tbc

--- ElvUI/Core/API/StatusReport.lua
+++ ElvUI/Core/API/StatusReport.lua
@@ -301,7 +301,7 @@ function E:UpdateStatusFrame()
 	Section2.Content.Line4.Text:SetFormattedText('Resolution: |cff4beb2c%s|r', E.resolution)
 
 	local Section3 = StatusFrame.Section3
-	Section3.Content.Line4.Text:SetFormattedText('Specialization: |cff4beb2c%s|r', GetSpecName())
+	Section3.Content.Line4.Text:SetFormattedText('Specialization: |cff4beb2c%s|r', E.Retail and GetSpecName() or UNKNOWN)
 	Section3.Content.Line5.Text:SetFormattedText('Level: |cff4beb2c%s|r', E.mylevel)
 	Section3.Content.Line6.Text:SetFormattedText('Zone: |cff4beb2c%s|r', GetRealZoneText() or UNKNOWN)
 
