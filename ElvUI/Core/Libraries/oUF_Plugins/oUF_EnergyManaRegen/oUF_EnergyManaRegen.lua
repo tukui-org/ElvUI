fdd9c763edea4ca30334784ca3dea9ee2f1da9ce
Simpy <kodi03466@hotmail.com>
2021-10-16 19:37:42 -0400

    EnergyManaRegen: clean up

--- ElvUI/Core/Libraries/oUF_Plugins/oUF_EnergyManaRegen/oUF_EnergyManaRegen.lua
+++ ElvUI/Core/Libraries/oUF_Plugins/oUF_EnergyManaRegen/oUF_EnergyManaRegen.lua
@@ -1,7 +1,7 @@
 local _, ns = ...
 local oUF = ns.oUF
 
-local _G = _G
+local next = next
 local GetTime = GetTime
 local UnitPower = UnitPower
 local UnitClass = UnitClass
@@ -12,21 +12,23 @@ local GetSpellPowerCost = GetSpellPowerCost
 
 local LastTickTime = GetTime()
 local TickDelay = 2.025 -- Average tick time is slightly over 2 seconds
-local CurrentValue = UnitPower('player')
-local LastValue = CurrentValue
 local myClass = select(2, UnitClass('player'))
 local Mp5Delay = 5
 local Mp5DelayWillEnd = nil
 local Mp5IgnoredSpells = {
-	[18182] = true, -- Improved Life Tap 1
-	[18183] = true, -- Improved Life Tap 2
-	[1454] = true, -- Life Tap 1
-	[1455] = true, -- Life Tap 2
-	[1456] = true, -- Life Tap 3
-	[11687] = true, -- Life Tap 4
-	[11688] = true, -- Life Tap 5
-	[11689] = true, -- Life Tap 6
+	[18182]	= true, -- Improved Life Tap 1
+	[18183]	= true, -- Improved Life Tap 2
+	[1454]	= true, -- Life Tap 1
+	[1455]	= true, -- Life Tap 2
+	[1456]	= true, -- Life Tap 3
+	[11687]	= true, -- Life Tap 4
+	[11688]	= true, -- Life Tap 5
+	[11689]	= true, -- Life Tap 6
 }
+
+local LastValue = UnitPower('player')
+local ENERGY = Enum.PowerType.Energy
+local MANA = Enum.PowerType.Mana
 
 -- Sets tick time to the last possible time based on the last tick
 local UpdateTickTime = function(now)
@@ -39,16 +41,16 @@ local Update = function(self, elapsed)
 
 	if element.sinceLastUpdate > 0.01 then
 		local powerType = UnitPowerType('player')
-		if powerType ~= Enum.PowerType.Energy and powerType ~= Enum.PowerType.Mana then
+		if powerType ~= ENERGY and powerType ~= MANA then
 			element.Spark:Hide()
 			return
 		end
 
-		CurrentValue = UnitPower('player', powerType)
+		local CurrentValue = UnitPower('player', powerType)
 		local MaxPower = UnitPowerMax('player', powerType)
 		local Now = GetTime()
 
-		if powerType == Enum.PowerType.Mana then
+		if powerType == MANA then
 			if CurrentValue >= MaxPower then
 				element:SetValue(0)
 				element.Spark:Hide()
@@ -60,14 +62,14 @@ local Update = function(self, elapsed)
 				Mp5DelayWillEnd = nil
 				UpdateTickTime(Now)
 			end
-		elseif powerType == Enum.PowerType.Energy then
+		elseif powerType == ENERGY then
 			-- If energy is not full we just wait for the next tick
 			if Now >= LastTickTime + TickDelay and CurrentValue >= MaxPower then
 				UpdateTickTime(Now)
 			end
 		end
 
-		if Mp5DelayWillEnd and powerType == Enum.PowerType.Mana then
+		if Mp5DelayWillEnd and powerType == MANA then
 			-- Show 5 second indicator
 			element.Spark:Show()
 			element:SetMinMaxValues(0, Mp5Delay)
@@ -87,9 +89,7 @@ end
 
 local OnUnitPowerUpdate = function()
 	local powerType = UnitPowerType('player')
-	if powerType ~= Enum.PowerType.Mana and powerType ~= Enum.PowerType.Energy then
-		return
-	end
+	if powerType ~= MANA and powerType ~= ENERGY then return end
 
 	-- We also register ticks from mp5 gear within the 5-second-rule to get a more accurate sync later.
 	-- Unfortunately this registers a tick when a mana pot or life tab is used.
@@ -97,14 +97,13 @@ local OnUnitPowerUpdate = function()
 	if CurrentValue > LastValue then
 		LastTickTime = GetTime()
 	end
+
 	LastValue = CurrentValue
 end
 
 local OnUnitSpellcastSucceeded = function(_, _, _, _, spellID)
 	local powerType = UnitPowerType('player')
-	if powerType ~= Enum.PowerType.Mana then
-		return
-	end
+	if powerType ~= MANA then return end
 
 	local spellCost = false
 	local costTable = GetSpellPowerCost(spellID)
@@ -129,17 +128,17 @@ local Enable = function(self, unit)
 	local element = self.EnergyManaRegen
 	local Power = self.Power
 
-	if (unit == 'player') and element and Power and myClass ~= 'WARRIOR' then
+	if unit == 'player' and element and Power and myClass ~= 'WARRIOR' then
 		element.__owner = self
 
-		if(element:IsObjectType('StatusBar')) then
+		if element:IsObjectType('StatusBar') then
 			element:SetStatusBarTexture([[Interface\Buttons\WHITE8X8]])
 			element:GetStatusBarTexture():SetAlpha(0)
 			element:SetMinMaxValues(0, 2)
 		end
 
 		local spark = element.Spark
-		if(spark and spark:IsObjectType('Texture')) then
+		if spark and spark:IsObjectType('Texture') then
 			spark:SetTexture([[Interface\CastingBar\UI-CastingBar-Spark]])
 			spark:SetSize(20, 20)
 			spark:SetBlendMode('ADD')
@@ -159,7 +158,7 @@ local Disable = function(self)
 	local element = self.EnergyManaRegen
 	local Power = self.Power
 
-	if (Power) and (element) then
+	if Power and element then
 		self:UnregisterEvent('UNIT_SPELLCAST_SUCCEEDED', OnUnitSpellcastSucceeded)
 		self:UnregisterEvent('UNIT_POWER_UPDATE', OnUnitPowerUpdate)
 
