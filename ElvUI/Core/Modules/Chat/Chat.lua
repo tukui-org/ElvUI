e99d617928dbf786a3fb6bfa69c354c921666aa3
Simpy <kodi03466@hotmail.com>
2021-10-28 14:39:53 -0400

    Chat: swap container to outside

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -1268,7 +1268,7 @@ function CH:PositionChat(chat)
 	end
 
 	if chat.FontStringContainer then
-		chat.FontStringContainer:SetInside(nil, 2, 2)
+		chat.FontStringContainer:SetOutside(nil, 1, 1)
 	end
 
 	if chat:IsShown() then
