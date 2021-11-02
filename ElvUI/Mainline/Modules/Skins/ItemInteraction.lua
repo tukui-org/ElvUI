44a1b69b223db9ae8d790194eb43ce2468206e5a
Simpy <kodi03466@hotmail.com>
2021-11-02 00:18:55 -0400

    Skins: clean up ItemInteractionFrame

--- ElvUI/Mainline/Modules/Skins/ItemInteraction.lua
+++ ElvUI/Mainline/Modules/Skins/ItemInteraction.lua
@@ -6,41 +6,32 @@ local _G = _G
 function S:Blizzard_ItemInteractionUI()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.itemInteraction) then return end
 
-	local ItemInteractionFrame = _G.ItemInteractionFrame
-	S:HandlePortraitFrame(ItemInteractionFrame)
+	local mainFrame = _G.ItemInteractionFrame
+	S:HandlePortraitFrame(mainFrame)
 
-	do -- ItemSlot
-		local ItemSlot = ItemInteractionFrame.ItemSlot
-		ItemSlot:StripTextures()
-		ItemSlot:SetTemplate()
+	local itemSlot = mainFrame.ItemSlot
+	itemSlot:StripTextures()
+	itemSlot:SetTemplate()
+	itemSlot:Size(58)
+	itemSlot:ClearAllPoints()
+	itemSlot:Point('TOPLEFT', 143, -97)
 
-		ItemSlot:Size(58)
-		ItemSlot:ClearAllPoints()
-		ItemSlot:Point('TOPLEFT', 143, -97)
+	itemSlot.GlowOverlay:SetAlpha(0)
 
-		ItemSlot.Icon:ClearAllPoints()
-		ItemSlot.Icon:Point('TOPLEFT', 1, -1)
-		ItemSlot.Icon:Point('BOTTOMRIGHT', -1, 1)
-		S:HandleIcon(ItemSlot.Icon)
+	itemSlot.Icon:ClearAllPoints()
+	itemSlot.Icon:Point('TOPLEFT', 1, -1)
+	itemSlot.Icon:Point('BOTTOMRIGHT', -1, 1)
+	S:HandleIcon(itemSlot.Icon)
 
-		ItemSlot.GlowOverlay:SetAlpha(0)
-	end
-
-	local ButtonFrame = ItemInteractionFrame.ButtonFrame
+	local ButtonFrame = mainFrame.ButtonFrame
 	ButtonFrame:StripTextures()
-	ButtonFrame.MoneyFrameEdge:SetAlpha(0)
-	ButtonFrame.BlackBorder:SetAlpha(0)
 	ButtonFrame.ButtonBorder:Hide()
 	ButtonFrame.ButtonBottomBorder:Hide()
+	ButtonFrame.MoneyFrameEdge:SetAlpha(0)
+	ButtonFrame.BlackBorder:SetAlpha(0)
 
 	S:HandleIcon(ButtonFrame.Currency.icon)
 	S:HandleButton(ButtonFrame.ActionButton)
-
-	-- Temp mover
-	ItemInteractionFrame:SetMovable(true)
-	ItemInteractionFrame:RegisterForDrag('LeftButton')
-	ItemInteractionFrame:SetScript('OnDragStart', function(s) s:StartMoving() end)
-	ItemInteractionFrame:SetScript('OnDragStop', function(s) s:StopMovingOrSizing() end)
 end
 
 S:AddCallbackForAddon('Blizzard_ItemInteractionUI')
