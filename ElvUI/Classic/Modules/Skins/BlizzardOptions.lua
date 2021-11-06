1d802be2fcd62424464e8691c69f73ac6b252327
Simpy <kodi03466@hotmail.com>
2021-11-06 13:15:36 -0400

    Revert "Revert SoM stuff for Classic for now"
    This reverts commit 615b1b475448fd48edb5c5697d5fd49ca8a43051.
    
    # Conflicts:
    #	ElvUI/Classic/Modules/Skins/BlizzardOptions.lua

--- ElvUI/Classic/Modules/Skins/BlizzardOptions.lua
+++ ElvUI/Classic/Modules/Skins/BlizzardOptions.lua
@@ -113,6 +113,7 @@ function S:BlizzardOptions()
 		_G.ChatConfigFrameDefaultButton,
 		_G.ChatConfigFrameRedockButton,
 		_G.ChatConfigFrameOkayButton,
+		_G.ChatConfigFrame.ToggleChatButton,
 		_G.ChatConfigCombatSettingsFiltersDeleteButton,
 		_G.ChatConfigCombatSettingsFiltersAddFilterButton,
 		_G.ChatConfigCombatSettingsFiltersCopyFilterButton,
@@ -352,12 +353,6 @@ function S:BlizzardOptions()
 
 	-- Toggle Test Audio Button - Wow 8.0
 	S:HandleButton(_G.AudioOptionsVoicePanel.TestInputDevice.ToggleTest)
-
-	local VUMeter = _G.AudioOptionsVoicePanelTestInputDevice.VUMeter
-	VUMeter:StripTextures()
-	VUMeter.Status:CreateBackdrop()
-	VUMeter.Status:SetStatusBarTexture(E.media.normTex)
-	E:RegisterStatusBar(VUMeter.Status)
 
 	-- PushToTalk KeybindButton - Wow 8.0
 	hooksecurefunc('AudioOptionsVoicePanel_InitializeCommunicationModeUI', S.AudioOptionsVoicePanel_InitializeCommunicationModeUI)
