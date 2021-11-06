7d3332664336c550c50e847cc21d0d22e1f8d133
Merathilis <neo0608@gmail.com>
2021-11-06 13:30:20 +0100

    Classic Skins: Fix Blizzard Interface Option error

--- ElvUI/Classic/Modules/Skins/BlizzardOptions.lua
+++ ElvUI/Classic/Modules/Skins/BlizzardOptions.lua
@@ -354,7 +354,7 @@ function S:BlizzardOptions()
 	S:HandleButton(_G.AudioOptionsVoicePanel.TestInputDevice.ToggleTest)
 
 	local VUMeter = _G.AudioOptionsVoicePanelTestInputDevice.VUMeter
-	VUMeter:SetBackdrop(nil)
+	VUMeter:StripTextures()
 	VUMeter.Status:CreateBackdrop()
 	VUMeter.Status:SetStatusBarTexture(E.media.normTex)
 	E:RegisterStatusBar(VUMeter.Status)
