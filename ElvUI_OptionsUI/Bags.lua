b160f30e3d8b21f45f7c864ec0d0c09aa4a20a6a
Simpy <kodi03466@hotmail.com>
2021-11-09 01:44:55 -0500

    Options: locales for bags autotoggle

--- ElvUI_OptionsUI/Bags.lua
+++ ElvUI_OptionsUI/Bags.lua
@@ -130,10 +130,10 @@ Bags.args.general.args.itemLevelGroup.args.positionGroup.args.itemLevelxOffset =
 Bags.args.general.args.itemLevelGroup.args.positionGroup.args.itemLevelyOffset = ACH:Range(L["Y-Offset"], nil, 12, { min = -45, max = 45, step = 1 }, nil, nil, nil, nil, function() return not E.db.bags.itemLevel end)
 
 Bags.args.general.args.autoToggle = ACH:Group(L["Auto Toggle"], nil, 11)
-Bags.args.general.args.autoToggle.args.toggles = ACH:MultiSelect('', nil, 1, { mail = 'Mail', guildBank = 'Guild Bank', auctionHouse = 'Auction House', tradeSkills = 'Trade Skills', trade = 'Trade' }, nil, nil, function(_, key) return E.db.bags.autoToggle[key] end, function(_, key, value) E.db.bags.autoToggle[key] = value B:AutoToggle() end)
+Bags.args.general.args.autoToggle.args.toggles = ACH:MultiSelect('', nil, 1, { mail = L["Mail"], guildBank = L["Guild Bank"], auctionHouse = L["Auction House"], tradeSkills = L["Trade Skills"], trade = L["Trade"] }, nil, nil, function(_, key) return E.db.bags.autoToggle[key] end, function(_, key, value) E.db.bags.autoToggle[key] = value B:AutoToggle() end)
 
 if E.Retail then
-	Bags.args.general.args.autoToggle.args.toggles.values.soulBind = 'Soul Binds'
+	Bags.args.general.args.autoToggle.args.toggles.values.soulBind = L["Soul Binds"]
 end
 
 Bags.args.colorGroup = ACH:Group(L["COLORS"], nil, 2, nil, function(info) local t = E.db.bags.colors[info[#info - 1]][info[#info]] local d = P.bags.colors[info[#info - 1]][info[#info]] return t.r, t.g, t.b, t.a, d.r, d.g, d.b end, function(info, r, g, b) local t = E.db.bags.colors[info[#info - 1]][info[#info]] t.r, t.g, t.b = r, g, b B:UpdateBagColors(info[#info - 1], info[#info], r, g, b) B:UpdateAllBagSlots() end, function() return not E.Bags.Initialized end)
