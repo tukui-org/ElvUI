d9b1fb97e436b8af8d0d1c1b2f75416ed7c4b04b
Luckyone <Luckyone.Development@pm.me>
2021-11-12 01:05:53 +0100

    remove this

--- ElvUI/Core/General/StatusReport.lua
+++ ElvUI/Core/General/StatusReport.lua
@@ -214,7 +214,7 @@ function E:CreateStatusFrame()
 	--Sections
 	StatusFrame.Section1 = E:CreateStatusSection(300, 125, nil, 30, StatusFrame, 'TOP', StatusFrame, 'TOP', -30)
 	StatusFrame.Section2 = E:CreateStatusSection(300, 150, nil, 30, StatusFrame, 'TOP', StatusFrame.Section1, 'BOTTOM', 0)
-	StatusFrame.Section3 = E:CreateStatusSection(300, 185, nil, 30, StatusFrame, 'TOP', StatusFrame.Section2, 'BOTTOM', 0)
+	StatusFrame.Section3 = E:CreateStatusSection(300, E.Retail and 185 or 165, nil, 30, StatusFrame, 'TOP', StatusFrame.Section2, 'BOTTOM', 0)
 
 	PluginFrame.SectionP = E:CreateStatusSection(280, nil, nil, 30, PluginFrame, 'TOP', PluginFrame, 'TOP', -10)
 
@@ -303,7 +303,7 @@ function E:UpdateStatusFrame()
 	local Section3 = StatusFrame.Section3
 
 	if E.Retail then
-		Section3.Content.Line4.Text:SetFormattedText('Specialization: |cff4beb2c%s|r', E.Retail and GetSpecName() or UNKNOWN)
+		Section3.Content.Line4.Text:SetFormattedText('Specialization: |cff4beb2c%s|r', GetSpecName() or UNKNOWN)
 		Section3.Content.Line5.Text:SetFormattedText('Level: |cff4beb2c%s|r', E.mylevel)
 		Section3.Content.Line6.Text:SetFormattedText('Zone: |cff4beb2c%s|r', GetRealZoneText() or UNKNOWN)
 	else
