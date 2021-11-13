57657a5dc3dbccb5e968aff923f943e9aab908bb
Simpy <kodi03466@hotmail.com>
2021-11-13 14:00:27 -0500

    Bags: move search table reset after the reset :P

--- ElvUI/Core/Modules/Bags/Bags.lua
+++ ElvUI/Core/Modules/Bags/Bags.lua
@@ -271,13 +271,13 @@ function B:ResetAndClear()
 	B.BankFrame.editBox:SetText('')
 	B.BankFrame.editBox:ClearFocus()
 
-	if next(B.SearchSlots) then
-		wipe(B.SearchSlots)
-	end
-
 	-- pass bool to say whether it was from a script,
 	-- as this only needs to update from the scripts
 	B:SearchReset(self == B)
+
+	if next(B.SearchSlots) then
+		wipe(B.SearchSlots)
+	end
 end
 
 function B:SearchSlotUpdate(slot, link, locked)
