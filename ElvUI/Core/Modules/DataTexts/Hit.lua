60204b1675c7372ce876e772dedac84e56c4adfb
Simpy <kodi03466@hotmail.com>
2021-12-08 11:22:29 -0500

    hit dt on classic

--- ElvUI/Core/Modules/DataTexts/Hit.lua
+++ ElvUI/Core/Modules/DataTexts/Hit.lua
@@ -2,6 +2,7 @@ local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
 
 local strjoin = strjoin
+local GetHitModifier = GetHitModifier
 local GetCombatRatingBonus = GetCombatRatingBonus
 local STAT_HIT_CHANCE = STAT_HIT_CHANCE
 local STAT_CATEGORY_ENHANCEMENTS = STAT_CATEGORY_ENHANCEMENTS
@@ -13,12 +14,11 @@ local displayString, lastPanel = ''
 local function OnEvent(self)
 	lastPanel = self
 
-	local hitRatingBonus = GetCombatRatingBonus(E.myclass == 'HUNTER' and CR_HIT_RANGED or CR_HIT_MELEE)
-
+	local hitRating = (E.Classic and GetHitModifier()) or GetCombatRatingBonus(E.myclass == 'HUNTER' and CR_HIT_RANGED or CR_HIT_MELEE)
 	if E.global.datatexts.settings.Hit.NoLabel then
-		self.text:SetFormattedText(displayString, hitRatingBonus)
+		self.text:SetFormattedText(displayString, hitRating)
 	else
-		self.text:SetFormattedText(displayString, E.global.datatexts.settings.Hit.Label ~= '' and E.global.datatexts.settings.Hit.Label or STAT_HIT_CHANCE..': ', hitRatingBonus)
+		self.text:SetFormattedText(displayString, E.global.datatexts.settings.Hit.Label ~= '' and E.global.datatexts.settings.Hit.Label or STAT_HIT_CHANCE..': ', hitRating)
 	end
 end
 
