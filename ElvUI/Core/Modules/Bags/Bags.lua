2cd9a6dd4b20fa4e843e262dde3d9b48850fddc3
Azilroka <darktemplar9900@yahoo.com>
2021-12-17 15:25:10 -0600

    tweak Sell Greys some more

--- ElvUI/Core/Modules/Bags/Bags.lua
+++ ElvUI/Core/Modules/Bags/Bags.lua
@@ -1279,9 +1279,10 @@ function B:GetGrays(vendor)
 	for bagID = 0, 4 do
 		for slotID = 1, GetContainerNumSlots(bagID) do
 			local _, count, _, _, _, _, itemLink, _, noValue, itemID = GetContainerItemInfo(bagID, slotID)
-			if itemLink and not B.ExcludeGrays[itemID] and not noValue then
-				local _, _, rarity, _, _, itype, _, _, _, _, itemPrice = GetItemInfo(itemLink)
-				if rarity and rarity == 0 and (itype and itype ~= 'Quest') and (itemPrice and itemPrice > 0) then
+			if itemLink and not noValue and not B.ExcludeGrays[itemID] then
+				local _, _, rarity, _, _, _, _, _, _, _, itemPrice, classID, _, bindType = GetItemInfo(itemLink)
+
+				if rarity and rarity == 0 and (classID ~= 12 or bindType ~= 4) then -- classID - 12 is Quest, bindType 4 is Quest
 					local stackCount = count or 1
 					local stackPrice = itemPrice * stackCount
 
