170271151e952fbc60c213dfe837a893abdc9310
Azilroka <darktemplar9900@yahoo.com>
2021-12-19 00:29:25 -0600

    The other slots

--- ElvUI/Core/Modules/Bags/Bags.lua
+++ ElvUI/Core/Modules/Bags/Bags.lua
@@ -728,10 +728,16 @@ function B:Holder_OnEnter()
 		if self.bagID == BACKPACK_CONTAINER then
 			local kb = GetBindingKey('TOGGLEBACKPACK')
 			GameTooltip:AddLine(kb and format('%s |cffffd200(%s)|r', _G.BACKPACK_TOOLTIP, kb) or _G.BACKPACK_TOOLTIP, 1, 1, 1)
+		elseif self.bagID == BANK_CONTAINER then
+			GameTooltip:AddLine(_G.BANK, 1, 1, 1)
 		elseif self.bagID == KEYRING_CONTAINER then
 			GameTooltip:AddLine(_G.KEYRING, 1, 1, 1)
 		elseif self.bag.numSlots == 0 then
 			GameTooltip:AddLine(_G.EQUIP_CONTAINER, 1, 1, 1)
+		elseif self.isBank then
+			GameTooltip:SetInventoryItem("player", self:GetInventorySlot())
+		else
+			GameTooltip:SetInventoryItem("player", self:GetID())
 		end
 
 		GameTooltip:AddLine(' ')
