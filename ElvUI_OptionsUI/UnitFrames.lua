ee143d2fad5f0ee5beb0ad36775c12392fe51d6e
Azilroka <darktemplar9900@yahoo.com>
2022-01-01 15:09:33 -0600

    UF: Fix Class Sort

--- ElvUI_OptionsUI/UnitFrames.lua
+++ ElvUI_OptionsUI/UnitFrames.lua
@@ -57,6 +57,14 @@ local blendModeValues = {
 local CUSTOMTEXT_CONFIGS, filters = {}, {}
 local carryFilterFrom, carryFilterTo
 
+local classTable = {}
+for i = 1, GetNumClasses() do
+	local classDisplayName, classTag = GetClassInfo(i)
+	if classTag then
+		classTable[classTag] = classDisplayName
+	end
+end
+
 -----------------------------------------------------------------------
 -- OPTIONS TABLES
 -----------------------------------------------------------------------
@@ -901,13 +909,8 @@ local function GetOptionsTable_GeneralGroup(updateFunc, groupName, numUnits)
 		config.args.sortingGroup.args.classSetup = ACH:Group(L["Class Order"], nil, 7, nil, nil, nil, nil, function() return E.db.unitframe.units[groupName].groupBy ~= 'CLASS' end)
 		config.args.sortingGroup.args.classSetup.inline = true
 
-		local classTable = {}
 		for i = 1, GetNumClasses() do
-			local classDisplayName, classTag = GetClassInfo(i)
-			if classTag then -- Classic ERA has nil on 10? maybe others
-				classTable[classTag] = classDisplayName
-				config.args.sortingGroup.args.classSetup.args['CLASS'..i] = ACH:Select(' ', nil, i, classTable)
-			end
+			config.args.sortingGroup.args.classSetup.args['CLASS'..i] = ACH:Select(' ', nil, i, classTable)
 		end
 	else
 		config.args.positionsGroup.args.width.set = function(info, value) if E.db.unitframe.units[groupName].castbar and E.db.unitframe.units[groupName].castbar.width == E.db.unitframe.units[groupName][info[#info]] then E.db.unitframe.units[groupName].castbar.width = value end E.db.unitframe.units[groupName][info[#info]] = value updateFunc(UF, groupName, numUnits) end
