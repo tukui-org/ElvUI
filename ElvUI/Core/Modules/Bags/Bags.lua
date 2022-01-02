24e188b8125d259933d3c7ea8903d226d7f64e84
Azilroka <darktemplar9900@yahoo.com>
2022-01-02 10:09:43 -0600

    Blizz broke delete grays for retail.. (probably classics in the future)

--- ElvUI/Core/Modules/Bags/Bags.lua
+++ ElvUI/Core/Modules/Bags/Bags.lua
@@ -1369,7 +1369,7 @@ function B:VendorGrayCheck()
 
 	if value == 0 then
 		E:Print(L["No gray items to delete."])
-	elseif not _G.MerchantFrame or not _G.MerchantFrame:IsShown() then
+	elseif not _G.MerchantFrame:IsShown() and not E.Retail then
 		E.PopupDialogs.DELETE_GRAYS.Money = value
 		E:StaticPopup_Show('DELETE_GRAYS')
 	else
