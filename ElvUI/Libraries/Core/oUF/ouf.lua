87412ab0c0b91452fb7c7ec30677a75284503be0
Simpy <kodi03466@hotmail.com>
2022-03-24 17:01:51 -0400

    actually keep this for TBC / classic

--- ElvUI/Libraries/Core/oUF/ouf.lua
+++ ElvUI/Libraries/Core/oUF/ouf.lua
@@ -915,13 +915,32 @@ do -- AuraUtil's ShouldSkipAuraUpdate by Blizzard (implemented and modified by S
 		return false
 	end
 
-	local ShouldSkipAuraUpdate = AuraUtil.ShouldSkipAuraUpdate
+	local function ShouldSkipUpdate(isFullUpdate, updatedAuras, relevantFunc, ...)
+		if isFullUpdate == nil then
+			isFullUpdate = true
+		end
+
+		local skipUpdate = false
+		if not isFullUpdate and updatedAuras ~= nil and relevantFunc ~= nil then
+			skipUpdate = true
+
+			for _, auraInfo in ipairs(updatedAuras) do
+				if relevantFunc(auraInfo, ...) then
+					skipUpdate = false
+					break
+				end
+			end
+		end
+
+		return skipUpdate
+	end
+
 	function oUF:ShouldSkipAuraUpdate(frame, event, unit, isFullUpdate, updatedAuras, overrideFunc)
 		if not unit or frame.unit ~= unit then
 			return true
 		else
 			local onlyDispellable = false -- frame.optionTable.displayOnlyDispellableDebuffs (blizzards); need to do something more advanced here perhaps if we want that to be functional?
-			return ShouldSkipAuraUpdate(isFullUpdate, updatedAuras, overrideFunc or CouldDisplayAura, onlyDispellable)
+			return ShouldSkipUpdate(isFullUpdate, updatedAuras, overrideFunc or CouldDisplayAura, onlyDispellable)
 		end
 	end
 end
