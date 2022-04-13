1ec044ed247c309daaeb943aa37e847bf619e3f2
Repooc <tjcooper1982@gmail.com>
2022-04-12 23:02:54 -0400

    nothing

--- ElvUI/Core/Modules/Tooltip/Tooltip.lua
+++ ElvUI/Core/Modules/Tooltip/Tooltip.lua
@@ -670,7 +670,6 @@ function TT:GameTooltip_OnTooltipSetItem(tt)
 	end
 
 	local name, link = tt:GetItem()
-
 	if not E.Retail and _G.CraftFrame:IsShown() and name == '' then
 		local id = tonumber(strmatch(owner:GetName(), '%d+'))
 		link = GetCraftReagentItemLink(GetCraftSelectionIndex(), id)
