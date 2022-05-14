ba269444e45b00b9653d643727aaa007b0e4daee
Simpy <kodi03466@hotmail.com>
2022-05-14 18:50:09 -0400

    spell haste dt for TBC (#176)

--- ElvUI/Core/Modules/DataTexts/SpellHaste.lua
+++ ElvUI/Core/Modules/DataTexts/SpellHaste.lua
@@ -2,21 +2,20 @@ local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
 
 local strjoin = strjoin
-local GetSpellHitModifier = GetSpellHitModifier
 local GetCombatRatingBonus = GetCombatRatingBonus
 local STAT_CATEGORY_ENHANCEMENTS = STAT_CATEGORY_ENHANCEMENTS
-local CR_HIT_SPELL = CR_HIT_SPELL
+local CR_HASTE_SPELL = CR_HASTE_SPELL
 
 local displayString, lastPanel = ''
 
 local function OnEvent(self)
 	lastPanel = self
 
-	self.text:SetFormattedText(displayString, E.Classic and GetSpellHitModifier() or GetCombatRatingBonus(CR_HIT_SPELL) or 0)
+	self.text:SetFormattedText(displayString, GetCombatRatingBonus(CR_HASTE_SPELL) or 0)
 end
 
 local function ValueColorUpdate(hex)
-	displayString = strjoin('', L["Spell Hit"], ': ', hex, '%.2f%%|r')
+	displayString = strjoin('', L["Spell Haste"], ': ', hex, '%.2f%%|r')
 
 	if lastPanel ~= nil then
 		OnEvent(lastPanel)
@@ -24,4 +23,4 @@ local function ValueColorUpdate(hex)
 end
 E.valueColorUpdateFuncs[ValueColorUpdate] = true
 
-DT:RegisterDatatext('Spell Hit', STAT_CATEGORY_ENHANCEMENTS, { 'COMBAT_RATING_UPDATE' }, OnEvent, nil, nil, nil, nil, L["Spell Hit"])
+DT:RegisterDatatext('Spell Haste', STAT_CATEGORY_ENHANCEMENTS, { 'UNIT_STATS', 'UNIT_AURA' }, OnEvent, nil, nil, nil, nil, L["Spell Haste"])
