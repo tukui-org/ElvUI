cd5a6ca71bde8626e26f78323f53705555240bfc
Simpy <kodi03466@hotmail.com>
2022-06-01 22:02:42 -0400

    meow

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -442,10 +442,8 @@ do --this can save some main file locals
 		-- Mis (NOTE: I will forever have the picture you accidently shared of the manikin wearing a strapon burned in my brain)
 		z['Twunkie-Area52']			= itsMis
 		z['Misoracle-Area52']		= itsMis
-		z['MisMayhem-Area52']		= itsMis
+		z['Mismayhem-Area52']		= itsMis
 		z['Misdîrect-Spirestone']	= itsMis
-		z['MisLight-Spirestone']	= itsMis
-		z['MisDivine-Spirestone']	= itsMis
 		z['Misillidan-Spirestone']	= itsMis
 		z['Mispel-Spirestone']		= itsMis
 		z['Misdecay-Spirestone']	= itsMis
