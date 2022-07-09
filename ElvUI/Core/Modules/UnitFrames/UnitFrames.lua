8a94cf19d7e121bcc6237f1119f3b9a4359204a9
Simpy <kodi03466@hotmail.com>
2022-07-09 12:46:49 -0400

    wut?

--- ElvUI/Core/Modules/UnitFrames/UnitFrames.lua
+++ ElvUI/Core/Modules/UnitFrames/UnitFrames.lua
@@ -991,7 +991,7 @@ function UF:CreateHeader(parent, groupFilter, overrideName, template, groupName,
 
 	local header = ElvUF:SpawnHeader(overrideName, headerTemplate, nil,
 		'oUF-initialConfigFunction', format('self:SetWidth(%d); self:SetHeight(%d);', db.width, db.height),
-		'groupFilter', groupFilter, 'showParty', true, 'showRaid', group ~= 'party', 'showSolo', true,
+		'groupFilter', groupFilter, 'showParty', true, 'showRaid', group ~= 'party', 'showSolo', false,
 		template and 'template', template
 	)
 
