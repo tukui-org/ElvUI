d1a834914d2d60a9685a9ef7d2b51d5e9c2f3ba2
Luckyone <Luckyone.Development@pm.me>
2022-07-22 02:38:58 +0200

    Talents (Missing: GlyphFrame)

--- ElvUI/Wrath/Modules/Skins/Talent.lua
+++ ElvUI/Wrath/Modules/Skins/Talent.lua
@@ -10,9 +10,7 @@ function S:Blizzard_TalentUI()
 	S:HandleFrame(_G.PlayerTalentFrame, true, nil, 11, -12, -32, 76)
 	S:HandleCloseButton(_G.PlayerTalentFrameCloseButton, _G.PlayerTalentFrame.backdrop)
 
-	_G.PlayerTalentFrameCancelButton:Kill()
-
-	for i = 1, 3 do
+	for i = 1, 4 do
 		S:HandleTab(_G['PlayerTalentFrameTab'..i])
 	end
 
@@ -22,7 +20,6 @@ function S:Blizzard_TalentUI()
 	S:HandleScrollBar(_G.PlayerTalentFrameScrollFrameScrollBar)
 	_G.PlayerTalentFrameScrollFrameScrollBar:Point('TOPLEFT', _G.PlayerTalentFrameScrollFrame, 'TOPRIGHT', 10, -16)
 
-	_G.PlayerTalentFrameSpentPoints:Point('TOP', 0, -42)
 	_G.PlayerTalentFrameTalentPointsText:Point('BOTTOMRIGHT', _G.PlayerTalentFrame, 'BOTTOMLEFT', 220, 84)
 
 	for i = 1, _G.MAX_NUM_TALENTS do
@@ -42,6 +39,8 @@ function S:Blizzard_TalentUI()
 			rank:SetFont(E.LSM:Fetch('font', E.db['general'].font), 12, 'OUTLINE')
 		end
 	end
+
+	_G.PlayerTalentFramePointsBar:StripTextures()
 end
 
 S:AddCallbackForAddon('Blizzard_TalentUI')
