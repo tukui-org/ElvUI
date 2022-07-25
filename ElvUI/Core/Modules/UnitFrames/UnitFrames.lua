b6e94a677700c73d6ce3745cc278f98b2006091e
Simpy <kodi03466@hotmail.com>
2022-07-24 23:46:34 -0400

    Revert "division by zero on warlocks"
    This reverts commit 0c1965c7dec862787434773c73f3715ce4c085f2.

--- ElvUI/Core/Modules/UnitFrames/UnitFrames.lua
+++ ElvUI/Core/Modules/UnitFrames/UnitFrames.lua
@@ -62,15 +62,12 @@ UF.headerFunctions = {}
 UF.classMaxResourceBar = {
 	DEATHKNIGHT = 6,
 	PALADIN = 5,
+	WARLOCK = 5,
 	MONK = 6,
 	MAGE = 4,
 	ROGUE = 6,
 	DRUID = 5
 }
-
-if not E.Wrath then
-	UF.classMaxResourceBar.WARLOCK = 5
-end
 
 UF.instanceMapIDs = {
 	[30]	= 40, -- Alterac Valley
