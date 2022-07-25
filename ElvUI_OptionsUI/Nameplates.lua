96b31f9791af06891db1252becd1586c2b2a3544
Luckyone <Luckyone.Development@pm.me>
2022-07-25 21:54:55 +0200

    Config: absorbs

--- ElvUI_OptionsUI/Nameplates.lua
+++ ElvUI_OptionsUI/Nameplates.lua
@@ -521,8 +521,8 @@ E.Options.args.nameplates.args.colorsGroup.args.healPrediction.inline = true
 
 E.Options.args.nameplates.args.colorsGroup.args.healPrediction.args.personal = ACH:Color(L["Personal"], nil, 1, true)
 E.Options.args.nameplates.args.colorsGroup.args.healPrediction.args.others = ACH:Color(L["Others"], nil, 2, true)
-E.Options.args.nameplates.args.colorsGroup.args.healPrediction.args.absorbs = ACH:Color(L["Absorbs"], nil, 3, true)
-E.Options.args.nameplates.args.colorsGroup.args.healPrediction.args.healAbsorbs = ACH:Color(L["Heal Absorbs"], nil, 4, true)
+E.Options.args.nameplates.args.colorsGroup.args.healPrediction.args.absorbs = ACH:Color(L["Absorbs"], nil, 3, true, nil, nil, nil, nil, not E.Retail)
+E.Options.args.nameplates.args.colorsGroup.args.healPrediction.args.healAbsorbs = ACH:Color(L["Heal Absorbs"], nil, 4, true, nil, nil, nil, nil, not E.Retail)
 
 E.Options.args.nameplates.args.colorsGroup.args.power = ACH:Group(L["Power Color"], nil, 7, nil, function(info) local t, d = E.db.nameplates.colors.power[info[#info]], P.nameplates.colors.power[info[#info]] return t.r, t.g, t.b, t.a, d.r, d.g, d.b, d.a end, function(info, r, g, b, a) local t = E.db.nameplates.colors.power[info[#info]] t.r, t.g, t.b, t.a = r, g, b, a NP:ConfigureAll() end)
 E.Options.args.nameplates.args.colorsGroup.args.power.inline = true
