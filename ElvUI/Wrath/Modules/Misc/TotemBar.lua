33eb35509d113830157b51270f247b1834678435
Eltreum <eltreum2048@gmail.com>
2022-07-25 23:56:27 -0300

    Change Totem Flyout Strata...
    ..so that it does not hide behind other frames like player UF or action bars

--- ElvUI/Wrath/Modules/Misc/TotemBar.lua
+++ ElvUI/Wrath/Modules/Misc/TotemBar.lua
@@ -93,7 +93,8 @@ function AB:MultiCastFlyoutFrame_ToggleFlyout(frame, type, parent)
 
 			AB:HookScript(button, 'OnEnter', 'TotemOnEnter')
 			AB:HookScript(button, 'OnLeave', 'TotemOnLeave')
-
+			
+			button.icon:SetFrameStrata('MEDIUM')
 			button.icon:SetDrawLayer('ARTWORK')
 			button.icon:SetInside(button)
 
