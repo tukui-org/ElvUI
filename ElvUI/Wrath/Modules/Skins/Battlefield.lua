ccb0ac6b5d8c7af56eae53b9fd72bdb38025392e
Luckyone <Luckyone.Development@pm.me>
2022-07-26 16:01:07 +0200

    Battlefield Queue Skin

--- ElvUI/Wrath/Modules/Skins/Battlefield.lua
+++ ElvUI/Wrath/Modules/Skins/Battlefield.lua
@@ -6,7 +6,21 @@ local _G = _G
 function S:SkinBattlefield()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.battlefield) then return end
 
-	-- TODO: Wrath
+	local BattlefieldFrame = _G.BattlefieldFrame
+	BattlefieldFrame:StripTextures(true)
+	BattlefieldFrame:CreateBackdrop('Transparent')
+	BattlefieldFrame.backdrop:Point('TOPLEFT', 10, -12)
+	BattlefieldFrame.backdrop:Point('BOTTOMRIGHT', -32, 73)
+
+	_G.BattlefieldFrameInfoScrollFrameChildFrameRewardsInfoDescription:SetTextColor(1, 1, 1)
+
+	S:HandleButton(_G.BattlefieldFrameCancelButton)
+	S:HandleButton(_G.BattlefieldFrameJoinButton)
+	S:HandleButton(_G.BattlefieldFrameGroupJoinButton)
+
+	_G.BattlefieldFrameGroupJoinButton:Point('RIGHT', _G.BattlefieldFrameJoinButton, 'LEFT', -2, 0)
+
+	S:HandleCloseButton(_G.BattlefieldFrameCloseButton)
 end
 
 S:AddCallback('SkinBattlefield')
