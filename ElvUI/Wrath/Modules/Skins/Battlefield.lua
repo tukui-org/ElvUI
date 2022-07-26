c72aa18e2e772d366388419719f821ff08aa86ad
Luckyone <Luckyone.Development@pm.me>
2022-07-27 00:21:40 +0200

    Battlefield Queue (Custom Backdrop & Text Color)

--- ElvUI/Wrath/Modules/Skins/Battlefield.lua
+++ ElvUI/Wrath/Modules/Skins/Battlefield.lua
@@ -6,6 +6,7 @@ local _G = _G
 function S:SkinBattlefield()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.battlefield) then return end
 
+	-- Main Frame
 	local BattlefieldFrame = _G.BattlefieldFrame
 	BattlefieldFrame:StripTextures(true)
 	BattlefieldFrame:CreateBackdrop('Transparent')
@@ -13,12 +14,20 @@ function S:SkinBattlefield()
 	BattlefieldFrame.backdrop:Point('BOTTOMRIGHT', -32, 73)
 
 	_G.BattlefieldFrameInfoScrollFrameChildFrameRewardsInfoDescription:SetTextColor(1, 1, 1)
+	_G.BattlefieldFrameInfoScrollFrameChildFrameDescription:SetTextColor(1, 1, 1)
 
 	S:HandleButton(_G.BattlefieldFrameCancelButton)
 	S:HandleButton(_G.BattlefieldFrameJoinButton)
 	S:HandleButton(_G.BattlefieldFrameGroupJoinButton)
 
 	_G.BattlefieldFrameGroupJoinButton:Point('RIGHT', _G.BattlefieldFrameJoinButton, 'LEFT', -2, 0)
+
+	-- Custom Backdrop
+	BottomBackdrop = CreateFrame('Frame', nil, BattlefieldFrame)
+	BottomBackdrop:CreateBackdrop('Transparent')
+	BottomBackdrop:Height(210)
+	BottomBackdrop:Width(330)
+	BottomBackdrop:Point('BOTTOM', BattlefieldFrame, 'BOTTOM', -12, 120)
 
 	S:HandleCloseButton(_G.BattlefieldFrameCloseButton)
 end
