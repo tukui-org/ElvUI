4628d2f3699b6a5d44741dac4e1bc5d360e72c6d
Luckyone <Luckyone.Development@pm.me>
2022-07-28 17:36:37 +0200

    DTs

--- ElvUI/Wrath/Modules/DataTexts/Currencies.lua
+++ ElvUI/Wrath/Modules/DataTexts/Currencies.lua
@@ -1,6 +1,8 @@
 local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
 
+-- TODO: Wrath
+
 local _G = _G
 local format, tonumber, wipe = format, tonumber, wipe
 local pairs, ipairs, unpack, tostring = pairs, ipairs, unpack, tostring
