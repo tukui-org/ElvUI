238326baa7262bd873cbcd03be76ed6ab9e09d98
Simpy <kodi03466@hotmail.com>
2022-09-04 17:20:57 -0400

    dont need this

--- ElvUI/Core/Modules/Misc/AFK.lua
+++ ElvUI/Core/Modules/Misc/AFK.lua
@@ -115,9 +115,9 @@ function AFK:SetAFK(status)
 	end
 end
 
-function AFK:OnEvent(event, arg1, ...)
+function AFK:OnEvent(event, arg1)
 	if event == 'PLAYER_REGEN_DISABLED' or event == 'LFG_PROPOSAL_SHOW' or event == 'UPDATE_BATTLEFIELD_STATUS' then
-		if event ~= 'UPDATE_BATTLEFIELD_STATUS' or (GetBattlefieldStatus(arg1, ...) == 'confirm') then
+		if event ~= 'UPDATE_BATTLEFIELD_STATUS' or (GetBattlefieldStatus(arg1) == 'confirm') then
 			AFK:SetAFK(false)
 		end
 
