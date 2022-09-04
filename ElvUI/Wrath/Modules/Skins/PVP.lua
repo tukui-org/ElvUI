8660482f795b3be6de492919f5d31bdbe6077f9c
Luckyone <Luckyone.Development@pm.me>
2022-09-04 11:01:10 +0200

    PVP Queue Popup + xml

--- ElvUI/Wrath/Modules/Skins/PVP.lua
+++ ElvUI/Wrath/Modules/Skins/PVP.lua
@@ -6,12 +6,11 @@ local _G = _G
 function S:PVPReadyDialog()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.pvp) then return end
 
-	--PVP QUEUE FRAME
+	-- PvP Queue Popup
 	_G.PVPReadyDialog:StripTextures()
 	_G.PVPReadyDialog:SetTemplate('Transparent')
 	S:HandleButton(_G.PVPReadyDialogEnterBattleButton)
 	S:HandleButton(_G.PVPReadyDialogHideButton)
-	--S:HandleCloseButton(_G.PVPReadyDialogCloseButton)
 end
 
 S:AddCallback('PVPReadyDialog')
