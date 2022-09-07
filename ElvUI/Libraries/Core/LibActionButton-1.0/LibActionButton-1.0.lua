588e8ecf8bd7679fcc7d0bde6470d8e54d7c9506
Simpy <kodi03466@hotmail.com>
2022-09-07 18:20:34 -0400

    blah

--- ElvUI/Libraries/Core/LibActionButton-1.0/LibActionButton-1.0.lua
+++ ElvUI/Libraries/Core/LibActionButton-1.0/LibActionButton-1.0.lua
@@ -60,8 +60,8 @@ lib.activeButtons = lib.activeButtons or {}
 lib.actionButtons = lib.actionButtons or {}
 lib.nonActionButtons = lib.nonActionButtons or {}
 
-lib.AuraButtons = lib.AuraButtons or {}
-local AuraButtons = lib.AuraButtons
+local AuraButtons = lib.AuraButtons or {}
+lib.AuraButtons = AuraButtons
 
 lib.ChargeCooldowns = lib.ChargeCooldowns or {}
 lib.NumChargeCooldowns = lib.NumChargeCooldowns or 0
