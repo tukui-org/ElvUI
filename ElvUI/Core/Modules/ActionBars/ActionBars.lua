8f4f3ca91a310268d3d011bf120f5faf23eae3a1
Simpy <kodi03466@hotmail.com>
2022-09-08 03:35:18 -0400

    cause

--- ElvUI/Core/Modules/ActionBars/ActionBars.lua
+++ ElvUI/Core/Modules/ActionBars/ActionBars.lua
@@ -1492,8 +1492,8 @@ function AB:LAB_CooldownUpdate(button, _, duration)
 	end
 end
 
-function AB:PLAYER_ENTERING_WORLD(_, initLogin, isReload)
-	AB:AdjustMaxStanceButtons('PLAYER_ENTERING_WORLD')
+function AB:PLAYER_ENTERING_WORLD(event, initLogin, isReload)
+	AB:AdjustMaxStanceButtons(event)
 
 	if (initLogin or isReload) and (E.Wrath and E.myclass == 'SHAMAN') and E.private.general.totemBar then
 		AB:SecureHook('ShowMultiCastActionBar', 'PositionAndSizeTotemBar')
