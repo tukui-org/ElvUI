3dcd742ffab3ad4410eb62c7d8bf19d178af0b10
Simpy <kodi03466@hotmail.com>
2022-09-17 12:53:27 -0400

    fix retail filters

--- ElvUI/Mainline/Filters/Filters.lua
+++ ElvUI/Mainline/Filters/Filters.lua
@@ -1467,69 +1467,69 @@ G.unitframe.aurafilters.RaidBuffsElvUI = {
 G.unitframe.aurawatch = {
 	GLOBAL = {},
 	ROGUE = {
-		[57934]		= UF:AuraWatch_AddSpell(57934, 'TOPRIGHT', {0.89, 0.09, 0.05}),		-- Tricks of the Trade
+		[57934]		= UF:AuraWatch_AddSpell(57934, nil, 'TOPRIGHT', {0.89, 0.09, 0.05}),		-- Tricks of the Trade
 	},
 	WARRIOR = {
-		[3411]		= UF:AuraWatch_AddSpell(3411, 'TOPRIGHT', {0.89, 0.09, 0.05}),		-- Intervene
+		[3411]		= UF:AuraWatch_AddSpell(3411, nil, 'TOPRIGHT', {0.89, 0.09, 0.05}),		-- Intervene
 	},
 	PRIEST = {
-		[139]		= UF:AuraWatch_AddSpell(139, 'BOTTOMLEFT', {0.4, 0.7, 0.2}),		-- Renew
-		[17]		= UF:AuraWatch_AddSpell(17, 'TOPLEFT', {0.7, 0.7, 0.7}, true), 		-- Power Word: Shield
-		[193065]	= UF:AuraWatch_AddSpell(193065, 'BOTTOMRIGHT', {0.54, 0.21, 0.78}),	-- Masochism
-		[194384]	= UF:AuraWatch_AddSpell(194384, 'TOPRIGHT', {1, 1, 0.66}), 			-- Atonement
-		[214206]	= UF:AuraWatch_AddSpell(214206, 'TOPRIGHT', {1, 1, 0.66}), 			-- Atonement (PvP)
-		[33206]		= UF:AuraWatch_AddSpell(33206, 'LEFT', {0.47, 0.35, 0.74}, true),	-- Pain Suppression
-		[41635]		= UF:AuraWatch_AddSpell(41635, 'BOTTOMRIGHT', {0.2, 0.7, 0.2}),		-- Prayer of Mending
-		[47788]		= UF:AuraWatch_AddSpell(47788, 'LEFT', {0.86, 0.45, 0}, true), 		-- Guardian Spirit
-		[6788]		= UF:AuraWatch_AddSpell(6788, 'BOTTOMLEFT', {0.89, 0.1, 0.1}), 		-- Weakened Soul
+		[139]		= UF:AuraWatch_AddSpell(139, nil, 'BOTTOMLEFT', {0.4, 0.7, 0.2}),		-- Renew
+		[17]		= UF:AuraWatch_AddSpell(17, nil, 'TOPLEFT', {0.7, 0.7, 0.7}, true), 		-- Power Word: Shield
+		[193065]	= UF:AuraWatch_AddSpell(193065, nil, 'BOTTOMRIGHT', {0.54, 0.21, 0.78}),	-- Masochism
+		[194384]	= UF:AuraWatch_AddSpell(194384, nil, 'TOPRIGHT', {1, 1, 0.66}), 			-- Atonement
+		[214206]	= UF:AuraWatch_AddSpell(214206, nil, 'TOPRIGHT', {1, 1, 0.66}), 			-- Atonement (PvP)
+		[33206]		= UF:AuraWatch_AddSpell(33206, nil, 'LEFT', {0.47, 0.35, 0.74}, true),	-- Pain Suppression
+		[41635]		= UF:AuraWatch_AddSpell(41635, nil, 'BOTTOMRIGHT', {0.2, 0.7, 0.2}),		-- Prayer of Mending
+		[47788]		= UF:AuraWatch_AddSpell(47788, nil, 'LEFT', {0.86, 0.45, 0}, true), 		-- Guardian Spirit
+		[6788]		= UF:AuraWatch_AddSpell(6788, nil, 'BOTTOMLEFT', {0.89, 0.1, 0.1}), 		-- Weakened Soul
 	},
 	DRUID = {
-		[774]		= UF:AuraWatch_AddSpell(774, 'TOPRIGHT', {0.8, 0.4, 0.8}), 			-- Rejuvenation
-		[155777]	= UF:AuraWatch_AddSpell(155777, 'RIGHT', {0.8, 0.4, 0.8}), 			-- Germination
-		[8936]		= UF:AuraWatch_AddSpell(8936, 'BOTTOMLEFT', {0.2, 0.8, 0.2}),		-- Regrowth
-		[33763]		= UF:AuraWatch_AddSpell(33763, 'TOPLEFT', {0.4, 0.8, 0.2}), 		-- Lifebloom
-		[188550]	= UF:AuraWatch_AddSpell(188550, 'TOPLEFT', {0.4, 0.8, 0.2}),		-- Lifebloom (Shadowlands Legendary)
-		[48438]		= UF:AuraWatch_AddSpell(48438, 'BOTTOMRIGHT', {0.8, 0.4, 0}),		-- Wild Growth
-		[207386]	= UF:AuraWatch_AddSpell(207386, 'TOP', {0.4, 0.2, 0.8}), 			-- Spring Blossoms
-		[102351]	= UF:AuraWatch_AddSpell(102351, 'LEFT', {0.2, 0.8, 0.8}),			-- Cenarion Ward (Initial Buff)
-		[102352]	= UF:AuraWatch_AddSpell(102352, 'LEFT', {0.2, 0.8, 0.8}),			-- Cenarion Ward (HoT)
-		[200389]	= UF:AuraWatch_AddSpell(200389, 'BOTTOM', {1, 1, 0.4}),				-- Cultivation
-		[203554]	= UF:AuraWatch_AddSpell(203554, 'TOP', {1, 1, 0.4}),				-- Focused Growth (PvP)
+		[774]		= UF:AuraWatch_AddSpell(774, nil, 'TOPRIGHT', {0.8, 0.4, 0.8}), 			-- Rejuvenation
+		[155777]	= UF:AuraWatch_AddSpell(155777, nil, 'RIGHT', {0.8, 0.4, 0.8}), 			-- Germination
+		[8936]		= UF:AuraWatch_AddSpell(8936, nil, 'BOTTOMLEFT', {0.2, 0.8, 0.2}),		-- Regrowth
+		[33763]		= UF:AuraWatch_AddSpell(33763, nil, 'TOPLEFT', {0.4, 0.8, 0.2}), 		-- Lifebloom
+		[188550]	= UF:AuraWatch_AddSpell(188550, nil, 'TOPLEFT', {0.4, 0.8, 0.2}),		-- Lifebloom (Shadowlands Legendary)
+		[48438]		= UF:AuraWatch_AddSpell(48438, nil, 'BOTTOMRIGHT', {0.8, 0.4, 0}),		-- Wild Growth
+		[207386]	= UF:AuraWatch_AddSpell(207386, nil, 'TOP', {0.4, 0.2, 0.8}), 			-- Spring Blossoms
+		[102351]	= UF:AuraWatch_AddSpell(102351, nil, 'LEFT', {0.2, 0.8, 0.8}),			-- Cenarion Ward (Initial Buff)
+		[102352]	= UF:AuraWatch_AddSpell(102352, nil, 'LEFT', {0.2, 0.8, 0.8}),			-- Cenarion Ward (HoT)
+		[200389]	= UF:AuraWatch_AddSpell(200389, nil, 'BOTTOM', {1, 1, 0.4}),				-- Cultivation
+		[203554]	= UF:AuraWatch_AddSpell(203554, nil, 'TOP', {1, 1, 0.4}),				-- Focused Growth (PvP)
 	},
 	PALADIN = {
-		[53563]		= UF:AuraWatch_AddSpell(53563, 'TOPRIGHT', {0.7, 0.3, 0.7}),			-- Beacon of Light
-		[156910]	= UF:AuraWatch_AddSpell(156910, 'TOPRIGHT', {0.7, 0.3, 0.7}),			-- Beacon of Faith
-		[200025]	= UF:AuraWatch_AddSpell(200025, 'TOPRIGHT', {0.7, 0.3, 0.7}),			-- Beacon of Virtue
-		[1022]		= UF:AuraWatch_AddSpell(1022, 'BOTTOMRIGHT', {0.2, 0.2, 1}, true), 		-- Blessing of Protection
-		[1044]		= UF:AuraWatch_AddSpell(1044, 'BOTTOMRIGHT', {0.89, 0.45, 0}, true),	-- Blessing of Freedom
-		[6940]		= UF:AuraWatch_AddSpell(6940, 'BOTTOMRIGHT', {0.89, 0.1, 0.1}, true),	-- Blessing of Sacrifice
-		[204018]	= UF:AuraWatch_AddSpell(204018, 'BOTTOMRIGHT', {0.2, 0.2, 1}, true),	-- Blessing of Spellwarding
-		[223306]	= UF:AuraWatch_AddSpell(223306, 'BOTTOMLEFT', {0.7, 0.7, 0.3}),			-- Bestow Faith
-		[287280]	= UF:AuraWatch_AddSpell(287280, 'TOPLEFT', {0.2, 0.8, 0.2}),			-- Glimmer of Light (T50 Talent)
-		[157047]	= UF:AuraWatch_AddSpell(157047, 'TOP', {0.15, 0.58, 0.84}),				-- Saved by the Light (T25 Talent)
+		[53563]		= UF:AuraWatch_AddSpell(53563, nil, 'TOPRIGHT', {0.7, 0.3, 0.7}),			-- Beacon of Light
+		[156910]	= UF:AuraWatch_AddSpell(156910, nil, 'TOPRIGHT', {0.7, 0.3, 0.7}),			-- Beacon of Faith
+		[200025]	= UF:AuraWatch_AddSpell(200025, nil, 'TOPRIGHT', {0.7, 0.3, 0.7}),			-- Beacon of Virtue
+		[1022]		= UF:AuraWatch_AddSpell(1022, nil, 'BOTTOMRIGHT', {0.2, 0.2, 1}, true), 		-- Blessing of Protection
+		[1044]		= UF:AuraWatch_AddSpell(1044, nil, 'BOTTOMRIGHT', {0.89, 0.45, 0}, true),	-- Blessing of Freedom
+		[6940]		= UF:AuraWatch_AddSpell(6940, nil, 'BOTTOMRIGHT', {0.89, 0.1, 0.1}, true),	-- Blessing of Sacrifice
+		[204018]	= UF:AuraWatch_AddSpell(204018, nil, 'BOTTOMRIGHT', {0.2, 0.2, 1}, true),	-- Blessing of Spellwarding
+		[223306]	= UF:AuraWatch_AddSpell(223306, nil, 'BOTTOMLEFT', {0.7, 0.7, 0.3}),			-- Bestow Faith
+		[287280]	= UF:AuraWatch_AddSpell(287280, nil, 'TOPLEFT', {0.2, 0.8, 0.2}),			-- Glimmer of Light (T50 Talent)
+		[157047]	= UF:AuraWatch_AddSpell(157047, nil, 'TOP', {0.15, 0.58, 0.84}),				-- Saved by the Light (T25 Talent)
 	},
 	SHAMAN = {
-		[61295]		= UF:AuraWatch_AddSpell(61295, 'TOPRIGHT', {0.7, 0.3, 0.7}),		-- Riptide
-		[974]		= UF:AuraWatch_AddSpell(974, 'BOTTOMRIGHT', {0.2, 0.2, 1}),			-- Earth Shield
+		[61295]		= UF:AuraWatch_AddSpell(61295, nil, 'TOPRIGHT', {0.7, 0.3, 0.7}),		-- Riptide
+		[974]		= UF:AuraWatch_AddSpell(974, nil, 'BOTTOMRIGHT', {0.2, 0.2, 1}),			-- Earth Shield
 	},
 	HUNTER = {
-		[90361]		= UF:AuraWatch_AddSpell(90361, 'TOP', {0.34, 0.47, 0.31}),			-- Spirit Mend (HoT)
+		[90361]		= UF:AuraWatch_AddSpell(90361, nil, 'TOP', {0.34, 0.47, 0.31}),			-- Spirit Mend (HoT)
 	},
 	MONK = {
-		[115175]	= UF:AuraWatch_AddSpell(115175, 'TOP', {0.6, 0.9, 0.9}),			-- Soothing Mist
-		[116841]	= UF:AuraWatch_AddSpell(116841, 'RIGHT', {0.12, 1.00, 0.53}),		-- Tiger's Lust (Freedom)
-		[116849]	= UF:AuraWatch_AddSpell(116849, 'TOPRIGHT', {0.2, 0.8, 0.2}, true),	-- Life Cocoon
-		[119611]	= UF:AuraWatch_AddSpell(119611, 'TOPLEFT', {0.3, 0.8, 0.6}),		-- Renewing Mist
-		[124682]	= UF:AuraWatch_AddSpell(124682, 'BOTTOMLEFT', {0.8, 0.8, 0.25}),	-- Enveloping Mist
-		[191840]	= UF:AuraWatch_AddSpell(191840, 'BOTTOMRIGHT', {0.27, 0.62, 0.7}),	-- Essence Font
-		[325209]	= UF:AuraWatch_AddSpell(325209, 'BOTTOM', {0.3, 0.6, 0.6}),			-- Enveloping Breath
+		[115175]	= UF:AuraWatch_AddSpell(115175, nil, 'TOP', {0.6, 0.9, 0.9}),			-- Soothing Mist
+		[116841]	= UF:AuraWatch_AddSpell(116841, nil, 'RIGHT', {0.12, 1.00, 0.53}),		-- Tiger's Lust (Freedom)
+		[116849]	= UF:AuraWatch_AddSpell(116849, nil, 'TOPRIGHT', {0.2, 0.8, 0.2}, true),	-- Life Cocoon
+		[119611]	= UF:AuraWatch_AddSpell(119611, nil, 'TOPLEFT', {0.3, 0.8, 0.6}),		-- Renewing Mist
+		[124682]	= UF:AuraWatch_AddSpell(124682, nil, 'BOTTOMLEFT', {0.8, 0.8, 0.25}),	-- Enveloping Mist
+		[191840]	= UF:AuraWatch_AddSpell(191840, nil, 'BOTTOMRIGHT', {0.27, 0.62, 0.7}),	-- Essence Font
+		[325209]	= UF:AuraWatch_AddSpell(325209, nil, 'BOTTOM', {0.3, 0.6, 0.6}),			-- Enveloping Breath
 	},
 	PET = {
 		-- Warlock Pets
-		[193396]	= UF:AuraWatch_AddSpell(193396, 'TOPRIGHT', {0.6, 0.2, 0.8}, true),		-- Demonic Empowerment
+		[193396]	= UF:AuraWatch_AddSpell(193396, nil, 'TOPRIGHT', {0.6, 0.2, 0.8}, true),		-- Demonic Empowerment
 		-- Hunter Pets
-		[272790]	= UF:AuraWatch_AddSpell(272790, 'TOPLEFT', {0.89, 0.09, 0.05}, true),	-- Frenzy
-		[136]		= UF:AuraWatch_AddSpell(136, 'TOPRIGHT', {0.2, 0.8, 0.2}, true),		-- Mend Pet
+		[272790]	= UF:AuraWatch_AddSpell(272790, nil, 'TOPLEFT', {0.89, 0.09, 0.05}, true),	-- Frenzy
+		[136]		= UF:AuraWatch_AddSpell(136, nil, 'TOPRIGHT', {0.2, 0.8, 0.2}, true),		-- Mend Pet
 	},
 	-- Keep those for reference to G.unitframe.aurawatch[E.myclass][SomeValue]
 	WARLOCK = {},
