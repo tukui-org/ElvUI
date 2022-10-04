8e6a91d2638928bb6ec8effb9e4985d495b397fd
Azilroka <darktemplar9900@yahoo.com>
2022-10-03 19:51:41 -0500

    Revert "Add this toggle back. Health Frequent Updates"
    This reverts commit 38384c819cfef50fccb898746a01bac5a36d896b.

--- ElvUI/Core/Modules/UnitFrames/Groups/Raid.lua
+++ ElvUI/Core/Modules/UnitFrames/Groups/Raid.lua
@@ -98,8 +98,6 @@ function UF:Update_RaidFrames(frame, db)
 		frame.USE_INFO_PANEL = not frame.USE_MINI_POWERBAR and not frame.USE_POWERBAR_OFFSET and db.infoPanel.enable
 		frame.INFO_PANEL_HEIGHT = frame.USE_INFO_PANEL and db.infoPanel.height or 0
 		frame.BOTTOM_OFFSET = UF:GetHealthBottomOffset(frame)
-
-		frame.Health.frequentUpdates = db.health.frequentUpdates
 	end
 
 	if db.enable and not frame:IsEnabled() then
