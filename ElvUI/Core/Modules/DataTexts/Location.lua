464018dadbdc3c7b7dc18f8fb4a220e3a4538599
Simpy <kodi03466@hotmail.com>
2022-10-14 17:34:06 -0400

    recode #375

--- ElvUI/Core/Modules/DataTexts/Location.lua
+++ ElvUI/Core/Modules/DataTexts/Location.lua
@@ -1,31 +1,27 @@
 local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
 
-
---WoW API caching
+local _G = _G
 local InCombatLockdown = InCombatLockdown
 local GetZonePVPInfo = GetZonePVPInfo
 local IsInInstance = IsInInstance
---
+local ToggleFrame = ToggleFrame
 
 local mapInfo = E.MapInfo
 
--- pulled from Blizz's ZoneText.lua
-local colors = {
-	["arena"] = {r = 1, g = 0.1, b = 0.1},
-	["combat"] = {r = 1, g = 0.1, b = 0.1},
-	["contested"] = {r = 1, g = 0.7, b = 0.10},
-	["friendly"] = {r = 0.1, g = 1, b = 0.1},
-	["hostile"] = {r = 1, g = 0.1, b = 0.1},
-	["instance"] = {r = 1, g = 0.1, b =  0.1},
-	["sanctuary"] = {r = 0.41, g = 0.8, b = 0.94},
+local colors = { -- pulled from Blizz's ZoneText.lua
+	none		= {r = 1, g = 1, b = 0},
+	arena		= {r = 1.0, g = 0.1, b = 0.1},
+	combat		= {r = 1.0, g = 0.1, b = 0.1},
+	contested	= {r = 1.0, g = 0.7, b = 0.1},
+	friendly	= {r = 0.1, g = 1.0, b = 0.1},
+	hostile		= {r = 1.0, g = 0.1, b = 0.1},
+	instance	= {r = 1.0, g = 0.1, b = 0.1},
+	sanctuary	= {r = 0.4, g = 0.8, b = 0.9},
 }
 
 local function GetStatus()
-	local pvpType = GetZonePVPInfo()
-	local inInstance = IsInInstance()
-
-	return inInstance and colors["instance"] or colors[pvpType] or {r = 1, g = 1, b = 0}
+	return IsInInstance() and colors.instance or colors[GetZonePVPInfo()] or colors.none
 end
 
 local function OnEvent(self)
@@ -35,45 +31,26 @@ local function OnEvent(self)
 	end
 
 	local db = E.global.datatexts.settings.Location
+	local color = db.color == 'REACTION' and GetStatus() or db.color == 'CLASS' and E:ClassColor(E.myclass) or db.customColor
 
-	local text
-	if db.showContinent then
-		text = mapInfo.continentName
+	local continent = db.showContinent and mapInfo.continentName or ''
+	local subzone = db.showSubZone and mapInfo.subZoneText or ''
+	local zone = db.showZone and ((mapInfo.subZoneText == mapInfo.zoneText and mapInfo.realZoneText) or mapInfo.zoneText) or ''
+
+	if zone ~= '' or subzone ~= '' or continent ~= '' then
+		local first = continent ~= '' and zone ~= '' and ': ' or ''
+		local second = (zone ~= '' or continent ~= '') and subzone ~= '' and ': ' or ''
+
+		self.text:SetFormattedText('%s%s%s%s%s%s|r', E:RGBToHex(color.r, color.g, color.b), continent, first, zone, second, subzone)
+	else
+		self.text:SetText('N/A')
 	end
-
-	if db.showZone then
-		text = text and (text .. ': ' .. mapInfo.zoneText) or mapInfo.zoneText
-	end
-
-	if db.showSubZone then
-		if mapInfo.subZoneText ~= mapInfo.zoneText then
-			text = text and (text .. ': ' .. mapInfo.subZoneText) or mapInfo.subZoneText
-		end
-	end
-
-	local color = db.customColor
-	if db.color == 'CLASS' then
-		local cc = E:ClassColor(E.myclass)
-		color = {r = cc.r, g = cc.g, b = cc.b}
-	elseif db.color == 'REACTION' then
-		color = GetStatus()
-	end
-
-	self.text:SetText(E:RGBToHex(color.r, color.g, color.b, nil, text or 'N/A'))
 end
 
---credit: ElvUI - Coordinates.lua datatext
 local function OnClick()
 	if InCombatLockdown() then _G.UIErrorsFrame:AddMessage(E.InfoColor.._G.ERR_NOT_IN_COMBAT) return end
-	_G.ToggleFrame(_G.WorldMapFrame)
+
+	ToggleFrame(_G.WorldMapFrame)
 end
 
-
-local events = {
-	'LOADING_SCREEN_DISABLED',
-	'ZONE_CHANGED_NEW_AREA',
-	'ZONE_CHANGED_INDOORS',
-	'ZONE_CHANGED'
-}
-
-DT:RegisterDatatext('Location', nil, events, OnEvent, nil, OnClick, nil, nil, L['Location'])
+DT:RegisterDatatext('Location', nil, { 'LOADING_SCREEN_DISABLED', 'ZONE_CHANGED_NEW_AREA', 'ZONE_CHANGED_INDOORS', 'ZONE_CHANGED' }, OnEvent, nil, OnClick, nil, nil, L['Location'])
