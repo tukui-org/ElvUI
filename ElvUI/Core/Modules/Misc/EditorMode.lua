e2f8cd88fbc21e0e94fbaf7d5e410c43940f88d9
Simpy <kodi03466@hotmail.com>
2022-10-16 13:40:39 -0400

    only shut down their movers when ours is not active

--- ElvUI/Core/Modules/Misc/EditorMode.lua
+++ ElvUI/Core/Modules/Misc/EditorMode.lua
@@ -5,15 +5,18 @@ local tremove = tremove
 local strmatch = strmatch
 
 local IgnoreFrames = {
-	'^ChatFrame%d+',
-	'MinimapCluster' -- for now, minimap header fix WoW10
+	['^ChatFrame%d+'] = function() return E.private.chat.enable end,
+
+	-- for now just going to use our mover, will need to add the setting
+	-- header underneath and rotate minimap settings
+	['MinimapCluster'] = function() return E.private.general.minimap.enable end
 }
 
 function EM:Initialize()
 	local frames = _G.EditModeManagerFrame.registeredSystemFrames
 	for index, frame in next, frames do
-		for _, ignore in next, IgnoreFrames do
-			if strmatch(frame:GetName(), ignore) then
+		for name, func in next, IgnoreFrames do
+			if strmatch(frame:GetName(), name) and func() then
 				tremove(frames, index)
 			end
 		end
