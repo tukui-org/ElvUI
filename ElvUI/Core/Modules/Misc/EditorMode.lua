9db5fabdfa4f767ad79b6503d25d3628e5b457ad
Simpy <kodi03466@hotmail.com>
2022-10-16 17:00:50 -0400

    blah

--- ElvUI/Core/Modules/Misc/EditorMode.lua
+++ ElvUI/Core/Modules/Misc/EditorMode.lua
@@ -1,33 +1,33 @@
 local E, L, V, P, G = unpack(ElvUI)
 local EM = E:GetModule('EditorMode')
 
+local _G = _G
 local tremove = tremove
 local strmatch = strmatch
 
-local CheckCastFrame = function() return E.private.unitframe.disabledBlizzardFrames.castbar end
-local CheckRaidFrame = function() return E.private.unitframe.disabledBlizzardFrames.raid end
-local CheckBossFrame = function() return E.private.unitframe.disabledBlizzardFrames.boss end
-local CheckArenaFrame = function() return E.private.unitframe.disabledBlizzardFrames.arena end
 local CheckTargetFrame = function() return E.private.unitframe.disabledBlizzardFrames.target end
+local CheckCastFrame = function() return E.private.unitframe.disabledBlizzardFrames.castbar end
+local CheckArenaFrame = function() return E.private.unitframe.disabledBlizzardFrames.arena end
 local CheckPartyFrame = function() return E.private.unitframe.disabledBlizzardFrames.party end
 local CheckFocusFrame = function() return E.private.unitframe.disabledBlizzardFrames.focus end
+local CheckRaidFrame = function() return E.private.unitframe.disabledBlizzardFrames.raid end
+local CheckBossFrame = function() return E.private.unitframe.disabledBlizzardFrames.boss end
 local CheckAuraFrame = function() return E.private.auras.disableBlizzard end
 local CheckActionBar = function() return E.private.actionbar.enable end
 
 local IgnoreFrames = {
-	--- MinimapCluster: header underneath and rotate minimap (will need to add the setting)
-	MinimapCluster = function() return E.private.general.minimap.enable end,
+	MinimapCluster = function() return E.private.general.minimap.enable end, -- header underneath and rotate minimap (will need to add the setting)
 	GameTooltipDefaultContainer = function() return E.private.tooltip.enable end,
 
 	-- UnitFrames
-	PlayerCastingBarFrame = CheckCastFrame,
-	PlayerFrame = function() return E.private.unitframe.disabledBlizzardFrames.player end,
 	PartyFrame = CheckPartyFrame,
-	TargetFrame = CheckTargetFrame,
 	FocusFrame = CheckFocusFrame,
+	TargetFrame = CheckTargetFrame,
+	PlayerCastingBarFrame = CheckCastFrame,
 	ArenaEnemyFramesContainer = CheckArenaFrame,
 	CompactRaidFrameContainer = CheckRaidFrame,
 	BossTargetFrameContainer = CheckBossFrame,
+	PlayerFrame = function() return E.private.unitframe.disabledBlizzardFrames.player end,
 
 	-- Auras
 	BuffFrame = function() return E.private.auras.disableBlizzard and E.private.auras.enable and E.private.auras.buffsHeader end,
@@ -52,38 +52,41 @@ local PatternFrames = {
 	['^ChatFrame%d+'] = function() return E.private.chat.enable end,
 }
 
+local function PatternIgnore(frameName)
+	for pattern, patternFunc in pairs(PatternFrames) do
+		if strmatch(frameName, pattern) and patternFunc() then
+			return true
+		end
+	end
+end
+
 function EM:Initialize()
 	local editMode = _G.EditModeManagerFrame
 
+	-- remove the initial registers
+	local registered = editMode.registeredSystemFrames
+	for i = #registered, 1, -1 do
+		local name = registered[i]:GetName()
+		local ignore = IgnoreFrames[name]
+
+		if ignore and ignore() or PatternIgnore(name) then
+			tremove(editMode.registeredSystemFrames, i)
+		end
+	end
+
 	-- account settings will be tainted
 	local mixin = editMode.AccountSettings
+	if CheckCastFrame then mixin.RefreshCastBar = E.noop end
+	if CheckAuraFrame then mixin.RefreshAuraFrame = E.noop end
 	if CheckBossFrame then mixin.RefreshBossFrames = E.noop end
 	if CheckRaidFrame then mixin.RefreshRaidFrames = E.noop end
 	if CheckArenaFrame then mixin.RefreshArenaFrames = E.noop end
-	if CheckCastFrame then mixin.RefreshCastBar = E.noop end
-	if CheckAuraFrame then mixin.RefreshAuraFrame = E.noop end
 	if CheckPartyFrame then mixin.RefreshPartyFrames = E.noop end
 	if CheckTargetFrame or CheckFocusFrame then mixin.RefreshTargetAndFocus = E.noop end
 	if CheckActionBar then
 		mixin.RefreshVehicleLeaveButton = E.noop
 		mixin.RefreshActionBarShown = E.noop
 		mixin.RefreshEncounterBar = E.noop
-	end
-
-	-- remove the initial registers
-	local frames = editMode.registeredSystemFrames
-	for index, frame in next, frames do
-		local frameName = frame:GetName()
-		local ignoreFunc = IgnoreFrames[frameName]
-		if ignoreFunc and ignoreFunc() then
-			tremove(frames, index)
-		end
-
-		for pattern, patternFunc in next, PatternFrames do
-			if strmatch(frameName, pattern) and patternFunc() then
-				tremove(frames, index)
-			end
-		end
 	end
 end
 
