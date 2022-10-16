9d236f8b7ae8977e777e12d486dc09c45ea16643
Simpy <kodi03466@hotmail.com>
2022-10-16 19:55:07 -0400

    fix addon list skin

--- ElvUI/Mainline/Modules/Skins/AddonManager.lua
+++ ElvUI/Mainline/Modules/Skins/AddonManager.lua
@@ -2,8 +2,10 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
+local next = next
 local gsub = gsub
 local unpack = unpack
+local select = select
 local GetAddOnInfo = GetAddOnInfo
 local GetAddOnEnableState = GetAddOnEnableState
 local UIDropDownMenu_GetSelectedValue = UIDropDownMenu_GetSelectedValue
@@ -13,7 +15,6 @@ function S:AddonList()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.addonManager) then return end
 
 	local AddonList = _G.AddonList
-	local maxShown = _G.MAX_ADDONS_DISPLAYED
 	local AddonCharacterDropDown = _G.AddonCharacterDropDown
 
 	S:HandlePortraitFrame(AddonList)
@@ -26,77 +27,74 @@ function S:AddonList()
 	S:HandleCheckBox(_G.AddonListForceLoad)
 	_G.AddonListForceLoad:Size(26, 26)
 
-	--[[hooksecurefunc('AddonList_Update', function()
-		for i = 1, maxShown do
-			local entry = _G['AddonListEntry'..i]
-			if entry and entry:IsShown() then
-				local id = entry:GetID()
-				local text = _G['AddonListEntry'..i..'Title']
-				local checkbox = _G['AddonListEntry'..i..'Enabled']
-				local checktex = checkbox:GetCheckedTexture()
+	hooksecurefunc('AddonList_Update', function()
+		for _, entry in next, { AddonList.ScrollBox.ScrollTarget:GetChildren() } do
+			local id = entry:GetID()
+			local text = entry.Title
+			local checkbox = entry.Enabled
+			local checktex = checkbox:GetCheckedTexture()
 
-				local checkall -- Get the character from the current list (nil is all characters)
-				local character = UIDropDownMenu_GetSelectedValue(AddonCharacterDropDown)
-				if character == true then
-					character = nil
-				else
-					checkall = GetAddOnEnableState(nil, id)
-				end
+			local checkall -- Get the character from the current list (nil is all characters)
+			local character = UIDropDownMenu_GetSelectedValue(AddonCharacterDropDown)
+			if character == true then
+				character = nil
+			else
+				checkall = GetAddOnEnableState(nil, id)
+			end
 
-				text:SetFontObject('ElvUIFontNormal')
-				entry.Status:SetFontObject('ElvUIFontSmall')
-				entry.Reload:SetFontObject('ElvUIFontSmall')
-				entry.Reload:SetTextColor(1.0, 0.3, 0.3)
-				entry.LoadAddonButton.Text:SetFontObject('ElvUIFontSmall')
+			text:SetFontObject('ElvUIFontNormal')
+			entry.Status:SetFontObject('ElvUIFontSmall')
+			entry.Reload:SetFontObject('ElvUIFontSmall')
+			entry.Reload:SetTextColor(1.0, 0.3, 0.3)
+			entry.LoadAddonButton.Text:SetFontObject('ElvUIFontSmall')
 
-				local checkstate = GetAddOnEnableState(character, id)
-				local enabledForSome = not character and checkstate == 1
-				local enabled = checkstate > 0
-				local disabled = not enabled or enabledForSome
+			local checkstate = GetAddOnEnableState(character, id)
+			local enabledForSome = not character and checkstate == 1
+			local enabled = checkstate > 0
+			local disabled = not enabled or enabledForSome
 
-				if disabled then
-					entry.Status:SetTextColor(0.4, 0.4, 0.4)
-				else
-					entry.Status:SetTextColor(0.7, 0.7, 0.7)
-				end
+			if disabled then
+				entry.Status:SetTextColor(0.4, 0.4, 0.4)
+			else
+				entry.Status:SetTextColor(0.7, 0.7, 0.7)
+			end
 
-				local name, title, _, loadable, reason = GetAddOnInfo(id)
-				if disabled or reason == 'DEP_DISABLED' then
-					text:SetText(gsub(title or name, '|c%x%x%x%x%x%x%x%x(.-)|?r?','%1'))
-				end
+			local name, title, _, loadable, reason = GetAddOnInfo(id)
+			if disabled or reason == 'DEP_DISABLED' then
+				text:SetText(gsub(title or name, '|c%x%x%x%x%x%x%x%x(.-)|?r?','%1'))
+			end
 
-				if enabledForSome then
-					text:SetTextColor(0.5, 0.5, 0.5)
-				elseif enabled and (loadable or reason == 'DEP_DEMAND_LOADED' or reason == 'DEMAND_LOADED') then
-					text:SetTextColor(0.9, 0.9, 0.9)
-				elseif enabled and reason ~= 'DEP_DISABLED' then
-					text:SetTextColor(1.0, 0.2, 0.2)
-				else
-					text:SetTextColor(0.3, 0.3, 0.3)
-				end
+			if enabledForSome then
+				text:SetTextColor(0.5, 0.5, 0.5)
+			elseif enabled and (loadable or reason == 'DEP_DEMAND_LOADED' or reason == 'DEMAND_LOADED') then
+				text:SetTextColor(0.9, 0.9, 0.9)
+			elseif enabled and reason ~= 'DEP_DISABLED' then
+				text:SetTextColor(1.0, 0.2, 0.2)
+			else
+				text:SetTextColor(0.3, 0.3, 0.3)
+			end
 
-				if not enabled and checkall == 1 then
-					checktex:SetVertexColor(0.3, 0.3, 0.3)
-					checktex:SetDesaturated(true)
-					checktex:Show()
-				elseif not checkstate or checkstate == 0 then
-					checktex:Hide()
-				elseif checkstate == 1 then
-					checktex:SetVertexColor(0.6, 0.6, 0.6)
-					checktex:SetDesaturated(true)
-					checktex:Show()
-				elseif checkstate == 2 then
-					checktex:SetVertexColor(unpack(E.media.rgbvaluecolor))
-					checktex:SetDesaturated(false)
-					checktex:Show()
-				end
+			if not enabled and checkall == 1 then
+				checktex:SetVertexColor(0.3, 0.3, 0.3)
+				checktex:SetDesaturated(true)
+				checktex:Show()
+			elseif not checkstate or checkstate == 0 then
+				checktex:Hide()
+			elseif checkstate == 1 then
+				checktex:SetVertexColor(0.6, 0.6, 0.6)
+				checktex:SetDesaturated(true)
+				checktex:Show()
+			elseif checkstate == 2 then
+				checktex:SetVertexColor(unpack(E.media.rgbvaluecolor))
+				checktex:SetDesaturated(false)
+				checktex:Show()
 			end
 		end
-	end)]]
+	end)
 
-	hooksecurefunc(AddonList.ScrollBox, 'Update', function(self)
-		for i = 1, self.ScrollTarget:GetNumChildren() do
-			local child = select(i, self.ScrollTarget:GetChildren())
+	hooksecurefunc(AddonList.ScrollBox, 'Update', function(frame)
+		for i = 1, frame.ScrollTarget:GetNumChildren() do
+			local child = select(i, frame.ScrollTarget:GetChildren())
 			if not child.IsSkinned then
 				S:HandleCheckBox(child.Enabled)
 				S:HandleButton(child.LoadAddonButton)
