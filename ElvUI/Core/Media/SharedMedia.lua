f6389a1f805a47d5e67d41511c410a4051c6e0b1
Simpy <kodi03466@hotmail.com>
2022-10-16 13:56:28 -0400

    nvm that dont work problem still exists client side

--- ElvUI/Core/Media/SharedMedia.lua
+++ ElvUI/Core/Media/SharedMedia.lua
@@ -216,7 +216,7 @@ AddMedia('logo','SuperBear')
 
 E.Media.CombatIcons.COMBAT = E.Media.Textures.Combat
 
-if not E.Retail then -- LSM Font Preloader ~Simpy
+do -- LSM Font Preloader ~Simpy
 	local preloader = CreateFrame('Frame')
 	preloader:SetPoint('TOP', UIParent, 'BOTTOM', 0, -500)
 	preloader:SetSize(100, 100)
