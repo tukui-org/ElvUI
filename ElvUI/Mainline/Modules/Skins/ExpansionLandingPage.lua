56ffa50581b425e8923821e7ae30d238a9322e22
Merathilis <neo0608@gmail.com>
2022-10-20 20:08:12 +0200

    Update ExpansionLandingPage skin WIP

--- ElvUI/Mainline/Modules/Skins/ExpansionLandingPage.lua
+++ ElvUI/Mainline/Modules/Skins/ExpansionLandingPage.lua
@@ -2,15 +2,43 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
+local select = select
+
+local hooksecurefunc = hooksecurefunc
 
 --ToDo: WoW10
 function S:Blizzard_ExpansionLandingPage()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.expansionLanding) then return end
 
-	-- local ExpansionLandingPage = _G.ExpansionLandingPage
-	-- .self.Overlay = DragohFlightLandingPage
+	local frame = _G.ExpansionLandingPage
+	local panel
 
-	--S:HandleCloseButton(ExpansionLandingPage.Overlay.CloseButton, ExpansionLandingPage.backdrop)
+	if frame.Overlay then
+		for i = 1, frame.Overlay:GetNumChildren() do
+			local child = select(i, frame.Overlay:GetChildren())
+			child:StripTextures()
+			child:SetTemplate('Transparent')
+
+			if child.DragonridingPanel then
+				panel = child
+				break
+			end
+		end
+	end
+
+	if not panel then return end
+
+	if panel.DragonridingPanel then
+		S:HandleButton(panel.DragonridingPanel.SkillsButton)
+	end
+
+	if panel.CloseButton then
+		S:HandleCloseButton(panel.CloseButton)
+	end
+
+	if panel.MajorFactionList then
+		-- ...
+	end
 end
 
 S:AddCallbackForAddon('Blizzard_ExpansionLandingPage')
