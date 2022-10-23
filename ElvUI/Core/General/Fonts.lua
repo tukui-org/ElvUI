9966878a8bf17994df19917110216a1ba54da678
Simpy <kodi03466@hotmail.com>
2022-10-23 12:54:18 -0400

    whack

--- ElvUI/Core/General/Fonts.lua
+++ ElvUI/Core/General/Fonts.lua
@@ -4,12 +4,17 @@ local LSM = E.Libs.LSM
 local _G = _G
 local strmatch = strmatch
 
-local NONE = E.WoW10 and '' or 'NONE'
-
 local function SetFont(obj, font, size, style, sr, sg, sb, sa, sox, soy, r, g, b)
 	if not obj then return end
 
-	obj:SetFont(font, size, style or NONE)
+	-- convert because of bad values between versions
+	if (style == 'NONE' or not style) and E.WoW10 then
+		style = ''
+	elseif (style == '' or not style) and not E.WoW10 then
+		style = 'NONE'
+	end
+
+	obj:SetFont(font, size, style)
 
 	if sr and sg and sb then
 		obj:SetShadowColor(sr, sg, sb, sa)
