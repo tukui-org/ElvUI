70d850fdb2cbdc4ca81e6f158a57c26fa81ef91d
Simpy <kodi03466@hotmail.com>
2022-10-23 13:24:32 -0400

    readd this

--- ElvUI/Core/Modules/Blizzard/AlertFrame.lua
+++ ElvUI/Core/Modules/Blizzard/AlertFrame.lua
@@ -119,6 +119,10 @@ function B:AlertMovers()
 	_G.GroupLootContainer:EnableMouse(false) -- Prevent this weird non-clickable area stuff since 8.1; Monitor this, as it may cause addon compatibility.
 	E:CreateMover(AlertFrameHolder, 'AlertFrameMover', L["Loot / Alert Frames"], nil, nil, E.PostAlertMove, nil, nil, 'general,blizzUIImprovements')
 
+	if not E.WoW10 then
+		_G.UIPARENT_MANAGED_FRAME_POSITIONS.GroupLootContainer = nil
+	end
+
 	--Replace AdjustAnchors functions to allow alerts to grow down if needed.
 	--We will need to keep an eye on this in case it taints. It shouldn't, but you never know.
 	for _, alertFrameSubSystem in ipairs(_G.AlertFrame.alertFrameSubSystems) do
