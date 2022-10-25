e88971be7bd8040355563397045ec36ea0a8d63a
Simpy <kodi03466@hotmail.com>
2022-10-25 13:16:08 -0400

    sseee

--- ElvUI/Libraries/Core/oUF/elements/classpower.lua
+++ ElvUI/Libraries/Core/oUF/elements/classpower.lua
@@ -115,7 +115,7 @@ local function Update(self, event, unit, powerType)
 
 		max = UnitPowerMax(unit, powerID)
 		mod = UnitPowerDisplayMod(powerID)
-		chargedPoints = GetUnitChargedPowerPoints(unit)
+		chargedPoints = oUF.isRetail and GetUnitChargedPowerPoints(unit)
 
 		-- UNIT_POWER_POINT_CHARGE doesn't provide a power type
 		powerType = powerType or ClassPowerType
