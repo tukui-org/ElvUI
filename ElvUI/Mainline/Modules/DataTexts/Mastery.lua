bfa3192a43f8a559f6ab9e1166834d70e1fcfaa7
Luckyone <Luckyone.Development@pm.me>
2022-10-27 22:50:02 +0200

    Bruuuuuuuuuuuuuuuuh

--- ElvUI/Mainline/Modules/DataTexts/Mastery.lua
+++ ElvUI/Mainline/Modules/DataTexts/Mastery.lua
@@ -18,15 +18,15 @@ local function OnEnter()
 	if primaryTalentTree then
 		local masterySpell, masterySpell2 = GetSpecializationMasterySpells(primaryTalentTree)
 		if masterySpell then
-			local tooltipInfo = CreateBaseTooltipInfo("GetSpellByID", masterySpell);
-			tooltipInfo.append = true;
-			DT.tooltip:ProcessInfo(tooltipInfo);
+			local tooltipInfo = CreateBaseTooltipInfo("GetSpellByID", masterySpell)
+			tooltipInfo.append = true
+			DT.tooltip:ProcessInfo(tooltipInfo)
 		end
 		if masterySpell2 then
 			DT.tooltip:AddLine(' ')
-			local tooltipInfo = CreateBaseTooltipInfo("GetSpellByID", masterySpell2);
-			tooltipInfo.append = true;
-			DT.tooltip:ProcessInfo(tooltipInfo);
+			local tooltipInfo = CreateBaseTooltipInfo("GetSpellByID", masterySpell2)
+			tooltipInfo.append = true
+			DT.tooltip:ProcessInfo(tooltipInfo)
 		end
 
 		DT.tooltip:Show()
