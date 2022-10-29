3cad2a0ce9ee0aeed3287abd2f906c3b33875143
Simpy <kodi03466@hotmail.com>
2022-10-28 21:45:56 -0400

    pretty mirror timers (trying to impress mera)

--- ElvUI/Mainline/Modules/Skins/MirrorTimers.lua
+++ ElvUI/Mainline/Modules/Skins/MirrorTimers.lua
@@ -2,26 +2,52 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
+local CreateFrame = CreateFrame
+local hooksecurefunc = hooksecurefunc
 
-function S:MirrorTimers()
+local function SkinMirrorTimer()
+	local i = 1
+	local frame = _G.MirrorTimer1
+	while frame do
+		if not frame.atlasHolder then
+			frame.atlasHolder = CreateFrame('Frame', nil, frame)
+			frame.atlasHolder:SetClipsChildren(true)
+			frame.atlasHolder:SetInside()
+
+			frame.StatusBar:SetParent(frame.atlasHolder)
+			frame.StatusBar:ClearAllPoints()
+			frame.StatusBar:SetSize(204, 22)
+			frame.StatusBar:Point('TOP', 0, 2)
+
+			frame:SetSize(200, 18)
+
+			frame.Text:FontTemplate()
+			frame.Text:ClearAllPoints()
+			frame.Text:SetParent(frame.StatusBar)
+			frame.Text:SetPoint('CENTER', frame.StatusBar, 0, 1)
+		end
+
+		frame:StripTextures()
+		frame:SetTemplate('Transparent')
+
+		i = i + 1
+		frame = _G['MirrorTimer'..i]
+	end
+end
+
+function S:MirrorTimers() -- Mirror Timers (Underwater Breath, etc.)
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.mirrorTimers) then return end
 
-	--Mirror Timers (Underwater Breath etc.) -- To do, make it pretty WoW10
-	for i = 1, _G.MIRRORTIMER_NUMTIMERS do
-		local bar = _G['MirrorTimer'..i]
-		local statusbar = bar.StatusBar or _G[bar:GetName()..'StatusBar']
+	local i = 1
+	local frame = _G.MirrorTimer1
+	while frame do
+		E:CreateMover(frame, 'MirrorTimer'..i..'Mover', L["MirrorTimer"]..i, nil, nil, nil, 'ALL,SOLO')
 
-		bar:SetTemplate()
-		bar:SetSize(200, 15)
-
-		bar.Text:FontTemplate()
-		bar.Border:Hide()
-
-		statusbar:SetStatusBarTexture(E.media.normTex)
-		statusbar:SetAllPoints()
-
-		E:CreateMover(bar, 'MirrorTimer'..i..'Mover', L["MirrorTimer"]..i, nil, nil, nil, 'ALL,SOLO')
+		i = i + 1
+		frame = _G['MirrorTimer'..i]
 	end
+
+	hooksecurefunc('MirrorTimer_Show', SkinMirrorTimer)
 end
 
 S:AddCallback('MirrorTimers')
