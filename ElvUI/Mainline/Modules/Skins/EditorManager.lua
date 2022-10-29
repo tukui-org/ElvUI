c4ce7546e044977ce02ea2c1646e08281c2b25b0
Simpy <kodi03466@hotmail.com>
2022-10-29 01:54:15 -0400

    updated skin for editor mode

--- ElvUI/Mainline/Modules/Skins/EditorManager.lua
+++ ElvUI/Mainline/Modules/Skins/EditorManager.lua
@@ -2,6 +2,7 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
+local next = next
 
 function S:EditorManagerFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.editor) then return end
@@ -9,13 +10,21 @@ function S:EditorManagerFrame()
 	local editMode = _G.EditModeManagerFrame
 	editMode:StripTextures()
 	editMode:CreateBackdrop('Transparent')
+	editMode.Tutorial:Kill()
 
 	S:HandleCloseButton(editMode.CloseButton)
 	S:HandleButton(editMode.RevertAllChangesButton)
 	S:HandleButton(editMode.SaveChangesButton)
-	--S:HandleDropDownBox(editMode.LayoutDropdown.DropDownMenu)
+	S:HandleDropDownBox(editMode.LayoutDropdown.DropDownMenu, 250)
 
-	-- ToDO: Wait if it not taints anymore xD
+	S:HandleCheckBox(editMode.ShowGridCheckButton.Button)
+	S:HandleCheckBox(editMode.EnableSnapCheckButton.Button)
+
+	for _, frame in next, { editMode.AccountSettings.Settings:GetChildren() } do
+		if frame.Button then
+			S:HandleCheckBox(frame.Button)
+		end
+	end
 end
 
 S:AddCallback('EditorManagerFrame')
