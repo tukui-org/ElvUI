87d4bc04d278e3af72b787bcff255d17d9a67e82
Simpy <kodi03466@hotmail.com>
2022-10-31 15:56:00 -0400

    test

--- ElvUI/Libraries/Core/LibActionButton-1.0/LibActionButton-1.0.lua
+++ ElvUI/Libraries/Core/LibActionButton-1.0/LibActionButton-1.0.lua
@@ -578,7 +578,7 @@ local function PickupAny(kind, target, detail, ...)
 end
 
 function Generic:OnEvent(event, key, down)
-	if WoW10 or not GetCVarBool('lockActionBars') then return end
+	if not GetCVarBool('lockActionBars') then return end
 
 	if event == 'OnLeave' then
 		self:RegisterForClicks('AnyDown')
