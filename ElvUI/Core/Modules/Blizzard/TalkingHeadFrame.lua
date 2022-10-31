c6138354617a6b25f7ee17c9a9885d7490726ebe
Simpy <kodi03466@hotmail.com>
2022-10-31 16:03:37 -0400

    clean up

--- ElvUI/Core/Modules/Blizzard/TalkingHeadFrame.lua
+++ ElvUI/Core/Modules/Blizzard/TalkingHeadFrame.lua
@@ -8,7 +8,7 @@ function B:ScaleTalkingHeadFrame()
 	local frame = _G.TalkingHeadFrame
 	frame:SetScale(E.db.general.talkingHeadFrameScale or 1)
 
-	--Reset Model Camera
+	-- Reset Model Camera
 	local model = frame.MainFrame.Model
 	if model.uiCameraID then
 		model:RefreshCamera()
@@ -16,29 +16,26 @@ function B:ScaleTalkingHeadFrame()
 		_G.Model_ApplyUICamera(model, model.uiCameraID)
 	end
 
-	--Use this to prevent the frame from auto closing, so you have time to test things.
+	-- Use this to prevent the frame from auto closing, so you have time to test things.
 	-- frame:UnregisterEvent('SOUNDKIT_FINISHED')
 	-- frame:UnregisterEvent('TALKINGHEAD_CLOSE')
 	-- frame:UnregisterEvent('LOADING_SCREEN_ENABLED')
 end
 
-local function InitializeTalkingHead()
-	--Prevent WoW from moving the frame around
+function B:PositionTalkingHead()
+	-- Prevent WoW from moving the frame around
 	if not E.Retail then
 		_G.UIPARENT_MANAGED_FRAME_POSITIONS.TalkingHeadFrame = nil
 	end
 
-	--Iterate through all alert subsystems in order to find the one created for TalkingHeadFrame, and then remove it.
-	--We do this to prevent alerts from anchoring to this frame when it is shown.
+	-- Iterate through all alert subsystems in order to find the one created for TalkingHeadFrame, and then remove it.
+	-- We do this to prevent alerts from anchoring to this frame when it is shown.
 	for index, alertFrameSubSystem in ipairs(_G.AlertFrame.alertFrameSubSystems) do
 		if alertFrameSubSystem.anchorFrame and alertFrameSubSystem.anchorFrame == _G.TalkingHeadFrame then
 			tremove(_G.AlertFrame.alertFrameSubSystems, index)
 		end
 	end
-end
 
-function B:PositionTalkingHead()
-	InitializeTalkingHead()
-
+	-- Now scale it
 	B:ScaleTalkingHeadFrame()
 end
