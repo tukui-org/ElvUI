ca2836b42b215598eb31c23153bf42dfd7cc59f6
Simpy <kodi03466@hotmail.com>
2022-11-07 13:18:56 -0500

    s

--- ElvUI/Core/Modules/Misc/Loot.lua
+++ ElvUI/Core/Modules/Misc/Loot.lua
@@ -1,4 +1,5 @@
---[[ Copyright (c) 2007-2012 Trond A Ekseth troeks@gmail.com
+--[[
+	Copyright (c) 2007-2012 Trond A Ekseth troeks@gmail.com
 
 	Permission is hereby granted, free of charge, to any person
 	obtaining a copy of this software and associated documentation
