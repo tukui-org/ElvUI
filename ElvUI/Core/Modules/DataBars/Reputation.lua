a62f1c7c628810ebb016b0452cec43b28419656a
Simpy <kodi03466@hotmail.com>
2022-11-06 19:17:24 -0500

    variation of #511

--- ElvUI/Core/Modules/DataBars/Reputation.lua
+++ ElvUI/Core/Modules/DataBars/Reputation.lua
@@ -56,15 +56,17 @@ function DB:ReputationBar_Update()
 			curValue = C_MajorFactions_HasMaximumRenown(factionID) and majorFactionData.renownLevelThreshold or majorFactionData.renownReputationEarned or 0
 			label = format('%s%s|r %s', renownHex, RENOWN_LEVEL_LABEL, majorFactionData.renownLevel)
 		end
-	elseif C_Reputation_IsFactionParagon(factionID) then
+	end
+
+	if not label and C_Reputation_IsFactionParagon(factionID) then
 		local current, threshold
 		current, threshold, _, rewardPending = C_Reputation_GetFactionParagonInfo(factionID)
 
 		if current and threshold then
 			label, minValue, maxValue, curValue, reaction = L["Paragon"], 0, threshold, current % threshold, 9
-		end
 
-		bar.Reward:SetPoint('CENTER', bar, DB.db.reputation.rewardPosition)
+			bar.Reward:SetPoint('CENTER', bar, DB.db.reputation.rewardPosition)
+		end
 	end
 
 	if not label then
