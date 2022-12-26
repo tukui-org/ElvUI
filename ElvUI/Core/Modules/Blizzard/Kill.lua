b9f890aef9a77153805509c55607454104397134
Simpy <kodi03466@hotmail.com>
2022-12-25 19:01:39 -0500

    kill more tutorials - this probably fixes that "cant click cause invisible frame" issue

--- ElvUI/Core/Modules/Blizzard/Kill.lua
+++ ElvUI/Core/Modules/Blizzard/Kill.lua
@@ -4,11 +4,9 @@ local B = E:GetModule('Blizzard')
 local _G = _G
 local hooksecurefunc = hooksecurefunc
 
-function B:KillBlizzard()
-	if not E.Retail then
-		_G.Advanced_UIScaleSlider:Kill()
-		_G.Advanced_UseUIScale:Kill()
-	end
+function B:KillBlizzard() -- current not E.Retail
+	_G.Advanced_UIScaleSlider:Kill()
+	_G.Advanced_UseUIScale:Kill()
 end
 
 local function AcknowledgeTips()
@@ -24,24 +22,43 @@ function B:DisableHelpTip() -- auto complete helptips
 	E:Delay(1, AcknowledgeTips)
 end
 
--- NOTE: ActionBars heavily conflicts with NPE
-local function ShutdownNPE(event)
+-- ActionBars heavily conflicts with NPE
+local function ShutdownNPE()
 	local NPE = _G.NewPlayerExperience
-	if NPE then
-		if NPE:GetIsActive() then
-			NPE:Shutdown()
-		end
+	if NPE and NPE:GetIsActive() then
+		NPE:Shutdown()
+	end
 
-		if event then
-			B:UnregisterEvent(event)
-		end
+	return NPE
+end
+
+-- similar to NPE but not NPE
+local function ShutdownTM()
+	local TM = _G.TutorialManager
+	if TM and TM:GetIsActive() then
+		TM:Shutdown()
+
+		-- these aren't hidden by the shutdown
+		_G.TutorialWalk_Frame:Kill()
+		_G.TutorialSingleKey_Frame:Kill()
+		_G.TutorialMainFrame_Frame:Kill()
+		_G.TutorialKeyboardMouseFrame_Frame:Kill()
+	end
+
+	return TM
+end
+
+local function ShutdownTutorials(event)
+	local TM, NPE = ShutdownTM(), ShutdownNPE()
+	if TM and NPE then -- they exist unregister this
+		B:UnregisterEvent(event)
 	end
 end
 
-function B:DisableNPE() -- disable new player experience
-	if _G.NewPlayerExperience then
-		ShutdownNPE()
-	else
-		B:RegisterEvent('ADDON_LOADED', ShutdownNPE)
+-- disable new player experience stuff
+function B:DisableTutorials()
+	local TM, NPE = ShutdownTM(), ShutdownNPE()
+	if not TM or not NPE then -- wait for them to exist
+		B:RegisterEvent('ADDON_LOADED', ShutdownTutorials)
 	end
 end
