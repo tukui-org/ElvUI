cecc7cb94ab1c695273bb158c46fe689bc0d82d2
Simpy <kodi03466@hotmail.com>
2023-01-02 03:20:04 -0500

    shutdown more tutorial stuff

--- ElvUI/Core/Modules/Blizzard/TutorialFrames.lua
+++ ElvUI/Core/Modules/Blizzard/TutorialFrames.lua
@@ -2,6 +2,7 @@ local E, L, V, P, G = unpack(ElvUI)
 local B = E:GetModule('Blizzard')
 
 local _G = _G
+local next = next
 local hooksecurefunc = hooksecurefunc
 
 function B:KillBlizzard() -- current not E.Retail
@@ -22,7 +23,7 @@ function B:DisableHelpTip() -- auto complete helptips
 	E:Delay(1, AcknowledgeTips)
 end
 
--- ActionBars heavily conflicts with NPE
+-- Blizzard_NewPlayerExperience: ActionBars heavily conflicts with this
 local function ShutdownNPE()
 	local NPE = _G.NewPlayerExperience
 	if NPE and NPE:GetIsActive() then
@@ -32,33 +33,89 @@ local function ShutdownNPE()
 	return NPE
 end
 
--- similar to NPE but not NPE
+-- Blizzard_TutorialManager: similar to NPE
+local Tutorial_Frames = {
+	'TutorialWalk_Frame',
+	'TutorialSingleKey_Frame',
+	'TutorialMainFrame_Frame',
+	'TutorialKeyboardMouseFrame_Frame'
+}
+
 local function ShutdownTM()
 	local TM = _G.TutorialManager
 	if TM and TM:GetIsActive() then
 		TM:Shutdown()
 
 		-- these aren't hidden by the shutdown
-		_G.TutorialWalk_Frame:Kill()
-		_G.TutorialSingleKey_Frame:Kill()
-		_G.TutorialMainFrame_Frame:Kill()
-		_G.TutorialKeyboardMouseFrame_Frame:Kill()
+		for _, name in next, Tutorial_Frames do
+			_G[name]:Kill()
+		end
 	end
 
 	return TM
 end
 
+-- Blizzard_Tutorials: implemented kinda weird
+local Blizzard_Tutorials = {
+	-- Blizzard_Tutorials_Professions.lua
+	'Class_ProfessionInventoryWatcher',
+	'Class_ProfessionGearCheckingService',
+	'Class_EquipProfessionGear',
+	'Class_FirstProfessionWatcher',
+	'Class_FirstProfessionTutorial',
+
+	-- Blizzard_Tutorials_Classes.lua
+	'Class_StarterTalentWatcher'
+}
+
+local function ShutdownGT()
+	local GT = _G.GameTutorials
+	if GT then
+		-- shut some down, they are running but not used
+		for _, name in next, Blizzard_Tutorials do
+			_G[name]:OnComplete()
+		end
+
+		-- these are extra annoying because while they have OnComplete
+		-- they also call things that will break because TM is shutdown
+		local CS = _G.Class_ChangeSpec
+		if CS then -- Class_ChangeSpec:OnComplete()
+			_G.Dispatcher:UnregisterEvent('PLAYER_SPECIALIZATION_CHANGED', CS)
+			_G.Dispatcher:UnregisterEvent('PLAYER_TALENT_UPDATE', CS)
+			_G.Dispatcher:UnregisterEvent('PLAYER_LEVEL_CHANGED', CS)
+			_G.EventRegistry:UnregisterCallback('TalentFrame.OpenFrame', CS)
+			_G.EventRegistry:UnregisterCallback('TalentFrame.CloseFrame', CS)
+			_G.EventRegistry:UnregisterCallback('TalentFrame.SpecTab.ActivateSpec', CS)
+
+			-- CS:CleanUpCallbacks() -- for now this is empty so no reason to do it?
+		end
+
+		local TP = _G.Class_TalentPoints
+		if TP then -- Class_TalentPoints:OnComplete()
+			_G.Dispatcher:UnregisterEvent('PLAYER_TALENT_UPDATE', TP)
+			_G.Dispatcher:UnregisterEvent('PLAYER_LEVEL_CHANGED', TP)
+			_G.Dispatcher:UnregisterEvent('ACTIVE_COMBAT_CONFIG_CHANGED', TP)
+			_G.EventRegistry:UnregisterCallback('TalentFrame.SpecTab.Show', TP)
+			_G.EventRegistry:UnregisterCallback('TalentFrame.TalentTab.Show', TP)
+			_G.EventRegistry:UnregisterCallback('TalentFrame.OpenFrame', TP)
+			_G.EventRegistry:UnregisterCallback('TalentFrame.CloseFrame', TP)
+		end
+	end
+
+	return GT
+end
+
 local function ShutdownTutorials(event)
-	local TM, NPE = ShutdownTM(), ShutdownNPE()
-	if TM and NPE then -- they exist unregister this
+	local TM, NPE, GT = ShutdownTM(), ShutdownNPE(), ShutdownGT()
+	if TM and NPE and GT then -- they exist unregister this
 		B:UnregisterEvent(event)
 	end
 end
 
 -- disable new player experience stuff
 function B:DisableTutorials()
-	local TM, NPE = ShutdownTM(), ShutdownNPE()
-	if not TM or not NPE then -- wait for them to exist
+	local TM, NPE, GT = ShutdownTM(), ShutdownNPE(), ShutdownGT()
+	if not TM or not NPE or not GT then -- wait for them to exist
 		B:RegisterEvent('ADDON_LOADED', ShutdownTutorials)
 	end
 end
