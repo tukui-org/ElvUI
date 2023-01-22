7721c4894d4290e0dda84b64e5632c949ff18ae0
Azilroka <darktemplar9900@yahoo.com>
2023-01-21 19:14:52 -0600

    DataTexts: Micro Bar

---
+++ ElvUI/Core/Modules/DataTexts/MicroBar.lua
@@ -0,0 +1,27 @@
+local E, L, V, P, G = unpack(ElvUI)
+local DT = E:GetModule('DataTexts')
+local M = E:GetModule('Minimap')
+
+local strjoin = strjoin
+local displayString
+
+local function OnEvent(self)
+	self.text:SetFormattedText(displayString, L["Micro Bar"])
+end
+
+local function OnClick(self, button)
+	if button == 'LeftButton' then
+		ToggleFrame(GameMenuFrame)
+	else
+		E:SetEasyMenuAnchor(E.EasyMenu, self)
+		_G.EasyMenu(M.RightClickMenuList, E.EasyMenu, nil, nil, nil, 'MENU')
+	end
+end
+
+local function ValueColorUpdate(self, hex)
+	displayString = strjoin('', hex, '%s|r')
+
+	OnEvent(self)
+end
+
+DT:RegisterDatatext("Micro Bar", nil, nil, OnEvent, nil, OnClick, nil, nil, L["Micro Bar"], nil, ValueColorUpdate)
