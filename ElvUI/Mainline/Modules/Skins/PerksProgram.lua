fcc470949308c82f5907433e2b5f2569397c7bc3
Simpy <kodi03466@hotmail.com>
2023-01-24 16:00:38 -0500

    clean up

--- ElvUI/Mainline/Modules/Skins/PerksProgram.lua
+++ ElvUI/Mainline/Modules/Skins/PerksProgram.lua
@@ -8,26 +8,27 @@ function S:Blizzard_PerksProgram()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.perks) then return end
 
 	local frame = _G.PerksProgramFrame
+	local products = frame.ProductsFrame
+	S:HandleButton(products.PerksProgramFilter.FilterDropDownButton)
+	products.PerksProgramCurrencyFrame.Text:FontTemplate(nil, 30)
+	S:HandleIcon(products.PerksProgramCurrencyFrame.Icon)
+	products.PerksProgramCurrencyFrame.Icon:Size(30)
 
-	S:HandleButton(frame.ProductsFrame.PerksProgramFilter.FilterDropDownButton)
-	frame.ProductsFrame.PerksProgramCurrencyFrame.Text:FontTemplate(nil, 30)
-	S:HandleIcon(frame.ProductsFrame.PerksProgramCurrencyFrame.Icon)
-	frame.ProductsFrame.PerksProgramCurrencyFrame.Icon:Size(30)
+	products.ProductsScrollBoxContainer.Border:Hide()
+	products.ProductsScrollBoxContainer:SetTemplate('Transparent')
+	products.ProductsScrollBoxContainer.PerksProgramHoldFrame:StripTextures()
+	S:HandleTrimScrollBar(products.ProductsScrollBoxContainer.ScrollBar, true)
 
-	frame.ProductsFrame.ProductsScrollBoxContainer.Border:Hide()
-	frame.ProductsFrame.ProductsScrollBoxContainer:SetTemplate('Transparent')
-	frame.ProductsFrame.ProductsScrollBoxContainer.PerksProgramHoldFrame:StripTextures()
-	S:HandleTrimScrollBar(frame.ProductsFrame.ProductsScrollBoxContainer.ScrollBar, true)
+	products.PerksProgramProductDetailsContainerFrame.Border:Hide()
+	products.PerksProgramProductDetailsContainerFrame:SetTemplate('Transparent')
 
-	frame.ProductsFrame.PerksProgramProductDetailsContainerFrame.Border:Hide()
-	frame.ProductsFrame.PerksProgramProductDetailsContainerFrame:SetTemplate('Transparent')
+	local footer = frame.FooterFrame
+	S:HandleButton(footer.LeaveButton, nil, nil, nil, true, nil, nil, nil, true)
+	S:HandleButton(footer.PurchaseButton, nil, nil, nil, true, nil, nil, nil, true)
 
-	S:HandleButton(frame.FooterFrame.LeaveButton, nil, nil, nil, true, nil, nil, nil, true)
-	S:HandleButton(frame.FooterFrame.PurchaseButton, nil, nil, nil, true, nil, nil, nil, true)
-
-	S:HandleCheckBox(frame.FooterFrame.TogglePlayerPreview)
-	S:HandleButton(frame.FooterFrame.RotateButtonContainer.RotateLeftButton, nil, nil, nil, true, nil, nil, nil, true)
-	S:HandleButton(frame.FooterFrame.RotateButtonContainer.RotateRightButton, nil, nil, nil, true, nil, nil, nil, true)
+	S:HandleCheckBox(footer.TogglePlayerPreview)
+	S:HandleButton(footer.RotateButtonContainer.RotateLeftButton, nil, nil, nil, true, nil, nil, nil, true)
+	S:HandleButton(footer.RotateButtonContainer.RotateRightButton, nil, nil, nil, true, nil, nil, nil, true)
 end
 
 S:AddCallbackForAddon('Blizzard_PerksProgram')
