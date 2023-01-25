44a9d835af8c9749eecde10c7fbd70ea9f3bcfa5
Brandon Jernigan <brandon@whatisboom.com>
2023-01-24 18:07:29 -0600

    feat: add rating tooltip lines to crit and mastery

--- ElvUI/Core/Modules/DataTexts/Crit.lua
+++ ElvUI/Core/Modules/DataTexts/Crit.lua
@@ -1,16 +1,30 @@
 local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
 
+local _G = _G
+local format = format
 local strjoin = strjoin
-
 local GetCritChance = GetCritChance
 local GetRangedCritChance = GetRangedCritChance
+local GetCombatRating = GetCombatRating
+local GetCombatRatingBonus = GetCombatRatingBonus
+local BreakUpLargeNumbers = BreakUpLargeNumbers
 local STAT_CATEGORY_ENHANCEMENTS = STAT_CATEGORY_ENHANCEMENTS
+local STAT_CRITICAL_STRIKE = STAT_CRITICAL_STRIKE
 local CRIT_ABBR = CRIT_ABBR
 
 local displayString = ''
 local spellCrit, rangedCrit, meleeCrit = 0, 0, 0
 local critChance = 0
+
+local function OnEnter()
+	DT.tooltip:ClearLines()
+	local text = HIGHLIGHT_FONT_COLOR_CODE..format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_CRITICAL_STRIKE)..' '..format('%.2F%%', critChance)..FONT_COLOR_CODE_CLOSE
+	local classTooltip = _G['STAT_CRITICAL_STRIKE_'..E.myclass..'_TOOLTIP']
+	if not classTooltip then classTooltip = STAT_CRITICAL_STRIKE_TOOLTIP end
+	DT.tooltip:AddLine(format('%s: %s [+%.2f%%]', STAT_CRITICAL_STRIKE, BreakUpLargeNumbers(GetCombatRating(CR_HASTE_MELEE)), GetCombatRatingBonus(CR_HASTE_MELEE)))
+	DT.tooltip:Show()
+end
 
 local function OnEvent(self)
 	rangedCrit = GetRangedCritChance()
@@ -37,4 +51,4 @@ local function ValueColorUpdate(self, hex)
 	OnEvent(self)
 end
 
-DT:RegisterDatatext('Crit', STAT_CATEGORY_ENHANCEMENTS, { 'UNIT_STATS', 'UNIT_AURA', 'PLAYER_DAMAGE_DONE_MODS'}, OnEvent, nil, nil, nil, nil, _G.STAT_CRITICAL_STRIKE, nil, ValueColorUpdate)
+DT:RegisterDatatext('Crit', STAT_CATEGORY_ENHANCEMENTS, { 'UNIT_STATS', 'UNIT_AURA', 'PLAYER_DAMAGE_DONE_MODS'}, OnEvent, nil, nil, not E.Classic and OnEnter, nil, _G.STAT_CRITICAL_STRIKE, nil, ValueColorUpdate)
