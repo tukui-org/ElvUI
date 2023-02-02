2c9f18ebcda836244534bc54c9589ea803959615
Azilroka <darktemplar9900@yahoo.com>
2023-02-01 19:00:23 -0600

    Actually not needed.. It's done from within UpdatePanelAttributes

--- ElvUI/Core/Modules/DataTexts/DataTexts.lua
+++ ElvUI/Core/Modules/DataTexts/DataTexts.lua
@@ -365,10 +365,6 @@ function DT:GetPanelSettings(name)
 	local gp = E:CopyTable(G.datatexts.customPanels[name], G.datatexts.newPanelInfo, true)
 	G.datatexts.customPanels[name] = gp
 
-	for i = 1, (E.global.datatexts.customPanels[name].numPoints or 1) do
-		if not db[i] then db[i] = '' end
-	end
-
 	-- pass the table back
 	return E:CopyTable(E.global.datatexts.customPanels[name], gp, true)
 end
