260c8acfed93fc31ff4f349eaee1fc2d1ecccbc7
Simpy <kodi03466@hotmail.com>
2023-03-23 18:20:55 -0400

    Revert "test this"
    This reverts commit cb5f608e00e6c2a038085833cfccdd2b0ba0697b.

--- ElvUI/Core/Modules/UnitFrames/UnitFrames.lua
+++ ElvUI/Core/Modules/UnitFrames/UnitFrames.lua
@@ -1410,16 +1410,24 @@ do
 		end
 	end
 
+	local function PlateShown(plate, show)
+		if show then
+			plate:Hide()
+		end
+	end
+
 	function ElvUF:DisableNamePlate(frame)
 		if not E.private.nameplates.enable then return end
-		if not frame or frame:IsForbidden() then return end
 
-		local plate = frame.UnitFrame
-		if plate and not disabledPlates[plate] then
+		local plate = frame and frame.UnitFrame
+		if not plate or plate:IsForbidden() then return end
+
+		if not disabledPlates[plate] then
 			disabledPlates[plate] = true
 
 			HandleFrame(plate, true)
-			plate:HookScript('OnShow', plate.Hide)
+			hooksecurefunc(plate, 'Show', plate.Hide)
+			hooksecurefunc(plate, 'SetShown', PlateShown)
 		end
 	end
 end
