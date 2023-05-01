47d5b23a6cb02627c2d278faf328791fd4f0c9d5
Simpy <kodi03466@hotmail.com>
2023-05-01 16:19:23 -0400

    another bar

--- ElvUI/Mainline/Modules/Skins/Gossip.lua
+++ ElvUI/Mainline/Modules/Skins/Gossip.lua
@@ -42,7 +42,7 @@ function S:GossipFrame()
 	local GossipFrame = _G.GossipFrame
 	S:HandlePortraitFrame(GossipFrame, true)
 
-	--S:HandleTrimScrollBar(_G.ItemTextScrollFrameScrollBar) -- ToDO: Season 2
+	S:HandleTrimScrollBar(_G.ItemTextScrollFrame.ScrollBar, true)
 	S:HandleTrimScrollBar(_G.GossipFrame.GreetingPanel.ScrollBar)
 	S:HandleButton(_G.GossipFrame.GreetingPanel.GoodbyeButton, true)
 
