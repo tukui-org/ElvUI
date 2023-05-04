a0b6922e88f713dfe214da9c39a232b26c060b8a
Simpy <kodi03466@hotmail.com>
2023-05-04 13:51:09 -0400

    eeee

--- ElvUI/Mainline/Modules/DataTexts/Crests.lua
+++ ElvUI/Mainline/Modules/DataTexts/Crests.lua
@@ -2,37 +2,35 @@ local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
 
 local _G = _G
+local next = next
 local format, floor = format, floor
 
 local GetItemCount = GetItemCount
 local C_CurrencyInfo_GetCurrencyInfo = C_CurrencyInfo.GetCurrencyInfo
 
+local FRAGMENTS_EARNED = gsub(_G.ITEM_UPGRADE_FRAGMENTS_TOTAL, '%s*|c.+$', '')
+
 local crests = {
-	{ --Flightstones
-		id = 2245,
+	[2245] = { -- Flightstones
 		fragment = false,
 		color = _G.HEIRLOOM_BLUE_COLOR
 	},
-	{ --Whelpling's Shadowflame Crest
-		id = 204193,
+	[204193] = { -- Whelpling's Shadowflame Crest
 		fragment = 204075,
 		fragmentCap = 2409,
 		color = _G.UNCOMMON_GREEN_COLOR
 	},
-	{ --Drake's Shadowflame Crest
-		id = 204195,
+	[204195] = { -- Drake's Shadowflame Crest
 		fragment = 204076,
 		fragmentCap = 2410,
 		color = _G.RARE_BLUE_COLOR
 	},
-	{ --Wyrm's Shadowflame Crest
-		id = 204196,
+	[204196] = { -- Wyrm's Shadowflame Crest
 		fragment = 204077,
 		fragmentCap = 2411,
 		color = _G.EPIC_PURPLE_COLOR
 	},
-	{ --Aspect's Shadowflame Crest
-		id = 204194,
+	[204194] = { -- Aspect's Shadowflame Crest
 		fragment = 204078,
 		fragmentCap = 2412,
 		color = _G.LEGENDARY_ORANGE_COLOR
@@ -40,40 +38,48 @@ local crests = {
 }
 
 local currency = {}
+local fragmentText, fragmentSplit = '%s | %s', '%s / 15'
+local crestText = '|T%s:16:16:0:0:64:64:4:60:4:60|t %s / %s'
+
+local function GetFragmentText(text, crest, count, fragments)
+	return format(fragmentText, text, crest.color:WrapTextInColorCode((fragments and fragments > 0) and format('%s+%s', count, fragments) or count))
+end
+
+local function GetCrestText(crest, currency)
+	return crest.color:WrapTextInColorCode(format(crestText, currency.iconFileID, currency.quantity, currency.maxQuantity))
+end
 
 local function OnEvent(self, _)
-	local payload = ''
-	for _, crest in pairs(crests) do
+	local text = ''
+	for id, crest in next, crests do
 		if crest.fragment then
-			local count = GetItemCount(crest.id)
-			local fragmentCount = floor(GetItemCount(crest.fragment)/15)
-			if fragmentCount > 0 then
-				payload = format('%s | %s', payload, crest.color:WrapTextInColorCode(format('%s+%s', count, fragmentCount)))
-			else
-				payload = format('%s | %s', payload, crest.color:WrapTextInColorCode(count))
-			end
+			text = GetFragmentText(text, crest, GetItemCount(id) or 0, floor((GetItemCount(crest.fragment) or 0) / 15))
 		else
-			 currency = C_CurrencyInfo_GetCurrencyInfo(crest.id)
-			 payload = crest.color:WrapTextInColorCode(currency.quantity)
+			currency = C_CurrencyInfo_GetCurrencyInfo(id)
+			text = crest.color:WrapTextInColorCode(currency.quantity)
 		end
 	end
-	self.text:SetFormattedText(payload)
+
+	self.text:SetFormattedText(text)
 end
 
 local function OnEnter(self)
 	DT.tooltip:ClearLines()
-	DT.tooltip:AddLine(strsplit('|c', _G.ITEM_UPGRADE_FRAGMENTS_TOTAL))
-	for _, crest in pairs(crests) do
-		if crest.fragment then
-			currency = C_CurrencyInfo_GetCurrencyInfo(crest.fragmentCap)
-			if currency.maxQuantity > 0 then
-				DT.tooltip:AddDoubleLine(crest.color:WrapTextInColorCode(format('|T%s:16:16:0:0:64:64:4:60:4:60|t %s / %s', currency.iconFileID, currency.quantity, currency.maxQuantity)), format('%s/15', GetItemCount(crest.fragment)))
+	DT.tooltip:AddLine(FRAGMENTS_EARNED)
+
+	for id, crest in next, crests do
+		local currency = C_CurrencyInfo_GetCurrencyInfo(crest.fragment and crest.fragmentCap or id)
+		if currency then
+			if crest.fragment then
+				if currency.maxQuantity > 0 then
+					DT.tooltip:AddDoubleLine(GetCrestText(crest, currency), format(fragmentSplit, GetItemCount(crest.fragment) or 0))
+				end
+			else
+				DT.tooltip:AddLine(GetCrestText(crest, currency))
 			end
-		else
-			currency = C_CurrencyInfo_GetCurrencyInfo(crest.id)
-			DT.tooltip:AddLine(crest.color:WrapTextInColorCode(format('|T%s:16:16:0:0:64:64:4:60:4:60|t %s / %s', currency.iconFileID, currency.quantity, currency.maxQuantity)))
 		end
 	end
+
 	DT.tooltip:Show()
 end
 
