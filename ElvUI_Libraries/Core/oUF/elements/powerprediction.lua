e259452503584aa2ddbd1d00b9d01abc7f798e11
Simpy <kodi03466@hotmail.com>
2023-06-20 14:39:26 -0400

    compatibility

--- ElvUI_Libraries/Core/oUF/elements/powerprediction.lua
+++ ElvUI_Libraries/Core/oUF/elements/powerprediction.lua
@@ -68,8 +68,8 @@ local function Update(self, event, unit)
 	local mainType = UnitPowerType(unit)
 	local mainMax = UnitPowerMax(unit, mainType)
 	local isPlayer = UnitIsUnit('player', unit)
-	-- TODO: 10.1.5
-	-- local altManaInfo = isPlayer and oUF.isRetail and ALT_MANA_BAR_PAIR_DISPLAY_INFO[playerClass]
+	local DISPLAY_INFO = isPlayer and oUF.isRetail and (ALT_POWER_BAR_PAIR_DISPLAY_INFO or ALT_MANA_BAR_PAIR_DISPLAY_INFO)
+	local altManaInfo = DISPLAY_INFO and DISPLAY_INFO[playerClass]
 	local hasAltManaBar = altManaInfo and altManaInfo[mainType]
 	local _, _, _, startTime, endTime, _, _, _, spellID = UnitCastingInfo(unit)
 
