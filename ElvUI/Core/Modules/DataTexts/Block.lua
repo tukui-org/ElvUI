c698a3025ab6757b6c413150e1b04f1c7dcbc8a6
Simpy <kodi03466@hotmail.com>
2023-07-01 17:06:47 -0400

    ss

--- ElvUI/Core/Modules/DataTexts/Block.lua
+++ ElvUI/Core/Modules/DataTexts/Block.lua
@@ -19,4 +19,3 @@ local function ApplySettings(_, hex)
 end
 
 DT:RegisterDatatext('Block', STAT_CATEGORY_DEFENSE, { 'UNIT_STATS', 'UNIT_AURA', 'SKILL_LINES_CHANGED' }, OnEvent, nil, nil, nil, nil, BLOCK, nil, ApplySettings)
-
