72ae0fb9a12eb5ad93a5f16eb588661f5f5686bf
Luckyone <Luckyone.Development@pm.me>
2023-07-24 21:52:43 +0200

    AH overlaps

--- ElvUI/Classic/Modules/Skins/AuctionHouse.lua
+++ ElvUI/Classic/Modules/Skins/AuctionHouse.lua
@@ -162,9 +162,8 @@ function S:Blizzard_AuctionUI()
 
 	_G.BrowseCloseButton:Point('BOTTOMRIGHT', 66, 6)
 	_G.BrowseBuyoutButton:Point('RIGHT', _G.BrowseCloseButton, 'LEFT', -4, 0)
-	_G.BrowseBidButton:Point('RIGHT', _G.BrowseBuyoutButton, 'LEFT', -4, 0)
 
-	_G.BrowseBidPrice:Point('BOTTOM', 25, 10)
+	_G.BrowseBidPrice:Point('BOTTOM', -102, 10)
 
 	S:HandleScrollBar(_G.BrowseFilterScrollFrameScrollBar)
 	S:HandleScrollBar(_G.BrowseScrollFrameScrollBar)
