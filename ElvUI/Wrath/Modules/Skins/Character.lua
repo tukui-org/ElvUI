37f82180fea28787cba294551046eaa1c29335d9
Luckyone <Luckyone.Development@pm.me>
2023-08-20 19:50:23 +0200

    GG

--- ElvUI/Wrath/Modules/Skins/Character.lua
+++ ElvUI/Wrath/Modules/Skins/Character.lua
@@ -559,9 +559,9 @@ function S:CharacterFrame()
 	S:HandleTab(_G.PVPParentFrameTab2)
 
 	-- Reposition Tabs
-	_G._G.PVPParentFrameTab1:ClearAllPoints()
-	_G._G.PVPParentFrameTab1:Point('TOPLEFT', _G.PVPParentFrame, 'BOTTOMLEFT', 1, 76)
-	_G._G.PVPParentFrameTab2:Point('TOPLEFT', _G._G.PVPParentFrameTab1, 'TOPRIGHT', -19, 0)
+	_G.PVPParentFrameTab1:ClearAllPoints()
+	_G.PVPParentFrameTab1:Point('TOPLEFT', _G.PVPParentFrame, 'BOTTOMLEFT', 1, 76)
+	_G.PVPParentFrameTab2:Point('TOPLEFT', _G.PVPParentFrameTab1, 'TOPRIGHT', -19, 0)
 
 	S:HandleButton(_G.PVPTeamDetailsAddTeamMember)
 	S:HandleNextPrevButton(_G.PVPTeamDetailsToggleButton)
