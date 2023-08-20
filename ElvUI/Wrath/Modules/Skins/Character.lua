f855d26c733ca90efd43fe4d061e5fe99d90601e
Luckyone <Luckyone.Development@pm.me>
2023-08-21 00:48:46 +0200

    Fix char tabs wrath

--- ElvUI/Wrath/Modules/Skins/Character.lua
+++ ElvUI/Wrath/Modules/Skins/Character.lua
@@ -181,14 +181,8 @@ function S:CharacterFrame()
 	-- Reposition Tabs
 	_G.CharacterFrameTab1:ClearAllPoints()
 	_G.CharacterFrameTab1:Point('TOPLEFT', _G.CharacterFrame, 'BOTTOMLEFT', 1, 76)
-
-	if E.myclass == 'HUNTER' or E.myclass == 'WARLOCK' then
-		_G.CharacterFrameTab2:Point('TOPLEFT', _G.CharacterFrameTab1, 'TOPRIGHT', -19, 0)
-		_G.CharacterFrameTab3:Point('TOPLEFT', _G.CharacterFrameTab2, 'TOPRIGHT', -19, 0)
-	else
-		_G.CharacterFrameTab3:Point('TOPLEFT', _G.CharacterFrameTab1, 'TOPRIGHT', -19, 0)
-	end
-
+	_G.CharacterFrameTab2:Point('TOPLEFT', _G.CharacterFrameTab1, 'TOPRIGHT', -19, 0)
+	_G.CharacterFrameTab3:Point('TOPLEFT', _G.CharacterFrameTab2, 'TOPRIGHT', -19, 0)
 	_G.CharacterFrameTab4:Point('TOPLEFT', _G.CharacterFrameTab3, 'TOPRIGHT', -19, 0)
 	_G.CharacterFrameTab5:Point('TOPLEFT', _G.CharacterFrameTab4, 'TOPRIGHT', -19, 0)
 
