1240d6c544eaa88aac34c07e6f6984eb66d2b993
Simpy <kodi03466@hotmail.com>
2023-08-21 13:06:06 -0400

    dead cache

--- ElvUI/Classic/Modules/Skins/TradeSkill.lua
+++ ElvUI/Classic/Modules/Skins/TradeSkill.lua
@@ -2,7 +2,6 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
-local strfind = strfind
 local unpack, select = unpack, select
 
 local GetItemInfo = GetItemInfo
