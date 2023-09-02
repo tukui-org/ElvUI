95e232e44fd5dd90f5a4571e2d4906c4dc9ed3ea
Simpy <kodi03466@hotmail.com>
2023-09-02 13:37:22 -0400

    updates for barber shop skin

--- ElvUI/Mainline/Modules/Skins/Barber.lua
+++ ElvUI/Mainline/Modules/Skins/Barber.lua
@@ -2,24 +2,88 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
+local max = max
+local unpack = unpack
 local hooksecurefunc = hooksecurefunc
+
+local function HandleButton(button)
+	S:HandleButton(button, nil, nil, nil, true, nil, nil, nil, true)
+	button:SetScale(E.uiscale)
+	button:Size(200, 50)
+end
 
 function S:Blizzard_BarbershopUI()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.barber) then return end
 
 	local frame = _G.BarberShopFrame
-	S:HandleButton(frame.ResetButton, nil, nil, nil, true, nil, nil, nil, true)
-	S:HandleButton(frame.CancelButton, nil, nil, nil, true, nil, nil, nil, true)
-	S:HandleButton(frame.AcceptButton, nil, nil, nil, true, nil, nil, nil, true)
+	HandleButton(frame.ResetButton)
+	HandleButton(frame.CancelButton)
+	HandleButton(frame.AcceptButton)
 end
 S:AddCallbackForAddon('Blizzard_BarbershopUI')
 
-local function HandleButton(button)
+local function HandleNextPrev(button)
 	S:HandleNextPrevButton(button)
 
 	-- remove these to fix error on SetHighlightAtlas from AlphaHighlightButtonMixin
 	button:SetScript('OnMouseUp', nil)
 	button:SetScript('OnMouseDown', nil)
+end
+
+local function SetSelectedCategory(list)
+	if list.selectionPopoutPool then
+		for frame in list.selectionPopoutPool:EnumerateActive() do
+			if not frame.IsSkinned then
+				if frame.DecrementButton then
+					HandleNextPrev(frame.DecrementButton)
+					HandleNextPrev(frame.IncrementButton)
+				end
+
+				if frame.Label then
+					frame.Label:FontTemplate()
+				end
+
+				local button = frame.Button
+				if button then
+					if button.HighlightTexture then
+						button.HighlightTexture:SetAlpha(0)
+					end
+
+					if button.NormalTexture then
+						button.NormalTexture:SetAlpha(0)
+					end
+
+					local popout = button.Popout
+					if popout then
+						local r, g, b, a = unpack(E.media.backdropfadecolor)
+						popout:StripTextures()
+						popout:SetTemplate('Transparent')
+						popout:SetBackdropColor(r, g, b, max(a, 0.8))
+					end
+
+					S:HandleButton(button, nil, nil, nil, true)
+					button.backdrop:SetInside(nil, 4, 4)
+				end
+
+				frame.IsSkinned = true
+			end
+		end
+	end
+
+	local pool = list.pools and list.pools:GetPool('CharCustomizeOptionCheckButtonTemplate')
+	if pool then
+		for frame in pool:EnumerateActive() do
+			if not frame.isSkinned then
+				if frame.Button then
+					S:HandleCheckBox(frame.Button)
+				end
+
+				if frame.Label then
+					frame.Label:FontTemplate()
+				end
+			end
+		end
+	end
 end
 
 function S:Blizzard_CharacterCustomize()
@@ -33,38 +97,7 @@ function S:Blizzard_CharacterCustomize()
 	S:HandleButton(frame.SmallButtons.RotateLeftButton, nil, nil, true)
 	S:HandleButton(frame.SmallButtons.RotateRightButton, nil, nil, true)
 
-	hooksecurefunc(frame, 'SetSelectedCategory', function(list)
-		if list.selectionPopoutPool then
-			for popout in list.selectionPopoutPool:EnumerateActive() do
-				if not popout.IsSkinned then
-					HandleButton(popout.DecrementButton)
-					HandleButton(popout.IncrementButton)
-
-					local button = popout.Button
-					button.HighlightTexture:SetAlpha(0)
-					button.NormalTexture:SetAlpha(0)
-
-					button.Popout:StripTextures()
-					button.Popout:SetTemplate('Transparent')
-					button.Popout:SetFrameStrata('TOOLTIP') -- to prevent text overlapping below the popout
-
-					S:HandleButton(button, nil, nil, nil, true)
-					button.backdrop:SetInside(nil, 4, 4)
-
-					popout.IsSkinned = true
-				end
-			end
-		end
-
-		local optionPool = list.pools and list.pools:GetPool('CharCustomizeOptionCheckButtonTemplate')
-		if optionPool then
-			for button in optionPool:EnumerateActive() do
-				if not button.isSkinned then
-					S:HandleCheckBox(button.Button)
-				end
-			end
-		end
-	end)
+	hooksecurefunc(frame, 'SetSelectedCategory', SetSelectedCategory)
 end
 
 S:AddCallbackForAddon('Blizzard_CharacterCustomize')
