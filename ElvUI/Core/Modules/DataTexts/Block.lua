a7865b0ea620f70006e8c2ac210e76790be95d74
Simpy <kodi03466@hotmail.com>
2023-09-23 18:18:45 -0400

    add decimal length to dodge, parry, block DT

--- ElvUI/Core/Modules/DataTexts/Block.lua
+++ ElvUI/Core/Modules/DataTexts/Block.lua
@@ -7,14 +7,18 @@ local GetBlockChance = GetBlockChance
 local BLOCK = BLOCK
 local STAT_CATEGORY_DEFENSE = STAT_CATEGORY_DEFENSE
 
-local displayString = ''
+local displayString, db = ''
 
 local function OnEvent(self)
 	self.text:SetFormattedText(displayString, BLOCK, GetBlockChance())
 end
 
-local function ApplySettings(_, hex)
-	displayString = strjoin('', '%s: ', hex, '%.f|r')
+local function ApplySettings(self, hex)
+	if not db then
+		db = E.global.datatexts.settings[self.name]
+	end
+
+	displayString = strjoin('', '%s: ', hex, '%.'..db.decimalLength..'f%%|r')
 end
 
 DT:RegisterDatatext('Block', STAT_CATEGORY_DEFENSE, { 'UNIT_STATS', 'UNIT_AURA', 'SKILL_LINES_CHANGED' }, OnEvent, nil, nil, nil, nil, BLOCK, nil, ApplySettings)
--- ElvUI/Core/Modules/DataTexts/Dodge.lua
+++ ElvUI/Core/Modules/DataTexts/Dodge.lua
@@ -7,14 +7,18 @@ local GetDodgeChance = GetDodgeChance
 local DODGE = DODGE
 local STAT_CATEGORY_DEFENSE = STAT_CATEGORY_DEFENSE
 
-local displayString = ''
+local displayString, db = ''
 
 local function OnEvent(self)
 	self.text:SetFormattedText(displayString, DODGE, GetDodgeChance())
 end
 
-local function ApplySettings(_, hex)
-	displayString = strjoin('', '%s: ', hex, '%.f|r')
+local function ApplySettings(self, hex)
+	if not db then
+		db = E.global.datatexts.settings[self.name]
+	end
+
+	displayString = strjoin('', '%s: ', hex, '%.'..db.decimalLength..'f%%|r')
 end
 
 DT:RegisterDatatext('Dodge', STAT_CATEGORY_DEFENSE, { 'UNIT_STATS', 'UNIT_AURA', 'SKILL_LINES_CHANGED' }, OnEvent, nil, nil, nil, nil, DODGE, nil, ApplySettings)
