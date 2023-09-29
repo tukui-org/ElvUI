e40f96ce7e1043d6f1b4cebd228a28ca8ef11520
Simpy <kodi03466@hotmail.com>
2023-09-29 19:50:29 -0400

    blah blah blah

--- ElvUI/Mainline/Modules/Blizzard/ObjectiveFrame.lua
+++ ElvUI/Mainline/Modules/Blizzard/ObjectiveFrame.lua
@@ -84,71 +84,7 @@ function B:ObjectiveTracker_AutoHideOnShow()
 	end
 end
 
-function B:ObjectiveTracker_HandleDefaultPosition(isDefault)
-	local info = Tracker.systemInfo
-	if not info then return end
-
-	-- we only need to do something when it was in the default position
-	local wasDefault = info.isInDefaultPosition
-	info.isInDefaultPosition = isDefault
-
-	-- something in here is magic and lets the Legion Scenario quest button work when actionbar module is on
-	-- this lets the first move in edit mode actually work without clicking [Reset To Default Position]
-	local parent = wasDefault and Tracker:GetManagedFrameContainer()
-	if parent and parent.showingFrames and parent.showingFrames[Tracker] then
-		parent:RemoveManagedFrame(Tracker)
-
-		-- now set this to UIParent when its not already the parent
-		if Tracker:GetParent() ~= UIParent then
-			Tracker:SetParent(UIParent)
-		end
-	end
-end
-
-function B:ObjectiveTracker_CorrectDefaultPositions(manager, default)
-	local info = Tracker.systemInfo
-	local systemID = info and info.system
-	if not systemID then return end
-
-	local managerInfo = manager.layoutInfo
-	local layouts = managerInfo and managerInfo.layouts
-	if not layouts then return end
-
-	for _, layout in next, layouts do
-		local systems = layout.systems
-		if systems then
-			for _, system in next, systems do
-				if system.system == systemID and system.isInDefaultPosition == nil then
-					system.isInDefaultPosition = default
-				end
-			end
-		end
-	end
-end
-
-function B:ObjectiveTracker_SetDefaultPosition()
-	local default = not not Tracker:IsInDefaultPosition()
-	B:ObjectiveTracker_HandleDefaultPosition(default)
-
-	-- this fixes an error with Make New Layout
-	B:ObjectiveTracker_CorrectDefaultPositions(self, default)
-end
-
-function B:ObjectiveTracker_ClearDefaultPosition()
-	B:ObjectiveTracker_HandleDefaultPosition()
-end
-
 function B:ObjectiveTracker_Setup()
-	if E.private.actionbar.enable then -- force this never case, to fix a taint when actionbars in use
-		hooksecurefunc(Tracker, 'UpdateSystem', B.ObjectiveTracker_ClearDefaultPosition)
-		B.ObjectiveTracker_ClearDefaultPosition() -- fake it to not default on loading in
-
-		-- this fixes an error while saving a new layout when `isInDefaultPosition` is sent to the C side
-		-- it happens because this var is not nilable but we need it to be nil in order to bypass `UIParent_ManageFramePositions`
-		hooksecurefunc(_G.EditModeManagerFrame, 'PrepareSystemsForSave', B.ObjectiveTracker_SetDefaultPosition)
-		hooksecurefunc(_G.EditModeManagerFrame, 'SaveLayouts', B.ObjectiveTracker_ClearDefaultPosition)
-	end
-
 	hooksecurefunc(_G.BonusObjectiveRewardsFrameMixin, 'AnimateReward', BonusRewards_SetPosition)
 
 	B:ObjectiveTracker_AutoHide()
