0fabf9ac49b0d20b219dad228b01334250b71208
Merathilis <neo0608@gmail.com>
2023-10-15 12:18:42 +0200

    Fix GossipFrame Text Color with Parchment On

--- ElvUI/Mainline/Modules/Skins/Gossip.lua
+++ ElvUI/Mainline/Modules/Skins/Gossip.lua
@@ -8,31 +8,9 @@ local select = select
 local strmatch = strmatch
 local hooksecurefunc = hooksecurefunc
 
-local function ReplaceGossipFormat(button, textFormat, text)
-	local newFormat, count = gsub(textFormat, '000000', 'ffffff')
-	if count > 0 then
-		button:SetFormattedText(newFormat, text)
-	end
-end
-
-local ReplacedGossipColor = {
-	['000000'] = 'ffffff',
-	['414141'] = '7b8489',
-}
-
-local function ReplaceGossipText(button, text)
-	if text and text ~= '' then
-		local newText, count = gsub(text, ':32:32:0:0', ':32:32:0:0:64:64:5:59:5:59')
-		if count > 0 then
-			text = newText
-			button:SetFormattedText('%s', text)
-		end
-
-		local colorStr, rawText = strmatch(text, '|c[fF][fF](%x%x%x%x%x%x)(.-)|r')
-		colorStr = ReplacedGossipColor[colorStr]
-		if colorStr and rawText then
-			button:SetFormattedText('|cff%s%s|r', colorStr, rawText)
-		end
+local function ReplaceTextColor(text, r)
+	if r ~= 1 then
+		text:SetTextColor(1, 1, 1)
 	end
 end
 
@@ -95,14 +73,14 @@ function S:GossipFrame()
 			GossipFrame.Background:Hide()
 		end
 
-		hooksecurefunc(GossipFrame.GreetingPanel.ScrollBox, 'Update', function(frame)
-			for _, button in next, { frame.ScrollTarget:GetChildren() } do
+		hooksecurefunc(GossipFrame.GreetingPanel.ScrollBox, 'Update', function(self)
+			for i = 1, self.ScrollTarget:GetNumChildren() do
+				local button = select(i, self.ScrollTarget:GetChildren())
 				if not button.IsSkinned then
-					local buttonText = select(3, button:GetRegions())
-					if buttonText and buttonText:IsObjectType('FontString') then
-						ReplaceGossipText(button, button:GetText())
-						hooksecurefunc(button, 'SetText', ReplaceGossipText)
-						hooksecurefunc(button, 'SetFormattedText', ReplaceGossipFormat)
+					local buttonText = button.GreetingText or button.GetFontString and button:GetFontString()
+					if buttonText then
+						buttonText:SetTextColor(1, 1, 1)
+						hooksecurefunc(buttonText, 'SetTextColor', ReplaceTextColor)
 					end
 
 					button.IsSkinned = true
