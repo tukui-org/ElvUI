35fb44c14f338f73c0c8eaf0df9de2ffd3c5afd6
Simpy <kodi03466@hotmail.com>
2023-10-19 08:44:38 -0400

    readd some of the gossip code back

--- ElvUI/Wrath/Modules/Skins/Gossip.lua
+++ ElvUI/Wrath/Modules/Skins/Gossip.lua
@@ -3,11 +3,40 @@ local S = E:GetModule('Skins')
 
 local _G = _G
 local next = next
+local gsub, strmatch = gsub, strmatch
 local hooksecurefunc = hooksecurefunc
 
 local function ReplaceTextColor(text, r, g, b)
 	if r ~= 1 or g ~= 1 or b ~= 1 then
 		text:SetTextColor(1, 1, 1)
+	end
+end
+
+local function ReplaceGossipFormat(button, textFormat, text)
+	local newFormat, count = gsub(textFormat, '000000', 'ffffff')
+	if count > 0 then
+		button:SetFormattedText(newFormat, text)
+	end
+end
+
+local ReplacedGossipColor = {
+	['000000'] = 'ffffff',
+	['414141'] = '7b8489',
+}
+
+local function ReplaceGossipText(button, text)
+	if text and text ~= '' then
+		local newText, count = gsub(text, ':32:32:0:0', ':32:32:0:0:64:64:5:59:5:59')
+		if count > 0 then
+			text = newText
+			button:SetFormattedText('%s', text)
+		end
+
+		local colorStr, rawText = strmatch(text, '|c[fF][fF](%x%x%x%x%x%x)(.-)|r')
+		colorStr = ReplacedGossipColor[colorStr]
+		if colorStr and rawText then
+			button:SetFormattedText('|cff%s%s|r', colorStr, rawText)
+		end
 	end
 end
 
@@ -20,10 +49,15 @@ end
 local function GreetingPanel_Update(frame)
 	for _, button in next, { frame.ScrollTarget:GetChildren() } do
 		if not button.IsSkinned then
-			local buttonText = button.GreetingText or (button.GetFontString and button:GetFontString())
-			if buttonText then
-				buttonText:SetTextColor(1, 1, 1)
-				hooksecurefunc(buttonText, 'SetTextColor', ReplaceTextColor)
+			if button.GreetingText then
+				button.GreetingText:SetTextColor(1, 1, 1)
+				hooksecurefunc(button.GreetingText, 'SetTextColor', ReplaceTextColor)
+			end
+
+			if button.GetFontString and button:GetFontString() then
+				ReplaceGossipText(button, button:GetText())
+				hooksecurefunc(button, 'SetText', ReplaceGossipText)
+				hooksecurefunc(button, 'SetFormattedText', ReplaceGossipFormat)
 			end
 
 			button.IsSkinned = true
