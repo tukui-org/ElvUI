628805f5fc69e2b59f135e511a34dabd0f4ef178
Elv <90285628+Elv-Tukui@users.noreply.github.com>
2023-11-08 21:57:37 -0500

    hi :)

--- ElvUI/Core/init.lua
+++ ElvUI/Core/init.lua
@@ -1,4 +1,4 @@
---[[
+--[[ 
 	~AddOn Engine~
 	To load the AddOn engine inside another addon add this to the top of your file:
 		local E, L, V, P, G = unpack(ElvUI) --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
