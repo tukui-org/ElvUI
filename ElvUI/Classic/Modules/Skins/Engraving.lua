38ce5aed62027f129ccb5e3121c927e3462ef37b
Luckyone <Luckyone.Development@pm.me>
2023-12-01 09:07:55 +0100

    Engraving Headers

--- ElvUI/Classic/Modules/Skins/Engraving.lua
+++ ElvUI/Classic/Modules/Skins/Engraving.lua
@@ -5,6 +5,8 @@ if not C_Engraving.IsEngravingEnabled then
 	return
 end
 
+local C_Engraving_GetRuneCategories = C_Engraving.GetRuneCategories
+
 function S:SkinEngravings()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.engraving) then return end
 
@@ -17,6 +19,18 @@ function S:SkinEngravings()
 	S:HandleEditBox(EngravingFrameSearchBox)
 	S:HandleDropDownBox(EngravingFrameFilterDropDown, 210)
 	S:HandleScrollBar(EngravingFrameScrollFrameScrollBar)
+
+	hooksecurefunc('EngravingFrame_UpdateRuneList', function()
+		local categories = C_Engraving_GetRuneCategories(true, true)
+		numHeaders = #categories
+		for i = 1, numHeaders do
+			if not _G['EngravingFrameHeader'..i].isSkinned then
+				_G['EngravingFrameHeader'..i]:StripTextures()
+				_G['EngravingFrameHeader'..i]:SetTemplate('Transparent')
+				_G['EngravingFrameHeader'..i].isSkinned = true
+			end
+		end
+	end)
 end
 
 S:AddCallbackForAddon('Blizzard_EngravingUI', 'SkinEngravings')
