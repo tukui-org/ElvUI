eccc08f05574753f99f4e47288630068b99aeeaf
Simpy <kodi03466@hotmail.com>
2024-01-04 16:53:55 -0500

    buttons not for retail

--- ElvUI/Core/Modules/Maps/Minimap.lua
+++ ElvUI/Core/Modules/Maps/Minimap.lua
@@ -47,7 +47,7 @@ local menuList = {
 	{ text = _G.SPELLBOOK_ABILITIES_BUTTON, microOffset = 'SpellbookMicroButton', func = function() ToggleFrame(_G.SpellBookFrame) end },
 	{ text = _G.TIMEMANAGER_TITLE, func = function() ToggleFrame(_G.TimeManagerFrame) end },
 	{ text = _G.CHAT_CHANNELS, func = function() _G.ToggleChannelFrame() end, icon = 2056011 }, -- Interface\ICONS\UI_Chat
-	{ text = _G.SOCIAL_BUTTON, microOffset = 'SocialsMicroButton', func =  function() _G.ToggleFriendsFrame() end },
+	{ text = _G.SOCIAL_BUTTON, microOffset = not E.Retail and 'SocialsMicroButton' or nil, func =  function() _G.ToggleFriendsFrame() end },
 	{ text = _G.TALENTS_BUTTON, microOffset = 'TalentMicroButton', func =  function() _G.ToggleTalentFrame() end },
 	{ text = _G.GUILD, microOffset = 'GuildMicroButton', func = function() if E.Retail then _G.ToggleGuildFrame() else _G.ToggleFriendsFrame(3) end end },
 }
@@ -106,7 +106,7 @@ tinsert(menuList, {
 	end
 })
 
-tinsert(menuList, { text = _G.HELP_BUTTON, bottom = true, func = function() _G.ToggleHelpFrame() end })
+tinsert(menuList, { text = _G.HELP_BUTTON, microOffset = not E.Retail and 'HelpMicroButton' or nil, bottom = true, func = function() _G.ToggleHelpFrame() end })
 
 M.RightClickMenu = menuFrame
 M.RightClickMenuList = menuList
