3a067cb48755c199e5a4b31cec14a68a0071bf39
Simpy <kodi03466@hotmail.com>
2024-01-19 15:08:51 -0500

    otherwise 90 won't work (maybe others)

--- ElvUI/Mainline/Modules/Blizzard/ColorPicker.lua
+++ ElvUI/Mainline/Modules/Blizzard/ColorPicker.lua
@@ -81,7 +81,7 @@ local function UpdateColorTexts(r, g, b, box)
 	end
 
 	-- we want those /255 values
-	r, g, b = floor(r*255), floor(g*255), floor(b*255)
+	r, g, b = E:Round(r*255), E:Round(g*255), E:Round(b*255)
 
 	ColorPickerFrame.Content.HexBox:SetText(format('%.2x%.2x%.2x', r, g, b))
 	_G.ColorPPBoxR:SetText(r)
