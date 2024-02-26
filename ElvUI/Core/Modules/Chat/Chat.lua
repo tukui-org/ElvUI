6d36ab1062d829ce165ea62d325f0cb9776b48bc
Simpy <kodi03466@hotmail.com>
2024-02-26 13:21:53 -0500

    handle TextToSpeech button in Voice icons

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -1134,9 +1134,18 @@ function CH:ChatEdit_DeactivateChat(editbox)
 	if style == 'im' then editbox:Hide() end
 end
 
-function CH:UpdateEditboxAnchors(event, cvar, value)
-	if event and cvar ~= 'chatStyle' then return
-	elseif not cvar then value = GetCVar('chatStyle') end
+function CH:CVAR_UPDATE(_, cvar, value)
+	if cvar == 'chatStyle' then
+		CH:UpdateEditboxAnchors(cvar, value)
+	elseif cvar == 'textToSpeech' then
+		CH:RepositionOverflowButton()
+	end
+end
+
+function CH:UpdateEditboxAnchors(cvar, value)
+	if not cvar then
+		value = GetCVar('chatStyle')
+	end
 
 	local classic = value == 'classic'
 	local leftChat = classic and _G.LeftChatPanel
@@ -2426,8 +2435,6 @@ function CH:SetupChat()
 		end
 	end
 
-	CH:ToggleHyperlink(CH.db.hyperlinkHover)
-
 	local chat = _G.GeneralDockManager.primary
 	_G.GeneralDockManager:ClearAllPoints()
 	_G.GeneralDockManager:Point('BOTTOMLEFT', chat, 'TOPLEFT', 0, 3)
@@ -2436,12 +2443,18 @@ function CH:SetupChat()
 	_G.GeneralDockManagerScrollFrame:Height(22)
 	_G.GeneralDockManagerScrollFrameChild:Height(22)
 
+	_G.CHAT_FONT_HEIGHTS = CH.FontHeights
+	_G.ChatFrameMenuButton:Kill()
+
+	CH:ToggleHyperlink(CH.db.hyperlinkHover)
+	CH:StyleOverflowButton()
+	CH:PositionChats()
+
+	_G.TextToSpeechButtonFrame:Hide()
+
 	if E.Retail then
 		_G.QuickJoinToastButton:Hide()
 	end
-
-	CH:StyleOverflowButton()
-	CH:PositionChats()
 
 	if not CH.HookSecured then
 		CH:SecureHook('FCF_OpenTemporaryWindow', 'SetupChat')
@@ -3067,7 +3080,8 @@ function CH:DefaultSmileys()
 end
 
 local channelButtons = {
-	_G.ChatFrameChannelButton,
+	_G.TextToSpeechButton, -- text to speech
+	_G.ChatFrameChannelButton -- main voice button
 }
 
 if E.Retail then
@@ -3101,10 +3115,48 @@ function CH:RepositionOverflowButton()
 	_G.GeneralDockManagerOverflowButtonList:SetFrameLevel(5)
 	_G.GeneralDockManagerOverflowButton:ClearAllPoints()
 
+	-- handle the overflow placement
 	if CH.db.pinVoiceButtons and not CH.db.hideVoiceButtons then
-		_G.GeneralDockManagerOverflowButton:Point('RIGHT', channelButtons[(E.Retail and channelButtons[3]:IsShown() and 3) or 1], 'LEFT', -4, 0)
+		_G.GeneralDockManagerOverflowButton:Point('RIGHT', channelButtons[(E.Retail and channelButtons[4]:IsShown() and 4) or 1], 'LEFT', -4, 0)
 	else
 		_G.GeneralDockManagerOverflowButton:Point('RIGHT', _G.GeneralDockManager, 'RIGHT', -4, 0)
+	end
+
+	-- handle buttons placement on the voice panel
+	local button1 = channelButtons[1] -- text to speech
+	local button2 = channelButtons[2] -- main voice button
+	local ttsEnabled = GetCVarBool('textToSpeech')
+
+	if not CH.db.hideVoiceButtons then
+		if CH.db.pinVoiceButtons then
+			button1:ClearAllPoints()
+			button2:ClearAllPoints()
+
+			if ttsEnabled then
+				button1:Point('RIGHT', _G.GeneralDockManager, 'RIGHT', 2, 0)
+				button2:Point('RIGHT', button1, 'LEFT')
+			else
+				button2:Point('RIGHT', _G.GeneralDockManager, 'RIGHT', 2, 0)
+			end
+		else
+			button1:ClearAllPoints()
+			button1:Point('TOP', CH.VoicePanel, 0, -2)
+
+			button2:ClearAllPoints()
+			if ttsEnabled then
+				button2:Point('TOP', button1, 'BOTTOM', 0, -2)
+			else
+				button2:Point('TOP', CH.VoicePanel, 0, -2)
+			end
+
+			CH.VoicePanel:Size(30, ttsEnabled and 114 or 86)
+		end
+	end
+
+	local ChatAlertFrame = _G.ChatAlertFrame
+	if ChatAlertFrame then
+		ChatAlertFrame:ClearAllPoints()
+		ChatAlertFrame:Point('BOTTOM', (ttsEnabled and button1) or button2, 'TOP', 1, 3)
 	end
 end
 
@@ -3122,13 +3174,13 @@ function CH:HandleChatVoiceIcons()
 	elseif CH.db.pinVoiceButtons then
 		for index, button in ipairs(channelButtons) do
 			S:HandleButton(button, nil, nil, true)
-			button.Icon:SetDesaturated(CH.db.desaturateVoiceIcons)
-			button:ClearAllPoints()
 
 			if index == 1 then
-				button:Point('RIGHT', _G.GeneralDockManager, 'RIGHT', 2, 0)
-			else
-				button:Point('RIGHT', channelButtons[index-1], 'LEFT')
+				button.highlightAtlas = nil
+			end
+
+			if button.Icon then
+				button.Icon:SetDesaturated(CH.db.desaturateVoiceIcons)
 			end
 		end
 
@@ -3165,7 +3217,6 @@ function CH:CreateChatVoicePanel()
 	local Holder = CreateFrame('Frame', 'ElvUIChatVoicePanel', E.UIParent)
 	Holder:ClearAllPoints()
 	Holder:Point('BOTTOMLEFT', _G.LeftChatPanel, 'TOPLEFT', 0, 1)
-	Holder:Size(30, 86)
 	Holder:SetTemplate('Transparent', nil, true)
 	Holder:SetBackdropColor(CH.db.panelColor.r, CH.db.panelColor.g, CH.db.panelColor.b, CH.db.panelColor.a)
 	E:CreateMover(Holder, 'SocialMenuMover', _G.BINDING_HEADER_VOICE_CHAT, nil, nil, nil, nil, nil, 'chat')
@@ -3175,15 +3226,19 @@ function CH:CreateChatVoicePanel()
 	Holder:SetScript('OnLeave', CH.LeaveVoicePanel)
 	CH.LeaveVoicePanel(Holder)
 
-	channelButtons[1]:ClearAllPoints()
-	channelButtons[1]:Point('TOP', Holder, 'TOP', 0, -2)
-
-	for _, button in ipairs(channelButtons) do
+	for index, button in ipairs(channelButtons) do
 		S:HandleButton(button, nil, nil, true)
-		button.Icon:SetParent(button)
-		button.Icon:SetDesaturated(CH.db.desaturateVoiceIcons)
-		button:SetParent(Holder)
 
+		if index == 1 then
+			button.highlightAtlas = nil
+		end
+
+		if button.Icon then
+			button.Icon:SetParent(button)
+			button.Icon:SetDesaturated(CH.db.desaturateVoiceIcons)
+		end
+
+		button:SetParent(Holder)
 		button:HookScript('OnEnter', CH.EnterVoicePanel)
 		button:HookScript('OnLeave', CH.LeaveVoicePanel)
 	end
@@ -3191,9 +3246,6 @@ function CH:CreateChatVoicePanel()
 	if E.Retail then
 		CH:SetupQuickJoin(Holder)
 	end
-
-	_G.ChatAlertFrame:ClearAllPoints()
-	_G.ChatAlertFrame:Point('BOTTOM', channelButtons[1], 'TOP', 1, 3)
 end
 
 function CH:SetupQuickJoin(holder)
@@ -3665,9 +3717,6 @@ function CH:Initialize()
 	if not ElvCharacterDB.ChatEditHistory then ElvCharacterDB.ChatEditHistory = {} end
 	if not ElvCharacterDB.ChatHistoryLog or not CH.db.chatHistory then ElvCharacterDB.ChatHistoryLog = {} end
 
-	_G.CHAT_FONT_HEIGHTS = CH.FontHeights
-	_G.ChatFrameMenuButton:Kill()
-
 	CH:SetupChat()
 	CH:DefaultSmileys()
 	CH:UpdateChatKeywords()
@@ -3702,7 +3751,7 @@ function CH:Initialize()
 	CH:RegisterEvent('UPDATE_FLOATING_CHAT_WINDOWS', 'SetupChat')
 	CH:RegisterEvent('GROUP_ROSTER_UPDATE', 'CheckLFGRoles')
 	CH:RegisterEvent('PLAYER_REGEN_DISABLED', 'ChatEdit_PleaseUntaint')
-	CH:RegisterEvent('CVAR_UPDATE', 'UpdateEditboxAnchors')
+	CH:RegisterEvent('CVAR_UPDATE')
 	CH:RegisterEvent('PET_BATTLE_CLOSE')
 
 	if E.Retail then
