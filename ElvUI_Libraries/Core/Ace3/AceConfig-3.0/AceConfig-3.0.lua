c0aa7c34c86fc78d06cebc837ed897dc71b9ec86
Paweł Jastrzębski <pawelj@iosphe.re>
2024-03-30 17:33:23 +0100

    Ace3 cleanup

--- ElvUI_Libraries/Core/Ace3/AceConfig-3.0/AceConfig-3.0.lua
+++ ElvUI_Libraries/Core/Ace3/AceConfig-3.0/AceConfig-3.0.lua
@@ -13,7 +13,7 @@ Very light wrapper library that combines all the AceConfig subcomponents into on
 ]]
 
 local cfgreg = LibStub("AceConfigRegistry-3.0-ElvUI")
-local cfgcmd = LibStub("AceConfigCmd-3.0-ElvUI")
+local cfgcmd = LibStub("AceConfigCmd-3.0")
 
 local MAJOR, MINOR = "AceConfig-3.0-ElvUI", 3
 local AceConfig = LibStub:NewLibrary(MAJOR, MINOR)
