4040594438494c145026ca2f18d42059a7a7fe1a
Simpy <kodi03466@hotmail.com>
2024-04-08 21:36:57 -0400

    clean up reforge skin

--- ElvUI/Cata/Modules/Skins/Reforge.lua
+++ ElvUI/Cata/Modules/Skins/Reforge.lua
@@ -1,56 +1,59 @@
 local E, L, V, P, G = unpack(select(2, ...))
 local S = E:GetModule('Skins')
 
+local _G = _G
 local unpack = unpack
+local hooksecurefunc = hooksecurefunc
 
-local GetReforgeItemInfo = GetReforgeItemInfo
+local GetItemIconByID = C_Item.GetItemIconByID
+local GetReforgeItemInfo = C_Reforge.GetReforgeItemInfo
+
+local function ReforgingFrameUpdate()
+	local _, itemID, _, quality = GetReforgeItemInfo()
+	local itemTexture = itemID and GetItemIconByID(itemID)
+	if itemTexture then
+		_G.ReforgingFrameItemButtonIconTexture:SetTexture(itemTexture)
+	end
+
+	--[[ Blizzard bug / commented out on their end / Blizzard_ReforgingUI.lua:101
+	if quality then
+		local r, g, b = GetItemQualityColor(quality)
+		_G.ReforgingFrameItemButton:SetBackdropBorderColor(r, g, b)
+	else
+		local r, g, b = unpack(E.media.bordercolor)
+		_G.ReforgingFrameItemButton:SetBackdropBorderColor(r, g, b)
+	end
+	]]
+end
 
 function S:Blizzard_ReforgingUI()
 	if not (E.private.skins.blizzard.enable or not E.private.skins.blizzard.reforge) then return end
 
-	_G.ReforgingFrame:StripTextures()
-	_G.ReforgingFrame:SetTemplate('Transparent')
+	local ReforgingFrame = _G.ReforgingFrame
+	ReforgingFrame:StripTextures()
+	ReforgingFrame:SetTemplate('Transparent')
 
 	_G.ReforgingFrameFinishedGlow:Kill()
-
 	_G.ReforgingFrameButtonFrame:StripTextures()
 
-	_G.ReforgingFrameRestoreMessage:SetTextColor(1, 1, 1)
-
 	S:HandleButton(_G.ReforgingFrameRestoreButton, true)
-
 	S:HandleButton(_G.ReforgingFrameReforgeButton, true)
-	_G.ReforgingFrameReforgeButton:Point('BOTTOMRIGHT', -3, 3)
+	S:HandleCloseButton(_G.ReforgingFrameCloseButton)
 
-	_G.ReforgingFrameItemButton:StripTextures()
-	_G.ReforgingFrameItemButton:SetTemplate('Default', true)
-	_G.ReforgingFrameItemButton:StyleButton()
+	ReforgingFrame.missingDescription:SetTextColor(1, 1, 1)
+	_G.ReforgingFrameRestoreMessage:SetTextColor(1, 1, 1)
+	_G.ReforgingFrameReforgeButton:Point('BOTTOMRIGHT', -3, 3)
 
 	_G.ReforgingFrameItemButtonIconTexture:SetInside()
 	_G.ReforgingFrameItemButtonIconTexture:SetTexCoord(unpack(E.TexCoords))
 
-	_G.ReforgingFrameItemButton.missingText:SetTextColor(1, 0.80, 0.10)
-	_G.ReforgingFrame.missingDescription:SetTextColor(1, 1, 1)
+	local ItemButton = _G.ReforgingFrameItemButton
+	ItemButton.missingText:SetTextColor(1, 0.80, 0.10)
+	ItemButton:StripTextures()
+	ItemButton:SetTemplate('Default', true)
+	ItemButton:StyleButton()
 
-	hooksecurefunc('ReforgingFrame_Update', function(self)
-		local _, itemID, _, quality = C_Reforge.GetReforgeItemInfo()
-
-		if itemID then
-			local itemTexture = C_Item.GetItemIconByID(itemID)
-			_G.ReforgingFrameItemButtonIconTexture:SetTexture(itemTexture)
-		end
-
-		-- Blizzard bug / commented out on their end / Blizzard_ReforgingUI.lua:101
-		--[[
-		if quality then
-			_G.ReforgingFrameItemButton:SetBackdropBorderColor(GetItemQualityColor(quality))
-		else
-			_G.ReforgingFrameItemButton:SetBackdropBorderColor(unpack(E.media.bordercolor))
-		end
-		]]
-	end)
-
-	S:HandleCloseButton(_G.ReforgingFrameCloseButton)
+	hooksecurefunc('ReforgingFrame_Update', ReforgingFrameUpdate)
 end
 
 S:AddCallbackForAddon('Blizzard_ReforgingUI')
