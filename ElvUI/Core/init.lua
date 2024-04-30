fff43555710d92c8fb045bd06d4d31f7e8db83e8
Simpy <kodi03466@hotmail.com>
2024-04-30 18:41:39 -0400

    13.62

--- ElvUI/Core/init.lua
+++ ElvUI/Core/init.lua
@@ -117,7 +117,7 @@ end
 function E:ParseVersionString(addon)
 	local version = GetAddOnMetadata(addon, 'Version')
 	if strfind(version, 'project%-version') then
-		return 13.61, '13.61-git', nil, true
+		return 13.62, '13.62-git', nil, true
 	else
 		local release, extra = strmatch(version, '^v?([%d.]+)(.*)')
 		return tonumber(release), release..extra, extra ~= ''
