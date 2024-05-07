18729da6052a89453645bd41b0afc5c038609887
Simpy <kodi03466@hotmail.com>
2024-05-07 12:59:44 -0400

    retail stable skin on cata

--- ElvUI/Cata/Modules/Skins/Stable.lua
+++ ElvUI/Cata/Modules/Skins/Stable.lua
@@ -2,51 +2,73 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
-local HasPetUI = HasPetUI
-local UnitExists = UnitExists
-local GetPetHappiness = GetPetHappiness
-local hooksecurefunc = hooksecurefunc
+local unpack = unpack
+local CreateFrame = CreateFrame
+
+local function PetButtons(btn, p)
+	local button = _G[btn]
+	local icon = _G[btn..'IconTexture']
+	local highlight = button:GetHighlightTexture()
+	button:StripTextures()
+
+	if button.Checked then
+		button.Checked:SetColorTexture(unpack(E.media.rgbvaluecolor))
+		button.Checked:SetAllPoints(icon)
+		button.Checked:SetAlpha(0.3)
+	end
+
+	if highlight then
+		highlight:SetColorTexture(1, 1, 1, 0.3)
+		highlight:SetAllPoints(icon)
+	end
+
+	if icon then
+		icon:SetTexCoord(unpack(E.TexCoords))
+		icon:ClearAllPoints()
+		icon:Point('TOPLEFT', p, -p)
+		icon:Point('BOTTOMRIGHT', -p, p)
+
+		button:SetFrameLevel(button:GetFrameLevel() + 2)
+		button:SetTemplate(nil, true)
+	end
+end
 
 function S:PetStableFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.stable) then return end
 
 	local PetStableFrame = _G.PetStableFrame
-	S:HandleFrame(PetStableFrame, true, nil, 10, -11, -32, 71)
+	S:HandlePortraitFrame(PetStableFrame)
 
-	S:HandleButton(_G.PetStablePurchaseButton)
-	S:HandleCloseButton(_G.PetStableFrameCloseButton)
-	S:HandleRotateButton(_G.PetStableModelRotateRightButton)
-	S:HandleRotateButton(_G.PetStableModelRotateLeftButton)
+	_G.PetStableLeftInset:Hide()
+	_G.PetStableBottomInset:Hide()
+	_G.PetStableFrameModelBg:Hide()
+	_G.PetStableDietTexture:SetTexture(132165)
+	_G.PetStableDietTexture:SetTexCoord(unpack(E.TexCoords))
+	_G.PetStableFrameInset:SetTemplate('Transparent')
 
-	S:HandleItemButton(_G.PetStableCurrentPet, true)
-	_G.PetStableCurrentPetIconTexture:SetDrawLayer('ARTWORK')
+	S:HandleModelSceneControlButtons(_G.PetStableModelScene.ControlFrame)
+	S:HandleButton(_G.PetStablePrevPageButton) -- Required to remove graphical glitch from Prev page button
+	S:HandleButton(_G.PetStableNextPageButton) -- Required to remove graphical glitch from Next page button
 
-	for i = 1, _G.NUM_PET_STABLE_SLOTS do
-		S:HandleItemButton(_G['PetStableStabledPet'..i], true)
-		_G['PetStableStabledPet'..i..'IconTexture']:SetDrawLayer('ARTWORK')
+	local p = E.PixelMode and 1 or 2
+	local PetStableSelectedPetIcon = _G.PetStableSelectedPetIcon
+	if PetStableSelectedPetIcon then
+		PetStableSelectedPetIcon:SetTexCoord(unpack(E.TexCoords))
+
+		local b = CreateFrame('Frame', nil, PetStableSelectedPetIcon:GetParent())
+		b:Point('TOPLEFT', PetStableSelectedPetIcon, -p, p)
+		b:Point('BOTTOMRIGHT', PetStableSelectedPetIcon, p, -p)
+		PetStableSelectedPetIcon:Size(37)
+		PetStableSelectedPetIcon:SetParent(b)
+		b:SetTemplate()
 	end
 
-	local PetStablePetInfo = _G.PetStablePetInfo
-	PetStablePetInfo:GetRegions():SetTexCoord(0.04, 0.15, 0.06, 0.30)
-	PetStablePetInfo:SetFrameLevel(_G.PetModelFrame:GetFrameLevel() + 2)
-	PetStablePetInfo:CreateBackdrop()
-	PetStablePetInfo:Size(22)
-
-	hooksecurefunc('PetStable_Update', function()
-		local hasPetUI, isHunterPet = HasPetUI()
-		if hasPetUI and not isHunterPet and UnitExists('pet') then return end
-
-		local happiness = GetPetHappiness()
-		local texture = PetStablePetInfo:GetRegions()
-
-		if happiness == 1 then
-			texture:SetTexCoord(0.41, 0.53, 0.06, 0.30)
-		elseif happiness == 2 then
-			texture:SetTexCoord(0.22, 0.345, 0.06, 0.30)
-		elseif happiness == 3 then
-			texture:SetTexCoord(0.04, 0.15, 0.06, 0.30)
-		end
-	end)
+	for i = 1, _G.NUM_PET_ACTIVE_SLOTS do
+		PetButtons('PetStableActivePet' .. i, p)
+	end
+	for i = 1, _G.NUM_PET_STABLE_SLOTS do
+		PetButtons('PetStableStabledPet' .. i, p)
+	end
 end
 
 S:AddCallback('PetStableFrame')
