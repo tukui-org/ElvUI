8d6a8ad29f96784f3701d78e7a2bbc717eaaf918
Merathilis <neo0608@gmail.com>
2024-05-17 11:28:39 +0200

    Boop²

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -490,6 +490,7 @@ do --this can save some main file locals
 			z['Player-1401-04217BB2']	= ElvPurple	-- [Alliance] Warlock:	Asragoth
 			z['Player-1401-0421EB9F']	= ElvBlue	-- [Alliance] Warrior:	Brìtt
 			z['Player-1401-0421F909']	= ElvRed	-- [Alliance] Paladin:	Damará
+			z["Player-1401-0AB0E6D1"]	= ElvRed --	 . [Alliance] Paladin:	Damara/WoW Remix
 			z['Player-1401-0421EC36']	= ElvBlue	-- [Alliance] Priest:	Jazira
 			z['Player-1401-0A9B0131']	= ElvYellow	-- [Alliance] Rogue:	Anonia
 			z['Player-1401-041E4D64']	= ElvGreen	-- [Alliance] Monk:		Maithilis
