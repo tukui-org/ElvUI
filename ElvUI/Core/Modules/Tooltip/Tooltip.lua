122cf0b8083895836acc4d3e3fa9050cc0e33032
Repooc <tjcooper1982@gmail.com>
2024-06-04 18:13:50 -0400

    fix error for unit is not a player

--- ElvUI/Core/Modules/Tooltip/Tooltip.lua
+++ ElvUI/Core/Modules/Tooltip/Tooltip.lua
@@ -403,7 +403,7 @@ end
 
 local lastGUID
 function TT:AddInspectInfo(tt, unit, numTries, r, g, b)
-	if tt.ItemLevelShown or (not unit) or (numTries > 3) or not CanInspect(unit) then return end
+	if tt.ItemLevelShown or (not unit) or (numTries > 3) or not UnitIsPlayer(unit) or not CanInspect(unit) then return end
 
 	local unitGUID = UnitGUID(unit)
 	if not unitGUID then return end
