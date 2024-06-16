83f1be77e0b7055e80920c1e478215811a77dfb0
Luckyone <Luckyone.Development@pm.me>
2024-06-16 14:59:48 +0200

    Revert "Fix reload on sod ptr"
    This reverts commit a0254a80a50434107eda03d7b561e45a1c805463.

--- ElvUI_Libraries/Classic/LibClassicSpecs/LibClassicSpecs.lua
+++ ElvUI_Libraries/Classic/LibClassicSpecs/LibClassicSpecs.lua
@@ -408,7 +408,7 @@ function LCS.GetSpecialization(isInspect, isPet)
 	local specIndex, maxSpent = 0, 0
 
 	for tabIndex = 1, GetNumTalentTabs() do
-		local spent = select((isCata or isClassic and 5) or 3, GetTalentTabInfo(tabIndex))
+		local spent = select(isCata and 5 or 3, GetTalentTabInfo(tabIndex))
 		if (spent > maxSpent) then
 			specIndex, maxSpent = tabIndex, spent
 		end
