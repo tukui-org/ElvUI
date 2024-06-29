c5b324e7fb373adf685942e19647ea247363dfd1
Repooc <tjcooper1982@gmail.com>
2024-06-29 18:07:46 -0400

    fix target translit tags to return nil instead of empty strings

--- ElvUI/Core/General/Tags.lua
+++ ElvUI/Core/General/Tags.lua
@@ -422,9 +422,12 @@ for textFormat, length in pairs({ veryshort = 5, short = 10, medium = 15, long =
 	end)
 
 	E:AddTag(format('target:%s:translit', textFormat), 'UNIT_TARGET', function(unit)
-		local targetName = Translit:Transliterate(UnitName(unit..'target'), translitMark)
-		if targetName then
-			return E:ShortenString(targetName, length)
+		local targetUnit = UnitName(unit..'target')
+		if targetUnit then
+			local targetName = Translit:Transliterate(targetUnit, translitMark)
+			if targetName then
+				return E:ShortenString(targetName, length)
+			end
 		end
 	end)
 end
