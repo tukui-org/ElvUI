a1e64ba3cf1ab57e56f4f3670367ab724f6c3445
Simpy <kodi03466@hotmail.com>
2024-07-01 14:00:04 -0400

    rename this

--- ElvUI/Core/General/Tags.lua
+++ ElvUI/Core/General/Tags.lua
@@ -422,11 +422,11 @@ for textFormat, length in pairs({ veryshort = 5, short = 10, medium = 15, long =
 	end)
 
 	E:AddTag(format('target:%s:translit', textFormat), 'UNIT_TARGET', function(unit)
-		local targetUnit = UnitName(unit..'target')
-		if targetUnit then
-			local targetName = Translit:Transliterate(targetUnit, translitMark)
-			if targetName then
-				return E:ShortenString(targetName, length)
+		local targetName = UnitName(unit..'target')
+		if targetName then
+			local translitName = Translit:Transliterate(targetName, translitMark)
+			if translitName then
+				return E:ShortenString(translitName, length)
 			end
 		end
 	end)
