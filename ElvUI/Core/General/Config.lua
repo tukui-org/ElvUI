387f4a5f450ce341f6049c84fd91b1233c49b5cb
Simpy <kodi03466@hotmail.com>
2024-07-03 16:14:04 -0400

    180

--- ElvUI/Core/General/Config.lua
+++ ElvUI/Core/General/Config.lua
@@ -670,18 +670,18 @@ function E:Config_SearchUpdate(userInput)
 	C:Search_ClearResults()
 
 	local text = self:GetText()
-	if strmatch(text, '%s*') then
-		local _, selected = E:Config_GetStatus(self.frame)
-		if selected == 'search' then
-			ACD:SelectGroup('ElvUI', self.selected or 'general') -- try to stay or swap back to general if it cant
-		end
-	elseif strmatch(text, '%S+') then
+	if strmatch(text, '%S+') then
 		C.SearchText = strtrim(strlower(text))
 
 		C:Search_Config()
 		C:Search_AddResults()
 
 		ACD:SelectGroup('ElvUI', 'search') -- trigger update
+	elseif strmatch(text, '%s*') then
+		local _, selected = E:Config_GetStatus(self.frame)
+		if selected == 'search' then
+			ACD:SelectGroup('ElvUI', self.selected or 'general') -- try to stay or swap back to general if it cant
+		end
 	end
 end
 
