b4ddf9946439ac5dc86d83d95e1b891d6a7e1cd5
Repooc <tjcooper1982@gmail.com>
2024-07-09 14:29:48 -0400

    Revert "revert this for now"
    This reverts commit 4fa75f4627fdd9f468908652628d4f100cd11362.

--- ElvUI_Libraries/Classic/LibClassicSpecs/LibClassicSpecs.lua
+++ ElvUI_Libraries/Classic/LibClassicSpecs/LibClassicSpecs.lua
@@ -407,9 +407,8 @@ function LCS.GetSpecialization(isInspect, isPet)
 
 	local specIndex, maxSpent = 0, 0
 
-	-- TODO: 1.15.3
 	for tabIndex = 1, GetNumTalentTabs() do
-		local spent = select(isCata and 5 or 3, GetTalentTabInfo(tabIndex))
+		local spent = select(5, GetTalentTabInfo(tabIndex))
 		if (spent > maxSpent) then
 			specIndex, maxSpent = tabIndex, spent
 		end
