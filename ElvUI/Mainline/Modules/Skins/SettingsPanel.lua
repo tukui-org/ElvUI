ea7fb01c7254204a3bb13448e241c369356123d8
Merathilis <neo0608@gmail.com>
2024-07-10 10:59:42 +0200

    Updated Settings Panel skin

--- ElvUI/Mainline/Modules/Skins/SettingsPanel.lua
+++ ElvUI/Mainline/Modules/Skins/SettingsPanel.lua
@@ -5,6 +5,38 @@ local _G = _G
 local next = next
 local hooksecurefunc = hooksecurefunc
 
+local function HandleDropDownArrow(button, direction)
+	button.NormalTexture:SetAlpha(0)
+	button.PushedTexture:SetAlpha(0)
+	button:GetHighlightTexture():SetAlpha(0)
+
+	local dis = button:GetDisabledTexture()
+	S:SetupArrow(dis, direction)
+	dis:SetVertexColor(0, 0, 0, .7)
+	dis:SetDrawLayer('OVERLAY')
+	dis:SetInside(button, 4, 4)
+
+	local tex = button:CreateTexture(nil, 'ARTWORK')
+	tex:SetInside(button, 4, 4)
+	S:SetupArrow(tex, direction)
+end
+
+local function HandleOptionDropDown(option)
+	local button = option.Button
+	S:HandleButton(button)
+	button.NormalTexture:SetAlpha(0)
+	button.HighlightTexture:SetAlpha(0)
+
+	HandleDropDownArrow(option.DecrementButton, 'left')
+	HandleDropDownArrow(option.IncrementButton, 'right')
+end
+
+local function HandleDropdown(option)
+	S:HandleButton(option.Dropdown)
+	S:HandleButton(option.DecrementButton)
+	S:HandleButton(option.IncrementButton)
+end
+
 local function HandleTabs(tab)
 	if tab then
 		tab:StripTextures(true)
@@ -102,7 +134,7 @@ function S:SettingsPanel()
 				end
 
 				local toggle = child.Toggle
-				if toggle then -- ToDo Handle the toggle. DF
+				if toggle then
 					toggle:GetPushedTexture():SetAlpha(0)
 				end
 
@@ -125,8 +157,17 @@ function S:SettingsPanel()
 					child.backdrop:Point('TOPLEFT', 15, -30)
 					child.backdrop:Point('BOTTOMRIGHT', -30, -5)
 				end
-				if child.CheckBox then
-					HandleCheckbox(child.CheckBox) -- this is atlas shit, so S.HandleCheckBox wont work right now
+				if child.Checkbox then
+					HandleCheckbox(child.Checkbox)
+				end
+				if child.Dropdown then -- isWW
+					HandleOptionDropDown(child.Dropdown)
+				end
+				if child.ColorBlindFilterDropDown then
+					HandleOptionDropDown(child.ColorBlindFilterDropDown)
+				end
+				if child.Control then
+					HandleDropdown(child.Control)
 				end
 				if child.Button then
 					if child.Button:GetWidth() < 250 then
