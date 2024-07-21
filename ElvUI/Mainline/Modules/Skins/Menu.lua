9aff0ee1de75eb181d0ec5e16e6f05a85dd22897
Simpy <kodi03466@hotmail.com>
2024-07-20 20:25:58 -0400

    not finished

--- ElvUI/Mainline/Modules/Skins/Menu.lua
+++ ElvUI/Mainline/Modules/Skins/Menu.lua
@@ -1,49 +1,47 @@
 local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
---[[
-	This Skin file handle the "new" Dropdown Menu. Its in:
+local hooksecurefunc = hooksecurefunc
 
-	Interface\AddOns\Blizzard_Menu\Menu.lua
-]]
+local function SkinFrame(frame)
+	frame:StripTextures()
+
+	if not frame.backdrop then
+		frame:CreateBackdrop('Transparent') -- :SetTemplate errors out
+
+		if frame.ScrollBar then
+			S:HandleTrimScrollBar(frame.ScrollBar)
+		end
+	end
+
+	for _, child in next, { frame:GetChildren() } do
+		local minLevel = child.MinLevel
+		if minLevel and not minLevel.IsSkinned then
+			S:HandleExitBox(minLevel)
+			minLevel.IsSkinned = true
+		end
+
+		local maxLevel = child.MaxLevel
+		if maxLevel and not maxLevel.IsSkinned then
+			S:HandleExitBox(maxLevel)
+			maxLevel.IsSkinned = true
+		end
+	end
+end
+
+local function OpenMenu(manager)
+	local menu = manager:GetOpenMenu()
+	if menu then
+		SkinFrame(menu)
+	end
+end
 
 function S:Blizzard_Menu()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.misc) then return end
 
-	local MenuManagerProxy = Menu.GetManager()
-
-	local IsSkinned
-	hooksecurefunc(MenuManagerProxy, 'OpenMenu', function()
-		local MenuFrame = MenuManagerProxy:GetOpenMenu()
-		if MenuFrame then
-			MenuFrame:StripTextures()
-
-			if not IsSkinned then
-				MenuFrame:CreateBackdrop('Transparent') -- :SetTemplate errors out
-
-				if MenuFrame.ScrollBar then
-					S:HandleTrimScrollBar(MenuFrame.ScrollBar)
-				end
-				IsSkinned = true
-			end
-
-			for i = 1, MenuFrame:GetNumChildren() do
-				local child = select(i, MenuFrame:GetChildren())
-
-				local minLevel = child.MinLevel
-				if minLevel and not minLevel.IsSkinned then
-					S:HandleExitBox(minLevel)
-					minLevel.IsSkinned = true
-				end
-
-				local maxLevel = child.MaxLevel
-				if maxLevel and not maxLevel.IsSkinned then
-					S:HandleExitBox(maxLevel)
-					maxLevel.IsSkinned = true
-				end
-			end
-		end
-	end)
+	local manager = _G.Menu.GetManager()
+	hooksecurefunc(manager, 'OpenMenu', OpenMenu)
+	hooksecurefunc(manager, 'OpenContextMenu', OpenMenu)
 end
 
 S:AddCallbackForAddon('Blizzard_Menu')
