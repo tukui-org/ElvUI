f6b6ab3269fa0e93aa1fae70f5702dee4f17696e
Simpy <kodi03466@hotmail.com>
2024-08-03 06:40:30 -0400

    short

--- ElvUI/Classic/Modules/Skins/QuestTimers.lua
+++ ElvUI/Classic/Modules/Skins/QuestTimers.lua
@@ -3,11 +3,11 @@ local S = E:GetModule('Skins')
 
 local _G = _G
 
-function S:SkinQuestTimers()
+function S:Blizzard_QuestTimer()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.questTimers) then return end
 
 	S:HandleFrame(_G.QuestTimerFrame, true)
 	_G.QuestTimerHeader:Point('TOP', 1, 8)
 end
 
-S:AddCallbackForAddon('Blizzard_QuestTimer', 'SkinQuestTimers')
+S:AddCallbackForAddon('Blizzard_QuestTimer')
