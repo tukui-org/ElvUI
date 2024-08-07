ab98ee0be9c33dc4651ff6ed26dbbc5d016912e4
Repooc <tjcooper1982@gmail.com>
2024-08-06 23:00:15 -0400

    might be safer than prev commit

--- ElvUI/Core/Modules/Nameplates/Nameplates.lua
+++ ElvUI/Core/Modules/Nameplates/Nameplates.lua
@@ -957,9 +957,8 @@ function NP:Initialize()
 			if frame.classNamePlateMechanicFrame then
 				frame.classNamePlateMechanicFrame:Hide()
 			end
-			if frame.classNamePlateAlternatePowerBar then -- Monk Stagger Bar (Might be for other classes?)
-				frame.classNamePlateAlternatePowerBar:Hide()
-				frame.classNamePlateAlternatePowerBar:UnregisterAllEvents()
+			if ClassNameplateBrewmasterBarFrame then -- Monk Stagger Bar (Might be for other classes?)
+				ClassNameplateBrewmasterBarFrame:Hide()
 			end
 			if frame.classNamePlatePowerBar then
 				frame.classNamePlatePowerBar:Hide()
