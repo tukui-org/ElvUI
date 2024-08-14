42935c330d2912d721f83a4688b6306294c259b0
Azilroka <darktemplar9900@yahoo.com>
2024-08-14 18:35:30 -0500

    unicorn

--- ElvUI/Core/Media/SharedMedia.lua
+++ ElvUI/Core/Media/SharedMedia.lua
@@ -213,6 +213,7 @@ AddMedia('logo','ElvPurple')
 AddMedia('logo','ElvYellow')
 AddMedia('logo','ElvRed')
 AddMedia('logo','ElvSimpy')
+AddMedia('logo','AngryUnicorn')
 
 AddMedia('logo','Bathrobe')
 AddMedia('logo','Rainbow')
