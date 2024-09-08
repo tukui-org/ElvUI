6ed3b58a563c9770ccdf1ae72e9b9e5018534f01
Simpy <kodi03466@hotmail.com>
2024-09-08 18:59:59 -0400

    swap - to / in rep databar

--- ElvUI/Core/Modules/DataBars/Reputation.lua
+++ ElvUI/Core/Modules/DataBars/Reputation.lua
@@ -106,17 +106,17 @@ function DB:ReputationBar_Update()
 		elseif textFormat == 'PERCENT' then
 			displayString = format('%s: %d%% [%s]', name, percent, standing)
 		elseif textFormat == 'CURMAX' then
-			displayString = format('%s: %s - %s [%s]', name, E:ShortValue(current), E:ShortValue(maximum), standing)
+			displayString = format('%s: %s / %s [%s]', name, E:ShortValue(current), E:ShortValue(maximum), standing)
 		elseif textFormat == 'CURPERC' then
-			displayString = format('%s: %s - %d%% [%s]', name, E:ShortValue(current), percent, standing)
+			displayString = format('%s: %s / %d%% [%s]', name, E:ShortValue(current), percent, standing)
 		elseif textFormat == 'CUR' then
 			displayString = format('%s: %s [%s]', name, E:ShortValue(current), standing)
 		elseif textFormat == 'REM' then
 			displayString = format('%s: %s [%s]', name, E:ShortValue(maximum - current), standing)
 		elseif textFormat == 'CURREM' then
-			displayString = format('%s: %s - %s [%s]', name, E:ShortValue(current), E:ShortValue(maximum - current), standing)
+			displayString = format('%s: %s / %s [%s]', name, E:ShortValue(current), E:ShortValue(maximum - current), standing)
 		elseif textFormat == 'CURPERCREM' then
-			displayString = format('%s: %s - %d%% (%s) [%s]', name, E:ShortValue(current), percent, E:ShortValue(maximum - current), standing)
+			displayString = format('%s: %s / %d%% (%s) [%s]', name, E:ShortValue(current), percent, E:ShortValue(maximum - current), standing)
 		end
 	end
 
