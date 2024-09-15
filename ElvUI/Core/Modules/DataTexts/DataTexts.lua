6713f825f211d1ab414e7cb6aa99c9c224c500fa
Azilroka <darktemplar9900@yahoo.com>
2024-09-15 16:35:16 -0500

    part of last commit

--- ElvUI/Core/Modules/DataTexts/DataTexts.lua
+++ ElvUI/Core/Modules/DataTexts/DataTexts.lua
@@ -748,7 +748,7 @@ do
 		DT:SortMenuList(QuickList)
 	end
 
-	function DT:Player_Invite(_, name, guid)
+	function DT:Player_Invite(name, guid)
 		if not (name and name ~= '') then return end
 		local isBNet = type(name) == 'number'
 
@@ -778,7 +778,7 @@ do
 		end
 	end
 
-	function DT:Player_Whisper(_, name, battleNet)
+	function DT:Player_Whisper(name, battleNet)
 		if battleNet then
 			ChatFrame_SendBNetTell(name)
 		else
