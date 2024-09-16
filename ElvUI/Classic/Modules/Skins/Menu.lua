66c6636947985f1337c7dc315a92bd95883fc4e7
Luckyone <Luckyone.Development@pm.me>
2024-09-16 14:13:26 +0200

    Add skin for right-click Dropdowns

---
+++ ElvUI/Classic/Modules/Skins/Menu.lua
@@ -0,0 +1,40 @@
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule('Skins')
+
+local _G = _G
+local hooksecurefunc = hooksecurefunc
+
+local backdrops = {}
+local function SkinFrame(frame)
+	frame:StripTextures()
+
+	if backdrops[frame] then
+		frame.backdrop = backdrops[frame] -- relink it back
+	else
+		frame:CreateBackdrop('Transparent') -- :SetTemplate errors out
+		backdrops[frame] = frame.backdrop -- keep below CreateBackdrop
+
+		if frame.ScrollBar then
+			S:HandleTrimScrollBar(frame.ScrollBar)
+		end
+	end
+end
+
+local function OpenMenu(manager)
+	local menu = manager:GetOpenMenu()
+	if menu then
+		SkinFrame(menu)
+	end
+end
+
+function S:Blizzard_Menu()
+	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.misc) then return end
+
+	local manager = _G.Menu.GetManager()
+	if manager then
+		hooksecurefunc(manager, 'OpenMenu', OpenMenu)
+		hooksecurefunc(manager, 'OpenContextMenu', OpenMenu)
+	end
+end
+
+S:AddCallbackForAddon('Blizzard_Menu')
