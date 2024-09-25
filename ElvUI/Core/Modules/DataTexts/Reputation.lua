7b9e612dc8dff3e0b850a5ee8ca73abcaa3ba61d
Simpy <kodi03466@hotmail.com>
2024-09-25 02:24:40 -0400

    rep error for datatext and databars

--- ElvUI/Core/Modules/DataTexts/Reputation.lua
+++ ElvUI/Core/Modules/DataTexts/Reputation.lua
@@ -54,7 +54,7 @@ local function OnEvent(self)
 		standing, currentReactionThreshold, nextReactionThreshold, currentStanding = info.reaction, info.reactionThreshold or 0, info.nextThreshold or huge, info.standing or 1
 	end
 
-	if not standing and factionID and C_Reputation_IsFactionParagon(factionID) then
+	if E.Retail and not standing and factionID and C_Reputation_IsFactionParagon(factionID) then
 		local current, threshold
 		current, threshold = C_Reputation_GetFactionParagonInfo(factionID)
 
@@ -108,7 +108,7 @@ local function OnEnter()
 	if not data then return end
 	local name, reaction, currentReactionThreshold, nextReactionThreshold, currentStanding, factionID = data.name, data.reaction, data.currentReactionThreshold, data.nextReactionThreshold, data.currentStanding, data.factionID
 
-	local isParagon = factionID and C_Reputation_IsFactionParagon(factionID)
+	local isParagon = E.Retail and factionID and C_Reputation_IsFactionParagon(factionID)
 	local standing
 
 	if isParagon then
