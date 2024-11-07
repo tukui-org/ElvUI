d22b34479e71cb5c35eaddbbb5a44ae5f819d13a
Simpy <kodi03466@hotmail.com>
2024-11-06 20:13:35 -0500

    shouldnt need to call SetSpellBookItem twice

--- ElvUI/Core/Modules/ActionBars/ActionBars.lua
+++ ElvUI/Core/Modules/ActionBars/ActionBars.lua
@@ -1024,8 +1024,7 @@ function AB:SpellButtonOnEnter(_, tt)
 	end
 
 	if tt == E.SpellBookTooltip then
-		local needsUpdate = tt:SetSpellBookItem(slotIndex, slotBank)
-		tt:SetScript('OnUpdate', (needsUpdate and AB.SpellBookTooltipOnUpdate) or nil)
+		tt:SetScript('OnUpdate', (tt.UpdateTooltip and AB.SpellBookTooltipOnUpdate) or nil)
 	end
 
 	if not E.Cata then -- Cata Flyout one will error cause its not updated
