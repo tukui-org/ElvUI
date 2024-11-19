2fa03bf69cff5731f5d114c4cba7aa3a8b742fcc
Luckyone <Luckyone.Development@pm.me>
2024-11-19 21:11:12 +0100

    Gamble

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -387,6 +387,8 @@ do --this can save some main file locals
 		end
 	elseif portal == 'EU' then
 		if E.Classic then
+			-- Luckyone Era Anniversary
+			z["Luckyone-Fengus' Ferocity"] = ElvGreen -- [Horde] Hunter
 			-- Luckyone Seasonal (5827: Living Flame EU)
 			z['Player-5827-0273D732']	= ElvGreen -- [Alliance] Hunter
 			z['Player-5827-0273D63E']	= ElvGreen -- [Alliance] Paladin
