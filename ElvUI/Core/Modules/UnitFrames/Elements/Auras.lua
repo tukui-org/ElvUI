3dc265247592b044c5a075263cf654c19a2bb7f1
Simpy <kodi03466@hotmail.com>
2024-11-24 22:24:40 -0500

    messed this part up

--- ElvUI/Core/Modules/UnitFrames/Elements/Auras.lua
+++ ElvUI/Core/Modules/UnitFrames/Elements/Auras.lua
@@ -664,25 +664,20 @@ function UF:AuraFilter(unit, button, name, icon, count, debuffType, duration, ex
 	end
 
 	local noDuration, allowDuration = UF:AuraDuration(db, duration)
-	if not self.filterList then
+	if not allowDuration or not self.filterList then
 		button.filterPass = nil
 		button.priority = 0
 
 		return allowDuration -- Allow all auras to be shown when the filter list is empty, while obeying duration sliders
-	elseif allowDuration then
-		if button.filterPass and UF:AuraUnchanged(button.auraInfo, name, icon, count, debuffType, duration, expiration, source, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll) then
-			return button.filterPass
-		else
-			local myPet, otherPet, canDispel, isFriend, unitIsCaster = UF:AuraPopulate(db, unit, button, name, icon, count, debuffType, duration, expiration, source, isStealable, spellID)
-			local pass, priority = UF:CheckFilter(source, name, spellID, canDispel, isFriend, button.isPlayer, unitIsCaster, myPet, otherPet, isBossDebuff, noDuration, castByPlayer, nameplateShowAll or (nameplateShowPersonal and (button.isPlayer or myPet)), E.MountIDs[spellID], self.filterList)
-
-			button.filterPass = pass
-			button.priority = priority or 0 -- this is the only difference from auarbars code
-
-			return pass
-		end
+	elseif UF:AuraUnchanged(button.auraInfo, name, icon, count, debuffType, duration, expiration, source, isStealable, nameplateShowPersonal, spellID, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll) then
+		return button.filterPass
 	else
-		button.filterPass = nil
-		button.priority = 0
+		local myPet, otherPet, canDispel, isFriend, unitIsCaster = UF:AuraPopulate(db, unit, button, name, icon, count, debuffType, duration, expiration, source, isStealable, spellID)
+		local pass, priority = UF:CheckFilter(source, name, spellID, canDispel, isFriend, button.isPlayer, unitIsCaster, myPet, otherPet, isBossDebuff, noDuration, castByPlayer, nameplateShowAll or (nameplateShowPersonal and (button.isPlayer or myPet)), E.MountIDs[spellID], self.filterList)
+
+		button.filterPass = pass
+		button.priority = priority or 0 -- This is the only difference from auarbars code
+
+		return pass
 	end
 end
