7951d63077d25e17338c8e4eb95ae9002f887920
Simpy <kodi03466@hotmail.com>
2025-03-02 19:53:15 -0500

    missed this at some point?

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -2071,10 +2071,8 @@ function CH:ChatFrame_MessageEventHandler(frame, event, arg1, arg2, arg3, arg4, 
 		local channelLength = strlen(arg4)
 		local infoType = chatType
 
-		if chatType == 'VOICE_TEXT' then -- the code here looks weird but its how blizzard has it ~Simpy
-			local leader = UnitIsGroupLeader(arg2)
-			infoType, chatType = _G.VoiceTranscription_DetermineChatTypeVoiceTranscription_DetermineChatType(leader)
-			info = _G.ChatTypeInfo[infoType]
+		if chatType == 'VOICE_TEXT' and not GetCVarBool('speechToText') then
+			return
 		elseif chatType == 'COMMUNITIES_CHANNEL' or ((strsub(chatType, 1, 7) == 'CHANNEL') and (chatType ~= 'CHANNEL_LIST') and ((arg1 ~= 'INVITE') or (chatType ~= 'CHANNEL_NOTICE_USER'))) then
 			if arg1 == 'WRONG_PASSWORD' then
 				local _, popup = _G.StaticPopup_Visible('CHAT_CHANNEL_PASSWORD')
@@ -2330,6 +2328,13 @@ function CH:ChatFrame_MessageEventHandler(frame, event, arg1, arg2, arg3, arg4, 
 		end
 
 		return true
+	elseif event == 'VOICE_CHAT_CHANNEL_TRANSCRIBING_CHANGED' then
+		if not frame.isTranscribing and arg2 then -- arg1 is channelID, arg2 is isNowTranscribing
+			local info = _G.ChatTypeInfo.SYSTEM
+			frame:AddMessage(_G.SPEECH_TO_TEXT_STARTED, info.r, info.g, info.b, info.id, nil, nil, nil, nil, nil, isHistory, historyTime)
+		end
+
+		frame.isTranscribing = arg2
 	end
 end
 
