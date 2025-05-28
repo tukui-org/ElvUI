8aca676ae5d32f290e20389d38c34acb7f07c042
Simpy <kodi03466@hotmail.com>
2025-05-28 01:33:33 -0400

    these API dont exist yet?

--- ElvUI/Core/Modules/Tooltip/Tooltip.lua
+++ ElvUI/Core/Modules/Tooltip/Tooltip.lua
@@ -299,14 +299,14 @@ function TT:SetUnitText(tt, unit, isPlayerUnit)
 
 		return nameColor
 	else
-		local isPetCompanion = (E.Retail or E.Mists) and UnitIsBattlePetCompanion(unit)
+		local isPetCompanion = E.Retail and UnitIsBattlePetCompanion(unit)
 		local levelLine, classLine = TT:GetLevelLine(tt, 1)
 		if levelLine then
 			local pvpFlag, classificationString, diffColor, level = '', ''
 			local creatureClassification = UnitClassification(unit)
 			local creatureType = UnitCreatureType(unit)
 
-			if isPetCompanion or ((E.Retail or E.Mists) and UnitIsWildBattlePet(unit)) then
+			if isPetCompanion or (E.Retail and UnitIsWildBattlePet(unit)) then
 				level = UnitBattlePetLevel(unit)
 
 				local petType = UnitBattlePetType(unit)
