30a4044c36f8fbd6b180ace35d7d2d2fb60308e6
Luckyone <Luckyone.Development@pm.me>
2025-06-29 00:07:38 +0200

    Taxi skin

--- ElvUI/Mists/Modules/Skins/Taxi.lua
+++ ElvUI/Mists/Modules/Skins/Taxi.lua
@@ -6,14 +6,7 @@ local _G = _G
 function S:TaxiFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.taxi) then return end
 
-	local TaxiFrame = _G.TaxiFrame
-	S:HandleFrame(TaxiFrame, true, nil, 11, -12, -32, 76)
-	_G.TaxiPortrait:Kill()
-	_G.TaxiMap:PointXY(-11, -71)
-	_G.TaxiRouteMap:PointXY(-11, -71)
-	_G.TaxiMerchant:SetTextColor(1, 1, 1)
-
-	S:HandleCloseButton(_G.TaxiCloseButton, TaxiFrame.backdrop)
+	S:HandleFrame(_G.TaxiFrame)
 end
 
 S:AddCallback('TaxiFrame')
