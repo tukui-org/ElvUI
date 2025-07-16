6dfffca7e454a8968bd767170d9107c885b80c2d
Repooc <tjcooper1982@gmail.com>
2025-07-15 20:45:08 -0400

    format this to be cleaner i suppose

--- ElvUI/Mists/Filters/Filters.lua
+++ ElvUI/Mists/Filters/Filters.lua
@@ -627,52 +627,52 @@ G.unitframe.aurawatch = {
 -- List of spells to display ticks
 G.unitframe.ChannelTicks = {
 	-- Warlock
-	[1120]	= 5, -- Drain Soul
-	[689]	= 5, -- Drain Life
-	[5740]	= 4, -- Rain of Fire
-	[755]	= 10, -- Health Funnel
-	[79268]	= 3, -- Soul Harvest
-	[1949]	= 15, -- Hellfire
-	[103103] = 4, -- Malefic Grasp
+	[1120]		= 5, -- Drain Soul
+	[689]		= 5, -- Drain Life
+	[5740]		= 4, -- Rain of Fire
+	[755]		= 10, -- Health Funnel
+	[79268]		= 3, -- Soul Harvest
+	[1949]		= 15, -- Hellfire
+	[103103] 	= 4, -- Malefic Grasp
 	-- Druid
-	[44203]	= 4, -- Tranquility
-	[16914]	= 10, -- Hurricane
+	[44203]		= 4, -- Tranquility
+	[16914]		= 10, -- Hurricane
 	-- Priest
-	[15407]	= 3, -- Mind Flay
-	[129197] = 3, -- Mind Flay (Insanity)
-	[48045]	= 5, -- Mind Sear
-	[47540]	= 3, -- Penance
-	[64901]	= 4, -- Hymn of Hope
-	[64843]	= 4, -- Divine Hymn
+	[15407]		= 3, -- Mind Flay
+	[129197] 	= 3, -- Mind Flay (Insanity)
+	[48045]		= 5, -- Mind Sear
+	[47540]		= 3, -- Penance
+	[64901]		= 4, -- Hymn of Hope
+	[64843]		= 4, -- Divine Hymn
 	-- Mage
-	[5143]	= 5, -- Arcane Missiles
-	[10]	= 8, -- Blizzard
-	[12051]	= 4, -- Evocation
+	[5143]		= 5, -- Arcane Missiles
+	[10]		= 8, -- Blizzard
+	[12051]		= 4, -- Evocation
 	-- Death Knight
-	[42650]	= 8, -- Army of the Dead
+	[42650]		= 8, -- Army of the Dead
 	-- First Aid
-	[102695]    = 8, -- Heavy Windwool Bandage
-    [102694]    = 8, -- Windwool Bandage
-    [74555]    = 8, -- Dense Embersilk Bandage
-    [74554]    = 8, -- Heavy Embersilk Bandage
-    [74553]    = 8, -- Embersilk Bandage
-	[45544]	= 8, -- Heavy Frostweave Bandage
-	[45543]	= 8, -- Frostweave Bandage
-	[27031]	= 8, -- Heavy Netherweave Bandage
-	[27030]	= 8, -- Netherweave Bandage
-	[23567]	= 8, -- Warsong Gulch Runecloth Bandage
-	[23696]	= 8, -- Alterac Heavy Runecloth Bandage
-	[24414]	= 8, -- Arathi Basin Runecloth Bandage
-	[18610]	= 8, -- Heavy Runecloth Bandage
-	[18608]	= 8, -- Runecloth Bandage
-	[10839]	= 8, -- Heavy Mageweave Bandage
-	[10838]	= 8, -- Mageweave Bandage
-	[7927]	= 8, -- Heavy Silk Bandage
-	[7926]	= 8, -- Silk Bandage
-	[3268]	= 7, -- Heavy Wool Bandage
-	[3267]	= 7, -- Wool Bandage
-	[1159]	= 6, -- Heavy Linen Bandage
-	[746]	= 6 -- Linen Bandage
+	[102695] 	= 8, -- Heavy Windwool Bandage
+    [102694] 	= 8, -- Windwool Bandage
+    [74555] 	= 8, -- Dense Embersilk Bandage
+    [74554] 	= 8, -- Heavy Embersilk Bandage
+    [74553]		= 8, -- Embersilk Bandage
+	[45544]		= 8, -- Heavy Frostweave Bandage
+	[45543]		= 8, -- Frostweave Bandage
+	[27031]		= 8, -- Heavy Netherweave Bandage
+	[27030]		= 8, -- Netherweave Bandage
+	[23567]		= 8, -- Warsong Gulch Runecloth Bandage
+	[23696]		= 8, -- Alterac Heavy Runecloth Bandage
+	[24414]		= 8, -- Arathi Basin Runecloth Bandage
+	[18610]		= 8, -- Heavy Runecloth Bandage
+	[18608]		= 8, -- Runecloth Bandage
+	[10839]		= 8, -- Heavy Mageweave Bandage
+	[10838]		= 8, -- Mageweave Bandage
+	[7927]		= 8, -- Heavy Silk Bandage
+	[7926]		= 8, -- Silk Bandage
+	[3268]		= 7, -- Heavy Wool Bandage
+	[3267]		= 7, -- Wool Bandage
+	[1159]		= 6, -- Heavy Linen Bandage
+	[746]		= 6 -- Linen Bandage
 }
 
 -- Spells that chain, second step
