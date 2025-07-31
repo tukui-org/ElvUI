d69a76a3ac36702d45b88932aa819aa3f77d0669
Merathilis <neo0608@gmail.com>
2025-07-31 12:26:10 +0200

    Update LoseOfControl Frame (untested)

--- ElvUI/Mainline/Modules/Skins/LossControl.lua
+++ ElvUI/Mainline/Modules/Skins/LossControl.lua
@@ -5,6 +5,7 @@ local _G = _G
 local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 
+-- check this on 11.2
 function S:LossOfControlFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.losscontrol) then return end
 
@@ -20,28 +21,28 @@ function S:LossOfControlFrame()
 	LossOfControlFrame.AbilityName:ClearAllPoints()
 	LossOfControlFrame:Size(LossOfControlFrame.Icon:GetWidth() + 50)
 
-	hooksecurefunc('LossOfControlFrame_SetUpDisplay', function(frame)
-		frame.Icon:ClearAllPoints()
-		frame.Icon:Point('CENTER', frame, 'CENTER', 0, 0)
+	hooksecurefunc(_G.LossOfControlMixin, 'SetUpDisplay', function(animate, data)
+		self.Icon:ClearAllPoints()
+		self.Icon:Point('CENTER', self, 'CENTER', 0, 0)
 
-		frame.AbilityName:ClearAllPoints()
-		frame.AbilityName:Point('BOTTOM', frame, 0, -28)
-		frame.AbilityName.scrollTime = nil
-		frame.AbilityName:FontTemplate(nil, 20, 'OUTLINE')
+		self.AbilityName:ClearAllPoints()
+		self.AbilityName:Point('BOTTOM', self, 0, -28)
+		self.AbilityName.scrollTime = nil
+		self.AbilityName:FontTemplate(nil, 20, 'OUTLINE')
 
-		frame.TimeLeft.NumberText:ClearAllPoints()
-		frame.TimeLeft.NumberText:Point('BOTTOM', frame, 4, -58)
-		frame.TimeLeft.NumberText.scrollTime = nil
-		frame.TimeLeft.NumberText:FontTemplate(nil, 20, 'OUTLINE')
+		self.TimeLeft.NumberText:ClearAllPoints()
+		self.TimeLeft.NumberText:Point('BOTTOM', self, 4, -58)
+		self.TimeLeft.NumberText.scrollTime = nil
+		self.TimeLeft.NumberText:FontTemplate(nil, 20, 'OUTLINE')
 
-		frame.TimeLeft.SecondsText:ClearAllPoints()
-		frame.TimeLeft.SecondsText:Point('BOTTOM', frame, 0, -80)
-		frame.TimeLeft.SecondsText.scrollTime = nil
-		frame.TimeLeft.SecondsText:FontTemplate(nil, 20, 'OUTLINE')
+		self.TimeLeft.SecondsText:ClearAllPoints()
+		self.TimeLeft.SecondsText:Point('BOTTOM', self, 0, -80)
+		self.TimeLeft.SecondsText.scrollTime = nil
+		self.TimeLeft.SecondsText:FontTemplate(nil, 20, 'OUTLINE')
 
 		-- always stop shake animation on start
-		if frame.Anim:IsPlaying() then
-			frame.Anim:Stop()
+		if self.Anim:IsPlaying() then
+			self.Anim:Stop()
 		end
 	end)
 end
