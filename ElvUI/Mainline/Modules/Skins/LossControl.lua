182e5561c92669be0239ae2f5eebbad46107c4a1
Simpy <kodi03466@hotmail.com>
2025-08-04 15:46:44 -0400

    make it a local function

--- ElvUI/Mainline/Modules/Skins/LossControl.lua
+++ ElvUI/Mainline/Modules/Skins/LossControl.lua
@@ -5,7 +5,36 @@ local _G = _G
 local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 
--- check this on 11.2
+local function SetupDisplay(frame)
+	if frame.Icon then
+		frame.Icon:ClearAllPoints()
+		frame.Icon:Point('CENTER', frame, 'CENTER', 0, 0)
+	end
+
+	if frame.AbilityName then
+		frame.AbilityName:ClearAllPoints()
+		frame.AbilityName:Point('BOTTOM', frame, 0, -28)
+		frame.AbilityName:FontTemplate(nil, 20, 'OUTLINE')
+		frame.AbilityName.scrollTime = nil -- huh?
+	end
+
+	if frame.TimeLeft then
+		frame.TimeLeft.NumberText:ClearAllPoints()
+		frame.TimeLeft.NumberText:Point('BOTTOM', frame, 4, -58)
+		frame.TimeLeft.NumberText:FontTemplate(nil, 20, 'OUTLINE')
+		frame.TimeLeft.NumberText.scrollTime = nil -- huh?
+
+		frame.TimeLeft.SecondsText:ClearAllPoints()
+		frame.TimeLeft.SecondsText:Point('BOTTOM', frame, 0, -80)
+		frame.TimeLeft.SecondsText:FontTemplate(nil, 20, 'OUTLINE')
+		frame.TimeLeft.SecondsText.scrollTime = nil -- huh?
+	end
+
+	-- always stop shake animation on start
+	if frame.Anim:IsPlaying() then
+		frame.Anim:Stop()
+	end
+end
 function S:LossOfControlFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.losscontrol) then return end
 
@@ -21,36 +50,7 @@ function S:LossOfControlFrame()
 	LossOfControlFrame.AbilityName:ClearAllPoints()
 	LossOfControlFrame:Size(LossOfControlFrame.Icon:GetWidth() + 50)
 
-	hooksecurefunc(LossOfControlFrame, 'SetUpDisplay', function(frame)
-		if not frame then return end
-
-		frame.Icon:ClearAllPoints()
-		frame.Icon:Point('CENTER', frame, 'CENTER', 0, 0)
-
-		if frame.AbilityName then
-			frame.AbilityName:ClearAllPoints()
-			frame.AbilityName:Point('BOTTOM', frame, 0, -28)
-			frame.AbilityName:FontTemplate(nil, 20, 'OUTLINE')
-			frame.AbilityName.scrollTime = nil -- huh?
-		end
-
-		if frame.TimeLeft then
-			frame.TimeLeft.NumberText:ClearAllPoints()
-			frame.TimeLeft.NumberText:Point('BOTTOM', frame, 4, -58)
-			frame.TimeLeft.NumberText:FontTemplate(nil, 20, 'OUTLINE')
-			frame.TimeLeft.NumberText.scrollTime = nil -- huh?
-
-			frame.TimeLeft.SecondsText:ClearAllPoints()
-			frame.TimeLeft.SecondsText:Point('BOTTOM', frame, 0, -80)
-			frame.TimeLeft.SecondsText:FontTemplate(nil, 20, 'OUTLINE')
-			frame.TimeLeft.SecondsText.scrollTime = nil -- huh?
-		end
-
-		-- always stop shake animation on start
-		if frame.Anim:IsPlaying() then
-			frame.Anim:Stop()
-		end
-	end)
+	hooksecurefunc(LossOfControlFrame, 'SetUpDisplay', SetupDisplay)
 end
 
 S:AddCallback('LossOfControlFrame')
