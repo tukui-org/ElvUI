90bb4a8d9b6b99ecc325ee134c29e07b8f880707
Simpy <kodi03466@hotmail.com>
2025-08-06 03:22:03 -0400

    13.95

--- ElvUI/Core/init.lua
+++ ElvUI/Core/init.lua
@@ -128,7 +128,7 @@ end
 function E:ParseVersionString(addon)
 	local version = GetAddOnMetadata(addon, 'Version')
 	if strfind(version, 'project%-version') then
-		return 13.94, '13.94-git', nil, true
+		return 13.95, '13.95-git', nil, true
 	else
 		local release, extra = strmatch(version, '^v?([%d.]+)(.*)')
 		return tonumber(release), release..extra, extra ~= ''
--- ElvUI_Libraries/Core/LibActionButton-1.0/LibActionButton-1.0.lua
+++ ElvUI_Libraries/Core/LibActionButton-1.0/LibActionButton-1.0.lua
@@ -1,7 +1,7 @@
 -- License: LICENSE.txt
 
 local MAJOR_VERSION = "LibActionButton-1.0-ElvUI"
-local MINOR_VERSION = 65 -- the real minor version is 126
+local MINOR_VERSION = 66 -- the real minor version is 126
 
 local LibStub = LibStub
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
