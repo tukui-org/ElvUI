25ca0b521b8d543e0a840307b3a6dcc00e485b15
Simpy <kodi03466@hotmail.com>
2025-08-24 05:56:37 -0400

    exists on all now

--- ElvUI_Libraries/Core/oUF_Plugins/oUF_AuraWatch.lua
+++ ElvUI_Libraries/Core/oUF_Plugins/oUF_AuraWatch.lua
@@ -9,7 +9,7 @@ local VISIBLE = 1
 local HIDDEN = 0
 
 local min, next, wipe, pairs, tinsert = min, next, wipe, pairs, tinsert
-local GetSpellTexture = C_Spell.GetSpellTexture or GetSpellTexture
+local GetSpellTexture = C_Spell.GetSpellTexture
 local UnpackAuraData = AuraUtil.UnpackAuraData
 local CreateFrame = CreateFrame
 local UnitIsUnit = UnitIsUnit
