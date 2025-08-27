c746ed46f2705fb4f9198e466a89cb35062970f8
Simpy <kodi03466@hotmail.com>
2025-08-27 00:12:04 -0400

    forgot to bump these

--- ElvUI_Libraries/Core/Ace3-ElvUI/AceGUIWidget-MultiLineEditBox-ElvUI.lua
+++ ElvUI_Libraries/Core/Ace3-ElvUI/AceGUIWidget-MultiLineEditBox-ElvUI.lua
@@ -1,7 +1,7 @@
 --[[-----------------------------------------------------------------------------
 MultiLineEditBox Widget (Modified to add Syntax highlighting from FAIAP)
 -------------------------------------------------------------------------------]]
-local Type, Version = "MultiLineEditBox-ElvUI", 35
+local Type, Version = "MultiLineEditBox-ElvUI", 36
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
