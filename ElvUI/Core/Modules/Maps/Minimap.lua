709c526f011743f7e1735520e32aa3fc6d77c6d6
Simpy <kodi03466@hotmail.com>
2025-09-11 11:00:12 -0400

    Revert "try it on this one only"
    This reverts commit b302415e12ebb2e01c43fd8d2e1f1ae0a40940f1.

--- ElvUI/Core/Modules/Maps/Minimap.lua
+++ ElvUI/Core/Modules/Maps/Minimap.lua
@@ -746,7 +746,6 @@ function M:Initialize()
 
 	if E.Retail then
 		MinimapCluster:KillEditMode()
-		MinimapCluster.IsInDefaultPosition = E.noop
 
 		local clusterHolder = CreateFrame('Frame', 'ElvUI_MinimapClusterHolder', MinimapCluster)
 		clusterHolder.savedWidth, clusterHolder.savedHeight = MinimapCluster:GetSize()
