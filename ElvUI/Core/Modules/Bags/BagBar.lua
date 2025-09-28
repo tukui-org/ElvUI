6773a5ab42480238f402b7d26f3ad2d82b2c4d16
Simpy <kodi03466@hotmail.com>
2025-09-27 22:18:26 -0400

    Revert "azil said prob dont need this now"
    This reverts commit 85656f92a0d3f7c05df307cfffb9cde92cc77078.

--- ElvUI/Core/Modules/Bags/BagBar.lua
+++ ElvUI/Core/Modules/Bags/BagBar.lua
@@ -108,6 +108,11 @@ function B:SkinBag(bag)
 		bag:GetNormalTexture():SetAlpha(0)
 		bag:GetHighlightTexture():SetAlpha(0)
 		bag.CircleMask:Hide()
+
+		if icon then -- needed for retail
+			icon.Show = nil
+			icon:Show()
+		end
 	end
 
 	if icon then
