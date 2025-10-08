3b4a4d40763dbc03097c031951ee81b143607392
Merathilis <neo0608@gmail.com>
2025-10-08 15:23:12 +0200

    Fix the formating before Simpy sees it

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -479,7 +479,7 @@ do --this can save some main file locals
 			z['Player-1401-04217BB2']	= ElvPurple	-- [Alliance] Warlock:	Asragoth
 			z['Player-1401-0421EB9F']	= ElvBlue	-- [Alliance] Warrior:	Brítt
 			z['Player-1401-0421F909']	= ElvRed	-- [Alliance] Paladin:	Damará
-			z['Player-1401-0B12A2DC']	= ElvRed --	 . [Alliance] Paladin:	Damara/Legion Remix
+			z['Player-1401-0B12A2DC']	= ElvRed	-- [Alliance] Paladin:	Damara/Legion Remix
 			z['Player-1401-0421EC36']	= ElvBlue	-- [Alliance] Priest:	Jazira
 			z['Player-1401-0A9B0131']	= ElvYellow	-- [Alliance] Rogue:	Anonia
 			z['Player-1401-041E4D64']	= ElvGreen	-- [Alliance] Monk:		Maithilis
