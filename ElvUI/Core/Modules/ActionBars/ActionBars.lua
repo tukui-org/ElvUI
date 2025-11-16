c61387cf91945b38dbc4e6ee14c9572293f437ad
Simpy <kodi03466@hotmail.com>
2025-11-16 07:50:28 -0500

    s

--- ElvUI/Core/Modules/ActionBars/ActionBars.lua
+++ ElvUI/Core/Modules/ActionBars/ActionBars.lua
@@ -899,7 +899,7 @@ do
 
 		if (E.Retail and (canGlide or CanGlide() or IsPossessBarVisible() or HasOverrideActionBar()))
 		or UnitCastingInfo('player') or UnitChannelInfo('player') or UnitExists('target') or UnitExists('focus')
-		or UnitExists('vehicle') or UnitAffectingCombat('player') or (UnitHealth('player') ~= UnitHealthMax('player')) then
+		or UnitExists('vehicle') or UnitAffectingCombat('player') or (not E.Midnight and (UnitHealth('player') ~= UnitHealthMax('player'))) then
 			self.mouseLock = true
 			E:UIFrameFadeIn(self, 0.2, self:GetAlpha(), 1)
 			AB:FadeBlings(1)
