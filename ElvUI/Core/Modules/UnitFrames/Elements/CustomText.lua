1b4dca602abbf41b7921cb47bc67f5cb8e5bc8f7
Simpy <kodi03466@hotmail.com>
2025-12-02 18:22:09 -0500

    titan: classic only

--- ElvUI/Core/Modules/UnitFrames/Elements/CustomText.lua
+++ ElvUI/Core/Modules/UnitFrames/Elements/CustomText.lua
@@ -11,7 +11,7 @@ UF.CustomTextForceFallback = {
 
 -- these need to be hidden; remove the fallback
 UF.CustomTextAvoidFallback = {
-	EnergyManaRegen	= E.Wrath or E.Classic,
+	EnergyManaRegen	= E.Classic,
 	EclipseBar		= E.Mists,
 	AdditionalPower	= E.Mists,
 	Stagger			= E.Mists
