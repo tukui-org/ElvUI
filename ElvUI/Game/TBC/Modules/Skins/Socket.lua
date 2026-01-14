7f8c80aef101c8ff73d4d96c89fc259a728993bf
Simpy <kodi03466@hotmail.com>
2026-01-13 19:11:26 -0500

    skin: sync sockets file

--- ElvUI/Game/TBC/Modules/Skins/Socket.lua
+++ ElvUI/Game/TBC/Modules/Skins/Socket.lua
@@ -5,14 +5,14 @@ local _G = _G
 local next, unpack = next, unpack
 local hooksecurefunc = hooksecurefunc
 
-local C_ItemSocketInfo_GetSocketItemInfo = C_ItemSocketInfo.GetSocketItemInfo
+local C_ItemSocketInfo_GetSocketTypes = C_ItemSocketInfo.GetSocketTypes
 
 local function UpdateItemSocketing()
 	local SocketingContainer = _G.ItemSocketingFrame.SocketingContainer
 	if not SocketingContainer or not SocketingContainer.SocketFrames then return end
 
 	for i, socket in next, SocketingContainer.SocketFrames do
-		local gemColor = C_ItemSocketInfo_GetSocketItemInfo(i)
+		local gemColor = C_ItemSocketInfo_GetSocketTypes(i)
 		local color = E.GemTypeInfo[gemColor]
 		if color then
 			socket:SetBackdropBorderColor(color.r, color.g, color.b)
@@ -25,6 +25,7 @@ end
 
 function S:Blizzard_ItemSocketingUI()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.socket) then return end
+
 	local ItemSocketingFrame = _G.ItemSocketingFrame
 	S:HandlePortraitFrame(ItemSocketingFrame)
 
@@ -42,6 +43,10 @@ function S:Blizzard_ItemSocketingUI()
 			button:StripTextures()
 			button:StyleButton()
 			button:SetTemplate(nil, true)
+
+			if button.Shine then
+				button.Shine:Kill()
+			end
 
 			if button.BracketFrame then
 				button.BracketFrame:Kill()
