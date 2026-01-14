6881dc3c0278477be199737638100eb51d633ecc
Simpy <kodi03466@hotmail.com>
2026-01-14 17:41:53 -0500

    tbc: fix tooltips

--- ElvUI/Game/Shared/Modules/Tooltip/Tooltip.lua
+++ ElvUI/Game/Shared/Modules/Tooltip/Tooltip.lua
@@ -1107,7 +1107,7 @@ function TT:Initialize()
 		TT:SecureHook(GameTooltip, 'SetUnitDebuffByAuraInstanceID', 'SetUnitAuraByAuraInstanceID')
 	end
 
-	if AddTooltipPostCall and not (E.Wrath or E.Mists) then -- exists but doesn't work atm on Cata
+	if AddTooltipPostCall and not (E.TBC or E.Wrath or E.Mists) then -- exists but doesn't work atm on Cata
 		AddTooltipPostCall(TooltipDataType.Spell, TT.GameTooltip_OnTooltipSetSpell)
 		AddTooltipPostCall(TooltipDataType.Macro, TT.GameTooltip_OnTooltipSetSpell)
 		AddTooltipPostCall(TooltipDataType.Item, TT.GameTooltip_OnTooltipSetItem)
