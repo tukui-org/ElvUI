aed88bb992c24d5a95de266b7574f6442df6406f
Simpy <kodi03466@hotmail.com>
2026-01-19 21:11:23 -0500

    dispel colors

--- ElvUI/Game/Shared/Defaults/Profile.lua
+++ ElvUI/Game/Shared/Defaults/Profile.lua
@@ -94,7 +94,7 @@ P.general = {
 		PALADIN = { b = 0.72, g = 0.54, r = 0.95 }
 	},
 	debuffColors = { -- handle colors of LibDispel
-		None = { r = 0.8, g = 0, b = 0 },
+		None = { r = 0.9, g = 0.2, b = 0.2 },
 		Magic = { r = 0.2, g = 0.6, b = 1 },
 		Curse = { r = 0.6, g = 0, b = 1 },
 		Disease = { r = 0.6, g = 0.4, b = 0 },
@@ -103,7 +103,7 @@ P.general = {
 
 		-- These dont exist in Blizzards color table
 		Bleed = { r = 1, g = 0.2, b = 0.6 },
-		EnemyNPC = { r = 0.9, g = 0.1, b = 0.1 },
+		EnemyNPC = { r = 1, g = 0.85, b = 0.2 },
 		BadDispel = { r = 0.05, g = 0.85, b = 0.94 },
 		Stealable = { r = 0.93, g = 0.91, b = 0.55 },
 	},
