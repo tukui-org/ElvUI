e9033e40b6d4345b7ff04021df263793b403dd89
Simpy <kodi03466@hotmail.com>
2026-01-21 13:20:43 -0500

    oUF: fix display frames in elvui

--- ElvUI_Libraries/Game/Shared/oUF/elements/auras.lua
+++ ElvUI_Libraries/Game/Shared/oUF/elements/auras.lua
@@ -167,7 +167,7 @@ local function updateAura(frame, which, unit, aura, index, offset, filter, visib
 		name, _, icon = oUF:GetSpellInfo(spellId)
 
 		if forceShow then
-			applications, dispelName, duration, expirationTime, sourceUnit = 5, (which == 'Debuffs' and '') or 'Magic', 0, 60, 'player'
+			applications, dispelName, duration, expirationTime, sourceUnit = 5, (which == 'Debuffs' and 'None') or 'Magic', 0, 60, 'player'
 		end
 	end
 
