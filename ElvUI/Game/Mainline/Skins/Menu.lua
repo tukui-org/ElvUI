4408b06b8fcbf6814d23f11f5cd91a74cc03dd35
Simpy <kodi03466@hotmail.com>
2026-01-31 20:22:07 -0500

    menu skin: attempt to prevent secret error

--- ElvUI/Game/Mainline/Skins/Menu.lua
+++ ElvUI/Game/Mainline/Skins/Menu.lua
@@ -12,7 +12,6 @@ local function SkinFrame(frame)
 		frame.backdrop = backdrops[frame] -- relink it back
 	else
 		frame:CreateBackdrop('Transparent') -- :SetTemplate errors out
-		frame.backdrop:NudgePoint(nil, 8, nil, 'BOTTOMRIGHT')
 		backdrops[frame] = frame.backdrop -- keep below CreateBackdrop
 
 		if frame.ScrollBar then
@@ -22,6 +21,10 @@ local function SkinFrame(frame)
 
 	if frame.backdrop then
 		frame.backdrop:OffsetFrameLevel(nil, frame)
+
+		if E:NoSecretValues(frame) then
+			frame.backdrop:NudgePoint(nil, 8, nil, 'BOTTOMRIGHT')
+		end
 	end
 end
 
