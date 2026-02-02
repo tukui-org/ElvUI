25b5a72eeff4a5734523301f21919c1d3ac31fea
Simpy <kodi03466@hotmail.com>
2026-02-01 19:48:31 -0500

    options: unload filter file on mainline

--- ElvUI_Options/Game/Shared/Filters.lua
+++ ElvUI_Options/Game/Shared/Filters.lua
@@ -1,10 +1,5 @@
 local E, _, V, P, G = unpack(ElvUI)
 
--- Legacy filter lists are not available for Retail
-if E.Retail then
-	return
-end
-
 local C, L = unpack(E.Config)
 local UF = E:GetModule('UnitFrames')
 local ACH = E.Libs.ACH
