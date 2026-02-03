923168996a9905b0dbff8f474f5d6dab5d0b3337
Simpy <kodi03466@hotmail.com>
2026-02-03 18:42:25 -0500

    wrath: copy tbc arena skin over

--- ElvUI/Game/Wrath/Skins/Arena.lua
+++ ElvUI/Game/Wrath/Skins/Arena.lua
@@ -16,33 +16,35 @@ function S:SkinArena()
 	S:HandleButton(_G.ArenaFrameCancelButton)
 	S:HandleButton(_G.ArenaFrameJoinButton)
 	S:HandleButton(_G.ArenaFrameGroupJoinButton)
+	S:HandleCloseButton(_G.ArenaFrameCloseButton)
 
 	_G.ArenaFrameZoneDescription:SetTextColor(1, 1, 1)
-
 	_G.ArenaFrameNameHeader:Point('TOPLEFT', _G.ArenaZone1, 'TOPLEFT', 8, 24)
 	_G.ArenaFrameGroupJoinButton:Point('RIGHT', _G.ArenaFrameJoinButton, 'LEFT', -2, 0)
 
-	local backdrop_level = ArenaFrame.backdrop:GetFrameLevel()
-
 	-- Custom Backdrop 1
-	local topBackdrop = CreateFrame('Frame', nil, ArenaFrame)
-	topBackdrop:SetTemplate('Transparent')
-	topBackdrop:Height(112)
-	topBackdrop:Width(330)
-	topBackdrop:Point('TOP', ArenaFrame, 'TOP', -12, -48)
-	topBackdrop:SetFrameLevel(backdrop_level)
-	ArenaFrame.TopBackdrop = topBackdrop
+	if not ArenaFrame.TopBackdrop then
+		local topBackdrop = CreateFrame('Frame', nil, ArenaFrame)
+		topBackdrop:SetTemplate('Transparent')
+		topBackdrop:Height(112)
+		topBackdrop:Width(330)
+		topBackdrop:Point('TOP', ArenaFrame, 'TOP', -12, -48)
+		topBackdrop:OffsetFrameLevel(nil, ArenaFrame.backdrop)
+
+		ArenaFrame.TopBackdrop = topBackdrop
+	end
 
 	-- Custom Backdrop 2
-	local bottomBackdrop = CreateFrame('Frame', nil, ArenaFrame)
-	bottomBackdrop:SetTemplate('Transparent')
-	bottomBackdrop:Height(240)
-	bottomBackdrop:Width(330)
-	bottomBackdrop:Point('BOTTOM', ArenaFrame, 'BOTTOM', -12, 108)
-	bottomBackdrop:SetFrameLevel(backdrop_level)
-	ArenaFrame.BottomBackdrop = bottomBackdrop
+	if not ArenaFrame.BottomBackdrop then
+		local bottomBackdrop = CreateFrame('Frame', nil, ArenaFrame)
+		bottomBackdrop:SetTemplate('Transparent')
+		bottomBackdrop:Height(240)
+		bottomBackdrop:Width(330)
+		bottomBackdrop:Point('BOTTOM', ArenaFrame, 'BOTTOM', -12, 108)
+		bottomBackdrop:OffsetFrameLevel(nil, ArenaFrame.backdrop)
 
-	S:HandleCloseButton(_G.ArenaFrameCloseButton)
+		ArenaFrame.BottomBackdrop = bottomBackdrop
+	end
 end
 
 S:AddCallback('SkinArena')
