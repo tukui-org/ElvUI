1dc9deec4dc055401e2d7307e95dfd51b2a77ac7
Simpy <kodi03466@hotmail.com>
2026-03-11 19:47:28 -0400

    UF: also check the other priest/shaman specs if we need to

--- ElvUI/Game/Shared/Modules/UnitFrames/Elements/ClassBars.lua
+++ ElvUI/Game/Shared/Modules/UnitFrames/Elements/ClassBars.lua
@@ -352,8 +352,9 @@ function UF:Configure_ClassBar(frame)
 			local additional = powerType == 'AdditionalPower'
 			local classpower = powerType == 'ClassPower'
 			if additional or classpower then
-				local normal, special = classpower and not (checkPriest or checkShaman), classpower and (allowPriest or allowShaman)
-				local allowed = activeBar and (normal or ((special or additional) and UF:ClassPower_ShouldShowAdditionalPower()))
+				local special = allowPriest or allowShaman
+				local normal = not (checkPriest or checkShaman) or not special
+				local allowed = classpower and activeBar and (normal or ((special or additional) and UF:ClassPower_ShouldShowAdditionalPower()))
 				if allowed and not enabled then
 					frame:EnableElement(powerType)
 				elseif enabled and not allowed then
