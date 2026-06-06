454484850185e530acccfe38aea3a72eb019c89d
Simpy <kodi03466@hotmail.com>
2026-06-05 22:47:54 -0400

    15.16

--- ElvUI_Libraries/Game/Shared/LibActionButton-1.0/LibActionButton-1.0.lua
+++ ElvUI_Libraries/Game/Shared/LibActionButton-1.0/LibActionButton-1.0.lua
@@ -1,7 +1,7 @@
 -- License: LICENSE.txt
 
 local MAJOR_VERSION = "LibActionButton-1.0-ElvUI"
-local MINOR_VERSION = 78 -- the real minor version is 147
+local MINOR_VERSION = 79 -- the real minor version is 147
 
 local LibStub = LibStub
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
