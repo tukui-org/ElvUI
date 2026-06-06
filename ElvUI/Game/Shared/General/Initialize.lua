b5b8cf49c8c353b3121d3bf6f191e4d5d87baa85
kodewdle <kodewdle@users.noreply.github.com>
2026-06-06 02:52:13 +0000

    CI: Git version bump

--- ElvUI/Game/Shared/General/Initialize.lua
+++ ElvUI/Game/Shared/General/Initialize.lua
@@ -144,7 +144,7 @@ end
 function E:ParseVersionString(addon)
 	local version = GetAddOnMetadata(addon, 'Version')
 	if strfind(version, 'project%-version') then
-		return 15.15, '15.15-git', nil, true
+		return 15.16, '15.16-git', nil, true
 	else
 		local release, extra = strmatch(version, '^v?([%d.]+)(.*)')
 		return tonumber(release), release..extra, extra ~= ''
