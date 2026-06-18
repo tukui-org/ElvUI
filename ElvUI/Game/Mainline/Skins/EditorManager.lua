9145eea66fb6da0494650d927d6e02f3d2811595
Simpy <2266754+kodewdle@users.noreply.github.com>
2026-06-17 20:56:06 -0400

    Update EditorManager.lua

--- ElvUI/Game/Mainline/Skins/EditorManager.lua
+++ ElvUI/Game/Mainline/Skins/EditorManager.lua
@@ -35,15 +35,15 @@ local function HandleDialogs()
 	end
 
 	for _, frame in next, { dialog.Settings:GetChildren() } do
-		local checkbox = frame.Button
-		if checkbox and not checkbox.backdrop then
-			HandleCheckBox(checkbox)
-		end
-
 		local dropdown = frame.Dropdown
 		if dropdown and not dropdown.IsSkinned then
 			S:HandleDropDownBox(dropdown, 250)
 			dropdown.IsSkinned = true
+		end
+
+		local checkbox = frame.Button
+		if checkbox and not checkbox.backdrop then
+			HandleCheckBox(checkbox)
 		end
 	end
 end
