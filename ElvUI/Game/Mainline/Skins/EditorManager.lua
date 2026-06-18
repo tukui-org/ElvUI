ed2607b28c04b8c2d1b20c82ed1725e4d500626a
Daenarys <androidwoz@gmail.com>
2026-06-18 02:57:50 +0200

    fix: editmode settings dropdowns not skinned (#1860)

--- ElvUI/Game/Mainline/Skins/EditorManager.lua
+++ ElvUI/Game/Mainline/Skins/EditorManager.lua
@@ -35,10 +35,10 @@ local function HandleDialogs()
 	end
 
 	for _, frame in next, { dialog.Settings:GetChildren() } do
-		local dd = frame.Dropdown
-		if dd and (dd.DropDownMenu and not dd.IsSkinned) then
-			S:HandleDropDownBox(dd.DropDownMenu, 250)
-			dd.IsSkinned = true
+		local dropdown = frame.Dropdown
+		if dropdown and not dropdown.IsSkinned then
+			S:HandleDropDownBox(dropdown, 250)
+			dropdown.IsSkinned = true
 		end
 
 		local checkbox = frame.Button
