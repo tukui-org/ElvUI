5b1520df3d178f0e3bcc9612644bbb7af801c98f
Luckyone <Luckyone.Development@pm.me>
2026-06-24 11:11:00 +0200

    Restore Ace checkboxes - check back later

--- ElvUI/Game/Shared/Modules/Skins/Ace3.lua
+++ ElvUI/Game/Shared/Modules/Skins/Ace3.lua
@@ -142,7 +142,8 @@ function S:Ace3_SkinCheckBox(widget, check, checkbg, highlight)
 		hooksecurefunc(widget, 'SetType', S.Ace3_CheckBoxSetType)
 
 		if E.private.skins.checkBoxSkin then
-			S.Ace3_CheckBoxSetDesaturated(check, check:GetDesaturation())
+			local desat = (check.IsDesaturated and check:IsDesaturated()) or (check.GetDesaturation and check:GetDesaturation())
+			S.Ace3_CheckBoxSetDesaturated(check, desat)
 			hooksecurefunc(check, 'SetDesaturated', S.Ace3_CheckBoxSetDesaturated)
 
 			checkbg.backdrop:SetInside(widget.checkbg, 5, 5)
