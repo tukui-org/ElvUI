09a603ce890d50582b162acf2cb349c33b435b07
Simpy <kodi03466@hotmail.com>
2026-08-11 23:00:15 -0400

    oUF: adjustment for 8dd23e27c4

--- ElvUI_Libraries/Game/Shared/oUF/elements/tags.lua
+++ ElvUI_Libraries/Game/Shared/oUF/elements/tags.lua
@@ -416,14 +416,14 @@ end
 tagFunctions.raidcolor = function(u)
 	local _, classToken = UnitClass(u)
 	if oUF:NotSecretValue(classToken) and classToken then
-		return Hex(_COLORS.class[class])
+		return Hex(_COLORS.class[classToken])
 	else
 		local id = u:match('arena(%d)$')
-		if(id) then
-			local specID = GetArenaOpponentSpec(tonumber(id))
-			if(specID and specID > 0) then
-				_, _, _, _, _, class = GetSpecializationInfoByID(specID)
-				return Hex(_COLORS.class[class])
+		local specID = id and GetArenaOpponentSpec(tonumber(id))
+		if specID and specID > 0 then
+			local _, _, _, _, _, classSpec = GetSpecializationInfoByID(specID)
+			if oUF:NotSecretValue(classSpec) and classSpec then
+				return Hex(_COLORS.class[classSpec])
 			end
 		end
 	end
