e59380f6f092615d7d3fc5d2b55dda027caa8583
Simpy <kodi03466@hotmail.com>
2026-08-25 16:53:07 -0400

    skin: remove duplicate QuestRewardScrollFrame

--- ElvUI/Game/TBC/Skins/Quest.lua
+++ ElvUI/Game/TBC/Skins/Quest.lua
@@ -356,7 +356,6 @@ function S:BlizzardQuestFrames()
 		_G.QuestLogQuestCount,
 		_G.QuestProgressScrollFrame,
 		_G.QuestRewardScrollChildFrame,
-		_G.QuestRewardScrollFrame,
 		_G.QuestRewardScrollFrame
 	}
 	for _, object in pairs(QuestStrip) do
