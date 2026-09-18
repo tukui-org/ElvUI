3b4bda705acb050e8700bab28c87bae6ad925bb4
Simpy <kodi03466@hotmail.com>
2026-09-18 06:12:46 -0400

    skin: shard unneeded check

--- ElvUI/Game/Shared/Modules/Blizzard/Blizzard.lua
+++ ElvUI/Game/Shared/Modules/Blizzard/Blizzard.lua
@@ -237,8 +237,8 @@ function BL:Initialize()
 		BL:SecureHook(_G.TimeAlertFrame, 'SetPoint', 'RepositionFrame')
 	end
 
-	if E.Forever then -- Shard Transfer Frame
-		local ShardFrame = _G.ShardTransferImminentFrame
+	local ShardFrame = _G.ShardTransferImminentFrame
+	if ShardFrame then
 		ShardFrame:ClearAllPoints()
 		ShardFrame:Point('BOTTOMLEFT', _G.LeftChatPanel, 'TOPLEFT', 30, 10) -- x offset for the minimize button anchored to its left
 		E:CreateMover(ShardFrame, 'ShardTransferMover', L["Shard Transfer Frame"], nil, nil, PostMove)
