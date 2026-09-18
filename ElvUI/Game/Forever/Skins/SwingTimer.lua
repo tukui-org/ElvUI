b338783a636ebacf9293eccf3ca1e53bd1d56d98
Luckyone <Luckyone.Development@pm.me>
2026-09-18 18:05:16 +0200

    SwingTimer

--- ElvUI/Game/Forever/Skins/SwingTimer.lua
+++ ElvUI/Game/Forever/Skins/SwingTimer.lua
@@ -1,8 +1,25 @@
 local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
+local _G = _G
+local next = next
+
+-- SwingTimerFrameTemplate
+local function HandleSwingTimer(frame)
+	frame:StripTextures()
+
+	local bar = frame.StatusBar
+	S:HandleStatusBar(bar)
+	bar.TypeLabel:FontTemplate()
+	bar.TimeLabel:FontTemplate()
+end
+
 function S:Blizzard_SwingTimer()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.swingTimer) then return end
+
+	for _, frame in next, { _G.SwingTimerMainHandFrame, _G.SwingTimerOffHandFrame, _G.SwingTimerRangedFrame } do
+		HandleSwingTimer(frame)
+	end
 end
 
 S:AddCallbackForAddon('Blizzard_SwingTimer')
