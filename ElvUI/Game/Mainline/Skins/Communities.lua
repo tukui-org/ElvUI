7a80aed83e0b13c4da7e8f8431c35d6934ede1b9
Luckyone <Luckyone.Development@pm.me>
2026-09-20 00:20:12 +0200

    fix it for all game versions

--- ElvUI/Game/Mainline/Skins/Communities.lua
+++ ElvUI/Game/Mainline/Skins/Communities.lua
@@ -360,6 +360,7 @@ function S:Blizzard_Communities()
 
 	-- Filters Frame
 	local FiltersFrame = _G.CommunitiesGuildNewsFiltersFrame
+	FiltersFrame:StripTextures()
 	FiltersFrame:SetTemplate('Transparent')
 	S:HandleCheckBox(FiltersFrame.GuildAchievement)
 	S:HandleCheckBox(FiltersFrame.Achievement)
@@ -383,13 +384,8 @@ function S:Blizzard_Communities()
 	end)
 
 	if E.private.skins.parchmentRemoverEnable then
-		for _, frame in pairs({
-			GuildDetailsFrameInfo,
-			GuildDetailsFrameNews,
-			FiltersFrame,
-		}) do
-			frame:StripTextures()
-		end
+		GuildDetailsFrameInfo:StripTextures()
+		GuildDetailsFrameNews:StripTextures()
 
 		-- Guild Challenges Background
 		local backdrop1 = CreateFrame('Frame', nil, GuildDetailsFrameInfo)
--- ElvUI/Game/TBC/Skins/Communities.lua
+++ ElvUI/Game/TBC/Skins/Communities.lua
@@ -369,6 +369,7 @@ function S:Blizzard_Communities()
 
 	-- Filters Frame
 	local FiltersFrame = _G.CommunitiesGuildNewsFiltersFrame
+	FiltersFrame:StripTextures()
 	FiltersFrame:SetTemplate('Transparent')
 	S:HandleCheckBox(FiltersFrame.GuildAchievement)
 	S:HandleCheckBox(FiltersFrame.Achievement)
@@ -392,13 +393,8 @@ function S:Blizzard_Communities()
 	end)
 
 	if E.private.skins.parchmentRemoverEnable then
-		for _, frame in pairs({
-			GuildDetailsFrameInfo,
-			GuildDetailsFrameNews,
-			FiltersFrame,
-		}) do
-			frame:StripTextures()
-		end
+		GuildDetailsFrameInfo:StripTextures()
+		GuildDetailsFrameNews:StripTextures()
 
 		-- Guild Challenges Background
 		local backdrop1 = CreateFrame('Frame', nil, GuildDetailsFrameInfo)
