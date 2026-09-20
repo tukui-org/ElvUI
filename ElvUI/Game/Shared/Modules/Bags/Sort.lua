17cd47b68471e2945c07d1818ff9339061c78860
Simpy <kodi03466@hotmail.com>
2026-09-19 21:24:25 -0400

    forever: reagent bag

--- ElvUI/Game/Shared/Modules/Bags/Sort.lua
+++ ElvUI/Game/Shared/Modules/Bags/Sort.lua
@@ -21,7 +21,7 @@ local SplitGuildBankItem = SplitGuildBankItem
 local ITEMQUALITY_POOR = Enum.ItemQuality.Poor
 local NUM_BAG_SLOTS = NUM_BAG_SLOTS + (E.Modern and 1 or 0) -- add the profession bag
 local BANK_CONTAINER = Enum.BagIndex.Bank
-local REAGENT_CONTAINER = E.Modern and Enum.BagIndex.ReagentBag or math.huge
+local REAGENT_CONTAINER = E.Retail and Enum.BagIndex.ReagentBag or math.huge
 
 local BagSlotFlags = Enum.BagSlotFlags
 local FILTER_FLAG_TRADE_GOODS = LE_BAG_FILTER_FLAG_TRADE_GOODS or BagSlotFlags.PriorityTradeGoods or BagSlotFlags.ClassProfessionGoods
