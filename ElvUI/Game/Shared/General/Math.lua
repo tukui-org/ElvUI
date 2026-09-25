d96f4e6ae6808b4d13d346f689852b6e7d626823
Simpy <kodi03466@hotmail.com>
2026-09-25 18:45:18 -0400

    API: clean up GetFormattedText and make `GetFormattedNumber`

--- ElvUI/Game/Shared/General/Math.lua
+++ ElvUI/Game/Shared/General/Math.lua
@@ -316,20 +316,24 @@ function E:GetXYOffset(position, forcedX, forcedY)
 	end
 end
 
+function E:GetFormattedNumber(value, decimal, short)
+	return short and E:ShortValue(value, decimal) or BreakUpLargeNumbers(value)
+end
+
 function E:GetFormattedText(style, min, max, dec, short)
 	if max == 0 then max = 1 end
 
 	if style == 'CURRENT' or ((style == 'CURRENT_MAX' or style == 'CURRENT_MAX_PERCENT' or style == 'CURRENT_PERCENT') and min == max) then
-		return format(E.GetFormattedTextStyles.CURRENT, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min))
+		return E:GetFormattedNumber(min, dec, short) -- no need to format to CURRENT: %s
 	else
 		local useStyle = E.GetFormattedTextStyles[style]
 		if not useStyle then return end
 
 		if style == 'DEFICIT' then
 			local deficit = max - min
-			return (deficit > 0 and format(useStyle, short and E:ShortValue(deficit, dec) or BreakUpLargeNumbers(deficit))) or ''
+			return deficit > 0 and format(useStyle, E:GetFormattedNumber(deficit, dec, short)) or ''
 		elseif style == 'CURRENT_MAX' then
-			return format(useStyle, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min), short and E:ShortValue(max, dec) or BreakUpLargeNumbers(max))
+			return format(useStyle, E:GetFormattedNumber(min, dec, short), E:GetFormattedNumber(max, dec, short))
 		elseif style == 'PERCENT' or style == 'CURRENT_PERCENT' or style == 'CURRENT_MAX_PERCENT' then
 			if dec then useStyle = gsub(useStyle, '%d', tonumber(dec) or 0) end
 			local perc = min / max * 100
@@ -337,9 +341,9 @@ function E:GetFormattedText(style, min, max, dec, short)
 			if style == 'PERCENT' then
 				return format(useStyle, perc)
 			elseif style == 'CURRENT_PERCENT' then
-				return format(useStyle, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min), perc)
+				return format(useStyle, E:GetFormattedNumber(min, dec, short), perc)
 			elseif style == 'CURRENT_MAX_PERCENT' then
-				return format(useStyle, short and E:ShortValue(min, dec) or BreakUpLargeNumbers(min), short and E:ShortValue(max, dec) or BreakUpLargeNumbers(max), perc)
+				return format(useStyle, E:GetFormattedNumber(min, dec, short), E:GetFormattedNumber(max, dec, short), perc)
 			end
 		end
 	end
