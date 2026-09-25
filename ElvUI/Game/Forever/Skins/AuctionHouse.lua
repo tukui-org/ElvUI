58aa09a95afc33825e066a287ec912fd40aa620c
Simpy <kodi03466@hotmail.com>
2026-09-24 20:06:43 -0400

    skin: check

--- ElvUI/Game/Forever/Skins/AuctionHouse.lua
+++ ElvUI/Game/Forever/Skins/AuctionHouse.lua
@@ -4,7 +4,6 @@ local S = E:GetModule('Skins')
 local _G = _G
 local next, unpack = next, unpack
 local hooksecurefunc = hooksecurefunc
-
 local CreateFrame = CreateFrame
 
 -- Credits: siweia (AuroraClassic)
@@ -23,13 +22,14 @@ local function HandleSearchBarFrame(Frame)
 end
 
 local function HandleListIcon(frame)
-	if not frame.tableBuilder then return end
+	local builder = frame.tableBuilder
+	if not builder then return end -- RefreshScrollFrame runs before Init on hidden lists
 
 	for i = 1, 22 do
-		local row = frame.tableBuilder.rows[i]
+		local row = builder.rows[i]
 		if row then
 			for j = 1, 4 do
-				local cell = row.cells and row.cells[j]
+				local cell = row.cells[j]
 				if cell and cell.Icon then
 					if not cell.IsSkinned then
 						S:HandleIcon(cell.Icon)
