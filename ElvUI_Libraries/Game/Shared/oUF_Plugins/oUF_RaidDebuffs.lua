3ab5fef1bdedbdcb3946fa7cf9cf9c3ec340939f
Luckyone <Luckyone.Development@pm.me>
2026-09-26 15:04:51 +0200

    Brackets

--- ElvUI_Libraries/Game/Shared/oUF_Plugins/oUF_RaidDebuffs.lua
+++ ElvUI_Libraries/Game/Shared/oUF_Plugins/oUF_RaidDebuffs.lua
@@ -150,7 +150,7 @@ local function CheckPriority(newPriority, oldPriority, newAura, oldAura)
 	if not newPriority then
 		return -- debuff_data missing priority
 	elseif newPriority == oldPriority and newAura and oldAura then
-		return newAura.auraInstanceID or 0 > oldAura.auraInstanceID or 0
+		return (newAura.auraInstanceID or 0) > (oldAura.auraInstanceID or 0)
 	else -- we sort by Aura Instance ID if they match priority
 		return newPriority > oldPriority
 	end
