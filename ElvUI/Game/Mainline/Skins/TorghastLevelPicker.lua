4eadd103a128e65c5a04e63d9a80f8a59be859ba
Simpy <kodi03466@hotmail.com>
2026-09-26 12:10:11 -0400

    skin: torghast layer select

--- ElvUI/Game/Mainline/Skins/TorghastLevelPicker.lua
+++ ElvUI/Game/Mainline/Skins/TorghastLevelPicker.lua
@@ -4,6 +4,17 @@ local S = E:GetModule('Skins')
 local _G = _G
 local hooksecurefunc = hooksecurefunc
 
+local function UpdateHighestAvailableLayer(page)
+	for layer in page.gossipOptionsPool:EnumerateActive() do
+		if not layer.IsSkinned then
+			layer.SelectedBorder:SetAtlas('charactercreate-ring-select')
+			layer.SelectedBorder:Size(120)
+			layer.SelectedBorder:Point('CENTER')
+			layer.IsSkinned = true
+		end
+	end
+end
+
 function S:Blizzard_TorghastLevelPicker()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.torghastLevelPicker) then return end
 
@@ -15,16 +26,7 @@ function S:Blizzard_TorghastLevelPicker()
 	S:HandleNextPrevButton(frame.Pager.NextPage)
 	S:HandleButton(frame.OpenPortalButton)
 
-	hooksecurefunc(frame, 'ScrollAndSelectHighestAvailableLayer', function(page)
-		for layer in page.gossipOptionsPool:EnumerateActive() do
-			if not layer.IsSkinned then
-				layer.SelectedBorder:SetAtlas('charactercreate-ring-select')
-				layer.SelectedBorder:Size(120)
-				layer.SelectedBorder:Point('CENTER')
-				layer.IsSkinned = true
-			end
-		end
-	end)
+	hooksecurefunc(frame, 'ScrollAndSelectHighestAvailableLayer', UpdateHighestAvailableLayer)
 end
 
 S:AddCallbackForAddon('Blizzard_TorghastLevelPicker')
