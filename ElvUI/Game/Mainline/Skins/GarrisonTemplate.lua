642317858faa95360fd88f823d68bea86ccc4943
Simpy <kodi03466@hotmail.com>
2026-09-26 12:32:35 -0400

    skin: garrison template

--- ElvUI/Game/Mainline/Skins/GarrisonTemplate.lua
+++ ElvUI/Game/Mainline/Skins/GarrisonTemplate.lua
@@ -5,26 +5,28 @@ local _G = _G
 local hooksecurefunc = hooksecurefunc
 local C_Garrison_GetFollowerInfo = C_Garrison.GetFollowerInfo
 
+local function ShowFollower(s, followerID)
+	local followerInfo = followerID and C_Garrison_GetFollowerInfo(followerID)
+	if not followerInfo then return end
+
+	if not s.PortraitFrameStyled then
+		S:HandleGarrisonPortrait(s.PortraitFrame)
+		s.PortraitFrameStyled = true
+	end
+
+	local r, g, b = E:GetItemQualityColor(followerInfo.quality or 1)
+
+	s.Name:SetVertexColor(r, g, b)
+	s.PortraitFrame.Portrait.backdrop:SetBackdropBorderColor(r, g, b)
+
+	s.XPBar:ClearAllPoints()
+	s.XPBar:Point('BOTTOMLEFT', s.PortraitFrame, 'BOTTOMRIGHT', 7, -15)
+end
+
 function S:Blizzard_GarrisonTemplates()
 	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.orderhall or not E.private.skins.blizzard.garrison then return end
 
-	hooksecurefunc(_G.GarrisonFollowerTabMixin, 'ShowFollower', function(s, followerID)
-		local followerInfo = followerID and C_Garrison_GetFollowerInfo(followerID)
-		if not followerInfo then return end
-
-		if not s.PortraitFrameStyled then
-			S:HandleGarrisonPortrait(s.PortraitFrame)
-			s.PortraitFrameStyled = true
-		end
-
-		local r, g, b = E:GetItemQualityColor(followerInfo.quality or 1)
-
-		s.Name:SetVertexColor(r, g, b)
-		s.PortraitFrame.Portrait.backdrop:SetBackdropBorderColor(r, g, b)
-
-		s.XPBar:ClearAllPoints()
-		s.XPBar:Point('BOTTOMLEFT', s.PortraitFrame, 'BOTTOMRIGHT', 7, -15)
-	end)
+	hooksecurefunc(_G.GarrisonFollowerTabMixin, 'ShowFollower', ShowFollower)
 end
 
 S:AddCallbackForAddon('Blizzard_GarrisonTemplates')
