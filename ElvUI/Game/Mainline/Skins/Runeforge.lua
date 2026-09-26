b7d87cdf5f02c200051997bc33f53533513b34a8
Simpy <kodi03466@hotmail.com>
2026-09-26 12:32:40 -0400

    skin: runeforge

--- ElvUI/Game/Mainline/Skins/Runeforge.lua
+++ ElvUI/Game/Mainline/Skins/Runeforge.lua
@@ -4,35 +4,35 @@ local S = E:GetModule('Skins')
 local _G = _G
 local hooksecurefunc = hooksecurefunc
 
+local function RefreshListDisplay(list)
+	if not list.elements then return end -- Blizzard bails while hidden, before the list is initialized
+
+	for i = 1, list:GetNumElementFrames() do
+		local button = list.elements[i]
+		if not button.IsSkinned then
+			button.Border:SetAlpha(0)
+			button.CircleMask:Hide()
+			S:HandleIcon(button.Icon, true)
+
+			button.IsSkinned = true
+		end
+	end
+end
+
 function S:Blizzard_RuneforgeUI()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.runeforge) then return end
 
 	local frame = _G.RuneforgeFrame
 	frame.Title:FontTemplate(nil, 22)
 	S:HandleCloseButton(frame.CloseButton)
-
 	S:HandleButton(frame.CreateFrame.CraftItemButton)
 
 	local powerFrame = frame.CraftingFrame.PowerFrame
-
 	local pageControl = powerFrame.PageControl
 	S:HandleNextPrevButton(pageControl.BackwardButton)
 	S:HandleNextPrevButton(pageControl.ForwardButton)
 
-	hooksecurefunc(powerFrame.PowerList, 'RefreshListDisplay', function(list)
-		if not list.elements then return end -- Blizzard bails while hidden, before the list is initialized
-
-		for i = 1, list:GetNumElementFrames() do
-			local button = list.elements[i]
-			if not button.IsSkinned then
-				button.Border:SetAlpha(0)
-				button.CircleMask:Hide()
-				S:HandleIcon(button.Icon, true)
-
-				button.IsSkinned = true
-			end
-		end
-	end)
+	hooksecurefunc(powerFrame.PowerList, 'RefreshListDisplay', RefreshListDisplay)
 end
 
 S:AddCallbackForAddon('Blizzard_RuneforgeUI')
